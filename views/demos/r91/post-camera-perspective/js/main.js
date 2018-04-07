9cabe46b8f900c0b76d28632a0863dd7e543b2ce
dustinpfister <dustin.pfister@gmail.com>
2018-04-07 09:50:11 -0400

    started new demo for a blog post

--- views/demos/r91/post-camera-perspective/js/main.js
+++ views/demos/r91/post-camera-perspective/js/main.js
@@ -4,10 +4,8 @@
     // a scene is needed to place objects in
     var scene = new THREE.Scene(),
 
-    // I will need an camera to look at objects in the scene
-    // https://en.wikipedia.org/wiki/Viewing_frustum
-    // THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far)
-    camera = new THREE.PerspectiveCamera(50, 320 / 240, 1, 1000),
+    // for this demo I will have an array of cameras
+    cam = new THREE.PerspectiveCamera(45, 16 / 9, 1, 1000),
 
     // I will need a geometry, in this case BoxGeometery
     geometry = new THREE.BoxGeometry(200, 200, 200),
@@ -15,7 +13,7 @@
     // I will need a material for the cube
     material = new THREE.MeshBasicMaterial({
             color: 0xff0000,
-            wireframe: true
+            wireframe: false
         });
 
     // I need a mesh that will tie a geometry and material together
@@ -32,11 +30,18 @@
     // initialize method
     var init = function () {
 
+        // setting position of the camera
+        // position is a property of Object3D
+        // and the value is an instance of Vector3
+        cam.position.set(350, 350, 350);
+
+        // lookAt is also a method of Object3D
+        cam.lookAt(0, 0, 0);
+
         scene.add(mesh);
-        camera.position.z = 250;
-        camera.position.x = 250;
-        camera.lookAt(0, 0, 0);
-        renderer.setSize(320, 240);
+
+        // 16:9 aspect ratio canvas
+        renderer.setSize(320, 180);
 
     },
 
@@ -55,8 +60,8 @@
         if (now - lt >= fr) {
 
             // changing field of view from 25 to 74 degrees
-            camera.fov = Math.floor(25 + 50 * bias);
-            camera.updateProjectionMatrix();
+            cam.aspect = 16 / 9;
+            cam.updateProjectionMatrix();
 
             i += 1;
             i = i % iMax;
@@ -73,7 +78,7 @@
         requestAnimationFrame(loop);
 
         update();
-        renderer.render(scene, camera);
+        renderer.render(scene, cam);
 
     };
 
