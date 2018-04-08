767b174bc775f192b49eef95dc83bc0e66506b7e
dustinpfister <dustin.pfister@gmail.com>
2018-04-08 18:03:16 -0400

    demo clean up

--- views/demos/r91/material-lambert/js/main.js
+++ views/demos/r91/material-lambert/js/main.js
@@ -5,35 +5,24 @@
     var scene = new THREE.Scene(),
 
     // I will need an camera to look at objects in the scene
-    camera = new THREE.PerspectiveCamera(50, 320 / 240, 1, 3000),
-
-    // I will need a geometry, in this case BoxGeometery
-    geometry = new THREE.BoxGeometry(200, 200, 200),
-
-    // I will need a material for the cube
-    lambert = new THREE.MeshLambertMaterial({
-            color: 0xff0000
-        });
+    camera = new THREE.PerspectiveCamera(50, 320 / 240, 1, 3000);
 
     // I need a mesh that will tie a geometry and material together
-    cube = new THREE.Mesh(geometry, lambert),
-    target = new THREE.Mesh(
-            new THREE.BoxGeometry(10, 10, 10),
-            new THREE.MeshBasicMaterial({
-                color: 0xffffff
-            })),
+    var cube = new THREE.Mesh(
+            new THREE.BoxGeometry(200, 200, 200),
+            new THREE.MeshLambertMaterial({
+                color: 0xff0000
+            }));
+    cube.position.set(0, 100, 0);
+    scene.add(cube);
 
     // In order to see anything I will also need a renderer
     // to use with my scene, and camera
-    renderer = new THREE.WebGLRenderer();
+    var renderer = new THREE.WebGLRenderer();
 
     // I must append the dom element used by the renderer to the html
     // that I am using.
     document.getElementById('demo').appendChild(renderer.domElement);
-
-    cube.position.set(0, 100, 0);
-    scene.add(cube);
-    scene.add(target);
 
     // background
     scene.background = new THREE.Color(0x000000);
@@ -51,26 +40,29 @@
     // spotlight
     var spotLight = new THREE.SpotLight(0xffffff);
     spotLight.position.set(0, 350, 0);
-    spotLight.castShadow = true;
     scene.add(spotLight);
+
+    // light box for the spot light
+    // so I know where it is
+    var lightBox = new THREE.Mesh(
+            new THREE.BoxGeometry(10, 10, 10),
+            new THREE.MeshBasicMaterial({
+                color: 0xffffff
+            }));
+    scene.add(lightBox);
 
     // set the position of the camera away from the cube, and
     // look at the origin.
     camera.position.set(500, 500, 500);
     camera.lookAt(0, 0, 0);
     renderer.setSize(320, 240);
-    //renderer.physicallyCorrectLights = true;
-
-    //renderer.render(scene, camera);
-
 
     var frame = 0,
     maxFrame = 100;
-
-    var loop = function () {
+    loop = function () {
 
         var per = frame / maxFrame,
-        bias = 1- Math.abs(.5 - per) / .5,
+        bias = 1 - Math.abs(.5 - per) / .5,
 
         r = Math.PI * 2 * per,
         x = Math.cos(r) * 300,
@@ -79,7 +71,7 @@
 
         requestAnimationFrame(loop);
 
-        target.position.set(x, z, y);
+        lightBox.position.set(x, z, y);
         spotLight.position.set(x, z, y);
         renderer.render(scene, camera);
 
