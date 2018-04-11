32cba8c3358c65597cdbc14e835967c1763eea29
dustinpfister <dustin.pfister@gmail.com>
2018-04-11 16:04:30 -0400

    basic spotlight demo clean up

--- views/demos/r91/spotlight/js/main.js
+++ views/demos/r91/spotlight/js/main.js
@@ -1,13 +1,17 @@
 
 (function () {
 
-    // a scene is needed to place objects in
-    var scene = new THREE.Scene(),
+    // Scene
+    var scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x0f0f0f);
 
-    // I will need an camera to look at objects in the scene
-    camera = new THREE.PerspectiveCamera(50, 320 / 240, 1, 3000);
+    // CAMERA
+    var camera = new THREE.PerspectiveCamera(50, 320 / 240, 1, 3000);
+    camera.position.set(500, 500, 500);
+    camera.lookAt(0,0,0);
 
-    // I need a mesh that will tie a geometry and material together
+    // A MESH with Lambert Material
+    // which responds to a light source.
     var cube = new THREE.Mesh(
             new THREE.BoxGeometry(200, 200, 200),
             new THREE.MeshLambertMaterial({
@@ -16,35 +20,14 @@
     cube.position.set(0, 100, 0);
     scene.add(cube);
 
-    // set up a render
-    var renderer = new THREE.WebGLRenderer();
-
-    // I must append the dom element used by the renderer to the html
-    // that I am using.
-    document.getElementById('demo').appendChild(renderer.domElement);
-
-    // background
-    scene.background = new THREE.Color(0x0f0f0f);
-
-    // spotlight
+    // SPOTLIGHT
     var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(0, 350, 0);
+    spotLight.position.set(350, 340, 170);
     scene.add(spotLight);
 
-    // add a cube that will serve at showing me where
-    // the spotlight is
-    spotLight.add(new THREE.Mesh(
-            new THREE.BoxGeometry(10, 10, 10),
-            new THREE.MeshBasicMaterial({
-                color: 0xffffff
-            })));
-
-    spotLight.position.set(350, 340, 170);
-
-    // set the position of the camera away from the cube, and
-    // look at the cube.
-    camera.position.set(500, 500, 500);
-    camera.lookAt(cube.position);
+    // RENDER
+    var renderer = new THREE.WebGLRenderer();
+    document.getElementById('demo').appendChild(renderer.domElement);
     renderer.setSize(320, 240);
 
     // render what we have
