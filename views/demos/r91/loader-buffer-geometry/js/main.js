ebab4d29b563cb716cb0c6c57c7ebc7168e50710
dustinpfister <dustin.pfister@gmail.com>
2018-04-12 10:02:59 -0400

    loader-buffer-geometry clean up

--- views/demos/r91/loader-buffer-geometry/js/main.js
+++ views/demos/r91/loader-buffer-geometry/js/main.js
@@ -5,8 +5,8 @@
     var scene = new THREE.Scene();
 
     // Camera
-    var camera = new THREE.PerspectiveCamera(75, 320 / 240, .5, 1000);
-    camera.position.set(3, 3, 3);
+    var camera = new THREE.PerspectiveCamera(65, 4 / 3, .5, 10);
+    camera.position.set(-1.5, 2.5, 1.5);
     camera.lookAt(0, 0, 0);
 
     // Render
@@ -14,7 +14,7 @@
     renderer.setSize(320, 240);
     document.getElementById('demo').appendChild(renderer.domElement);
 
-    // instantiate a loader
+    // Loader
     var loader = new THREE.BufferGeometryLoader();
 
     // load a resource
@@ -25,56 +25,38 @@
         // onLoad callback
         function (geometry) {
 
-        console.log(geometry);
+            // create a mesh with the geometry
+            // and a material, and add it to the scene
+            var mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({}));
+            scene.add(mesh);
 
-        var mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({
-                    //color: 0xff0000,
-                    //wireframe: true
-                }));
-
-        scene.add(mesh);
-
-        var frame = 0,
-        maxFrame = 500;
-
-        var loop = function () {
-
-            var per = frame / maxFrame,
-
-            r = Math.PI * 2 * per;
-
-            mesh.rotation.set(0, r, 0);
-
-            requestAnimationFrame(loop);
-
+            // render the scene
             renderer.render(scene, camera);
 
-            frame += 1;
-            frame = frame % maxFrame;
-
-        };
-
-        loop();
-
-    });
-
-    /*
-    var loader = new THREE.JSONLoader();
-
-    loader.load(
-
-    'loader-json/js/three_1.json',
-
-    function(){
-
-    console.log('foo?');
-
-    },
-    function(){},
-    function(){}
+        }
 
     );
-     */
 
-}
-    ());
+}());
+
+/*
+var frame = 0,
+maxFrame = 500;
+
+var loop = function () {
+
+var per = frame / maxFrame,
+
+r = Math.PI * 2 * per;
+
+mesh.rotation.set(0, r, 0);
+
+requestAnimationFrame(loop);
+
+renderer.render(scene, camera);
+
+frame += 1;
+frame = frame % maxFrame;
+
+};
+*/
