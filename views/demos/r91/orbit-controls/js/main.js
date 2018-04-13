d6ef766af457bc6bafe04ec03de864348ab71b11
dustinpfister <dustin.pfister@gmail.com>
2018-04-13 18:53:07 -0400

    orbit controls clean up

--- views/demos/r91/orbit-controls/js/main.js
+++ views/demos/r91/orbit-controls/js/main.js
@@ -1,54 +1,39 @@
 
 (function () {
-
+ 
     // Scene
     var scene = new THREE.Scene();
-
-    // CAMERA
-    var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
-    camera.position.set(250, 200, 250);
+ 
+    // Camera
+    var camera = new THREE.PerspectiveCamera(45, 4/3, .5, 100);
+    camera.position.set(2, 2, 2);
     camera.lookAt(0, 0, 0);
-
-    // add controls for the camera
+ 
+    // Orbit Controls
     var controls = new THREE.OrbitControls(camera);
 
-    // I will need a geometry, in this case BoxGeometery
-    var geometry = new THREE.BoxGeometry(200, 200, 200);
-
-    // I will need a material for the cube
-    var material = new THREE.MeshBasicMaterial({
+    // Something to look at
+    scene.add(new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1), 
+        new THREE.MeshBasicMaterial({
             color: 0xff0000,
             wireframe: true
-        });
-
-    // I need a mesh that will tie a geometry and material together
-    var mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-
-    // In order to see anything I will also need a renderer
-    // to use with my scene, and camera
+        })));
+ 
+    // Render
     var renderer = new THREE.WebGLRenderer();
-
-    // I must append the dom element used by the renderer to the html
-    // that I am using.
+    renderer.setSize(320, 240);
     document.getElementById('demo').appendChild(renderer.domElement);
 
-    renderer.setSize(320, 240);
-
-    // finally I call renderer.render to draw the current
-    // state of the scene, from the perspective of the camera
+    // loop
     function animate() {
-
+ 
         requestAnimationFrame(animate);
-
-        // required if controls.enableDamping or controls.autoRotate are set to true
         controls.update();
-
         renderer.render(scene, camera);
-
+ 
     };
-
+ 
     animate();
-
-}
-    ());
+ 
+}());
