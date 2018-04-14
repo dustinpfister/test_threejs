64518e3845e09eb39a59bd6568b66d0297806f66
dustinpfister <dustin.pfister@gmail.com>
2018-04-13 20:31:09 -0400

    orbit controls

--- views/demos/r91/orbit-controls/js/main.js
+++ views/demos/r91/orbit-controls/js/main.js
@@ -1,25 +1,42 @@
 
 (function () {
- 
+
     // Scene
     var scene = new THREE.Scene();
- 
+
     // Camera
-    var camera = new THREE.PerspectiveCamera(45, 4/3, .5, 100);
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
     camera.position.set(2, 2, 2);
     camera.lookAt(0, 0, 0);
- 
+
     // Orbit Controls
     var controls = new THREE.OrbitControls(camera);
+    //controls.autoRotate = true;
+    //controls.autoRotateSpeed = 8;
+    //controls.target = new THREE.Vector3(.5, .5, .5);
 
+    //  horizontally angle control
+    //controls.minAzimuthAngle = -Math.PI / 4;
+    //controls.maxAzimuthAngle = Math.PI / 4;
+
+    // vertical angle control
+    //controls.minPolarAngle = -Math.PI / 2;
+    //controls.maxPolarAngle = Math.PI / 2;
+	
+	//controls.enableDamping = true;
+	//controls.dampingFactor = .2;
+
+	console.log(controls.panningMode);
+	//controls.panningMode = 1;
+	
     // Something to look at
     scene.add(new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1), 
-        new THREE.MeshBasicMaterial({
-            color: 0xff0000,
-            wireframe: true
-        })));
- 
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({
+                color: 0xff0000,
+                wireframe: true
+            })));
+
     // Render
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(320, 240);
@@ -27,13 +44,14 @@
 
     // loop
     function animate() {
- 
+
         requestAnimationFrame(animate);
         controls.update();
         renderer.render(scene, camera);
- 
+
     };
- 
+
     animate();
- 
-}());
+
+}
+    ());
