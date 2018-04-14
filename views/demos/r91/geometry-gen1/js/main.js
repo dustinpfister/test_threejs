559a2260e1e93badbbe1eec43e5ed4540849e0d0
dustinpfister <dustin.pfister@gmail.com>
2018-04-14 14:03:56 -0400

    genPanes function

--- views/demos/r91/geometry-gen1/js/main.js
+++ views/demos/r91/geometry-gen1/js/main.js
@@ -1,6 +1,60 @@
 
 (function () {
 
+    var genPanes = function (count) {
+
+        var geometry = new THREE.Geometry(),
+
+        offset = 4,
+        i = 0,
+        x = 0,
+        y = 0,
+        z = 0;
+
+        count = count || 6;
+        while (z < count) {
+
+            y = 0;
+            while (y < 2) {
+
+                x = 0;
+                while (x < 2) {
+
+                    geometry.vertices.push(new THREE.Vector3(x, y, z));
+
+                    x += 1;
+
+                }
+
+                y += 1;
+
+            }
+
+            z += 1;
+
+        }
+
+        i = 0;
+        offset = 0;
+        while (i < count) {
+
+            offset = i * 4;
+
+            geometry.faces.push(
+                new THREE.Face3(0 + offset, 1 + offset, 2 + offset),
+                new THREE.Face3(3 + offset, 2 + offset, 1 + offset));
+            i += 1;
+        }
+
+        // compute Normals
+        geometry.computeVertexNormals();
+        // normalize the geometry
+        geometry.normalize();
+
+        return geometry;
+
+    };
+
     // SCENE
     var scene = new THREE.Scene();
 
@@ -9,55 +63,10 @@
     camera.position.set(3, 3, 3);
     camera.lookAt(0, 0, 0);
 
-    var geometry = new THREE.Geometry();
-
-    var count = 6,
-    x = 0,
-    y = 0,
-    z = 0;
-    while (z < count) {
-
-        y = 0;
-        while (y < 2) {
-
-            x = 0;
-            while (x < 2) {
-
-                geometry.vertices.push(new THREE.Vector3(x, y, z));
-
-                x += 1;
-
-            }
-
-            y += 1;
-
-        }
-
-        z += 1;
-
-    }
-
-    var i = 0,
-    offset = 0;
-    while (i < count) {
-
-        offset = i * 4;
-
-        geometry.faces.push(
-            new THREE.Face3(0 + offset, 1 + offset, 2 + offset),
-            new THREE.Face3(3 + offset, 2 + offset, 1 + offset));
-        i += 1;
-    }
-
-    // compute Normals
-    geometry.computeVertexNormals();
-    // normalize the geometry
-    geometry.normalize();
-
     // MESH with Geometry, and Basic Material
     scene.add(new THREE.Mesh(
 
-            geometry,
+            genPanes(),
 
             // Material
             new THREE.MeshNormalMaterial({
