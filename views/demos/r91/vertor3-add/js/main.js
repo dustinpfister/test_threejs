22b3d8dbb1bcb66744f001b868fea4e95ad40b03
dustinpfister <dustin.pfister@gmail.com>
2018-04-15 16:49:35 -0400

    vector add demo

---
+++ views/demos/r91/vertor3-add/js/main.js
@@ -0,0 +1,55 @@
+
+(function () {
+
+    // SCENE
+    var scene = new THREE.Scene();
+
+    // CAMERA
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(1.5, 1.5, 1.5);
+    camera.lookAt(0, 0, 0);
+
+    // Orbit Controls
+    var controls = new THREE.OrbitControls(camera);
+
+    // drawing a square with lines
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(0, 0, -1),
+        new THREE.Vector3(1, 0, -1),
+        new THREE.Vector3(1, 0, 0),
+        new THREE.Vector3(0, 0, 0));
+    var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({
+                color: 0x0000ff
+            }));
+    scene.add(line);
+
+    var cube = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({
+                color: 0xffffff,
+                wireframe: true
+            }));
+
+    cube.scale.set(.1, .1, .1);
+    scene.add(cube);
+
+    // RENDER
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(320, 240);
+    document.getElementById('demo').appendChild(renderer.domElement);
+
+    var loop = function () {
+
+        requestAnimationFrame(loop);
+
+        controls.update();
+
+        renderer.render(scene, camera);
+
+    };
+
+    loop();
+}
+    ());
