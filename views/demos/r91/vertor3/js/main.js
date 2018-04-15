14a09bbe8c5d323d4bc5951a43d3f91db6179cf9
dustinpfister <dustin.pfister@gmail.com>
2018-04-15 16:35:09 -0400

    done with basic vector demo

--- views/demos/r91/vertor3/js/main.js
+++ views/demos/r91/vertor3/js/main.js
@@ -6,7 +6,7 @@
 
     // CAMERA
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
-    camera.position.set(1.5, 1.5, 2);
+    camera.position.set(-1, 1, 1);
     camera.lookAt(0, 0, 0);
 
     // Orbit Controls
@@ -18,12 +18,23 @@
     var geometry = new THREE.Geometry();
 
     geometry.vertices.push(a, b);
+    geometry.normalize();
 
     var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({
                 color: 0x0000ff
             }));
 
     scene.add(line);
+
+    var cube = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({
+                color: 0xffffff,
+                wireframe: true
+            }));
+
+    cube.scale.set(.1,.1,.1);
+    scene.add(cube);
 
     // RENDER
     var renderer = new THREE.WebGLRenderer();
