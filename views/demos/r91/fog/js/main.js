3c87f3a9665f8bdeff509f37fa966dc96ca6e6fc
dustinpfister <dustin.pfister@gmail.com>
2018-04-16 15:25:31 -0400

    fog demo does not use orbit controls

--- views/demos/r91/fog/js/main.js
+++ views/demos/r91/fog/js/main.js
@@ -3,16 +3,18 @@
 
     // Scene
     var scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xffffff);
-    scene.fog = new THREE.Fog(0xffffff, 0.0025,15);
+    fogColor = new THREE.Color(0xffffff);
+
+    scene.background = fogColor;
+    scene.fog = new THREE.Fog(fogColor, 0.0025, 10);
 
     // Camera
     var camera = new THREE.PerspectiveCamera(75, 320 / 240, .025, 20);
-    camera.position.set(5, 5, 5);
+    camera.position.set(1, 1, 1);
     camera.lookAt(0, 0, 0);
 
     // Orbit Controls
-    var controls = new THREE.OrbitControls(camera);
+    //var controls = new THREE.OrbitControls(camera);
 
     // Geometry
     var geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -20,7 +22,7 @@
     // Material
     var material = new THREE.MeshLambertMaterial({
             color: 0xff0000,
-            emissive : 0x000000
+            emissive: 0x000000
         });
 
     // Mesh
@@ -33,13 +35,23 @@
     document.getElementById('demo').appendChild(renderer.domElement);
 
     // Loop
-    var loop = function () {
+    var frame = 0,
+    maxFrame = 500,
+    loop = function () {
+
+        var per = frame / maxFrame,
+        bias = Math.abs(.5 - per) / .5;
 
         requestAnimationFrame(loop);
 
-        controls.update();
+        camera.position.z = 1 * 14 * bias;
+        camera.lookAt(0, 0, 0);
+        //controls.update();
 
         renderer.render(scene, camera);
+
+        frame += 1;
+        frame = frame % maxFrame;
 
     };
 
