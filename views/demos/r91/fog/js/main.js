f4a9d1dd3e231ff38b0caffe49af24f626b911fc
dustinpfister <dustin.pfister@gmail.com>
2018-04-16 11:57:57 -0400

    fog example working

--- views/demos/r91/fog/js/main.js
+++ views/demos/r91/fog/js/main.js
@@ -3,22 +3,24 @@
 
     // Scene
     var scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
+    scene.fog = new THREE.Fog(0xffffff, 0.0025,15);
 
     // Camera
-    var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
-    camera.position.set(250, 200, 250);
+    var camera = new THREE.PerspectiveCamera(75, 320 / 240, .025, 20);
+    camera.position.set(5, 5, 5);
     camera.lookAt(0, 0, 0);
 
     // Orbit Controls
     var controls = new THREE.OrbitControls(camera);
 
     // Geometry
-    var geometry = new THREE.BoxGeometry(200, 200, 200);
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
 
     // Material
-    var material = new THREE.MeshNormalMaterial({
-            //color: 0xff0000,
-            //wireframe: true
+    var material = new THREE.MeshLambertMaterial({
+            color: 0xff0000,
+            emissive : 0x000000
         });
 
     // Mesh
