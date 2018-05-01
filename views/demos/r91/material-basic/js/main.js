f992e572a7ddb74dabae0e65c235857382489482
dustinpfister <dustin.pfister@gmail.com>
2018-05-01 10:14:57 -0400

    basic material

---
+++ views/demos/r91/material-basic/js/main.js
@@ -0,0 +1,33 @@
+
+(function () {
+
+    // SCENE
+    var scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
+
+    // CAMERA
+    var camera = new THREE.PerspectiveCamera(50, 320 / 240, 1, 3000);
+    camera.position.set(2, 2, 2);
+    camera.lookAt(0, 0, 0);
+
+    // CUBE
+    var cube = new THREE.Mesh(
+
+            // box geometry
+            new THREE.BoxGeometry(1, 1, 1),
+
+            // basic material
+            new THREE.MeshBasicMaterial({
+                color: 0xff0000
+            }));
+    scene.add(cube);
+
+    // RENDER
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(320, 240);
+    document.getElementById('demo').appendChild(renderer.domElement);
+
+    renderer.render(scene, camera);
+
+}
+    ());
