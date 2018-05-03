3e0752a85b5020c252134e86293128b96cbf8423
dustinpfister <dustin.pfister@gmail.com>
2018-05-03 08:11:05 -0400

    scene demo

---
+++ views/demos/r91/scene/js/main.js
@@ -0,0 +1,42 @@
+
+(function () {
+
+    // Scene
+    var scene = new THREE.Scene();
+    scene.background = THREE.Color(0xffffff);
+    //scene.position.set(0, 1.5, 0);
+
+    // Camera
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
+    camera.position.set(2, 2, 2);
+    camera.lookAt(0, 0, 0);
+
+    // Orbit Controls
+    var controls = new THREE.OrbitControls(camera);
+
+    // Something to look at
+    scene.add(new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({
+                color: 0xff0000,
+                wireframe: true
+            })));
+
+    // Render
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(320, 240);
+    document.getElementById('demo').appendChild(renderer.domElement);
+
+    // loop
+    function animate() {
+
+        requestAnimationFrame(animate);
+        controls.update();
+        renderer.render(scene, camera);
+
+    };
+
+    animate();
+
+}
+    ());
