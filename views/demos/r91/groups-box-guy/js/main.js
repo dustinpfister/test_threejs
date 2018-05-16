f6e670242ed417d34eab50227fc010ef2cc3c1d3
dustinpfister <dustin.pfister@gmail.com>
2018-05-16 14:31:50 -0400

    it's not snowflake

--- views/demos/r91/groups-box-guy/js/main.js
+++ views/demos/r91/groups-box-guy/js/main.js
@@ -5,8 +5,8 @@
     var scene = new THREE.Scene();
 
     // Camera
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 20);
-    camera.position.set(4, 4, 4);
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 100);
+    camera.position.set(10, 10, 10);
 
     // Orbit Controls
     var controls = new THREE.OrbitControls(camera);
@@ -14,9 +14,29 @@
 
     scene.add(guy.group);
 
-    // SPOTLIGHT
-    var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(5, 5, 5);
+    // floor
+    var floorGeometry = new THREE.PlaneGeometry(100, 100, 8, 8);
+
+    var floor = new THREE.Mesh(
+
+            floorGeometry,
+
+            new THREE.MeshStandardMaterial({
+
+                color: 0x00afaf,
+                emissive: 0x001a1a,
+                transparent: true,
+                opacity: .5,
+                side: THREE.DoubleSide
+
+            }));
+
+    floor.rotation.set(Math.PI / 2, 0, 0)
+    scene.add(floor);
+
+    // PointLIGHT
+    var light = new THREE.PointLight(0xffffff);
+    light.position.set(0, 20, -20);
     scene.add(spotLight);
 
     // Render
@@ -26,11 +46,12 @@
 
     // loop
     var frame = 0,
-    maxFrame = 100;
+    maxFrame = 200;
     var loop = function () {
 
         var per = frame / maxFrame,
-        bias = Math.abs(.5 - per) / .5;
+        bias = Math.abs(.5 - per) / .5,
+        r = Math.PI * 2 * per;
 
         requestAnimationFrame(loop);
         renderer.render(scene, camera);
@@ -39,6 +60,11 @@
         guy.moveArm('arm_left', per, 0);
         guy.moveHead(per);
         guy.moveLegs(per);
+
+        guy.group.position.y = .5 - bias;
+        guy.group.position.x = Math.cos(r) * 5;
+        guy.group.position.z = Math.sin(r) * 5;
+        guy.group.lookAt(0, 0, 0);
 
         frame += 1;
         frame = frame % maxFrame;
