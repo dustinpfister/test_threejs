87e87a519a88a08d5a0503aab9c8a83120182d2b
dustinpfister <dustin.pfister@gmail.com>
2018-05-17 16:43:31 -0400

    cool model in the works here

--- views/demos/r91/camera-orthographic/js/main.js
+++ views/demos/r91/camera-orthographic/js/main.js
@@ -12,11 +12,11 @@
             width,
             height,
             height * -1,
-            .1,
+            .01,
             100);
 
     // set to same position, and look at the origin
-    camera.position.set(1, 2, 2);
+    camera.position.set(4, 4, 4);
     camera.lookAt(0, 0, 0);
 
     // set zoom
@@ -27,6 +27,8 @@
     var light = new THREE.PointLight();
     light.position.set(0, 2, 0);
     camera.add(light);
+
+    new THREE.OrbitControls(camera);
 
     // make the camera part of the scene
     scene.add(camera);
@@ -56,8 +58,12 @@
     scene.add(plane);
 
     // place some boxes on the plane
-    var boxCount = 5,
-    box
+    var boxCount = 30,
+    box,
+    x,
+    y,
+    z,
+    boxArray = [],
     boxIndex = 0;
     while (boxIndex < boxCount) {
 
@@ -72,11 +78,30 @@
 
                 }));
 
+        x = Math.floor(5 * Math.random());
+        y = 0;
+        z = Math.floor(5 * Math.random());
+
+        if (boxArray[z] === undefined) {
+
+            boxArray[z] = [];
+
+        }
+
+        if (boxArray[z][x] === undefined) {
+
+            boxArray[z][x] = [];
+
+        }
+
+        boxArray[z][x].push(box);
+        y = boxArray[z][x].length - 1;
+
         box.position.set(
 
-            -2 + Math.floor(5 * Math.random()),
-            0,
-            -2 + Math.floor(5 * Math.random()));
+            -2 + x,
+            y,
+            -2 + z);
         scene.add(box);
 
         boxIndex += 1;
@@ -87,7 +112,15 @@
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
 
-    renderer.render(scene, camera);
+    var loop = function () {
+
+        requestAnimationFrame(loop);
+
+        renderer.render(scene, camera);
+
+    };
+
+    loop();
 
 }
     ());
