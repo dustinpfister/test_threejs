9043e1191014b418f84eca8c9de87768b9600cf9
dustinpfister <dustin.pfister@gmail.com>
2018-05-17 16:50:38 -0400

    cube stack model

---
+++ views/demos/r91/camera-orthographic/js/cube_stack.js
@@ -0,0 +1,165 @@
+
+var CubeStack = (function () {
+
+    var Stack = function () {
+
+        var boxCount = 15,
+        box,
+        x,
+        y,
+        z,
+        plane,
+        boxArray = [],
+        boxIndex = 0;
+
+        this.group = new THREE.Group();
+
+        plane = new THREE.Mesh(
+                // plane geometry
+                new THREE.PlaneGeometry(5, 5, 5, 5),
+                // materials
+                [
+                    new THREE.MeshStandardMaterial({
+                        color: 0x00ff00,
+                        emissive: 0x0a0a0a,
+                        side: THREE.DoubleSide
+                    }),
+                    new THREE.MeshStandardMaterial({
+                        color: 0x0000ff,
+                        emissive: 0x0a0a0a,
+                        side: THREE.DoubleSide
+                    })
+                ]);
+        plane.position.set(0,  - .5, 0);
+        plane.rotation.set(Math.PI / 2, 0, 0);
+        plane.geometry.faces.forEach(function (face, i) {
+            face.materialIndex = i % 2;
+        });
+        this.group.add(plane);
+
+        // place some boxes on the plane
+        while (boxIndex < boxCount) {
+
+            box = new THREE.Mesh(
+
+                    new THREE.BoxGeometry(1, 1, 1),
+
+                    new THREE.MeshStandardMaterial({
+
+                        color: 0x00ffff,
+                        emissive: 0x0a0a0a
+
+                    }));
+
+            x = Math.floor(5 * Math.random());
+            y = 0;
+            z = Math.floor(5 * Math.random());
+
+            if (boxArray[z] === undefined) {
+
+                boxArray[z] = [];
+
+            }
+
+            if (boxArray[z][x] === undefined) {
+
+                boxArray[z][x] = [];
+
+            }
+
+            boxArray[z][x].push(box);
+            y = boxArray[z][x].length - 1;
+
+            box.position.set(
+
+                -2 + x,
+                y,
+                -2 + z);
+            this.group.add(box);
+
+            boxIndex += 1;
+
+        }
+
+    };
+
+    return Stack;
+
+    /*
+    // Plane
+    var plane = new THREE.Mesh(
+    // plane geometry
+    new THREE.PlaneGeometry(5, 5, 5, 5),
+    // materials
+    [
+    new THREE.MeshStandardMaterial({
+    color: 0x00ff00,
+    emissive: 0x0a0a0a,
+    side: THREE.DoubleSide
+    }),
+    new THREE.MeshStandardMaterial({
+    color: 0x0000ff,
+    emissive: 0x0a0a0a,
+    side: THREE.DoubleSide
+    })
+    ]);
+    plane.position.set(0,  - .5, 0);
+    plane.rotation.set(Math.PI / 2, 0, 0);
+    plane.geometry.faces.forEach(function (face, i) {
+    face.materialIndex = i % 2;
+    });
+    scene.add(plane);
+
+    // place some boxes on the plane
+    var boxCount = 30,
+    box,
+    x,
+    y,
+    z,
+    boxArray = [],
+    boxIndex = 0;
+    while (boxIndex < boxCount) {
+
+    box = new THREE.Mesh(
+
+    new THREE.BoxGeometry(1, 1, 1),
+
+    new THREE.MeshStandardMaterial({
+
+    color: 0x00ffff,
+    emissive: 0x0a0a0a
+
+    }));
+
+    x = Math.floor(5 * Math.random());
+    y = 0;
+    z = Math.floor(5 * Math.random());
+
+    if (boxArray[z] === undefined) {
+
+    boxArray[z] = [];
+
+    }
+
+    if (boxArray[z][x] === undefined) {
+
+    boxArray[z][x] = [];
+
+    }
+
+    boxArray[z][x].push(box);
+    y = boxArray[z][x].length - 1;
+
+    box.position.set(
+
+    -2 + x,
+    y,
+    -2 + z);
+    scene.add(box);
+
+    boxIndex += 1;
+    }
+     */
+
+}
+    ());
--- views/demos/r91/camera-orthographic/js/main.js
+++ views/demos/r91/camera-orthographic/js/main.js
@@ -33,79 +33,9 @@
     // make the camera part of the scene
     scene.add(camera);
 
-    // Plane
-    var plane = new THREE.Mesh(
-            // plane geometry
-            new THREE.PlaneGeometry(5, 5, 5, 5),
-            // materials
-            [
-                new THREE.MeshStandardMaterial({
-                    color: 0x00ff00,
-                    emissive: 0x0a0a0a,
-                    side: THREE.DoubleSide
-                }),
-                new THREE.MeshStandardMaterial({
-                    color: 0x0000ff,
-                    emissive: 0x0a0a0a,
-                    side: THREE.DoubleSide
-                })
-            ]);
-    plane.position.set(0,  - .5, 0);
-    plane.rotation.set(Math.PI / 2, 0, 0);
-    plane.geometry.faces.forEach(function (face, i) {
-        face.materialIndex = i % 2;
-    });
-    scene.add(plane);
+    var stack = new CubeStack();
 
-    // place some boxes on the plane
-    var boxCount = 30,
-    box,
-    x,
-    y,
-    z,
-    boxArray = [],
-    boxIndex = 0;
-    while (boxIndex < boxCount) {
-
-        box = new THREE.Mesh(
-
-                new THREE.BoxGeometry(1, 1, 1),
-
-                new THREE.MeshStandardMaterial({
-
-                    color: 0x00ffff,
-                    emissive: 0x0a0a0a
-
-                }));
-
-        x = Math.floor(5 * Math.random());
-        y = 0;
-        z = Math.floor(5 * Math.random());
-
-        if (boxArray[z] === undefined) {
-
-            boxArray[z] = [];
-
-        }
-
-        if (boxArray[z][x] === undefined) {
-
-            boxArray[z][x] = [];
-
-        }
-
-        boxArray[z][x].push(box);
-        y = boxArray[z][x].length - 1;
-
-        box.position.set(
-
-            -2 + x,
-            y,
-            -2 + z);
-        scene.add(box);
-
-        boxIndex += 1;
-    }
+    scene.add(stack.group);
 
     // Render
     var renderer = new THREE.WebGLRenderer();
