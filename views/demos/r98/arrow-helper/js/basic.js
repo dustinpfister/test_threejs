1eb846e506d947cdf08e330b9e83092a02ac2084
dustinpfister <dustin.pfister@gmail.com>
2018-11-10 08:27:25 -0500

    new main

--- views/demos/r98/arrow-helper/js/basic.js
+++ views/demos/r98/arrow-helper/js/basic.js
@@ -7,7 +7,7 @@ var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
 camera.position.set(2, 2, 2);
 camera.lookAt(0, 0, 0);
 
-var arrow = new THREE.ArrowHelper(
+scene.add(new THREE.ArrowHelper(
         // first argument is the direction
         new THREE.Vector3(2, 2, 0).normalize(),
         // second argument is the orgin
@@ -15,11 +15,7 @@ var arrow = new THREE.ArrowHelper(
         // length
         2.5,
         // color
-        0x00ff00);
-
-scene.add(arrow);
-
-console.log(arrow);
+        0x00ff00));
 
 // RENDERER
 var renderer = new THREE.WebGLRenderer();
@@ -27,23 +23,4 @@ renderer.setSize(320, 240);
 
 //renderer.physicallyCorrectLights = true;
 document.getElementById('demo').appendChild(renderer.domElement);
-
-var frame = 0,
-maxFrame = 50;
-var loop = function () {
-
-    requestAnimationFrame(loop);
-
-    var per = frame / maxFrame;
-
-
-    //arrow.setDirection(dir);
-
-    renderer.render(scene, camera);
-
-    frame += 1;
-    frame %= maxFrame;
-
-};
-
-loop();
+renderer.render(scene, camera);
