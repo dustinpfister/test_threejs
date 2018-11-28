0c743c399e56882219300022e5f879772cc61562
dustinpfister <dustin.pfister@gmail.com>
2018-11-27 19:23:42 -0500

    loop example

--- views/demos/r98/webglrenderer-loop/js/main.js
+++ views/demos/r98/webglrenderer-loop/js/main.js
@@ -16,12 +16,32 @@
     camera.lookAt(0, 0, 0);
 
     // add something to the scene
-    scene.add(new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1), new THREE.MeshBasicMaterial({
+    var cube = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1), new THREE.MeshBasicMaterial({
                 color: 0xff0000
-            })));
+            }));
+    scene.add(cube);
 
     // render the scene with the camera
-    renderer.render(scene, camera);
+    var frame = 0,
+    frameMax = 50;
+
+    var loop = function () {
+
+        requestAnimationFrame(loop);
+
+        var per = frame / frameMax,
+        a = Math.PI * 2 * per;
+
+        cube.rotation.y = a;
+
+        renderer.render(scene, camera);
+
+        frame += 1;
+        frame %= frameMax;
+
+    };
+
+    loop();
 
 }
     ());
