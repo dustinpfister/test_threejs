c3bb11bce0b409542e230a9efb9f56dd6c69e968
dustinpfister <dustin.pfister@gmail.com>
2019-05-28 15:39:34 -0400

    changing lightProbe intensity

--- views/demos/r104/light-probe/js/main.js
+++ views/demos/r104/light-probe/js/main.js
@@ -4,28 +4,22 @@ var scene = new THREE.Scene();
 
 // CAMERA
 var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
-camera.position.set(25, 25, 25);
+camera.position.set(20, 20, 20);
 camera.lookAt(0, 0, 0);
-// MESH
-var mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(20, 20, 20),
-        new THREE.MeshStandardMaterial({
-            color: 0xff0000
-        }));
-scene.add(mesh);
-mesh.position.set(0, 0, 60);
-camera.lookAt(mesh.position);
 
 // add AmbientLight
-var light = new THREE.AmbientLight(0xffffff);
-scene.add(light);
+//var light = new THREE.AmbientLight(0xffffff);
+//scene.add(light);
 
 var pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(50, 50, 50)
 scene.add(pointLight);
 
 var lightProbe = new THREE.LightProbe();
-console.log(mesh);
+
 scene.add(lightProbe);
+
+console.log(lightProbe);
 
 // LOAD CUBE TEXTURE
 new THREE.CubeTextureLoader()
@@ -47,8 +41,23 @@ new THREE.CubeTextureLoader()
 
     scene.background = cubeTexture;
 
-    console.log(THREE.LightProbeGenerator)
-    console.log(lightProbe.copy);
+    lightProbe.copy(new THREE.LightProbeGenerator.fromCubeTexture(cubeTexture));
+
+    console.log(lightProbe);
+
+    // MESH
+    var mesh = new THREE.Mesh(
+            new THREE.BoxGeometry(20, 20, 20),
+            new THREE.MeshStandardMaterial({
+                color: 0xff0000,
+                metalness: 0,
+                roughness: 0,
+                envMap: cubeTexture,
+                envMapIntensity: 1
+            }));
+    scene.add(mesh);
+    mesh.position.set(0, 0, 0);
+    camera.lookAt(mesh.position);
 
     // RENDERER
     var renderer = new THREE.WebGLRenderer();
@@ -57,5 +66,19 @@ new THREE.CubeTextureLoader()
     //renderer.physicallyCorrectLights = true;
     document.getElementById('demo').appendChild(renderer.domElement);
     renderer.render(scene, camera);
+
+    var loop = function () {
+
+        setTimeout(loop, 1000);
+
+		var r = Math.random().toFixed(2);
+		
+		console.log(r);
+		
+        lightProbe.intensity = r;
+
+    };
+
+    loop();
 
 });
