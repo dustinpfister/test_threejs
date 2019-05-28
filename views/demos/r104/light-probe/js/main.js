8ce54a66f2dd3d8ce42a9a110a1e58e2bf790aaa
dustinpfister <dustin.pfister@gmail.com>
2019-05-28 14:19:15 -0400

    added r104 and started light probe demo

---
+++ views/demos/r104/light-probe/js/main.js
@@ -0,0 +1,35 @@
+
+// SCENE
+var scene = new THREE.Scene();
+
+// CAMERA
+var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
+camera.position.set(25, 25, 25);
+camera.lookAt(0,0,0);
+// MESH
+var mesh = new THREE.Mesh(
+        new THREE.BoxGeometry(20, 20, 20),
+        new THREE.MeshStandardMaterial({
+            color: 0xff0000
+        }));
+scene.add(mesh);
+mesh.position.set(0,0,60);
+camera.lookAt(mesh.position);
+
+// add AmbientLight
+var light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
+
+var pointLight = new THREE.PointLight(0xffffff);
+scene.add(pointLight);
+
+var lightProbe = new THREE.LightProbe();
+scene.add(lightProbe);
+
+// RENDERER
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(320, 240);
+
+//renderer.physicallyCorrectLights = true;
+document.getElementById('demo').appendChild(renderer.domElement);
+renderer.render(scene, camera);
