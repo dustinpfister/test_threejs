97f159c9c3ca9af866aa00108cd4886af244237f
dustinpfister <dustin.pfister@gmail.com>
2019-05-28 15:25:01 -0400

    added cube texture

--- views/demos/r104/light-probe/js/main.js
+++ views/demos/r104/light-probe/js/main.js
@@ -5,7 +5,7 @@ var scene = new THREE.Scene();
 // CAMERA
 var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
 camera.position.set(25, 25, 25);
-camera.lookAt(0,0,0);
+camera.lookAt(0, 0, 0);
 // MESH
 var mesh = new THREE.Mesh(
         new THREE.BoxGeometry(20, 20, 20),
@@ -13,7 +13,7 @@ var mesh = new THREE.Mesh(
             color: 0xff0000
         }));
 scene.add(mesh);
-mesh.position.set(0,0,60);
+mesh.position.set(0, 0, 60);
 camera.lookAt(mesh.position);
 
 // add AmbientLight
@@ -24,14 +24,38 @@ var pointLight = new THREE.PointLight(0xffffff);
 scene.add(pointLight);
 
 var lightProbe = new THREE.LightProbe();
+console.log(mesh);
 scene.add(lightProbe);
 
-console.log(lightProbe.copy);
+// LOAD CUBE TEXTURE
+new THREE.CubeTextureLoader()
+.setPath('/img/cube/skybox/')
+.load(
 
-// RENDERER
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(320, 240);
+    // urls of images used in the cube texture
+    [
+        'px.jpg',
+        'nx.jpg',
+        'py.jpg',
+        'ny.jpg',
+        'pz.jpg',
+        'nz.jpg'
+    ],
 
-//renderer.physicallyCorrectLights = true;
-document.getElementById('demo').appendChild(renderer.domElement);
-renderer.render(scene, camera);
+    // what to do when loading is over
+    function (cubeTexture) {
+
+    scene.background = cubeTexture;
+
+    console.log(THREE.LightProbeGenerator)
+    console.log(lightProbe.copy);
+
+    // RENDERER
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(320, 240);
+
+    //renderer.physicallyCorrectLights = true;
+    document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+
+});
