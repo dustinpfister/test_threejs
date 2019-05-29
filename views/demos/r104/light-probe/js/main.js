ba43a813e2d9b84ae8d1346171737c37977180e0
dustinpfister <dustin.pfister@gmail.com>
2019-05-28 21:47:43 -0400

    light probe movement

--- views/demos/r104/light-probe/js/main.js
+++ views/demos/r104/light-probe/js/main.js
@@ -2,19 +2,20 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 1000);
 camera.position.set(25, 25, 25);
 camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer({antialias: true});
+var renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
 renderer.setSize(320, 240);
 renderer.gammaOutput = true;
 renderer.gammaFactor = 2.2; // approximate sRGB
 document.getElementById('demo').appendChild(renderer.domElement);
 
 var pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(0, 50, 0)
+pointLight.position.set(0, 50, 0);
 scene.add(pointLight);
 
 // LIGHT PROBE
 var lightProbe = new THREE.LightProbe();
-lightProbe.position.set(0,25,0);
 scene.add(lightProbe);
 
 new THREE.CubeTextureLoader()
@@ -29,13 +30,13 @@ new THREE.CubeTextureLoader()
         'nz.jpg'
     ],
     function (cubeTexture) {
-        cubeTexture.encoding = THREE.sRGBEncoding;
-        scene.background = cubeTexture;
+    cubeTexture.encoding = THREE.sRGBEncoding;
+    scene.background = cubeTexture;
 
-        // Using the lightProbe copy method with LightPropeGen
-        lightProbe.copy(new THREE.LightProbeGenerator.fromCubeTexture(cubeTexture));
+    // Using the lightProbe copy method with LightPropeGen
+    lightProbe.copy(new THREE.LightProbeGenerator.fromCubeTexture(cubeTexture));
 
-        var mesh = new THREE.Mesh(
+    var mesh = new THREE.Mesh(
             new THREE.SphereBufferGeometry(20, 32, 32),
             new THREE.MeshStandardMaterial({
                 color: 0x0000af,
@@ -44,20 +45,27 @@ new THREE.CubeTextureLoader()
                 envMap: cubeTexture,
                 envMapIntensity: 1
             }));
-        scene.add(mesh);
-        var frame = 0,
-        maxFrame = 250;
-        var loop = function () {
-            setTimeout(loop, 33);
-            var per = frame / maxFrame,
-            bias = 1 - Math.abs(0.5 - per) / 0.5;
-            frame += 1;
-            frame %= maxFrame;
+    scene.add(mesh);
+    var frame = 0,
+    maxFrame = 250;
+    var loop = function () {
+        setTimeout(loop, 33);
+        var per = frame / maxFrame,
+        bias = 1 - Math.abs(0.5 - per) / 0.5;
+        frame += 1;
+        frame %= maxFrame;
 
-            // Change Light Probe intensity
-            lightProbe.intensity = bias;
+        // Change Light Probe intensity
+        lightProbe.intensity = bias;
 
-            renderer.render(scene, camera);
-        };
-        loop();
+        // Change the light probe position
+        var radian = Math.PI * 2 * per;
+        pointLight.position.set(
+            Math.cos(radian) * 50,
+            Math.sin(radian) * 50,
+            0);
+
+        renderer.render(scene, camera);
+    };
+    loop();
 });
