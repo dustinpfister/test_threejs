afe82c538d1aa68b4555b9e9b1d37fb24d32892f
dustinpfister <dustin.pfister@gmail.com>
2019-06-02 14:44:32 -0400

    add cube method

--- views/demos/r104/point-light/js/main.js
+++ views/demos/r104/point-light/js/main.js
@@ -1,7 +1,7 @@
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 1000);
-camera.position.set(25, 25, 25);
+camera.position.set(0, 40, 40);
 camera.lookAt(0, 0, 0);
 
 var renderer = new THREE.WebGLRenderer({
@@ -11,20 +11,27 @@ renderer.setSize(320, 240);
 document.getElementById('demo').appendChild(renderer.domElement);
 
 var pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(20, 30, 40);
+pointLight.position.set(0, 0, 0);
+pointLight.add(new THREE.Mesh(
+                new THREE.SphereGeometry(1, 10, 10),
+                new THREE.MeshBasicMaterial({
+                    color: 0xffffff
+                })));
 scene.add(pointLight);
 
-// I will need a geometry, in this case BoxGeometery
-var geometry = new THREE.BoxGeometry(20, 20, 20),
+var addCube = function (scene, size, x, y, z) {
+    var geometry = new THREE.BoxGeometry(size, size, size),
+    material = new THREE.MeshStandardMaterial({
+            color: 0xff0000
+        });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    scene.add(mesh);
+};
 
-// I will need a material for the cube
-material = new THREE.MeshStandardMaterial({
-        color: 0xff0000
-    });
-
-// I need a mesh that will tie a geometry and material together
-mesh = new THREE.Mesh(geometry, material);
-
-scene.add(mesh);
+addCube(scene, 10, 15, 0, 0);
+addCube(scene, 10, -15, 0, 0);
+addCube(scene, 10, 0, -15, 0);
+addCube(scene, 10, 0, 0, -15);
 
 renderer.render(scene, camera);
