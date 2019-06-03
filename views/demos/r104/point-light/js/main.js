28a9e7d2fa3c6894feb8df9e66b6ff7048988a68
dustinpfister <dustin.pfister@gmail.com>
2019-06-03 11:37:30 -0400

    comments

--- views/demos/r104/point-light/js/main.js
+++ views/demos/r104/point-light/js/main.js
@@ -1,6 +1,5 @@
 var scene = new THREE.Scene();
-
-// create the point light and add it to the scene
+// create some point lights and add it to the scene
 var addPointLight = function (scene, color, x, y, z) {
     var pointLight = new THREE.PointLight(color);
     pointLight.position.set(x, y, z);
@@ -12,17 +11,16 @@ var addPointLight = function (scene, color, x, y, z) {
     scene.add(pointLight);
     return pointLight;
 };
-
-var whitePointLight = addPointLight(scene, 0xffffff, 0, 0, 0);
-var redPointLight = addPointLight(scene, 0xff0000, 30, 0, 0);
-var greenPointLight = addPointLight(scene, 0x00ff00, 0, 30, 0);
-var bluePointLight = addPointLight(scene, 0x0000ff, 0, 0, 30);
-
-// create some cubs
+var whitePointLight = addPointLight(scene, 0xffffff, 0, 0, 0),
+redPointLight = addPointLight(scene, 0xff0000, 30, 0, 0),
+greenPointLight = addPointLight(scene, 0x00ff00, 0, 30, 0),
+bluePointLight = addPointLight(scene, 0x0000ff, 0, 0, 30);
+// create some cubes
 var addCube = function (scene, size, x, y, z) {
     var geometry = new THREE.BoxGeometry(size, size, size),
     material = new THREE.MeshStandardMaterial({
-            color: 0xffffff
+            color: 0xffffff,
+            emissive: 0x0f0f0f
         });
     mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(x, y, z);
@@ -32,42 +30,36 @@ addCube(scene, 10, 15, 0, 0);
 addCube(scene, 10, -15, 0, 0);
 addCube(scene, 10, 0, 0, 15);
 addCube(scene, 10, 0, 0, -15);
-
+// need a camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 1000);
 camera.position.set(37, 37, 37);
 camera.lookAt(0, 0, 0);
-
 var renderer = new THREE.WebGLRenderer({
         antialias: true
     });
 renderer.setSize(320, 240);
 document.getElementById('demo').appendChild(renderer.domElement);
-
+// loop
 var frame = 0,
 maxFrame = 180,
 per,
 bias,
-y,
 loop = function () {
-
     setTimeout(loop, 33);
-
     per = frame / maxFrame;
     bias = 1 - Math.abs(0.5 - per) / 0.5,
     r = Math.PI * 2 * per,
     sin = Math.sin(r) * 30,
     cos = Math.cos(r) * 30;
-
+    // update point lights
     whitePointLight.position.y = 20 * bias;
-
-    redPointLight.position.set(cos,sin,0);
-    greenPointLight.position.set(cos,0,sin);
-    bluePointLight.position.set(0,cos,sin);
-
+    redPointLight.position.set(cos, sin, 0);
+    greenPointLight.position.set(cos, 0, sin);
+    bluePointLight.position.set(0, cos, sin);
+    // render
     renderer.render(scene, camera);
-
+    // step frame
     frame += 1;
     frame %= maxFrame;
-
 };
 loop();
