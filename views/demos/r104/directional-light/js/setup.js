aa6b67d56428c5b36aa66d9f4d424633d534ce54
dustinpfister <dustin.pfister@gmail.com>
2019-06-04 12:31:00 -0400

    directional light folder

--- views/demos/r104/directional-light/js/setup.js
+++ views/demos/r104/directional-light/js/setup.js
@@ -3,7 +3,7 @@ var scene = new THREE.Scene();
 
 // CAMERA
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 1000);
-camera.position.set(0, 0, -2);
+camera.position.set(1.25, 1.25, 1.25);
 camera.lookAt(0, 0, 0);
 
 // RENDER
@@ -11,21 +11,9 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(320, 240);
 document.getElementById('demo').appendChild(renderer.domElement);
 
-// FACE 3
-var geometry = new THREE.Geometry();
-geometry.vertices = [
-    new THREE.Vector3(0, 0, 0),
-    new THREE.Vector3(0, 1, 0),
-    new THREE.Vector3(1, 1, 0)
-];
-geometry.faces.push(new THREE.Face3(0, 1, 2, new THREE.Vector3(0, 0, 1), new THREE.Color(0x00ff00), 0));
-geometry.computeVertexNormals();
-geometry.computeFaceNormals();
-
-var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
-            side: THREE.DoubleSide,
-            vertexColors: THREE.FaceColors
-        }));
-scene.add(mesh);
+// Something in the scene
+scene.add(new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshStandardMaterial({color:0xff0000,emissive: 0x0a0a0a})));
 
 renderer.render(scene, camera);
