2b16f954871211676c70d1c8230c249b05a131f4
dustinpfister <dustin.pfister@gmail.com>
2019-06-05 13:26:26 -0400

    move light demo

--- views/demos/r104/directional-light-move/js/setup.js
+++ views/demos/r104/directional-light-move/js/setup.js
@@ -1,40 +1,35 @@
 // SCENE
 var scene = new THREE.Scene();
+
 // directional light
 var dl = new THREE.DirectionalLight(0xffffff, 1);
 scene.add(dl);
-// CAMERA
+
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 1000);
-camera.position.set(1.2, 1.2, 1.2);
+camera.position.set(10, 15, 10);
 camera.lookAt(0, 0, 0);
-// RENDER
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(320, 240);
 document.getElementById('demo').appendChild(renderer.domElement);
-// Something in the scene
-var mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshStandardMaterial({
-            color: 0xff0000,
-            emissive: 0x0a0a0a
-        }));
+var material = new THREE.MeshStandardMaterial({color: 0xff0000,emissive: 0x0a0a0a});
+var mesh = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2),material);
+mesh.position.y=2;
 scene.add(mesh);
+var plane = new THREE.Mesh(new THREE.PlaneGeometry(12, 12, 8), material);
+plane.rotation.set(-Math.PI / 2, 0, 0);
+scene.add(plane)
 
 var frame = 0,
 maxFrame = 100;
 var loop = function () {
-
     setTimeout(loop, 33);
-
     var per = frame / maxFrame,
     r = Math.PI * 2 * per;
 
-    dl.position.set(Math.cos(r), 1, Math.sin(r));
+    // change directional light position
+    dl.position.set(Math.cos(r)*10, 2, Math.sin(r)*10);
 
     frame = (frame + 1) % maxFrame;
-
-    // render
     renderer.render(scene, camera);
-
 };
 loop();
