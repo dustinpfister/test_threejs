a6229406b69089e3ab58d8c7df20e8c7892ff4ef
dustinpfister <dustin.pfister@gmail.com>
2019-06-05 11:14:38 -0400

    directional light clean up

--- views/demos/r104/directional-light/js/setup.js
+++ views/demos/r104/directional-light/js/setup.js
@@ -1,9 +1,14 @@
 // SCENE
 var scene = new THREE.Scene();
 
+// directional light
+var dl = new THREE.DirectionalLight(0xffffff, 1);
+dl.position.set(1,2,3);
+scene.add(dl);
+
 // CAMERA
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 1000);
-camera.position.set(3.25, 3.25, 3.25);
+camera.position.set(1.2, 1.2, 1.2);
 camera.lookAt(0, 0, 0);
 
 // RENDER
@@ -11,9 +16,6 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(320, 240);
 document.getElementById('demo').appendChild(renderer.domElement);
 
-var dl = new THREE.DirectionalLight(0xffffff, 1);
-dl.position.set(1,0.5,0.25)
-scene.add(dl);
 
 // Something in the scene
 var mesh = new THREE.Mesh(
@@ -23,8 +25,6 @@ var mesh = new THREE.Mesh(
             emissive: 0x0a0a0a
         }));
 scene.add(mesh);
-mesh.position.set(2,1,0)
 
-dl.target = mesh;
-
+// render
 renderer.render(scene, camera);
