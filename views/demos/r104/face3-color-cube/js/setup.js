80047ecd28992990b029eef5ba5888c5c19f7898
dustinpfister <dustin.pfister@gmail.com>
2019-06-05 15:45:33 -0400

    set face3 color of box geometry

---
+++ views/demos/r104/face3-color-cube/js/setup.js
@@ -0,0 +1,31 @@
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(1, 1.2, 1);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(320, 240);
+document.getElementById('demo').appendChild(renderer.domElement);
+
+// a box geometry
+var box = new THREE.BoxGeometry(1, 1, 1);
+// for each face3
+box.faces.forEach(function (face3, i) {
+    if (i % 2) {
+        // vertex colors array
+        face3.vertexColors = [
+            new THREE.Color(0xff0000),
+            new THREE.Color(0x00ff00),
+            new THREE.Color(0x0000ff)];
+    } else {
+        // or just face color
+        face3.color = new THREE.Color(0xffffff);
+    }
+});
+
+var mesh = new THREE.Mesh(box,
+        new THREE.MeshBasicMaterial({
+            vertexColors: THREE.FaceColors
+        }));
+scene.add(mesh);
+renderer.render(scene, camera);
