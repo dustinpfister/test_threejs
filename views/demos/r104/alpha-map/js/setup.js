409a567ceecf2758be3e465bd514c8c5631202b0
dustinpfister <dustin.pfister@gmail.com>
2019-06-06 21:25:28 -0400

    comments

--- views/demos/r104/alpha-map/js/setup.js
+++ views/demos/r104/alpha-map/js/setup.js
@@ -10,10 +10,9 @@ document.getElementById('demo').appendChild(renderer.domElement);
 
 var canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d');
-
 canvas.width = 64;
 canvas.height = 64;
-
+// drawing gray scale areas
 ctx.fillStyle = '#404040';
 ctx.fillRect(0, 0, 32, 32);
 ctx.fillStyle = '#808080';
@@ -22,16 +21,18 @@ ctx.fillStyle = '#c0c0c0';
 ctx.fillRect(0, 32, 32, 32);
 ctx.fillStyle = '#f0f0f0';
 ctx.fillRect(32, 32, 32, 32);
-
 var texture = new THREE.CanvasTexture(canvas);
 
-document.body.appendChild(canvas);
-
+// creating a mesh that is uing the Basic material
 var mesh = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshBasicMaterial({
             color: 0x00ffff,
+            // using the alpha map property to set the texture
+            // as the alpha map for the material
             alphaMap: texture,
+            // I also need to make sure the transparent
+            // property is true
             transparent: true,
             side: THREE.DoubleSide
         }));
