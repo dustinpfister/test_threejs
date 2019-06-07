2d44d37c11de57cdaa7869070c3e907ec6f3824a
dustinpfister <dustin.pfister@gmail.com>
2019-06-06 20:53:01 -0400

    started alpha map example

---
+++ views/demos/r104/alpha-map/js/setup.js
@@ -0,0 +1,15 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(1, 1.3, 1);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+renderer.setSize(320, 240);
+document.getElementById('demo').appendChild(renderer.domElement);
+
+var mesh = new THREE.Mesh(new THREE.BoxGeometry(1,1,1))
+
+scene.add(mesh);
+
+renderer.render(scene, camera);
