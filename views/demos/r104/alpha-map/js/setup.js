63b6c4a42bc99c083dd10a15334ccc5280133526
dustinpfister <dustin.pfister@gmail.com>
2019-06-06 21:48:18 -0400

    clean up

--- views/demos/r104/alpha-map/js/setup.js
+++ views/demos/r104/alpha-map/js/setup.js
@@ -2,12 +2,11 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
 camera.position.set(1, 1.3, 1);
 camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer({
-        antialias: true
-    });
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize(320, 240);
 document.getElementById('demo').appendChild(renderer.domElement);
-
+ 
+// creating a texture with canvas
 var canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d');
 canvas.width = 64;
@@ -22,7 +21,7 @@ ctx.fillRect(0, 32, 32, 32);
 ctx.fillStyle = '#f0f0f0';
 ctx.fillRect(32, 32, 32, 32);
 var texture = new THREE.CanvasTexture(canvas);
-
+ 
 // creating a mesh that is uing the Basic material
 var mesh = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
@@ -36,7 +35,5 @@ var mesh = new THREE.Mesh(
             transparent: true,
             side: THREE.DoubleSide
         }));
-
 scene.add(mesh);
-
 renderer.render(scene, camera);
