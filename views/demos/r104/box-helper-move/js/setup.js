2ad40e39776fd18b933829eb37c89d545e8e1d3c
dustinpfister <dustin.pfister@gmail.com>
2019-06-10 21:37:11 -0400

    move box

--- views/demos/r104/box-helper-move/js/setup.js
+++ views/demos/r104/box-helper-move/js/setup.js
@@ -1,16 +1,35 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(2, 2, 2);
+camera.position.set(6, 6, 6);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(320, 240);
 document.getElementById('demo').appendChild(renderer.domElement);
 
-var sphere = new THREE.SphereGeometry();
-var object = new THREE.Mesh(sphere, new THREE.MeshBasicMaterial(0xff0000));
-var box = new THREE.BoxHelper(object, 0xffff00);
-scene.add(box);
+var mesh = new THREE.Mesh(new THREE.SphereGeometry(2, 30, 30), new THREE.MeshBasicMaterial({
+            color: 0xff0000
+        }));
+var box = new THREE.BoxHelper(mesh, 0xffff00);
 
-var mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1));
+mesh.add(box);
 scene.add(mesh);
-renderer.render(scene, camera);
+
+var frame = 0, maxFrame = 48;
+var loop = function () {
+
+    setTimeout(loop, 1000 / 12);
+
+    var per = frame / maxFrame,
+    bias = 1 - Math.abs(0.5 - per) / 0.5;
+
+    // change position and rotation
+    mesh.position.z = 5 * bias;
+    mesh.rotation.y = Math.PI * per;
+
+    renderer.render(scene, camera);
+
+    frame += 1;
+    frame %= maxFrame
+
+};
+loop();
