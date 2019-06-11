4ff54a2161c055127ecc1613227c5f7ca9a459ea
dustinpfister <dustin.pfister@gmail.com>
2019-06-11 18:39:45 -0400

    basic box helper demo clean up

--- views/demos/r104/box-helper/js/setup.js
+++ views/demos/r104/box-helper/js/setup.js
@@ -9,8 +9,8 @@ document.getElementById('demo').appendChild(renderer.domElement);
 var mesh = new THREE.Mesh(new THREE.SphereGeometry(1, 30, 30), new THREE.MeshStandardMaterial({
             color: 0xff0000
         }));
-var box = new THREE.BoxHelper(mesh, 0xffffff);
-mesh.add(box);
+// add a box helper
+mesh.add(new THREE.BoxHelper(mesh, 0xffffff));
 scene.add(mesh);
 
 // light
@@ -18,6 +18,4 @@ var light = new THREE.PointLight(0xffffff);
 light.position.set(0,2,2);
 scene.add(light);
 
-var mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1));
-scene.add(mesh);
 renderer.render(scene, camera);
