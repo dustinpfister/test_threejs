ac5237ff577168b8fa33e25f2791a3bdae103408
dustinpfister <dustin.pfister@gmail.com>
2019-06-11 11:36:06 -0400

    is webgl

---
+++ views/demos/r104/is-webgl/js/setup.js
@@ -0,0 +1,46 @@
+var isWebGL = function (ctxNum) {
+    ctxNum = ctxNum === undefined ? '' : ctxNum;
+    try {
+        var canvas = document.createElement('canvas');
+        return !!(window['WebGL' + ctxNum + 'RenderingContext'] &&
+            (canvas.getContext('webgl' + ctxNum) ||
+                canvas.getContext('experimental-webgl' + ctxNum)));
+    } catch (e) {
+        return false;
+    }
+};
+
+var container = document.getElementById('demo');
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
+
+if (isWebGl()) {
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(320, 240);
+    container.appendChild(renderer.domElement);
+
+    var mesh = new THREE.Mesh(new THREE.SphereGeometry(1, 30, 30), new THREE.MeshStandardMaterial({
+                color: 0xff0000
+            }));
+    var box = new THREE.BoxHelper(mesh, 0xffffff);
+    mesh.add(box);
+    scene.add(mesh);
+
+    // light
+    var light = new THREE.PointLight(0xffffff);
+    light.position.set(0, 2, 2);
+    scene.add(light);
+
+    var mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1));
+    scene.add(mesh);
+    renderer.render(scene, camera);
+
+} else {
+
+    container.innerText = 'No WebGL 1 support'
+
+}
