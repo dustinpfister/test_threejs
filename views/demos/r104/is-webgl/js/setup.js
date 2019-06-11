726064bb1a6a36df744e6aeaeaf889f19be7806b
dustinpfister <dustin.pfister@gmail.com>
2019-06-11 11:52:19 -0400

    can use light is webgl

--- views/demos/r104/is-webgl/js/setup.js
+++ views/demos/r104/is-webgl/js/setup.js
@@ -17,26 +17,31 @@ var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
 camera.position.set(2, 2, 2);
 camera.lookAt(0, 0, 0);
 
-if (isWebGl()) {
+var mesh = new THREE.Mesh(new THREE.SphereGeometry(1, 30, 30),
+        [
+            new THREE.MeshBasicMaterial({
+                color: 0xff0000
+            }),
+            new THREE.MeshStandardMaterial({
+                color: 0xff0000
+            }),
+        ]);
+scene.add(mesh);
+
+if (isWebGL()) {
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(320, 240);
     container.appendChild(renderer.domElement);
 
-    var mesh = new THREE.Mesh(new THREE.SphereGeometry(1, 30, 30), new THREE.MeshStandardMaterial({
-                color: 0xff0000
-            }));
-    var box = new THREE.BoxHelper(mesh, 0xffffff);
-    mesh.add(box);
-    scene.add(mesh);
-
-    // light
+    // Can use light with Web GL
+    mesh.geometry.faces.forEach(function (face) {
+        face.materialIndex = 1;
+    });
     var light = new THREE.PointLight(0xffffff);
     light.position.set(0, 2, 2);
     scene.add(light);
 
-    var mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1));
-    scene.add(mesh);
     renderer.render(scene, camera);
 
 } else {
