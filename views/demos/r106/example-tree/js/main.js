4f726dfc9d8b4bd8ed497561f6808ce84b1e816f
dustinpfister <dustin.pfister@gmail.com>
2019-07-30 17:19:19 -0400

    tree example

---
+++ views/demos/r106/example-tree/js/main.js
@@ -0,0 +1,31 @@
+
+(function () {
+    // SCENE
+    var scene = new THREE.Scene();
+    // CAMERA
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(5, 5, 5);
+    camera.lookAt(0, 0, 0);
+
+    var cone = new THREE.ConeGeometry(1, 7, 32);
+    var coneMaterial = new THREE.MeshBasicMaterial({
+            color: 0x00ff00
+        });
+    scene.add(new THREE.Mesh(
+            cone,
+            coneMaterial));
+
+    // RENDER
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(320, 240);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    // CONTROLS
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    // LOOP
+    var loop = function () {
+        requestAnimationFrame(loop);
+        renderer.render(scene, camera);
+    };
+    loop();
+}
+    ());
