16b17721682dc97264ad63e90079991a9340823b
dustinpfister <dustin.pfister@gmail.com>
2019-12-19 10:14:58 -0500

    cleaner create cube stack

--- views/demos/r111/object3d/js/main.js
+++ views/demos/r111/object3d/js/main.js
@@ -1,20 +1,33 @@
 
-var createCubeStack = function () {
-    // Object3D
-    var obj = new THREE.Object3D();
-    // a mesh that inherits from Object3D
-    var low = new THREE.Mesh(
+// create cube stack method
+var createCubeStack = function (original) {
+    var stack = {},
+    original = original || new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshNormalMaterial());
-    low.position.y = -1;
-    obj.add(low);
-    var high = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshNormalMaterial());
-    high.rotation.set(0, 1, 0);
-    obj.add(high);
-    return obj;
-
+            new THREE.MeshNormalMaterial()),
+    cube;
+    stack.group = new THREE.Object3D();
+    // create cubes in the stack
+    var i = 0,
+    len = 3,
+    per;
+    while (i < len) {
+        per = i / len;
+        cube = original.clone();
+        cube.position.set(0, -len / 2 + i, 0);
+        cube.rotation.set(0, Math.PI * 2 * per, 0);
+        stack.group.add(cube)
+        i += 1;
+    }
+	stack.set = function(per){
+		
+		stack.group.children.forEach(function(cube){
+			
+			
+		});
+		
+	};
+    return stack;
 };
 
 // Scene
@@ -22,12 +35,12 @@ var scene = new THREE.Scene();
 
 // Camera
 var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-camera.position.set(3, 3, 3);
+camera.position.set(5, 5, 5);
 camera.lookAt(0, 0, 0);
 
 var stack = createCubeStack();
 
-scene.add( stack );
+scene.add(stack.group);
 
 // Render
 var renderer = new THREE.WebGLRenderer();
@@ -47,7 +60,7 @@ var loop = function () {
 
     //high.rotation.set(0, Math.PI * 2 * per, 0);
     //low.rotation.set(0, -Math.PI * 2 * per, 0);
-    stack.position.set(0, -1 + 2 * bias, 0);
+    stack.group.position.set(0, -1 + 2 * bias, 0);
 
     frame += 1;
     frame = frame % maxFrame;
