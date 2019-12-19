3660be844c058875bcd58990cfc84e7edc666e20
dustinpfister <dustin.pfister@gmail.com>
2019-12-19 12:21:55 -0500

    wireframe canvas

--- views/demos/r111/wireframe/js/main.js
+++ views/demos/r111/wireframe/js/main.js
@@ -1,4 +1,5 @@
-var createCube = function () {
+// create a basic write frame cube
+var createBasicWireCube = function () {
     return new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshBasicMaterial({
@@ -7,15 +8,41 @@ var createCube = function () {
         }));
 };
 
+// create a canvas texture
+var createCanvasTexture = function () {
+    var canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d');
+    canvas.width = 16;
+    canvas.height = 16;
+    ctx.strokeStyle = '#ffffff';
+    ctx.strokeRect(0.5, 0.5, canvas.width - 1, canvas.height - 1);
+    var texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+    return texture;
+};
+
+// create a cube with a canvas as a texture
+var createCanvasWireCube = function () {
+    var texture = createCanvasTexture()
+        return new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({
+                map: texture
+            }));
+};
+
 // Scene
 var scene = new THREE.Scene();
 
 // Camera
 var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-camera.position.set(2, 2, 2);
-camera.lookAt(0, 0, 0);
+camera.position.set(5, 5, 5);
+camera.lookAt(1, 0, 0);
 
-scene.add(createCube());
+scene.add(createBasicWireCube());
+var cube = createCanvasWireCube();
+cube.position.set(3, 0, 0)
+scene.add(cube);
 
 // Render
 var renderer = new THREE.WebGLRenderer();
