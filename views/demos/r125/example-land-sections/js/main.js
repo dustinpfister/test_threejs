5e5986cc92ca43726e40c1283967c36e29de7229
dustinpfister <dustin.pfister@gmail.com>
2021-02-15 13:03:14 -0500

    creating a section group of cubes

--- views/demos/r125/example-land-sections/js/main.js
+++ views/demos/r125/example-land-sections/js/main.js
@@ -9,13 +9,26 @@
     camera.position.set(2, 2, 2);
     camera.lookAt(0, 0, 0);
 
-    // Something to look at
-    scene.add(new THREE.Mesh(
+    var sections = new THREE.Group();
+
+    var sectionCount = 12,
+    sectionIndex = 0;
+    while(sectionIndex < sectionCount){
+        var mesh = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshBasicMaterial({
                 color: 0xff0000,
                 wireframe: true
-            })));
+            }));
+        mesh.position.x = 0;
+        mesh.position.y = 0;
+        mesh.position.z = -6 + sectionIndex * 2;
+        sections.add(mesh);
+        sectionIndex += 1;
+    }
+
+    // Something to look at
+    scene.add(sections);
 
     // Render
     var renderer = new THREE.WebGLRenderer();
