2bce37e802075ef30a84331b8b6f97f91c978f7b
dustinpfister <dustin.pfister@gmail.com>
2021-02-15 12:22:44 -0500

    example-land-sections r125 demo folder started

---
+++ views/demos/r125/example-land-sections/js/main.js
@@ -0,0 +1,38 @@
+
+(function () {
+
+    // Scene
+    var scene = new THREE.Scene();
+
+    // Camera
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
+    camera.position.set(2, 2, 2);
+    camera.lookAt(0, 0, 0);
+
+    // Something to look at
+    scene.add(new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({
+                color: 0xff0000,
+                wireframe: true
+            })));
+
+    // Render
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(320, 240);
+    document.getElementById('demo').appendChild(renderer.domElement);
+
+    // Orbit Controls The DOM element must now be given as a second argument
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    // loop
+    function animate() {
+        requestAnimationFrame(animate);
+        controls.update();
+        renderer.render(scene, camera);
+    };
+
+    animate();
+
+}
+    ());
