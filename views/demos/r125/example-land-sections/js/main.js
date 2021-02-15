a9b56fa6159ed44f10732a9df1f80bf51d334ef9
dustinpfister <dustin.pfister@gmail.com>
2021-02-15 13:34:14 -0500

    game object started

--- views/demos/r125/example-land-sections/js/main.js
+++ views/demos/r125/example-land-sections/js/main.js
@@ -1,6 +1,31 @@
 
 (function () {
 
+    var game = {
+        sun: {
+            x: 0,
+            y: 0
+        },
+        sectionDist: 25,
+        sections: []
+    };
+    var i = 0,
+    section,
+    radian,
+    sectionCount = 10;
+    while(i < sectionCount){
+         radian = Math.PI / sectionCount * i;
+         section = {
+             x: Math.cos(radian) * game.sectionDist,
+             y: Math.sin(radian) * game.sectionDist
+         };
+         game.sections.push(section);
+         i += 1;
+    }
+
+    console.log(game);
+
+
     // Scene
     var scene = new THREE.Scene();
 
@@ -12,7 +37,7 @@
     var mainGroup = Sections.create();
     scene.add(mainGroup);
 
-    Sections.setSunPos({}, mainGroup);
+    Sections.setSunPos(game, mainGroup);
 
     // Render
     var renderer = new THREE.WebGLRenderer();
