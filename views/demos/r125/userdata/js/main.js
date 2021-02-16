8c9f33d7047a701f13ca249811ed4be49bee212c
dustinpfister <dustin.pfister@gmail.com>
2021-02-16 15:43:30 -0500

    going in new direction with user data example

--- views/demos/r125/userdata/js/main.js
+++ views/demos/r125/userdata/js/main.js
@@ -19,13 +19,19 @@
             materials[0]
         );
         mesh.userData.materalIndex = 1;
+        mesh.userData.pitchPPS = 0;
+        mesh.userData.pitch = Math.PI / 180 * 270;
+        mesh.userData.heading = Math.PI / 180 * 180;
         group.add(mesh);
         return group;
     };
 
-    var updateSphereGroup = function(group){
+    var updateSphereGroup = function(group, secs){
         group.children.forEach(function(mesh){
             mesh.material = materials[mesh.userData.materalIndex];
+            mesh.position.x += Math.cos(mesh.userData.pitch) * mesh.userData.pitchPPS;
+            mesh.position.y += Math.sin(mesh.userData.pitch) * mesh.userData.pitchPPS;
+            mesh.position.z += Math.cos(mesh.userData.heading) * mesh.userData.headingPPS;
         });
     };
 
@@ -35,12 +41,12 @@
 
     // Camera
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(2, 2, 2);
+    camera.position.set(0, 0, 5);
     camera.lookAt(0, 0, 0);
 
     // create and add sphere group
     var group = createSphereGroup();
-    updateSphereGroup(group);
+    updateSphereGroup(group, 0);
     scene.add(group);
 
     // Render
@@ -52,10 +58,15 @@
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
     // loop
+    var lt = new Date();
     function animate() {
+        var now = new Date(),
+        secs = (now - lt) / 1000;
         requestAnimationFrame(animate);
         controls.update();
+        updateSphereGroup(group, secs);
         renderer.render(scene, camera);
+        lt = now;
     };
 
     animate();
