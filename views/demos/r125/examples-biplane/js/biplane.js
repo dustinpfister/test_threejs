bced10e987db9d2926f619ed597394b16492332d
dustinpfister <dustin.pfister@gmail.com>
2021-02-17 14:08:13 -0500

    create body helper

--- views/demos/r125/examples-biplane/js/biplane.js
+++ views/demos/r125/examples-biplane/js/biplane.js
@@ -3,6 +3,7 @@ var Biplane = (function () {
 
     var api = {};
 
+    // create a wing
     var createWing = function(y){
         var wing = new THREE.Mesh(
             new THREE.BoxGeometry(2,1,8),
@@ -14,9 +15,22 @@ var Biplane = (function () {
         return wing;
     };
 
+    // create a body
+    var createBody = function(){
+        var body = new THREE.Mesh(
+            new THREE.BoxGeometry(10,2,2),
+            new THREE.MeshLambertMaterial({
+                color: 0xff0000
+            })
+        );
+        body.position.x = -2;
+        return body;
+    };
+
     api.create = function(){
         var plane = new THREE.Group();
 
+        plane.add(createBody());
         plane.add(createWing(-1));
         plane.add(createWing(1));
 
