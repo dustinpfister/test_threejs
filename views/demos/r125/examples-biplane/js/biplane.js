ea8c3452ae0814159e5d3da5fa55043a03d3f36a
dustinpfister <dustin.pfister@gmail.com>
2021-02-17 14:11:07 -0500

    create tail helper

--- views/demos/r125/examples-biplane/js/biplane.js
+++ views/demos/r125/examples-biplane/js/biplane.js
@@ -27,10 +27,24 @@ var Biplane = (function () {
         return body;
     };
 
+    // create a body
+    var createTail = function(){
+        var body = new THREE.Mesh(
+            new THREE.BoxGeometry(1,2,2),
+            new THREE.MeshLambertMaterial({
+                color: 0xff0000
+            })
+        );
+        body.position.x = -6.5;
+        body.position.y = 2;
+        return body;
+    };
+
     api.create = function(){
         var plane = new THREE.Group();
 
         plane.add(createBody());
+        plane.add(createTail());
         plane.add(createWing(-1));
         plane.add(createWing(1));
 
