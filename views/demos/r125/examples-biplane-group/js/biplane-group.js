0b82d27bd054d97f9d46fd451cfc2f78c117c5c8
dustinpfister <dustin.pfister@gmail.com>
2021-02-18 16:15:52 -0500

    cool little effect so far

--- views/demos/r125/examples-biplane-group/js/biplane-group.js
+++ views/demos/r125/examples-biplane-group/js/biplane-group.js
@@ -5,6 +5,10 @@ var BiplaneGroup = (function () {
 
     var api = {};
 
+    var bias = function(per){
+        return Math.abs(0.5 - per) / 0.5;
+    };
+
     // update
     api.update = function(group, secs){
         var i = 0,
@@ -16,16 +20,18 @@ var BiplaneGroup = (function () {
             bi = group.children[i];
             radian = Math.PI * 2 / len * i;
             x = Math.cos(radian) * 10;
-            y = 0; //bi.userData.y;
+            y = -5 + 10 * bias(bi.userData.yFrame / 50);
             z = Math.sin(radian) * 10;
             bi.position.set(x,y,z);
             // make leader roll
             if(i === 0){
                 bi.rotation.set(bi.userData.r,0,0);
-                bi.userData.r += Math.PI / 180 * 45 * secs;
+                bi.userData.r += Math.PI / 180 * 180 * secs;
                 bi.userData.r %= (Math.PI * 2);
             }
             Biplane.update(bi, secs);
+            bi.userData.yFrame += 1;
+            bi.userData.yFrame %= 50;
             i += 1;
         }
     };
@@ -39,6 +45,7 @@ var BiplaneGroup = (function () {
         while(i < BIPLANE_COUNT){
             var bi = Biplane.create();
             //bi.userData.y = -5 + 10 * (i / 3);
+            bi.userData.yFrame = Math.floor(50 * (i / BIPLANE_COUNT));
             bi.userData.r = 0;
             group.add(bi);
             i += 1;
