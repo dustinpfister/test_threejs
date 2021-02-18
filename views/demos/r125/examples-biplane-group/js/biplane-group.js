d586afcf5bdf765a78be1072f9dab81e69d11de4
dustinpfister <dustin.pfister@gmail.com>
2021-02-18 15:53:53 -0500

    basic update method working

--- views/demos/r125/examples-biplane-group/js/biplane-group.js
+++ views/demos/r125/examples-biplane-group/js/biplane-group.js
@@ -3,17 +3,38 @@ var BiplaneGroup = (function () {
 
     var api = {};
 
+    // update
+    api.update = function(group, secs){
+        var i = 0,
+        bi,
+        radian,
+        x,y,z,
+        len = group.children.length;
+        while(i < len){
+            bi = group.children[i];
+            radian = Math.PI * 2 / len * i;
+            x = Math.cos(radian) * 10;
+            y = 0;
+            z = Math.sin(radian) * 10;
+            bi.position.set(x,y,z);
+            i += 1;
+        }
+    };
+
+
     // main create method
     api.create = function(opt){
         opt = opt || {};
 
         var group = new THREE.Group();
-        var bi = Biplane.create();
-        group.add(bi);
+        var i = 0;
+        while(i < 3){
+            var bi = Biplane.create();
+            group.add(bi);
+            i += 1;
+        }
+        api.update(group, 0);
         return group;
-    };
-
-    api.update = function(bi, secs){
     };
 
     return api;
