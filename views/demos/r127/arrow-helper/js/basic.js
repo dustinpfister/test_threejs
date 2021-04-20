4e66a6a5b5b09483d075d07eb5ee32a3f8d669f5
dustinpfister <dustin.pfister@gmail.com>
2021-04-20 10:56:52 -0400

    arrow-helper r127 demo folder

---
+++ views/demos/r127/arrow-helper/js/basic.js
@@ -0,0 +1,28 @@
+
+// SCENE
+var scene = new THREE.Scene();
+
+// CAMERA
+var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
+camera.position.set(0, 0, 3);
+camera.lookAt(0, 0, 0);
+
+var arrow = new THREE.ArrowHelper(
+        // first argument is the direction
+        new THREE.Vector3(2, 2, 0).normalize(),
+        // second argument is the orgin
+        new THREE.Vector3(0, 0, 0),
+        // length
+        2.2,
+        // color
+        0x00ff00);
+
+scene.add(arrow);
+
+// RENDERER
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(320, 240);
+document.getElementById('demo').appendChild(renderer.domElement);
+
+// just render once
+renderer.render(scene, camera);
---
+++ views/demos/r127/arrow-helper/js/main.js
@@ -0,0 +1,48 @@
+
+// SCENE
+var scene = new THREE.Scene();
+
+// CAMERA
+var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
+camera.position.set(0, 0, 3);
+camera.lookAt(0, 0, 0);
+
+var arrow = new THREE.ArrowHelper(
+        // first argument is the direction
+        new THREE.Vector3(2, 2, 0).normalize(),
+        // second argument is the orgin
+        new THREE.Vector3(0, 0, 0),
+        // length
+        2.2,
+        // color
+        0x00ff00);
+
+scene.add(arrow);
+
+// RENDERER
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(320, 240);
+document.getElementById('demo').appendChild(renderer.domElement);
+
+var frame = 0,
+maxFrame = 500,
+loop = function () {
+
+    requestAnimationFrame(loop);
+
+    var per = frame / maxFrame,
+    rad = Math.PI * 2 * per,
+    x = Math.cos(rad),
+    y = Math.sin(rad);
+
+    var dir = new THREE.Vector3(x, y, 0).normalize();
+    arrow.setDirection(dir);
+
+    renderer.render(scene, camera);
+
+    frame += 1;
+    frame %= maxFrame;
+
+};
+
+loop();
