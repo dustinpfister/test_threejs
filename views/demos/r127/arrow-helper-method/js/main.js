59cf4982963906c3b2abe6bb37de398db4952008
dustinpfister <dustin.pfister@gmail.com>
2021-04-20 13:19:09 -0400

    arrow for cube

--- views/demos/r127/arrow-helper-method/js/main.js
+++ views/demos/r127/arrow-helper-method/js/main.js
@@ -2,19 +2,21 @@
 // scene
 var scene = new THREE.Scene();
 
+// arrows for scene
+utils.addArrow(scene, 2, 0, 0, 3.2, 'lime');
+utils.addArrow(scene, 0, 2, 0, 2.5, 'blue');
+utils.addArrow(scene, 0, 0, 2, 2.7, 'cyan');
+
 // camera
 var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
 camera.position.set(2.5, 2.5, 2.5);
 camera.lookAt(0, 0, 0);
 
 // cube
-utils.addCube(scene);
+var cube = utils.addCube(scene, -2.5, 0, .5);
+// arrow for cube
+utils.addArrow(cube, 0, 2, 0, 2.5, 'blue');
 
-// ARROW HELPER
-utils.addArrow(scene, 2, 0, 0, 3.2, 'lime');
-utils.addArrow(scene, 0, 2, 0, 2.5, 'blue');
-utils.addArrow(scene, 0, 0, 2, 2.7, 'cyan');
-utils.addArrow(scene, -2, 2, 2, 2.7, 'white');
 
 // RENDERER
 var renderer = new THREE.WebGLRenderer();
