d02d3a53faf758778300f88d55ef3a6fab430626
dustinpfister <dustin.pfister@gmail.com>
2021-04-20 11:02:14 -0400

    clean up

--- views/demos/r127/arrow-helper/js/main.js
+++ views/demos/r127/arrow-helper/js/main.js
@@ -4,19 +4,18 @@ var scene = new THREE.Scene();
 
 // CAMERA
 var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
-camera.position.set(0, 0, 3);
+camera.position.set(3, 3, 3);
 camera.lookAt(0, 0, 0);
 
 var arrow = new THREE.ArrowHelper(
         // first argument is the direction
         new THREE.Vector3(2, 2, 0).normalize(),
-        // second argument is the orgin
+        // second argument is the origin
         new THREE.Vector3(0, 0, 0),
         // length
         2.2,
         // color
         0x00ff00);
-
 scene.add(arrow);
 
 // RENDERER
@@ -35,6 +34,7 @@ loop = function () {
     x = Math.cos(rad),
     y = Math.sin(rad);
 
+    // can change the direction
     var dir = new THREE.Vector3(x, y, 0).normalize();
     arrow.setDirection(dir);
 
