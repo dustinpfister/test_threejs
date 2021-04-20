c0486135c8573dcfa7884dd80cff7fa3808dc153
dustinpfister <dustin.pfister@gmail.com>
2021-04-20 13:16:15 -0400

    add cube method working

--- views/demos/r127/arrow-helper-method/js/main.js
+++ views/demos/r127/arrow-helper-method/js/main.js
@@ -8,21 +8,13 @@ camera.position.set(2.5, 2.5, 2.5);
 camera.lookAt(0, 0, 0);
 
 // cube
-var geometry = new THREE.BoxGeometry(2, 2, 2);
-var material = new THREE.MeshBasicMaterial({
-        color: 0xff0000,
-        wireframe: true
-    });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
+utils.addCube(scene);
 
 // ARROW HELPER
 utils.addArrow(scene, 2, 0, 0, 3.2, 'lime');
 utils.addArrow(scene, 0, 2, 0, 2.5, 'blue');
 utils.addArrow(scene, 0, 0, 2, 2.7, 'cyan');
 utils.addArrow(scene, -2, 2, 2, 2.7, 'white');
-
 
 // RENDERER
 var renderer = new THREE.WebGLRenderer();
