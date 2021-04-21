22762ee4aa11483cf2a33bce70e0c221b0f45d61
dustinpfister <dustin.pfister@gmail.com>
2021-04-21 14:01:14 -0400

    materials-transparent example

---
+++ views/demos/r127/materials-transparent/js/main.js
@@ -0,0 +1,37 @@
+
+var createCube = function (size, material, x, y, z) {
+    var geometry = new THREE.BoxGeometry(size, size, size),
+    cube = new THREE.Mesh(geometry, material);
+    cube.position.set(x, y, z);
+    return cube;
+};
+
+var materials = {};
+
+materials.sand = new THREE.MeshBasicMaterial({
+        color: 'yellow'
+    });
+
+materials.glass = new THREE.MeshBasicMaterial({
+        color: 'cyan',
+        transparent: true,
+        opacity: 0.4
+    });
+
+var glassCube = createCube(1, materials.glass, 0, 0, 2),
+cube = createCube(1, materials.sand, 0, 0, 0);
+
+// scene
+var scene = new THREE.Scene();
+
+scene.add(glassCube);
+scene.add(cube);
+
+// camera
+var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
+camera.position.set(0.9, 0, 3.5);
+camera.lookAt(-1, 0, 0);
+// RENDERER
+var renderer = new THREE.WebGLRenderer();
+document.getElementById('demo').appendChild(renderer.domElement);
+renderer.render(scene, camera);
