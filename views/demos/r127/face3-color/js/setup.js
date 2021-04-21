d91f0933912d0ffde53371e8f3f4e5ed2f4fdce4
dustinpfister <dustin.pfister@gmail.com>
2021-04-21 12:26:18 -0400

    new face3 color example

---
+++ views/demos/r127/face3-color/js/setup.js
@@ -0,0 +1,77 @@
+// SCENE
+var scene = new THREE.Scene();
+
+// CAMERA
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 1000);
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
+
+// RENDER
+var renderer = new THREE.WebGLRenderer();
+document.getElementById('demo').appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+
+var colors = new Uint8Array([
+            255, 0, 0,
+            0, 255, 0,
+            0, 0, 255,
+            0, 0, 255,
+            0, 255, 0,
+            255, 0, 0,
+
+            255, 0, 0,
+            255, 255, 0,
+            255, 0, 255,
+            255, 0, 255,
+            255, 255, 0,
+            255, 0, 0,
+
+            255, 0, 0,
+            0, 255, 0,
+            0, 0, 255,
+            0, 0, 255,
+            0, 255, 0,
+            255, 0, 0,
+
+            255, 0, 0,
+            0, 255, 0,
+            0, 0, 255,
+            0, 0, 255,
+            0, 255, 0,
+            255, 0, 0,
+
+            255, 0, 0,
+            0, 255, 0,
+            0, 0, 255,
+            0, 0, 255,
+            0, 255, 0,
+            255, 0, 0,
+
+            255, 0, 0,
+            0, 255, 0,
+            0, 0, 255,
+            0, 0, 255,
+            0, 255, 0,
+            255, 0, 0,
+
+            255, 0, 0,
+            0, 255, 0,
+            0, 0, 255,
+            0, 0, 255,
+            0, 255, 0,
+            255, 0, 0,
+
+        ]);
+
+// Don't forget to normalize the array! (third param = true)
+geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3, true));
+
+
+var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+            //side: THREE.DoubleSide,
+            vertexColors: true
+        }));
+scene.add(mesh);
+
+renderer.render(scene, camera);
