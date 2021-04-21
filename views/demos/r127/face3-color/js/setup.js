c0e72c3744f897f5803c94155ef5240ac0761cfa
dustinpfister <dustin.pfister@gmail.com>
2021-04-21 12:28:12 -0400

    new face3 color example looks good

--- views/demos/r127/face3-color/js/setup.js
+++ views/demos/r127/face3-color/js/setup.js
@@ -1,17 +1,8 @@
-// SCENE
-var scene = new THREE.Scene();
 
-// CAMERA
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 1000);
-camera.position.set(2, 2, 2);
-camera.lookAt(0, 0, 0);
-
-// RENDER
-var renderer = new THREE.WebGLRenderer();
-document.getElementById('demo').appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 
+// add a colors prop to the geometry
 var colors = new Uint8Array([
             255, 0, 0,
             0, 255, 0,
@@ -67,9 +58,16 @@ var colors = new Uint8Array([
 // Don't forget to normalize the array! (third param = true)
 geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3, true));
 
+// SCENE
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 1000);
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer();
+document.getElementById('demo').appendChild(renderer.domElement);
 
+// MESH that uses the vertex colors
 var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
-            //side: THREE.DoubleSide,
             vertexColors: true
         }));
 scene.add(mesh);
