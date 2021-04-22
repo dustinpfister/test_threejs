595653c42a4dfb121f201a0120777c68ceb9e227
dustinpfister <dustin.pfister@gmail.com>
2021-04-22 16:30:32 -0400

    cleaner basic example

--- views/demos/r127/buffer-geometry/js/main.js
+++ views/demos/r127/buffer-geometry/js/main.js
@@ -1,6 +1,23 @@
 
 (function () {
 
+    // GEOMETRY
+    var geometry = new THREE.BufferGeometry();
+    var vertices = new Float32Array([
+                0, 0, 0,
+                1, 0, 0,
+                1, 1, 0
+            ]);
+    // create position property
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+
+    // MESH with GEOMETRY, and Basic MATERIAL
+    var custom = new THREE.Mesh(
+            geometry,
+            new THREE.MeshBasicMaterial({
+                side: THREE.DoubleSide
+            }));
+
     // SCENE
     var scene = new THREE.Scene();
 
@@ -8,22 +25,8 @@
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
     camera.position.set(0, 0.5, 3);
 
-    // GEOMETRY
-    var geometry = new THREE.BufferGeometry();
-    var vertices = new Float32Array([
-                0,0,0,
-                1,0,0,
-                1,1,0
-            ]);
-    // create position property
-    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-
-    // MESH with GEOMETRY, and Normal MATERIAL
-    scene.add(new THREE.Mesh(
-            geometry,
-            new THREE.MeshBasicMaterial({
-                side: THREE.DoubleSide
-            })));
+    // add custom to the scene
+    scene.add(custom);
 
     // RENDER
     var renderer = new THREE.WebGLRenderer();
