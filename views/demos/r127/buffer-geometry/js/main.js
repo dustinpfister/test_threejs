e7782254ab8775be9a4e8c811a696c9a5a6d7d41
dustinpfister <dustin.pfister@gmail.com>
2021-04-22 15:08:19 -0400

    basic buffer geomerty example

---
+++ views/demos/r127/buffer-geometry/js/main.js
@@ -0,0 +1,34 @@
+
+(function () {
+
+    // SCENE
+    var scene = new THREE.Scene();
+
+    // CAMERA
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
+    camera.position.set(0, 0.5, 3);
+
+    // GEOMETRY
+    var geometry = new THREE.BufferGeometry();
+    var vertices = new Float32Array([
+                0,0,0,
+                1,0,0,
+                1,1,0
+            ]);
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+
+    // MESH with GEOMETRY, and Normal MATERIAL
+    scene.add(new THREE.Mesh(
+            geometry,
+            new THREE.MeshBasicMaterial({
+                side: THREE.DoubleSide
+            })));
+
+    // RENDER
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+
+}
+    ());
