168e4cf6d77e3b3bee865111f11e9aaf28cc9983
dustinpfister <dustin.pfister@gmail.com>
2021-04-22 09:48:56 -0400

    cleaner r91 example for material index

--- views/demos/r91/face3-material-index/js/main.js
+++ views/demos/r91/face3-material-index/js/main.js
@@ -1,17 +1,10 @@
 
 (function () {
 
-    // SCENE
-    var scene = new THREE.Scene();
+    // REVISION 91 was used for this example
+    console.log(THREE.REVISION);
 
-    // CAMERA
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
-    camera.position.set(2, 2, 2);
-    camera.lookAt(0, 0, 0);
-
-    // Orbit Controls
-    var controls = new THREE.OrbitControls(camera);
-
+    // an array of materials
     var materials = [
         new THREE.MeshBasicMaterial({
             color: 0xff0000
@@ -24,30 +17,35 @@
         })
     ];
 
-    // GEOMETRY
+    // create the mesh
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-
+    // set material index values
     var len = materials.length;
     geometry.faces.forEach(function (face3, i) {
         face3.materialIndex = Math.floor(i % (len * 2) / 2);
     });
-
+    // create the mesh
     var mesh = new THREE.Mesh(
-
-            // geometry as first argument
             geometry,
             materials);
 
+    // SCENE
+    var scene = new THREE.Scene();
+    // add mesh to scene
     scene.add(mesh);
-
+    // some helpers
     scene.add(new THREE.FaceNormalsHelper(mesh, 2, 0x00ff00, 1));
     scene.add(new THREE.VertexNormalsHelper(mesh, 2, 0xff0000, 1));
 
-    // RENDER
+    // camera, orbit controls, renderer
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(2, 2, 2);
+    camera.lookAt(0, 0, 0);
+    var controls = new THREE.OrbitControls(camera);
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(320, 240);
+    renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
-
+    // loop
     var loop = function () {
         requestAnimationFrame(loop);
         renderer.render(scene, camera);
