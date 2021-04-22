722a111638c754a1ce058ce6a8d8145e120e8c60
dustinpfister <dustin.pfister@gmail.com>
2021-04-22 10:05:51 -0400

    comments

--- views/demos/r127/face3-material-index/js/main.js
+++ views/demos/r127/face3-material-index/js/main.js
@@ -4,9 +4,7 @@
     // REVISION 127 was used for this example
     console.log(THREE.REVISION);
 
-    // SCENE
-    var scene = new THREE.Scene();
-
+    // ARRAY OF MATERIALS
     var materials = [
         new THREE.MeshBasicMaterial({
             color: 0xff0000
@@ -19,21 +17,24 @@
         })
     ];
 
-    // GEOMETRY
+    // geometry
     var geometry = new THREE.BoxGeometry(1, 1, 1);
 
+    // SET THE INDEX VALUES FOR EACH FACE
     geometry.groups.forEach(function (face, i) {
         face.materialIndex = Math.floor(i % materials.length);
     });
-    // MESH
+
+    // mesh
     var mesh = new THREE.Mesh(
             // geometry as first argument
             geometry,
             // array of materials as the second argument
             materials);
+    // scene, add mesh
+    var scene = new THREE.Scene();
     scene.add(mesh);
-
-    // CAMERA, RENDER
+    // camera, renderer
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
     camera.position.set(2, 2, 2);
     camera.lookAt(0, 0, 0);
