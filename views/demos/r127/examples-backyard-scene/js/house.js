12119c82d1c52670b328522c5dbbcd327369b632
dustinpfister <dustin.pfister@gmail.com>
2021-04-23 10:58:01 -0400

    trianle helper

--- views/demos/r127/examples-backyard-scene/js/house.js
+++ views/demos/r127/examples-backyard-scene/js/house.js
@@ -2,9 +2,28 @@
 
     // default materials
     var materials_default = {
-        sides: new THREE.MeshStandardMaterial({
-            color: 0xffffff
+        base: new THREE.MeshStandardMaterial({
+            color: 0xffffff,
+            side: THREE.DoubleSide
+        }),
+        tri: new THREE.MeshBasicMaterial({
+            color: 0x4a4a4a,
+            side: THREE.DoubleSide
         })
+    };
+
+    var HouseTriangle = function(materials){
+        materials = materials || materials_default;
+        var geometry = new THREE.BufferGeometry();
+        var vertices = new Float32Array([
+                -1, 0, 0,
+                0, 1, 0,
+                2, 0, 0
+            ]);
+        geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+        return new THREE.Mesh(
+            geometry, 
+            materials.tri);
     };
 
     // create and return a house
@@ -13,8 +32,16 @@
         // mian house group
         var house = new THREE.Group();
         // base of house is just a BOX
-        var base = new THREE.Mesh(new THREE.BoxGeometry(3, 2, 4), materials.sides);
+        var base = new THREE.Mesh(new THREE.BoxGeometry(3, 2, 4), materials.base);
         house.add(base);
+
+        var tri1 = HouseTriangle(materials);
+        tri1.position.set(-0.5, 1 , 2);
+        house.add(tri1);
+        var tri2 = HouseTriangle(materials);
+        tri2.position.set(-0.5, 1 , -2);
+        house.add(tri2);
+
         house.castShadow = true;
         house.receiveShadow = false;
         return house;
