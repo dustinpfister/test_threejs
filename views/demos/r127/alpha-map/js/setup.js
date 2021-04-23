5e62593abe2f6bb5e5e0f593bbdf43055c8fc132
dustinpfister <dustin.pfister@gmail.com>
2021-04-23 16:16:26 -0400

    alpha map example updated

---
+++ views/demos/r127/alpha-map/js/setup.js
@@ -0,0 +1,42 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(1, 1.3, 1);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+document.getElementById('demo').appendChild(renderer.domElement);
+ 
+// creating a texture with canvas
+var canvas = document.createElement('canvas'),
+ctx = canvas.getContext('2d');
+canvas.width = 64;
+canvas.height = 64;
+// drawing gray scale areas
+ctx.fillStyle = '#404040';
+ctx.fillRect(0, 0, 32, 32);
+ctx.fillStyle = '#808080';
+ctx.fillRect(32, 0, 32, 32);
+ctx.fillStyle = '#c0c0c0';
+ctx.fillRect(0, 32, 32, 32);
+ctx.fillStyle = '#f0f0f0';
+ctx.fillRect(32, 32, 32, 32);
+var texture = new THREE.CanvasTexture(canvas);
+ 
+// creating a mesh that is using the Basic material
+var mesh = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshBasicMaterial({
+            color: 0x00ffff,
+            // using the alpha map property to set the texture
+            // as the alpha map for the material
+            alphaMap: texture,
+            // I also need to make sure the transparent
+            // property is true
+            transparent: true,
+            // even when opacity is one the alpha map will 
+            // still effect transparency this can just be used to set it even lower
+            opacity: 1,
+            side: THREE.DoubleSide
+        }));
+scene.add(mesh);
+renderer.render(scene, camera);
