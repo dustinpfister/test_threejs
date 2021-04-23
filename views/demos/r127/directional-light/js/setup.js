773ab1b19df7f4ae888a973ce2afd1b228ec2271
dustinpfister <dustin.pfister@gmail.com>
2021-04-23 09:53:03 -0400

    comments

--- views/demos/r127/directional-light/js/setup.js
+++ views/demos/r127/directional-light/js/setup.js
@@ -1,17 +1,20 @@
-// SCENE
+// Have a scene
 var scene = new THREE.Scene();
-// directional light
+
+// ADD A DIRECTIONAL LIGHT to the scene
 var dl = new THREE.DirectionalLight(0xffffff, 1);
 scene.add(dl);
-// CAMERA
+
+// have a camera
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 1000);
 camera.position.set(1.2, 1.2, 1.2);
 camera.lookAt(0, 0, 0);
-// RENDER
+// Have a renderer
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(320, 240);
 document.getElementById('demo').appendChild(renderer.domElement);
-// Something in the scene
+// Add Something in the scene that makes use of a material
+// that will respond to light such as the standard material
 var mesh = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshStandardMaterial({
@@ -19,7 +22,5 @@ var mesh = new THREE.Mesh(
             emissive: 0x0a0a0a
         }));
 scene.add(mesh);
-// render
+// Render the scene
 renderer.render(scene, camera);
-
-console.log(dl.position);
