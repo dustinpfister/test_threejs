1bd70bb3bd771fd20ee59bdf260bd3b4ffcf30df
dustinpfister <dustin.pfister@gmail.com>
2021-04-24 15:13:19 -0400

    just chnaged size of move box helper example

--- views/demos/r127/box-helper-move/js/setup.js
+++ views/demos/r127/box-helper-move/js/setup.js
@@ -3,7 +3,7 @@ var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
 camera.position.set(6, 6, 6);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(320, 240);
+renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
 
 var mesh1 = new THREE.Mesh(
