4b19a817f0dd1c1e52c95d97e40e9de498b2b91d
dustinpfister <dustin.pfister@gmail.com>
2021-04-25 11:09:33 -0400

    moving guy and wheel

--- views/demos/r127/examples-backyard/js/main.js
+++ views/demos/r127/examples-backyard/js/main.js
@@ -3,9 +3,8 @@ scene.background = new THREE.Color(0x00ffff);
 
 // directional light
 var dl = new THREE.DirectionalLight(0xffffff, 1);
-dl.position.set(3,4,3);
+dl.position.set(3, 4, 3);
 scene.add(dl);
-
 
 // add AmbientLight
 var light = new THREE.AmbientLight(0xffffff);
@@ -20,10 +19,9 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
 
-
 // add the house
 var house = HouseMod.create();
-house.position.set(-3, 1.05, 0);
+house.position.set(-2, 1.05, 0);
 scene.add(house);
 
 // ground
@@ -36,18 +34,18 @@ var materials = {
 
 var ground = new THREE.Mesh(new THREE.BoxGeometry(12, 12, 0.25), materials.ground);
 
-ground.position.set(0,-0.075,0);
+ground.position.set(0, -0.075, 0);
 ground.rotation.set(-Math.PI / 2, 0, 0);
 scene.add(ground);
 
 var wheel = WheelMod.create();
-wheel.group.scale.set(0.5,0.5,0.5);
-wheel.group.position.set(2, 1.5, 3);
+wheel.group.scale.set(0.5, 0.5, 0.5);
+wheel.group.position.set(2, 1.5, 2);
 scene.add(wheel.group);
 
 var guy = GuyMod.create();
 guy.group.scale.set(0.25, 0.25, 0.25);
-guy.group.position.set(0,0.8,5.5);
+guy.group.position.set(0, 0.8, 5.5);
 scene.add(guy.group);
 
 // CONTROLS
@@ -61,8 +59,14 @@ var loop = function () {
     var per = frame / maxFrame,
     r = Math.PI * 2 * per;
 
+    wheel.wheel.rotation.z = r;
+
+    GuyMod.walk(guy, per, 4);
+    guy.group.position.set(Math.cos(r) * 5,0.8,Math.sin(r) * 5);
+    guy.group.rotation.set(0,1.57,0);
+
     // change directional light position
-    dl.position.set(Math.cos(r) * 5, Math.sin(r) * 5, 0);
+    //dl.position.set(Math.cos(r) * 5, Math.sin(r) * 5, 0);
     controls.update();
 
     frame = (frame + 1) % maxFrame;
