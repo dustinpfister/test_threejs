9eeaf79f6872a11e84bf323c2048d08c07e5ab58
dustinpfister <dustin.pfister@gmail.com>
2021-04-26 14:00:43 -0400

    new position for basic box-geometry demo

--- views/demos/r127/box-geometry/js/main.js
+++ views/demos/r127/box-geometry/js/main.js
@@ -12,7 +12,7 @@ scene.add(box);
  
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(2, 2, 2);
+camera.position.set(0.8, 1.3, 0.8);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
