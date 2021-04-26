acb61bc43c11607f5b528f0a3f2ce464965172bc
dustinpfister <dustin.pfister@gmail.com>
2021-04-26 13:56:12 -0400

    box-geomerty demo

---
+++ views/demos/r127/box-geometry/js/main.js
@@ -0,0 +1,20 @@
+// creating a box mesh with the Box Geometry constructor,
+// and the normal material
+var box = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshNormalMaterial());
+ 
+// creating a scene
+var scene = new THREE.Scene();
+ 
+// add the box mesh to the scene
+scene.add(box);
+ 
+// camera and renderer
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+document.getElementById('demo').appendChild(renderer.domElement);
+renderer.render(scene, camera);
