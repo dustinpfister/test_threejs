f3addacfa55c98d9bf23c077537180ebf5996590
dustinpfister <dustin.pfister@gmail.com>
2021-04-27 11:07:21 -0400

    comment

--- views/demos/r127/mesh-copy/js/main.js
+++ views/demos/r127/mesh-copy/js/main.js
@@ -21,10 +21,12 @@ scene.add(original);
 var i = 0, mesh, rad, x, z;
 while (i < 10) {
     mesh = original.clone();
+    // changes made to position and rotation to not effect original
     rad = Math.PI * 2 * (i / 10);
     x = Math.cos(rad) * 3;
     z = Math.sin(rad) * 3;
     mesh.position.set(x, 0, z);
+    mesh.lookAt(original.position);
     scene.add(mesh);
     i += 1;
 }
