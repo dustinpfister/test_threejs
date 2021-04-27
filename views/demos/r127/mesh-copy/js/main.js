5f6c517d4b9f123bf68579596dd3bfc66c0bb732
dustinpfister <dustin.pfister@gmail.com>
2021-04-27 11:05:12 -0400

    comments to mesh-copy

--- views/demos/r127/mesh-copy/js/main.js
+++ views/demos/r127/mesh-copy/js/main.js
@@ -1,25 +1,23 @@
-
-
 // SCENE
 var scene = new THREE.Scene();
 
 // CAMERA
-var camera = new THREE.PerspectiveCamera(40, 16 / 9, 0.1, 1000);
-camera.position.set(6, 6, 6);
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(5, 3, 5);
 camera.lookAt(0, 0, 0);
 
 // RENDER
 var renderer = new THREE.WebGLRenderer();
 document.getElementById('demo').appendChild(renderer.domElement);
-renderer.setSize(360, 180);
+renderer.setSize(640, 480);
 
-// MESH
+// MESH original
 var original = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
 scene.add(original);
 
-// Copy the mesh a bunch of times
+// Mesh cloned a bunch of times from original
 var i = 0, mesh, rad, x, z;
 while (i < 10) {
     mesh = original.clone();
