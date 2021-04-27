f3ac8c5727eb67e6f90831fad034e8019eb3c2b3
dustinpfister <dustin.pfister@gmail.com>
2021-04-27 14:26:25 -0400

    loop for color map example

--- views/demos/r127/standard-material-map/js/main.js
+++ views/demos/r127/standard-material-map/js/main.js
@@ -13,7 +13,6 @@
 }
     (this['utils'] = {}));
 
-
 var colorMap = utils.createCanvasTexture(function (ctx, canvas) {
         ctx.fillStyle = 'red';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -27,27 +26,51 @@ var colorMap = utils.createCanvasTexture(function (ctx, canvas) {
 // creating a box with the standard material
 var box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshStandardMaterial({ map: colorMap}));
- 
+        new THREE.MeshStandardMaterial({
+            map: colorMap
+        }));
+
 // creating a scene
 var scene = new THREE.Scene();
- 
+
 // add the box mesh to the scene
 scene.add(box);
 
 // adding a light source
 var sun = new THREE.Mesh(
-        new THREE.SphereGeometry(0.5, 40, 40),
+        new THREE.SphereGeometry(0.25, 40, 40),
         new THREE.MeshBasicMaterial());
 sun.add(new THREE.PointLight(0xffffff, 1));
 sun.position.set(2, 6, 16);
 scene.add(sun);
- 
+
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(0.8, 1.3, 0.8);
+camera.position.set(5, 5, 5);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
-renderer.render(scene, camera);
+
+var lt = new Date(),
+radian = 0;
+
+var loop = function () {
+
+    var now = new Date(),
+    secs = (now - lt) / 1000;
+    lt = now;
+
+    requestAnimationFrame(loop);
+
+    radian += 1.57 * secs;
+    radian %= Math.PI * 2;
+
+    var x = Math.cos(radian) * 5,
+    y = 2,
+    z = Math.sin(radian) * 5;
+    sun.position.set(x, y, z);
+
+    renderer.render(scene, camera);
+};
+loop();
