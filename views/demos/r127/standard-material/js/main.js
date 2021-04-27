64423fd9f0502641697fb3b81fe5dc9987cee48c
dustinpfister <dustin.pfister@gmail.com>
2021-04-27 14:08:31 -0400

    adding a standard material r127 demo

---
+++ views/demos/r127/standard-material/js/main.js
@@ -0,0 +1,27 @@
+// creating a box with the standard material
+var box = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshStandardMaterial({ color: 'red'}));
+ 
+// creating a scene
+var scene = new THREE.Scene();
+ 
+// add the box mesh to the scene
+scene.add(box);
+
+// adding a light source
+var sun = new THREE.Mesh(
+        new THREE.SphereGeometry(0.5, 40, 40),
+        new THREE.MeshBasicMaterial());
+sun.add(new THREE.PointLight(0xffffff, 1));
+sun.position.set(8, 4, 2);
+scene.add(sun);
+ 
+// camera and renderer
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(0.8, 1.3, 0.8);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+document.getElementById('demo').appendChild(renderer.domElement);
+renderer.render(scene, camera);
