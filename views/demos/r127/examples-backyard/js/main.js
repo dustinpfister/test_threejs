06a38e240b1ba6fe9a8fc59f83e4bd26d439293c
dustinpfister <dustin.pfister@gmail.com>
2021-04-28 09:19:28 -0400

    using random grid method for grass texture

--- views/demos/r127/examples-backyard/js/main.js
+++ views/demos/r127/examples-backyard/js/main.js
@@ -63,19 +63,7 @@ house.position.set(-2, 1.05, 0);
 scene.add(house);
 
 // ground
-var grassTexture = canvasTextureMod.createCanvasTexture(function (ctx, canvas) {
-        var i = 0,
-        x,
-        y,
-        len = canvas.width * canvas.height;
-        while (i < len) {
-            x = i % canvas.width;
-            y = Math.floor(i / canvas.width);
-            ctx.fillStyle = 'rgb(0,' + Math.floor(128 + 100 * Math.random()) + ',0)';
-            ctx.fillRect(x, y, 1, 1);
-            i += 1;
-        }
-    });
+var grassTexture = canvasTextureMod.randomGrid(['0','r1','0']);
 var materials = {
     ground: [
         new THREE.MeshStandardMaterial({
