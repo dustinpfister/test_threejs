4859950735bd37b2e555749e54a4c55b527bffb7
dustinpfister <dustin.pfister@gmail.com>
2021-04-29 19:28:03 -0400

    just loading the cube

--- views/demos/r127/dae-loader/js/main.js
+++ views/demos/r127/dae-loader/js/main.js
@@ -8,14 +8,14 @@
                 color: 'white'
             }));
     sun.add(new THREE.PointLight(0xffffff));
-    sun.position.set(8, 1, 1);
+    sun.position.set(2, 5, 3);
 
     // SCENE
     var scene = new THREE.Scene();
     scene.add(sun);
     // CAMERA
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
-    camera.position.set(8, 8, 8);
+    camera.position.set(5, 5, 5);
     camera.lookAt(0, 0, 0);
     scene.add(camera);
 
@@ -27,49 +27,11 @@
 
     var loader = new THREE.ColladaLoader();
     loader.load("/dae/box/box.dae", function (result) {
-
         console.log(result);
         scene.background = new THREE.Color('cyan');
-        scene.add(result.scene);
+        scene.add(result.scene.children[2]);
         renderer.render(scene, camera);
     });
 
 }
     ());
-
-/*
-(function () {
-
-// SCENE
-var scene = new THREE.Scene();
-
-var sun = new THREE.Mesh(
-new THREE.SphereGeometry(1, 20, 20),
-new THREE.MeshBasicMaterial({
-color: 'white'
-}));
-// camera
-var scene = new THREE.Scene();
-scene.add(sun);
-// camera
-var camera = new THREE.PerspectiveCamera(75, 320 / 240, .025, 20);
-camera.position.set(1, 1, 1);
-camera.lookAt(0, 0, 0);
-// renderer
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
-
-
-var loader = new THREE.ColladaLoader();
-loader.load("/dae/box/box.dae", function (result) {
-
-console.log(result);
-scene.background = new THREE.Color('cyan');
-scene.add(result.scene);
-renderer.render(scene, camera);
-});
-
-}
-());
-*/
