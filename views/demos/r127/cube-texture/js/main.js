6a2b03c708f7e59be3c24d2ead5a32a214b511f9
dustinpfister <dustin.pfister@gmail.com>
2021-04-29 16:14:46 -0400

    some clean up of the cube texture loader example

--- views/demos/r127/cube-texture/js/main.js
+++ views/demos/r127/cube-texture/js/main.js
@@ -1,26 +1,21 @@
 
 (function () {
 
-    console.log(new THREE.CubeTexture);
-
-    // SCENE
+    // camera
     var scene = new THREE.Scene();
-
-    // CAMERA
+    // camera
     var camera = new THREE.PerspectiveCamera(75, 320 / 240, .025, 20);
     camera.position.set(1, 1, 1);
     camera.lookAt(0, 0, 0);
-
-    // RENDER
+    // renderer
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(320, 240);
+    renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
 
     // LOAD CUBE TEXTURE
     new THREE.CubeTextureLoader()
     .setPath('/img/cube/skybox/')
     .load(
-
         // urls of images used in the cube texture
         [
             'px.jpg',
@@ -30,34 +25,24 @@
             'pz.jpg',
             'nz.jpg'
         ],
-
         // what to do when loading is over
         function (cubeTexture) {
-
             // Geometry
             var geometry = new THREE.SphereGeometry(1, 20, 20);
-
             // Material
             var material = new THREE.MeshBasicMaterial({
-
                 // CUBE TEXTURE can be used with
                 // the environment map property of
                 // a material.
                 envMap: cubeTexture
-
             });
-
             // Mesh
             var mesh = new THREE.Mesh(geometry, material);
             scene.add(mesh);
-
             // CUBE TEXTURE is also an option for a background
             scene.background = cubeTexture;
-
             renderer.render(scene, camera);
-
         }
-
     );
 
 }
