3c1ff159846090d8eed5d617abe87b4708574a8e
dustinpfister <dustin.pfister@gmail.com>
2021-05-01 16:07:10 -0400

    cleaned up a basic example

--- views/demos/r127/mesh/js/main.js
+++ views/demos/r127/mesh/js/main.js
@@ -1,10 +1,9 @@
 
 (function () {
-
     // Scene
     var scene = new THREE.Scene();
 
-    // Using A Mesh
+    // CREATEING AND ADDING A MESH TO A SCENE
     scene.add(new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshDepthMaterial()));
@@ -13,25 +12,11 @@
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
     camera.position.set(1.5, 1.5, 1.5);
     camera.lookAt(0, 0, 0);
-
-    // Orbit Controls
-    var controls = new THREE.OrbitControls(camera);
-
-    // RENDERER
+    // renderer
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(320, 240);
+    renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
-
-    var loop = function () {
-
-        requestAnimationFrame(loop);
-
-        // render the scene with the camera
-        renderer.render(scene, camera);
-
-    }
-
-    loop();
-
+    // render the scene with the camera
+    renderer.render(scene, camera);
 }
     ());
