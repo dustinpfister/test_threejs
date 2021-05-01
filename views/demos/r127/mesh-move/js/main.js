b9097c8c5d6fc7ff942418f9bdaa32bab784df22
dustinpfister <dustin.pfister@gmail.com>
2021-05-01 16:24:18 -0400

    some clean up of the mesh move demo

--- views/demos/r127/mesh-move/js/main.js
+++ views/demos/r127/mesh-move/js/main.js
@@ -1,49 +1,37 @@
 
 (function () {
-
-    // Scene
+    // scene
     var scene = new THREE.Scene();
 
-    // The Mesh
-    // assigning it to a variable so I can work
-    // with it elsewhere
+    // THE MESH
     var mesh = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshDepthMaterial());
+            new THREE.MeshNormalMaterial());
     scene.add(mesh);
 
-    // Camera
+    // camera
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(3, 1, 3);
+    camera.position.set(4, 2, 4);
     camera.lookAt(0, 0, 0);
-
-    // Orbit Controls
-    var controls = new THREE.OrbitControls(camera);
-
-    // RENDERER
+    // render
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(320, 240);
+    renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
 
+    // loop
     var frame = 0,
     maxFrame = 50,
     loop = function () {
-
         var per = frame / maxFrame,
         r = Math.PI * 2 * per;
-
         requestAnimationFrame(loop);
-
         // move the mesh with Object3D.position
         mesh.position.x = Math.cos(r) * 2;
         mesh.position.z = Math.sin(r) * 2;
-
         // a Object3D method
         mesh.lookAt(0, 0, 0);
-
         // render the scene with the camera
         renderer.render(scene, camera);
-
         frame += 1;
         frame %= maxFrame;
     }
