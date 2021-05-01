ef241eae903568a6e7b5b8869cd33c4304654366
dustinpfister <dustin.pfister@gmail.com>
2021-05-01 16:48:29 -0400

    moved some code around

--- views/demos/r127/mesh-material-array/js/main.js
+++ views/demos/r127/mesh-material-array/js/main.js
@@ -1,13 +1,7 @@
 
 (function () {
-
     // SCENE
     var scene = new THREE.Scene();
-
-    // CAMERA
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(2, 2, 2);
-    camera.lookAt(0, 0, 0);
 
     // the materials array
     var materials = [
@@ -28,25 +22,26 @@
             side: THREE.DoubleSide
         })
     ];
-
     // a box geometry
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-
     // for all faces
     geometry.groups.forEach(function (face, i) {
         face.materialIndex = i % materials.length;
     });
-
     // add to scene with the Mesh
     scene.add(new THREE.Mesh(
             geometry,
             materials));
 
+
+    // CAMERA
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
+    camera.position.set(2, 2, 2);
+    camera.lookAt(0, 0, 0);
     // RENDER
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
-
     renderer.render(scene, camera);
 
 }
