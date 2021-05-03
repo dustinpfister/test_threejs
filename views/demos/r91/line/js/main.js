c96b174abb124d4f8cf20a4e3aa3b407dd7f9dc8
dustinpfister <dustin.pfister@gmail.com>
2021-05-03 09:53:12 -0400

    cleaned up old r91 demo

--- views/demos/r91/line/js/main.js
+++ views/demos/r91/line/js/main.js
@@ -1,26 +1,8 @@
 
 (function () {
 
-    var obj = new THREE.Object3D();
-
-    // {"x":0,"y":0,"z":0}
-    console.log(JSON.stringify(obj.position));
-
-    obj.position.set(1, 2, 3);
-
-    // {"x":1,"y":2,"z":3}
-    console.log(JSON.stringify(obj.position));
-
     // Scene
     var scene = new THREE.Scene();
-
-    // Camera
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(0, 0, -30);
-    camera.lookAt(0, 0, 0);
-
-    // Orbit Controls
-    //var controls = new THREE.OrbitControls(camera);
 
     var geometry = new THREE.Geometry();
     geometry.vertices.push(
@@ -33,24 +15,16 @@
             }));
     scene.add(line);
 
+    // Camera
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
+    camera.position.set(0, 0, -30);
+    camera.lookAt(0, 0, 0);
+
     // Render
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(320, 240);
     document.getElementById('demo').appendChild(renderer.domElement);
     renderer.render(scene, camera);
-
-    // loop
-    /*
-    var loop = function () {
-
-    requestAnimationFrame(loop);
-    //controls.update();
-    renderer.render(scene, camera);
-
-    };
-
-    loop();
-     */
 
 }
     ());
