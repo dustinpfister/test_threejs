e0e565ff6cea6220e01f09f47bae38da36aa2a66
dustinpfister <dustin.pfister@gmail.com>
2021-05-05 11:34:32 -0400

    some clean up of main.js

--- views/demos/r127/camera-orthographic/js/main.js
+++ views/demos/r127/camera-orthographic/js/main.js
@@ -1,10 +1,10 @@
 
 (function () {
 
-    // Scene
+    // scene
     var scene = new THREE.Scene();
 
-    // Camera
+    // ORTHOGRAPHIC CAMERA
     var width = 3.2,
     height = 2.4,
     camera = new THREE.OrthographicCamera(
@@ -14,44 +14,29 @@
             height * -1,
             .01,
             100);
-
-    // set to same position, and look at the origin
     camera.position.set(4, 4, 4);
     camera.lookAt(0, 0, 0);
-
-    // set zoom
     camera.zoom = .75;
     camera.updateProjectionMatrix();
-
-    // point light above the camera
-    var light = new THREE.PointLight();
+    var light = new THREE.PointLight(); // point light above the camera
     light.position.set(0, 2, 0);
     camera.add(light);
-
-    //new THREE.OrbitControls(camera);
-
-    // make the camera part of the scene
-    scene.add(camera);
+    scene.add(camera); // make the camera part of the scene
 
     // create and add the cube stack
     var stack = new CubeStack({
-
             boxCount: 25
-
         });
     scene.add(stack.group);
 
-    // Render
+    // render
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
 
     var loop = function () {
-
         requestAnimationFrame(loop);
-
         renderer.render(scene, camera);
-
     };
 
     loop();
