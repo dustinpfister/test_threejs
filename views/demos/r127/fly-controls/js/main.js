464a67af251b7246fe84e9e4867070139230ea7f
dustinpfister <dustin.pfister@gmail.com>
2021-05-05 13:06:23 -0400

    started an r127 fly controls folder

---
+++ views/demos/r127/fly-controls/js/main.js
@@ -0,0 +1,37 @@
+
+(function () {
+
+    // Scene
+    var scene = new THREE.Scene();
+    scene.background = new THREE.Color('blue');
+
+    // Camera
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 1.4, 2.8);
+    camera.position.set(1.3, 1.5, 1.3);
+    camera.lookAt(0, 0, 0);
+
+    // Something to look at
+    scene.add(new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshDepthMaterial()));
+
+    // Render
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+
+    // Orbit Controls The DOM element must now be given as a second argument
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    // loop
+    var loop = function () {
+        requestAnimationFrame(loop);
+        // UPDATE CONTROLS
+        controls.update();
+        renderer.render(scene, camera);
+    };
+
+    loop();
+
+}
+    ());
