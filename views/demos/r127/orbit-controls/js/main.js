46a50f0e6a1afe5eff1c228781e5558f75004ec3
dustinpfister <dustin.pfister@gmail.com>
2021-05-05 12:52:57 -0400

    using depth material in orbit controls demo

--- views/demos/r127/orbit-controls/js/main.js
+++ views/demos/r127/orbit-controls/js/main.js
@@ -5,21 +5,18 @@
     var scene = new THREE.Scene();
 
     // Camera
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(2, 2, 2);
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 0.5, 2.8);
+    camera.position.set(1.4, 1.4, 1.4);
     camera.lookAt(0, 0, 0);
 
     // Something to look at
     scene.add(new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshBasicMaterial({
-                color: 0xff0000,
-                wireframe: true
-            })));
+            new THREE.MeshDepthMaterial()));
 
     // Render
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(320, 240);
+    renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
 
     // Orbit Controls The DOM element must now be given as a second argument
