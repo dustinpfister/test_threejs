5b0a64760f620d26b0d1092597605da04ac2a427
dustinpfister <dustin.pfister@gmail.com>
2021-05-05 13:24:03 -0400

    better example to look at

--- views/demos/r127/fly-controls/js/main.js
+++ views/demos/r127/fly-controls/js/main.js
@@ -6,21 +6,27 @@
     scene.background = new THREE.Color('blue');
 
     // Camera
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 0.1, 100);
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 100);
     camera.position.set(4, 4, 4);
     camera.lookAt(0, 0, 0);
 
     // Something to look at
-    scene.add(new THREE.Mesh(
+    var groundBox = new THREE.Mesh(
+            new THREE.BoxGeometry(10, 1, 10),
+            new THREE.MeshDepthMaterial());
+    groundBox.position.set(0,-1,0);
+    scene.add(groundBox);
+    var box = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshNormalMaterial()));
+            new THREE.MeshNormalMaterial());
+    scene.add(box);
 
     // Render
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
 
-    // FLY CONTROLS the camera is given as the first argument, and 
+    // FLY CONTROLS the camera is given as the first argument, and
     // the DOM element must now be given as a second argument
     var controls = new THREE.FlyControls(camera, renderer.domElement);
 
