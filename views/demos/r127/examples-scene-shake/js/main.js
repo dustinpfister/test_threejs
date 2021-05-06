a9f6fe43411fd6c55c8730bd9cec9b75c24bb9f4
dustinpfister <dustin.pfister@gmail.com>
2021-05-06 14:08:51 -0400

    clean up

--- views/demos/r127/examples-scene-shake/js/main.js
+++ views/demos/r127/examples-scene-shake/js/main.js
@@ -3,19 +3,16 @@
     var scene = new THREE.Scene();
     var gridHelper = new THREE.GridHelper(5, 5);
     scene.add(gridHelper);
-
     // box is a MESH base off of OBJECT3D
     var box = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshNormalMaterial());
     box.position.set(0, 0.5, 0);
     scene.add(box);
-
     // camera DO NOT ADD TO SCENE
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
     camera.position.set(5, 5, 5);
     camera.lookAt(0, 0, 0);
-
     // render
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
@@ -33,6 +30,8 @@
             active: true
         })
     };
+
+    // events
     var pointerDown = function () {
         state.shake.active = false;
     };
@@ -41,9 +40,9 @@
     };
     renderer.domElement.addEventListener('mousedown', pointerDown);
     renderer.domElement.addEventListener('mouseup', pointerUp);
+
     // update
     var update = function (state, secs) {
-
         if (state.shake.active) {
             state.shake.pos = 0.05 + 1.9 * state.bias;
             state.shake.deg = 0.50 + 18 * state.bias;
@@ -54,6 +53,7 @@
         //ShakeMod.update(state.shake, secs);
         ShakeMod.applyToObject3d(state.shake, scene);
     };
+
     // loop
     var loop = function () {
         state.per = state.frame / state.maxFrame;
