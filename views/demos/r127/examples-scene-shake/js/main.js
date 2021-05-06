a9d16c7ee5f5723c44f83f133b338c18f22c71b8
dustinpfister <dustin.pfister@gmail.com>
2021-05-06 12:27:58 -0400

    started a scene shake example folder

---
+++ views/demos/r127/examples-scene-shake/js/main.js
@@ -0,0 +1,53 @@
+(function () {
+    // scene and grid helper
+    var scene = new THREE.Scene();
+    var gridHelper = new THREE.GridHelper(6, 6);
+    scene.add(gridHelper);
+
+    // box is a MESH base off of OBJECT3D
+    var box = new THREE.Mesh(
+            new THREE.BoxGeometry(3, 3, 3),
+            new THREE.MeshNormalMaterial());
+    scene.add(box);
+
+    // camera
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
+    camera.position.set(5, 5, 5);
+    camera.lookAt(0, 0, 0);
+    // render
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+
+    // state object
+    var state = {
+        frame: 0,
+        maxFrame: 200,
+        fps: 30,
+        lt: new Date(),
+        euler: new THREE.Euler(0, 0, 0)
+    };
+    // update
+    var update = function (state, secs) {
+        // DOING A SPIN ALONG THE Z AXIS
+        state.euler.z = Math.PI * 8 * state.per;
+        box.rotation.copy(state.euler);
+    };
+    // loop
+    var loop = function () {
+        state.per = state.frame / state.maxFrame;
+        state.bias = 1 - Math.abs(state.per - 0.5) / 0.5;
+        var now = new Date();
+        secs = (now - state.lt) / 1000;
+        requestAnimationFrame(loop);
+        if (secs > 1 / state.fps) {
+            update(state, secs);
+            renderer.render(scene, camera);
+            state.frame += state.fps * secs;
+            state.frame %= state.maxFrame;
+            state.lt = now;
+        }
+    };
+    loop();
+}
+    ());
