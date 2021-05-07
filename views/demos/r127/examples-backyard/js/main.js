e79046dbd3fc10c21660ff3b9fa2daf01cb63341
dustinpfister <dustin.pfister@gmail.com>
2021-05-07 14:08:57 -0400

    basic clean up

--- views/demos/r127/examples-backyard/js/main.js
+++ views/demos/r127/examples-backyard/js/main.js
@@ -195,9 +195,7 @@ var cubeTextureLoaded = function (cubeTexture) {
 // LOAD CUBE TEXTURE
 var loadfail = false;
 new THREE.CubeTextureLoader()
-.setPath('./../../../img/cube/skybox/')
-//.load(['px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg'],
-//.setPath('./../../../img/cube/milky/')
+.setPath('/img/cube/milky/')
 .load(['px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg'],
     cubeTextureLoaded,
     function () {},
