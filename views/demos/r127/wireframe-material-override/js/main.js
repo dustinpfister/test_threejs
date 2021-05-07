64a7bc2a0ed1c1911033a133367046a3490ef4f6
dustinpfister <dustin.pfister@gmail.com>
2021-05-07 16:05:56 -0400

    started a material over ride wire frame demo

---
+++ views/demos/r127/wireframe-material-override/js/main.js
@@ -0,0 +1,47 @@
+(function () {
+
+    // Scene
+    var scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x0000ff);
+
+    // mesh1
+    var mesh = new THREE.Mesh(
+            new THREE.BoxGeometry(0.5, 0.5, 0.5),
+            new THREE.MeshDepthMaterial());
+    scene.add(mesh);
+
+    var mesh2 = new THREE.Mesh(
+            new THREE.BoxGeometry(0.5, 0.5, 0.5),
+            new THREE.MeshDepthMaterial());
+    mesh2.position.set(-1.5, 0, 0);
+    scene.add(mesh2);
+
+    // Camera
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 0.75, 10);
+    camera.position.set(1.15, 0.85, 0.75);
+    camera.lookAt(0, 0, 0);
+    var light = new THREE.PointLight(0xffffff, 1);
+    light.position.set(1, 1, 0);
+    camera.add(light)
+    scene.add(camera);
+    // Render
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+
+    var lt = new Date(),
+    fps = 30;
+    var loop = function () {
+        var now = new Date(),
+        secs = (now - lt) / 1000;
+        requestAnimationFrame(loop);
+        if (secs > 1 / fps) {
+            renderer.render(scene, camera);
+            lt = now;
+        }
+    };
+
+    loop();
+
+}
+    ());
