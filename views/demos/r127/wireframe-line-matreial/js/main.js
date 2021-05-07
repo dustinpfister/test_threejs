ab79c80aae29063797a2c2adfa69a771cbc19290
dustinpfister <dustin.pfister@gmail.com>
2021-05-07 13:22:14 -0400

    commnets

--- views/demos/r127/wireframe-line-matreial/js/main.js
+++ views/demos/r127/wireframe-line-matreial/js/main.js
@@ -1,10 +1,11 @@
 (function () {
 
+    // bog geometry and an edge geometry created from it
     var boxGeo = new THREE.BoxGeometry(1, 1, 1),
     edgeGeo = new THREE.EdgesGeometry(boxGeo);
 
-    // mesh in wire frame mode
-    var mesh = new THREE.LineSegments(
+    // Line Segments
+    var line = new THREE.LineSegments(
             edgeGeo,
             new THREE.LineBasicMaterial({
                 color: new THREE.Color('white')
@@ -13,7 +14,7 @@
     // Scene
     var scene = new THREE.Scene();
     scene.background = new THREE.Color('blue');
-    scene.add(mesh);
+    scene.add(line);
     // Camera
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
     camera.position.set(2, 2, 2);
