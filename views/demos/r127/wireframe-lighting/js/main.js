aa4b8b96ce4396eee2611cf9e0a003b8eea7645c
dustinpfister <dustin.pfister@gmail.com>
2021-05-07 13:49:46 -0400

    good lighting example

--- views/demos/r127/wireframe-lighting/js/main.js
+++ views/demos/r127/wireframe-lighting/js/main.js
@@ -4,27 +4,60 @@
     var scene = new THREE.Scene();
     scene.background = new THREE.Color(0x0000ff);
 
-    // mesh in wire frame mode
+    // mesh1
     var mesh = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.BoxGeometry(0.5, 0.5, 0.5),
             new THREE.MeshStandardMaterial({
                 color: 0xffffff, // color is white
-                emissive: 0x0000df, // emissive color is like the background color
+                emissive: 0x0000ff, // emissive color is the same as the background color
                 wireframe: true
             }));
     scene.add(mesh);
+
+    var mesh2 = new THREE.Mesh(
+            new THREE.BoxGeometry(0.5, 0.5, 0.5),
+            new THREE.MeshStandardMaterial({
+                color: 0xffffff, // color is white
+                emissive: 0x000000, // emissive color is Black
+                wireframe: true
+            }));
+    mesh2.position.set(-1.5, 0, 0);
+    scene.add(mesh2);
 
     // Camera
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
     camera.position.set(2, 2, 2);
     camera.lookAt(0, 0, 0);
     var light = new THREE.PointLight(0xffffff, 1);
+    light.position.set(1, 1, 0);
     camera.add(light)
     scene.add(camera);
     // Render
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
-    renderer.render(scene, camera);
+
+    var lt = new Date(),
+    deg = 0,
+    fps = 30;
+    var loop = function () {
+        var now = new Date(),
+        secs = (now - lt) / 1000;
+        requestAnimationFrame(loop);
+        if (secs > 1 / fps) {
+            deg += 10 * secs;
+            deg %= 30;
+            var per = deg / 30,
+            radian = Math.PI * 2 * per,
+            bias = 1 - Math.abs(per - 0.5) / 0.5;
+            light.position.set(Math.cos(radian) * 2, Math.sin(radian) * 2, 0);
+            light.intensity = 1 * bias;
+            renderer.render(scene, camera);
+            lt = now;
+        }
+    };
+
+    loop();
+
 }
     ());
