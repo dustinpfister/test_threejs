6ca7da6b222fde581170ea7558d1d62b54e9347e
dustinpfister <dustin.pfister@gmail.com>
2021-05-09 12:01:02 -0400

    clean up of plane check demo

--- views/demos/r127/plane-check/js/setup.js
+++ views/demos/r127/plane-check/js/setup.js
@@ -8,32 +8,12 @@ var renderer = new THREE.WebGLRenderer({
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
 
-var materialArray = [
-    new THREE.MeshBasicMaterial({
-        color: 0xe0e0e0,
-        side: THREE.DoubleSide
-    }),
-    new THREE.MeshBasicMaterial({
-        color: 0x505050,
-        side: THREE.DoubleSide
-    })
-];
-
-// add a plane
-var plane = new THREE.Mesh(
-        new THREE.PlaneGeometry(5, 5, 1, 1),
-        materialArray);
-plane.geometry.addGroup(0, 6, 0);
-plane.position.set(-10, 0, 0);
-plane.rotation.set(-Math.PI * 0.5, 0, 0);
-scene.add(plane);
-
 // standard checker
 var check = mkChecker({
         w: 10,
         h: 10,
-        sw: 2 + Math.round(Math.random() * 18),
-        sh: 3 + Math.round(Math.random() * 17)
+        sw: 12,
+        sh: 12
     });
 scene.add(check);
 
