ac09890c7b3a92f815fb1b7cd093f18f519ecd8f
dustinpfister <dustin.pfister@gmail.com>
2021-05-09 15:27:36 -0400

    great new basic example

--- views/demos/r127/userdata/js/main.js
+++ views/demos/r127/userdata/js/main.js
@@ -1,10 +1,13 @@
 
 (function () {
 
-    var createCube = function () {
+    var createCube = function (rotationRates, position) {
         var cube = new THREE.Mesh(
                 new THREE.BoxGeometry(1, 1, 1),
                 new THREE.MeshNormalMaterial());
+        var ud = cube.userData;
+        ud.rotationRates = rotationRates || [0, 3.14, 0];
+        cube.position.copy(position || new THREE.Vector3(0, 0, 0));
         return cube;
     };
 
@@ -13,15 +16,25 @@
     };
 
     var updateCube = function (cube, secs) {
-        cube.rotation.x += 1 * secs;
+        var ud = cube.userData,
+        rr = ud.rotationRates;
+        cube.rotation.x += rr[0] * secs;
+        cube.rotation.y += rr[1] * secs;
+        cube.rotation.z += rr[2] * secs;
         cube.rotation.x = clampRadian(cube.rotation.x);
+        cube.rotation.y = clampRadian(cube.rotation.y);
+        cube.rotation.z = clampRadian(cube.rotation.z);
     };
 
     // Scene
     var scene = new THREE.Scene();
 
-    var cube = createCube();
-    scene.add(cube);
+    // add some cubes
+    var cubes = new THREE.Group();
+    cubes.add(createCube([1.57, 0.00, 0.00], new THREE.Vector3(3, 0, 0)));
+    cubes.add(createCube([3.14, 1.57, 0.25], new THREE.Vector3(0, 0, 0)));
+    cubes.add(createCube([0.00, 0.00, 6.28], new THREE.Vector3(-3, 0, 0)));
+    scene.add(cubes);
 
     // Camera
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
@@ -40,7 +53,9 @@
         secs = (now - lt) / 1000;
         requestAnimationFrame(loop);
         if (secs > 1 / fps) {
-            updateCube(cube, secs);
+            cubes.children.forEach(function (cube) {
+                updateCube(cube, secs);
+            });
             renderer.render(scene, camera);
             lt = now;
         }
