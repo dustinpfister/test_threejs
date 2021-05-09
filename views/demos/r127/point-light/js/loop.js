7f46a850570a1c0136c542debf7ca3684e581c23
dustinpfister <dustin.pfister@gmail.com>
2021-05-09 11:25:50 -0400

    fps var for point-light demo

--- views/demos/r127/point-light/js/loop.js
+++ views/demos/r127/point-light/js/loop.js
@@ -9,20 +9,30 @@ loop = function () {
     requestAnimationFrame(loop);
     var r = Math.PI * 2 * per,
     sin = Math.sin(r) * 30,
-    cos = Math.cos(r) * 30;
+    cos = Math.cos(r) * 30,
+    now = new Date(),
+    secs = (now - lt) / 1000;
 
     per = frame / maxFrame;
     bias = 1 - Math.abs(0.5 - per) / 0.5;
 
-    // update point lights
-    whitePointLight.position.y = 20 * bias;
-    redPointLight.position.set(cos, sin, 0);
-    greenPointLight.position.set(cos, 0, sin);
-    bluePointLight.position.set(0, cos, sin);
-    // render
-    renderer.render(scene, camera);
-    // step frame
-    frame += 1;
-    frame %= maxFrame;
+    if (secs > 1 / fps) {
+
+        // update point lights
+        whitePointLight.position.y = 20 * bias;
+        redPointLight.position.set(cos, sin, 0);
+        greenPointLight.position.set(cos, 0, sin);
+        bluePointLight.position.set(0, cos, sin);
+
+        // render
+        renderer.render(scene, camera);
+        lt = new Date();
+
+        // step frame
+        frame += fps * secs;
+        frame %= maxFrame;
+
+    }
+
 };
 loop();
