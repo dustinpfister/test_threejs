f62539bd54f49eed5dd91f517d44c5d2d8e29788
dustinpfister <dustin.pfister@gmail.com>
2021-05-10 14:52:09 -0400

    three lights

--- views/demos/r127/examples-nested-groups/js/nested-groups.js
+++ views/demos/r127/examples-nested-groups/js/nested-groups.js
@@ -6,9 +6,24 @@
 
     var createPointLight = function(color){
         color = color || new THREE.Color('white');
-        var light = new THREE.Mesh(new THREE.SphereGeometry(1,30,30), new THREE.MeshBasicMaterial({color: color}));
+        var light = new THREE.Mesh(new THREE.SphereGeometry(0.25, 30, 30), new THREE.MeshBasicMaterial({color: color}));
         light.add(new THREE.PointLight(color, 1));
         return light;
+    };
+
+    var createPointLightGroup = function(){
+        var lightGroup = new THREE.Group();
+        // lights
+        var light = createPointLight(new THREE.Color('lime'));
+        light.position.set(0, 8, 0);
+        lightGroup.add(light);
+        var light = createPointLight(new THREE.Color('blue'));
+        light.position.set(0, -8, 0);
+        lightGroup.add(light);
+        var light = createPointLight(new THREE.Color('white'));
+        light.position.set(8, 0, 0);
+        lightGroup.add(light);
+        return lightGroup;
     };
 
 
@@ -44,16 +59,20 @@
         // add camera to nested
         nested.add(camera);
         // lights
+        var lightGroup = nud.lightGroup = createPointLightGroup();
+        nested.add(lightGroup);
+/*
         var light = createPointLight(new THREE.Color('lime'));
         light.position.set(5, 5, 5);
         nested.add(light);
+*/
         // shake objects
         nud.shakeObjects = createShakeObjects(nud);
         nested.add(nud.shakeObjects);
         // shake object
         nud.shake = ShakeMod.create({
-           pos: 0.125,
-           deg: 0.00
+           pos: 0.05,
+           deg: 2.00
         });
         return nested;
 
@@ -62,6 +81,7 @@
   
     // update the nested groups
     api.update = function(nested, secs) {
+       // camera
        var nud = nested.userData;
        nud.cameraRadian += Math.PI / 180 * 10 * secs;
        nud.cameraRadian %= Math.PI * 2;
@@ -75,6 +95,8 @@
        ShakeMod.applyToObject3d(nud.shake, nud.shakeObjects);
        // update cube group
        CubeGroupMod.update(nud.cubes1, secs);
+       // lights
+       nud.lightGroup.rotation.x += Math.PI / 180 * 45 * secs;
     };
 
 }
