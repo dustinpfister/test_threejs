9ac3465414fb00207e43e57d71cc73a464b395cf
dustinpfister <dustin.pfister@gmail.com>
2021-05-11 11:12:02 -0400

    create cube group helper

--- views/demos/r127/object3d-scale-group/js/main.js
+++ views/demos/r127/object3d-scale-group/js/main.js
@@ -1,25 +1,30 @@
-var size = 1,
-scale = 1 / 2,
-halfScale = scale / 2;
 
-var group = new THREE.Group();
-var box = new THREE.Mesh(
-        new THREE.BoxGeometry(size, size, size),
-        new THREE.MeshNormalMaterial());
-box.position.set(0, 0, 0);
-group.add(box);
+var createCubeGroup = function () {
+    var size = 1,
+    scale = 1 / 2,
+    halfScale = scale / 2;
+    var group = new THREE.Group();
+    var box = new THREE.Mesh(
+            new THREE.BoxGeometry(size, size, size),
+            new THREE.MeshNormalMaterial());
+    box.position.set(0, 0, 0);
+    group.add(box);
+    var i = 0,
+    len = 4;
+    while (i < len) {
+        var copy1 = box.clone(),
+        r = Math.PI * 2 / 4 * i,
+        x = Math.cos(r) * 1,
+        z = Math.sin(r) * 1;
+        copy1.scale.set(scale, scale, scale);
+        copy1.position.set(x, 0, z);
+        group.add(copy1);
+        i += 1;
+    }
+    return group;
+};
 
-var i = 0, len = 4;
-while (i < len) {
-    var copy1 = box.clone(),
-    r = Math.PI * 2 / 4 * i,
-    x = Math.cos(r) * 1,
-    z = Math.sin(r) * 1;
-    copy1.scale.set(scale, scale, scale);
-    copy1.position.set(x, 0, z);
-    group.add(copy1);
-    i += 1;
-}
+var group = createCubeGroup();
 
 var grid = new THREE.GridHelper(5, 5);
 group.add(grid);
