23b6cf7e498f950454ab2b1baf07bd7928a8ce92
dustinpfister <dustin.pfister@gmail.com>
2021-05-11 11:10:11 -0400

    group example

--- views/demos/r127/object3d-scale-group/js/main.js
+++ views/demos/r127/object3d-scale-group/js/main.js
@@ -1,29 +1,36 @@
-var size = 3,
-scale = 1 / size,
+var size = 1,
+scale = 1 / 2,
 halfScale = scale / 2;
 
+var group = new THREE.Group();
 var box = new THREE.Mesh(
         new THREE.BoxGeometry(size, size, size),
         new THREE.MeshNormalMaterial());
 box.position.set(0, 0, 0);
+group.add(box);
 
-var group = new THREE.Group();
-
-var copy1 = box.clone();
-copy1.scale.set(scale, scale, scale);
-copy1.position.set(2, 0, 0);
+var i = 0, len = 4;
+while (i < len) {
+    var copy1 = box.clone(),
+    r = Math.PI * 2 / 4 * i,
+    x = Math.cos(r) * 1,
+    z = Math.sin(r) * 1;
+    copy1.scale.set(scale, scale, scale);
+    copy1.position.set(x, 0, z);
+    group.add(copy1);
+    i += 1;
+}
 
 var grid = new THREE.GridHelper(5, 5);
+group.add(grid);
 
 // scene
 var scene = new THREE.Scene();
-scene.add(box);
-scene.add(copy1);
-scene.add(grid);
+scene.add(group);
 
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(3, 3, 3);
+camera.position.set(3, 7, 3);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
