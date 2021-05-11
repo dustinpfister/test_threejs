f6fbc631dbd4f1527f481f40da6911ad826a502d
dustinpfister <dustin.pfister@gmail.com>
2021-05-11 11:23:28 -0400

    group example looks good

--- views/demos/r127/object3d-scale-group/js/main.js
+++ views/demos/r127/object3d-scale-group/js/main.js
@@ -24,18 +24,31 @@ var createCubeGroup = function () {
     return group;
 };
 
-var group = createCubeGroup();
-
-var grid = new THREE.GridHelper(5, 5);
-group.add(grid);
-
 // scene
 var scene = new THREE.Scene();
-scene.add(group);
+var grid = new THREE.GridHelper(7, 7);
+scene.add(grid);
+
+// group1 with DEFAULT SCALE
+var group1 = createCubeGroup();
+group1.position.set(0, 0, 0);
+scene.add(group1);
+
+// group2 with 0.5 SCALE
+var group2 = createCubeGroup();
+group2.scale.set(0.5, 0.5, 0.5);
+group2.position.set(3, 0, 3);
+scene.add(group2);
+
+// group3 with 2 SCALE
+var group3 = createCubeGroup();
+group3.scale.set(2, 2, 2);
+group3.position.set(-3, 0, -3);
+scene.add(group3);
 
 // camera and renderer
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(3, 7, 3);
+var camera = new THREE.PerspectiveCamera(40, 320 / 240, 0.1, 100);
+camera.position.set(7, 7, 7);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
