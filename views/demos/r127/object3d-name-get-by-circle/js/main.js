b0e10fb80c10cd9a47ba698364ee2a3eb2b2676d
dustinpfister <dustin.pfister@gmail.com>
2021-05-12 10:15:06 -0400

    circle example

---
+++ views/demos/r127/object3d-name-get-by-circle/js/main.js
@@ -0,0 +1,36 @@
+// creating a group
+var group = new THREE.Group();
+group.name = 'boxGroup';
+var i = 0,
+len = 5;
+while(i < len){
+    var box = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshNormalMaterial());
+    box.position.set(0, 0, 0);
+    box.name = 'box' + i;
+    group.add(box);
+    i += 1;
+}
+
+// box helper
+group.add(new THREE.BoxHelper(group));
+group.position.set(0, 0, 0);
+
+// scene
+var scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(5, 5));
+scene.add(group);
+
+// GETTING BOX1 BY THE NAME
+var box = group.getObjectByName('box0');
+box.rotation.set(Math.PI / 180 * 45, 0, 0);
+
+// camera and renderer
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(4, 4, 4);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+document.getElementById('demo').appendChild(renderer.domElement);
+renderer.render(scene, camera);
