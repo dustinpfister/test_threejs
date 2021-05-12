ba77b481c9d1ddd3eba6e2a149f78539ca847485
dustinpfister <dustin.pfister@gmail.com>
2021-05-12 10:08:19 -0400

    comment

--- views/demos/r127/object3d-name-get-by/js/main.js
+++ views/demos/r127/object3d-name-get-by/js/main.js
@@ -1,27 +1,29 @@
 var group = new THREE.Group();
 group.name = 'boxGroup';
-
+// adding teo boxes to the group with names
 var box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
 box.position.set(-2, 0, 0);
 box.name = 'box1';
 group.add(box);
-
 var box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
 box.position.set(0, 0, 0);
 box.name = 'box2';
 group.add(box);
-
+// box helper
 group.add(new THREE.BoxHelper(group));
 group.position.set(0, 0, 0);
 
 // scene
 var scene = new THREE.Scene();
-scene.add(new THREE.GridHelper(5, 5))
+scene.add(new THREE.GridHelper(5, 5));
 scene.add(group);
+
+var box = group.getObjectByName('box1');
+box.rotation.set(1.57, 0, 0);
 
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
