7060fedbb3949e94f55cf7ce2350b23988f94314
dustinpfister <dustin.pfister@gmail.com>
2021-05-12 10:04:14 -0400

    added box and grid helpers

--- views/demos/r127/object3d-name-get-by/js/main.js
+++ views/demos/r127/object3d-name-get-by/js/main.js
@@ -1,18 +1,31 @@
+var group = new THREE.Group();
+group.name = 'boxGroup';
+
+var box = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshNormalMaterial());
+box.position.set(-2, 0, 0);
+box.name = 'box1';
+group.add(box);
+
 var box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
 box.position.set(0, 0, 0);
+box.name = 'box2';
+group.add(box);
 
-console.log(box.id);   // the id of the box
-console.log(box.name); // the name of the box
-
+group.add(new THREE.BoxHelper(group));
+group.position.set(0, 0, 0);
 
 // scene
 var scene = new THREE.Scene();
-scene.add(box);
+scene.add(new THREE.GridHelper(5, 5))
+scene.add(group);
+
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(3, 3, 3);
+camera.position.set(4, 4, 4);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
