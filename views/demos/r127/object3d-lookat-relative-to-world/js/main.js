7b9607dcc2bf6e7d8adb245d5caed5cf1ff9413e
dustinpfister <dustin.pfister@gmail.com>
2021-05-13 13:08:42 -0400

    pointer mesh

--- views/demos/r127/object3d-lookat-relative-to-world/js/main.js
+++ views/demos/r127/object3d-lookat-relative-to-world/js/main.js
@@ -4,24 +4,18 @@ var scene = new THREE.Scene();
 scene.add( new THREE.GridHelper(3, 3));
 
 var group = new THREE.Group();
-
-var m1 = new THREE.Mesh(
-        new THREE.ConeGeometry(0.5, 1),
+var geo = new THREE.CylinderGeometry( 0, 0.5, 1, 12 );
+geo.rotateX( Math.PI / 2 );
+var pointer = new THREE.Mesh(
+        geo,
         new THREE.MeshNormalMaterial());
-m1.position.set(0, 0, 0);
-m1.rotation.set(Math.PI * 0.5, 0, 0);
-group.add(m1);
+pointer.position.set(0, 0, 0);
+group.add(pointer);
 scene.add(group);
-
 group.add(new THREE.BoxHelper(group));
-
 group.position.set(-1, 0, -1);
 
-m1.lookAt(0, 0, 0);
-
-// MAKING THESE TWO MESH OBJECTS LOOK AT EACH OTHER
-//m1.lookAt(m2.position);
-//m2.lookAt(m1.position);
+group.lookAt(0, 0, 0);
 
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 100);
