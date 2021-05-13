3ba33aae06022094a629c7146fcbddfb7e738341
dustinpfister <dustin.pfister@gmail.com>
2021-05-13 12:38:27 -0400

    using a cone

--- views/demos/r127/object3d-lookat-relative-to-world/js/main.js
+++ views/demos/r127/object3d-lookat-relative-to-world/js/main.js
@@ -1,14 +1,23 @@
 
 // creating a scene
 var scene = new THREE.Scene();
-scene.add( new THREE.GridHelper(7, 7));
+scene.add( new THREE.GridHelper(3, 3));
+
+var group = new THREE.Group();
 
 var m1 = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.ConeGeometry(0.5, 1),
         new THREE.MeshNormalMaterial());
-//m1.position.set(0, 0, 4);
-scene.add(m1);
+m1.position.set(0, 0, 0);
+m1.rotation.set(Math.PI * 0.5, 0, 0);
+group.add(m1);
+scene.add(group);
 
+group.add(new THREE.BoxHelper(group));
+
+group.position.set(-1, 0, -1);
+
+m1.lookAt(0, 0, 0);
 
 // MAKING THESE TWO MESH OBJECTS LOOK AT EACH OTHER
 //m1.lookAt(m2.position);
@@ -16,7 +25,7 @@ scene.add(m1);
 
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 100);
-camera.position.set(5, 7, 5);
+camera.position.set(2, 4, 2);
 camera.lookAt(0,0,0);
 
 var renderer = new THREE.WebGLRenderer();
