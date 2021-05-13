6ffd57b33971477e49a16fbc5c9bce30b0bb8964
dustinpfister <dustin.pfister@gmail.com>
2021-05-13 13:16:02 -0400

    world relative example looks good

--- views/demos/r127/object3d-lookat-relative-to-world/js/main.js
+++ views/demos/r127/object3d-lookat-relative-to-world/js/main.js
@@ -1,26 +1,43 @@
 
 // creating a scene
 var scene = new THREE.Scene();
-scene.add( new THREE.GridHelper(3, 3));
+scene.add(new THREE.GridHelper(3, 3));
 
+// creating a group
 var group = new THREE.Group();
-var geo = new THREE.CylinderGeometry( 0, 0.5, 1, 12 );
-geo.rotateX( Math.PI / 2 );
+
+// creating and adding a pointer mesh to the group
+var geo = new THREE.CylinderGeometry(0, 0.5, 1, 12);
+geo.rotateX(Math.PI / 2);
 var pointer = new THREE.Mesh(
         geo,
         new THREE.MeshNormalMaterial());
 pointer.position.set(0, 0, 0);
 group.add(pointer);
-scene.add(group);
-group.add(new THREE.BoxHelper(group));
-group.position.set(-1, 0, -1);
 
-group.lookAt(0, 0, 0);
+// creating and adding a cube
+var cube = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshNormalMaterial());
+cube.position.set(0,0,2);
+group.add(cube);
+
+
+// box helper for the group
+group.add(new THREE.BoxHelper(group));
+
+// changing the position of the group to something other that 0,0,0
+group.position.set(-1.0, 0, -1.0);
+
+scene.add(group);
+
+// pointer looks at 0,0,0 in the grid, but not the center of the group
+pointer.lookAt(0, 0, 0);
 
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 100);
 camera.position.set(2, 4, 2);
-camera.lookAt(0,0,0);
+camera.lookAt(0, 0, 0);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
