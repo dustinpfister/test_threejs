5ec914fa43f430842cf4888a51a7dfec1cfa3094
dustinpfister <dustin.pfister@gmail.com>
2021-05-13 08:57:36 -0400

    updating all objects in main.js

--- views/demos/r127/object3d-name-get-by-object2/js/main.js
+++ views/demos/r127/object3d-name-get-by-object2/js/main.js
@@ -10,7 +10,7 @@ scene.add(group1);
 
 // create a group
 var group2 = BoxGroup.create();
-group2.position.set(0, 0, -15);
+group2.position.set(-15, 0, -15);
 scene.add(group2);
 
 // create a group
@@ -21,7 +21,7 @@ scene.add(group3); // add group
 
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(5, 5, 5);
+camera.position.set(10, 5, 5);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
@@ -34,16 +34,20 @@ var lt = new Date(),
 frame = 0,
 maxFrame = 600,
 fps = 30;
-var loop = function(){
+var loop = function () {
     var now = new Date(),
     per = frame / maxFrame,
     bias = 1 - Math.abs(per - 0.5) / 0.5,
     secs = (now - lt) / 1000;
     requestAnimationFrame(loop);
-    if(secs > 1 / fps){
+    if (secs > 1 / fps) {
 
         group1.userData.heading = 360 * per;
         BoxGroup.update(group1);
+
+        group2.userData.heading = 90;
+        group2.userData.pitch = 180 * Math.sin(Math.PI * 4 * per);
+        BoxGroup.update(group2);
 
         group3.userData.heading = 360 * per;
         group3.userData.pitch = 180 * Math.sin(Math.PI * 4 * per);
