47ebd52407a57c04f24372cd78f811b034ed1190
dustinpfister <dustin.pfister@gmail.com>
2021-05-13 13:24:11 -0400

    relative to group example started

---
+++ views/demos/r127/object3d-lookat-relative-to-group/js/main.js
@@ -0,0 +1,40 @@
+
+// creating a scene
+var scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(5, 5));
+
+// creating a group
+var group = new THREE.Group();
+// creating and adding a pointer mesh to the group
+var geo = new THREE.CylinderGeometry(0, 0.5, 1, 12);
+geo.rotateX(Math.PI / 2);
+var pointer = new THREE.Mesh(
+        geo,
+        new THREE.MeshNormalMaterial());
+pointer.position.set(0, 0, 0);
+group.add(pointer);
+// creating and adding a cube
+var cube = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshNormalMaterial());
+cube.position.set(0, 0, 4);
+group.add(cube);
+// box helper for the group
+group.add(new THREE.BoxHelper(group));
+// changing the position of the group to something other that 0,0,0
+group.position.set(-2.0, 0, -2.0);
+// add group to the scene
+scene.add(group);
+
+// POINTER LOOKS AT CUBE POSITION RELATIVE TO THE SCENE, BUT NOT RELATIVE TO THE GROUP
+pointer.lookAt(cube.position);
+
+// camera and renderer
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 100);
+camera.position.set(0, 4, 4);
+camera.lookAt(0, 0, 0);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+document.getElementById('demo').appendChild(renderer.domElement);
+renderer.render(scene, camera);
