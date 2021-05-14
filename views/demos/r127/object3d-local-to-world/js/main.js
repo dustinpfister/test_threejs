f66819251e4c211aea4c6f2fd34ea75914bdf9e6
dustinpfister <dustin.pfister@gmail.com>
2021-05-14 10:05:24 -0400

    local to world method does not seem to work

--- views/demos/r127/object3d-local-to-world/js/main.js
+++ views/demos/r127/object3d-local-to-world/js/main.js
@@ -1,14 +1,24 @@
-
 (function () {
 
     // SCENE
     var scene = new THREE.Scene();
-    scene.add(new THREE.GridHelper(10, 10));
+    scene.add(new THREE.GridHelper(7, 7));
 
+    var group = new THREE.Group();
+    scene.add(group)
     var box = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshNormalMaterial());
-    scene.add(box);
+    group.add(box);
+
+    // using localToWorld method but the function does
+    // not seem to work as it should. if the group is positioned
+    // to 2,0,0 and I pass the vector 0,0,0 to the localToWorld
+    // method I would expect 2,0,0 returned, but the result is 0,0,0
+    group.position.set(2, 0, 0);
+    var v = new THREE.Vector3(0, 0, 0);
+    console.log(box.localToWorld(v));
+    console.log(v.x);
 
     // CAMERA
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 100);
