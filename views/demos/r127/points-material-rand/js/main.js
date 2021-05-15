d0edda979e09dd1bab402a63c2f90df974c6a958
dustinpfister <dustin.pfister@gmail.com>
2021-05-15 09:56:12 -0400

    great basic points material

---
+++ views/demos/r127/points-material-rand/js/main.js
@@ -0,0 +1,47 @@
+
+(function () {
+
+    // SCENE
+    var scene = new THREE.Scene();
+
+    // CAMERA
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(50, 50, 50);
+    camera.lookAt(0, 0, 0);
+
+    var i = 0,
+    verts = [];
+    while (i < 500) {
+        var pt = new THREE.Vector3();
+        pt.set(
+            THREE.Math.randFloatSpread(45),
+            THREE.Math.randFloatSpread(45),
+            THREE.Math.randFloatSpread(45));
+        verts.push(pt.x, pt.y, pt.z);
+        i += 1;
+    }
+    // GEOMETRY
+    var geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(verts, 3));
+    // MESH with GEOMETRY, and Normal MATERIAL
+    scene.add(
+        new THREE.Points(
+            geometry,
+            new THREE.PointsMaterial({
+                color: 0x00afaf
+            })));
+
+    // RENDER
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+
+    var loop = function () {
+        requestAnimationFrame(loop);
+        renderer.render(scene, camera);
+    };
+
+    loop();
+
+}
+    ());
