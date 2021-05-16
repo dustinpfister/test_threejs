66dac25badcc81c871d338c5f55c4597b732e8ea
dustinpfister <dustin.pfister@gmail.com>
2021-05-16 09:55:29 -0400

    near far depth material demo looks good

--- views/demos/r127/camera-near-far-and-depth-material/js/main.js
+++ views/demos/r127/camera-near-far-and-depth-material/js/main.js
@@ -1,61 +1,46 @@
 
 (function () {
-
-    // a scene is needed to place objects in
+    // a scene
     var scene = new THREE.Scene();
+    scene.background = new THREE.Color(0.7, 0.7, 0.7);
 
-    // so here I am setting the values of the perspective camera
+    // setting the values of the perspective camera
     var fieldOfView = 45,
-    aspectRatio = 16 / 9,
-    near = 1,
-    far = 1000,
+    aspectRatio = 4 / 3,
+    near = 0.1,
+    far = 100,
     camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
 
-    // In order to see anything I will also need a renderer
-    // to use with my scene, and camera
+    // render
     var renderer = new THREE.WebGLRenderer();
-    // I must append the dom element used by the renderer to the html
-    // that I am using.
     document.getElementById('demo').appendChild(renderer.domElement);
-
     // initialize method
     var init = function () {
         // add plane to the scene
         var plane = new THREE.Mesh(
-                new THREE.PlaneBufferGeometry(500, 500, 8, 8),
-                new THREE.MeshBasicMaterial({
-                    color: 0x00afaf,
+                new THREE.PlaneBufferGeometry(4, 4),
+                new THREE.MeshDepthMaterial({
                     side: THREE.DoubleSide
                 }));
         plane.rotation.x = Math.PI / 2;
         scene.add(plane);
         // add a cube to the scene
         cube = new THREE.Mesh(
-                new THREE.BoxGeometry(200, 200, 200),
-                new THREE.MeshNormalMaterial({}));
-        cube.position.set(0, 100, 0);
+                new THREE.BoxGeometry(1, 1, 1),
+                new THREE.MeshDepthMaterial({}));
+        cube.position.set(0, 0.5, 0);
         scene.add(cube);
-        // setting position of the camera
-        // position is a property of Object3D
-        // and the value is an instance of Vector3
-        camera.position.set(400, 400, 400);
+        camera.position.set(3.5, 4.5, 3.5);
         camera.lookAt(0, 0, 0);
-        // setting a background color
-        scene.background = new THREE.Color(.7, .7, .7);
-        // 16:9 aspect ratio canvas
         renderer.setSize(640, 480);
     };
-
     // update method
     var update = function (per) {
-        var bias = 1 - Math.abs(.5 - per) / .5;
-        // changing aspect, and field of view
-        camera.aspect = .5 + 1.5 * bias;
-        camera.fov = 50 + 25 * bias;
-        // I must call this to get it to work
+        var bias = 1 - Math.abs(.5 - per) / 0.5;
+        camera.far = 4.5 + 8.5 * bias;
+        camera.near = 0.1 + 8.9 * bias;
         camera.updateProjectionMatrix();
     };
-
     // loop
     var frame = 0,
     frameMax = 30 * 10,
@@ -73,7 +58,6 @@
             lt = now;
         }
     };
-
     // call init, and start loop
     init();
     loop();
