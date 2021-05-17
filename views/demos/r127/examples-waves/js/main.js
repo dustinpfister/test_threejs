2ef5882382a2b12f46d285a174aa3fcbc5b573dd
dustinpfister <dustin.pfister@gmail.com>
2021-05-17 08:38:24 -0400

    rotating camera on the z axis

--- views/demos/r127/examples-waves/js/main.js
+++ views/demos/r127/examples-waves/js/main.js
@@ -54,7 +54,7 @@
             // then Material
             new THREE.PointsMaterial({
                 size: .125,
-                color: new THREE.Color(0.0, 0.5, 0.5)
+                color: new THREE.Color(0.0, 0.25, 0.25)
             }));
     };
 
@@ -117,9 +117,10 @@
 
         if (secs > 1 / fps) {
             updatePoints(points, per * 8 % 1);
-            var d = 0.8 + 2.5 * (1 - bias);
+            var d = 0.5 + 2.5 * (1 - bias);
             camera.position.set(d, 2.5, d);
-            //camera.lookAt(0, 0, 0);
+            camera.lookAt(-10, -10, -10);
+            camera.rotation.z = Math.PI * 2 * per;
             renderer.render(scene, camera);
             frame += fps * secs;
             frame %= maxFrame;
