13d4ce24b94a0f6ed8d1783bf04eddea334a896f
dustinpfister <dustin.pfister@gmail.com>
2021-05-17 16:48:55 -0400

    basic raycaster example looks good

--- views/demos/r127/raycaster-cube-group/js/main.js
+++ views/demos/r127/raycaster-cube-group/js/main.js
@@ -14,37 +14,28 @@ function onMouseMove( event ) {
     //console.log(mouse.x.toFixed(2), mouse.y.toFixed(2));
 };
 
+// creating a box mesh with the Box Geometry constructor,
+// and the normal material
+
+
 // creating a scene
 var scene = new THREE.Scene();
-scene.add(new THREE.GridHelper(9, 9));
 
-
-var boxGroup = new THREE.Group();
-scene.add(boxGroup);
-
-// box 1
+/*
 var box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
-box.position.set(0, 0, 0);
-boxGroup.add(box);
-// box 2
-box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
-box.position.set(3, 0, 0);
-boxGroup.add(box);
-// box 3
-box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
-box.position.set(-3, 0, 0);
-boxGroup.add(box);
+scene.add(box);
+*/
 
-
+// add the box mesh to the scene
+var cubeGroup = CubeGroupMod.create();
+cubeGroup.position.x = 3;
+scene.add(cubeGroup);
+ 
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(5, 5, 5);
+camera.position.set(3, 3, 3);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
@@ -56,34 +47,30 @@ renderer.domElement.addEventListener( 'mousemove', onMouseMove, false );
 
 // loop
 var lt = new Date(),
-//frame = 0,
-//maxFrame = 300,
+frame = 0,
+maxFrame = 300,
 fps = 5;
 var loop = function () {
     var now = new Date(),
-    //per = frame / maxFrame,
-    //bias = 1 - Math.abs(per - 0.5) / 0.5,
+    per = frame / maxFrame,
+    bias = 1 - Math.abs(per - 0.5) / 0.5,
     secs = (now - lt) / 1000;
     requestAnimationFrame(loop);
     if (secs > 1 / fps) {
 
-        // default scale
-        boxGroup.children.forEach(function(obj){
-            obj.scale.set(1, 1, 1);
-        });
-
         // update the picking ray with the camera and mouse position
 	raycaster.setFromCamera( mouse, camera );
-        var intersects = raycaster.intersectObjects(boxGroup.children, true );
+
+        var intersects = raycaster.intersectObjects( cubeGroup.children, true );
+
         if(intersects.length > 0){
-            var mesh = intersects[0].object;
-            mesh.scale.set(2, 2, 2);
+            console.log(intersects[0].object.parent); 
         }
 
-        // render
+
         renderer.render(scene, camera);
-        //frame += fps * secs;
-        //frame %= maxFrame;
+        frame += fps * secs;
+        frame %= maxFrame;
         lt = now;
     }
 }
