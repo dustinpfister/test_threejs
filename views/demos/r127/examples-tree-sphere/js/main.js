a94673aabf3aff6a3ce72a033987c893fd4e638a
dustinpfister <dustin.pfister@gmail.com>
2021-05-19 16:37:09 -0400

    not using world module in example-tree-sphere

--- views/demos/r127/examples-tree-sphere/js/main.js
+++ views/demos/r127/examples-tree-sphere/js/main.js
@@ -1,63 +1,13 @@
 (function () {
-    var MATERIALS_TREE = {
-        sphere: new THREE.MeshStandardMaterial({
-            //color: 0x00ff80,
-            map: canvasTextureMod.randomGrid(['0', 'r1', '64'], 32, 32, 150),
-            side: THREE.DoubleSide
-        }),
-        trunk: new THREE.MeshStandardMaterial({
-            color: 0xffaf80,
-            map: canvasTextureMod.randomGrid(['r1', 'r1', '64'], 32, 32, 150),
-            side: THREE.DoubleSide
-        })
-    };
-    var MATERIALS_LIGHTS = {
-        sun: new THREE.MeshStandardMaterial({
-            emissive: 'white',
-            emissiveMap: canvasTextureMod.randomGrid(['r1', 'r1', '0'])
-        }),
-        moon: new THREE.MeshStandardMaterial({
-            emissive: 'white',
-            emissiveMap: canvasTextureMod.randomGrid(['0', 'r1', 'ri'])
-        })
-    };
-    var MATERIALS_GROUND = {
-        grass: new THREE.MeshStandardMaterial({
-            color: 'white',
-            map: canvasTextureMod.randomGrid(['0', 'r1', '64'], 128, 125, 200),
-        })
-    };
 
     // creating a scene
     var scene = new THREE.Scene();
 
-    var worldOptions = {
-        MATERIALS_GROUND: MATERIALS_GROUND,
-        MATERIALS_TREE: MATERIALS_TREE,
-        MATERIALS_LIGHTS: MATERIALS_LIGHTS,
-        lightsDPSY: 20,
-        lightsDPSZ: 5,
-        worldRotation: 5
-    };
-    var world = WorldMod.create(worldOptions);
-    scene.add(world);
-
-    // world2
-    worldOptions.worldRotation = 65;
-    worldOptions.lightsDPSY = 75;
-    worldOptions.lightsDPSZ = 25;
-    var world2 = WorldMod.create(worldOptions);
-    world2.position.set(-28, -3, -5);
-    scene.add(world2);
-
-    // world3
-    worldOptions.worldRotation = 1;
-    worldOptions.lightsDPSX = 25;
-    worldOptions.lightsDPSY = 25;
-    worldOptions.lightsDPSZ = 0;
-    var world3 = WorldMod.create(worldOptions);
-    world3.position.set(-15, -20, -50);
-    scene.add(world3);
+    var tree = TreeSphereMod.create({
+            sphereSize: 0.25 + 0.75 * Math.random(),
+            trunkLength: 1 + 4 * Math.random()
+        });
+    scene.add(tree);
 
     // camera and renderer
     var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
@@ -72,33 +22,6 @@
     renderer.setViewport(0, 0, 640, 480);
     var container = document.getElementById('demo');
     container.appendChild(renderer.domElement);
-    var full = false;
-    var toggleFull = function (canvas) {
-        var canvas = renderer.domElement;
-        full = !full;
-        container.style.position = 'static';
-        container.style.width = '640px';
-        container.style.height = '480px';
-        canvas.style.width = '640px';
-        canvas.style.height = '480px';
-        if (full) {
-            canvas.style.width = 'auto';
-            canvas.style.height = window.innerHeight + 'px';
-            canvas.style.margin = 'auto';
-            container.style.position = 'fixed';
-            container.style.width = '100%';
-            container.style.height = '100%';
-            container.style.background = 'black';
-            container.style.left = '0px';
-            container.style.top = '0px';
-        }
-    };
-    // press f for full screen
-    window.addEventListener('keydown', function (e) {
-        if (e.key === 'f') {
-            toggleFull();
-        }
-    });
 
     var lt = new Date(),
     sunRadian = Math.PI,
@@ -108,9 +31,9 @@
         secs = (now - lt) / 1000;
         requestAnimationFrame(loop);
         if (secs > 1 / fps) {
-            WorldMod.update(world, secs);
-            WorldMod.update(world2, secs);
-            WorldMod.update(world3, secs);
+            //WorldMod.update(world, secs);
+            //WorldMod.update(world2, secs);
+            //WorldMod.update(world3, secs);
             renderer.render(scene, camera);
             lt = now;
         }
