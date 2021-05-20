7f21b188a5fda162b61ce0c737051400409cc4ca
dustinpfister <dustin.pfister@gmail.com>
2021-05-20 09:04:49 -0400

    positioning cube

--- views/demos/r127/buffer-geometry-rotation/js/main.js
+++ views/demos/r127/buffer-geometry-rotation/js/main.js
@@ -13,17 +13,20 @@
     // USING BUFFER GEOMERTY ROTATEX METHOD
     cone.geometry.rotateX(Math.PI * 0.5);
 
-    cone.lookAt(-3, 0, 3); // using Object3d (base class of Mesh) lookAt
     cone.add(new THREE.BoxHelper(cone)); // adding a box helper
     scene.add(cone); // add custom to the scene
 
-    var cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshNormalMaterial());
-    cube.position.set(0, 0, 3);
+    var cube = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshNormalMaterial());
+    cube.position.set(3, 0, 3);
     scene.add(cube)
+
+    //cone.lookAt(-3, 0, 3); // using Object3d (base class of Mesh) lookAt
 
     // camera render
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
-    camera.position.set(5, 5, 5);
+    camera.position.set(6, 8, 6);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
