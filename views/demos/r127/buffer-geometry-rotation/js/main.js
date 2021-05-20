82bd13b444c6ccba4e596790c31afdd9fd0628d2
dustinpfister <dustin.pfister@gmail.com>
2021-05-20 09:02:22 -0400

    added a cube to the scene

--- views/demos/r127/buffer-geometry-rotation/js/main.js
+++ views/demos/r127/buffer-geometry-rotation/js/main.js
@@ -1,10 +1,8 @@
 
 (function () {
-
     // scene
     var scene = new THREE.Scene();
     scene.add(new THREE.GridHelper(7, 7)); // grid helper for the scene
-
     // geometry
     var geometry = new THREE.ConeGeometry(0.5, 03, 30, 30);
     // Mesh
@@ -15,14 +13,13 @@
     // USING BUFFER GEOMERTY ROTATEX METHOD
     cone.geometry.rotateX(Math.PI * 0.5);
 
-    // using Object3d (base class of Mesh) lookAt
-    cone.lookAt(-3, 0, 3);
-
-    // adding a box helper
-    cone.add(new THREE.BoxHelper(cone));
-
+    cone.lookAt(-3, 0, 3); // using Object3d (base class of Mesh) lookAt
+    cone.add(new THREE.BoxHelper(cone)); // adding a box helper
     scene.add(cone); // add custom to the scene
 
+    var cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshNormalMaterial());
+    cube.position.set(0, 0, 3);
+    scene.add(cube)
 
     // camera render
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
