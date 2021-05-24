50a3cba4a597c44dbb86234e7d4c50966011d911
dustinpfister <dustin.pfister@gmail.com>
2021-05-24 12:23:17 -0400

    toggling in basic example

--- views/demos/r127/object3d-visible/js/main.js
+++ views/demos/r127/object3d-visible/js/main.js
@@ -5,12 +5,25 @@ var box = new THREE.Mesh(
         new THREE.MeshNormalMaterial());
 box.visible = false;
 scene.add(box);
- 
+
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(0.8, 1.3, 0.8);
+camera.position.set(1, 1, 1);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
-renderer.render(scene, camera);
+
+var lt = new Date(),
+fps = 2;
+var loop = function () {
+    var now = new Date(),
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if (secs > 1 / fps) {
+        box.visible = !box.visible;
+        renderer.render(scene, camera);
+        lt = now;
+    }
+};
+loop();
