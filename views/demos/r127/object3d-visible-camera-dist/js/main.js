2466ea4c2d1c32733a8727a1aa7fee215463b198
dustinpfister <dustin.pfister@gmail.com>
2021-05-24 13:49:28 -0400

    commnets

--- views/demos/r127/object3d-visible-camera-dist/js/main.js
+++ views/demos/r127/object3d-visible-camera-dist/js/main.js
@@ -1,20 +1,23 @@
 var scene = new THREE.Scene();
 scene.add(new THREE.GridHelper(10, 10));
 
-var box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
-scene.add(box);
-
-// camera and renderer
-var far = 20,
+// some values
+var near = 8.25,
+far = 20,
 maxDist = 10;
+
+// CREATING CAMERA WITH NEAR AND FAR VALUES
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 8.25, far);
 camera.position.set(8, 8, 8);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
+
+var box = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshNormalMaterial());
+scene.add(box);
 
 var lt = new Date(),
 frame = 0,
@@ -27,6 +30,7 @@ var loop = function () {
     secs = (now - lt) / 1000;
     requestAnimationFrame(loop);
     if (secs > 1 / fps) {
+        // CHANGING POSITION OF BOX SO THAT IT GOES IN ANY OUT OF THE RENDER RANGE OF THE CAMERA
         var dist = maxDist - (maxDist * 2) * bias;
         box.position.x = dist * -1;
         box.position.z = dist * -1;
