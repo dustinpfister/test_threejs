85f13f6012cb65ee141a908e00ca3e4b84a818df
dustinpfister <dustin.pfister@gmail.com>
2021-05-24 13:45:40 -0400

    camera example looks good

--- views/demos/r127/object3d-visible-camera-dist/js/main.js
+++ views/demos/r127/object3d-visible-camera-dist/js/main.js
@@ -8,8 +8,8 @@ scene.add(box);
 
 // camera and renderer
 var far = 20,
-maxDist = 7;
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, far);
+maxDist = 10;
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 8.25, far);
 camera.position.set(8, 8, 8);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
@@ -17,16 +17,22 @@ renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
 
 var lt = new Date(),
-fps = 2;
+frame = 0,
+maxFrame = 240,
+fps = 15;
 var loop = function () {
     var now = new Date(),
+    per = frame / maxFrame,
+    bias = 1 - Math.abs(per - 0.5) / 0.5,
     secs = (now - lt) / 1000;
     requestAnimationFrame(loop);
     if (secs > 1 / fps) {
-        var dist = maxDist;
+        var dist = maxDist - (maxDist * 2) * bias;
         box.position.x = dist * -1;
         box.position.z = dist * -1;
         renderer.render(scene, camera);
+        frame += fps * secs;
+        frame %= maxFrame;
         lt = now;
     }
 };
