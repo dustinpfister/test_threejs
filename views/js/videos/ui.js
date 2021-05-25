7bdf3606f326c3b7c072d62bac1160ddccb6f47d
dustinpfister <dustin.pfister@gmail.com>
2021-05-25 15:14:54 -0400

    started work on ui.js

--- views/js/videos/ui.js
+++ views/js/videos/ui.js
@@ -1,1 +1,11 @@
 console.log('I am the video ui');
+
+var videoUI = {};
+
+videoUI.onFrameUp
+
+var onFrameUp = function () {
+    console.log('frame+');
+};
+
+document.getElementById('ui_frame_+').addEventListener('click', onFrameUp);
