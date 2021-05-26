1fedb32971fc8e2fce5cc72cf5f23c2a46fbf41b
dustinpfister <dustin.pfister@gmail.com>
2021-05-26 11:07:33 -0400

    sphere-segments demo started

---
+++ views/demos/r127/sphere-segments/js/main.js
@@ -0,0 +1,44 @@
+
+(function () {
+
+    var createSphereAt = function (x, z, w, h, r) {
+        w = w === undefined ? 30 : w;
+        h = h === undefined ? 15 : h;
+        r = r === undefined ? 0.5 : r;
+        // mesh
+        var mesh = new THREE.Mesh(
+                // USING A SPHERE GEOMETRY WITH A RADIUS OF 0.5
+                new THREE.SphereGeometry(r, w, h),
+                // standard material
+                new THREE.MeshStandardMaterial({
+                    color: 0xff0000,
+                    emissive: 0x404040
+                }));
+        mesh.position.set(x, 0, z);
+        return mesh;
+    };
+
+    // creating a scene
+    var scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(8, 8, 0xff0000));
+
+    var sphere = createSphereAt(0, 0);
+    scene.add(sphere);
+
+    // camera
+    var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+    camera.position.set(3, 3, 3);
+    camera.lookAt(0, 0, 0);
+    var light = new THREE.PointLight(0xffffff); // point light
+    light.position.x = 1;
+    light.position.y = 1;
+    camera.add(light);
+    scene.add(camera);
+    // render
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+
+}
+    ());
