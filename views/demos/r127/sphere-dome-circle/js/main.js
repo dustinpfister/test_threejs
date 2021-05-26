448bf3d1bedcfd03f60c75feb7fd918d99b07c45
dustinpfister <dustin.pfister@gmail.com>
2021-05-26 13:47:57 -0400

    adding a circle

--- views/demos/r127/sphere-dome-circle/js/main.js
+++ views/demos/r127/sphere-dome-circle/js/main.js
@@ -16,8 +16,16 @@
                 new THREE.SphereGeometry(r, 30, 30, 0, Math.PI * 2, 0, Math.PI * 0.5),
                 // standard material
                 material);
+        if (cap) {
+            var circle = new THREE.Mesh(
+                    // USING A SPHERE GEOMETRY WITH A RADIUS OF 0.5
+                    new THREE.CircleGeometry(r, 30),
+                    // standard material
+                    material);
+            mesh.add(circle);
+        }
         mesh.position.set(x, 0, z);
-        mesh.geometry.rotateX(Math.PI * 2 * rPer);
+        mesh.rotateX(Math.PI * 2 * rPer);
         return mesh;
     };
 
@@ -27,6 +35,7 @@
 
     scene.add(createDomeAt(0, 0, 0.0));
     scene.add(createDomeAt(0, 1.5, 0.5, 0.75));
+    scene.add(createDomeAt(1.5, 0, 0.5, 0.75, true));
 
     // camera
     var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
