c25d969d4981c2c4f77f90c8b33a00fb8b95ddea
dustinpfister <dustin.pfister@gmail.com>
2021-05-26 10:36:23 -0400

    point light

--- views/demos/r127/sphere/js/main.js
+++ views/demos/r127/sphere/js/main.js
@@ -4,18 +4,27 @@
     // creating a scene
     var scene = new THREE.Scene();
 
+    // mesh
     var mesh = new THREE.Mesh(
-            new THREE.SphereGeometry(0.5, 10, 10),
+            // USING A SPHERE GEOMETRY WITH A RADIUS OF 0.5
+            new THREE.SphereGeometry(0.5),
+           // standard material
             new THREE.MeshStandardMaterial({
                 color: 0xff0000,
                 emissive: 0x404040
             }));
-    scene.add(mesh); // add the box mesh to the scene
+    scene.add(mesh); // add the mesh to the scene
 
-    // camera and renderer
+    // camera
     var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-    camera.position.set(0.5, 0.75, 0.5);
+    camera.position.set(0.75, 1, 0.75);
+    var light = new THREE.PointLight(0xffffff); // point light
+    light.position.x = 1;
+    light.position.y = 1;
+    camera.add(light);
     camera.lookAt(0, 0, 0);
+    scene.add(camera);
+    // render
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
