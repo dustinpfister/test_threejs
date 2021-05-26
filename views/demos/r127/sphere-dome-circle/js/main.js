1a84264987f48472e4b54ae0bc763462f3f81861
dustinpfister <dustin.pfister@gmail.com>
2021-05-26 13:42:33 -0400

    external material

--- views/demos/r127/sphere-dome-circle/js/main.js
+++ views/demos/r127/sphere-dome-circle/js/main.js
@@ -1,19 +1,21 @@
 
 (function () {
 
+    var material = new THREE.MeshStandardMaterial({
+            color: 0xffff00,
+            emissive: 0x404040,
+            side: THREE.DoubleSide
+        });
+
     // create a Dome at helper
-    var createDomeAt = function (x, z, rPer, r) {
+    var createDomeAt = function (x, z, rPer, r, cap) {
         r = r === undefined ? 0.5 : r;
         // mesh
         var mesh = new THREE.Mesh(
                 // USING A SPHERE GEOMETRY WITH A RADIUS OF 0.5
                 new THREE.SphereGeometry(r, 30, 30, 0, Math.PI * 2, 0, Math.PI * 0.5),
                 // standard material
-                new THREE.MeshStandardMaterial({
-                    color: 0xff0000,
-                    emissive: 0x404040,
-                    side: THREE.DoubleSide
-                }));
+                material);
         mesh.position.set(x, 0, z);
         mesh.geometry.rotateX(Math.PI * 2 * rPer);
         return mesh;
@@ -24,7 +26,7 @@
     scene.add(new THREE.GridHelper(8, 8, 0xff0000));
 
     scene.add(createDomeAt(0, 0, 0.0));
-    scene.add(createDomeAt(0, 1, 0.5));
+    scene.add(createDomeAt(0, 1.5, 0.5, 0.75));
 
     // camera
     var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
