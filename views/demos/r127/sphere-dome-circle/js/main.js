d8e682f0bec41db8afecb9113b092e7ea50f9086
dustinpfister <dustin.pfister@gmail.com>
2021-05-26 13:59:27 -0400

    dome with circle example looks good

--- views/demos/r127/sphere-dome-circle/js/main.js
+++ views/demos/r127/sphere-dome-circle/js/main.js
@@ -12,20 +12,20 @@
         r = r === undefined ? 0.5 : r;
         // mesh
         var mesh = new THREE.Mesh(
-                // USING A SPHERE GEOMETRY WITH A RADIUS OF 0.5
+                // USING A SPHERE GEOMETRY
                 new THREE.SphereGeometry(r, 30, 30, 0, Math.PI * 2, 0, Math.PI * 0.5),
                 // standard material
                 material);
         if (cap) {
             var circle = new THREE.Mesh(
-                    // USING A SPHERE GEOMETRY WITH A RADIUS OF 0.5
+                    // USING A CIRCLE GEOMETRY
                     new THREE.CircleGeometry(r, 30),
                     // standard material
                     material);
             circle.geometry.rotateX(Math.PI * 0.5);
             mesh.add(circle);
         }
-        mesh.position.set(x, 0, z);
+        mesh.position.set(x, 0.5, z);
         mesh.geometry.rotateX(Math.PI * 2 * rPer);
         return mesh;
     };
@@ -40,26 +40,27 @@
 
     // camera
     var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-    camera.position.set(2.0, 2.0, 2.0);
+    camera.position.set(2.5, 2.5, 2.5);
     camera.lookAt(0, 0, 0);
     var light = new THREE.PointLight(0xffffff); // point light
-    light.position.x = 1;
-    light.position.y = 1;
+    light.position.x = 4;
+    light.position.y = -0.5;
     camera.add(light);
     scene.add(camera);
     // render
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.render(scene, camera);
 
-    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    //var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-    var loop = function () {
-        requestAnimationFrame(loop);
-        renderer.render(scene, camera);
+    //var loop = function () {
+    //    requestAnimationFrame(loop);
 
-    };
-    loop();
+
+    //};
+    //loop();
 
 }
     ());
