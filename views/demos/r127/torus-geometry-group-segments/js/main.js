fdb7b81b9f61d6c12f21a0988751cbdd6817f8e0
dustinpfister <dustin.pfister@gmail.com>
2021-05-27 10:18:25 -0400

    adjusting values

--- views/demos/r127/torus-geometry-group-segments/js/main.js
+++ views/demos/r127/torus-geometry-group-segments/js/main.js
@@ -1,11 +1,9 @@
-
-
-
 var createDonutChild = function(index, len){
-    var radius = 0.6,
+    var per = index / len,
+    radius = 0.6,
     tubeRadius = 0.25,
-    radialSegments = 16,
-    tubeSegments = 32;
+    radialSegments = 4 + Math.round(20 * per),
+    tubeSegments = 4 + Math.round(20 * per);
     var donut = new THREE.Mesh(
         new THREE.TorusGeometry(radius, tubeRadius, radialSegments, tubeSegments),
         new THREE.MeshNormalMaterial());
@@ -14,28 +12,28 @@ var createDonutChild = function(index, len){
 
 var createDonutGroup = function(){
     var i = 0,
-    len = 5,
+    len = 10,
     group = new THREE.Group();
     while(i < len){
         var donut = createDonutChild(i, len);
-        donut.position.set(-5 + i * 2, 0, 0);
+        donut.position.set(0, 0, 3 - i * 0.75);
         group.add(donut);
         i += 1;
     }
     return group;
 };
 
-
 // creating a scene
 var scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(10, 10));
 
 var group = createDonutGroup();
 scene.add(group);
  
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(5, 5, 5);
-camera.lookAt(0, 0.25, 0);
+camera.position.set(3, 3, 3.5);
+camera.lookAt(0, 0, 0.5);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
