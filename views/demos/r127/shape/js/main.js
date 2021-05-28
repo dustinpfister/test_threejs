d9d13fef0e7afa0b38733d16f723b91a4b251137
dustinpfister <dustin.pfister@gmail.com>
2021-05-28 10:14:55 -0400

    started basic shape example

---
+++ views/demos/r127/shape/js/main.js
@@ -0,0 +1,32 @@
+ 
+// creating a scene
+var scene = new THREE.Scene();
+
+const heartShape = new THREE.Shape();
+
+heartShape.moveTo( 25, 25 );
+heartShape.bezierCurveTo( 25, 25, 20, 0, 0, 0 );
+heartShape.bezierCurveTo( - 30, 0, - 30, 35, - 30, 35 );
+heartShape.bezierCurveTo( - 30, 55, - 10, 77, 25, 95 );
+heartShape.bezierCurveTo( 60, 77, 80, 55, 80, 35 );
+heartShape.bezierCurveTo( 80, 35, 80, 0, 50, 0 );
+heartShape.bezierCurveTo( 35, 0, 25, 25, 25, 25 );
+
+const extrudeSettings = { depth: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
+
+const geometry = new THREE.ExtrudeGeometry( heartShape, extrudeSettings );
+geometry.rotateX(Math.PI * 1)
+const mesh = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial() );
+
+ 
+// add the box mesh to the scene
+scene.add(mesh);
+ 
+// camera and renderer
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(100, 100, 100);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+document.getElementById('demo').appendChild(renderer.domElement);
+renderer.render(scene, camera);
