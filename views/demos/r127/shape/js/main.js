9b78bb4b3365b7b4996d7d02716d1f16ec5ccb98
dustinpfister <dustin.pfister@gmail.com>
2021-05-28 10:18:38 -0400

    basic shape example just has three points

--- views/demos/r127/shape/js/main.js
+++ views/demos/r127/shape/js/main.js
@@ -2,19 +2,15 @@
 // creating a scene
 var scene = new THREE.Scene();
 
-var heartShape = new THREE.Shape();
+var tri = new THREE.Shape();
 
-heartShape.moveTo( 25, 25 );
-heartShape.bezierCurveTo( 25, 25, 20, 0, 0, 0 );
-heartShape.bezierCurveTo( - 30, 0, - 30, 35, - 30, 35 );
-heartShape.bezierCurveTo( - 30, 55, - 10, 77, 25, 95 );
-heartShape.bezierCurveTo( 60, 77, 80, 55, 80, 35 );
-heartShape.bezierCurveTo( 80, 35, 80, 0, 50, 0 );
-heartShape.bezierCurveTo( 35, 0, 25, 25, 25, 25 );
+tri.moveTo( 0, 25 );
+tri.lineTo( 25, -25 );
+tri.lineTo( -25, -25 );
 
 var extrudeSettings = { depth: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
 
-var geometry = new THREE.ExtrudeGeometry( heartShape, extrudeSettings );
+var geometry = new THREE.ExtrudeGeometry( tri, extrudeSettings );
 geometry.rotateX(Math.PI * 1)
 var mesh = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial() );
 
