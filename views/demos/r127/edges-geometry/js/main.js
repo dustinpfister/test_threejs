ba4078bb7128b3315fa2e553e5a9cb7e13e2f536
dustinpfister <dustin.pfister@gmail.com>
2021-05-31 11:51:19 -0400

    adjust camera

--- views/demos/r127/edges-geometry/js/main.js
+++ views/demos/r127/edges-geometry/js/main.js
@@ -2,8 +2,8 @@
     // box geometry and...
     var boxGeo = new THREE.BoxGeometry(1, 1, 1),
     // AN EDGE GEOMETRY CREATED FROM IT
-    edgeGeo = new THREE.EdgesGeometry(boxGeo);
-    var line = new THREE.LineSegments(
+    edgeGeo = new THREE.EdgesGeometry(boxGeo),
+    line = new THREE.LineSegments(
             edgeGeo,
             new THREE.LineBasicMaterial({
                 color: new THREE.Color('white')
@@ -13,7 +13,7 @@
     scene.background = new THREE.Color('blue');
     scene.add(line);
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(2, 2, 2);
+    camera.position.set(1.25, 1.75, 1.25);
     camera.lookAt(0, 0, 0);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
