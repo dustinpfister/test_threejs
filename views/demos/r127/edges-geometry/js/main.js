1095cd5679ce89be0696f1cbd153fe51f841886b
dustinpfister <dustin.pfister@gmail.com>
2021-05-31 11:49:27 -0400

    comments

--- views/demos/r127/edges-geometry/js/main.js
+++ views/demos/r127/edges-geometry/js/main.js
@@ -1,25 +1,20 @@
 (function () {
-
-    // bog geometry and an edge geometry created from it
+    // box geometry and...
     var boxGeo = new THREE.BoxGeometry(1, 1, 1),
+    // AN EDGE GEOMETRY CREATED FROM IT
     edgeGeo = new THREE.EdgesGeometry(boxGeo);
-
-    // Line Segments
     var line = new THREE.LineSegments(
             edgeGeo,
             new THREE.LineBasicMaterial({
                 color: new THREE.Color('white')
             }));
-
-    // Scene
+    // Scene, camera renderer
     var scene = new THREE.Scene();
     scene.background = new THREE.Color('blue');
     scene.add(line);
-    // Camera
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
     camera.position.set(2, 2, 2);
     camera.lookAt(0, 0, 0);
-    // Render
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
