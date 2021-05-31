5b4feff841a75dbcb9a3e1ffa64cd9982fcb9f3c
dustinpfister <dustin.pfister@gmail.com>
2021-05-31 14:26:52 -0400

    using the translate method for the geometry

--- views/demos/r127/box-geometry-rotation-position-geo/js/main.js
+++ views/demos/r127/box-geometry-rotation-position-geo/js/main.js
@@ -15,9 +15,10 @@ var box2 = new THREE.Mesh(
         new THREE.MeshBasicMaterial({
             color: new THREE.Color('gray')
         }));
-box2.geometry.rotateY(Math.PI * 0.25); // Rotating geometry
+box2.geometry.rotateY(Math.PI * 0.25); // ROTATING THE GEOMERTY
+box2.geometry.translate(0, 1, 0); // TRANSLATING THE GEOMMETRY
 box2.add(new THREE.BoxHelper(box2, new THREE.Color('red'))); // box helper
-box2.position.set(2, 0, 0); // Setting position of mesh
+box2.position.set(2, 0, 0); // SETTING THE POSITION OF THE MESH
 scene.add(box2);
 
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
