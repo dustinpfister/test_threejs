4fba9e533f859df4c844b8627177082740fe56e5
dustinpfister <dustin.pfister@gmail.com>
2021-05-31 14:10:14 -0400

    added grid helper

--- views/demos/r127/box-geometry-rotation-position-mesh/js/main.js
+++ views/demos/r127/box-geometry-rotation-position-mesh/js/main.js
@@ -1,5 +1,6 @@
 
 var scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(10, 10));
 
 var box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
@@ -26,33 +27,24 @@ state = {
 var loop = function () {
     var now = new Date(),
     secs = (now - lt) / 1000;
-
     requestAnimationFrame(loop);
-
     state.per = state.frame / state.maxFrame;
     state.bias = 1 - Math.abs(state.per - 0.5) / 0.5;
     state.radian = Math.PI * 2 * state.bias;
-
     state.p.z = -2 * Math.cos(Math.PI * 2 * state.bias);
     state.p.x = -2 * Math.sin(Math.PI * 8 * state.bias);
-
     // changing values
     state.r.x += 1 * secs;
     state.r.y += 2 * secs;
     state.r.z += 3 * secs;
-
     // copy the state of the THREE.Euler instance in the state object
     // as the new rotation value of the box
     box.rotation.copy(state.r);
-
     // using the copy method for Vector3 also
     box.position.copy(state.p);
-
     renderer.render(scene, camera);
-
     state.frame += 4 * secs;
     state.frame %= state.maxFrame;
-
     lt = now;
 };
 loop();
