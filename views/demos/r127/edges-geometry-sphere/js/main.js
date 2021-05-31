5d57a9b4d49a6703f8d7993c9ee7ee9fb5c96faa
dustinpfister <dustin.pfister@gmail.com>
2021-05-31 13:23:34 -0400

    added a mesh object

--- views/demos/r127/edges-geometry-sphere/js/main.js
+++ views/demos/r127/edges-geometry-sphere/js/main.js
@@ -15,11 +15,21 @@
                 color: new THREE.Color('white')
             }));
     line2.position.set(0.75, 0, 0);
+
+    var mesh1 = new THREE.LineSegments(
+            sphereGeo,
+            new THREE.MeshBasicMaterial({
+                color: new THREE.Color('gray'),
+                wireframe: true
+            }));
+    mesh1.position.set(0, 0, -1.75);
+
     // Scene, camera renderer
     var scene = new THREE.Scene();
     scene.background = new THREE.Color('blue');
     scene.add(line1);
     scene.add(line2);
+    scene.add(mesh1);
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
     camera.position.set(1.75, 2.00, 1.75);
     camera.lookAt(0, 0, 0);
