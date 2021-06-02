4fc9cad65643c65e8190e5c48fb919133e139870
dustinpfister <dustin.pfister@gmail.com>
2021-06-02 13:37:27 -0400

    updated raycaster demo

--- views/demos/r127/object3d-parent-raycaster/js/main.js
+++ views/demos/r127/object3d-parent-raycaster/js/main.js
@@ -1,33 +1,62 @@
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2(1, 1);
 
+// on mouse move
 var onMouseMove = function( event ) {
-    // calculate mouse position in normalized device coordinates
-    // (-1 to +1) for both components
     var canvas = event.target,
     box = canvas.getBoundingClientRect(),
     x = event.clientX - box.left,
     y = event.clientY - box.top;
-    // set mouse Vector2 values
     mouse.x = ( x / canvas.scrollWidth ) * 2 - 1;
     mouse.y = - ( y / canvas.scrollHeight ) * 2 + 1;
-    //console.log(mouse.x.toFixed(2), mouse.y.toFixed(2));
 };
 
-var update = function(group){
-    // default scale
-    group.children.forEach(function(obj){
-        obj.scale.set(1, 1, 1);
-    });
-    // update the picking ray with the camera and mouse position
+// update the picking ray with the camera and mouse position
+var update = function(cubeGroups, secs){
     raycaster.setFromCamera( mouse, camera );
-    var intersects = raycaster.intersectObjects(group.children, true );
-    if(intersects.length > 0){
-        var mesh = intersects[0].object;
-        mesh.scale.set(2, 2, 2);
-    }
+    cubeGroups.children.forEach(function(cubeGroup){
+        var intersects = raycaster.intersectObjects( cubeGroup.children, true );
+        if(intersects.length > 0){
+            var mesh = intersects[0].object,
+            group = mesh.parent;
+            group.userData.active = true;
+        }
+        CubeGroupMod.update(cubeGroup, secs);
+    });
 };
 
+// creating a scene
+var scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(9, 9));
+
+
+var cubeGroups = new THREE.Group();
+scene.add(cubeGroups);
+
+var cg = CubeGroupMod.create({
+   maxFrame: 30,
+   yDelta: 0.5,
+   xzDelta: 0.5
+});
+cg.position.x = 0;
+cubeGroups.add(cg);
+
+var cg = CubeGroupMod.create({
+   maxFrame: 30,
+   yDelta: 0.5,
+   xzDelta: 0.5
+});
+cg.position.x = 3;
+cubeGroups.add(cg);
+
+var cg = CubeGroupMod.create({
+   maxFrame: 30,
+   yDelta: 0.5,
+   xzDelta: 0.5
+});
+cg.position.x = -3;
+cubeGroups.add(cg);
+ 
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
 camera.position.set(5, 5, 5);
@@ -36,51 +65,30 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
 
-// Attach mouse event
+
 renderer.domElement.addEventListener( 'mousemove', onMouseMove, false );
 
-// creating a scene
-var scene = new THREE.Scene();
-scene.add(new THREE.GridHelper(9, 9));
 
-
-var boxGroup = new THREE.Group();
-scene.add(boxGroup);
-
-// box 1
-var box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
-box.position.set(0, 0, 0);
-boxGroup.add(box);
-// box 2
-box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
-box.position.set(3, 0, 0);
-boxGroup.add(box);
-// box 3
-box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
-box.position.set(-3, 0, 0);
-boxGroup.add(box);
-
-// orbit controls
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 // loop
 var lt = new Date(),
+frame = 0,
+maxFrame = 300,
 fps = 30;
 var loop = function () {
     var now = new Date(),
+    per = frame / maxFrame,
+    bias = 1 - Math.abs(per - 0.5) / 0.5,
     secs = (now - lt) / 1000;
     requestAnimationFrame(loop);
     if (secs > 1 / fps) {
-        // update
-        update(boxGroup);
-        // render
+
+        update(cubeGroups, secs);
+
         renderer.render(scene, camera);
+        frame += fps * secs;
+        frame %= maxFrame;
         lt = now;
     }
 }
