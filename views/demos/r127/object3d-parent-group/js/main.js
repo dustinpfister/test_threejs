f8ad87f31447ba2ede876929a9c34143734a015f
dustinpfister <dustin.pfister@gmail.com>
2021-06-02 12:13:50 -0400

    group is working good

--- views/demos/r127/object3d-parent-group/js/main.js
+++ views/demos/r127/object3d-parent-group/js/main.js
@@ -1,21 +1,34 @@
+var createGroup = function(){
+    var group = new THREE.Group();
+    // adding a cone
+    var cone = new THREE.Mesh(
+        new THREE.ConeGeometry(0.5, 1, 10, 10),
+        new THREE.MeshNormalMaterial());
+    cone.geometry.rotateX(Math.PI * 0.5);
+    cone.position.set(0, 0, 2.0);
+    group.add(cone);
+    // adding a box
+    var box = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 3),
+        new THREE.MeshNormalMaterial());
+    box.position.set(0, 0, 0);
+    group.add(box);  
+    return group;
+};
+
+
 // scene
 var scene = new THREE.Scene();
 scene.add(new THREE.GridHelper(5, 5));
 
-// adding a mesh
-var mesh = new THREE.Mesh(
-        new THREE.ConeGeometry(0.5, 1, 10, 10),
-        new THREE.MeshNormalMaterial());
-mesh.position.set(0, 0.5, 0);
-scene.add(mesh);
+var group = createGroup();
+scene.add(group);
 
-// getting the parent of the mesh, and prefroming an action on it
-var parent = mesh.parent;
-parent.rotation.x = Math.PI * 2 * Math.random();
+group.lookAt(-10, 10, -10);
 
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(4, 4, 4);
+camera.position.set(5, 5, 5);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
