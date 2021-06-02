173dc20cc76613e00795a45118c1db19400fa23f
dustinpfister <dustin.pfister@gmail.com>
2021-06-02 11:58:36 -0400

    basic object3d-parent example looks good

--- views/demos/r127/object3d-parent/js/main.js
+++ views/demos/r127/object3d-parent/js/main.js
@@ -1,32 +1,17 @@
-// creating a group
-var group = new THREE.Group();
-group.name = 'boxGroup';
-// adding two boxes to the group with names
-var box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
-box.position.set(0, 0, 0);
-box.name = 'box1';
-group.add(box);
-var box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
-box.position.set(-2, 0, 0);
-box.name = 'box2';
-group.add(box);
-
-// box helper
-group.add(new THREE.BoxHelper(group));
-group.position.set(0, 0, 0);
-
 // scene
 var scene = new THREE.Scene();
 scene.add(new THREE.GridHelper(5, 5));
-scene.add(group);
 
-// GETTING BOX1 BY THE NAME
-var box = group.getObjectByName('box1');
-box.rotation.set(Math.PI / 180 * 45, 0, 0);
+// adding a mesh
+var mesh = new THREE.Mesh(
+        new THREE.ConeGeometry(0.5, 1, 10, 10),
+        new THREE.MeshNormalMaterial());
+mesh.position.set(0, 0.5, 0);
+scene.add(mesh);
+
+// getting the parent of the mesh, and prefroming an action on it
+var parent = mesh.parent;
+parent.rotation.x = Math.PI * 2 * Math.random();
 
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
