296bd95f973b431d0f720e61bcfaa96e6711930d
dustinpfister <dustin.pfister@gmail.com>
2021-06-02 12:18:53 -0400

    two groups and names

--- views/demos/r127/object3d-parent-group/js/main.js
+++ views/demos/r127/object3d-parent-group/js/main.js
@@ -1,17 +1,20 @@
-var createGroup = function(){
+var createGroup = function(gid){
     var group = new THREE.Group();
+    group.name = 'group_' + gid;
     // adding a cone
     var cone = new THREE.Mesh(
         new THREE.ConeGeometry(0.5, 1, 10, 10),
         new THREE.MeshNormalMaterial());
     cone.geometry.rotateX(Math.PI * 0.5);
     cone.position.set(0, 0, 2.0);
+    cone.name = 'mesh_ ' + group.name + '_cone';
     group.add(cone);
     // adding a box
     var box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 3),
         new THREE.MeshNormalMaterial());
     box.position.set(0, 0, 0);
+    box.name = 'mesh_ ' + group.name + '_cone';
     group.add(box);  
     return group;
 };
@@ -19,12 +22,17 @@ var createGroup = function(){
 
 // scene
 var scene = new THREE.Scene();
-scene.add(new THREE.GridHelper(5, 5));
+scene.name = 'scene_myWorld';
+scene.add(new THREE.GridHelper(9, 9));
 
-var group = createGroup();
+var group = createGroup('0');
 scene.add(group);
-
 group.lookAt(-10, 10, -10);
+
+group = createGroup('1');
+group.position.set(3, 1.5, 0);
+group.lookAt(3, 10, 0);
+scene.add(group);
 
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
