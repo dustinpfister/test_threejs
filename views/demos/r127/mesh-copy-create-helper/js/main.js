b84adf620d17c6156fa466c589c912a16108b9ba
dustinpfister <dustin.pfister@gmail.com>
2021-06-03 11:22:47 -0400

    create helper demo works

--- views/demos/r127/mesh-copy-create-helper/js/main.js
+++ views/demos/r127/mesh-copy-create-helper/js/main.js
@@ -2,15 +2,41 @@
 var scene = new THREE.Scene();
 scene.add(new THREE.GridHelper(10, 10));
 
-// MESH original
-var original = new THREE.Mesh(
+var group = new THREE.Group();
+scene.add(group);
+
+var createBox = function(){
+    var box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
-       // Now using the Standard material
         new THREE.MeshStandardMaterial({
             color: 'red'
         }));
-scene.add(original);
+    return box;
+};
+var mainBox = createBox();
+group.add(mainBox);
 
+// Mesh cloned a bunch of times from original
+var i = 0, mesh, rad, x, z;
+while (i < 10) {
+    mesh = createBox();
+    // changes made to position and rotation to not effect original
+    rad = Math.PI * 2 * (i / 10);
+    x = Math.cos(rad) * 3;
+    z = Math.sin(rad) * 3;
+    mesh.position.set(x, 0, z);
+    mesh.lookAt(mainBox.position);
+    group.add(mesh);
+    i += 1;
+}
+
+// changing the color of the main box ONLY EFFECTS THE MAIN BOX
+mainBox.material.color.setRGB(0, 1, 0);
+
+// CAMERA
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(8, 5, 8);
+camera.lookAt(0, 0, 0);
 // add a light source
 var sun = new THREE.Mesh(
         new THREE.SphereGeometry(0.5, 40, 40),
@@ -19,27 +45,6 @@ sun.add(new THREE.PointLight(0xffffff, 1));
 sun.position.set(8, 3, 0);
 scene.add(sun);
 
-// Mesh cloned a bunch of times from original
-var i = 0, mesh, rad, x, z;
-while (i < 10) {
-    mesh = original.clone();
-    // changes made to position and rotation to not effect original
-    rad = Math.PI * 2 * (i / 10);
-    x = Math.cos(rad) * 3;
-    z = Math.sin(rad) * 3;
-    mesh.position.set(x, 0, z);
-    mesh.lookAt(original.position);
-    scene.add(mesh);
-    i += 1;
-}
-
-// a change to the color of the original will effect all the clones also
-original.material.color.setRGB(0, 1, 0);
-
-// CAMERA
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(8, 5, 8);
-camera.lookAt(0, 0, 0);
 
 // RENDER
 var renderer = new THREE.WebGLRenderer();
