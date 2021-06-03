44a23210ea72557a1acdf245281b6d52598257de
dustinpfister <dustin.pfister@gmail.com>
2021-06-03 11:46:32 -0400

    added some grid helper

--- views/demos/r127/object3d-traverse/js/main.js
+++ views/demos/r127/object3d-traverse/js/main.js
@@ -3,61 +3,21 @@
 
     // Scene
     var scene = new THREE.Scene();
+    // adding helpers
+    var helper = new THREE.GridHelper(10, 10, 'lime', '#ffffff');
+    scene.add(helper);
+    helper = new THREE.GridHelper(10, 10, 'lime', '#2a2a2a');
+    helper.rotation.z = Math.PI * 0.5;
+    scene.add(helper);
 
-    // Camera
+    // camera, renderer
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(3, 1, 3);
-
-    // creating an instance of Object3D
-    var obj = new THREE.Object3D();
-
-    // a mesh that inherits from Object3D
-    var low = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshNormalMaterial());
-    low.position.y = -1;
-    obj.add(low);
-
-    var high = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshNormalMaterial());
-    high.rotation.set(0, 1, 0);
-    obj.add(high);
-
-    scene.add(obj);
-
-    // Render
+    camera.position.set(10, 8, 10);
+    camera.lookAt(0, 0, 0);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
-
-
-    // Orbit Controls
-    var controls = new THREE.OrbitControls(camera, renderer.domElement);
-    camera.lookAt(0,  - .5, 0);
-
-    // loop
-    var frame = 0,
-    maxFrame = 100;
-    var loop = function () {
-
-        var per = frame / maxFrame,
-        bias = Math.abs(.5 - per) / .5;
-
-        requestAnimationFrame(loop);
-        renderer.render(scene, camera);
-
-        high.rotation.set(0, Math.PI * 2 * per, 0);
-        low.rotation.set(0, -Math.PI * 2 * per, 0);
-        obj.position.set(0, -1 + 2 * bias, 0);
-
-        frame += 1;
-        frame = frame % maxFrame;
-
-    };
-
     renderer.render(scene, camera);
-    loop();
 
 }
     ());
