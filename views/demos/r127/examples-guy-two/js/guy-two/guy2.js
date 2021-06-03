a32bc026dc31a7e51bbc2d1d4b2bc2808efe0682
dustinpfister <dustin.pfister@gmail.com>
2021-06-03 16:29:09 -0400

    arm

--- views/demos/r127/examples-guy-two/js/guy-two/guy2.js
+++ views/demos/r127/examples-guy-two/js/guy-two/guy2.js
@@ -4,7 +4,9 @@
     var HARD_MATERIAL = new THREE.MeshNormalMaterial();
 
     var DEFAULT_MATERIALS = {
-        chest: HARD_MATERIAL
+        head: HARD_MATERIAL,
+        chest: HARD_MATERIAL,
+        arm: HARD_MATERIAL
     };
 
     api.create = function(opt){
@@ -20,9 +22,14 @@
         group.add(chest);
 
         // add head
-        var head = new THREE.Mesh(new THREE.BoxGeometry(opt.headSize, opt.headSize, opt.headSize), opt.materials.chest);
+        var head = new THREE.Mesh(new THREE.BoxGeometry(opt.headSize, opt.headSize, opt.headSize), opt.materials.head);
         head.position.set(0, opt.chestLength / 2 + opt.headSize / 2 + opt.spacing, 0);
         group.add(head);
+
+        // left arm
+        var leftArm = new THREE.Mesh(new THREE.BoxGeometry(0.5, 1, 0.5), opt.materials.arm);
+        leftArm.position.set(0.75 + opt.spacing, 0, 0);
+        group.add(leftArm);
 
         return group;
 
--- views/demos/r127/examples-guy-two/js/main.js
+++ views/demos/r127/examples-guy-two/js/main.js
@@ -2,8 +2,15 @@
 var scene = new THREE.Scene();
 scene.add( new THREE.GridHelper(10, 10, 'white', 'lime') );
 
-var guy = Guy2.create({spacing: 0.1});
+var guy = Guy2.create({spacing: 0.05});
 scene.add(guy);
+
+var box = new THREE.Mesh( new THREE.BoxGeometry(1, 1, 1), new THREE.MeshNormalMaterial() );
+box.position.set(3, 0, 3);
+scene.add(box);
+
+guy.lookAt(box.position);
+
 
 // camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
