07086f418d6a7c9b1266286d23726bda478cf88e
dustinpfister <dustin.pfister@gmail.com>
2021-06-03 11:57:16 -0400

    changing color of all helpers

--- views/demos/r127/object3d-traverse/js/main.js
+++ views/demos/r127/object3d-traverse/js/main.js
@@ -3,20 +3,33 @@
 
     // Scene
     var scene = new THREE.Scene();
-    // adding helpers
-    var helper = new THREE.GridHelper(10, 10, 'lime', '#ffffff');
+
+    // ADDING GRID HELPERS TO THE SCENE
+    var helper = new THREE.GridHelper(10, 10, 'white', '#2a2a2a');
     scene.add(helper);
-    helper = new THREE.GridHelper(10, 10, 'lime', '#2a2a2a');
+    helper = new THREE.GridHelper(10, 10, 'white', '#2a2a2a');
     helper.rotation.z = Math.PI * 0.5;
     scene.add(helper);
+
+    // ADDING A MESH
+    scene.add( new THREE.Mesh( new THREE.BoxGeometry(1,1, 1), new THREE.MeshNormalMaterial() ) )
 
     // camera, renderer
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
     camera.position.set(10, 8, 10);
     camera.lookAt(0, 0, 0);
+    scene.add(camera); // ADDING CAMERA OBJECT TO THE SCENE
+
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
+
+    scene.traverse(function(obj){
+        if(obj.type === 'GridHelper'){
+            console.log(obj.material.color = new THREE.Color(0, 1, 0));
+        }
+    });
+
     renderer.render(scene, camera);
 
 }
