82cadaebbb27db3cf6b04413f889bb7a44c13b37
dustinpfister <dustin.pfister@gmail.com>
2021-06-04 15:26:33 -0400

    old tree examples copied over to r127 folder

---
+++ views/demos/r127/examples-tree/js/main.js
@@ -0,0 +1,36 @@
+
+
+(function () {
+    // SCENE
+    var scene = new THREE.Scene();
+    // CAMERA
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(8, 8, 8);
+    camera.lookAt(0, 0, 0);
+    // LIGHT
+    scene.add(camera);
+    var light = new THREE.PointLight(0xffffff);
+    camera.add(light);
+
+    // BASIC TREE
+    var tree = new Tree({
+            coneMaterial: new THREE.MeshStandardMaterial({
+                color: 0x00af00
+            })
+        });
+    scene.add(tree.group);
+
+    // RENDER
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    // CONTROLS
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    // LOOP
+    var loop = function () {
+        requestAnimationFrame(loop);
+        renderer.render(scene, camera);
+    };
+    loop();
+}
+    ());
