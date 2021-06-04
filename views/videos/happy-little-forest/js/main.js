9bcfaefe391e00f8e853e42b1dc40960bc608a18
dustinpfister <dustin.pfister@gmail.com>
2021-06-04 16:10:58 -0400

    we have some ground

--- views/videos/happy-little-forest/js/main.js
+++ views/videos/happy-little-forest/js/main.js
@@ -3,12 +3,23 @@
     // SCENE
     var scene = new THREE.Scene();
 
+    // GROUND
+    var ground = TileMod.create({
+            w: 100,
+            h: 100,
+            sw: 32,
+            sh: 32
+        });
+    TileMod.setCheckerBoard(ground);
+    scene.add(ground);
+
     // GUY
     var guy = GuyMod.create();
+    guy.group.position.set(0, 3, 0);
     scene.add(guy.group);
 
     // CAMERA
-    var camera = new THREE.PerspectiveCamera(50, 8 / 6, 0.05, 100);
+    var camera = new THREE.PerspectiveCamera(40, 8 / 6, 0.05, 100);
     camera.add(new THREE.PointLight());
     scene.add(camera);
 
@@ -28,7 +39,7 @@
     video.sequence.push({
         maxFrame: 30,
         forFrame: function (seq) {
-            camera.position.set(5, 5, 5);
+            camera.position.set(10, 8, 10);
             camera.lookAt(guy.group.position)
             renderer.render(scene, camera);
         }
