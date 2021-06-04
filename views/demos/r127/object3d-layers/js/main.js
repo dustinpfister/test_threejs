03682cdbda66ca4906926bf638d8e51927045634
dustinpfister <dustin.pfister@gmail.com>
2021-06-04 09:34:11 -0400

    basic idea working

--- views/demos/r127/object3d-layers/js/main.js
+++ views/demos/r127/object3d-layers/js/main.js
@@ -3,10 +3,14 @@
 
     // Scene
     var scene = new THREE.Scene();
+    var grid = new THREE.GridHelper(10, 10);
+    grid.layers.set(0);
+    scene.add(grid);
 
     // Camera
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
     camera.position.set(5, 5, 5);
+    camera.lookAt(0, 0, 0);
 
     // Render
     var renderer = new THREE.WebGLRenderer();
@@ -18,20 +22,26 @@
     layerModes = [[0], [1], [0, 1]],
     layerModeIndex = 0;
 
-    var setToLayerMode = function(obj, index){
-		
-		
-	};
+    var setToLayerMode = function (obj, index) {
+        obj.layers.disableAll();
+        layerModes[index].forEach(function (layerNum) {
+            obj.layers.enable(layerNum);
+        });
+    };
 
     var loop = function () {
         var now = new Date(),
         secs = (now - lt) / 1000;
         requestAnimationFrame(loop);
-        if (secs > 3) {
+        if (secs > 1) {
+            setToLayerMode(camera, layerModeIndex);
+            layerModeIndex += 1;
+            layerModeIndex %= layerModes.length;
             renderer.render(scene, camera);
             lt = now;
         }
     };
+    setToLayerMode(camera, layerModeIndex);
     renderer.render(scene, camera);
     loop();
 
