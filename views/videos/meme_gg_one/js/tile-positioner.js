5e7379914832c9ec918896dcc48b60bfb464a73e
dustinpfister <dustin.pfister@gmail.com>
2021-06-05 12:05:31 -0400

    tile values look good

--- views/videos/meme_gg_one/js/tile-positioner.js
+++ views/videos/meme_gg_one/js/tile-positioner.js
@@ -5,11 +5,16 @@
         console.log(plane);
 
         var param = plane.geometry.parameters,
-        len = param.width * param.height,
+        len = param.widthSegments * param.heightSegments,
         i = 0,
+        x,
+        z,
         tileX,
-        tileY;
+        tileZ;
         while (i < len) {
+            tileX = i % param.widthSegments;
+            tileZ = Math.floor(i / param.widthSegments);
+            console.log(i, tileX, tileZ);
             i += 1;
         }
     };
