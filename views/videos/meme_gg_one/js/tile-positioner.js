ec3a9196c6dee65e5ebb1dcdecc3f1f675803233
dustinpfister <dustin.pfister@gmail.com>
2021-06-05 12:08:39 -0400

    default forCell function

--- views/videos/meme_gg_one/js/tile-positioner.js
+++ views/videos/meme_gg_one/js/tile-positioner.js
@@ -2,7 +2,9 @@
 
     api.forCells = function (plane, opt) {
         opt = opt || {};
-        console.log(plane);
+        opt.forCell = opt.forCell || function (plane, x, y, tileX, tileZ, i) {
+            console.log(i, tileX, tileZ);
+        };
 
         var param = plane.geometry.parameters,
         len = param.widthSegments * param.heightSegments,
@@ -14,7 +16,9 @@
         while (i < len) {
             tileX = i % param.widthSegments;
             tileZ = Math.floor(i / param.widthSegments);
-            console.log(i, tileX, tileZ);
+            x = 0;
+            y = 0;
+            opt.forCell(plane, x, y, tileX, tileZ, i);
             i += 1;
         }
     };
