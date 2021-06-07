09de5271f1b7e7c0b0c409d812581b8aea240c06
dustinpfister <dustin.pfister@gmail.com>
2021-06-07 12:16:18 -0400

    using object for pos

--- views/demos/r127/buffer-geometry-attributes-position-tri-helper/js/main.js
+++ views/demos/r127/buffer-geometry-attributes-position-tri-helper/js/main.js
@@ -2,18 +2,20 @@
 (function () {
 
     // set location of a vert given an index value in geometry.index
-    var setVert = function(geometry, vertIndex, x, y, z){
+    var setVert = function(geometry, vertIndex, pos){
+        pos = pos || {};
         var posIndex = geometry.index.array[vertIndex] * 3;
-        position.array[posIndex] = x === undefined ? position.array[posIndex]: x;
-        position.array[posIndex + 1] = y === undefined ? position.array[posIndex + 1]: y;
-        position.array[posIndex + 2] = z === undefined ? position.array[posIndex + 2]: z;
+        position.array[posIndex] = pos.x === undefined ? position.array[posIndex]: pos.x;
+        position.array[posIndex + 1] = pos.y === undefined ? position.array[posIndex + 1]: pos.y;
+        position.array[posIndex + 2] = pos.z === undefined ? position.array[posIndex + 2]: pos.z;
     };
 
-    var setTri = function(geometry, triIndex, x, y, z){
+    var setTri = function(geometry, triIndex, pos){
+        pos = pos || {};
         var vertIndex = triIndex * 3;
-        setVert(geometry, vertIndex, x, y, z);
-        setVert(geometry, vertIndex + 1, x, y, z);
-        setVert(geometry, vertIndex + 2, x, y, z);
+        setVert(geometry, vertIndex, pos);
+        setVert(geometry, vertIndex + 1, pos);
+        setVert(geometry, vertIndex + 2, pos);
     };
 
     // scene
@@ -35,28 +37,20 @@
     console.log( index.count / 3);   /* 12 (index.count / 3 === number of triangles ) */
 
     // example 2 on set tri helper
-    setTri(geometry, 0, 1);
-    setTri(geometry, 1, 1);
+    setTri(geometry, 0, {x: 1});
+    setTri(geometry, 1, {x: 1});
+    setTri(geometry, 2, {x: -1});
+    setTri(geometry, 3, {x: -1});
 
-    setTri(geometry, 2, -1);
-    setTri(geometry, 3, -1);
+    setTri(geometry, 4, {y: 1});
+    setTri(geometry, 5, {y: 1});
+    setTri(geometry, 6, {y: -1});
+    setTri(geometry, 7, {y: -1});
 
-    setTri(geometry, 4, undefined, 1);
-    setTri(geometry, 5, undefined, 1);
-
-    setTri(geometry, 6, undefined, -1);
-    setTri(geometry, 7, undefined, -1);
-
-    setTri(geometry, 8, undefined, undefined, 1);
-    setTri(geometry, 9, undefined, undefined, 1);
-
-    setTri(geometry, 10, undefined, undefined, -1);
-    setTri(geometry, 11, undefined, undefined, -1);
-
-    // example 1 on set vert
-    //setVert(geometry, 0, 1, 0.25, 1.25);
-    //setVert(geometry, 16, 1, 0.25, 1.25);
-    //setVert(geometry, 26, 1, 0.25, 1.25);
+    setTri(geometry, 8, {z: 1});
+    setTri(geometry, 9, {z: 1});
+    setTri(geometry, 10, {z: -1});
+    setTri(geometry, 11, {z: -1});
 
 
     var mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({
