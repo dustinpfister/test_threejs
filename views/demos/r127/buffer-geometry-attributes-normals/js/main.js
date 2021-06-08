18678a23ef152e8aa91a960925a48093b45ae378
dustinpfister <dustin.pfister@gmail.com>
2021-06-08 13:05:34 -0400

    comments

--- views/demos/r127/buffer-geometry-attributes-normals/js/main.js
+++ views/demos/r127/buffer-geometry-attributes-normals/js/main.js
@@ -7,17 +7,17 @@
     // GEOMETRY - starting with a cube
     var geometry = new THREE.BoxGeometry(1, 1, 1);
 
-    // check out the normal attribute of a cube
-    var normal = geometry.getAttribute('normal');
-    var position = geometry.getAttribute('position');
-    console.log(normal);
-	console.log(position);
-
     // use the geometry with a mesh
     var mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({
                 side: THREE.FrontSide //THREE.DoubleSide
             }));
 
+
+    // check out the normal attribute of a cube
+    var normal = geometry.getAttribute('normal');
+    var position = geometry.getAttribute('position');
+
+    // create and set up an arrow helper to find the direction of the first normal value
     var dir = new THREE.Vector3(normal.array[0], normal.array[1], normal.array[2]),
     origin = new THREE.Vector3(position.array[0], position.array[1], position.array[2]);
     var helper = new THREE.ArrowHelper(dir, origin, 1, 0x00ff00);
