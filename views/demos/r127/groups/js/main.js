a25d71f52d3ae605412ecca68cde01e940db8ff0
dustinpfister <dustin.pfister@gmail.com>
2021-06-09 14:14:06 -0400

    commnets

--- views/demos/r127/groups/js/main.js
+++ views/demos/r127/groups/js/main.js
@@ -3,17 +3,15 @@
 
     // Scene
     var scene = new THREE.Scene();
-
-    // Camera
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 50);
-    camera.position.set(10, 10, 10);
-    camera.lookAt(0, 0, 0);
+    scene.add(new THREE.GridHelper(10, 10));
 
     // CREATING A GROUP
     var group = new THREE.Group();
     var i = 0,
+    radius = 2,
     count = 5;
     while (i < count) {
+        // creating a mesh
         var bx = new THREE.Mesh(
                 new THREE.BoxGeometry(1, 1, 1),
                 new THREE.MeshBasicMaterial({
@@ -21,17 +19,21 @@
                     wireframe: true
                 })),
         r = Math.PI * 2 / count * i;
+        // set position of mesh
         bx.position.set(
-            Math.cos(r) * 4,
+            Math.cos(r) * radius,
             0,
-            Math.sin(r) * 4);
-        bx.lookAt(0, 0, 0);
+            Math.sin(r) * radius);
+        // add mesh to the group
         group.add(bx);
         i += 1;
     }
     scene.add(group);
 
-    // Render
+    // Camera and Render
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 50);
+    camera.position.set(8, 8, 8);
+    camera.lookAt(0, 0, 0);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
