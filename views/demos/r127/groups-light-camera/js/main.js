964eb1abee1516abf77f06873e1347ea12a7ab38
dustinpfister <dustin.pfister@gmail.com>
2021-06-09 14:32:57 -0400

    cleaned up the camera example

--- views/demos/r127/groups-light-camera/js/main.js
+++ views/demos/r127/groups-light-camera/js/main.js
@@ -8,10 +8,7 @@
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 50);
     camera.position.set(10, 10, 10);
     camera.lookAt(0, 0, 0);
-
-    // Orbit Controls
-    var controls = new THREE.OrbitControls(camera);
-
+    scene.add(camera); // adding the camera to the scene
 
     // positioning a light above the camera
     var light = new THREE.PointLight();
@@ -20,28 +17,20 @@
 
     // positioning a mesh in front of the camera
     var withCamera = new THREE.Mesh(
-
             new THREE.BoxGeometry(.1, .1, .1),
             new THREE.MeshStandardMaterial({
-
                 color: 0xffffff,
                 emissive: 0x1a1a1a
-
             }));
     withCamera.position.set( - .25, .2,  - .75);
     camera.add(withCamera);
 
-    // adding the camera to the scene
-    scene.add(camera);
-
+    // adding another mesh object directly to the scene
     scene.add(new THREE.Mesh(
             new THREE.BoxGeometry(5, 5, 5),
-
             new THREE.MeshStandardMaterial({
-
                 color: 0x00ff00
-
-            })))
+            })));
 
     // Render
     var renderer = new THREE.WebGLRenderer();
@@ -52,22 +41,15 @@
     var frame = 0,
     maxFrame = 500;
     var loop = function () {
-
         var per = frame / maxFrame,
         bias = Math.abs(.5 - per) / .5;
-
         requestAnimationFrame(loop);
         renderer.render(scene, camera);
-
         withCamera.rotation.set(Math.PI * 4 * per,
             Math.PI * 2 * per, 0);
-
         frame += 1;
         frame = frame % maxFrame;
-
     };
-
-    renderer.render(scene, camera);
     loop();
 
 }
