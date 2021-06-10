f9b4dfb9ce1b9c8576009d6c4b0b8db2bb9b326e
dustinpfister <dustin.pfister@gmail.com>
2021-06-10 12:21:54 -0400

    arrow helpers and noramls might helper with this

--- views/demos/r127/examples-sphere-mutate/js/main.js
+++ views/demos/r127/examples-sphere-mutate/js/main.js
@@ -1,18 +1,32 @@
 
 (function () {
 
+    // set a given arrow helper to the given normal index
+    var setArrowHelperToNormal = function (geometry, arrowHelper, normalIndex) {
+        // check out the normal attribute of a cube
+        var normal = geometry.getAttribute('normal');
+        var position = geometry.getAttribute('position');
+        var values = normal.array.slice(normalIndex * 3, normalIndex * 3 + 3);
+        var dir = new THREE.Vector3(values[0], values[1], values[2]);
+        var values = position.array.slice(normalIndex * 3, normalIndex * 3 + 3);
+        var origin = new THREE.Vector3(values[0], values[1], values[2]);
+        arrowHelper.setDirection(dir);
+        arrowHelper.position.copy(origin);
+        arrowHelper.setColor(0x00ff00);
+    };
+
     // set location of a vert given an index value in geometry.index
-    var setVert = function(geometry, vertIndex, pos){
+    var setVert = function (geometry, vertIndex, pos) {
         pos = pos || {};
         var posIndex = geometry.index.array[vertIndex] * 3,
         position = geometry.getAttribute('position');
-        position.array[posIndex] = pos.x === undefined ? position.array[posIndex]: pos.x;
-        position.array[posIndex + 1] = pos.y === undefined ? position.array[posIndex + 1]: pos.y;
-        position.array[posIndex + 2] = pos.z === undefined ? position.array[posIndex + 2]: pos.z;
+        position.array[posIndex] = pos.x === undefined ? position.array[posIndex] : pos.x;
+        position.array[posIndex + 1] = pos.y === undefined ? position.array[posIndex + 1] : pos.y;
+        position.array[posIndex + 2] = pos.z === undefined ? position.array[posIndex + 2] : pos.z;
     };
 
     // set pos for tri index
-    var setTri = function(geometry, triIndex, pos){
+    var setTri = function (geometry, triIndex, pos) {
         pos = pos || {};
         var vertIndex = triIndex * 3;
         setVert(geometry, vertIndex, pos);
@@ -26,13 +40,27 @@
     // GEOMETRY
     var geometry = new THREE.SphereGeometry(0.5, 15, 15);
     var mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({
-        side: THREE.DoubleSide
-    }));
+                //wireframe: true,
+                side: THREE.DoubleSide
+            }));
     scene.add(mesh);
+
+    var pos = {
+        x: 0,
+        y: 0.25,
+        z: 0
+    };
+
+    var helper = new THREE.ArrowHelper();
+    scene.add(helper);
+
+    setArrowHelperToNormal(geometry, helper, 3);
+
+    setVert(geometry, 3, pos);
 
     // CAMERA
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
-    camera.position.set(1, 1, 1);
+    camera.position.set(-2, 2, 2);
     camera.lookAt(mesh.position);
 
     // RENDER
@@ -44,11 +72,11 @@
     lt = new Date(),
     maxFrames = 300,
     FPS = 30;
-    var loop = function(){
+    var loop = function () {
         var now = new Date(),
         secs = (now - lt) / 1000;
         requestAnimationFrame(loop);
-        if(secs > 1 / FPS){
+        if (secs > 1 / FPS) {
             per += 1 / (maxFrames / FPS) * secs;
             per %= 1;
             renderer.render(scene, camera);
