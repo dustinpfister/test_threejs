83a0542e47d25aac02395327f16c52a0372261bc
dustinpfister <dustin.pfister@gmail.com>
2021-06-13 10:06:53 -0400

    using normal material

--- views/demos/r127/vector3/js/main.js
+++ views/demos/r127/vector3/js/main.js
@@ -6,23 +6,29 @@
     scene.add(new THREE.GridHelper(9, 9));
 
     // length
-    var r = Math.PI / 180 * 45,
-    x = Math.cos(r),
-    y = Math.sin(r),
+    var r = Math.PI / 180 * 90,
+    x = Math.cos(r) * 2,
+    z = Math.sin(r) * 2,
 
-    vec = new THREE.Vector3(x, y, 0);
+    vec = new THREE.Vector3(x, 0, z);
 
     console.log(vec.isVector3); // true
     console.log(vec.x, vec.y, vec.z); // 0.70... 0.70... 0
     console.log(vec.length()); // 1
 
+    // just creating and adding a cube
     var cube = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshBasicMaterial({
-                color: 0xffffff,
-                wireframe: true
-            }));
+            new THREE.MeshNormalMaterial());
+    cube.position.copy(vec);
     scene.add(cube);
+
+
+    // just creating and adding a cube
+    var cube2 = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshNormalMaterial());
+    scene.add(cube2);
 
     // CAMERA
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
@@ -31,16 +37,7 @@
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.render(scene, camera);
 
-    // Orbit Controls
-    var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-    var loop = function () {
-        requestAnimationFrame(loop);
-        controls.update();
-        renderer.render(scene, camera);
-    };
-
-    loop();
 }
     ());
