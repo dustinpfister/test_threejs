6bfbf269ef5a4d7312bf1363aae75dcee76036f2
dustinpfister <dustin.pfister@gmail.com>
2021-06-13 09:58:58 -0400

    cleaner basic example

--- views/demos/r127/vector3/js/main.js
+++ views/demos/r127/vector3/js/main.js
@@ -1,6 +1,10 @@
 
 (function () {
 
+    // scene
+    var scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(9, 9));
+
     // length
     var r = Math.PI / 180 * 45,
     x = Math.cos(r),
@@ -12,70 +16,6 @@
     console.log(vec.x, vec.y, vec.z); // 0.70... 0.70... 0
     console.log(vec.length()); // 1
 
-    // addScalar
-    var vec = new THREE.Vector3(3, 3, 7);
-    vec.addScalar(10);
-
-    console.log(vec.x, vec.y, vec.z); // 13 13 17
-
-    // distance
-    var a = new THREE.Vector3(10, 10, 10),
-    b = new THREE.Vector3(10, 5, 10);
-    console.log(a.distanceTo(b)); // 5
-
-    // clone
-    var original = new THREE.Vector3(10, 10, 10),
-    copy = original.clone();
-
-    copy.x += 5;
-
-    console.log(copy.x); // 15
-    console.log(original.x); // 10
-
-    // copy
-    var a = new THREE.Vector3(1, 2, 1),
-    copy = new THREE.Vector3().copy(a);
-
-    copy.z += 2;
-
-    console.log(a.z); // 1
-    console.log(copy.z); // 3
-
-    // normalize
-    var vec = new THREE.Vector3(7, 7, 7);
-
-    console.log(vec.length()); // 12.12...
-
-    vec.normalize();
-
-    console.log(vec.x, vec.y, vec.z); // 0.57... 0.57... 0.57...
-    console.log(vec.length()); // 1
-
-    // SCENE
-    var scene = new THREE.Scene();
-
-    // CAMERA
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
-    camera.position.set(1, 1, 1);
-    camera.lookAt(0, 0, 0);
-
-    /*
-    var a = new THREE.Vector3(5, 5, 5),
-    b = new THREE.Vector3(-5, -5, -5);
-
-    var geometry = new THREE.Geometry();
-
-    geometry.vertices.push(a, b);
-    geometry.normalize();
-
-    var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({
-    color: 0x0000ff
-    }));
-
-    scene.add(line);
-
-     */
-
     var cube = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshBasicMaterial({
@@ -84,7 +24,10 @@
             }));
     scene.add(cube);
 
-    // RENDER
+    // CAMERA
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(3, 3, 3);
+    camera.lookAt(0, 0, 0);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
