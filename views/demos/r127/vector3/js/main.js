8cce9cb51ae767216cd257a1c03b1508287699b8
dustinpfister <dustin.pfister@gmail.com>
2021-06-13 10:13:05 -0400

    commnets

--- views/demos/r127/vector3/js/main.js
+++ views/demos/r127/vector3/js/main.js
@@ -16,18 +16,24 @@
     console.log(vec.x, vec.y, vec.z); // 0.70... 0.70... 0
     console.log(vec.length()); // 1
 
-    // just creating and adding a cube
+    // THE POSITION PROPERTY OF A MESH (OR ANYTHING BASED OFF OF OBJECT3D )
+    // IS AN INSTANCE OF Vector3 SO I CAN USE THE Vector3.copy METHOD TO
+    // COPY THE VALUES OF A STAND ALONE INSTANCE OF Vector3 TO THE INSTANCE
+    // OF Vector3 STORED IN THE POSITON PROPERTY OF A MESH TO SET THE POSTION
+    // OF THE MESH
     var cube = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshNormalMaterial());
     cube.position.copy(vec);
     scene.add(cube);
 
-
-    // just creating and adding a cube
+    // I CAN ALSO USE THE Vector3.set METHOD OF THE INSTNACE OF Vector3 IN THE
+    // POSIITON PROPERTY TO SET THE POSIITON OF THE MESH WITH NUMBERS IN THE FORM
+    // OF NUMBER LITERALS AND/OR VARIABLES
     var cube2 = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshNormalMaterial());
+    cube2.position.set(2, 0, 0);
     scene.add(cube2);
 
     // CAMERA
