c6b618974c3aeff9d147aafc3ea79b0517767b13
dustinpfister <dustin.pfister@gmail.com>
2021-06-15 17:46:39 -0400

    clean up

--- views/videos/bw_1_are_you_okay/js/main.js
+++ views/videos/bw_1_are_you_okay/js/main.js
@@ -5,11 +5,9 @@
     // SCENE
     var scene = new THREE.Scene();
     scene.background = new THREE.Color('#00afaf');
-    //scene.add(new THREE.GridHelper(7, 7));
 
     // WORLD
     var world = BetaWorld.create();
-    //world.position.set(0, -30, 0);
     scene.add(world);
 
     // GUY1
@@ -26,17 +24,6 @@
     guy2.group.lookAt(2, 1.55, 1);
     scene.add(guy2.group);
 
-    // LIGHT
-/*
-    var light_sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 30, 30));
-    light_sphere.position.set(0, 10, -5);
-    var light = new THREE.PointLight(0xffffff, 1);
-    light_sphere.add(light);
-    scene.add(light_sphere);
-    var light_ambient = new THREE.AmbientLight(0xffffff, 0.25);
-    scene.add(light_ambient);
-*/
-
     // CAMERA
     var camera = new THREE.PerspectiveCamera(40, 8 / 6, 0.05, 150);
     camera.position.set(10, 10, 10);
@@ -50,7 +37,7 @@
     container.appendChild(renderer.domElement);
 
     var video = {
-        frame: 1470, 
+        frame: 0, // 1470 - sq10
         canvas: renderer.domElement,
         sequence: []
     };
@@ -67,8 +54,6 @@
             guy1.arm_left.rotation.z = 0;
             guy1.arm_right.rotation.z = 0;
             guy1.body.rotation.y = 0;
-            //guy1.group.position.set(-2, 1.55, 0);
-            //guy1.group.position.set(0, 1.55, 0);
             BetaWorld.positionObject(world, guy1.group, 0, 0, 1.55);
 
             // GUY2
@@ -76,13 +61,6 @@
             BetaWorld.positionObject(world, guy2.group, 0.05, 0, 1.55);
 
             // CAMERA
-/*
-            var x = 10 - 10 * seq.per,
-            y = 10 - 5 * seq.per,
-            z = 10 - 5 * seq.per;
-            camera.position.set(x, y, z);
-*/
-
             var camLat = 0.14 - 0.07 * seq.per,
             camLong = 0.15,
             camAlt = 10 - 5 * seq.per;
@@ -106,7 +84,6 @@
             guy1.arm_left.rotation.z = 0;
             guy1.arm_right.rotation.z = 0;
             guy1.body.rotation.y = 0;
-            //guy1.group.position.set(-2, 1.55, 0);
             BetaWorld.positionObject(world, guy1.group, 0, 0, 1.55);
 
             // GUY2
@@ -114,9 +91,6 @@
             BetaWorld.positionObject(world, guy2.group, 0.05, 0, 1.55);
 
             // CAMERA
-            //camera.position.set(0, 5, 5);
-            //camera.lookAt(guy2.group.position);
-
             var camLat = 0.07,
             camLong = 0.15,
             camAlt = 5;
@@ -139,7 +113,6 @@
             guy1.arm_left.rotation.z = 0;
             guy1.arm_right.rotation.z = 0;
             guy1.body.rotation.y = 0;
-            //guy1.group.position.set(-2, 1.55, 0);
             BetaWorld.positionObject(world, guy1.group, 0, 0, 1.55);
 
             // GUY2
@@ -147,9 +120,6 @@
             BetaWorld.positionObject(world, guy2.group, 0.05, 0, 1.55);
 
             // CAMERA
-            //camera.position.set(0, 5, 5);
-            //camera.lookAt(guy2.group.position);
-
             var camLat = 0.07,
             camLong = 0.15,
             camAlt = 5;
@@ -173,7 +143,6 @@
             guy1.arm_left.rotation.z = 0;
             guy1.arm_right.rotation.z = 0;
             guy1.body.rotation.y = 0;
-            //guy1.group.position.set(-2, 1.55, 0);
             BetaWorld.positionObject(world, guy1.group, 0, 0, 1.55);
 
             // GUY2
@@ -181,26 +150,14 @@
             BetaWorld.positionObject(world, guy2.group, 0.05, 0, 1.55);
 
             // CAMERA
-            //camera.position.set(0, 5, 5);
-            //var v = new THREE.Vector3()
-            //v.copy(guy2.group.position);
-            //v.x = guy2.group.position.x - 4 * seq.per
-            //camera.lookAt(guy1.group.position);
-
             var camLat = 0.07,
             camLong = 0.15,
             camAlt = 5;
             BetaWorld.positionObject(world, camera, camLat, camLong, camAlt);
-
             var v = guy2.group.position.clone(),
             d = guy2.group.position.distanceTo(guy1.group.position);
-
             v.x = guy2.group.position.x - d * seq.per;
             v.y = guy2.group.position.y + 0.2 * seq.per;
-            
-            //console.log(v);
-            //console.log(guy1.group.position)
-
             camera.lookAt(v);
 
             renderer.render(scene, camera);
@@ -219,7 +176,6 @@
             guy1.arm_left.rotation.z = 0;
             guy1.arm_right.rotation.z = 0;
             guy1.body.rotation.y = 0;
-            //guy1.group.position.set(-2, 1.55, 0);
             BetaWorld.positionObject(world, guy1.group, 0, 0, 1.55);
 
             // GUY2
@@ -249,7 +205,6 @@
             guy1.arm_left.rotation.z = 0;
             guy1.arm_right.rotation.z = 0;
             guy1.body.rotation.y = 0;
-            //guy1.group.position.set(-2, 1.55, 0);
             BetaWorld.positionObject(world, guy1.group, 0, 0, 1.55);
 
             // GUY2
@@ -257,13 +212,6 @@
             BetaWorld.positionObject(world, guy2.group, 0.05, 0, 1.55);
 
             // CAMERA
-/*
-            var x = 5 * seq.per,
-            y = 5,
-            z = 5 - 5 * seq.per;
-            camera.position.set(x, 5, z);
-            camera.lookAt(guy1.group.position);
-*/
             var camLat = 0.07 + 0.02 * seq.per,
             camLong = 0.15 - 0.15 * seq.per,
             camAlt = 5 + 1 * seq.per;
@@ -286,7 +234,6 @@
             guy1.arm_left.rotation.z = 0;
             guy1.arm_right.rotation.z = 0;
             guy1.body.rotation.y = 0;
-            //guy1.group.position.set(-2, 1.55, 0);
             BetaWorld.positionObject(world, guy1.group, 0, 0, 1.55);
 
             // GUY2
@@ -294,9 +241,6 @@
             BetaWorld.positionObject(world, guy2.group, 0.05, 0, 1.55);
 
             // CAMERA
-            //camera.position.set(5, 5, 0);
-            //camera.lookAt(guy1.group.position);
-
             var camLat = 0.09,
             camLong = 0,
             camAlt = 6;
@@ -319,7 +263,6 @@
             guy1.arm_left.rotation.z = 0;
             guy1.arm_right.rotation.z = 0;
             guy1.body.rotation.y = 0;
-            //guy1.group.position.set(-2, 1.55, 0);
             BetaWorld.positionObject(world, guy1.group, 0, 0, 1.55);
 
             // GUY2
@@ -327,8 +270,6 @@
             BetaWorld.positionObject(world, guy2.group, 0.05, 0, 1.55);
 
             // CAMERA
-            //camera.position.set(5, 5, 0);
-            //camera.lookAt(guy1.group.position);
             var camLat = 0.09,
             camLong = 0,
             camAlt = 6;
@@ -353,7 +294,6 @@
             guy1.arm_left.rotation.z = Math.PI * 0.5 * -1 * seq.per;
             guy1.arm_right.rotation.z = Math.PI * 0.5 * seq.per;
             guy1.body.rotation.y = Math.PI * 0.5 * seq.per;
-            //guy1.group.position.set(-2, 1.55, 0);
             BetaWorld.positionObject(world, guy1.group, 0, 0, 1.55);
 
             // GUY2
@@ -361,8 +301,6 @@
             BetaWorld.positionObject(world, guy2.group, 0.05, 0, 1.55);
 
             // CAMERA
-            //camera.position.set(5, 5, 0);
-            //camera.lookAt(guy1.group.position);
             var camLat = 0.09,
             camLong = 0,
             camAlt = 6;
@@ -388,7 +326,6 @@
             guy1.arm_right.rotation.z = Math.PI * 0.5;
             var a = Math.pow(seq.per * 4, 2);
             guy1.body.rotation.y = Math.PI * 0.5 + Math.PI * 2 * a;
-            //guy1.group.position.set(-2, 1.55, 0);
             BetaWorld.positionObject(world, guy1.group, 0, 0, 1.55);
 
             // GUY2
@@ -396,8 +333,6 @@
             BetaWorld.positionObject(world, guy2.group, 0.05, 0, 1.55);
 
             // CAMERA
-            //camera.position.set(5, 5, 0);
-            //camera.lookAt(guy1.group.position);
             var camLat = 0.09,
             camLong = 0,
             camAlt = 6;
@@ -422,8 +357,6 @@
             guy1.arm_right.rotation.z = Math.PI * 0.5;
             var a = (seq.secsTotal / 10 * 16) * seq.secs;
             guy1.body.rotation.y = Math.PI * 0.5 + Math.PI * 2 * a;
-
-//            guy1.group.position.set(-2, 1.55 + 8 * seq.per, 0);
             BetaWorld.positionObject(world, guy1.group, 0, 0, 1.55 + 8 * seq.per);
 
             // GUY2
@@ -431,8 +364,6 @@
             BetaWorld.positionObject(world, guy2.group, 0.05, 0, 1.55);
 
             // CAMERA
-            //camera.position.set(5 + 10 * seq.per, 5 - 10 * seq.per, 60 * seq.per);
-            //camera.lookAt(guy1.group.position);
             var camLat = 0.09 + 0.2 * seq.per,
             camLong = 0.25 * seq.per,
             camAlt = 6;  //+ 14 * seq.per;
