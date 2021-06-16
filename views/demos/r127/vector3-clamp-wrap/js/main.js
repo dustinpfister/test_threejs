1cdacbf335ecaec3c2f8b6f215caae264fce4d6e
dustinpfister <dustin.pfister@gmail.com>
2021-06-16 12:06:12 -0400

    strated a wrap demo

---
+++ views/demos/r127/vector3-clamp-wrap/js/main.js
@@ -0,0 +1,29 @@
+
+(function () {
+
+    // scene
+    var scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(5, 5));
+
+    // creating a mesh
+    var mesh = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshNormalMaterial());
+    scene.add(mesh);
+
+    mesh.position.set(0, 0, -5);
+    mesh.position.clamp(
+        new THREE.Vector3(-2, 0, -2),
+        new THREE.Vector3(2, 0, 2));
+
+    // CAMERA
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(5, 5, 5);
+    camera.lookAt(0, 0, 0);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+
+}
+    ());
