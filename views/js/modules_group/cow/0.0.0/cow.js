d3adeb49b207f3a9df3f1453198a29cda7d1a5d6
dustinpfister <dustin.pfister@gmail.com>
2021-06-17 18:02:31 -0400

    started legs

--- views/js/modules_group/cow/0.0.0/cow.js
+++ views/js/modules_group/cow/0.0.0/cow.js
@@ -7,11 +7,31 @@
         eyes : m
     };
 
-    var createBody = function(opt){
+    var createBodyGroup = function(opt){
         var group = new THREE.Group();
         var body = new THREE.Mesh(new THREE.BoxGeometry(1, 0.5, 0.5), opt.materials.body);
         body.position.set(0, 0.5, 0);
         group.add(body);
+        return group;
+    };
+
+    var createLeg = function(opt){
+        var leg = new THREE.Mesh(new THREE.BoxGeometry(0.125, 0.5, 0.125), opt.materials.body);
+        return leg;
+    };
+
+    var createLegsGroup = function(opt){
+        var group = new THREE.Group();
+
+        var leg1 = createLeg(opt);
+        leg1.position.set(0.9, 0.25, 0);
+        group.add(leg1);
+        var leg2 = createLeg(opt);
+        group.add(leg2);
+        var leg3 = createLeg(opt);
+        group.add(leg3);
+        var leg4 = createLeg(opt);
+        group.add(leg4);
         return group;
     };
 
@@ -21,8 +41,12 @@
         opt.materials = opt.materials || MATERIALS;
         var group = new THREE.Group();
         
-        var body = createBody(opt);
+        var body = createBodyGroup(opt);
         group.add(body);
+
+        var legs = createLegsGroup(opt);
+        group.add(legs);
+
         return group;
     };
 }
