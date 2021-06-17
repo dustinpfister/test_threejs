3fdebb2927cef8ba90f523577d212af816dd478b
dustinpfister <dustin.pfister@gmail.com>
2021-06-17 14:22:36 -0400

    commnets

--- views/demos/r127/vector3-apply-axis-angle-loop/js/main.js
+++ views/demos/r127/vector3-apply-axis-angle-loop/js/main.js
@@ -9,7 +9,6 @@
     var mesh = new THREE.Mesh(
             new THREE.ConeGeometry(0.5, 1, 30, 30),
             new THREE.MeshNormalMaterial());
-
     mesh.geometry.rotateX(Math.PI * 0.5);
     mesh.position.set(1, 0, 1);
     scene.add(mesh);
@@ -24,15 +23,18 @@
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
 
+    // LOOP
     var degree = 0,
     v,
     lt = new Date(),
     fps = 30;
+    // update method
     var update = function (secs) {
         v = new THREE.Vector3(0, 1, 0);
         degree = 45 * secs;
         mesh.position.applyAxisAngle(v, Math.PI / 180 * degree);
     };
+    // loop method
     var loop = function () {
         var now = new Date(),
         secs = (now - lt) / 1000;
