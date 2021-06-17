5fe99b47510c220fb049a19561da495e70252d4a
dustinpfister <dustin.pfister@gmail.com>
2021-06-17 13:54:26 -0400

    basic example looking good

--- views/demos/r127/vector3-apply-axis-angle/js/main.js
+++ views/demos/r127/vector3-apply-axis-angle/js/main.js
@@ -3,17 +3,24 @@
 
     // scene
     var scene = new THREE.Scene();
-    scene.add(new THREE.GridHelper(5, 5));
+    scene.add(new THREE.GridHelper(6, 6));
 
-    // creating a mesh
+    // mesh
     var mesh = new THREE.Mesh(
             new THREE.ConeGeometry(0.5, 1, 30, 30),
             new THREE.MeshNormalMaterial());
-    scene.add(mesh);
-    mesh.geometry.rotateX(Math.PI * 0.5);
-    mesh.lookAt(0, 1, 0);
 
-    // CAMERA
+    mesh.geometry.rotateX(Math.PI * 0.5);
+    mesh.position.set(1, 0, 1);
+    //mesh.lookAt(0, 3, 0);
+    scene.add(mesh);
+
+    var v = new THREE.Vector3(0, 1, 0);
+    mesh.position.applyAxisAngle(v, Math.PI / 180 * 180);
+
+    console.log(mesh.position.clone().round()); // {x: -1, y: 0, z: -1}
+
+    // camera, render
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
     camera.position.set(5, 5, 5);
     camera.lookAt(0, 0, 0);
