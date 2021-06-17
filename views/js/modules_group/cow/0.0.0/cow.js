9a74267211ddb6d312c621a87a387c9699a98b04
dustinpfister <dustin.pfister@gmail.com>
2021-06-17 17:55:39 -0400

    create body helper

--- views/js/modules_group/cow/0.0.0/cow.js
+++ views/js/modules_group/cow/0.0.0/cow.js
@@ -6,13 +6,22 @@
         head : m,
         eyes : m
     };
+
+    var createBody = function(opt){
+        var group = new THREE.Group();
+        var body = new THREE.Mesh(new THREE.BoxGeometry(1, 0.5, 0.5), opt.materials.body);
+        body.position.set(0, 0.5, 0);
+        group.add(body);
+        return group;
+    };
+
     // create method
     api.create = function (opt) {
         opt = opt || {};
         opt.materials = opt.materials || MATERIALS;
         var group = new THREE.Group();
-        // body mesh
-        var body = new THREE.Mesh(new THREE.BoxGeometry(), opt.materials.body);
+        
+        var body = createBody(opt);
         group.add(body);
         return group;
     };
