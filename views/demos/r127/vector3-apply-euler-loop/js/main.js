d2753f22a3f1310ddea3f34f9d65e94f05bc6763
dustinpfister <dustin.pfister@gmail.com>
2021-06-18 10:37:26 -0400

    clean up

--- views/demos/r127/vector3-apply-euler-loop/js/main.js
+++ views/demos/r127/vector3-apply-euler-loop/js/main.js
@@ -9,6 +9,7 @@
         return cube;
     };
 
+    // vector fro angles helper
     var vectorFromAngles = function (a, b, c, len, startVec) {
         len = len === undefined ? 1 : len;
         startVec = startVec === undefined ? new THREE.Vector3(1, 0, 0) : startVec;
@@ -26,12 +27,11 @@
 
     var cube = createCube();
     scene.add(cube);
-
+    // USING MY VECTOR FROM ANGLES METHOD
     var v = vectorFromAngles(90, 0, 0, 1);
-    console.log(v);
     cube.position.copy(v);
 
-    // CAMERA
+    // camera, render
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
     camera.position.set(5, 5, 5);
     camera.lookAt(0, 0, 0);
@@ -39,6 +39,7 @@
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
 
+    // loop
     var lt = new Date(),
     a = 0,
     b = 0,
@@ -47,15 +48,12 @@
     var loop = function () {
         var now = new Date(),
         secs = (now - lt) / 1000;
-
         requestAnimationFrame(loop);
-
         if (secs > 1 / fps) {
             b += 90 * secs;
             b %= 360;
             var v = vectorFromAngles(a, b, c, 1.5);
             cube.position.copy(v);
-
             lt = now;
             renderer.render(scene, camera);
         }
