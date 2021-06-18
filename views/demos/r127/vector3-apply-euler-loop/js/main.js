6e499a61bc6cc7a1345379ada55904c70cee80e7
dustinpfister <dustin.pfister@gmail.com>
2021-06-18 10:35:55 -0400

    start vec argument

--- views/demos/r127/vector3-apply-euler-loop/js/main.js
+++ views/demos/r127/vector3-apply-euler-loop/js/main.js
@@ -2,30 +2,27 @@
 (function () {
 
     // simple create cube helper
-    var createCube = function(){
+    var createCube = function () {
         var cube = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshNormalMaterial());
+                new THREE.BoxGeometry(1, 1, 1),
+                new THREE.MeshNormalMaterial());
         return cube;
     };
 
-    var vectorFromAngles = function(a, b, c, len){
-        len = len = undefined ? 1 : len;
+    var vectorFromAngles = function (a, b, c, len, startVec) {
+        len = len === undefined ? 1 : len;
+        startVec = startVec === undefined ? new THREE.Vector3(1, 0, 0) : startVec;
         var e = new THREE.Euler(
-            THREE.MathUtils.degToRad(a),
-            THREE.MathUtils.degToRad(b), 
-            THREE.MathUtils.degToRad(c));
-        var v = new THREE.Vector3(0, 1, 0).applyEuler(e).normalize();
+                THREE.MathUtils.degToRad(a),
+                THREE.MathUtils.degToRad(b),
+                THREE.MathUtils.degToRad(c));
+        var v = startVec.applyEuler(e).normalize();
         return v.multiplyScalar(len);
     };
-
-
 
     // scene
     var scene = new THREE.Scene();
     scene.add(new THREE.GridHelper(9, 9));
-
-
 
     var cube = createCube();
     scene.add(cube);
@@ -42,19 +39,18 @@
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
 
-
     var lt = new Date(),
     a = 0,
     b = 0,
-    c = 90,
+    c = 0,
     fps = 30;
-    var loop = function(){
+    var loop = function () {
         var now = new Date(),
-        secs = ( now - lt ) / 1000;
+        secs = (now - lt) / 1000;
 
         requestAnimationFrame(loop);
 
-        if(secs > 1 / fps){
+        if (secs > 1 / fps) {
             b += 90 * secs;
             b %= 360;
             var v = vectorFromAngles(a, b, c, 1.5);
