75c79fd0bbc7b2ffb578b9c5c87e4bc654bd9282
dustinpfister <dustin.pfister@gmail.com>
2021-06-18 09:13:46 -0400

    simple basic example for apply euler

--- views/demos/r127/vector3-apply-euler/js/main.js
+++ views/demos/r127/vector3-apply-euler/js/main.js
@@ -9,61 +9,29 @@
         return cube;
     };
 
-    var vectorFromAngles = function(a, b, c, len){
-        len = len = undefined ? 1 : len;
-        var e = new THREE.Euler(
-            THREE.MathUtils.degToRad(a),
-            THREE.MathUtils.degToRad(b), 
-            THREE.MathUtils.degToRad(c));
-        var v = new THREE.Vector3(0, 1, 0).applyEuler(e).normalize();
-        return v.multiplyScalar(len);
-    };
-
-
-
     // scene
     var scene = new THREE.Scene();
     scene.add(new THREE.GridHelper(9, 9));
 
-
-
+    // creating a cube and adding it to the scene
     var cube = createCube();
     scene.add(cube);
-
-    var v = vectorFromAngles(90, 0, 0, 1);
-    console.log(v);
+    // USING THE APPLY EULER Vector3 METHOD
+    var e = new THREE.Euler(
+        THREE.MathUtils.degToRad(45),
+        THREE.MathUtils.degToRad(0), 
+        THREE.MathUtils.degToRad(0));
+    var v = new THREE.Vector3(1, 0, 0).applyEuler(e).normalize().multiplyScalar(3);
     cube.position.copy(v);
 
-    // CAMERA
+    // camera, render
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
     camera.position.set(5, 5, 5);
     camera.lookAt(0, 0, 0);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
-
-
-    var lt = new Date(),
-    a = 0,
-    b = 0,
-    c = 90,
-    fps = 30;
-    var loop = function(){
-        var now = new Date(),
-        secs = ( now - lt ) / 1000;
-
-        requestAnimationFrame(loop);
-
-        if(secs > 1 / fps){
-            b += 90 * secs;
-            b %= 360;
-            var v = vectorFromAngles(a, b, c, 1.5);
-            cube.position.copy(v);
-
-            lt = now;
-            renderer.render(scene, camera);
-        }
-    };
-    loop();
+    renderer.render(scene, camera);
+ 
 }
     ());
