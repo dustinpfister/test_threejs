26d2d498ae8bc8089ab50544aa13aedcaf41097d
dustinpfister <dustin.pfister@gmail.com>
2021-06-21 15:49:23 -0400

    basic loader example looks good

--- views/demos/r127/texture-loader/js/main.js
+++ views/demos/r127/texture-loader/js/main.js
@@ -9,11 +9,21 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
 
-var box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial());
+var loader = new THREE.TextureLoader();
 
-// add the box mesh to the scene
-scene.add(box);
-
-renderer.render(scene, camera);
+loader.load(
+    // the first argument is the relative or absolute path of the file
+    '/img/smile-face/smile_face.png',
+    // the second argument is an on done call back
+    function (texture) {
+        // using the texture for a material and a Mesh
+        var box = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({
+                map: texture
+            }));
+        // add the box mesh to the scene
+        scene.add(box);
+        renderer.render(scene, camera);
+    }
+);
