b3db817a336c81e8044f633ae2fd19d77b7df3ac
dustinpfister <dustin.pfister@gmail.com>
2021-06-21 16:48:44 -0400

    load texture method

--- views/demos/r127/texture-loader-more-than-one/js/main.js
+++ views/demos/r127/texture-loader-more-than-one/js/main.js
@@ -1,3 +1,17 @@
+
+var loadTexure = function (url) {
+    var loader = new THREE.TextureLoader();
+    return new Promise(function (resolve, reject) {
+        var onDone = function (texture) {
+            resolve(texture);
+        };
+        var onError = function (err) {
+            reject(err)
+        };
+        loader.load(url, onDone, function () {}, onError);
+    });
+};
+
 // creating a scene
 var scene = new THREE.Scene();
 
@@ -9,21 +23,23 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
 
+/*
 var loader = new THREE.TextureLoader();
 
 loader.load(
-    // the first argument is the relative or absolute path of the file
-    '/img/smile-face/smile_face.png',
-    // the second argument is an on done call back
-    function (texture) {
-        // using the texture for a material and a Mesh
-        var box = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshBasicMaterial({
-                map: texture
-            }));
-        // add the box mesh to the scene
-        scene.add(box);
-        renderer.render(scene, camera);
-    }
+// the first argument is the relative or absolute path of the file
+'/img/smile-face/smile_face.png',
+// the second argument is an on done call back
+function (texture) {
+// using the texture for a material and a Mesh
+var box = new THREE.Mesh(
+new THREE.BoxGeometry(1, 1, 1),
+new THREE.MeshBasicMaterial({
+map: texture
+}));
+// add the box mesh to the scene
+scene.add(box);
+renderer.render(scene, camera);
+}
 );
+*/
