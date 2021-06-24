70d03ca8655d9d86bb8057ca22a66145fb2aa147
dustinpfister <dustin.pfister@gmail.com>
2021-06-24 12:35:04 -0400

    clean basic example

--- views/forpost/threejs-examples-tree/s2_basic/js/main.js
+++ views/forpost/threejs-examples-tree/s2_basic/js/main.js
@@ -1,16 +1,8 @@
 
 (function () {
-    // SCENE
+    // scene
     var scene = new THREE.Scene();
     scene.add(new THREE.GridHelper(15, 15));
-    // CAMERA
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
-    camera.position.set(10, 10, 10);
-
-    // LIGHT
-    scene.add(camera);
-    var light = new THREE.PointLight(0xffffff);
-    camera.add(light);
 
     // BASIC TREE
     var tree = new Tree({
@@ -22,17 +14,16 @@
         });
     scene.add(tree.group);
 
-    // RENDER
+    // render, camera, light
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
+    camera.position.set(10, 10, 10);
+    scene.add(camera);
+    var light = new THREE.PointLight(0xffffff);
+    camera.add(light);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
-
-    // LOOP
-    var loop = function () {
-        requestAnimationFrame(loop);
-        camera.lookAt(0, 2, 0);
-        renderer.render(scene, camera);
-    };
-    loop();
+    camera.lookAt(0, 2, 0);
+    renderer.render(scene, camera);
 }
     ());
--- views/forpost/threejs-examples-tree/todo.md
+++ views/forpost/threejs-examples-tree/todo.md
@@ -7,9 +7,9 @@
 * (done) the index.ejs file will make use of the tree model in the views/js/modules_group folder v0.0.0 for now
 * (done) store additional code that makes use of tree.js v0.0.0 in a local js folder just like with the demos
 * (done) start a README.md file for this for post folder
+* (done) add a few sections to the demo tree instance in s2_basic
+* (done) add a grid helper to the scene in s2 and s3
 
-* add a few sections to the demo tree instance
-* have an updated loop that makes use of the ushual fps, frame, and maxframe values
-* add a grid helper to the scene
-* custom materials in main.js
-* update blog post to make sure it reflects the current state of main.js here, as well as tree v0.0.0
+* have an updated loop that makes use of the ushual fps, frame, and maxframe values in s3
+
+* update blog post to make sure it reflects the current state of THIS FORPOST FOLDER, as well as TREE V0.0.0
