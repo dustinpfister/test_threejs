64a87a67ae3b36f8dd9e44eef84d598edff00226
dustinpfister <dustin.pfister@gmail.com>
2021-06-24 12:25:31 -0400

    s2 and s3 section folders for threejs-examples-tree

---
+++ views/forpost/threejs-examples-tree/s3_loop/js/main.js
@@ -0,0 +1,38 @@
+
+(function () {
+    // SCENE
+    var scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(15, 15));
+    // CAMERA
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
+    camera.position.set(10, 10, 10);
+
+    // LIGHT
+    scene.add(camera);
+    var light = new THREE.PointLight(0xffffff);
+    camera.add(light);
+
+    // BASIC TREE
+    var tree = new Tree({
+            sections: 8,
+            conesPerSection: 16,
+            coneMaterial: new THREE.MeshStandardMaterial({
+                color: 0x00af00
+            })
+        });
+    scene.add(tree.group);
+
+    // RENDER
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+
+    // LOOP
+    var loop = function () {
+        requestAnimationFrame(loop);
+        camera.lookAt(0, 2, 0);
+        renderer.render(scene, camera);
+    };
+    loop();
+}
+    ());
