30c8496bed2db3d3e599accd471b39eef0f31851
dustinpfister <dustin.pfister@gmail.com>
2021-06-25 09:30:07 -0400

    load one method is now working great

---
+++ views/js/utils/dae_tools/0.1.0/dae_tools.js
@@ -0,0 +1,61 @@
+(function (api) {
+
+    // create aa daeObjects state object
+    api.create = function () {
+        var state = {
+           results: []
+        };
+        return state;
+    };
+
+    // load one dae file
+    api.loadOne = function(daeObjects, url, onDone){
+        // I will want a manager for this
+        var manager = new THREE.LoadingManager();
+        // 
+        var loader = new THREE.ColladaLoader(manager);
+        onDone = onDone || function(){};
+        // return a promise
+        return new Promise(function(resolve, reject){
+            // call on done, and resolve the promise only when the dae file AND all textures load
+            manager.onLoad = function(){
+                var len = daeObjects.results.length;
+                onDone(daeObjects[len -1 ], daeObjects.results, daeObjects);
+                resolve(daeObjects);
+            };
+            // load the dae file and any textures
+            loader.load(url,
+                // done
+                function (result) {
+                    daeObjects.results.push(result);
+                },
+                // progress
+                function(xhr){
+                  
+                },
+                // error
+                function(e){
+                    reject(e);
+                }
+             );
+        });
+    };
+
+    // create a group from a dae result object
+    api.createGroup = function(daeObjects, what){
+        var result = typeof what === 'object' ? what : daeObjects.results[what];
+        var group = new THREE.Group();
+        // copy mesh objects only
+        result.scene.children.forEach(function(obj){
+            if(obj.type === 'Mesh'){
+                group.add(obj.clone());
+            }
+        });
+        // copy result.scene rotation to group
+        group.rotation.copy(result.scene.rotation);
+        return group;
+    };
+
+
+}
+    (this['DAE'] = {}));
--- views/js/utils/dae_tools/todo.md
+++ views/js/utils/dae_tools/todo.md
@@ -3,6 +3,9 @@
 ## 0.x.0 - load all method
 * have a DAE.loadAll method that can be used to load a collection of dea loader results into a dae object
 
+## () 0.1.0 - onDone callback fires when textures load
+* (done) The DAE.loadOne method on done callback/promise resolve should happen when the dea file loads AND all textures are laoded too. 
+
 ## ( done 6/23/21 ) 0.0.0 - Basic tools
 * (done) have a DAE.create method that will create a dea state object
 * (done) have a DAE.loadOne method that can be used to load just one dea loader result into a dea state object
