b2dc1d4bb585353aa95a827942d13184ba23de81
dustinpfister <dustin.pfister@gmail.com>
2021-06-28 15:04:38 -0400

    clean up

--- views/forpost/threejs-canvas-texture/s4_animation/js/main.js
+++ views/forpost/threejs-canvas-texture/s4_animation/js/main.js
@@ -1,6 +1,5 @@
 
 (function () {
-
     // state object
     var state = {
        frame: 0,
@@ -16,7 +15,6 @@
         y = canvas.height / 2 * bias;
         w = canvas.width - canvas.width * bias;
         h = canvas.height - canvas.height * bias;
-
         ctx.lineWidth = 3;
         ctx.fillStyle = '#000000';
         ctx.strokeStyle = '#ff00ff';
@@ -25,34 +23,21 @@
     };
     // create canvas obj
     var canvasObj = canvasMod.createCanvasObject(state, draw);
-
     // filter
     canvasObj.texture.magFilter = THREE.NearestFilter;
-
-    // Basic MATERIAL using TEXTURE
-/*
-    var material = new THREE.MeshBasicMaterial({
-            map: canvasObj.texture
-        });
-*/
-
     // SCENE
     var scene = new THREE.Scene();
     fogColor = new THREE.Color(0xffffff);
-
     scene.background = fogColor;
     scene.fog = new THREE.Fog(fogColor, 0.0025, 20);
     scene.fog = new THREE.FogExp2(fogColor, 0.1);
-
     // CAMERA
     var camera = new THREE.PerspectiveCamera(75, 320 / 240, .025, 20);
     camera.position.set(1, 1, 1);
     camera.lookAt(0, 0, 0);
-
     // using create cube method
     var mesh = canvasMod.createCube(canvasObj);
     scene.add(mesh);
-
     // Render
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
@@ -62,21 +47,15 @@
     var loop = function () {
         var now = new Date(),
         secs = (now - state.lt) / 1000;
-
         requestAnimationFrame(loop);
-
         if(secs > 1 / state.fps){
             canvasObj.draw();
             renderer.render(scene, camera);
-
             state.frame += state.fps * secs;
             state.frame = state.frame % state.maxFrame;
             state.lt = now;
-
         }
-
     };
-
     loop();
 }
     ());
