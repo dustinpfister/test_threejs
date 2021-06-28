83120d62fc0c60024e0fe63286f51af2e010e977
dustinpfister <dustin.pfister@gmail.com>
2021-06-28 10:18:40 -0400

    iife for canvas mod

--- views/forpost/threejs-canvas-texture/s3_create_canvas_helper/js/canvasmod.js
+++ views/forpost/threejs-canvas-texture/s3_create_canvas_helper/js/canvasmod.js
@@ -1,43 +1,42 @@
-var canvasMod = {};
+(function(api){
 
-canvasMod.draw = function (ctx, canvas) {
-    ctx.fillStyle = '#000000';
-    ctx.lineWidth = 1;
-    ctx.fillRect(0.5, 0.5, canvas.width - 1, canvas.height - 1);
-    ctx.strokeStyle = '#ff0000';
-    ctx.strokeRect(0.5, 0.5, canvas.width - 1, canvas.height - 1);
-}
+    api.draw = function (ctx, canvas) {
+        ctx.fillStyle = '#000000';
+        ctx.lineWidth = 1;
+        ctx.fillRect(0.5, 0.5, canvas.width - 1, canvas.height - 1);
+        ctx.strokeStyle = '#00ff00';
+        ctx.strokeRect(0.5, 0.5, canvas.width - 1, canvas.height - 1);
+    };
  
-// create and return a canvas texture
-canvasMod.createCanvasTexture = function (state, drawFunc) {
-    drawFunc = drawFunc || canvasMod.draw;
-    var canvas = document.createElement('canvas'),
-    ctx = canvas.getContext('2d');
-    canvas.width = 16;
-    canvas.height = 16;
-
-    var texture = new THREE.Texture(canvas);
-    texture.needsUpdate = true;
-    var canvasObj = {
-        texture: texture,
-        canvas: canvas,
-        ctx: ctx,
-        state: state,
-        draw: function(){
-           drawFunc.call(state, ctx, canvas, state);
-        }
+    // create and return a canvas texture
+    api.createCanvasTexture = function (state, drawFunc) {
+        drawFunc = drawFunc || canvasMod.draw;
+        var canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d');
+        canvas.width = 16;
+        canvas.height = 16;
+        var texture = new THREE.Texture(canvas);
+        texture.needsUpdate = true;
+        var canvasObj = {
+            texture: texture,
+            canvas: canvas,
+            ctx: ctx,
+            state: state,
+            draw: function(){
+                drawFunc.call(state, ctx, canvas, state);
+            }
+        };
+        canvasObj.draw();
+        return canvasObj;
+    };
+ 
+    // create a cube the makes use of a canvas texture
+    api.createCube = function (texture) {
+        return new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({
+                map: texture
+            }));
     };
 
-    canvasObj.draw();
-
-    return canvasObj;
-};
- 
-// create a cube the makes use of a canvas texture
-canvasMod.createCube = function (texture) {
-    return new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({
-            map: texture
-        }));
-};
+}( this['canvasMod'] = {} ));
