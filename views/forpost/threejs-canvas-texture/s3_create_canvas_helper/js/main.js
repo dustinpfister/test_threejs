3101d89ad799bd2c6a854fa714fbd4c725ccae53
dustinpfister <dustin.pfister@gmail.com>
2021-06-28 10:12:12 -0400

    way better s3 example that makes use of a canvasObj that is returned by the helper rather than a texture

--- views/forpost/threejs-canvas-texture/s3_create_canvas_helper/js/main.js
+++ views/forpost/threejs-canvas-texture/s3_create_canvas_helper/js/main.js
@@ -1,6 +1,6 @@
-var can3 = {};
+var canvasMod = {};
 
-can3.draw = function (ctx, canvas) {
+canvasMod.draw = function (ctx, canvas) {
     ctx.fillStyle = '#000000';
     ctx.lineWidth = 1;
     ctx.fillRect(0.5, 0.5, canvas.width - 1, canvas.height - 1);
@@ -9,20 +9,32 @@ can3.draw = function (ctx, canvas) {
 }
  
 // create and return a canvas texture
-can3.createCanvasTexture = function (draw) {
-    draw = draw || can3.draw;
+canvasMod.createCanvasTexture = function (state, drawFunc) {
+    drawFunc = drawFunc || canvasMod.draw;
     var canvas = document.createElement('canvas'),
     ctx = canvas.getContext('2d');
     canvas.width = 16;
     canvas.height = 16;
-    draw(ctx, canvas);
+
     var texture = new THREE.Texture(canvas);
     texture.needsUpdate = true;
-    return texture;
+    var canvasObj = {
+        texture: texture,
+        canvas: canvas,
+        ctx: ctx,
+        state: state,
+        draw: function(){
+           drawFunc.call(state, ctx, canvas, state);
+        }
+    };
+
+    canvasObj.draw();
+
+    return canvasObj;
 };
  
 // create a cube the makes use of a canvas texture
-can3.createCube = function (texture) {
+canvasMod.createCube = function (texture) {
     return new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshBasicMaterial({
@@ -39,22 +51,26 @@ camera.position.set(2, 2, 2);
 camera.lookAt(0, 0, 1);
  
 // create texture with default draw method
-var texture = can3.createCanvasTexture();
-var cube = can3.createCube(texture);
+var canvasObj = canvasMod.createCanvasTexture();
+
+var cube = canvasMod.createCube(canvasObj.texture);
 scene.add(cube);
- 
+
+
 // create texture with custom draw method
-texture = can3.createCanvasTexture(function (ctx, canvas) {
-        ctx.fillStyle = 'red';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = 'black';
-        ctx.beginPath();
-        ctx.arc(canvas.width / 2 + 0.5, canvas.height / 2 + 0.5, canvas.width / 2 - 2, 0, Math.PI * 2);
-        ctx.fill();
-    });
-cube = can3.createCube(texture);
+var draw = function (ctx, canvas, state) {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2 + 0.5, canvas.height / 2 + 0.5, canvas.width / 2 - 2, 0, Math.PI * 2);
+    ctx.fill();
+};
+var canvasObj = canvasMod.createCanvasTexture({}, );
+cube = canvasMod.createCube(canvasObj.texture);
 cube.position.set(0, 0, 2)
 scene.add(cube);
+
  
 // RENDER
 var renderer = new THREE.WebGLRenderer();
--- views/forpost/threejs-canvas-texture/todo.md
+++ views/forpost/threejs-canvas-texture/todo.md
@@ -1,13 +1,13 @@
 # threejs-canvas-texture todo list
 
-## () - Start a s4 Stochastic and Deterministic examples section
+## () - Start a s5 Stochastic and Deterministic examples section
 * have one Mesh canvas texture example that makes use of a pure function to create a state that is then rendered to a canvas
 * have one Mesh canvas texture example that is Stochastic \(random\)
 * have both kinds of mesh objects with texture that are updated in diferent ways in the same scene
 
-## () - needed improvements of s2, and s3 examples
-* The create canvas helper functionin s2 should return an object that contains references to the canvas, and context.
-* The s3 example should also make use of a caanvas helper function.
+## () - needed improvements of s3, and s4 examples
+* The create canvas helper functionin s3 should return an object that contains references to the canvas, and context.
+* The s4 example should also make use of a caanvas helper function.
 
 ## ( done 06/27/2021 ) - first state of for post folder
 * (done) copy over current source code examples to new section folders for them
