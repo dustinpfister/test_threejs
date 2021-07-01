d73e7e1de15f74c34b51891538e609150d41ce83
dustinpfister <dustin.pfister@gmail.com>
2021-07-01 15:14:24 -0400

    s4 folder on material index

---
+++ views/forpost/threejs-face3/s4_material_index/js/main.js
@@ -0,0 +1,43 @@
+
+(function () {
+    // an array of materials
+    var materials = [
+        new THREE.MeshBasicMaterial({
+            color: 0xff0000
+        }),
+        new THREE.MeshBasicMaterial({
+            color: 0x00ff00
+        }),
+        new THREE.MeshBasicMaterial({
+            color: 0x0000ff
+        })
+    ];
+    // create the mesh
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    // set material index values
+    var len = materials.length;
+    geometry.faces.forEach(function (face3, i) {
+        face3.materialIndex = Math.floor(i % (len * 2) / 2);
+    });
+    // create the mesh
+    var mesh = new THREE.Mesh(
+            geometry,
+            materials);
+    // SCENE
+    var scene = new THREE.Scene();
+    // add mesh to scene
+    scene.add(mesh);
+    // some helpers
+    scene.add(new THREE.FaceNormalsHelper(mesh, 2, 0x00ff00, 1));
+    scene.add(new THREE.VertexNormalsHelper(mesh, 2, 0xff0000, 1));
+
+    // camera, orbit controls, renderer
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(2, 2, 2);
+    camera.lookAt(0, 0, 0);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+}
+    ());
