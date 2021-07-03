b5dadaaac0b2614fbdfec18d7b52b99eb0d9bf30
dustinpfister <dustin.pfister@gmail.com>
2021-07-03 13:36:14 -0400

    640 by 480 for basic example

--- views/forpost/threejs-camera-perspective/s2_basic/js/main.js
+++ views/forpost/threejs-camera-perspective/s2_basic/js/main.js
@@ -1,31 +1,24 @@
 (function () {
+    // CAMERA
+    var fieldOfView = 40,
+    aspectRatio = 4 / 3,
+    near = 0.1,
+    far = 1000,
+    camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
+    camera.position.set(2, 2, 2); // position camera
+    camera.lookAt(0, 0, 0);       // have camera look at 0,0,0
  
-// CAMERA
-var fieldOfView = 40,
-aspectRatio = 16 / 9,
-near = 0.1,
-far = 1000,
-camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
- 
-// scene
-var scene = new THREE.Scene();
-scene.add(new THREE.GridHelper(10, 10));
-
-// RENDER
-var renderer = new THREE.WebGLRenderer();
-document.getElementById('demo').appendChild(renderer.domElement);
-renderer.setSize(320, 180);
- 
-// MESH
-scene.add(new THREE.Mesh(
+    // scene
+    var scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(10, 10));
+    // mesh
+    scene.add(new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial()));
- 
-// position things
-camera.position.set(2, 2, 2);
-camera.lookAt(0, 0, 0);
-// draw the scene
-renderer.render(scene, camera);
- 
+    // renderer
+    var renderer = new THREE.WebGLRenderer();
+    document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.setSize(640, 480);
+    renderer.render(scene, camera);
 }
     ());
