d57536bc99be17e783f1d2ad14ef2307d623bfe3
dustinpfister <dustin.pfister@gmail.com>
2021-07-04 12:03:58 -0400

    added a camera2

--- views/forpost/threejs-camera-perspective/s5_camera_helper/js/main.js
+++ views/forpost/threejs-camera-perspective/s5_camera_helper/js/main.js
@@ -7,7 +7,13 @@
     aspectRatio = 4 / 3,
     near = 1,
     far = 15,
-    camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
+    camera1 = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
+    scene.add(camera1);
+    camera1.position.set(2, 2, 2);
+    camera1.lookAt(0, 0.5, 0);
+
+    var camera2 = new THREE.PerspectiveCamera(45, 4 / 3, 0.1, 20);
+    scene.add(camera2);
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
@@ -21,15 +27,15 @@
         cube.position.set(0, 0.5, 0);
         scene.add(cube);
         // camera pos
-        camera.position.set(2, 2, 2);
-        camera.lookAt(0, 0.5, 0);
+        camera2.position.set(2, 2, 2);
+        camera2.lookAt(0, 0.5, 0);
     };
     // update method
     var update = function (per, bias, secs) {
         // update aspect and fov
-        camera.near = 0.1 + 3 * bias;
-        camera.far = 15 - 13 * bias;
-        camera.updateProjectionMatrix();
+        //camera.near = 0.1 + 3 * bias;
+        //camera.far = 15 - 13 * bias;
+        //camera.updateProjectionMatrix();
     };
     // loop
     var per = 0,
@@ -48,7 +54,7 @@
         requestAnimationFrame(loop);
         if(secs > 1 / fps){
             update(per, bias, secs);
-            renderer.render(scene, camera);
+            renderer.render(scene, camera2);
             frame += fps * secs;
             frame %= frameMax;
             lt = now;
