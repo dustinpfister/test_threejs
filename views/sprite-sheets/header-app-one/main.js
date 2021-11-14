c1fa70311a9292c519352ba1ad6a58ef94dcb0ef
dustinpfister <dustin.pfister@gmail.com>
2021-11-14 14:56:44 -0500

    looks like I might have my first sheet

--- views/sprite-sheets/header-app-one/main.js
+++ views/sprite-sheets/header-app-one/main.js
@@ -4,20 +4,18 @@
     var scene = new THREE.Scene();
  
     var ship = new THREE.Mesh(
-        new THREE.ConeGeometry(1, 4, 3, 1),
+        new THREE.ConeGeometry(1, 4, 6, 1),
         new THREE.MeshNormalMaterial()
         //new THREE.MeshBasicMaterial({wireframe: true})
     );
     ship.geometry.rotateX(Math.PI / 180 * 90);
-    //ship.geometry.rotateZ(Math.PI * 1.5);
-
-    ship.lookAt(1, 0, 1);
+    ship.geometry.rotateZ(Math.PI * 1.2);
 
     scene.add(ship);
 
     // CAMERA
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
-    camera.position.set(0, 2, 4);
+    var camera = new THREE.PerspectiveCamera(40, 4 / 3, 0.5, 1000);
+    camera.position.set(0, 5, 5);
     camera.lookAt(ship.position);
  
     // RENDER
@@ -25,18 +23,34 @@
     renderer.setSize(640, 480);
 
 
-    renderer.render(scene, camera);
-
 
     var canvas = document.createElement('canvas'),
     ctx = canvas.getContext('2d');
-    canvas.width = 128 * 8;
-    canvas.height = 128;
+
     document.getElementById('sheet').appendChild(canvas);
+
+    var cellSize = 32,
+    cellCount = 8;
+    canvas.width = cellSize * cellCount;
+    canvas.height = cellSize;
+
 
     ctx.fillRect(0,0, canvas.width, canvas.height);
  
-    ctx.drawImage(renderer.domElement, 0, 0, 640, 480, 0, 0, 128, 128);
+
+    var cellIndex = 0;
+    while(cellIndex < cellCount){
+
+        var d = 360 / cellCount * cellIndex,
+        radian = Math.PI / 180 * d, 
+        x = Math.cos(radian) * 5,
+        z = Math.sin(radian) * 5;
+        ship.lookAt(x, 0, z);
+        renderer.render(scene, camera);
+        ctx.drawImage(renderer.domElement, 0, 0, 640, 480, cellIndex * cellSize, 0, cellSize, cellSize);
+
+        cellIndex += 1;
+    }
 
 }
     ());
