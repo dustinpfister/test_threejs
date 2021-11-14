34874642e89039006eec99fb255f544bbad0eea2
dustinpfister <dustin.pfister@gmail.com>
2021-11-14 14:42:35 -0500

    started to get togeather a system for creating a sheet

--- views/sprite-sheets/header-app-one/main.js
+++ views/sprite-sheets/header-app-one/main.js
@@ -3,24 +3,40 @@
     // SCENE
     var scene = new THREE.Scene();
  
-    var ship = new THREE.Mesh(new THREE.ConeGeometry(1, 4, 3, 1), 
-        new THREE.MeshBasicMaterial({
-            wireframe: true
-        })
+    var ship = new THREE.Mesh(
+        new THREE.ConeGeometry(1, 4, 3, 1),
+        new THREE.MeshNormalMaterial()
+        //new THREE.MeshBasicMaterial({wireframe: true})
     );
+    ship.geometry.rotateX(Math.PI / 180 * 90);
+    //ship.geometry.rotateZ(Math.PI * 1.5);
+
+    ship.lookAt(1, 0, 1);
 
     scene.add(ship);
 
     // CAMERA
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
-    camera.position.set(3, 3, 3);
+    camera.position.set(0, 2, 4);
     camera.lookAt(ship.position);
  
     // RENDER
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
-    document.getElementById('sheet').appendChild(renderer.domElement);
+
+
     renderer.render(scene, camera);
+
+
+    var canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d');
+    canvas.width = 128 * 8;
+    canvas.height = 128;
+    document.getElementById('sheet').appendChild(canvas);
+
+    ctx.fillRect(0,0, canvas.width, canvas.height);
  
+    ctx.drawImage(renderer.domElement, 0, 0, 640, 480, 0, 0, 128, 128);
+
 }
     ());
