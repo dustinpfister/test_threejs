50746a750cd575fe561481cae1e9efc6b9c6d2c0
dustinpfister <dustin.pfister@gmail.com>
2021-12-03 14:11:43 -0500

    started an sm object

--- views/demos/r135/examples-menus/js/main.js
+++ views/demos/r135/examples-menus/js/main.js
@@ -1,6 +1,29 @@
-var raycaster = new THREE.Raycaster();
-var pointer = new THREE.Vector2(1, 1);
 
+// State Machine (sm) object
+var sm = {
+  lt: new Date(),
+  fps: 30,
+  raycaster: new THREE.Raycaster(),
+  pointer: new THREE.Vector2(1, 1)
+};
+
+// create and return a pointer down hander for the given sm object
+var createPointerDownHandler = function(sm){
+    return function(event) {
+        var canvas = event.target,
+        box = canvas.getBoundingClientRect(),
+        x = event.clientX - box.left,
+        y = event.clientY - box.top;
+        // update sm.pointer values
+        sm.pointer.x = ( x / canvas.scrollWidth ) * 2 - 1;
+        sm.pointer.y = - ( y / canvas.scrollHeight ) * 2 + 1;
+console.log(sm.pointer);
+    };
+};
+
+var box = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshNormalMaterial());
 
 
 // update the picking ray with the camera and mouse position
@@ -17,6 +40,8 @@ var scene = new THREE.Scene();
 scene.add(new THREE.GridHelper(9, 9));
 
 
+scene.add(box);
+
 // CAMERA and RENDERER
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
 camera.position.set(5, 5, 5);
@@ -27,36 +52,21 @@ document.getElementById('demo').appendChild(renderer.domElement);
 
 // EVENTS
 
-// on Pointer Down
-var onPointerDown = function( event ) {
-    var canvas = event.target,
-    box = canvas.getBoundingClientRect(),
-    x = event.clientX - box.left,
-    y = event.clientY - box.top;
-    pointer.x = ( x / canvas.scrollWidth ) * 2 - 1;
-    pointer.y = - ( y / canvas.scrollHeight ) * 2 + 1;
 
-console.log(pointer.x, pointer.y);
-
-};
-renderer.domElement.addEventListener( 'pointerdown', onPointerDown, false );
+renderer.domElement.addEventListener( 'pointerdown', createPointerDownHandler(sm), false );
 
 
 // ORBIT CONTROLS
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 // LOOP
-var lt = new Date(),
-//frame = 0,
-//maxFrame = 300,
-fps = 30;
 var loop = function () {
     var now = new Date(),
     //per = frame / maxFrame,
     //bias = 1 - Math.abs(per - 0.5) / 0.5,
-    secs = (now - lt) / 1000;
+    secs = (now - sm.lt) / 1000;
     requestAnimationFrame(loop);
-    if (secs > 1 / fps) {
+    if (secs > 1 / sm.fps) {
         // update
         update({}, secs);
         // redner
@@ -65,7 +75,7 @@ var loop = function () {
         //frame += fps * secs;
         //frame %= maxFrame;
 
-        lt = now;
+        sm.lt = now;
     }
 }
 loop();
