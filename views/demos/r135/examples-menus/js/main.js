8dbad40f4b167069385ba6950da24c8879981010
dustinpfister <dustin.pfister@gmail.com>
2021-12-03 14:21:13 -0500

    create sm object helper

--- views/demos/r135/examples-menus/js/main.js
+++ views/demos/r135/examples-menus/js/main.js
@@ -1,12 +1,4 @@
 
-// State Machine (sm) object
-var sm = {
-  lt: new Date(),
-  fps: 30,
-  raycaster: new THREE.Raycaster(),
-  pointer: new THREE.Vector2(1, 1)
-};
-
 // create and return a pointer down hander for the given sm object
 var createPointerDownHandler = function(sm){
     return function(event) {
@@ -17,13 +9,45 @@ var createPointerDownHandler = function(sm){
         // update sm.pointer values
         sm.pointer.x = ( x / canvas.scrollWidth ) * 2 - 1;
         sm.pointer.y = - ( y / canvas.scrollHeight ) * 2 + 1;
-console.log(sm.pointer);
+        console.log(sm.pointer);
     };
 };
 
-var box = new THREE.Mesh(
+// STATE MACHINE (sm) OBJECT
+var createSMObject = function(){
+    var sm = {
+        lt: new Date(),
+        fps: 30,
+        raycaster: new THREE.Raycaster(),
+        pointer: new THREE.Vector2(1, 1),
+        camera : new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000),
+        renderer : new THREE.WebGLRenderer(),
+        scene : new THREE.Scene()
+    };
+    // add grid helper to the scene
+    sm.scene.add(new THREE.GridHelper(9, 9));
+    // adding a box to the scene
+    var box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
+    sm.scene.add(box);
+    // starting positon and look at for camera
+    sm.camera.position.set(5, 5, 5);
+    sm.camera.lookAt(0, 0, 0);
+    // renderer
+    sm.renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(sm.renderer.domElement);
+    // EVENTS
+    sm.renderer.domElement.addEventListener( 'pointerdown', createPointerDownHandler(sm), false );
+    // return the sm object
+    return sm;
+};
+
+
+
+
+// create sm object
+var sm = createSMObject();
 
 
 // update the picking ray with the camera and mouse position
@@ -35,29 +59,11 @@ var update = function(state, secs){
     // var mesh = intersects[0].object
 };
 
-// SCENE
-var scene = new THREE.Scene();
-scene.add(new THREE.GridHelper(9, 9));
 
-
-scene.add(box);
-
-// CAMERA and RENDERER
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(5, 5, 5);
-camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
-
-// EVENTS
-
-
-renderer.domElement.addEventListener( 'pointerdown', createPointerDownHandler(sm), false );
 
 
 // ORBIT CONTROLS
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
+var controls = new THREE.OrbitControls(sm.camera, sm.renderer.domElement);
 
 // LOOP
 var loop = function () {
@@ -70,7 +76,7 @@ var loop = function () {
         // update
         update({}, secs);
         // redner
-        renderer.render(scene, camera);
+        sm.renderer.render(sm.scene, sm.camera);
 
         //frame += fps * secs;
         //frame %= maxFrame;
