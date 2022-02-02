2ffdcd0824ae544fe042a1df7256d1d804c6d1c8
dustinpfister <dustin.pfister@gmail.com>
2022-02-02 14:05:16 -0500

    drawing to the draw canvas element

--- views/forpost/threejs-examples-tool-source-layer-3d/s1_basic/js/draw.js
+++ views/forpost/threejs-examples-tool-source-layer-3d/s1_basic/js/draw.js
@@ -1,10 +1,10 @@
 
 (function () {
 
-    var container = document.getElementById('demo');
-    var canvas = document.createElement('canvas'),
+    // create and append canvae element
+    var container = document.getElementById('demo'),
+    canvas = document.createElement('canvas'),
     ctx = canvas.getContext('2d');
-
     canvas.width = 320;
     canvas.height = 240;
     canvas.style.position = 'absolute';
@@ -12,7 +12,6 @@
     canvas.style.height = '240px';
     ctx.fillStyle = 'rgba(0,0,0, 0.5)'
     ctx.fillRect(0,0, canvas.width, canvas.height)
-
     container.appendChild(canvas);
  
     // source later UI
@@ -20,7 +19,7 @@
         el:'#draw-layer-ui',
         template: '<div>'+
             '<h4>DRAW UI: </h4>'+
-            '<span> Color: <input v-model="color" type="color" ></span>' + 
+            '<span> Color: <input v-model="color" type="color" ></span>' +
         '</div>',
         mounted: function () {
             
@@ -38,18 +37,16 @@
         }
     });
 
+    // draw later events
     var drawState = {
        down: false
     };
-
     canvas.addEventListener('pointerdown', (e) => {
         drawState.down = true;
     });
-
     canvas.addEventListener('pointerup', (e) => {
         drawState.down = false;
     });
-
     canvas.addEventListener('pointermove', (e) => {
         e.preventDefault();
         if(drawState.down){
