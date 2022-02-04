8a78f41dd14ce04305c689a00a4066064429266f
dustinpfister <dustin.pfister@gmail.com>
2022-02-04 10:11:19 -0500

    setting position of mesh

--- views/forpost/threejs-vector3-set-from-spherical-coords/s2-av-simple-scene/js/main.js
+++ views/forpost/threejs-vector3-set-from-spherical-coords/s2-av-simple-scene/js/main.js
@@ -4,11 +4,13 @@
     // scene
     var scene = new THREE.Scene();
     scene.add(new THREE.GridHelper(20, 20));
-    scene.background = new THREE.Color('cyan');
+    scene.background = new THREE.Color('black');
 
     // camera
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
-    camera.position.set(-15, 15, -15);
+    camera.position.set(-17, 10, -17);
+	//camera.
+	//THREE.MathUtils.degToRad(90)
     camera.lookAt(0, 0, 0);
     scene.add(camera);
 
@@ -26,18 +28,19 @@
 
     // A Mesh with a Sphere for geometry and using the Standard Material
     var mesh = new THREE.Mesh(
-        new THREE.SphereGeometry(1, 30, 30),
+        new THREE.SphereGeometry(3, 30, 30),
         new THREE.MeshBasicMaterial({
-            color: new THREE.Color('red')
+            color: new THREE.Color('red'),
+            wireframe: true
         })
     );
     scene.add(mesh);
 
-    var v = new THREE.Vector3(0,0,0);
-    var phi = THREE.MathUtils.degToRad(54.74),
-    theta = THREE.MathUtils.degToRad(45);
-    v.setFromSphericalCoords(10, phi, theta);
-
+    // USING setFromSphericalCoords to set position of the Mesh
+    var radius = 10,
+    phi = THREE.MathUtils.degToRad(90),
+    theta = THREE.MathUtils.degToRad(270);
+    mesh.position.setFromSphericalCoords(radius, phi, theta);
     renderer.render(scene, camera);
 
 }
