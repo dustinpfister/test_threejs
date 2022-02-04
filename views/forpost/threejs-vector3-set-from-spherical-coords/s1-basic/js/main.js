0c7ddde2ec7664b2fe1974bab82f9d16fc85eb39
dustinpfister <dustin.pfister@gmail.com>
2022-02-04 11:52:02 -0500

    comment for a basic example

--- views/forpost/threejs-vector3-set-from-spherical-coords/s1-basic/js/main.js
+++ views/forpost/threejs-vector3-set-from-spherical-coords/s1-basic/js/main.js
@@ -1,17 +1,11 @@
 
 (function () {
-
     var v = new THREE.Vector3(0,0,0);
-
     var phi = THREE.MathUtils.degToRad(54.74),
     theta = THREE.MathUtils.degToRad(45);
     v.setFromSphericalCoords(10, phi, theta);
-
     var p = document.createElement('p');
     p.innerText = v.x.toFixed(2) + ', ' + v.y.toFixed(2) + ',' + v.z.toFixed(2);
-    document.body.appendChild(p);
-
-    console.log(v);
-
+    document.body.appendChild(p); // 5.77, 5.77,5.77
 }
     ());
