a65b4337352c671589d1a51649abd5fa56308ba5
dustinpfister <dustin.pfister@gmail.com>
2022-02-09 10:13:09 -0500

    apply euler example looking good so far

--- views/forpost/threejs-euler/s5-av-vector3-apply-euler/js/main.js
+++ views/forpost/threejs-euler/s5-av-vector3-apply-euler/js/main.js
@@ -4,6 +4,7 @@
     // Scene, Camera, and Renderer
     // ---------- ----------
     var scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper())
     var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
     camera.position.set(3, 3, 3);
     camera.lookAt(0, 0, 0);
@@ -18,22 +19,27 @@
             new THREE.MeshNormalMaterial());
     scene.add(mesh);
     // ---------- ----------
-    // LOOP
+    // LOOP - Using Vector3.applyEuler with an instance of THREE.Euler
     // ---------- ----------
     var state = {
-        lt: new Date()
+        lt: new Date(),
+        radian : 0
     };
     var loop = function () {
         var now = new Date(),
         secs = (now - state.lt) / 1000;
         requestAnimationFrame(loop);
         if (secs >= 0.075) {
-            mesh.rotation.x += THREE.MathUtils.degToRad(20) * secs;
-            mesh.rotation.x = THREE.MathUtils.euclideanModulo(mesh.rotation.x, Math.PI * 2)
+            state.radian += THREE.MathUtils.degToRad(90) * secs;
+            state.radian = THREE.MathUtils.euclideanModulo(state.radian, Math.PI * 2);
+            var e = new THREE.Euler(0, state.radian, 0);
+            mesh.position.set(1, 0, 0)
+            mesh.position.applyEuler(e);
             renderer.render(scene, camera);
             state.lt = now;
         }
     };
     loop();
+
 }
     ());
