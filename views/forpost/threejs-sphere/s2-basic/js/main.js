47cdd345eb22305596595bcbe1676543b22a0c65
dustinpfister <dustin.pfister@gmail.com>
2022-02-10 07:52:46 -0500

    threejs-sphere for post folder

---
+++ views/forpost/threejs-sphere/s2-basic/js/main.js
@@ -0,0 +1,33 @@
+(function () {
+ 
+    // creating a scene
+    var scene = new THREE.Scene();
+ 
+    // mesh
+    var mesh = new THREE.Mesh(
+            // USING A SPHERE GEOMETRY WITH A RADIUS OF 0.5
+            new THREE.SphereGeometry(0.5),
+           // standard material
+            new THREE.MeshStandardMaterial({
+                color: 0xff0000,
+                emissive: 0x404040
+            }));
+    scene.add(mesh); // add the mesh to the scene
+ 
+    // camera
+    var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+    camera.position.set(0.75, 1, 0.75);
+    var light = new THREE.PointLight(0xffffff); // point light
+    light.position.x = 1;
+    light.position.y = 1;
+    camera.add(light);
+    camera.lookAt(0, 0, 0);
+    scene.add(camera);
+    // render
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+ 
+}
+    ());
---
+++ views/forpost/threejs-sphere/todo.md
@@ -0,0 +1,4 @@
+# threejs-sphere todo list
+
+### () - update basic example
+* update basic example to use the depth material
