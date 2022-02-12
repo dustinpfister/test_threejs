6c3ebb5e0e395f2a7892b657e33f8ae546f8bc02
dustinpfister <dustin.pfister@gmail.com>
2022-02-12 08:55:59 -0500

    new style

--- views/forpost/threejs-examples-tool-source-layer-3d/r0/js/main.js
+++ views/forpost/threejs-examples-tool-source-layer-3d/r0/js/main.js
@@ -19,17 +19,22 @@
     renderer.setSize(640, 480);
     var container = document.getElementById('demo');
     renderer.domElement.style.position = 'absolute';
-    //container.style.width = "640px";
+    container.style.width = "940px";
     container.style.height = "480px";
+    container.style.position='relative';
     container.appendChild(renderer.domElement);
+
+    var div_sl = document.createElement('div');
+    div_sl.id = 'source-layer-ui';
+    container.appendChild(div_sl);
 
     // ---------- ----------
     // SOURCE LAYER UI
     // ---------- ----------
     var vm = new Vue({
         el:'#source-layer-ui',
-        template: '<div>'+
-            '<h4>SOURCE LAYER 3D <span>{{ version }}<span> UI: </h4>' +
+        template: '<div style="background:#00afaf;position:relative;left:640px;width:280px;padding:10px;">'+
+            '<h4>SOURCE LAYER 3D <span>{{ version }}</span> UI: </h4>' +
             '<span> zoom:  <input v-model="zoom" type="range" min="2" max="20" step="0.25"></span> {{ zoom }} <br> ' + 
             '<span> phi:   <input v-model="phi" type="range" min="0" max="360" step="1"></span> {{ phi }} <br>' + 
             '<span> theta: <input v-model="theta" type="range" min="0" max="360" step="1"></span> {{ theta }} <br>' + 
@@ -50,6 +55,7 @@
                 }
             });
             this.loadDEAFiles();
+            console.log(div_sl.style.background);
         },
         updated: function () {
             this.setZoom();
@@ -81,7 +87,6 @@
             }
         }
     });
-
 
 }
     ());
