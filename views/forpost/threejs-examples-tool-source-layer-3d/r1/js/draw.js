117255c06997faea95e1c88212468093f8682fc0
dustinpfister <dustin.pfister@gmail.com>
2022-02-12 09:25:33 -0500

    started an r1 folder

---
+++ views/forpost/threejs-examples-tool-source-layer-3d/r1/js/draw.js
@@ -0,0 +1,93 @@
+(function () {
+
+    // ---------- ----------
+    // CANVAS
+    // ---------- ----------
+    var container = document.getElementById('demo'),
+    canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d');
+    canvas.width = 640;
+    canvas.height = 480;
+    canvas.style.position = 'absolute';
+    canvas.style.top = '0px';
+    canvas.style.left = '0px';
+    canvas.style.width = '640px';
+    canvas.style.height = '480px';
+
+    canvas.onselectstart = function () { return false; }
+    container.appendChild(canvas);
+
+    var div_dl = document.createElement('div');
+    div_dl.id = 'draw-layer-ui';
+    container.appendChild(div_dl);
+
+
+    // ---------- ----------
+    // DRAW LAYER UI
+    // ---------- ----------
+    var vm = new Vue({
+        el:'#draw-layer-ui',
+        template: '<div style="background:#4faf00;position:relative;left:640px;width:280px;padding:10px;">'+
+            '<h4>DRAW UI: </h4>'+
+            '<span> Color: <input v-model="color" type="color" ></span><br>' +
+            '<span> Size:  <input v-model="size" type="range" min="0.5" max="20" step="0.25"></span> {{ size }} <br>' +
+            '<span> Clear: <input type="button" value="Clear" v-on:click="clear" ></span><br>' +
+        '</div>',
+        mounted: function () {
+            
+        },
+        updated: function () {
+         
+        },
+        data: {
+            color: '#000000',
+            size: 1
+        },
+        methods: {
+            clear: function(){
+                ctx.clearRect(-1, -1, canvas.width + 2, canvas.height + 2);
+            }
+        }
+    });
+ 
+    // ---------- ----------
+    // DRAW LAYER EVENTS
+    // ---------- ----------
+    var drawState = {
+       down: false
+    };
+    canvas.addEventListener('pointerdown', (e) => {
+        e.preventDefault();
+		e.stopPropagation();
+        if(e.pointerType === 'mouse'){
+            if(e.button === 0){
+                drawState.down = true;
+            }
+        }else{
+            drawState.down = true;
+        }
+    });
+    canvas.addEventListener('pointerup', (e) => {
+        drawState.down = false;
+    });
+    canvas.addEventListener('pointerout', (e) => {
+        drawState.down = false;
+    });
+    canvas.addEventListener('pointermove', (e) => {
+        e.preventDefault();
+		
+		e.stopPropagation();
+        if(drawState.down){
+            var bx = canvas.getBoundingClientRect(),
+            dat = vm.$data,
+            x = e.clientX - bx.left,
+            y = e.clientY - bx.top;
+            ctx.fillStyle = dat.color;
+            ctx.beginPath();
+            ctx.arc(x, y, dat.size, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    });
+
+}
+    ());
---
+++ views/forpost/threejs-examples-tool-source-layer-3d/r1/js/main.js
@@ -0,0 +1,94 @@
+
+(function () {
+
+    // ---------- ----------
+    // SCENE CAMERA LIGHT, RENDERER, and CONTAINER
+    // ---------- ----------
+    var scene = new THREE.Scene();  // scene
+    scene.add(new THREE.GridHelper(20, 20));
+    scene.background = new THREE.Color('cyan');
+    var pl = new THREE.PointLight(0xffffff); // point light
+    pl.position.set(2, 5, 3);
+    scene.add(pl);
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);  // camera
+    camera.position.set(-15, 15, -15);
+    camera.add(pl);
+    camera.lookAt(0, 0, 0);
+    scene.add(camera);
+    var renderer = new THREE.WebGLRenderer(); // render
+    renderer.setSize(640, 480);
+    var container = document.getElementById('demo');
+    renderer.domElement.style.position = 'absolute';
+    container.style.width = "940px";
+    container.style.height = "480px";
+    container.style.position='relative';
+    container.appendChild(renderer.domElement);
+
+    var div_sl = document.createElement('div');
+    div_sl.id = 'source-layer-ui';
+    container.appendChild(div_sl);
+
+    // ---------- ----------
+    // SOURCE LAYER UI
+    // ---------- ----------
+
+    var vm = new Vue({
+        el:'#source-layer-ui',
+        template: '<div style="background:#00afaf;position:relative;left:640px;width:280px;padding:10px;">'+
+            '<h4>SOURCE LAYER 3D <span>{{ version }}</span> UI: </h4>' +
+            '<span> zoom:  <input v-model="zoom" type="range" min="2" max="20" step="0.25"></span> {{ zoom }} <br> ' + 
+            '<span> phi:   <input v-model="phi" type="range" min="0" max="360" step="1"></span> {{ phi }} <br>' + 
+            '<span> theta: <input v-model="theta" type="range" min="0" max="360" step="1"></span> {{ theta }} <br>' + 
+        '</div>',
+        mounted: function () {
+            this.setZoom();
+            // creating dea objects
+            this.$data.daeObjects = DAE.create({
+                onItemProgress: function(per, n, d){},
+                onFileLoad: function(result, allResults, daeObjects){},
+                onLoad: function(daeObjects, results){
+                    results.forEach(function(result, i){
+                        var group = DAE.createGroup(daeObjects, result);
+                        group.z = 0;
+                        scene.add(group);
+                    });
+                    renderer.render(scene, camera);
+                }
+            });
+            this.loadDEAFiles();
+            console.log(div_sl.style.background);
+        },
+        updated: function () {
+            this.setZoom();
+        },
+        data: {
+            version: 'r1',
+            zoom: 20,
+            phi: 65,
+            theta: 45,
+            daeObjects: null
+        },
+        methods: {
+            setZoom: function(){
+                var dat = this.$data,
+                phi = dat.phi / 360 * (Math.PI * 2),
+                theta = dat.theta / 360 * (Math.PI * 2);
+                camera.position.setFromSphericalCoords(dat.zoom, phi, theta);
+                camera.lookAt(0,0,0);
+                renderer.render(scene, camera);
+            },
+            loadDEAFiles: function(){
+                DAE.loadAll(this.$data.daeObjects, {
+                    baseUrl: '/dae',
+                    relUrls: [
+                       //'rpi4/rpi4_start_box.dae',
+                       'obj/obj.dae'
+                    ]
+                });
+            }
+        }
+    });
+
+
+}
+    ());
