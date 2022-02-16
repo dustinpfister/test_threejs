0bc1d33fa01daa116aa3e0700ab0ce75ebb472c0
dustinpfister <dustin.pfister@gmail.com>
2022-02-16 13:21:32 -0500

    added a grid helper

--- views/forpost/threejs-box-helper/s3-group/js/setup.js
+++ views/forpost/threejs-box-helper/s3-group/js/setup.js
@@ -1,34 +1,32 @@
 // create a GROUP
 var group = new THREE.Group();
 
-// add a Sphere to the group
-var sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 30, 30), 
-    new THREE.MeshNormalMaterial());
-group.add(sphere);
-// add a Box to the group
-var box = new THREE.Mesh(
+// adding a Sphere, and box to the group, and
+// adjusting the position of one of the children of the group
+group.add(new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 30, 30), 
+    new THREE.MeshNormalMaterial()));
+group.add(new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1), 
-    new THREE.MeshNormalMaterial());
-box.position.set(2,0,0);
-group.add(box);
+    new THREE.MeshNormalMaterial()));
+group.children[1].position.set(2, 0, 0);
 
-// add a BOX HELPER for the GROUP
-var helper = new THREE.BoxHelper(group, 0xffffff);
-group.add(helper);
+// BOX HELPER
+group.add(new THREE.BoxHelper(group, 0xffffff));
 
 // Once the helper is added I can then change the position
-group.position.set(0,-1,0);
-group.rotation.set(-1,1.57,2);
+group.position.set(0,0,0);
+group.rotation.set(0,1,1);
 
 // start a scene
 var scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(8, 8, 0xffffff, 0xff0000));
 // add the GROUP to the scene
 scene.add(group);
 
 // everything else
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(4, 4, 4);
+camera.position.set(5, 5, 5);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
--- views/forpost/threejs-box-helper/todo.md
+++ views/forpost/threejs-box-helper/todo.md
@@ -3,3 +3,5 @@
 ## () - revise old examples
 * (done) make s2-move example less complex by just having one mesh object
 * (done) do away with light source and switch to the normal material for the s2-move example
+* (done) make the group in s3-group by just diretcly adding THREE.Mesh calls as argument values for the add method
+* have a grid helper for the s3-group example
