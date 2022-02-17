8cedfba6c2e5309fab4a47bdedc8f6012bacb9fb
dustinpfister <dustin.pfister@gmail.com>
2022-02-17 09:21:33 -0500

    grid helper

--- views/forpost/threejs-camera-move/s2-move-camera-helper/js/main.js
+++ views/forpost/threejs-camera-move/s2-move-camera-helper/js/main.js
@@ -1,5 +1,6 @@
 (function () {
-    var moveCamera = function (camera, per) {
+    // move camera helper
+    var moveCamera = function (camera, subject, per) {
         var rad = Math.PI * 2 * per,
         x = Math.cos(rad) * 3,
         y = -3 + 6 * (1 - Math.abs(per - 0.5) / 0.5),
@@ -9,40 +10,35 @@
         camera.position.set(x, y, z);
         // the rotation property or the lookAt method
         // can be used to set rotation
-        camera.lookAt(0, 0, 0);
+        camera.lookAt(subject);
     };
 
     // CAMERA
-    var width = 360,
-    height = 180,
-    fieldOfView = 40,
-    aspectRatio = width / height,
-    near = 0.1,
-    far = 1000,
+    var width = 640, height = 480,
+    fieldOfView = 40, aspectRatio = width / height,
+    near = 0.1, far = 1000,
     camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
-
     // SCENE
     var scene = new THREE.Scene();
- 
+    scene.add(new THREE.GridHelper(8, 8))
     // RENDER
     var renderer = new THREE.WebGLRenderer();
     document.getElementById('demo').appendChild(renderer.domElement);
     renderer.setSize(width, height);
- 
     // MESH
-    scene.add(new THREE.Mesh(
+    var mesh = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshBasicMaterial({
             color: 0xff0000,
             wireframe: true
-        })));
- 
+        }));
+    scene.add(mesh);
     // APP LOOP
     var frame = 0,
     frameMax = 100;
     var loop = function () {
         requestAnimationFrame(loop);
-        moveCamera(camera, frame / frameMax);
+        moveCamera(camera, mesh.position, frame / frameMax);
         renderer.render(scene, camera);
         frame += 1;
         frame %= frameMax;
