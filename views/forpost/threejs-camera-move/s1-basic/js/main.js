42145ad4193da90f51f7bf2012399e980ca1ec6b
dustinpfister <dustin.pfister@gmail.com>
2022-02-17 09:47:23 -0500

    cleaner basic example

--- views/forpost/threejs-camera-move/s1-basic/js/main.js
+++ views/forpost/threejs-camera-move/s1-basic/js/main.js
@@ -1,51 +1,45 @@
 (function () {
-    var moveCamera = function (camera, per) {
-        var rad = Math.PI * 2 * per,
-        x = Math.cos(rad) * 3,
-        y = -3 + 6 * (1 - Math.abs(per - 0.5) / 0.5),
-        z = Math.sin(rad) * 3;
-        // position property can be used to set
-        // the position of a camera
-        camera.position.set(x, y, z);
-        // the rotation property or the lookAt method
-        // can be used to set rotation
-        camera.lookAt(0, 0, 0);
-    };
-
     // CAMERA
-    var width = 360,
-    height = 180,
-    fieldOfView = 40,
-    aspectRatio = width / height,
-    near = 0.1,
-    far = 1000,
+    var width = 640, height = 480,
+    fieldOfView = 40, aspectRatio = width / height,
+    near = 0.1, far = 1000,
     camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
-
     // SCENE
     var scene = new THREE.Scene();
- 
+    scene.add(new THREE.GridHelper(8, 8))
     // RENDER
     var renderer = new THREE.WebGLRenderer();
     document.getElementById('demo').appendChild(renderer.domElement);
     renderer.setSize(width, height);
- 
     // MESH
-    scene.add(new THREE.Mesh(
+    var mesh = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({
-            color: 0xff0000,
-            wireframe: true
-        })));
- 
+        new THREE.MeshNormalMaterial());
+    scene.add(mesh);
+    // starting pos for cameare
+    camera.position.set(10, 0, 0);
+    camera.lookAt(mesh.position);
     // APP LOOP
-    var frame = 0,
-    frameMax = 100;
+    var secs = 0,
+    fps_update = 20,   // fps rate to update ( low fps for low CPU use, but chopy video )
+    fps_movement = 30, // fps rate to move camera
+    frame = 0,
+    frameMax = 90,
+    lt = new Date();
     var loop = function () {
+        var now = new Date(),
+        secs = (now - lt) / 1000,
+        per = frame / frameMax,
+        bias = (1 - Math.abs(per - 0.5) / 0.5);
         requestAnimationFrame(loop);
-        moveCamera(camera, frame / frameMax);
-        renderer.render(scene, camera);
-        frame += 1;
-        frame %= frameMax;
+        if(secs > 1 / fps_update){
+            // moving the camera
+            camera.position.set(10, -2 + 4 * bias, 0);
+            renderer.render(scene, camera);
+            frame += fps_movement * secs;
+            frame %= frameMax;
+            lt = now;
+        }
     };
     loop();
 }
