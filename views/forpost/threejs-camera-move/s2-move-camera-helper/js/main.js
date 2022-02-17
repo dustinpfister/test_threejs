8108a4f591ca3350b4421ca92fe2fd68c6b3cd62
dustinpfister <dustin.pfister@gmail.com>
2022-02-17 10:00:55 -0500

    moving mesh

--- views/forpost/threejs-camera-move/s2-move-camera-helper/js/main.js
+++ views/forpost/threejs-camera-move/s2-move-camera-helper/js/main.js
@@ -1,6 +1,18 @@
 (function () {
-    // move camera helper
-    var moveCamera = function (camera, subject, per) {
+
+    // create camera helper
+    var createCamera = function(opt){
+        opt = opt || {};
+        var width = 640, height = 480,
+        fieldOfView = 40, aspectRatio = width / height,
+        near = 0.1, far = 1000,
+        camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
+        camera.userData.subject = new THREE.Vector3();
+        return camera;
+    };
+
+    // move camera update helper
+    var moveCamera = function (camera, per) {
         var rad = Math.PI * 2 * per,
         x = Math.cos(rad) * 6,
         y = -4 + 8 * (1 - Math.abs(per - 0.5) / 0.5),
@@ -10,42 +22,46 @@
         camera.position.set(x, y, z);
         // the rotation property or the lookAt method
         // can be used to set rotation
-        camera.lookAt(subject);
+        camera.lookAt(camera.userData.subject);
     };
 
     // CAMERA
-    var width = 640, height = 480,
-    fieldOfView = 40, aspectRatio = width / height,
-    near = 0.1, far = 1000,
-    camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
+    var camera = createCamera();
+
     // SCENE
     var scene = new THREE.Scene();
     scene.add(new THREE.GridHelper(8, 8))
     // RENDER
     var renderer = new THREE.WebGLRenderer();
     document.getElementById('demo').appendChild(renderer.domElement);
-    renderer.setSize(width, height);
+    renderer.setSize(640, 480);
     // MESH
     var mesh = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({
-            color: 0xff0000,
-            wireframe: true
-        }));
+        new THREE.MeshNormalMaterial());
+    mesh.position.set(3, 0, 0);
     scene.add(mesh);
+
+    camera.userData.subject = mesh.position;
+
     // APP LOOP
     var secs = 0,
-    fps_update = 20,   // fps rate to update ( low fps for low CPU use, but chopy video )
+    fps_update = 30,   // fps rate to update ( low fps for low CPU use, but chopy video )
     fps_movement = 60, // fps rate to move camera
     frame = 0,
     frameMax = 600,
     lt = new Date();
     var loop = function () {
         var now = new Date(),
-        secs = (now - lt) / 1000;
+        secs = (now - lt) / 1000,
+        per = Math.round(frame) / frameMax,
+        bias = (1 - Math.abs(per - 0.5) / 0.5);
         requestAnimationFrame(loop);
         if(secs > 1 / fps_update){
-            moveCamera(camera, mesh.position, Math.round(frame) / frameMax);
+            // calling camera update method
+            moveCamera(camera, per);
+            // moving mesh
+            mesh.position.x = -2 + 4 * bias;
             renderer.render(scene, camera);
             frame += fps_movement * secs;
             frame %= frameMax;
