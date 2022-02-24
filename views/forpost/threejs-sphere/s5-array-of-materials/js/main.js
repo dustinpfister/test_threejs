e0c1426bcc66b065a621e21ca88bbef2d16a791c
dustinpfister <dustin.pfister@gmail.com>
2022-02-24 12:52:07 -0500

    plaed with style

--- views/forpost/threejs-sphere/s5-array-of-materials/js/main.js
+++ views/forpost/threejs-sphere/s5-array-of-materials/js/main.js
@@ -5,12 +5,12 @@
     var scene = new THREE.Scene();
     scene.add( new THREE.GridHelper(10, 10))
     var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-    camera.position.set(2, 2, 2);
-    var light = new THREE.PointLight(0xffffff); // point light
-    light.position.set(1, 1, 0);
-    camera.add(light);
+    camera.position.set(0.75, 0.50, 0.75);
     camera.lookAt(0, 0, 0);
     scene.add(camera);
+    var light = new THREE.PointLight(0xffffff, 1.5); // point light
+    light.position.set(1, 1, 0);
+    camera.add(light);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
@@ -18,21 +18,21 @@
     // THE SPHERE
     // ---------- ----------
     var materials = [
-        new THREE.MeshStandardMaterial({
+        new THREE.MeshPhongMaterial({
             color: 0x880000,
-            emissive: 0x202020
+            emissive: 0x181818
         }),
-        new THREE.MeshStandardMaterial({
+        new THREE.MeshPhongMaterial({
             color: 0x00ff00,
-            emissive: 0x404040
+            emissive: 0x1f1f1f
         }),
-        new THREE.MeshStandardMaterial({
+        new THREE.MeshPhongMaterial({
             color: 0x008800,
-            emissive: 0x202020
+            emissive: 0x181818
         }),
-        new THREE.MeshStandardMaterial({
+        new THREE.MeshPhongMaterial({
             color: 0xff0000,
-            emissive: 0x404040
+            emissive: 0x1f1f1f
         })
     ];
     var geometry = new THREE.SphereGeometry(0.5, 15, 15);
@@ -40,6 +40,9 @@
     len = position.array.length * 2, //!!! this is not a good way to get len it would seem
     mi = 0,
     i = 0;
+
+
+
     while (i < len) {
         mi = i / 3 % 4;
         geometry.addGroup(i, 3, mi);
@@ -49,7 +52,7 @@
             geometry, // USING A SPHERE GEOMETRY
             materials // PASSING AN ARRAY OF MATERIALS
     );
-    mesh.position.set(0, 1, 0);
+    mesh.position.set(0, 0, 0);
     scene.add(mesh);
     // ---------- ----------
     // CALLING RENDER OF RENDERER
