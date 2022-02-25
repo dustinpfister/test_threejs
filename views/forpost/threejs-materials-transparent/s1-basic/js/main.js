25e7d906db0f1011f26a423fb07daf6f20146340
dustinpfister <dustin.pfister@gmail.com>
2022-02-25 08:40:34 -0500

    comments

--- views/forpost/threejs-materials-transparent/s1-basic/js/main.js
+++ views/forpost/threejs-materials-transparent/s1-basic/js/main.js
@@ -1,12 +1,16 @@
 (function () {
+    // ********** **********
     // SCENE, CAMERA, and RENDERER
+    // ********** **********
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
     camera.position.set(0.9, 0, 3.5);
     camera.lookAt(-1, 0, 0);
     var renderer = new THREE.WebGLRenderer();
     document.getElementById('demo').appendChild(renderer.domElement);
+    // ********** **********
     // CREATE MESH OBJECTS
+    // ********** **********
     var createCube = function (size, material, x, y, z) {
         var geometry = new THREE.BoxGeometry(size, size, size),
         cube = new THREE.Mesh(geometry, material);
@@ -26,6 +30,8 @@
     cube = createCube(1, materials.sand, 0, 0, 0);
     scene.add(glassCube);
     scene.add(cube);
+    // ********** **********
     // RENDER
+    // ********** **********
     renderer.render(scene, camera);
 }());
