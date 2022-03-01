3f3828326e5d1cdf70d657c40b3134c497f5fc23
dustinpfister <dustin.pfister@gmail.com>
2022-03-01 12:15:47 -0500

    way better basic example

--- views/forpost/threejs-spotlights/s1-basic/js/main.js
+++ views/forpost/threejs-spotlights/s1-basic/js/main.js
@@ -1,29 +1,39 @@
 (function () {
- 
     // SCENE, CAMERA, RENDERER
     var scene = new THREE.Scene();
-    scene.add(new THREE.GridHelper(10, 10))
+    scene.background = new THREE.Color('#0f0f0f');
     var camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
-    camera.position.set(10, 20, 20);
+    camera.position.set(5, 8, 12);
     camera.lookAt(0,0,0);
     var renderer = new THREE.WebGLRenderer();
     document.getElementById('demo').appendChild(renderer.domElement);
     renderer.setSize(640, 480);
- 
     // SPOTLIGHT
-    var spotLight = new THREE.SpotLight(0xffffff, 3, 20, Math.PI * 0.25, 1);
+    var color = new THREE.Color('white'),
+    intensity = 1,
+    distance = 30,
+    angle = Math.PI * 0.05,
+    penumbra = 0.25,
+    decay = 0.5;
+    var spotLight = new THREE.SpotLight(color, intensity, distance, angle, penumbra, decay);
     spotLight.position.set(8, 8, 0);
-    spotLight.add( new THREE.SpotLightHelper(spotLight));
     scene.add(spotLight);
- 
+    scene.add( new THREE.AmbientLight(0xffffff, 0.07));
+   // MESH OBJECTS
     var cube = new THREE.Mesh(
-            new THREE.BoxGeometry(3, 3, 3),
-            new THREE.MeshLambertMaterial({
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshStandardMaterial({
                 color: 0xff0000
             }));
+    cube.position.set(0, 1, 0);
     scene.add(cube);
-
-    // render what we have
+    var floor = new THREE.Mesh(
+            new THREE.BoxGeometry(10, 1, 10),
+            new THREE.MeshStandardMaterial({
+                color: 0x008800
+            }));
+    scene.add(floor);
+    // RENDER
     renderer.render(scene, camera);
  
 }
