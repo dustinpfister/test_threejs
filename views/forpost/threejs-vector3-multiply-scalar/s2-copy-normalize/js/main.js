a7531284d0aaa2d0b36a2ba77689470e204b47d2
dustinpfister <dustin.pfister@gmail.com>
2022-03-23 11:27:17 -0400

    adjusting rotation

--- views/forpost/threejs-vector3-multiply-scalar/s2-copy-normalize/js/main.js
+++ views/forpost/threejs-vector3-multiply-scalar/s2-copy-normalize/js/main.js
@@ -20,7 +20,7 @@
  
 
     // SETTING POSITION WITH Vector3.copy, normalize, and Vector3.multiplyScalar
-    var radian = Math.PI / 180 * 90,
+    var radian = THREE.MathUtils.degToRad(90 + 45), //Math.PI / 180 * 90,
     radius = 4;
     var vec = new THREE.Vector3(
         Math.cos(radian) * radius,
@@ -30,7 +30,11 @@
     cube1.position.copy(vec);
     var scalar = 1 + Math.round(2 * Math.random())
     cube2.position.copy(vec).normalize().multiplyScalar(scalar);
- 
+    // adjust rotation of cubes
+    cube1.lookAt(0, 0, 0);
+    cube2.lookAt(0, 0, 0);
+    cube3.lookAt(cube1.position);
+
     // redner static scene
     renderer.render(scene, camera);
 }
