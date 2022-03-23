5c7057648bbb503133cb5fa376fe6acbbf7300c2
dustinpfister <dustin.pfister@gmail.com>
2022-03-23 11:48:14 -0400

    translate example looks good

--- views/forpost/threejs-vector3-multiply-scalar/s3-copy-translate-normalize/js/main.js
+++ views/forpost/threejs-vector3-multiply-scalar/s3-copy-translate-normalize/js/main.js
@@ -9,7 +9,8 @@
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
 
-    // MESH OBJECTS
+    // CREATING ANSD POSITIONING MESH OBJECTS WITH Vector3 METHODS
+    // including copy, add, normalize, and multiplyScalar
     var cube1 = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshNormalMaterial());
@@ -17,9 +18,10 @@
     var radian = THREE.MathUtils.degToRad(90 + 25),
     radius = 4;
     var vec = new THREE.Vector3(Math.cos(radian) * radius, 0, Math.sin(radian) * radius);
-    [[0,0,0,0], [0,0,0,1], [0,0,0,3], [0,0,0,4]].forEach(function(data){
-        var mesh = cube1.clone();
-        mesh.position.copy(vec).normalize().multiplyScalar(data[3]);
+    [[0,0,0,0], [-2,1,0,1.5], [-4,2,0,3], [-8,3,0,4.5]].forEach(function(data){
+        var mesh = cube1.clone(),
+        x = data[0], y = data[1], z = data[2], scalar = data[3];
+        mesh.position.copy(vec).add(new THREE.Vector3(x, y, z) ).normalize().multiplyScalar(scalar);
         mesh.lookAt(cube1.position);
         scene.add(mesh);
     });
--- views/forpost/threejs-vector3-multiply-scalar/todo.md
+++ views/forpost/threejs-vector3-multiply-scalar/todo.md
@@ -3,8 +3,8 @@
 ## ( ) - start video1 for project
 * start video1 for this post
 
-## ( ) - copy, translate, normalize, and multiply-scalar
-* example that uses translate on top of copy and normailze
+## ( done 03/23/2022 ) - copy, translate, normalize, and multiply-scalar
+* (done) example that uses translate on top of copy and normailze
 
 ## ( done 03/23/2022 ) - copy, normalize and multiply-scalar
 * (done) have one mesh that is positioned using copy, normalize, and multiply sclar
