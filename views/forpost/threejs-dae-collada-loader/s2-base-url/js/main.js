7f93cc53214002a8c998fad8cfb2b16b6553af12
dustinpfister <dustin.pfister@gmail.com>
2022-04-01 15:36:40 -0400

    worked out a custom manager

--- views/forpost/threejs-dae-collada-loader/s2-base-url/js/main.js
+++ views/forpost/threejs-dae-collada-loader/s2-base-url/js/main.js
@@ -13,7 +13,7 @@
     renderer.render(scene, camera);
     var pl = new THREE.PointLight(0xffffff);
     pl.position.set(2, 5, 3);
-	scene.add(pl);
+    scene.add(pl);
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
  
     // APP LOOP
@@ -23,18 +23,30 @@
         controls.update();
     };
  
-    // CREATE A COLLADALOADER INSTANCE
-    var loader = new THREE.ColladaLoader();
-    // SETTING THE BASE RESOURCE URL FOR TEXTTURES
-    loader.setResourcePath('/dae/guy2/guy2-skin-mrg1/');
-    // CALL THE LOAD METHOD, PASS THE ABSOLUTE OR RELATIVE PATH
-    // TO THE *.DAE FILE AS THE FIRST ARGUMENT, AND A DONE CALLBACK
-    // AS THE SECOND ARGUMENT
-    loader.load('/dae/guy2/guy2.dae', function (result) {
-        // adding the child that I want to the scene
-        scene.add(result.scene.children[2]);
+    // starting a custom manager
+    var manager = new THREE.LoadingManager();
+    manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
+        console.log('manager.onStart');
+    };
+    manager.onLoad = function ( ) {
+        console.log('manager.onLoad: Dae and textures now loaded.');
         // start the app loop
         loop();
+    };
+    manager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
+        console.log('manager.onProgress: ' + itemsLoaded + '/' + itemsTotal);
+    };
+    manager.onError = function ( url ) {};
+ 
+    // CREATE A COLLADALOADER INSTANCE
+    var loader = new THREE.ColladaLoader(manager);
+    // SETTING THE BASE RESOURCE URL FOR TEXTTURES
+    loader.setResourcePath('/dae/guy2/guy2-skin-mrg1/');
+    // THEN LOADING AS USHUAL
+    loader.load('/dae/guy2/guy2.dae', function (result) {
+        console.log('cb of loader.load');
+        // adding the child that I want to the scene
+        scene.add(result.scene.children[2]);
     });
 
 }
