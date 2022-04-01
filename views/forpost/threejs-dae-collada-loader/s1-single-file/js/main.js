fd6e07f5e07acd7b6e2c7f77e29b53b87f4299a0
dustinpfister <dustin.pfister@gmail.com>
2022-04-01 13:45:44 -0400

    started dae collada loader forpost folder

---
+++ views/forpost/threejs-dae-collada-loader/s1-single-file/js/main.js
@@ -0,0 +1,45 @@
+
+(function () {
+
+    // point light
+    var pl = new THREE.PointLight(0xffffff);
+    pl.position.set(2, 5, 3);
+    // scene
+    var scene = new THREE.Scene();
+    scene.background = new THREE.Color('cyan');
+    //scene.add(pl);
+    // camera
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(5, 5, 5);
+    camera.add(pl);
+    camera.lookAt(0, 0, 0);
+    scene.add(camera);
+    // render
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+    // controls
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    // app loop
+    var loop = function () {
+        requestAnimationFrame(loop);
+        renderer.render(scene, camera);
+        controls.update();
+    };
+
+    // CREATE A COLLADALOADER INSTANCE
+    var loader = new THREE.ColladaLoader();
+    // CALL THE LOAD METHOD, PASS THE ABSOLUTE OR RELATIVE PATH
+    // TO THE *.DAE FILE AS THE FIRST ARGUMENT, AND A DONE CALLBACK
+    // AS THE SECOND ARGUMENT
+    loader.load("/dae/obj/obj.dae", function (result) {
+        // adding the child that I want to the scene
+        scene.add(result.scene.children[2]);
+        // start the app loop
+        loop();
+    });
+
+}
+    ());
