e6d85bb7d22c8b7a9ef419c29de5d0eb31a0cb64
dustinpfister <dustin.pfister@gmail.com>
2022-04-01 15:22:24 -0400

    started a base url example

---
+++ views/forpost/threejs-dae-collada-loader/s2-base-url/js/main.js
@@ -0,0 +1,39 @@
+
+(function () {
+    // SCENE, CAMERA, RENDERER, and LIGHT SOURCE
+    var scene = new THREE.Scene();
+    scene.background = new THREE.Color('cyan');
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(5, 5, 5);
+    camera.lookAt(0, 0, 0);
+    scene.add(camera);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+    var pl = new THREE.PointLight(0xffffff);
+    pl.position.set(2, 5, 3);
+	scene.add(pl);
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+ 
+    // APP LOOP
+    var loop = function () {
+        requestAnimationFrame(loop);
+        renderer.render(scene, camera);
+        controls.update();
+    };
+ 
+    // CREATE A COLLADALOADER INSTANCE
+    var loader = new THREE.ColladaLoader();
+    // CALL THE LOAD METHOD, PASS THE ABSOLUTE OR RELATIVE PATH
+    // TO THE *.DAE FILE AS THE FIRST ARGUMENT, AND A DONE CALLBACK
+    // AS THE SECOND ARGUMENT
+    loader.load("/dae/guy2/guy2.dae", function (result) {
+        // adding the child that I want to the scene
+        scene.add(result.scene.children[2]);
+        // start the app loop
+        loop();
+    });
+
+}
+    ());
