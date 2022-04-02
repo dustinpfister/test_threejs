7708991c1ed8d23320663026ab4e6521be05294a
dustinpfister <dustin.pfister@gmail.com>
2022-04-02 11:50:30 -0400

    additional callbacks example looks good

--- views/forpost/threejs-buffer-geometry-loader/s2-additional-callbacks/js/main.js
+++ views/forpost/threejs-buffer-geometry-loader/s2-additional-callbacks/js/main.js
@@ -1,27 +1,27 @@
 
 (function () {
-
+ 
     // Scene
     var scene = new THREE.Scene();
-
+ 
     // Camera
     var camera = new THREE.PerspectiveCamera(65, 4 / 3, .5, 10);
     camera.position.set(2, 2, 2);
     camera.lookAt(0, 0, 0);
-
+ 
     // Render
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
-
+ 
     var light = new THREE.PointLight(0xffffff, 1, 100);
     light.position.set(2, 2, 2);
     scene.add(light);
-
+ 
     var light2 = new THREE.PointLight(0xffffff, 1, 100);
     light2.position.set(-2, -2, -2);
     scene.add(light2);
-
+ 
     var frame = 0,
     maxFrame = 200,
     mesh;
@@ -34,28 +34,28 @@
         frame += 1;
         frame %= maxFrame;
     };
-
+ 
     // Loader
     var loader = new THREE.BufferGeometryLoader();
-
+ 
     // load a resource
     loader.load(
-        // resource URL
+        // URL
         '/forpost/threejs-buffer-geometry-loader/buffer-geo/three_2.json',
-        // onLoad callback
-        function (geometry) {
-        // create a mesh with the geometry
-        // and a material, and add it to the scene
-        mesh = new THREE.Mesh(
-                geometry,
-                new THREE.MeshStandardMaterial({
-                    color: 0x00ff0000,
-                    emissive: 0x2a2a2a,
-                    side: THREE.DoubleSide
-                }));
-        scene.add(mesh);
-        loop();
-    });
-
+        // Load Done
+        function ( geometry ) {
+            var mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({}));
+            scene.add(mesh);
+            renderer.render(scene, camera);
+        },
+        // Progress
+        function ( xhr ) {
+            console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+        },
+        // Error
+        function ( err ) {
+            console.log( 'An error happened' );
+        }
+    );
 }
     ());
