9ebeb6dd48687e8ade3faf5bf848180ab1cf33d9
dustinpfister <dustin.pfister@gmail.com>
2022-04-08 11:38:45 -0400

    basic example looks good

--- views/forpost/threejs-object3d-rotation/s1-basic/js/main.js
+++ views/forpost/threejs-object3d-rotation/s1-basic/js/main.js
@@ -4,8 +4,9 @@
     // ---------- ----------
     var scene = new THREE.Scene();
     scene.background = new THREE.Color('#0f0f0f');
+    scene.add(new THREE.GridHelper(10, 10));
     var camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
-    camera.position.set(5, 8, 12);
+    camera.position.set(5, 5, 5);
     camera.lookAt(0,0,0);
     var renderer = new THREE.WebGLRenderer();
     document.getElementById('demo').appendChild(renderer.domElement);
@@ -13,41 +14,22 @@
     // ---------- ----------
     // MESH OBJECTS
     // ---------- ----------
-    var cube = new THREE.Mesh(
+    var mkCube = function(){
+        return new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshStandardMaterial({
-                color: 0xff0000
-            }));
-    scene.add(cube);
-    // ---------- ----------
-    // CALLING RENDER OF RENDERER IN AN ANIMATION LOOP
-    // ---------- ----------
-    // APP LOOP
-    var secs = 0,
-    fps_update = 30,   // fps rate to update ( low fps for low CPU use, but choppy video )
-    fps_movement = 60, // fps rate to move camera
-    frame = 0,
-    frameMax = 600,
-    lt = new Date();
-    // update
-    var update = function(){
-        var per = Math.round(frame) / frameMax,
-        bias = 1 - Math.abs(0.5 - per) / 0.5;
+            new THREE.MeshNormalMaterial());
     };
-    // loop
-    var loop = function () {
-        var now = new Date(),
-        secs = (now - lt) / 1000;
-        requestAnimationFrame(loop);
-        if(secs > 1 / fps_update){
-            update();
-            renderer.render(scene, camera);
-            frame += fps_movement * secs;
-            frame %= frameMax;
-            lt = now;
-        }
-    };
-    loop();
+    [-45, 0, 45, 20].forEach(function(d, i, arr){
+        var cube = mkCube(),
+        p = i / (arr.length - 1 );
+        cube.position.set(-3 + 6 * p, 0, 0);
+        cube.rotation.y = THREE.MathUtils.degToRad(d);
+        scene.add(cube);
+    });
+    // ---------- ----------
+    // CALLING RENDER OF RENDERER
+    // ---------- ----------
+    renderer.render(scene, camera);
  
 }
     ());
