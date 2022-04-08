82d4a99504c52cfd13ab1ea2701ab48c2e7eaaed
dustinpfister <dustin.pfister@gmail.com>
2022-04-08 11:42:21 -0400

    s2 example looking good so far

--- views/forpost/threejs-object3d-rotation/s2-ani/js/main.js
+++ views/forpost/threejs-object3d-rotation/s2-ani/js/main.js
@@ -4,8 +4,9 @@
     // ---------- ----------
     var scene = new THREE.Scene();
     scene.background = new THREE.Color('#0f0f0f');
+    scene.add(new THREE.GridHelper(10, 10));
     var camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
-    camera.position.set(5, 8, 12);
+    camera.position.set(5, 5, 5);
     camera.lookAt(0,0,0);
     var renderer = new THREE.WebGLRenderer();
     document.getElementById('demo').appendChild(renderer.domElement);
@@ -13,12 +14,20 @@
     // ---------- ----------
     // MESH OBJECTS
     // ---------- ----------
-    var cube = new THREE.Mesh(
+    var mkCube = function(){
+        return new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshStandardMaterial({
-                color: 0xff0000
-            }));
-    scene.add(cube);
+            new THREE.MeshNormalMaterial());
+    };
+    var theCubes = new THREE.Group();
+    scene.add(theCubes);
+    [-45, 0, 45, 20].forEach(function(d, i, arr){
+        var cube = mkCube(),
+        p = i / (arr.length - 1 );
+        cube.position.set(-3 + 6 * p, 0, 0);
+        cube.rotation.y = THREE.MathUtils.degToRad(d);
+        theCubes.add(cube);
+    });
     // ---------- ----------
     // CALLING RENDER OF RENDERER IN AN ANIMATION LOOP
     // ---------- ----------
@@ -33,6 +42,9 @@
     var update = function(){
         var per = Math.round(frame) / frameMax,
         bias = 1 - Math.abs(0.5 - per) / 0.5;
+        // rotating the group of cubes
+        theCubes.rotation.y = Math.PI * 2 * per;
+        //
     };
     // loop
     var loop = function () {
