2dca052df71a85ba7c938affdc5232c85586108f
dustinpfister <dustin.pfister@gmail.com>
2022-04-15 16:32:56 -0400

    s3 random example

--- views/forpost/threejs-data-texture/s3-random/js/main.js
+++ views/forpost/threejs-data-texture/s3-random/js/main.js
@@ -11,18 +11,11 @@ var width = 16, height = 16;
 var size = width * height;
 var data = new Uint8Array( 4 * size );
 for ( let i = 0; i < size; i ++ ) {
-    var stride = i * 4,
-    x = i % width,
-    y = Math.floor(i / width),
-    v2 = new THREE.Vector2(x, y),
-    d = v2.distanceTo( new THREE.Vector2(width / 2, height / 2) ),
-    iPer = i / size,
-    dPer = d / (width / 2);
-    dPer = dPer > 1 ? 1 : dPer;
-    // set r, g, b, and alpha data values
-    data[ stride ] = 255 - Math.floor(255 * dPer);
-    data[ stride + 1 ] = Math.floor(64 * iPer);
-    data[ stride + 2 ] = 64 - Math.floor(64 * iPer);
+    var stride = i * 4;
+    var v = Math.floor( Math.random() * 255 );
+    data[ stride ] = v;
+    data[ stride + 1 ] = v;
+    data[ stride + 2 ] = v;
     data[ stride + 3 ] = 255;
 }
 var texture = new THREE.DataTexture( data, width, height );
