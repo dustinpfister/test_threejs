03c8deac02c289a3a78ef2a58a145282264aadc5
dustinpfister <dustin.pfister@gmail.com>
2022-04-15 11:48:06 -0400

    data texture demo looks good

---
+++ views/demos/r135/data-texture/js/main.js
@@ -0,0 +1,34 @@
+// scene, camera, and renderer
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+document.getElementById('demo').appendChild(renderer.domElement);
+// USING THREE DATA TEXTURE To CREATE A RAW DATA TEXTURE
+var width = 512, height = 512;
+var size = width * height;
+var data = new Uint8Array( 4 * size );
+var per,r,g,b,a = 255;
+for ( let i = 0; i < size; i ++ ) {
+	const stride = i * 4;
+        per = i / size;
+        // set r, g, b, and alpha data values
+	data[ stride ] = 32 + Math.floor(128 * per); // red
+	data[ stride + 1 ] = 255 - 200 * per;        // green
+	data[ stride + 2 ] = 255;                    // blue
+	data[ stride + 3 ] = 255;                    // alpha
+}
+const texture = new THREE.DataTexture( data, width, height );
+texture.needsUpdate = true;
+// creating a mesh with this texture as a color map
+var box = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({
+        map: texture
+    })
+);
+scene.add(box);
+// render
+renderer.render(scene, camera);
