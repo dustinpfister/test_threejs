487c2136565649cc073d61f64bcb597884c2868d
dustinpfister <dustin.pfister@gmail.com>
2022-04-18 16:23:55 -0400

    position

--- views/forpost/threejs-examples-weird-walk-one/s1-demo/js/main.js
+++ views/forpost/threejs-examples-weird-walk-one/s1-demo/js/main.js
@@ -3,10 +3,10 @@
     // SCENE, CAMERA, LIGHT, and RENDERER
     // ********** **********
     var scene = new THREE.Scene();
-    scene.add( new THREE.GridHelper(10, 10) );
+    //scene.add( new THREE.GridHelper(10, 10) );
     var camera = new THREE.PerspectiveCamera(50, 8 / 9, 0.05, 100);
     camera.position.set(4, 4, 4);
-    camera.lookAt(0, 1, 0);
+    camera.lookAt(0, 1.75, 0);
     scene.add(camera);
     var dl = new THREE.DirectionalLight(0xffffff, 0.8);
     dl.position.set(0.1, 1.0, 0);
@@ -116,7 +116,7 @@
         var now = new Date(),
         secs = (now - lt) / 1000;
         requestAnimationFrame(loop);
-        if (secs > 0.05) {
+        if (secs > 1 / 30) {
             var per = frame / maxFrame * 5 % 1,
             bias = Math.abs(0.5 - per) / 0.5;
 
