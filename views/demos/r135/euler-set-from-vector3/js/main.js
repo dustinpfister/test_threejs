501d543daeacea8b7a55c3f8152001d104c075ca
dustinpfister <dustin.pfister@gmail.com>
2022-04-18 11:52:44 -0400

    writing about this method does not seeem t be worth it

--- views/demos/r135/euler-set-from-vector3/js/main.js
+++ views/demos/r135/euler-set-from-vector3/js/main.js
@@ -1,20 +1,23 @@
-// creating a box mesh with the Box Geometry constructor,
-// and the normal material
-var box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
- 
 // creating a scene
 var scene = new THREE.Scene();
- 
-// add the box mesh to the scene
-scene.add(box);
- 
-// camera and renderer
+scene.add( new THREE.GridHelper(10, 10) );
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(0.8, 1.3, 0.8);
+camera.position.set(3, 3, 3);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
+ 
+var box = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshNormalMaterial());
+var a = new THREE.Vector3(8, 0, 0)
+
+//box.rotation.setFromVector3(a);
+box.rotation.x = 8;
+
+console.log(box.rotation.x);
+scene.add(box);
+ 
+// render
 renderer.render(scene, camera);
