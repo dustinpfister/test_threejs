843686c91a8798c8615ef25e48ebb7c3b042ca6f
dustinpfister <dustin.pfister@gmail.com>
2022-04-19 11:02:40 -0400

    removed old code

--- views/forpost/threejs-examples-weird-walk-one/s1-demo/js/main.js
+++ views/forpost/threejs-examples-weird-walk-one/s1-demo/js/main.js
@@ -15,108 +15,6 @@
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
     // ********** **********
-    // WEIRD GUY MODULE
-    // ********** **********
-/*
-    var weirdGuy = (function(){
-
-        var materials = [
-            new THREE.MeshStandardMaterial( { emissive: 0xffaf00, emissiveIntensity: 0.2, wireframe:false } ),
-            new THREE.MeshStandardMaterial( { emissive: 0x0000ff, emissiveIntensity: 0.2 } ),
-            new THREE.MeshStandardMaterial( { emissive: 0xffffff, emissiveIntensity: 0.2 } ),
-            new THREE.MeshStandardMaterial( { emissive: 0x2a2a2a, emissiveIntensity: 0.2 } )
-        ];
-
-        var api = {};
-
-        // create a new weird guy
-        api.create = function(opt){
-            opt = opt || {};
-            var guy = new THREE.Group();
-            guy.name = opt.guyID || 'guy';
-            // BODY
-            var body = new THREE.Mesh(
-                new THREE.BoxGeometry(1, 1.5, 1),
-                materials[0]
-            );
-            body.name = guy.name + '_body';
-            guy.add(body);
-            // EYES
-            ['eye1', 'eye2'].forEach(function(nameStr, i){
-                var eye = new THREE.Mesh(
-                    new THREE.SphereGeometry(0.2, 30, 30),
-                    materials[2]
-                );
-                eye.name = guy.name + '_' + nameStr;
-                eye.position.set(-0.2 + 0.4 * i, 0.2, 0.5);
-                var innerEye = new THREE.Mesh(
-                    new THREE.SphereGeometry(0.1, 30, 30),
-                    materials[3]
-                );
-                innerEye.position.set(0, 0, 0.125);
-                eye.add(innerEye);
-                body.add(eye);
-            });
-            // ADD MOUTH
-            var mouth = new THREE.Mesh(
-                new THREE.BoxGeometry(0.5, 0.125, 0.25),
-                materials[3]
-            );
-            mouth.name = guy.name + '_mouth';
-            mouth.position.set(0, -0.3, 0.5);
-            body.add(mouth);
-            // ADD ARMS
-            ['arm1', 'arm2'].forEach(function(nameStr, i){
-                var arm = new THREE.Mesh(
-                    new THREE.BoxGeometry(0.25, 1.5, 0.25),
-                    materials[0]
-                );
-
-                arm.geometry.translate( 0, 0.75, 0 );
-
-                arm.name = guy.name + '_' + nameStr;
-                arm.position.set(-0.625 + 1.25 * i, 0.0, 0);
-                body.add(arm);
-            });
-            // ADD PELVIS
-            var pelvis = new THREE.Mesh(
-                new THREE.BoxGeometry(1, 0.5, 1),
-                materials[1]
-            );
-            pelvis.name = guy.name + '_pelvis';
-            pelvis.position.set(0, -1.0, 0);
-            guy.add(pelvis);
-            // ADD LEGS
-            ['leg1', 'leg2'].forEach(function(nameStr, i){
-                var leg = new THREE.Mesh(
-                    new THREE.BoxGeometry(0.25, 1.5, 1),
-                    materials[1]
-                );
-                leg.name = guy.name + '_' + nameStr;
-                leg.position.set(-0.25 + 0.5 * i, -1, 0);
-                pelvis.add(leg);
-            });
-            return guy;
-        };
-
-        // setWalk
-        api.setWalk = function(guy, walkPer){
-            var leg1 = guy.getObjectByName(guy.name + '_leg1'),
-            leg2 = guy.getObjectByName(guy.name + '_leg2')
-            // set scale of legs
-            leg1.scale.y = walkPer;
-            leg2.scale.y = 1 - walkPer;
-            // adjust position of legs
-            leg1.position.y = -1.0 + 0.75 * (1 - walkPer);
-            leg2.position.y = -1.0 + 0.75 * walkPer;   
-        };
-
-        // return the api
-        return api;
-
-    }());
-*/
-    // ********** **********
     // WEIRD GUY INSTANCE
     // ********** **********
     var guy = weirdGuy.create({
@@ -125,7 +23,6 @@
     guy.position.y = 2.75;
     scene.add(guy);
     weirdGuy.setWalk(guy, 0);
-
     // ********** **********
     // ANIMATION LOOP
     // ********** **********
