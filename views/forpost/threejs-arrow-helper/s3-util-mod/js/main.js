347c878653ef935ccbe855e7313df6e9ad21c4a0
dustinpfister <dustin.pfister@gmail.com>
2022-04-20 11:42:06 -0400

    s3-util-mod folder

---
+++ views/forpost/threejs-arrow-helper/s3-util-mod/js/main.js
@@ -0,0 +1,14 @@
+// basic scene
+var sceneObj = utils.createBasicScene(),
+scene = sceneObj.scene;
+scene.add( new THREE.GridHelper(10, 10) );
+// ARROWS FOR SCENE
+utils.addArrow(scene, 2, 0, 0, 3.2, 'lime');
+utils.addArrow(scene, 0, 2, 0, 2.5, 'blue');
+utils.addArrow(scene, 0, 0, 2, 2.7, 'cyan');
+// cube
+var cube = utils.addCube(scene, -2.5, 0, .5);
+// ARROW FOR CUBE
+utils.addArrow(cube, 0, 2, 0, 2.5, 'blue');
+// render
+sceneObj.draw();
