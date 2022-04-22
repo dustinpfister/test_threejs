40333fbcb734c6a0bb300cba9c6209fa3a3d06a5
dustinpfister <dustin.pfister@gmail.com>
2022-04-22 12:01:05 -0400

    threejs-texture-loader for post folder

---
+++ views/forpost/threejs-texture-loader/s1-basic/js/setup.js
@@ -0,0 +1,26 @@
+// creating a scene
+var scene = new THREE.Scene();
+// camera and renderer
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(0.8, 1.3, 0.8);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+document.getElementById('demo').appendChild(renderer.domElement);
+var loader = new THREE.TextureLoader();
+loader.load(
+    // the first argument is the relative or absolute path of the file
+    '/img/smile-face/smile_face.png',
+    // the second argument is an on done call back
+    function (texture) {
+        // using the texture for a material and a Mesh
+        var box = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({
+                map: texture
+            }));
+        // add the box mesh to the scene
+        scene.add(box);
+        renderer.render(scene, camera);
+    }
+);
---
+++ views/forpost/threejs-texture-loader/todo.md
@@ -0,0 +1,5 @@
+# threejs-texture-loader todo list
+
+
+## (  ) - first set
+* () just have a first set of examples here
