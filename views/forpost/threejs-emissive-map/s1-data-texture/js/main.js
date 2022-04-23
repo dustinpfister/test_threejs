c9c0173201e503fd5293070113d579c663c4d6a5
dustinpfister <dustin.pfister@gmail.com>
2022-04-23 12:25:45 -0400

    good data texture example

--- views/forpost/threejs-emissive-map/s1-data-texture/js/main.js
+++ views/forpost/threejs-emissive-map/s1-data-texture/js/main.js
@@ -20,30 +20,35 @@ var createDataTexture = function (rPer, gPer, bPer) {
     return texture;
 };
 // create emissive cube helper
-var createCube = function (emissiveMap, map) {
+var createCube = function (emissiveMap, map, emissiveIntensity) {
     return new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshStandardMaterial({
             color: new THREE.Color(1, 1, 1),
             map: map || null,
-            emissiveIntensity: 1,
-            emissive: new THREE.Color(1, 0, 0),
+            emissiveIntensity: emissiveIntensity || 0,
+            emissive: new THREE.Color(1, 1, 1),
             emissiveMap: emissiveMap || null
         }));
 };
 // scene
 var scene = new THREE.Scene();
 scene.add( new THREE.GridHelper(10, 10));
-// mesh
-var box = createCube(createDataTexture(1,1,1));
-scene.add(box);
+// mesh objects
+[ [[1,1,1], 1], [[0,0,1], 0.25], [[0,1,0], 0.1] ].forEach(function(cubeArgs, i, arr){
+    var emissiveMap = createDataTexture.apply(null, cubeArgs[0]);
+    var map = null;
+    var box = createCube(emissiveMap, map, cubeArgs[1]);
+    box.position.x = -5 + 10 * (i / arr.length);
+    scene.add(box);
+});
 // light
-var light = new THREE.PointLight(new THREE.Color(1, 1, 1), 1);
-light.position.set(8, 6, 2);
-scene.add(light);
+//var light = new THREE.PointLight(new THREE.Color(1, 1, 1), 1);
+//light.position.set(8, 10, 2);
+//scene.add(light);
 // camera, render
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(1, 1, 1);
+var camera = new THREE.PerspectiveCamera(40, 320 / 240, 0.1, 1000);
+camera.position.set(4, 4, 4);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
