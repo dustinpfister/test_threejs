a82c0a4c30bd663432023f55e38eef913dadb5a9
dustinpfister <dustin.pfister@gmail.com>
2022-04-23 12:04:07 -0400

    s1-data-texture example

---
+++ views/forpost/threejs-emissive-map/s1-data-texture/js/main.js
@@ -0,0 +1,45 @@
+var createDataTexture = function () {
+    var width = 16,
+    height = 16;
+    var size = width * height;
+    var data = new Uint8Array(4 * size);
+    for (let i = 0; i < size; i++) {
+        var stride = i * 4;
+        var v = Math.floor(THREE.MathUtils.seededRandom() * 255);
+        data[stride] = v;
+        data[stride + 1] = v;
+        data[stride + 2] = v;
+        data[stride + 3] = 255;
+    }
+    var texture = new THREE.DataTexture(data, width, height);
+    texture.needsUpdate = true;
+    return texture;
+};
+// create emissive cube helper
+var createEmissiveCube = function () {
+    return new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshStandardMaterial({
+            color: new THREE.Color(1, 1, 1),
+            emissiveIntensity: 1,
+            emissive: new THREE.Color(1, 0, 0),
+            emissiveMap: createDataTexture()
+        }));
+};
+// scene
+var scene = new THREE.Scene();
+// mesh
+var box = createEmissiveCube();
+scene.add(box);
+// light
+var light = new THREE.PointLight(new THREE.Color(1, 1, 1), 1);
+light.position.set(8, 6, 2);
+scene.add(light);
+// camera, render
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(1, 1, 1);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+document.getElementById('demo').appendChild(renderer.domElement);
+renderer.render(scene, camera);
