a6e846e0479e923b5ae7b64ec99fbdb2d44c4fe8
dustinpfister <dustin.pfister@gmail.com>
2022-04-25 10:36:44 -0400

    whitespace

--- views/forpost/threejs-buffer-geometry/s3-uv/js/main.js
+++ views/forpost/threejs-buffer-geometry/s3-uv/js/main.js
@@ -1,7 +1,7 @@
 (function () {
     // SCENE, CAMERA, RENDERER, LIGHT
     var scene = new THREE.Scene();
-    scene.add( new THREE.GridHelper(10, 10) );
+    scene.add(new THREE.GridHelper(10, 10));
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
     camera.position.set(0, 0.5, 3);
     var renderer = new THREE.WebGLRenderer();
@@ -10,12 +10,13 @@
     var dl = new THREE.DirectionalLight(0xffffff, 1);
     dl.position.set(1, 3, 2)
     scene.add(dl);
+ 
     // MESH with GEOMETRY, and Normal MATERIAL
     var geometry = new THREE.BufferGeometry();
     var vertices = new Float32Array([
-                0,0,0,
-                1,0,0,
-                1,1,0
+                0, 0, 0,
+                1, 0, 0,
+                1, 1, 0
             ]);
     // create position property
     geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
@@ -26,23 +27,21 @@
                 0, 1, 1, 1
             ]);
     geometry.setAttribute('uv', new THREE.BufferAttribute(uvs, 2));
-
-console.log(geometry);
-
     // data texture
-var width = 4, height = 4;
-var size = width * height;
-var data = new Uint8Array( 4 * size );
-for ( let i = 0; i < size; i ++ ) {
-    var stride = i * 4;
-    var v = Math.floor( THREE.MathUtils.seededRandom() * 255 );
-    data[ stride ] = v;
-    data[ stride + 1 ] = v;
-    data[ stride + 2 ] = v;
-    data[ stride + 3 ] = 255;
-}
-var texture = new THREE.DataTexture( data, width, height );
-texture.needsUpdate = true;
+    var width = 4,
+    height = 4;
+    var size = width * height;
+    var data = new Uint8Array(4 * size);
+    for (let i = 0; i < size; i++) {
+        var stride = i * 4;
+        var v = Math.floor(THREE.MathUtils.seededRandom() * 255);
+        data[stride] = v;
+        data[stride + 1] = v;
+        data[stride + 2] = v;
+        data[stride + 3] = 255;
+    }
+    var texture = new THREE.DataTexture(data, width, height);
+    texture.needsUpdate = true;
     var mesh1 = new THREE.Mesh(
             geometry,
             new THREE.MeshStandardMaterial({
@@ -50,7 +49,7 @@ texture.needsUpdate = true;
                 side: THREE.FrontSide
             }));
     mesh1.rotateY(Math.PI * 0.15);
-    mesh1.position.x  = -0.50;
+    mesh1.position.x = -0.50;
     scene.add(mesh1);
     // vertex helper
     var vertHelper = new THREE.VertexNormalsHelper(mesh1, 0.5, 0x00ff00);
