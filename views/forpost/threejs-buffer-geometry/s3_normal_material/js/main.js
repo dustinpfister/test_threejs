578b14ce9a1b772f79eec8feae6680c86cfb1006
dustinpfister <dustin.pfister@gmail.com>
2022-04-25 10:10:59 -0400

    fixed up s3 normal material

--- views/forpost/threejs-buffer-geometry/s3_normal_material/js/main.js
+++ views/forpost/threejs-buffer-geometry/s3_normal_material/js/main.js
@@ -1,13 +1,13 @@
 (function () {
- 
-    // SCENE
+    // SCENE, CAMERA
     var scene = new THREE.Scene();
- 
-    // CAMERA
+    scene.add( new THREE.GridHelper(10, 10) );
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
     camera.position.set(0, 0.5, 3);
- 
-    // GEOMETRY
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    // MESH with GEOMETRY, and Normal MATERIAL
     var geometry = new THREE.BufferGeometry();
     var vertices = new Float32Array([
                 0,0,0,
@@ -16,32 +16,30 @@
             ]);
     // create position property
     geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
- 
     // compute vertex normals
     geometry.computeVertexNormals();
-
-console.log()
-
-console.log(geometry.attributes);
- 
-    // MESH with GEOMETRY, and Normal MATERIAL
-     var mesh = new THREE.Mesh(
+    var mesh1 = new THREE.Mesh(
             geometry,
             new THREE.MeshNormalMaterial({
-                side: THREE.DoubleSide
+                side: THREE.FrontSide
             }));
-    mesh.rotateY(Math.PI * 0.25);
-    scene.add(mesh);
-
-    var vertHelper = new THREE.VertexNormalsHelper(mesh, 0.5, 0x00ff00);
+    mesh1.rotateY(Math.PI * 0.15);
+    mesh1.position.x  = 0.50;
+    scene.add(mesh1);
+    var mesh2 = new THREE.Mesh(
+            geometry,
+            new THREE.MeshNormalMaterial({
+                side: THREE.BackSide
+            }));
+    mesh2.rotateY(Math.PI * 0.75);
+    mesh2.position.x  = -0.50;
+    scene.add(mesh2);
+    // vertex helper
+    var vertHelper = new THREE.VertexNormalsHelper(mesh1, 0.5, 0x00ff00);
+    scene.add(vertHelper)
+    var vertHelper = new THREE.VertexNormalsHelper(mesh2, 0.5, 0x00ff00);
     scene.add(vertHelper) 
-
-
-    // RENDER
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-    renderer.render(scene, camera);
  
-}
-    ());
+    // RENDER
+    renderer.render(scene, camera);
+}());
