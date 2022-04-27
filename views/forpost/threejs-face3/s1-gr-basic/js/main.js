e811ad8d579ab73a03281827655474194647f9cd
dustinpfister <dustin.pfister@gmail.com>
2022-04-27 09:18:48 -0400

    new groups example for face3 for post folder

---
+++ views/forpost/threejs-face3/s1-gr-basic/js/main.js
@@ -0,0 +1,24 @@
+(function () { 
+    // SCENE, CAMERA, RENDER
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(1.5, 1.5, 1.5);
+    camera.lookAt(0, 0, 0);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    // USING GROUPS
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    [ 0, 1, 2, 0, 1, 2 ].forEach( function(mi, i){
+        geometry.groups[i].materialIndex = mi;
+    });
+    // MESH
+    var mesh = new THREE.Mesh(geometry, [
+        new THREE.MeshBasicMaterial( { color: 0x0000ff } ),
+        new THREE.MeshBasicMaterial( { color: 0x00ffff } ),
+        new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
+    ]);
+    scene.add(mesh);
+    // render
+    renderer.render(scene, camera);
+}());
--- views/forpost/threejs-face3/todo.md
+++ views/forpost/threejs-face3/todo.md
@@ -1,9 +1,9 @@
 # threejs-face3 todo list
 
 
-## () - new example for groups that will work in r127+
-* have a new s2 example at the top that makes use of the material index value of the group objects of a geometry
-* move all other examples down
+## ( done 04/27/2022 ) - new example for groups that will work in r127+
+* (done) have a new s2 example at the top that makes use of the material index value of the group objects of a geometry
+* (done) move all other examples down
 
 ## ( done 07/01/2021 ) - first state of for post folder
 * (done) folders for current state
