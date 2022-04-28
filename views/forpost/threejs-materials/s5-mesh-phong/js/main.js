93c66e99399c3d08120caa8279c9c42adba7bb42
dustinpfister <dustin.pfister@gmail.com>
2022-04-28 10:09:20 -0400

    phong material folder

---
+++ views/forpost/threejs-materials/s5-mesh-phong/js/main.js
@@ -0,0 +1,36 @@
+(function () {
+    // Scene
+    var scene = new THREE.Scene();
+    scene.add( new THREE.GridHelper(10, 10) )
+    scene.background = new THREE.Color('blue');
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 0.5, 100);
+    camera.position.set(3, 3, 3);
+    camera.lookAt(0, 0, 0);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    // I WILL WANT A LIGHT SOURCE
+    var dl = new THREE.DirectionalLight(0xffffff, 1.0);
+    dl.position.set(4, 2, 1);
+    scene.add(dl);
+    // INSTANCE OF THE PHONG MATERIAL
+    var material = new THREE.MeshPhongMaterial({
+        color: 0xff0000,
+        shininess: 120
+    });
+    // MESH with SPJHERE Geometry with the material 
+    scene.add(new THREE.Mesh(
+        // SPHERE GEOMETRY
+        new THREE.SphereGeometry(1, 30, 30),
+        material
+    ));
+    // LOOP
+    var loop = function () {
+        requestAnimationFrame(loop);
+        controls.update();         // UPDATE CONTROLS
+        renderer.render(scene, camera); // render
+    };
+    loop();
+}
+    ());
