2a47d44e82eb18551875fce8b0fc0b54eb02bda9
dustinpfister <dustin.pfister@gmail.com>
2022-04-29 07:16:01 -0400

    s3 map example looks good

--- views/forpost/threejs-lambert-material/s3-map/js/main.js
+++ views/forpost/threejs-lambert-material/s3-map/js/main.js
@@ -7,17 +7,34 @@
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
+    // data texture
+    var width = 16,
+    height = 16;
+    var size = width * height;
+    var data = new Uint8Array(4 * size);
+    for (let i = 0; i < size; i++) {
+        var stride = i * 4;
+        var v = Math.floor(THREE.MathUtils.seededRandom() * 255);
+        data[stride] = v;
+        data[stride + 1] = v;
+        data[stride + 2] = v;
+        data[stride + 3] = 255;
+    }
+    var texture = new THREE.DataTexture(data, width, height);
+    texture.needsUpdate = true;
     // add plane to the scene
     var plane = new THREE.Mesh(
-        new THREE.PlaneGeometry(1500, 1500, 8, 8),
-        new THREE.MeshLambertMaterial({
-            color: 0x00afaf,
-            emissive: 0x004a4a,
-            emissiveIntensity: 0.75,
-            side: THREE.DoubleSide
-    }));
+            new THREE.PlaneGeometry(1500, 1500, 8, 8),
+            new THREE.MeshLambertMaterial({
+                color: 0xffffff,
+                map: texture,
+                emissive: 0x004a4a,
+                emissiveIntensity: 0.75,
+                side: THREE.DoubleSide
+            }));
     plane.rotation.x = Math.PI / 2;
     scene.add(plane);
+    scene.add( new THREE.AmbientLight(0xffffff, 0.05));
     // SPOTLIGHT
     var spotLight = new THREE.SpotLight(0xffffff, 1, 300, Math.PI / 180 * 40, 1, 0),
     spotLightHelper = new THREE.SpotLightHelper(spotLight);
@@ -25,8 +42,9 @@
     scene.add(spotLight);
     spotLight.position.set(150, 200, -100);
     spotLightHelper.update();
+
     // render
     renderer.render(scene, camera);
- 
+
 }
     ());
