d4f651b9a607e9c6a7691315138e663d6a67d848
dustinpfister <dustin.pfister@gmail.com>
2022-04-29 07:00:45 -0400

    basic example looks okay

--- views/forpost/threejs-lambert-material/s1-basic/js/main.js
+++ views/forpost/threejs-lambert-material/s1-basic/js/main.js
@@ -1,13 +1,13 @@
 (function () {
     // Scene
     var scene = new THREE.Scene();
- 
-    // Camera
     var camera = new THREE.PerspectiveCamera(50, 320 / 240, 1, 3000);
     camera.position.set(500, 500, 500);
     camera.lookAt(0, 0, 0);
- 
-    // Cube
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    // MESH OF A BOX GEOMETRY AND THE LAMBERT MATERIAL
     var cube = new THREE.Mesh(
             new THREE.BoxGeometry(100, 100, 100),
             new THREE.MeshLambertMaterial({
@@ -15,37 +15,23 @@
             }));
     cube.position.set(0, 100, 0);
     scene.add(cube);
- 
-    // Render
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
- 
-    // background
-    scene.background = new THREE.Color(0x000000);
- 
-    // add plane to the scene
+    // MESH OF A PLANE GEOMETRY AND THE LAMBERT MATERIAL
     var plane = new THREE.Mesh(
-            new THREE.PlaneBufferGeometry(1500, 1500, 8, 8),
+            new THREE.PlaneGeometry(1500, 1500, 8, 8),
             new THREE.MeshLambertMaterial({
                 color: 0x00afaf,
                 side: THREE.DoubleSide
             }));
     plane.rotation.x = Math.PI / 2;
     scene.add(plane);
- 
-    // spotlight, and spotLight helper
-    var spotLight = new THREE.SpotLight(),
+    // SPOTLIGHT
+    var spotLight = new THREE.SpotLight(0xffffff, 1, 300, Math.PI / 180 * 40, 1, 0),
     spotLightHelper = new THREE.SpotLightHelper(spotLight);
     spotLight.add(spotLightHelper);
     scene.add(spotLight);
- 
-    // set position of spotLight,
-    // and helper bust be updated when doing that
-    spotLight.position.set(100, 200, -100);
+    spotLight.position.set(150, 200, -100);
     spotLightHelper.update();
- 
+    // render
     renderer.render(scene, camera);
- 
 }
     ());
