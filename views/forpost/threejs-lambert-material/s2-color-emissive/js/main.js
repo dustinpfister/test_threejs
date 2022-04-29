044c3511fc4ef9e3503115172b53e89e542a44f1
dustinpfister <dustin.pfister@gmail.com>
2022-04-29 07:03:07 -0400

    better emissive example

--- views/forpost/threejs-lambert-material/s2-color-emissive/js/main.js
+++ views/forpost/threejs-lambert-material/s2-color-emissive/js/main.js
@@ -7,18 +7,24 @@
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
- 
     // add plane to the scene
     var plane = new THREE.Mesh(
         new THREE.PlaneGeometry(1500, 1500, 8, 8),
         new THREE.MeshLambertMaterial({
             color: 0x00afaf,
-            emissive: 0x2a2a2a,
-            emissiveIntensity: 0.5,
+            emissive: 0x004a4a,
+            emissiveIntensity: 0.75,
             side: THREE.DoubleSide
     }));
     plane.rotation.x = Math.PI / 2;
     scene.add(plane);
+    // SPOTLIGHT
+    var spotLight = new THREE.SpotLight(0xffffff, 1, 300, Math.PI / 180 * 40, 1, 0),
+    spotLightHelper = new THREE.SpotLightHelper(spotLight);
+    spotLight.add(spotLightHelper);
+    scene.add(spotLight);
+    spotLight.position.set(150, 200, -100);
+    spotLightHelper.update();
     // render
     renderer.render(scene, camera);
  
