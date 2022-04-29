e43c058a32fd5d97c05e3ea0c6ae235530e66d42
dustinpfister <dustin.pfister@gmail.com>
2022-04-29 09:05:19 -0400

    new cube stack module looking good so far

--- views/forpost/threejs-camera-orthographic/s3-compare-to-perspective/js/cube_stack.js
+++ views/forpost/threejs-camera-orthographic/s3-compare-to-perspective/js/cube_stack.js
@@ -1,25 +1,24 @@
 // Cube Stack example for s3-compare-to-perspective example in threejs-camera-orthographic
 var CubeStack = (function () {
     // the stack constructor
-	var api = {};
-	
-	api.create = function(opt){
-		var boxCount = 20,
+    var api = {};
+
+    api.create = function (opt) {
+        var boxCount = 20,
         box,
         x,
         y,
         z,
-        plane,
         boxArray = [],
         boxIndex = 0;
         opt = opt || {};
-		
-		var gx = 5,
-		gy = 5;
-		
+
+        var gx = 5,
+        gy = 5;
+
         var group = new THREE.Group();
-		
-		// place some boxes on the plane
+
+        // place some boxes on the plane
         while (boxIndex < boxCount) {
             box = new THREE.Mesh(
                     new THREE.BoxGeometry(1, 1, 1),
@@ -45,35 +44,10 @@ var CubeStack = (function () {
             group.add(box);
             boxIndex += 1;
         }
-		
-		return group;
-		
-	};
-	
-	return api;
-}
-    ());
-/*
-var CubeStack = (function () {
-    // the stack constructor
-    return function (opt) {
-        var boxCount,
-        box,
-        x,
-        y,
-        z,
-        plane,
-        boxArray = [],
-        boxIndex = 0;
-        opt = opt || {};
-        this.boxCount = opt.boxCount === undefined ? 15 : opt.boxCount;
-        this.gx = 5;
-        this.gy = 5;
-        // this is what can be added to the scene
-        this.group = new THREE.Group();
-        plane = new THREE.Mesh(
+
+        var plane = new THREE.Mesh(
                 // plane geometry
-                new THREE.PlaneGeometry(this.gx, this.gy, this.gx, this.gy),
+                new THREE.PlaneGeometry(gx, gy, gx, gy),
                 // materials
                 new THREE.MeshStandardMaterial({
                     color: 0x00ff00,
@@ -81,34 +55,12 @@ var CubeStack = (function () {
                 }));
         plane.position.set(0, -0.5, 0);
         plane.rotation.set(-Math.PI / 2, 0, 0);
-        this.group.add(plane);
-        // place some boxes on the plane
-        while (boxIndex < this.boxCount) {
-            box = new THREE.Mesh(
-                    new THREE.BoxGeometry(1, 1, 1),
-                    new THREE.MeshStandardMaterial({
-                        color: 0x00ffff,
-                        emissive: 0x0a0a0a
-                    }));
-            x = Math.floor(this.gx * Math.random());
-            y = 0;
-            z = Math.floor(this.gy * Math.random());
-            if (boxArray[z] === undefined) {
-                boxArray[z] = [];
-            }
-            if (boxArray[z][x] === undefined) {
-                boxArray[z][x] = [];
-            }
-            boxArray[z][x].push(box);
-            y = boxArray[z][x].length - 1;
-            box.position.set(
-                -2 + x,
-                y,
-                -2 + z);
-            this.group.add(box);
-            boxIndex += 1;
-        }
+        group.add(plane);
+
+        return group;
+
     };
+
+    return api;
 }
     ());
-	*/
