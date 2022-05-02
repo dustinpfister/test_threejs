148aadfd45755a5c89ded0f81021c198c3255158
dustinpfister <dustin.pfister@gmail.com>
2022-05-02 13:35:32 -0400

    animation is looking good

--- views/forpost/threejs-examples-cube-stack-grid/s1-first/js/main.js
+++ views/forpost/threejs-examples-cube-stack-grid/s1-first/js/main.js
@@ -4,10 +4,9 @@
     // ********** **********
     var scene = new THREE.Scene();
     scene.background = new THREE.Color(0.1, 0.1, 0.1);
-    scene.add(new THREE.GridHelper(14, 14));
     var camera = new THREE.PerspectiveCamera(40, 640 / 480, 0.05, 100);
-    camera.position.set(25, 25, 0);
-    camera.lookAt(0, 0, 0);
+    camera.position.set(25, 25, 25);
+    camera.lookAt(0, -5, 0);
     scene.add(camera);
     var dl = new THREE.DirectionalLight(0xffffff, 0.8);
     dl.position.set(5, 10, 1);
@@ -15,7 +14,9 @@
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
+    // ********** **********
     // CUBE STACK GRID
+    // ********** **********
     var soPalette = [
         { boxCount: 3, colors: [ [0,1,0, [64, 255]], [0,1,1, [64, 255]] ], planeColor: 1 },
         { boxCount: 10 },
@@ -31,13 +32,9 @@
     ];
     var csg = CubeStackGrid.create({ gw: 5, gh: 5, stackGW: 7, stackGH: 5, stackOptionPalette: soPalette, sopArray: sopArray});
     scene.add(csg);
-    console.log(csg)
-
     // ********** **********
     // ANIMATION LOOP
     // ********** **********
-    renderer.render(scene, camera);
-/*
     var frame = 0,
     maxFrame = 300,
     lt = new Date();
@@ -48,7 +45,7 @@
         secs = (now - lt) / 1000;
         requestAnimationFrame(loop);
         if (secs > 1 / 24) {
-            // apply effect
+             csg.rotation.y = Math.PI * 2 * per
             // draw
             renderer.render(scene, camera);
             frame += 20 * secs;
@@ -57,7 +54,6 @@
         }
     };
     loop();
-*/
 
 }
     ());
