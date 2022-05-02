7405b72dce9b33fadd74f78f5f4298b0a6a65434
dustinpfister <dustin.pfister@gmail.com>
2022-05-02 11:41:33 -0400

    started work on create method

--- views/forpost/threejs-examples-cube-stack-grid/s1-first/js/cube-stack-grid.js
+++ views/forpost/threejs-examples-cube-stack-grid/s1-first/js/cube-stack-grid.js
@@ -1,11 +1,24 @@
 // Cube Stack Grid example from threejs-examples-cube-stack-grid
 var CubeStackGrid = (function () {
+
+    var DEFAULT_FOREACHTILE = function(sopIndex, i, csg, opt){};
+
     // the public api
     var api = {};
 
-    // public create method
+    // create a new Cube Stack Grid (csg) object
     api.create = function (opt) {
-        
+        opt = opt || {};
+        // cube stack grid object is an instance of THREE.Group
+        var csg = new THREE.Group(),
+        ud = csg.userDatal
+
+        ud.gw = opt.gw === undefined ? 4 : opt.gw;
+        ud.gh = opt.gw === undefined ? 4 : opt.gw;
+        ud.space = opt.gw === undefined ? 4 : opt.gw;
+        ud.forEachTile = opt.forEachTile === undefined ? DEFAULT_FOREACHTILE : opt.forEachTile;
+
+        return csg;
     };
     // return public api
     return api;
--- views/forpost/threejs-examples-cube-stack-grid/todo.md
+++ views/forpost/threejs-examples-cube-stack-grid/todo.md
@@ -5,12 +5,12 @@
 * (done) start with source code from s2 section of cube-stack example
 * (done) start a cube-stack-grid.js module
 
-* have a CubeStackGrid.create method with options object
-* gw and gh options for CubeStackGrid.create
-* space option for CubeStackGrid.create
-* have the cubeGrid object be an instance of THREE.Group
-* have options in the userData of cubeGrid object
-* have a forEachTile option for CubeStackGrid.create
+* (done) have a CubeStackGrid.create method with options object
+* (done) gw and gh options for CubeStackGrid.create
+* (done) space option for CubeStackGrid.create
+* (done) have the cubeGrid object be an instance of THREE.Group
+* (done) have options in the userData of cubeGrid object
+* (done) have a forEachTile option for CubeStackGrid.create
 
 * use the cube-stack-grid.js module in main.js
 
