3b0ac92d2666e50438c1ad3c9465ba6ca7627f1f
dustinpfister <dustin.pfister@gmail.com>
2022-05-02 12:12:32 -0400

    custom cube stack options palette

--- views/forpost/threejs-examples-cube-stack-grid/s1-first/js/main.js
+++ views/forpost/threejs-examples-cube-stack-grid/s1-first/js/main.js
@@ -6,7 +6,7 @@
     scene.background = new THREE.Color(0.1, 0.1, 0.1);
     var camera = new THREE.PerspectiveCamera(40, 640 / 480, 0.05, 100);
     camera.position.set(25, 25, 25);
-    camera.lookAt(0, 2, 0);
+    camera.lookAt(0, -5, 0);
     scene.add(camera);
     var dl = new THREE.DirectionalLight(0xffffff, 0.8);
     dl.position.set(5, 10, 1);
@@ -15,13 +15,20 @@
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
     // CUBE STACK GRID
-    var csg = CubeStackGrid.create({ gw: 5, gh: 5, sopArray:[
+    var soPalette = [
+        { boxCount: 3 },
+        { boxCount: 10 },
+        { boxCount: 15 },
+        { boxCount: 20 }
+    ];
+    var sopArray = [
         0,0,0,0,0,
         0,1,2,1,0,
         0,2,3,2,0,
         0,1,2,1,0,
         0,0,0,0,0
-    ]});
+    ];
+    var csg = CubeStackGrid.create({ gw: 5, gh: 5, stackOptionPalette: soPalette, sopArray: sopArray});
     scene.add(csg);
     console.log(csg)
 
