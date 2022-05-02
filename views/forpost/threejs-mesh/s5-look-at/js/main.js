188eae27f05ce32efa662fc73d7fa8f2e0ba4936
dustinpfister <dustin.pfister@gmail.com>
2022-05-02 09:34:16 -0400

    look at example working okay

--- views/forpost/threejs-mesh/s5-look-at/js/main.js
+++ views/forpost/threejs-mesh/s5-look-at/js/main.js
@@ -1,45 +1,38 @@
 (function () {
-    // SCENE
+    // Scene
     var scene = new THREE.Scene();
+    scene.add( new THREE.GridHelper(10, 10) );
  
-    // the materials array
-    var materials = [
-        // material 0 (red basic)
-        new THREE.MeshBasicMaterial({
-            color: 0xff0000,
-            side: THREE.DoubleSide
-        }),
-        // material 1 (green basic)
-        new THREE.MeshBasicMaterial({
-            color: 0x00ff00,
-            side: THREE.DoubleSide
-        }),
-        // material 2 (blue basic)
-        new THREE.MeshBasicMaterial({
-            color: 0x0000ff,
-            side: THREE.DoubleSide
-        })
-    ];
-    // a box geometry
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    // for all faces
-    geometry.groups.forEach(function (face, i) {
-        face.materialIndex = i % materials.length;
-    });
-    // add to scene with the Mesh
-    scene.add(new THREE.Mesh(
-            geometry,
-            materials));
+    // CREATEING AND ADDING A MESH OBJECTS
+    var mesh = new THREE.Mesh(
+            new THREE.ConeGeometry(0.5, 2, 30),
+            new THREE.MeshNormalMaterial());
+   // ROTTAING GEOMERTY OF THE CONE
+    mesh.geometry.rotateX(1.57)
+    scene.add(mesh);
+    var i = 0, len = 5, x, y, z, copy;
+    while(i < len){
+        copy = mesh.clone();
+        x = -5 + 10 * (i / len);
+        y = 0;
+        z = -3;
+        copy.position.set(x, y, z);
+        // USING LOOK AT METHOD
+        copy.lookAt(mesh.position);
+        scene.add(copy);
+        i += 1;
+    }
+
  
-    // CAMERA
+    // Camera
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(2, 2, 2);
+    camera.position.set(2, 5, 5);
     camera.lookAt(0, 0, 0);
-    // RENDER
+    // renderer
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
+    // render the scene with the camera
     renderer.render(scene, camera);
- 
 }
     ());
--- views/forpost/threejs-mesh/todo.md
+++ views/forpost/threejs-mesh/todo.md
@@ -1,7 +1,7 @@
 # threejs-mesh todo list
 
 ## () - new s5-look-at example
-* () have an exmaple of the look at method
+* () have an example of the look at method
 
 ## ( done 05/02/2022 ) - new s4-clone example
 * (done) have an example of the clone method
