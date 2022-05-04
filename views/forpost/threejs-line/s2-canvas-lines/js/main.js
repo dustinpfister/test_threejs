52c62df5099c69fe36886d733e6ebf3f2efea0f1
dustinpfister <dustin.pfister@gmail.com>
2022-05-04 11:22:03 -0400

    canvas example is looking better

--- views/forpost/threejs-line/s2-canvas-lines/js/main.js
+++ views/forpost/threejs-line/s2-canvas-lines/js/main.js
@@ -1,5 +1,14 @@
-    // GEOMETRY
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
+
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
+    camera.position.set(1.4, 1.4, 1.4);
+    camera.lookAt(0, 0, 0);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+
+
  
     // CANVAS
     var canvas = document.createElement('canvas'),
@@ -16,6 +25,9 @@
     var texture = new THREE.Texture(canvas);
     texture.needsUpdate = true;
  
+    // GEOMETRY
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+
     // MATERIAL
     var material = new THREE.MeshBasicMaterial({
             map: texture
@@ -24,3 +36,5 @@
     // MESH
     var mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
+
+    renderer.render(scene, camera);
