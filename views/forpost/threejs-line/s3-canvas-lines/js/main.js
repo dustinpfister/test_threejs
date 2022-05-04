6b6ab8827af454f91f8749fd99e9ba90ddb1e789
dustinpfister <dustin.pfister@gmail.com>
2022-05-04 11:35:00 -0400

    white space

--- views/forpost/threejs-line/s3-canvas-lines/js/main.js
+++ views/forpost/threejs-line/s3-canvas-lines/js/main.js
@@ -1,4 +1,3 @@
-
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
     camera.position.set(1.4, 1.4, 1.4);
@@ -7,34 +6,25 @@
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
     renderer.render(scene, camera);
-
-
- 
     // CANVAS
     var canvas = document.createElement('canvas'),
     ctx = canvas.getContext('2d');
- 
     canvas.width = 8;
     canvas.height = 8;
- 
     ctx.fillStyle = '#000000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.strokeStyle = '#ff00ff';
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
- 
     var texture = new THREE.Texture(canvas);
     texture.needsUpdate = true;
- 
     // GEOMETRY
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-
     // MATERIAL
     var material = new THREE.MeshBasicMaterial({
             map: texture
         });
- 
     // MESH
     var mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
-
+    // render
     renderer.render(scene, camera);
