214da575b3aecdfcfabb7f91c14f3db7fbc19b6d
dustinpfister <dustin.pfister@gmail.com>
2022-05-06 11:10:57 -0400

    setting group name

--- views/demos/r135/proro-lookat-with-apply-euler-domain/js/main.js
+++ views/demos/r135/proro-lookat-with-apply-euler-domain/js/main.js
@@ -14,32 +14,34 @@ var materials = [
     new THREE.MeshNormalMaterial()
 ];
 
-var mkPart = function(gid, partName, w, h, d, x, y, z){
+var mkPart = function(g, partName, w, h, d, x, y, z){
     // the mesh object
     var m = new THREE.Mesh(
     new THREE.BoxGeometry(w, h, d),
     materials[0]);
     // name of part
-    m.name = gid + '_' + partName;
+    m.name = g.name + '_' + partName;
     // position it
     m.position.set(x, y, z);
     return m;
 };
 
-var mkModel = function(){
+var mkModel = function(gName){
     var g = new THREE.Group();
+    g.name = gName || 'g-' + g.uuid;
     // add parts
-    g.add( mkPart('g1', 'body', 1, 1, 4, 0, 0, 0) );
-    g.add( mkPart('g1', 'tain', 0.5, 1, 1, 0, 1, -1.5) );
-    g.add( mkPart('g1', 'rwing', 2, 0.5, 1, -1.5, 0, 0) );
-    g.add( mkPart('g1', 'lwing', 2, 0.5, 1, 1.5, 0, 0) );
+    g.add( mkPart(g, 'body', 1, 1, 4, 0, 0, 0) );
+    g.add( mkPart(g, 'tain', 0.5, 1, 1, 0, 1, -1.5) );
+    g.add( mkPart(g, 'rwing', 2, 0.5, 1, -1.5, 0, 0) );
+    g.add( mkPart(g, 'lwing', 2, 0.5, 1, 1.5, 0, 0) );
     return g;
 };
 
-var g = mkModel();
+var g = mkModel('g1');
 scene.add(  g );
-
 g.lookAt(1, 0, 0)
+
+console.log(g)
 
 //******** **********
 //
