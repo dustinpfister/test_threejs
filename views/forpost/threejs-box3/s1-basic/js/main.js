0b1dadb92c7ffdb16449a7e06f5c8e08139bbfe2
dustinpfister <dustin.pfister@gmail.com>
2022-05-09 08:54:41 -0400

    good basic example of the box3 class

--- views/forpost/threejs-box3/s1-basic/js/main.js
+++ views/forpost/threejs-box3/s1-basic/js/main.js
@@ -1,42 +1,24 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(1, 1.3, 1);
-camera.lookAt(0, 0, 0);
+var camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
+camera.position.set(1.25, 1.25, 1.25);
+camera.lookAt(0, -0.125, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
- 
-// creating a texture with canvas
-var canvas = document.createElement('canvas'),
-ctx = canvas.getContext('2d');
-canvas.width = 64;
-canvas.height = 64;
-// drawing gray scale areas
-ctx.fillStyle = '#404040';
-ctx.fillRect(0, 0, 32, 32);
-ctx.fillStyle = '#808080';
-ctx.fillRect(32, 0, 32, 32);
-ctx.fillStyle = '#c0c0c0';
-ctx.fillRect(0, 32, 32, 32);
-ctx.fillStyle = '#f0f0f0';
-ctx.fillRect(32, 32, 32, 32);
-var texture = new THREE.CanvasTexture(canvas);
- 
-// creating a mesh that is using the Basic material
+// CREATE A NEW BOX3
+var min = new THREE.Vector3(-0.5, -0.75, -1.125),
+max = new THREE.Vector3(0.125, 0.25, 0.5);
+var box3 = new THREE.Box3(min, max);
+// THE GET SIZE METHOD OF BOX 3
+// can be used to copy a size to a Vector3 instance
+var s = new THREE.Vector3();
+box3.getSize(s);
+// I CAN THEN USE THAT SIZE VECTOR TO DO
+// SOMETHING LIKE SCALING A MESH OBJECT
 var mesh = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({
-            color: 0x00ffff,
-            // using the alpha map property to set the texture
-            // as the alpha map for the material
-            alphaMap: texture,
-            // I also need to make sure the transparent
-            // property is true
-            transparent: true,
-            // even when opacity is one the alpha map will 
-            // still effect transparency this can just be used to set it even lower
-            opacity: 1,
-            side: THREE.DoubleSide
-        }));
+        new THREE.MeshNormalMaterial());
 scene.add(mesh);
+mesh.scale.copy(s);
+// redner
 renderer.render(scene, camera);
