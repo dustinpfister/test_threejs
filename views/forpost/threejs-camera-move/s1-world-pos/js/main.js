525092ce2d45467ee55ef4a1a77bacb66c1add09
dustinpfister <dustin.pfister@gmail.com>
2022-05-10 09:15:56 -0400

    renamed basic example for camera move folder

--- views/forpost/threejs-camera-move/s1-world-pos/js/main.js
+++ views/forpost/threejs-camera-move/s1-world-pos/js/main.js
@@ -1,13 +1,11 @@
 (function () {
-    // CAMERA
+    // SCENE, CAMERA, and RENDERER
+    var scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(20, 20))
     var width = 640, height = 480,
     fieldOfView = 40, aspectRatio = width / height,
     near = 0.1, far = 1000,
     camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
-    // SCENE
-    var scene = new THREE.Scene();
-    scene.add(new THREE.GridHelper(8, 8))
-    // RENDER
     var renderer = new THREE.WebGLRenderer();
     document.getElementById('demo').appendChild(renderer.domElement);
     renderer.setSize(width, height);
@@ -16,15 +14,12 @@
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
     scene.add(mesh);
-    // starting pos for camera
-    camera.position.set(10, 0, 0);
-    camera.lookAt(mesh.position);
     // APP LOOP
     var secs = 0,
     fps_update = 20,   // fps rate to update ( low fps for low CPU use, but choppy video )
     fps_movement = 30, // fps rate to move camera
     frame = 0,
-    frameMax = 90,
+    frameMax = 300,
     lt = new Date();
     var loop = function () {
         var now = new Date(),
@@ -33,8 +28,8 @@
         bias = (1 - Math.abs(per - 0.5) / 0.5);
         requestAnimationFrame(loop);
         if(secs > 1 / fps_update){
-            // moving the camera
-            camera.position.set(10, -2 + 4 * bias, 0);
+            // MOVING THE CAMERA IN THE LOOP
+            camera.position.set(3 * bias, 1 + 2 * bias, 10);
             renderer.render(scene, camera);
             frame += fps_movement * secs;
             frame %= frameMax;
--- views/forpost/threejs-camera-move/todo.md
+++ views/forpost/threejs-camera-move/todo.md
@@ -7,16 +7,15 @@
 ## () - new event based movement section
 * start a new section example that has to do with event driven movement
 
-## () - s4-world-to-relative
+## () - s3-world-to-relative
 * start a new demo that has to do with changing a camera position form a fixed point to an object relative position
 * switch from world relative to object relative
 * switch from object relative to world relative
 
-## () - s3-relative-pos-example
+## () - s2-relative-pos-example
 * example about having a camera relative to an object in motion
 
-## () - new s2-world-pos example
-* rename s2-move-camera-helper to sx-move-camera-helper
+
 
 ## ( done 02/17/2022 ) - improve 2-move-camera-helper
 * (done) have more than one move function to use with the move camera helper
