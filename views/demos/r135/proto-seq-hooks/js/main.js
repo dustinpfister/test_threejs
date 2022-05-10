5ea9efd379d19cf65dbbf8f5c9ed5943b9c5961d
dustinpfister <dustin.pfister@gmail.com>
2022-05-10 14:06:01 -0400

    going to work on seq-hooks as a proto

---
+++ views/demos/r135/proto-seq-hooks/js/main.js
@@ -0,0 +1,99 @@
+(function () {
+    // SCENE, CAMERA, and RENDERER
+    var scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(10, 10))
+    var width = 640, height = 480,
+    fieldOfView = 40, aspectRatio = width / height,
+    near = 0.1, far = 1000,
+    camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
+    var renderer = new THREE.WebGLRenderer();
+    document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.setSize(width, height);
+
+    // MESH
+    var mesh = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshNormalMaterial());
+    scene.add(mesh);
+
+    var mesh2 = new THREE.Mesh(
+        new THREE.SphereGeometry(0.5, 30, 30),
+        new THREE.MeshNormalMaterial());
+    scene.add(mesh2);
+
+    // a sequnces object
+    var seq = {
+        objectIndex: 0,
+        per: 0,
+        bias: 0,
+        frame: 0,
+        frameMax: 300,
+        beforeObjects: function(seq){
+            var r = Math.PI * 2 * seq.per;
+            var x = Math.cos(r) * 4;
+            var z = Math.sin(r) * 4;
+            mesh2.position.set(x, 0, z);
+        },
+        afterObjects: function(seq){
+            camera.lookAt(mesh.position);
+        },
+        objects: [
+            {
+                per: 0,
+                update: function(seq, partPer, partBias){
+                    camera.position.set(10, 10, 10);
+                }
+            },
+            {
+                per: 0.25,
+                update: function(seq, partPer, partBias){
+                    camera.position.set(10 - 20 * partPer, 10, 10);
+                }
+            },
+            {
+                per: 0.30,
+                update: function(seq, partPer, partBias){
+                    camera.position.set(-10, 10 - 7 * partPer, 10);
+                }
+            },
+            {
+                per: 0.35,
+                update: function(seq, partPer, partBias){
+                    camera.position.set(-10, 3, 10);
+                    var x = 10 * partBias;
+                    camera.lookAt(mesh.position.clone().add(new THREE.Vector3(x, 0, 0)));
+                }
+            },
+            {
+                per: 0.75,
+                update: function(seq, partPer, partBias){
+                    camera.position.set(-10, 3 - 10 * partPer, 10 - 30 * partPer);
+                }
+            }
+        ]
+    };
+
+
+
+    // APP LOOP
+    var secs = 0,
+    fps_update = 10,
+    fps_movement = 30,
+    lt = new Date();
+    var loop = function () {
+        var now = new Date(),
+        secs = (now - lt) / 1000;
+        requestAnimationFrame(loop);
+        if(secs > 1 / fps_update){
+            
+            seqHooks.setFrame(seq, seq.frame)
+
+            renderer.render(scene, camera);
+            seq.frame += fps_movement * secs;
+            seq.frame %= seq.frameMax;
+            lt = now;
+        }
+    };
+    loop();
+}
+    ());
---
+++ views/demos/r135/proto-seq-hooks/js/seq-hooks.js
@@ -0,0 +1,51 @@
+var seqHooks = (function () {
+
+    var api = {};
+
+    // HELPERS
+    var getPer = function(a, b){
+        return a / b;
+    };
+    var getBias = function(per){
+        return 1 - Math.abs( 0.5 - per ) / 0.5;
+    };
+
+    // set frame method
+    api.setFrame = function(seq, frame){
+        seq.frame = frame;
+        seq.per = getPer(seq.frame, seq.frameMax);
+        seq.bias = getBias(seq.per);
+        // update object index
+        seq.objectIndex = 0;
+        var i = 0, len = seq.objects.length;
+        while(i < len){
+            var obj = seq.objects[i];
+            var per2 = 1;
+            if(seq.objects[i + 1] != undefined){
+                per2 = seq.objects[i + 1].per;
+            }
+            // if this is the current object update object 
+            // index as well as other relavent values
+            if(seq.per >= obj.per && seq.per < per2){
+                seq.objectIndex = i;
+                seq.partFrameMax = Math.floor( (per2 - obj.per) * seq.frameMax );
+                seq.partFrame = seq.frame - Math.floor(seq.frameMax * obj.per);
+                seq.partPer = getPer(seq.partFrame, seq.partFrameMax);
+                seq.partBias = getBias(seq.partPer);
+                break;
+            }
+            i += 1;
+        }
+        // call before hook
+        seq.beforeObjects(seq);
+        // call update for current object
+        var obj = seq.objects[seq.objectIndex];
+        obj.update(seq, seq.partPer, seq.partBias);
+        // call after objects hook
+        seq.afterObjects(seq);
+    };
+
+    return api;
+
+}
+    ());
