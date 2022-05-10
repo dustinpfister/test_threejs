613ff65e653804ba996e516457aa8f6284feab5c
dustinpfister <dustin.pfister@gmail.com>
2022-05-10 10:30:17 -0400

    gettig mesh size

--- views/forpost/threejs-box3/s4-seeded-random/js/main.js
+++ views/forpost/threejs-box3/s4-seeded-random/js/main.js
@@ -1,25 +1,35 @@
 var scene = new THREE.Scene();
-scene.add( new THREE.GridHelper(10, 10))
 var camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
-camera.position.set(1.25, 1.25, 1.25);
-camera.lookAt(0, 0.4, 0);
+camera.position.set(-2, 0.5, 4);
+camera.lookAt(0, -0.25, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
-// mesh object
-var mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
-// UISNG COMPUTE BOUNDING BOX OF THE GEOMETRY
-// TO CREATE A BOX3 for the mesh at the boundingBox
-// property of the geometry
-mesh.geometry.computeBoundingBox();
-// GETTING SIZE
-var s = new THREE.Vector3();
-var box3 = mesh.geometry.boundingBox;
-box3.getSize(s);
-// USING SIZE VECTOR3 to set Y position of mesh
-mesh.position.y = s.y / 2;
-scene.add(mesh);
+// create a new box3 with helper
+var min = new THREE.Vector3(-1.0, -1.0, -1.0),
+max = new THREE.Vector3(1.0, 1.0, 1.0);
+var box3 = new THREE.Box3(min, max);
+var box3Helper = new THREE.Box3Helper(box3, 0x00ff00);
+box3Helper.material.linewidth = 3;
+scene.add(box3Helper);
+// CREATE AND POSITION MESH OBJECTS IN THE BOX
+var i = 0, len = 3;
+while(i < len){
+    var mesh = new THREE.Mesh( new THREE.BoxGeometry(0.2, 0.2, 0.2), new THREE.MeshNormalMaterial() );
+    // get box size
+    var boxSize = new THREE.Vector3();
+    box3.getSize(boxSize);
+    // get mesh size
+    mesh.geometry.computeBoundingBox();
+    var meshSize = new THREE.Vector3()
+    mesh.geometry.boundingBox.getSize(meshSize); 
+
+    mesh.position.x = box3.min.x + meshSize.x / 2  + ( boxSize.x - meshSize.x ) * 0;
+
+    scene.add(mesh);
+    i += 1;
+};
+
+
 // render
 renderer.render(scene, camera);
