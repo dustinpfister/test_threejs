3e9f9453dea5e7b2b9e709ee141efb3e6f88c13c
dustinpfister <dustin.pfister@gmail.com>
2022-05-18 10:33:23 -0400

    s3-1-camera

--- views/forpost/threejs-object3d/s3-1-camera/js/main.js
+++ views/forpost/threejs-object3d/s3-1-camera/js/main.js
@@ -1,4 +1,5 @@
 (function () {
+    // scene, renderer
     var scene = new THREE.Scene();
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
@@ -6,18 +7,15 @@
     var gridHelper = new THREE.GridHelper(4, 4);
     gridHelper.scale.set(2.5, 2.5, 2.5);
     scene.add(gridHelper);
- 
-    // camera is based off of OBJECT3D
+    // CAMERA IS BASED OFF OF OBJECT3D
     var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
     camera.position.set(10, 10, 10);
     camera.lookAt(0, 0, 0);
-
-    // box mesh
+    // mesh
     var box = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshNormalMaterial());
     scene.add(box);
- 
     // state object
     var state = {
         frame: 0,
