1a7be7725b992ed5424b08022b1b55446dc142f6
dustinpfister <dustin.pfister@gmail.com>
2022-05-19 09:51:37 -0400

    started an object3d-parent for post folder

---
+++ views/forpost/threejs-object3d-parent/s1-basic/js/main.js
@@ -0,0 +1,23 @@
+// scene
+var scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(5, 5));
+ 
+// adding a mesh
+var mesh = new THREE.Mesh(
+        new THREE.ConeGeometry(0.5, 1, 10, 10),
+        new THREE.MeshNormalMaterial());
+mesh.position.set(0, 0.5, 0);
+scene.add(mesh);
+ 
+// getting the parent of the mesh, and preforming an action on it
+var parent = mesh.parent;
+parent.rotation.x = Math.PI * 2 * Math.random();
+ 
+// camera and renderer
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(4, 4, 4);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+document.getElementById('demo').appendChild(renderer.domElement);
+renderer.render(scene, camera);
---
+++ views/forpost/threejs-object3d-parent/todo.md
@@ -0,0 +1,4 @@
+# threejs-object3d-parent todo list
+
+## ( ) - s1-basic example
+* () have an s1-basic example section folder
