747a5c0b3d464c8740e560b71789d304c673a9bb
dustinpfister <dustin.pfister@gmail.com>
2022-05-19 15:42:08 -0400

    creating grid group

--- views/demos/r135/proro-object-grid-wrap/js/main.js
+++ views/demos/r135/proro-object-grid-wrap/js/main.js
@@ -1,11 +1,53 @@
 //******** **********
+// ObjectGridWrap module
+//******** **********
+var ObjectGridWrap = (function(){
+
+    var  DEFAULT_SOURCE_OBJECTS = [
+        new THREE.Mesh( new THREE.BoxGeometry( 1, 1, 1), new THREE.MeshNormalMaterial()),
+        new THREE.Mesh( new THREE.SphereGeometry( 0.5, 30, 30), new THREE.MeshNormalMaterial())
+    ];
+
+    var DEFAULT_OBJECT_INDICES = [0,0,0,0,
+                                  0,1,1,0,
+                                  0,1,1,0,
+                                  0,0,0,0];
+
+    var api = {};
+
+    api.create = function(opt){
+        opt = opt || {};
+        opt.sourceObjects = opt.sourceObjects || DEFAULT_SOURCE_OBJECTS;
+        opt.objectIndices = opt.objectIndices || DEFAULT_OBJECT_INDICES;
+        opt.w = 4;
+        opt.h = 4;
+        opt.alphaX = 0; // alpha x and z values
+        opt.alphaZ = 0;
+        var grid = new THREE.Group();
+        var i = 0, len = opt.w * opt.h;
+        while(i < len){
+            var objIndex = opt.objectIndices[i];
+            var mesh = opt.sourceObjects[objIndex].clone();
+            grid.add(mesh);
+            i += 1;
+        };
+        return grid;
+    };
+
+
+    return api;
+
+}());
+
+
+//******** **********
 // SCENE, CAMERA, RENDERER
 //******** **********
 var scene = new THREE.Scene();
 scene.background = new THREE.Color('#4a4a4a');
-scene.add( new THREE.GridHelper(10, 10, 0x00ff00, 0x4a4a4a) )
+scene.add( new THREE.GridHelper(10, 10, 0x00ff00, 0xffffff) )
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(10, 10, 10);
+camera.position.set(-10, 10, 0);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
@@ -17,6 +59,9 @@ var dl = new THREE.DirectionalLight(0xffffff, 1);
 dl.position.set(2, -1, -1.5);
 scene.add(dl);
 
+
+var grid = ObjectGridWrap.create();
+scene.add(grid)
 
 
 //******** **********
