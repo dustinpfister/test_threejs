89ea76cd6bbcc2af2dcbadeac8df61fe599fecc2
dustinpfister <dustin.pfister@gmail.com>
2022-05-21 13:50:18 -0400

    r1 started

---
+++ views/forpost/threejs-examples-object-grid-wrap/s1-r1/js/main.js
@@ -0,0 +1,90 @@
+//******** **********
+// SCENE, CAMERA, RENDERER
+//******** **********
+var scene = new THREE.Scene();
+scene.background = new THREE.Color('#000000');
+//scene.add( new THREE.GridHelper(10, 10, 0x00ff00, 0xffffff) )
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(-10, 5, 0);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+document.getElementById('demo').appendChild(renderer.domElement);
+//******** **********
+// LIGHT
+//******** **********
+var dl = new THREE.DirectionalLight(0xffffff, 1);
+dl.position.set(-2, 1, 3);
+scene.add(dl);
+//******** **********
+// GRID OPTIONS
+//******** **********
+var tw = 20,
+th = 20,
+space = 1.25;
+// source objects
+var mkBox = function(color, h){
+    var box = new THREE.Group();
+    var mesh = new THREE.Mesh(
+        new THREE.BoxGeometry( 1, h, 0.25 + 0.25),
+        new THREE.MeshStandardMaterial({ color: color}) );
+    mesh.position.y = h / 2;
+    mesh.rotation.y = Math.PI / 180 * 20 * -1;
+    var ground = new THREE.Mesh(
+        new THREE.BoxGeometry( space, 0.1, space),
+        new THREE.MeshStandardMaterial({ color: 0xffffff}) );
+    ground.position.y = 0.05 * -1;
+    box.add(mesh)  
+    box.add(ground);
+    return box;
+};
+var array_source_objects = [
+    mkBox(0xff0000, 0.5),
+    mkBox(0x00ff00, 1),
+    mkBox(0x0000ff, 1.5),
+    mkBox(0x00ffff, 2),
+    mkBox(0xff00ff, 2.5)
+];
+var array_oi = [],
+len = tw * th, i = 0;
+while(i < len){
+    array_oi.push( Math.floor( array_source_objects.length * THREE.MathUtils.seededRandom() ) );
+    i += 1;
+}
+//******** **********
+// CREATE GRID
+//******** **********
+var grid = ObjectGridWrap.create({
+    space: space,
+    tw: tw,
+    th: th,
+    aOpacity: 1.25,
+    sourceObjects: array_source_objects,
+    objectIndices: array_oi
+});
+scene.add(grid);
+//******** **********
+// LOOP
+//******** **********
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+var fps = 30,
+lt = new Date(),
+frame = 0,
+maxFrame = 300;
+var loop = function () {
+    var now = new Date(),
+    per = frame / maxFrame,
+    bias = 1 - Math.abs(0.5 - per) / 0.5,
+    secs = (now - lt) / 1000,
+    ud = grid.userData;
+    requestAnimationFrame(loop);
+    if(secs > 1 / fps){
+        ObjectGridWrap.setPos(grid, (1 - per) * 2, Math.cos(Math.PI * bias) * 0.25 );
+        ObjectGridWrap.update(grid);
+        renderer.render(scene, camera);
+        frame += fps * secs;
+        frame %= maxFrame;
+        lt = now;
+    }
+};
+loop();
---
+++ views/forpost/threejs-examples-object-grid-wrap/s1-r1/js/object-grid-wrap.js
@@ -0,0 +1,145 @@
+//******** **********
+// ObjectGridWrap module - based off of threejs-examples-object-grid-wrap r1
+// * now works with mesh objects that use an array of materials
+//******** **********
+var ObjectGridWrap = (function(){
+    // public API
+    var api = {};
+    // some defaults
+    var  DEFAULT_SOURCE_OBJECTS = [
+        new THREE.Mesh( new THREE.BoxGeometry( 1, 1, 1), new THREE.MeshNormalMaterial()),
+        new THREE.Mesh( new THREE.SphereGeometry( 0.5, 30, 30), new THREE.MeshNormalMaterial())
+    ];
+    var DEFAULT_OBJECT_INDICES = [1,0,1,0,1,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,1,0,1,0,1];
+    // default cloner method
+    var DEFAULT_CLONER = function(opt, objectIndex){
+        var obj_root = opt.sourceObjects[objectIndex].clone();
+        obj_root.traverse(function(obj){
+            if(obj.material){
+                if(obj.material instanceof Array){
+                    obj.material = obj.material.map(function(m){
+                        return m.clone();
+                    });
+                }else{
+                    obj.material = obj.material.clone();
+                }
+            }
+        });
+        return obj_root;
+    };
+    // get a 'true' position in the form of a Vector2 for the given object index
+    // by true position I mean how things are with the state of the objectIndices array
+    // it can also be thought of as a kind of 'home position' as well
+    var getTruePos = function(grid, objectIndex){
+        var ud = grid.userData,
+        trueX = objectIndex % ud.tw,
+        trueZ = Math.floor(objectIndex / ud.tw);
+        return new THREE.Vector2(trueX, trueZ);
+    };
+    // get the adjusted position in which alphaX, and alphaZ values are applyed
+    var getAdjustedPos = function(grid, objectIndex){
+        var ud = grid.userData,
+        v_true = getTruePos(grid, objectIndex);
+        // adjusted by alphas
+        var ax = (v_true.x + ud.tw * ud.alphaX) % ud.tw;
+        var az = (v_true.y + ud.th * ud.alphaZ) % ud.th;
+        return new THREE.Vector2(ax, az);        
+    };
+    // The create method will create and return a new THREE.Group with desired source objects
+    // and induces for where clones of these objects shall be placed
+    api.create = function(opt){
+        opt = opt || {};
+        opt.sourceObjects = opt.sourceObjects || DEFAULT_SOURCE_OBJECTS;
+        opt.objectIndices = opt.objectIndices || DEFAULT_OBJECT_INDICES;
+        opt.tw = opt.tw === undefined ? 5: opt.tw; // tile width and height
+        opt.th = opt.th === undefined ? 5: opt.th;
+        opt.alphaX = 0; // alpha x and z values
+        opt.alphaZ = 0;
+        opt.cloner = opt.cloner || DEFAULT_CLONER;
+        var grid = new THREE.Group();
+        var ud = grid.userData;
+        ud.space = opt.space === undefined ? 1 : opt.space;
+        ud.alphaX = opt.alphaX;
+        ud.alphaZ = opt.alphaZ;
+        ud.tw = opt.tw;
+        ud.th = opt.th;
+        ud.aOpacity = opt.aOpacity === undefined ? 1.0 : opt.aOpacity;
+        var i = 0, len = opt.tw * opt.th;
+        while(i < len){
+            var objIndex = opt.objectIndices[i];
+            var obj = opt.cloner(opt, objIndex);
+            grid.add(obj);
+            i += 1;
+        };
+        api.update(grid);
+        return grid;
+    };
+    // set grid to alphas helper
+    var setGridToAlphas = function(grid, objectIndex){
+        var ud = grid.userData;
+        var obj = grid.children[objectIndex];
+        var v_adjust = getAdjustedPos(grid, objectIndex);
+        // use spacing
+        var x = v_adjust.x * ud.space;
+        var z = v_adjust.y * ud.space;
+        // subtract so that objects are centered
+        x -= (ud.tw - 1) * ud.space / 2;
+        z -= (ud.th - 1) * ud.space / 2;
+        // set position
+        obj.position.set(x, 0, z);
+    };
+    // set opacity for object and any and all nested objects
+    var setOpacity = function(obj_root, alpha){
+        obj_root.traverse(function(obj){
+            // any object with a material
+            if(obj.material){
+                //obj.material.transparent = true;
+                //obj.material.opacity = alpha;
+                if(obj.material instanceof Array){
+                    obj.material.forEach(function(m){
+                        m.transparent = true;
+                        m.opacity = alpha;
+                    });
+                }else{
+                    obj.material.transparent = true;
+                    obj.material.opacity = alpha;
+                }
+            }
+        });
+    };
+    // Object opacity check
+    var objectOpacityCheck = function(grid, objectIndex){
+        var ud = grid.userData,
+        obj = grid.children[objectIndex],
+        v_center = new THREE.Vector2(ud.tw / 2, ud.th / 2),
+        distMax = v_center.distanceTo( new THREE.Vector2(0.5, 0.5) );
+        var v_adjust = getAdjustedPos(grid, objectIndex);
+        var v2 = new THREE.Vector2(v_adjust.x + 0.5, v_adjust.y + 0.5),
+        d = v2.distanceTo( v_center );
+        d *= ud.aOpacity;        
+        d = d < 0 ? 0 : d;
+        d = d > distMax ? distMax : d;
+        var b = d / distMax;
+        b = 1 - b;
+        b = parseFloat(b.toFixed(2));
+        // call set opacity helper
+        setOpacity(obj, b);
+        //console.log(i, '(' + trueX + ',' + trueZ + ')', 'd=' + d.toFixed(2), distMax.toFixed(2), b);
+    };
+    // set position
+    api.setPos = function(grid, x, z){
+        var ud = grid.userData;
+        ud.alphaX = THREE.MathUtils.euclideanModulo(x, 1);
+        ud.alphaZ = THREE.MathUtils.euclideanModulo(z, 1);
+    };
+    // main update method
+    api.update = function(grid){
+        grid.children.forEach(function(obj, i){
+            setGridToAlphas(grid, i);
+            objectOpacityCheck(grid, i);
+        });
+    };
+    // return public API
+    return api;
+
+}());
