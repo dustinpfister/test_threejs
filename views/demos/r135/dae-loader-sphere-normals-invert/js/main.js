987e22a2a39edecce4f51273c4b5fc1c9a021fe0
dustinpfister <dustin.pfister@gmail.com>
2022-05-31 11:19:08 -0400

    clean up

--- views/demos/r135/dae-loader-sphere-normals-invert/js/main.js
+++ views/demos/r135/dae-loader-sphere-normals-invert/js/main.js
@@ -1,31 +1,23 @@
 
 (function () {
 
-    // scene
+    // SCENE
     var scene = new THREE.Scene();
     scene.add(new THREE.GridHelper(20, 20));
     scene.background = new THREE.Color('cyan');
-
-    //LIGHT
-    var dl = new THREE.DirectionalLight(0xffffff, 1);
-    scene.add(dl);
-
-    // point light
-    //var pl = new THREE.PointLight(0xffffff);
-    //pl.position.set(2, 5, 3);
-    //scene.add(pl);
-
-    // camera
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
     camera.position.set(-15, 15, -15);
     camera.lookAt(0, 0, 0);
     scene.add(camera);
-    // render
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
-    renderer.render(scene, camera);
-    // controls
+
+    // LIGHT
+    var dl = new THREE.DirectionalLight(0xffffff, 1);
+    scene.add(dl);
+
+    // CONTROL
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
     // app loop
