534ed73356d7c1bdeab0f645c9eefff1da2bfc68
dustinpfister <dustin.pfister@gmail.com>
2022-06-05 11:22:31 -0400

    code clean up

--- views/forpost/threejs-examples-lines-sphere-circles/r0/js/main.js
+++ views/forpost/threejs-examples-lines-sphere-circles/r0/js/main.js
@@ -13,10 +13,9 @@ document.getElementById('demo').appendChild(renderer.domElement);
 //******** **********
 // LINES
 //******** **********
-
 // just create one circle for a set of circles that form a sphere like shape
 // createSphereCirclePoints(maxRadius, circleCount, circleIndex, pointsPerCircle)
-var createSphereCirclePoints = function(maxRadius, circleCount, circleIndex, pointsPerCircle, randomDelta){
+var createSphereCirclePoints = function(maxRadius, circleCount, circleIndex, pointsPerCircle){
     var points = [];
     var sPer = circleIndex / circleCount;
     var radius = Math.sin( Math.PI * 1.0 * sPer ) * maxRadius;
@@ -25,22 +24,18 @@ var createSphereCirclePoints = function(maxRadius, circleCount, circleIndex, poi
     // buch points for the current circle
     while(i < pointsPerCircle){
         // might want to subtract 1 or 0 for this cPer expression
-        var cPer =  i / (pointsPerCircle - 1);
+        var cPer =  i / ( pointsPerCircle - 1 );
         var radian = Math.PI * 2 * cPer;
         var v = new THREE.Vector3();
         v.x = Math.cos(radian) * radius;
         v.y = y;
         v.z = Math.sin(radian) * radius;
-        var a = v.clone().normalize().multiplyScalar( maxRadius - randomDelta * THREE.MathUtils.seededRandom() );
-        // other cool ideas with deltas
-        //var a = v.clone().normalize().multiplyScalar( 1 + maxRadius * (i / (perCircle - 1)) );
-        //var a = v.clone().normalize().multiplyScalar( (maxRadius * (cPer * 1.25 + sPer * 5)) * 0.25 );
-        points.push(a);
+        points.push( v.clone().normalize().multiplyScalar(maxRadius) );
         i += 1;
     }
     return points;
 };
-
+// create sphere Lines helper
 var createSphereLines = function(maxRadius, circleCount, pointsPerCircle, randomDelta, colors){
     colors = colors || [0xff0000,0x00ff00,0x0000ff]
     var lines = new THREE.Group();
@@ -60,18 +55,14 @@ var createSphereLines = function(maxRadius, circleCount, pointsPerCircle, random
     };
     return lines;
 };
-
 var g = createSphereLines(5, 20, 50, 1.0, [0x00ffff, 0x008800, 0x008888, 0x00ff00]);
-console.log(g);
 scene.add(g);
 
 
 //******** **********
 // LOOP
 //******** **********
-
-new THREE.OrbitControls(camera, renderer.domElement);
-
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
 var fps = 30,
 lt = new Date(),
 frame = 0,
