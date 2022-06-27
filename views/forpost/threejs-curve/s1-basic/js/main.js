ab6f933efbc7d7a058c216a17fd769a2adb579f6
dustinpfister <dustin.pfister@gmail.com>
2022-06-27 11:18:34 -0400

    new basic example done

--- views/forpost/threejs-curve/s1-basic/js/main.js
+++ views/forpost/threejs-curve/s1-basic/js/main.js
@@ -5,8 +5,8 @@ let scene = new THREE.Scene();
 scene.background = new THREE.Color('#000000');
 scene.add( new THREE.GridHelper(10, 10, 0x00ff00, 0x4a4a4a) )
 let camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(10, 10, 10);
-camera.lookAt(0, 0, 0);
+camera.position.set(0, 5, 10);
+camera.lookAt(0, 3, 0);
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
@@ -16,29 +16,27 @@ document.getElementById('demo').appendChild(renderer.domElement);
 let dl = new THREE.DirectionalLight(0xffffff, 1);
 dl.position.set(3, 10, 1).normalize();
 scene.add(dl);
-
 scene.add( new THREE.AmbientLight(0xffffff, 0.05) )
-
 //******** **********
 // CURVE, TubeGeometry, Mesh
 //******** **********
-class CustomSinCurve extends THREE.Curve {
-    constructor( scale = 1 ) {
+// basic cuve class extending three curve
+class BasicCurve extends THREE.Curve {
+    constructor() {
         super();
-        this.scale = scale;
     }
     getPoint( t, optionalTarget = new THREE.Vector3() ) {
-        let tx = t * 3 - 1.5,
-        ty = Math.sin( 24 * Math.PI * t ) * 0.5,
-        tz = Math.cos( 24 * Math.PI * t ) * 0.25;
-        return optionalTarget.set( tx, ty, tz ).multiplyScalar( this.scale );
+        var tx = -2 + 4 * t,
+        ty = Math.pow(2, 5 * t) / Math.pow(2, 5) * 7,
+        tz = 0;
+        return optionalTarget.set( tx, ty, tz );
     }
 };
-let path = new CustomSinCurve( 5 ),
+// creating a tube geometry with path and additional arguments using basic curve
+let path = new BasicCurve(),
 tubularSegments = 800,
 radius = 0.25,
 radialSegments = 20;
-// creating a tube geometry with path and additional arguments
 let mesh = new THREE.Mesh( 
     new THREE.TubeGeometry( path, tubularSegments, radius, radialSegments, false ), 
     new THREE.MeshStandardMaterial( { color: 0xff0000, side: THREE.DoubleSide })
--- views/forpost/threejs-curve/todo.md
+++ views/forpost/threejs-curve/todo.md
@@ -3,9 +3,9 @@
 ## () - finhish sx-data-curve section
 * finish the data curve section
 
-## () - rename s1-basic to s2-spiral1 and start new s1-basic
-* rename s1-basic to s2-spiral1
-* start new s1-basic that is just a simple Math.pow curve
+## ( done 06/27/2022 ) - rename s1-basic to s2-spiral1 and start new s1-basic
+* (done) rename s1-basic to s2-spiral1
+* (done) start new s1-basic that is just a simple Math.pow curve
 
 ## ( done 06/27/2022 ) - make data curve section folder draft
 * (done) I am thinking that I should make the data curve section a draft folder for now
