d312029967e7f9aa76f4afc08e752d20382d9667
dustinpfister <dustin.pfister@gmail.com>
2022-07-02 13:03:45 -0400

    new proto folder

---
+++ views/demos/r140/proto-weird-face-1-eyes/js/main.js
@@ -0,0 +1,58 @@
+(function () {
+    // SCENE, CAMERA, RENDERER, LIGHT
+    var scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(20, 20));
+    scene.background = new THREE.Color('cyan');
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.1, 1000);
+    camera.position.set(2, 1, 2);
+    camera.lookAt(0, 0, 0);
+    scene.add(camera);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    var dl = new THREE.DirectionalLight(0xffffff, 1);
+    dl.position.set(2, 1, 3)
+    scene.add(dl);
+    // CONTROL
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    // APP LOOP
+    var frame = 0, frameMax = 300,
+    mouth, m0, m1;
+    var loop = function () {
+        requestAnimationFrame(loop);
+        renderer.render(scene, camera);
+        var per = frame / frameMax,
+        bias = 1 - Math.abs( per - 0.5) / 0.5;
+        // lerp mouth
+        lerpGeo(mouth.geometry, m0.geometry, m1.geometry, bias);
+        // step frame
+        frame += 1;
+        frame %= frameMax;
+    };
+    // USING DAE TOOLS TO LOAD THE *.dae FILE CONTENTS
+    var daeObjects = DAE.create({
+        onItemProgress: function(per, n, d){
+            console.log('progress: ' + per.toFixed(2) + ' ( ' + n + '/' + d + ' )');
+        },
+        onFileLoad: function(result, allResults, daeObjects){
+            console.log('fileLoad');
+        },
+        onLoad: function(daeObjects, results){
+            // mouth objects
+            var rScene = daeObjects.results[1].scene;
+            m0 = rScene.getObjectByName('mouth-0');
+            m1 = rScene.getObjectByName('mouth-1');
+            // mouth is based off of m0
+            mouth = new THREE.Mesh(m0.geometry.clone(), m0.material )
+            scene.add(mouth);
+            // start loop
+            loop();
+        }
+    });
+    // load dae files
+    DAE.loadAll(daeObjects, {
+        baseUrl: '/dae/weird-face-1',
+        relUrls: ['weird-face-1b.dae', 'mouths-1c.dae']
+    });
+}
+    ());
