ce1f252c10e7a879d1317fad31c42029f067c705
dustinpfister <dustin.pfister@gmail.com>
2022-07-07 10:57:38 -0400

    comments

--- views/forpost/threejs-examples-weird-face-1/r0/js/main.js
+++ views/forpost/threejs-examples-weird-face-1/r0/js/main.js
@@ -1,8 +1,8 @@
 (function () {
+    //******** **********
     // SCENE, CAMERA, RENDERER, LIGHT
+    //******** **********
     var scene = new THREE.Scene();
-    //var grid = new THREE.GridHelper(20, 20);
-    //scene.add( grid );
     scene.background = new THREE.Color('#444444');
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.1, 1000);
     camera.position.set(1.5, 0.25, 1.5);
@@ -14,9 +14,13 @@
     var dl = new THREE.DirectionalLight(0xffffff, 1);
     dl.position.set(2, 1, 3)
     scene.add(dl);
+    //******** **********
     // CONTROL
+    //******** **********
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
+    //******** **********
     // APP LOOP
+    //******** **********
     var frame = 0, frameMax = 300,
     nose, m0, m1;
     var loop = function () {
@@ -40,7 +44,9 @@
         frame += 1;
         frame %= frameMax;
     };
+    //******** **********
     // USING DAE TOOLS TO LOAD THE *.dae FILE CONTENTS
+    //******** **********
     var daeObjects = DAE.create({
         onItemProgress: function(per, n, d){
             console.log('progress: ' + per.toFixed(2) + ' ( ' + n + '/' + d + ' )');
@@ -70,5 +76,4 @@
         baseUrl: '/dae/weird-face-1',
         relUrls: ['weird-face-1c.dae', 'mouths-1c.dae']
     });
-}
-    ());
+}());
