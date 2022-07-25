fe8e496193a2170e009286b3934ec5fd2ca7d388
dustinpfister <dustin.pfister@gmail.com>
2022-07-25 13:09:59 -0400

    cretaing a data texture

--- views/forpost/threejs-emissive-map/s3-ani-loop/js/main.js
+++ views/forpost/threejs-emissive-map/s3-ani-loop/js/main.js
@@ -1,56 +1,103 @@
-// create data texture helper
-var createDataTexture = function (rPer, gPer, bPer) {
-    rPer = rPer || 0;
-    gPer = gPer || 0;
-    bPer = bPer || 0;
-    var width = 16,
-    height = 16;
-    var size = width * height;
-    var data = new Uint8Array(4 * size);
-    for (let i = 0; i < size; i++) {
-        var stride = i * 4;
-        var v = Math.floor(THREE.MathUtils.seededRandom() * 255);
-        data[stride] = v * rPer;
-        data[stride + 1] = v * gPer;
-        data[stride + 2] = v * bPer;
-        data[stride + 3] = 255;
-    }
-    var texture = new THREE.DataTexture(data, width, height);
-    texture.needsUpdate = true;
-    return texture;
+//******** **********
+// DATA TEXTURE METHODS
+//******** **********
+// make a data textture by passing w, h, and a function
+// that will be called for each pixel
+var datatex = (function () {
+    var api = {};
+    // mk data texture helper
+    api.mkDataTexture = function (data, w) {
+        data = data || [];
+        w = w || 0;
+        var width = w,
+        height = data.length / 4 / w;
+        var texture = new THREE.DataTexture(data, width, height);
+        texture.needsUpdate = true;
+        return texture;
+    };
+    // create a data texture with a method that will be called for each pix
+    api.forEachPix = function (w, h, forEach) {
+        var width = w === undefined ? 5 : w,
+        height = h === undefined ? 5 : h;
+        var size = width * height;
+        var data = new Uint8Array(4 * size);
+        for (let i = 0; i < size; i++) {
+            var stride = i * 4;
+            var x = i % width;
+            var y = Math.floor(i / width);
+            var obj = forEach(x, y, w, h, i, stride, data);
+            obj = obj || {};
+            data[stride] = obj.r || 0;
+            data[stride + 1] = obj.g || 0;
+            data[stride + 2] = obj.b || 0;
+            data[stride + 3] = obj.a === undefined ? 255: obj.a;
+        }
+        return api.mkDataTexture(data, width)
+    };
+    // return the api
+    return api;
+}
+    ());
+// square texture
+var circleTexture = function(t){
+    t = t === undefined ? 0 : t;
+    return datatex.forEachPix(16, 16, function(x, y, w, h, i, stride, data){
+        var v = new THREE.Vector2(x, y);
+        var d = v.distanceTo( new THREE.Vector2(w / 2, h / 2) );
+        var cv = d / 16 * 255;
+        return {
+            r: cv
+        };
+    });
 };
-// create emissive cube helper
-var createCube = function (emissiveMap, map, emissiveIntensity) {
-    return new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshStandardMaterial({
-            color: new THREE.Color(1, 1, 1),
-            map: map || null,
-            emissiveIntensity: emissiveIntensity || 0,
-            emissive: new THREE.Color(1, 1, 1),
-            emissiveMap: emissiveMap || null
-        }));
-};
-// scene
+
+//******** **********
+// SCENE, CAMERA, RENDERER
+//******** **********
 var scene = new THREE.Scene();
 scene.add( new THREE.GridHelper(10, 10));
-// mesh objects
-[ [[1,1,1], 1], [[0,0,1], 0.25], [[0,1,0], 0.1] ].forEach(function(cubeArgs, i, arr){
-    var emissiveMap = createDataTexture.apply(null, cubeArgs[0]);
-    var map = null;
-    var box = createCube(emissiveMap, map, cubeArgs[1]);
-    box.position.x = -5 + 10 * (i / arr.length);
-    scene.add(box);
-});
-// light
-var light = new THREE.PointLight(new THREE.Color(1, 1, 1), 0.25);
-light.position.set(8, 10, 2);
-scene.add(light);
-// camera, render
 var camera = new THREE.PerspectiveCamera(40, 320 / 240, 0.1, 1000);
-camera.position.set(4, 4, 4);
+camera.position.set(3, 2, 3);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 document.getElementById('demo').appendChild(renderer.domElement);
-renderer.render(scene, camera);
+//******** **********
+// LIGHT
+//******** **********
+var dl = new THREE.DirectionalLight(0xffffff, 1);
+dl.position.set(8, 10, 2);
+scene.add(dl);
+//******** **********
+// MESH
+//******** **********
+var mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshStandardMaterial({
+        map: circleTexture()
+    })
+);
+scene.add(mesh);
+//******** **********
+// LOOP
+//******** **********
+var fps = 30,
+lt = new Date(),
+frame = 0,
+maxFrame = 600;
+var loop = function () {
+    var now = new Date(),
+    per = frame / maxFrame,
+    bias = 1 - Math.abs(0.5 - per) / 0.5,
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if(secs > 1 / fps){
+
+
+        renderer.render(scene, camera);
+        frame += fps * secs;
+        frame %= maxFrame;
+        lt = now;
+    }
+};
+loop();
