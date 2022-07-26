33682f36b45c52c1eb5d82bb1ac03a2c52ddee05
dustinpfister <dustin.pfister@gmail.com>
2022-07-26 10:31:29 -0400

    nice animation

--- views/forpost/threejs-examples-position-things-to-sphere-surface/r1/js/main.js
+++ views/forpost/threejs-examples-position-things-to-sphere-surface/r1/js/main.js
@@ -17,32 +17,34 @@ var sphere1 = new THREE.Mesh(
     new THREE.MeshNormalMaterial({wireframe: true})
 );
 scene.add(sphere1);
-
 var box1 =  new THREE.Mesh(
     new THREE.BoxGeometry(0.5, 0.5, 0.5),
     new THREE.MeshNormalMaterial()
 );
 scene.add(box1);
-// position to sphere
-SphereWrap.positionToSphere(sphere1, box1, 0.0, 0.0, 0.25);
-// using lookAt method to set box rotation
-box1.lookAt(sphere1.position);
-
 //******** **********
 // LOOP
 //******** **********
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 var lt = new Date(),
 frame = 0,
-maxFrame = 600,
+maxFrame = 100,
 fps = 30;
 var loop = function () {
     var now = new Date(),
     per = frame / maxFrame,
-    bias = 1 - Math.abs(per - 0.5) / 0.5,
     secs = (now - lt) / 1000;
     requestAnimationFrame(loop);
     if (secs > 1 / fps) {
+ 
+        // position to sphere
+        var p = per * 6 % 1,
+        b = 1 - Math.abs(0.5 - p) / 0.5;
+        SphereWrap.positionToSphere(sphere1, box1, 0.75 - 0.5 * b, per, 0.25);
+ 
+        // using lookAt method to set box rotation
+        box1.lookAt(sphere1.position);
+ 
         renderer.render(scene, camera);
         frame += fps * secs;
         frame %= maxFrame;
--- views/forpost/threejs-examples-position-things-to-sphere-surface/todo.md
+++ views/forpost/threejs-examples-position-things-to-sphere-surface/todo.md
@@ -4,8 +4,8 @@
 * new sphere map module that makes use of a raycaster as a way to do this
 * have weird spahere like shapes for the demo
 
-## () - r1 - use set from euler method
-* () new sphere wrap module that uses set from euler in place of solution using groups
+## ( done 07/26/2022 ) - r1 - use set from euler method
+* (done) new sphere wrap module that uses set from euler in place of solution using groups
 
 ## ( done 05/14/2021 ) - first version of example
 * (done) first version of sphere_wrap.js that uses groups done
