98ecad74e85eedbfc26c1f73696f6d3b4d1aa669
dustinpfister <dustin.pfister@gmail.com>
2022-07-26 09:33:11 -0400

    threejs-examples-position-things-to-sphere-surface for post folder

---
+++ views/forpost/threejs-examples-position-things-to-sphere-surface/r0/js/main.js
@@ -0,0 +1,80 @@
+var scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(10, 10));
+
+// camera and renderer
+var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(3.0, 3.0, 3.0);
+camera.lookAt(0, 0, 0);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+document.getElementById('demo').appendChild(renderer.domElement);
+
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+// add wrap the the scene, and some cube objects
+var wrap = SphereWrap.createWrap();
+scene.add(wrap);
+SphereWrap.addObjectToWrap(wrap, 'cube');
+SphereWrap.addObjectToWrap(wrap, 'cube2');
+
+// adding a cone rather than the default cube
+// some times I might want to rotate the geometry when doing so
+// rather than Object3d as I will always have that looking at the origin of the
+// sphere wrap group
+var cone = new THREE.Mesh(
+        new THREE.ConeGeometry(0.25, 0.5, 30, 30),
+        new THREE.MeshNormalMaterial({
+            wireframe: true
+        }));
+cone.geometry.rotateX(Math.PI * 1.5);
+SphereWrap.addObjectToWrap(wrap, 'cone', cone);
+
+var wrap2 = SphereWrap.createWrap();
+SphereWrap.addObjectToWrap(wrap2, 'box');
+SphereWrap.setObjToLatLong(wrap2, 'box', 0.75, 0.9, 1.25);
+wrap2.position.set(-4, 0, -4);
+scene.add(wrap2);
+
+// dist and lat log values
+var dist = 1.25, // radius + half of mesh height
+latPer = 0.75, // 0 - 1
+longPer = 0.5; // 0 - 1
+SphereWrap.setObjToLatLong(wrap, 'cube', latPer, longPer, dist);
+SphereWrap.setObjToLatLong(wrap, 'cube2', 0, 0, 1.25);
+SphereWrap.setObjToLatLong(wrap, 'cone', 0.9, 1, 1.25);
+// loop
+var lt = new Date(),
+frame = 0,
+maxFrame = 600,
+fps = 30;
+var loop = function () {
+    var now = new Date(),
+    per = frame / maxFrame,
+    bias = 1 - Math.abs(per - 0.5) / 0.5,
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if (secs > 1 / fps) {
+
+        // setting lat and long for 'cube'
+        latPer = 0.25 + Math.sin(Math.PI * bias) * 0.25;
+        longPer = per;
+        SphereWrap.setObjToLatLong(wrap, 'cube', latPer, longPer, dist);
+
+        // rotating cube2
+        var obj = wrap.getObjectByName('cube2');
+        obj.geometry.rotateZ(Math.PI / 180 * 20 * secs);
+
+        // cone
+        SphereWrap.setObjToLatLong(wrap, 'cone', 1 - 0.25 * bias, 1 - 1 * per, dist);
+
+        // whole group
+        wrap.position.x = 1 - 2 * bias;
+        wrap.position.z = Math.sin(Math.PI * 2 * bias) * 2;
+
+        renderer.render(scene, camera);
+        frame += fps * secs;
+        frame %= maxFrame;
+        lt = now;
+    }
+}
+loop();
---
+++ views/forpost/threejs-examples-position-things-to-sphere-surface/r0/js/sphere_wrap.js
@@ -0,0 +1,64 @@
+(function (api) {
+
+    // create wrap method
+    api.createWrap = function () {
+        // create a wrap group
+        var wrap = new THREE.Group();
+        // add a sphere to the wrap
+        var sphere = new THREE.Mesh(
+                new THREE.SphereGeometry(1, 40, 40),
+                new THREE.MeshNormalMaterial({
+                    wireframe: true
+                }));
+        wrap.userData.sphere = sphere;
+        wrap.add(sphere);
+        // create a surface group and add to wrap
+        var surface = new THREE.Group();
+        wrap.userData.surface = surface;
+        wrap.add(surface);
+        return wrap;
+    };
+
+    // set to lat and long helper
+    api.setObjToLatLong = function (wrap, childName, latPer, longPer, dist) {
+        var childWrap = wrap.getObjectByName('objwrap_' + childName),
+        child = childWrap.children[0]; //wrap.getObjectByName(childName),
+        // set lat
+        var radian = Math.PI * -0.5 + Math.PI * latPer,
+        x = Math.cos(radian) * dist,
+        y = Math.sin(radian) * dist;
+        child.position.set(x, y, 0);
+        // set long
+        childWrap.rotation.y = Math.PI * 2 * longPer;
+        // look at origin of wrap
+		var v = new THREE.Vector3();
+		wrap.getWorldPosition(v);
+        child.lookAt(v);
+    };
+
+    // Add an Object to a Sphere Wrap Group
+    api.addObjectToWrap = function (wrap, objectName, obj) {
+        // create an obj
+        obj = obj || new THREE.Mesh(
+                new THREE.BoxGeometry(0.5, 0.5, 0.5),
+                new THREE.MeshNormalMaterial({
+                    wireframe: false
+                }));
+        obj.name = objectName;
+        var objWrap = new THREE.Group();
+        objWrap.name = 'objwrap_' + objectName;
+        objWrap.add(obj);
+        // obj wrap user data object
+        var ud = objWrap.userData;
+        ud.latPer = 0;
+        ud.longPer = 0;
+        var radius = wrap.userData.sphere.geometry.parameters.radius;
+        ud.dist = radius + 0.25;
+        // add the objWrap group to the surface group
+        wrap.userData.surface.add(objWrap);
+        //set position for the first time
+        api.setObjToLatLong(wrap, objectName, ud.latPer, ud.longPer, ud.dist);
+    };
+
+}
+    (this['SphereWrap'] = {}));
