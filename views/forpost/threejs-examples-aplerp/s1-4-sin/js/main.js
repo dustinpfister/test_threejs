21da7a6d2309575bc63461decb0416e3f9855bb9
dustinpfister <dustin.pfister@gmail.com>
2022-07-28 12:15:04 -0400

    linkng the sin example

--- views/forpost/threejs-examples-aplerp/s1-4-sin/js/main.js
+++ views/forpost/threejs-examples-aplerp/s1-4-sin/js/main.js
@@ -17,11 +17,19 @@
     //******** **********
     // MATH.SIN
     //******** **********
-    var parabolaGetAlpha = function(state, param){
-        var r = Math.PI * 6 * state.p;
+    var sinGetAlpha = function(state, param){
+        param.piM = param.piM === undefined ? 2 : param.piM;
+        param.bMulti = param.bMulti=== undefined ? 0.1 : param.bMulti;
+        param.aOffset = param.aOffset=== undefined ? 0.5 : param.aOffset;
+        var r = Math.PI * param.piM * state.p;
         var b = Math.sin( r );
-        var a = state.p + b * 0.1;
-console.log(b)
+        var a = state.p + b * param.bMulti;
+        // apply aOffset
+        a += param.aOffset;
+        a %= 1;
+        // clamp
+        a = a < 0 ? 0 : a;
+        a = a > 1 ? 1 : a;
         return a;
     };
     var v1 = new THREE.Vector3(-5, 0, 0);
@@ -29,10 +37,13 @@ console.log(b)
     var group = apLerp.createSpheresGroup({
             v1: v1,
             v2: v2,
-            count: 100,
+            count: 30,
             include: false,
-            getAlpha: parabolaGetAlpha,
+            getAlpha: sinGetAlpha,
             gaParam: {
+                piM: 4,
+                bMulti: 0.05,
+                aOffset: 0
             }
         });
     scene.add(group);
