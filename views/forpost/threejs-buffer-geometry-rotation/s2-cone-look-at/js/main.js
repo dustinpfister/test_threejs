d7d9c0379437011a3d341a27d2439d48f263eea3
dustinpfister <dustin.pfister@gmail.com>
2022-08-08 14:57:18 -0400

    have current example in post in for post folder

--- views/forpost/threejs-buffer-geometry-rotation/s2-cone-look-at/js/main.js
+++ views/forpost/threejs-buffer-geometry-rotation/s2-cone-look-at/js/main.js
@@ -1,31 +1,36 @@
 (function () {
-
-    // SCENE, CAMERA, RENDERER
+    // scene
     var scene = new THREE.Scene();
-    scene.add( new THREE.GridHelper(10, 10) );
+    scene.add(new THREE.GridHelper(7, 7)); // grid helper for the scene
+    // geometry
+    var geometry = new THREE.ConeGeometry(0.5, 03, 30, 30);
+    // Mesh
+    var cone = new THREE.Mesh(
+            geometry,
+            new THREE.MeshNormalMaterial());
+ 
+    // USING BUFFER GEOMERTY ROTATEX METHOD
+    cone.geometry.rotateX(Math.PI * 0.5);
+ 
+    cone.add(new THREE.BoxHelper(cone)); // adding a box helper
+    scene.add(cone); // add custom to the scene
+ 
+    // adding a cube to have the cone point to
+    var cube = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshNormalMaterial());
+    cube.position.set(3, 0, 3);
+    scene.add(cube)
+ 
+    cone.lookAt(cube.position); // using Object3d (base class of Mesh) lookAt
+ 
+    // camera render
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
-    camera.position.set(0, 1, 3);
+    camera.position.set(6, 8, 6);
+    camera.lookAt(cone.position);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
- 
-    // GEOMETRY, MESH
-    var geometry = new THREE.BufferGeometry();
-    var vertices = new Float32Array([
-                -1, 0, 0,
-                1, 0, 0,
-                1, 1.25, 0
-            ]);
-    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-    var mesh = new THREE.Mesh(
-            geometry,
-            new THREE.MeshBasicMaterial({
-                side: THREE.DoubleSide
-            }));
-    scene.add(mesh);
-    camera.lookAt(mesh.position)
- 
-    // RENDER
     renderer.render(scene, camera);
  
 }
