1cfc1073293918e65fccb1d7e6f93de6c1724341
dustinpfister <dustin.pfister@gmail.com>
2022-08-09 10:10:21 -0400

    new rotation example

--- views/forpost/threejs-buffer-geometry/s6-rotation/js/main.js
+++ views/forpost/threejs-buffer-geometry/s6-rotation/js/main.js
@@ -1,48 +1,66 @@
 (function () {
-    // SCENE
+    //******** **********
+    // SCENE, CAMERA, RENDER
+    //******** **********
     var scene = new THREE.Scene();
-    scene.add( new THREE.GridHelper(10, 10) );
- 
-    // geometry
-    var geometry = new THREE.BufferGeometry();
-    var vertices = new Float32Array([
-                0, 0, 0, // triangle 1
-                1, 0, 0,
-                1, 1, 0
-            ]);
-    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-    // TRANSLATE AND ROTATE
-    geometry.rotateZ(Math.PI / 180 * 135);
-    geometry.translate(0.75, 0, 0);
-
-    // mesh
-    var custom = new THREE.Mesh(
-            geometry,
-            new THREE.MeshBasicMaterial({
-                color: 'red',
-                side: THREE.DoubleSide
-            }));
-    scene.add(custom);
-    // render, camera, and loop
+    scene.add(new THREE.GridHelper(7, 7)); // grid helper for the scene
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
+    camera.position.set(6, 8, 6);
+    camera.lookAt(0, 0, 0);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
-    camera.position.set(1, 2, 4);
-    camera.lookAt(0, 0, 0);
+    //******** **********
+    // MESH OBJECTS
+    //******** **********
+    var mesh1 = new THREE.Mesh(
+        new THREE.ConeGeometry(0.25, 2, 30, 30),
+        new THREE.MeshNormalMaterial()
+    );
+    mesh1.position.set(-1.5, 0, 0);
+    scene.add(mesh1);
+    var mesh2 = new THREE.Mesh(
+        new THREE.ConeGeometry(0.25, 2, 30, 30),
+        new THREE.MeshNormalMaterial()
+    );
+    mesh2.position.set(1.5, 0, 0);
+    scene.add(mesh2);
+    // CHILD MESH OBEJECTS
+    var childMaterial = new THREE.MeshNormalMaterial({ 
+        transparent: true,
+        opacity: 0.5
+    });
+    mesh1.add( new THREE.Mesh(
+        new THREE.BoxGeometry(2, 2, 2),
+        childMaterial) );
+    mesh2.add( new THREE.Mesh(
+        new THREE.BoxGeometry(2, 2, 2),
+        childMaterial) );
+    //******** **********
+    // LOOP
+    //******** **********
     var frame = 0,
     maxFrame = 200,
     fps_target = 24,
     lt = new Date();
     var loop = function () {
         var now = new Date(),
-        secs = (now - lt) / 1000;
+        secs = (now - lt) / 1000,
+        per = frame / maxFrame,
+        bias = Math.abs(.5 - per) / .5;
         requestAnimationFrame(loop);
         if (secs >= 1 / fps_target) {
-            var per = frame / maxFrame,
-            bias = Math.abs(.5 - per) / .5,
-            r = -Math.PI * 2 * per;
-            custom.rotation.set(0, Math.PI * 2 * per, 0);
+            // ROTATION OF GEOMETRY COMPARED TO MESH
+            var rx = Math.PI * 2 * per,
+            rz = Math.PI * 8 * per;
+            // USING COPY AND ROTATION METHODS
+            mesh1.geometry.copy( new THREE.ConeGeometry(0.25, 2, 30, 30) );
+            mesh1.geometry.rotateX( rx );
+            mesh1.geometry.rotateZ( rz );
+            // USING OBJECT3D ROTATION PROPERTY OF MESH2 to ROTATE THE MESH OBJECT
+            // RATHER THAN THE GEOMETRY
+            mesh2.rotation.set(rx ,0, rz)
+            // render, step frame, ect...
             renderer.render(scene, camera);
             frame += 1;
             frame %= maxFrame;
