82fc00e4e3065428c5433f7f3b313a213fa69a94
dustinpfister <dustin.pfister@gmail.com>
2022-08-24 17:04:07 -0400

    have the main file set up in a good way so far

--- views/forpost/threejs-examples-object-grid-wrap-land/r3/js/main.js
+++ views/forpost/threejs-examples-object-grid-wrap-land/r3/js/main.js
@@ -18,11 +18,11 @@ dl.position.set(-8, 2, 4);
 scene.add(dl);
 //camera.add(dl);
 //scene.add(camera);
-scene.add( new THREE.AmbientLight(0xffffff, 0.05 ) )
+scene.add( new THREE.AmbientLight(0xffffff, 0.05 ) );
 //******** **********
-// GRID
+// GRID OPTIONS
 //******** **********
-var grid = ObjectGridWrapLand.create({
+var gridOpt = {
     tw: 14,
     th: 14,
     crackSize: 0,
@@ -59,65 +59,13 @@ var grid = ObjectGridWrapLand.create({
         0, 8, 2, 2, 2, 5, 0, 0, 0, 0, 8, 2, 5, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ]
-});
-grid.scale.set(1, 1, 1);
-
-ObjectGridWrapLand.setDataTextures(grid)
-
-scene.add(grid);
-//******** **********
-// ADDING CHILD MESH OBJECTS
-//******** **********
-var mkCone = function(height){
-    return new THREE.Mesh(
-        new THREE.ConeGeometry(0.5, height, 30, 30),
-        new THREE.MeshStandardMaterial({color: new THREE.Color('#00ff88')})
-    );
 };
-// can make another system that involves a grid if index values
-// but with child objects
-var mkMeshFunctions = [
-    null,
-    function(){
-        return mkCone(2)
-    },
-    function(){
-        return mkCone(3)
-    },
-    function(){
-        return mkCone(4)
-    }
-];
-// object index grid
-[
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,2,0,0,0,0,0,0,0,1,0,0,
-    0,0,1,0,0,0,3,0,0,1,2,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,1,0,1,1,0,0,
-    0,0,0,1,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,1,0,1,2,1,0,0,
-    0,0,2,0,0,0,0,0,0,2,0,0,0,0,
-    0,0,0,1,0,0,0,2,1,0,1,1,0,0,
-    0,0,1,0,0,0,0,1,0,1,3,3,0,1,
-    0,1,0,1,0,1,2,0,1,2,1,1,2,0,
-    0,0,0,0,2,0,0,1,0,3,1,1,0,0,
-    0,1,0,1,0,1,0,0,0,1,2,3,1,1,
-    0,0,0,0,0,0,0,0,0,0,1,0,1,0
-].forEach(function(objIndex, i){
-    var mkMesh = mkMeshFunctions[objIndex];
-    if(mkMesh){
-        var mesh = mkMesh(),
-        x = i % grid.userData.tw,
-        y = Math.floor(i / grid.userData.tw)
-        ObjectGridWrapLand.addAt(grid, mesh, x, y);
-    }
-});
 //******** **********
 // LOOP
 //******** **********
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 var fps = 30,
+grid,
 lt = new Date(),
 frame = 0,
 maxFrame = 600;
@@ -139,4 +87,25 @@ var loop = function () {
         lt = now;
     }
 };
-loop();
+//******** **********
+// DAE - load land-set-one dae file
+//******** **********
+// CREATE A COLLADALOADER INSTANCE
+var loader = new THREE.ColladaLoader();
+// CALL THE LOAD METHOD, PASS THE ABSOLUTE OR RELATIVE PATH
+// TO THE *.DAE FILE AS THE FIRST ARGUMENT, AND A DONE CALLBACK
+// AS THE SECOND ARGUMENT
+loader.load("/dae/land-set-one/land-set-1b.dae", function (result) {
+    console.log(result.scene);
+    //******** **********
+    // CREATE GRID with gridOpt
+    //******** **********
+    grid = ObjectGridWrapLand.create(gridOpt);
+    grid.scale.set(1, 1, 1);
+    ObjectGridWrapLand.setDataTextures(grid)
+    scene.add(grid);
+    //******** **********
+    // START LOOP
+    //******** **********
+    loop();
+});
