bf75fd70368c778f118e49b85d804fee61c675c3
dustinpfister <dustin.pfister@gmail.com>
2022-08-25 14:42:50 -0400

    we are looking good now

--- views/demos/r140/proto-many-object-tweening/README.md
+++ views/demos/r140/proto-many-object-tweening/README.md
@@ -1,3 +1,3 @@
 # many-object-tweening proto folder
 
-
+The idea here is to use the alerp geo method I worked out of my threejs example on the subject to work out a new system for animation. There is having many objects in a dae file that all contain differing states of the same geometry, and just using the lerp geo method to swich between one and the other.
--- views/demos/r140/proto-many-object-tweening/js/main.js
+++ views/demos/r140/proto-many-object-tweening/js/main.js
@@ -1,11 +1,40 @@
 
 (function () {
 
+    // LERP GEO FUNCTION based off the method from threejs-examples-lerp-geo
+    var lerpGeo = function(geo, geoA, geoB, alpha){
+        alpha = alpha || 0;
+        // pos, and new pos
+        let pos = geo.getAttribute('position');
+        // positions for a and b
+        let posA = geoA.getAttribute('position');
+        let posB = geoB.getAttribute('position');
+        // loop over pos and lerp between posA and posB
+        var i = 0, len = pos.array.length;
+        while(i < len){
+            // creating Vector3 instances for current posA and PosB vertices
+            var v = new THREE.Vector3(posA.array[i], posA.array[i + 1], posA.array[i + 2]);
+            var v2 = new THREE.Vector3(posB.array[i], posB.array[i + 1], posB.array[i + 2]);
+            // lerping between v and v2 with given alpha value
+            v.lerp(v2, alpha);
+            // set pos vertex to state of v
+            pos.array[i] = v.x;
+            pos.array[i + 1] = v.y;
+            pos.array[i + 2] = v.z;
+            i += 3;
+        }
+        // the needs update bool of pos should be set true
+        pos.needsUpdate = true;
+
+        geo.computeVertexNormals();
+
+    };
+
     // scene
     var scene = new THREE.Scene();
     scene.background = new THREE.Color('cyan');
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
-    camera.position.set(5, 5, 5);
+    camera.position.set(2, 2, 2);
     camera.lookAt(0, 0, 0);
     scene.add(camera);
     var renderer = new THREE.WebGLRenderer();
@@ -20,21 +49,47 @@
 
     // controls
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    // source object
+    var sourceObj = {};
+    var mesh;
+
     // app loop
+    var lt = new Date();
     var loop = function () {
+
+        var now = new Date();
+        var secs = (now - lt) / 1000 ;
+
         requestAnimationFrame(loop);
-        renderer.render(scene, camera);
+
+        if(secs >= 1 / 30){
+            lerpGeo(mesh.geometry, sourceObj.box_1.geometry, sourceObj.box_2.geometry, 1 );
+            lt = new Date();
+        }
+
         controls.update();
+        renderer.render(scene, camera);
     };
 
     // load dae, start loop
     var loader = new THREE.ColladaLoader();
     loader.load("/dae/many-object-tweening/many-object-tweening-1a.dae", function (result) {
-        [ 'box-1', 'box-2', 'box-3' ].forEach(function(objName, i, arr){
-            var sourceObj = result.scene.getObjectByName(objName);
-            sourceObj.position.set(-3 + 6 * ( i / ( arr.length - 1) ), 0, 0);
-            scene.add(sourceObj);
+        // get objects by name
+        [ 'box_1', 'box_2' ].forEach(function(objName, i, arr){
+            // get the source object and change position to 0, 0, 0
+            var obj = result.scene.getObjectByName(objName);
+            obj.position.set(0, 0, 0);
+            // log name and count of position attribute
+            console.log( obj.name, obj.geometry.attributes.position.count );
+            // add ref to sourceObj
+            sourceObj[objName] = obj;
         });
+        // can now make new mesh objects by cloning a source object
+        mesh = sourceObj.box_1.clone();
+        mesh.geometry = sourceObj.box_1.geometry.clone();
+        scene.add(mesh);
+
         loop();
     });
 
