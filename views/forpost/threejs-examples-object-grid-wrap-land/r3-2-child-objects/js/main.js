103020a2e523cacc261661957927d94747ba1735
dustinpfister <dustin.pfister@gmail.com>
2022-08-30 15:34:37 -0400

    better view

--- views/forpost/threejs-examples-object-grid-wrap-land/r3-2-child-objects/js/main.js
+++ views/forpost/threejs-examples-object-grid-wrap-land/r3-2-child-objects/js/main.js
@@ -5,7 +5,7 @@ var scene = new THREE.Scene();
 scene.background = new THREE.Color('#00afaf');
 //scene.add( new THREE.GridHelper(10, 10, 0x00ff00, 0xffffff) )
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1.0, 30);
-camera.position.set(-7.09, 9.35, 9.03);
+camera.position.set(-5, 8, 8);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
@@ -99,84 +99,83 @@ ObjectGridWrapLand.load('/dae/land-set-one/land-set-1c.dae')
     grid.scale.set(1, 1, 1);
     ObjectGridWrapLand.setDataTextures(grid)
     scene.add(grid);
-//******** **********
-// ADDING CHILD MESH OBJECTS
-//******** **********
-var cObjScale = 0.50;
-// get a random radian
-var getRandomRadian = function(){
-    return Math.PI * 2 * THREE.MathUtils.seededRandom();
-};
-var setRandomScale = function(mesh, sMin, sMax){
-    var scale = sMin + ( sMax - sMin ) * THREE.MathUtils.seededRandom();
-    mesh.scale.set(scale, scale, scale);
-};
-// can make another system that involves a grid of index values
-// but with child objects
-var mkMeshFunctions = [
-    null,
-    function(){
-        var mesh = sObj.tree_1.clone();
-        mesh.material = sObj.tree_1.material.clone();
-        setRandomScale(mesh, 0.25, 0.75);
-        return mesh;
-    },
-    function(){
-        var mesh = sObj.tree_2.clone();
-        mesh.material = sObj.tree_2.material.clone();
-        setRandomScale(mesh, 0.25, 0.75);
-        mesh.rotation.y = getRandomRadian()
-        return mesh;
-    },
-    function(){
-        var mesh = sObj.tree_3.clone();
-        mesh.material = sObj.tree_3.material.clone();
-        setRandomScale(mesh, 0.35, 0.50);
-        mesh.rotation.y = getRandomRadian()
-        return mesh;
-    }
-];
-// object index grid
-[
-    2,2,0,0,0,0,0,0,0,0,2,2,1,2,
-    2,1,2,2,0,0,0,0,0,0,0,1,2,0,
-    0,2,1,2,2,0,3,0,0,1,2,2,1,2,
-    0,0,2,2,2,2,0,0,0,0,3,2,2,2,
-    0,0,0,2,2,0,0,0,1,0,1,1,2,2,
-    0,0,0,1,2,2,2,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,1,0,1,2,1,0,0,
-    0,0,2,0,0,0,0,0,0,2,0,0,0,0,
-    0,0,0,1,0,0,0,2,1,0,1,1,0,0,
-    0,0,1,0,0,0,0,1,0,1,3,3,0,1,
-    0,1,0,1,0,1,2,0,1,2,1,1,2,0,
-    0,0,0,0,2,0,0,1,0,3,1,1,2,2,
-    2,1,0,1,0,1,0,0,2,1,2,3,1,1,
-    2,2,0,0,0,0,0,0,0,2,1,2,1,2
-].forEach(function(objIndex, i){
-    var mkMesh = mkMeshFunctions[objIndex];
-    if(mkMesh){
-        var mesh = mkMesh(),
-        x = i % grid.userData.tw,
-        y = Math.floor(i / grid.userData.tw);
-        // add at method
-        ObjectGridWrapLand.addAt(grid, mesh, x, y, function(tile, mesh, tileUD){
-            // default to just the alt value of the tile
-            mesh.position.y = tileUD.alt;
-            // if tile is a cube
-            if(tileUD.isCube){
-                mesh.position.y = tileUD.alt + 0.25;
-            }
-            // if tile is a cube
-            if(tileUD.isSlope){
-                mesh.position.y = tileUD.alt - 0.25;
-            }
-            // if tile is a cube
-            if(tileUD.isCorner){
-                mesh.position.y = tileUD.alt - 0.25;
-            }
-        });
-    }
-});
+    //******** **********
+    // ADDING CHILD MESH OBJECTS
+    //******** **********
+    // get a random radian
+    var getRandomRadian = function(){
+        return Math.PI * 2 * THREE.MathUtils.seededRandom();
+    };
+    var setRandomScale = function(mesh, sMin, sMax){
+        var scale = sMin + ( sMax - sMin ) * THREE.MathUtils.seededRandom();
+        mesh.scale.set(scale, scale, scale);
+    };
+    // can make another system that involves a grid of index values
+    // but with child objects
+    var mkMeshFunctions = [
+        null,
+        function(){
+            var mesh = sObj.tree_1.clone();
+            mesh.material = sObj.tree_1.material.clone();
+            setRandomScale(mesh, 0.25, 0.75);
+            return mesh;
+        },
+        function(){
+            var mesh = sObj.tree_2.clone();
+            mesh.material = sObj.tree_2.material.clone();
+            setRandomScale(mesh, 0.25, 0.75);
+            mesh.rotation.y = getRandomRadian()
+            return mesh;
+        },
+        function(){
+            var mesh = sObj.tree_3.clone();
+            mesh.material = sObj.tree_3.material.clone();
+            setRandomScale(mesh, 0.35, 0.50);
+            mesh.rotation.y = getRandomRadian()
+            return mesh;
+        }
+    ];
+    // object index grid
+    [
+        2,2,0,0,0,0,0,0,0,0,2,2,1,2,
+        2,1,2,2,0,0,0,0,0,0,0,1,2,0,
+        0,2,1,2,2,0,3,0,0,1,2,2,1,2,
+        0,0,2,2,2,2,0,0,0,0,3,2,2,2,
+        0,0,0,2,2,0,0,0,1,0,1,1,2,2,
+        0,0,0,1,2,2,2,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,1,0,1,2,1,0,0,
+        0,0,2,0,0,0,0,0,0,2,0,0,0,0,
+        0,0,0,1,0,0,0,2,1,0,1,1,0,0,
+        0,0,1,0,0,0,0,1,0,1,3,3,0,1,
+        0,1,0,1,0,1,2,0,1,2,1,1,2,0,
+        0,0,0,0,2,0,0,1,0,3,1,1,2,2,
+        2,1,0,1,0,1,0,0,2,1,2,3,1,1,
+        2,2,0,0,0,0,0,0,0,2,1,2,1,2
+    ].forEach(function(objIndex, i){
+        var mkMesh = mkMeshFunctions[objIndex];
+        if(mkMesh){
+            var mesh = mkMesh(),
+            x = i % grid.userData.tw,
+            y = Math.floor(i / grid.userData.tw);
+            // add at method
+            ObjectGridWrapLand.addAt(grid, mesh, x, y, function(tile, mesh, tileUD){
+                // default to just the alt value of the tile
+                mesh.position.y = tileUD.alt;
+                // if tile is a cube
+                if(tileUD.isCube){
+                    mesh.position.y = tileUD.alt + 0.25;
+                }
+                // if tile is a cube
+                if(tileUD.isSlope){
+                    mesh.position.y = tileUD.alt - 0.25;
+                }
+                // if tile is a cube
+                if(tileUD.isCorner){
+                    mesh.position.y = tileUD.alt - 0.25;
+                }
+            });
+        }
+    });
     //******** **********
     // START LOOP
     //******** **********
