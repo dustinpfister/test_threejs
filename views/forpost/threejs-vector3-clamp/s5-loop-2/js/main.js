dcde301203d668db1c02484a6ebfa2315f03402d
dustinpfister <dustin.pfister@gmail.com>
2022-09-01 14:36:40 -0400

    loop2 example now uses wrap

--- views/forpost/threejs-vector3-clamp/s5-loop-2/js/main.js
+++ views/forpost/threejs-vector3-clamp/s5-loop-2/js/main.js
@@ -20,6 +20,34 @@
     //-------- ----------
     // HELPERS
     //-------- ----------
+    // mod method
+    var mod = function (a, b) {
+        return THREE.MathUtils.euclideanModulo(a, b);
+    };
+    // wrap and axis
+    var wrapAxis = function(vec, vecMin, vecMax, axis){
+        axis = axis || 'x';
+        var maxD = new THREE.Vector2(vecMin[axis], 0).distanceTo( new THREE.Vector2(vecMax[axis], 0) );
+        var d = new THREE.Vector2(vec[axis], 0).distanceTo( new THREE.Vector2(vecMin[axis], 0) );
+        if(maxD === 0){
+           vec[axis] = 0;
+        }else{
+            if(vec[axis] >= vecMax[axis]){
+                vec[axis] = vecMin[axis] + mod(d, maxD);
+            }
+            if(vec[axis] < vecMin[axis]){
+                vec[axis] = vecMax[axis] - mod(d, maxD);
+            }
+        }
+    };
+    // wrap a vector
+    var wrapVector = function (vec, vecMin, vecMax) {
+        vecMin = vecMin || new THREE.Vector3(0, 0, 0);
+        vecMax = vecMax || new THREE.Vector3(1, 1, 1);
+        wrapAxis(vec, vecMin, vecMax, 'x');
+        wrapAxis(vec, vecMin, vecMax, 'y');
+        wrapAxis(vec, vecMin, vecMax, 'z');
+    };
     // get a random axis
     var randAxis = function () {
         return (0.25 + 1.25 * Math.random()) * (Math.random() < 0.5 ? -1 : 1);
@@ -30,7 +58,7 @@
         color = color || 0xffffff;
         var group = new THREE.Group();
         var i = 0,
-        len = 50;
+        len = 10;
         while (i < len) {
             var mesh = new THREE.Mesh(
                 new THREE.BoxGeometry(1.0, 1.0, 1.0), 
@@ -79,6 +107,14 @@
                     ud.alpha = 0;
                 }
             }
+            // if clamp type is wrap
+            if(ud.clampType === 'wrap'){
+                wrapVector(
+                    pos,
+                    new THREE.Vector3(-2, -2, -2),
+                    new THREE.Vector3(2, 2, 2));
+                //ud.alpha = ud.alpha % 2;
+            }
         });
     };
     //-------- ----------
@@ -88,6 +124,8 @@
     scene.add(group1);
     var group2 = createGroup('length', 0x00ff00);
     scene.add(group2);
+    var group3 = createGroup('wrap', 0x00ffff);
+    scene.add(group3);
     var frame = 0,
     maxFrame = 300,
     fps = 20,
@@ -101,6 +139,7 @@
         if (secs > 1 / fps) {
             update(group1, 0.1);
             update(group2, 0.1);
+            update(group3, 0.1);
             renderer.render(scene, camera);
             frame += fps * secs;
             frame %= maxFrame;
