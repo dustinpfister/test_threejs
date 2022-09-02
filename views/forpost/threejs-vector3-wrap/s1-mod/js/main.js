280aa612060073d091b33182309bcccdf6e8c4a9
dustinpfister <dustin.pfister@gmail.com>
2022-09-02 08:02:03 -0400

    cleaner example

--- views/forpost/threejs-vector3-wrap/s1-mod/js/main.js
+++ views/forpost/threejs-vector3-wrap/s1-mod/js/main.js
@@ -1,7 +1,7 @@
-
 (function () {
-
+    //-------- ----------
     // SCENE, CAMERA, RENDERER
+    //-------- ----------
     var scene = new THREE.Scene();
     scene.add(new THREE.GridHelper(10, 10));
     var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
@@ -10,20 +10,19 @@
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
-
+    //-------- ----------
     // MESH
+    //-------- ----------
     var mesh1 = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshNormalMaterial());
     mesh1.position.set(0, 0, -3);
     scene.add(mesh1);
-
     var mesh2 = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshNormalMaterial());
     mesh2.position.set(0, 0, -2);
     scene.add(mesh2);
-
     var mesh3 = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshNormalMaterial());
@@ -34,8 +33,6 @@
             new THREE.MeshNormalMaterial());
     mesh4.position.set(0, 0, 3);
     scene.add(mesh4);
-
-
     //-------- ----------
     // LOOP
     //-------- ----------
@@ -50,18 +47,14 @@
         bias = 1 - Math.abs(0.5 - per) / 0.5;
         requestAnimationFrame(loop);
         if (secs > 1 / fps) {
-
             mesh1.position.x -= 5 * secs;
             mesh1.position.x %= 5;
             mesh2.position.x += 5 * secs;
             mesh2.position.x %= 5;
-
             mesh3.position.x -= 5 * secs;
             mesh3.position.x = THREE.MathUtils.euclideanModulo(mesh3.position.x, 5);
             mesh4.position.x += 5 * secs;
             mesh4.position.x = THREE.MathUtils.euclideanModulo(mesh4.position.x, 5);
-
-
             renderer.render(scene, camera);
             frame += fps * secs;
             frame %= maxFrame;
@@ -69,6 +62,5 @@
         }
     };
     loop();
-
 }
     ());
