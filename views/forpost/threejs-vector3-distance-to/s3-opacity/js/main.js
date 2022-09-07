f6e5d06e31ac7c9144dd2c07c7ae414049238655
dustinpfister <dustin.pfister@gmail.com>
2022-09-07 10:52:43 -0400

    create group method started

--- views/forpost/threejs-vector3-distance-to/s3-opacity/js/main.js
+++ views/forpost/threejs-vector3-distance-to/s3-opacity/js/main.js
@@ -1,88 +1,65 @@
-
 (function () {
+    //-------- ----------
+    // HELPER FUNCTIONS
+    //-------- ----------
 
-    // simple create cube helper
-    var createCube = function () {
-        var cube = new THREE.Mesh(
-                new THREE.BoxGeometry(1, 1, 1),
-                new THREE.MeshNormalMaterial());
-        return cube;
+    // get a start position by passing two values that are 0 - 1
+    let getStartPosition = (a, b) => {
+        a = a === undefined ? 0 : a;
+        b = b === undefined ? 0 : b;
+        let pos = new THREE.Vector3( 5, 0, 0);
+        let e = new THREE.Euler(0, a * Math.PI * 2, b * Math.PI * 2);
+        return pos.applyEuler(e);
+    };
+    // get a seeded random start position
+    let getSeededRandomStartPosition = function(){
+        return getStartPosition(
+            THREE.MathUtils.seededRandom(), 
+            THREE.MathUtils.seededRandom() );
     };
 
-    // move cube by difference and percent
-    var moveObjByDiff = function (obj, pos, per) {
-        per = per === undefined ? 1 : per;
-        per = per > 1 ? per % 1 : per;
-        var diff = obj.position.clone().sub(pos);
-        obj.position.sub(diff.multiplyScalar(per));
-    };
 
-    var moveObjByDistDiff = function (obj, pos, maxDist, maxPer) {
-        maxDist = maxDist === undefined ? 5 : maxDist;
-        maxPer = maxPer === undefined ? 0.25 : maxPer;
-        var d = obj.position.distanceTo(pos),
-        per = maxPer;
-        if (d <= maxDist) {
-            per = d / maxDist * maxPer;
+    let createGroup = () => {
+        let group = new THREE.Group();
+        let i = 0, count = 40;
+        while(i < count){
+            let mesh = new THREE.Mesh( new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial() );
+            mesh.position.copy( getSeededRandomStartPosition()  );
+            mesh.lookAt(0, 0, 0);
+            group.add(mesh);
+            i += 1;
         }
-        moveObjByDiff(obj, pos, per);
+        return group;
     };
-
-    var minDistCheck = function (obj, pos, minDist) {
-        minDist = minDist === undefined ? 0.125 : minDist;
-        var d = obj.position.distanceTo(pos);
-        if (d < minDist) {
-            return true;
-        }
-        return false;
-    };
-
-    var newRandomStartPos = function (maxLength) {
-        maxLength = maxLength === undefined ? 10 : maxLength;
-        return new THREE.Vector3().random().subScalar(0.5).normalize().multiplyScalar(maxLength);
-    };
-
-    // scene
-    var scene = new THREE.Scene();
-    scene.add(new THREE.GridHelper(7, 7));
-
-    // cubes
-    var cube1 = createCube();
-    cube1.position.set(0.001, 0, 0);
-    scene.add(cube1);
-    var cube2 = createCube();
-    cube2.position.copy(newRandomStartPos());
-    scene.add(cube2);
-
-    //moveObjByDiff(cube2, cube1.position, 1);
-
-    // camera, render
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    //-------- ----------
+    // SCENE, CAMERA, RENDERER
+    //-------- ----------
+    let scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(10, 10));
+    let camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
     camera.position.set(8, 10, 8);
     camera.lookAt(0, 0, 0);
-    var renderer = new THREE.WebGLRenderer();
+    let renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
+    //-------- ----------
+    // OBJECTS
+    //-------- ----------
 
-    var update = function () {
-        // move
-        moveObjByDistDiff(cube2, cube1.position, 2, 0.125);
-        // check distance
-        if (minDistCheck(cube2, cube1.position, 0.25)) {
-            // if below min dist set new pos
+    let group = createGroup();
+    scene.add(group);
 
-            cube2.position.copy(newRandomStartPos());
-        }
-    };
 
-    var lt = new Date(),
+    //-------- ----------
+    // LOOP
+    //-------- ----------
+    let lt = new Date(),
     fps = 30;
-    var loop = function () {
-        var now = new Date(),
+    let loop = function () {
+        let now = new Date(),
         secs = (now - lt) / 1000;
         requestAnimationFrame(loop);
         if (secs > 1 / fps) {
-            update();
             lt = now;
             renderer.render(scene, camera);
         }
