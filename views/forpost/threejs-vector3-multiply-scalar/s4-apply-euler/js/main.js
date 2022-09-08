35c9b9ec30dc57f60cee7f9a7f47551c25832763
dustinpfister <dustin.pfister@gmail.com>
2022-09-08 12:58:13 -0400

    app loop

--- views/forpost/threejs-vector3-multiply-scalar/s4-apply-euler/js/main.js
+++ views/forpost/threejs-vector3-multiply-scalar/s4-apply-euler/js/main.js
@@ -1,33 +1,51 @@
 (function () {
+    //-------- ----------
     // SCENE, CAMERA, RENDERER
-    var scene = new THREE.Scene();
-    scene.add(new THREE.GridHelper(9, 9));
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    //-------- ----------
+    const scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(10, 10));
+    const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
     camera.position.set(7, 7, 7);
     camera.lookAt(0, 0, 0);
-    var renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     document.getElementById('demo').appendChild(renderer.domElement);
+    //-------- ----------
+    // HELPERS
+    //-------- ----------
+    
 
-    // CREATING ANSD POSITIONING MESH OBJECTS WITH Vector3 METHODS
-    // including copy, add, normalize, and multiplyScalar
-    var cube1 = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshNormalMaterial());
+    //-------- ----------
+    // OBJECTS
+    //-------- ----------
+    const mesh1 = new THREE.Mesh( new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial());
+    scene.add(mesh1);
 
-    var radian = THREE.MathUtils.degToRad(90 + 25),
-    radius = 4;
-    var vec = new THREE.Vector3(Math.cos(radian) * radius, 0, Math.sin(radian) * radius);
-    [[0,0,0,0], [-2,1,0,1.5], [-4,2,0,3], [-8,3,0,4.5]].forEach(function(data){
-        var mesh = cube1.clone(),
-        x = data[0], y = data[1], z = data[2], scalar = data[3];
-        mesh.position.copy(vec).add(new THREE.Vector3(x, y, z) ).normalize().multiplyScalar(scalar);
-        mesh.lookAt(cube1.position);
-        scene.add(mesh);
-    });
-    scene.children[1].lookAt(scene.children[2].position)
- 
-    // render static scene
-    renderer.render(scene, camera);
+
+    //-------- ----------
+    // LOOP
+    //-------- ----------
+    var frame = 0,
+    maxFrame = 300,
+    fps = 20,
+    lt = new Date();
+    var loop = function () {
+        var now = new Date(),
+        secs = (now - lt) / 1000,
+        per = frame / maxFrame,
+        bias = 1 - Math.abs(0.5 - per) / 0.5;
+        requestAnimationFrame(loop);
+        if (secs > 1 / fps) {
+            renderer.render(scene, camera);
+            frame += fps * secs;
+            frame %= maxFrame;
+            lt = now;
+        }
+    };
+    loop();
+    //-------- ----------
+    // CONTROLS
+    //-------- ----------
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
 }
     ());
