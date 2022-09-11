b8bae1006bf89b9abf4fe1ddd7c6a9fd55c47c99
dustinpfister <dustin.pfister@gmail.com>
2022-09-11 13:35:22 -0400

    looks like this demo is working well

--- views/demos/r140/svg-loader-2-merge-geo/js/main.js
+++ views/demos/r140/svg-loader-2-merge-geo/js/main.js
@@ -5,7 +5,7 @@
     const scene = new THREE.Scene();
     scene.background = new THREE.Color('#000000');
     const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.1, 1000);
-    camera.position.set(200, 200, 200);
+    camera.position.set(5, 5, 5);
     camera.lookAt(0, 0, 0);
     scene.add(camera);
     const renderer = new THREE.WebGLRenderer();
@@ -14,6 +14,26 @@
     const dl = new THREE.DirectionalLight(0xffffff, 1);
     dl.position.set(2, 1, 3)
     scene.add(dl);
+    //-------- ----------
+    // HELPERS
+    //-------- ----------
+    // create a single mesh from 
+    const createGeoFromSVG = (data) => {
+        const paths = data.paths;
+        // geoArray in place of group
+        const geoArray = [];
+        for ( let i = 0; i < paths.length; i ++ ) {
+            const path = paths[ i ];
+            const shapes = THREE.SVGLoader.createShapes( path );
+            // for each shape create geometry and push to geoArray
+            for ( let j = 0; j < shapes.length; j ++ ) {
+                const shape = shapes[ j ];
+                geoArray.push(new THREE.ShapeGeometry( shape ) );
+            }
+        }
+        const geo = THREE.BufferGeometryUtils.mergeBufferGeometries(geoArray);
+        return geo;
+    };
     //-------- ----------
     // GRID
     //-------- ----------
@@ -54,27 +74,14 @@
         '/demos/r140/svg-loader-1/svg/fff.svg',
         // called when the resource is loaded
         function ( data ) {
-            const paths = data.paths;
-            //const group = new THREE.Group();
-            const geoArray = [];
-            for ( let i = 0; i < paths.length; i ++ ) {
-                const path = paths[ i ];
-                // create a shape
-                const shapes = THREE.SVGLoader.createShapes( path );
-                // for each shape create a mesh and add it to the group
-                for ( let j = 0; j < shapes.length; j ++ ) {
-                    const shape = shapes[ j ];
-                    geoArray.push(new THREE.ShapeGeometry( shape ) );
-                    //const mesh = new THREE.Mesh( geometry, material );
-                    //group.add( mesh );
-                }
-            }
-
-console.log(THREE.BufferGeometryUtils)
-
-            const geo = THREE.BufferGeometryUtils.mergeBufferGeometries(geoArray);
-           
-            //scene.add( group );
+            const geo = createGeoFromSVG(data);
+            geo.center();
+            geo.rotateX(Math.PI * 1);
+            geo.scale(0.025, 0.025, 0.025);
+            const mesh = new THREE.Mesh( geo, new THREE.MeshBasicMaterial({
+                side: THREE.DoubleSide
+            }));
+            scene.add(mesh);
         },
         // called when loading is in progresses
         function ( xhr ) {
