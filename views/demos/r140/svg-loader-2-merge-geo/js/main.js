2488148a1903d9b511b086d8e6011629ab0b7b1b
dustinpfister <dustin.pfister@gmail.com>
2022-09-11 13:22:39 -0400

    utils folder for r140

---
+++ views/demos/r140/svg-loader-2-merge-geo/js/main.js
@@ -0,0 +1,89 @@
+(function () {
+    //-------- ----------
+    // SCENE, CAMERA, RENDERER, LIGHT
+    //-------- ----------
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('#000000');
+    const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.1, 1000);
+    camera.position.set(200, 200, 200);
+    camera.lookAt(0, 0, 0);
+    scene.add(camera);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    const dl = new THREE.DirectionalLight(0xffffff, 1);
+    dl.position.set(2, 1, 3)
+    scene.add(dl);
+    //-------- ----------
+    // GRID
+    //-------- ----------
+    const grid = new THREE.GridHelper(10, 10, 0xffffff, 0xff0000);
+    grid.material.linewidth = 3;
+    grid.material.transparent = true;
+    grid.material.opacity = 0.25;;
+    scene.add(grid);
+    //-------- ----------
+    // CONTROL
+    //-------- ----------
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    //-------- ----------
+    // LOOP
+    //-------- ----------
+    let fps = 30,
+    lt = new Date();
+    const loop = function () {
+        let now = new Date(),
+        secs = (now - lt) / 1000;
+        requestAnimationFrame(loop);
+        if (secs > 1 / fps) {
+            // render
+            renderer.render(scene, camera);
+            lt = now;
+        }
+    };
+    loop();
+
+    //-------- ----------
+    // SVG LOADER
+    //-------- ----------
+    // instantiate a loader
+    const loader = new THREE.SVGLoader();
+    // load a SVG resource
+    loader.load(
+        // resource URL
+        '/demos/r140/svg-loader-1/svg/fff.svg',
+        // called when the resource is loaded
+        function ( data ) {
+            const paths = data.paths;
+            //const group = new THREE.Group();
+            const geoArray = [];
+            for ( let i = 0; i < paths.length; i ++ ) {
+                const path = paths[ i ];
+                // create a shape
+                const shapes = THREE.SVGLoader.createShapes( path );
+                // for each shape create a mesh and add it to the group
+                for ( let j = 0; j < shapes.length; j ++ ) {
+                    const shape = shapes[ j ];
+                    geoArray.push(new THREE.ShapeGeometry( shape ) );
+                    //const mesh = new THREE.Mesh( geometry, material );
+                    //group.add( mesh );
+                }
+            }
+
+console.log(THREE.BufferGeometryUtils)
+
+            const geo = THREE.BufferGeometryUtils.mergeBufferGeometries(geoArray);
+           
+            //scene.add( group );
+        },
+        // called when loading is in progresses
+        function ( xhr ) {
+            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+        },
+        // called when loading has errors
+        function ( error ) {
+            console.log( 'An error happened' );
+            console.log(error)
+        }
+    );
+}());
