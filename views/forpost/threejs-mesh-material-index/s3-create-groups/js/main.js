78a1b147481561bea38293cd0bfb03a188827345
dustinpfister <dustin.pfister@gmail.com>
2022-09-14 11:27:09 -0400

    create groups exmaple looks great

---
+++ views/forpost/threejs-mesh-material-index/s3-create-groups/js/main.js
@@ -0,0 +1,45 @@
+(function () {
+    //-------- ----------
+    // SCENE, CAMERA, RENDERER
+    //-------- ----------
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(4, 7, 4);
+    camera.lookAt(0, 0, 0);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    //-------- ----------
+    // GEOMETRY AND GROUPS
+    //-------- ----------
+    // new plane geometry
+    var geometry = new THREE.PlaneGeometry(5, 5, 2, 2);
+    geometry.rotateX(Math.PI * 1.5);
+    // adding groups
+    geometry.addGroup(0, 6, 0);  // give a start vertiex index, count of vertex and material index for each call
+    geometry.addGroup(6, 6, 1);
+    geometry.addGroup(12, 6, 1);
+    geometry.addGroup(18, 6, 0);
+    //-------- ----------
+    // MESH
+    //-------- ----------
+    var mesh = new THREE.Mesh(
+        // geometry as first argument
+        geometry,
+        // array of materials as the second argument
+        [
+            new THREE.MeshBasicMaterial({
+                color: 0xff0000
+            }),
+            new THREE.MeshBasicMaterial({
+                color: 0x00ff00
+            })
+        ]
+    );
+    scene.add(mesh);
+    //-------- ----------
+    // RENDER
+    //-------- ----------
+    renderer.render(scene, camera);
+}
+    ());
