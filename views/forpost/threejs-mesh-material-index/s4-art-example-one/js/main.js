a1589c79d34625c488d8416d5369b54b8ad8233a
dustinpfister <dustin.pfister@gmail.com>
2022-09-14 12:53:04 -0400

    data textures

--- views/forpost/threejs-mesh-material-index/s4-art-example-one/js/main.js
+++ views/forpost/threejs-mesh-material-index/s4-art-example-one/js/main.js
@@ -21,6 +21,130 @@
     pl.position.set(-3, 2, -3);
     scene.add(pl);
     //-------- ----------
+    // HELPERS
+    //-------- ----------
+    // create a plane geo with groups set up to work with images
+    const createPlaneGeo = () => {
+        const geometry = new THREE.PlaneGeometry(5, 5, 16, 16);
+        geometry.rotateX(Math.PI * 1.5);
+        let pxIndex = 0, len = 256;
+        while(pxIndex < len){
+            geometry.addGroup(pxIndex * 6, 6, 0);
+            pxIndex += 1;
+        }
+        return geometry;
+    };
+    // update plane geo to given imageindex and images array
+    const updatePlaneGeo = (geometry, images, imageIndex) => {
+        const img = images[imageIndex];
+        let pxIndex = 0, len = 256;
+        while(pxIndex < len){
+            const group = geometry.groups[pxIndex]
+            group.materialIndex = img[pxIndex];
+            pxIndex += 1;
+        }
+        return geometry;
+    };
+    const mkMaterial = (color, opacity, texture) => {
+        return new THREE.MeshPhongMaterial({
+            color: color,
+            map: texture || null,
+            side: THREE.DoubleSide,
+            transparent: true,
+            opacity: opacity
+        })
+    };
+    // data texture module
+    var datatex = (function () {
+        var api = {};
+        // mk data texture helper
+        api.mkDataTexture = function (data, w) {
+            data = data || [];
+            w = w || 0;
+            var width = w, //20,
+            height = data.length / 4 / w;
+            var texture = new THREE.DataTexture(data, width, height);
+            texture.needsUpdate = true;
+            return texture;
+        };
+        // create a data texture with a method that will be called for each pix
+        api.forEachPix = function (w, h, forEach) {
+            var width = w === undefined ? 5 : w,
+            height = h === undefined ? 5 : h;
+            var size = width * height;
+            var data = new Uint8Array(4 * size);
+            for (let i = 0; i < size; i++) {
+                var stride = i * 4;
+                var x = i % width;
+                var y = Math.floor(i / width);
+                var obj = forEach(x, y, w, h, i, stride, data);
+                obj = obj || {};
+                data[stride] = obj.r || 0;
+                data[stride + 1] = obj.g || 0;
+                data[stride + 2] = obj.b || 0;
+                data[stride + 3] = obj.a === undefined ? 255: obj.a;
+            }
+            return api.mkDataTexture(data, width)
+        };
+        // from px data method
+        api.fromPXDATA = function(pxData, width, palette){
+            palette = palette || [
+                [0,0,0,255],
+                [255,255,255,255]
+            ];
+            var height = Math.floor(pxData.length / width);
+            return api.forEachPix(width, height, function(x, y, w, h, i){
+                var obj = {};
+                var colorIndex = pxData[i];
+                var color = palette[colorIndex];
+                obj.r = color[0];
+                obj.g = color[1];
+                obj.b = color[2];
+                obj.a = color[3];
+                return obj;
+            });
+        };
+        // simple gray scale seeded random texture
+        api.seededRandom = function (w, h, rPer, gPer, bPer, range) {
+            w = w === undefined ? 5 : w,
+            h = h === undefined ? 5 : h;
+            rPer = rPer === undefined ? 1 : rPer;
+            gPer = gPer === undefined ? 1 : gPer;
+            bPer = bPer === undefined ? 1 : bPer;
+            range = range || [0, 255]
+            var size = w * h;
+            var data = new Uint8Array(4 * size);
+            for (let i = 0; i < size; i++) {
+                var stride = i * 4;
+                var v = Math.floor(range[0] + THREE.MathUtils.seededRandom() * (range[1] - range[0]));
+                data[stride] = v * rPer;
+                data[stride + 1] = v * gPer;
+                data[stride + 2] = v * bPer;
+                data[stride + 3] = 255;
+            }
+            return api.mkDataTexture(data, w);
+        };
+        // return the api
+        return api;
+    }
+    ());
+    // create plane mesh to use with images
+    const createPlaneMesh = (materials) => {
+        // new plane geometry
+        var geometry = createPlaneGeo(images, 1);
+        var mesh = new THREE.Mesh(
+            // geometry as first argument
+            geometry,
+            // array of materials as the second argument
+            materials || [
+                mkMaterial(0xffffff, 1, null),
+                mkMaterial(0x000000, 1, null),
+                mkMaterial(0x888888, 1, null)
+            ]
+        );
+        return mesh;
+    };
+    //-------- ----------
     // PLANE MATERIAL INDEX DATA
     //-------- ----------
     const images = [
@@ -60,59 +184,13 @@
             0,0,0,0,0,2,0,0,0,0,2,0,0,0,0,0,
             0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
         ]
-    ]
+    ];
     //-------- ----------
-    // HELPERS
+    // TEXTURES
     //-------- ----------
-    // create a plane geo with groups set up to work with images
-    const createPlaneGeo = () => {
-        const geometry = new THREE.PlaneGeometry(5, 5, 16, 16);
-        geometry.rotateX(Math.PI * 1.5);
-        let pxIndex = 0, len = 256;
-        while(pxIndex < len){
-            geometry.addGroup(pxIndex * 6, 6, 0);
-            pxIndex += 1;
-        }
-        return geometry;
-    };
-    // update plane geo to given imageindex and images array
-    const updatePlaneGeo = (geometry, images, imageIndex) => {
-        const img = images[imageIndex];
-        let pxIndex = 0, len = 256;
-        while(pxIndex < len){
-            const group = geometry.groups[pxIndex]
-            group.materialIndex = img[pxIndex];
-            pxIndex += 1;
-        }
-        return geometry;
-    };
-    const mkMaterial = (color, opacity, texture) => {
-        return new THREE.MeshPhongMaterial({
-            color: color,
-            map: texture || null,
-            side: THREE.DoubleSide,
-            transparent: true,
-            opacity: opacity
-        })
-    };
-    // create plane mesh to use with images
-    const createPlaneMesh = () => {
-        // new plane geometry
-        var geometry = createPlaneGeo(images, 1);
-        var mesh = new THREE.Mesh(
-            // geometry as first argument
-            geometry,
-            // array of materials as the second argument
-            [
-                mkMaterial(0xffffff, 1, null),
-                mkMaterial(0x000000, 1, null),
-                mkMaterial(0x888888, 1, null)
-            ]
-        );
-        return mesh;
-    };
+    const texture_rnd1 = datatex.seededRandom(32, 32, 1, 1, 1, [128, 255]);
     //-------- ----------
-    // MESH
+    // MESH OBJECTS
     //-------- ----------
     var mesh = new THREE.Mesh(
         // geometry as first argument
@@ -130,7 +208,11 @@
     mesh.position.set(0, 1.5, 0);
     scene.add(mesh);
     // plane
-    const plane = createPlaneMesh();
+    const plane = createPlaneMesh([
+       mkMaterial(0xffffff, 1, texture_rnd1),
+       mkMaterial(0x222222, 1, texture_rnd1),
+       mkMaterial(0x888888, 1, texture_rnd1)
+    ]);
     updatePlaneGeo(plane.geometry, images, 0);
     scene.add(plane);
     // sphere
