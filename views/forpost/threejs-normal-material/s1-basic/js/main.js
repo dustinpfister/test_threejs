2a347d6cf6a625fd2f780d6f7a272fef468b5b79
dustinpfister <dustin.pfister@gmail.com>
2022-09-15 09:51:54 -0400

    cleaned up basic example

--- views/forpost/threejs-normal-material/s1-basic/js/main.js
+++ views/forpost/threejs-normal-material/s1-basic/js/main.js
@@ -1,18 +1,22 @@
-// scene
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
 var scene = new THREE.Scene();
 scene.add( new THREE.GridHelper(10, 10) );
- 
-// BOX MESH USING THE NORMAL MATERIAL
-var box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshNormalMaterial());
-scene.add(box);
- 
-// camera and renderer
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
 camera.position.set(2, 3, 2);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
+(document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// BOX MESH USING THE NORMAL MATERIAL
+//-------- ----------
+var box = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshNormalMaterial());
+scene.add(box);
+//-------- ----------
+// RENDER
+//-------- ----------
 renderer.render(scene, camera);
