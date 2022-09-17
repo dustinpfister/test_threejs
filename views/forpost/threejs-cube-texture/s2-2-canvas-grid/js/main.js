87b8ab52be8cb72a4ffb5dd2098513c10d42c2c7
dustinpfister <dustin.pfister@gmail.com>
2022-09-17 09:15:06 -0400

    canvas texture exmaple looks okay

--- views/forpost/threejs-cube-texture/s2-2-canvas-grid/js/main.js
+++ views/forpost/threejs-cube-texture/s2-2-canvas-grid/js/main.js
@@ -14,33 +14,43 @@
     //-------- ----------
     // CREATING A CUBE TEXTURE WITH CANVAS
     //-------- ----------
-    const grid = {
-        w: 4,
+    const grid1 = {
+        w: 8,
         pxData: [
-            0,0,0,0,
-            0,1,1,0,
-            0,1,1,0,
-            0,0,0,0,
+            2,0,0,0,0,0,0,2,
+            0,2,0,0,0,0,2,0,
+            0,0,1,1,1,1,0,0,
+            0,0,1,2,2,1,0,0,
+            0,0,1,2,2,1,0,0,
+            0,0,1,1,1,1,0,0,
+            0,2,0,0,0,0,2,0,
+            2,0,0,0,0,0,0,2
         ],
-        pal: [ [1,0,0], [0,1,0] ]
+        pal: [ [1,1,1], [0,0,0], [0.5,0.5,0.5] ]
+    };
+ 
+    const getTextureFromGrid = (grid, canvasSize) => {
+        canvasSize = canvasSize === undefined ? 64 : canvasSize;
+        return canvasTextureMod.createCanvasTexture((ctx, canvas) => {
+            ctx.fillStyle='white';
+            ctx.fillRect(0,0,canvas.width, canvas.height);
+            let i = 0, len = grid.pxData.length;
+            while(i < len){
+                let pX = i % grid.w;
+                let pY = Math.floor(i / grid.w);
+                let c = grid.pal[ grid.pxData[i] ];
+                let color = new THREE.Color(c[0], c[1], c[2]);
+                ctx.fillStyle = color.getStyle();
+                let pxW = canvas.width / grid.w;
+                let pxH = canvas.height / grid.w;
+                ctx.fillRect(pX * pxW, pY * pxH, pxW, pxH);
+                i += 1;
+            }
+        }, canvasSize);
     };
 
-    const texture = canvasTextureMod.createCanvasTexture((ctx, canvas) => {
-        ctx.fillStyle='white';
-        ctx.fillRect(0,0,canvas.width, canvas.height);
-        let i = 0, len = grid.pxData.length;
-        while(i < len){
-            let pX = i % grid.w;
-            let pY = Math.floor(i / grid.w);
-            let c = grid.pal[ grid.pxData[i] ];
-            let color = new THREE.Color(c[0], c[1], c[2]);
-            ctx.fillStyle = color.getStyle();
-            let pxW = canvas.width / grid.w;
-            let pxH = canvas.height / grid.w;
-            ctx.fillRect(pX * pxW, pY * pxH, pxW, pxH);
-            i += 1;
-        }
-    }, 128)
+    const texture =  getTextureFromGrid(grid1, 256);
+
     // same texture for all sides
     cubeTexture = new THREE.CubeTexture(new Array(6).fill(texture.image));
     cubeTexture.needsUpdate = true;
