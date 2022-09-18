c51dabdf3ab90fe4ad84c66bba8173c59975bc30
dustinpfister <dustin.pfister@gmail.com>
2022-09-18 14:19:57 -0400

    cleaned up basic bg example

--- views/forpost/threejs-scene/s3-1-bg-basic/js/main.js
+++ views/forpost/threejs-scene/s3-1-bg-basic/js/main.js
@@ -1,14 +1,17 @@
-// CREATE A SCENE
-var scene = new THREE.Scene();
-// background
-scene.background = new THREE.Color(0xffffff);
-var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-camera.position.set(2, 1, 2); 
+//-------- ----------
+// SCENE OBJECT - SETTING SOLID BACKGROUND COLOR
+//-------- ----------
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0, 0.4, 0.6);
+//-------- ----------
+// CAMERA, RENDERER, MESH
+//-------- ----------
+const camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
+camera.position.set(2, 2, 2); 
 camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
-var container = document.getElementById('demo') || document.body;
-container.appendChild(renderer.domElement);
-var mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshNormalMaterial());
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+const mesh = new THREE.Mesh(new THREE.SphereGeometry(0.5, 30, 30), new THREE.MeshNormalMaterial());
 scene.add(mesh);
 renderer.render(scene, camera);
