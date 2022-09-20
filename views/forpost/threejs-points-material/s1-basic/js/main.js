516e0c2f149df754239834abed3138db8dc489c4
dustinpfister <dustin.pfister@gmail.com>
2022-09-20 09:16:22 -0400

    updared the basic example

--- views/forpost/threejs-points-material/s1-basic/js/main.js
+++ views/forpost/threejs-points-material/s1-basic/js/main.js
@@ -1,25 +1,29 @@
 
 (function () {
-    // scene
-    var scene = new THREE.Scene();
-    // geometry
-    var geometry = new THREE.SphereGeometry(1, 10, 60);
-    // THREE.Points INSTANCE UISNG THREE.PointsMaterial
-    var pt = new THREE.Points(
+    //-------- ----------
+    // SCENE, CAMERA, RENDERER
+    //-------- ----------
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000); // camera
+    camera.position.set(2, 2, 2);
+    camera.lookAt(0, 0, 0);
+    const renderer = new THREE.WebGLRenderer(); // render
+    renderer.setSize(640, 480);
+    (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+    //-------- ----------
+    // POINTS
+    //-------- ----------
+    const geometry = new THREE.SphereGeometry(1, 10, 60);
+    const pt = new THREE.Points(
             geometry,
             new THREE.PointsMaterial({
                 color: 0x00afaf,
-                size: 0.025
+                size: 0.05
             }));
     scene.add(pt);
-    // camera and renderer
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000); // camera
-    camera.position.set(2, 2, 2);
-    camera.lookAt(0, 0, 0);
-    var renderer = new THREE.WebGLRenderer(); // render
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
+    //-------- ----------
+    // RENDER
+    //-------- ----------
     renderer.render(scene, camera);
-
 }
     ());
