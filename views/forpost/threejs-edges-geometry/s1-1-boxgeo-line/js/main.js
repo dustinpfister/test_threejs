76779cae0b13763de4d91dfee451b19c90622449
dustinpfister <dustin.pfister@gmail.com>
2022-09-21 12:06:49 -0400

    many basic examples

---
+++ views/forpost/threejs-edges-geometry/s1-1-boxgeo-line/js/main.js
@@ -0,0 +1,27 @@
+(function () {
+    //-------- ----------
+    // SCENE, CAMERA, RENDERER
+    //-------- ----------
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('blue');
+    const camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
+    camera.position.set(1.25, 1.75, 1.25);
+    camera.lookAt(0, 0, 0);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+    //-------- ----------
+    // EDGE GEOMETRY CREATED FROM BOX GEOMETRY
+    //-------- ----------
+    const boxGeo = new THREE.BoxGeometry(1, 1, 1),
+    line = new THREE.Line(
+            boxGeo,
+            new THREE.LineBasicMaterial({
+                color: new THREE.Color('white')
+            }));
+    scene.add(line);
+    //-------- ----------
+    // SCENE
+    //-------- ----------
+    renderer.render(scene, camera);
+}());
--- views/forpost/threejs-edges-geometry/todo.md
+++ views/forpost/threejs-edges-geometry/todo.md
@@ -1,6 +1,9 @@
 # threejs-edges-geometry tod list
 
-## () -
+## () - break down basic section
+* () rename s1-basic to s1-3-line-segments
+* () new s1-2-line example
+* () new s1-1-box
 
 ## ( done 09/21/2022 ) - started for post folder
 * (done) started for post folder based on r127 demos
