9a7b5ddc95e624f95177602e7bca04d0d76319f8
dustinpfister <dustin.pfister@gmail.com>
2022-09-22 15:06:56 -0400

    started a threejs-vector3-apply-euler for post folder

---
+++ views/forpost/threejs-vector3-apply-euler/s1-basic/js/main.js
@@ -0,0 +1,37 @@
+
+(function () {
+
+    // simple create cube helper
+    var createCube = function(){
+        var cube = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshNormalMaterial());
+        return cube;
+    };
+
+    // scene
+    var scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(9, 9));
+
+    // creating a cube and adding it to the scene
+    var cube = createCube();
+    scene.add(cube);
+    // USING THE APPLY EULER Vector3 METHOD
+    var e = new THREE.Euler(
+        THREE.MathUtils.degToRad(0),
+        THREE.MathUtils.degToRad(45), 
+        THREE.MathUtils.degToRad(0));
+    var v = new THREE.Vector3(2, 0, 0).applyEuler(e);
+    cube.position.copy(v);
+
+    // camera, render
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(5, 5, 5);
+    camera.lookAt(0, 0, 0);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+ 
+}
+    ());
---
+++ views/forpost/threejs-vector3-apply-euler/s2-loop/js/main.js
@@ -0,0 +1,63 @@
+
+(function () {
+
+    // simple create cube helper
+    var createCube = function () {
+        var cube = new THREE.Mesh(
+                new THREE.BoxGeometry(1, 1, 1),
+                new THREE.MeshNormalMaterial());
+        return cube;
+    };
+
+    // vector fro angles helper
+    var vectorFromAngles = function (a, b, c, len, startVec) {
+        len = len === undefined ? 1 : len;
+        startVec = startVec === undefined ? new THREE.Vector3(1, 0, 0) : startVec;
+        var e = new THREE.Euler(
+                THREE.MathUtils.degToRad(a),
+                THREE.MathUtils.degToRad(b),
+                THREE.MathUtils.degToRad(c));
+        var v = startVec.applyEuler(e).normalize();
+        return v.multiplyScalar(len);
+    };
+
+    // scene
+    var scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(9, 9));
+
+    var cube = createCube();
+    scene.add(cube);
+    // USING MY VECTOR FROM ANGLES METHOD
+    var v = vectorFromAngles(90, 0, 0, 1);
+    cube.position.copy(v);
+
+    // camera, render
+    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+    camera.position.set(5, 5, 5);
+    camera.lookAt(0, 0, 0);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    document.getElementById('demo').appendChild(renderer.domElement);
+
+    // loop
+    var lt = new Date(),
+    a = 0,
+    b = 0,
+    c = 0,
+    fps = 30;
+    var loop = function () {
+        var now = new Date(),
+        secs = (now - lt) / 1000;
+        requestAnimationFrame(loop);
+        if (secs > 1 / fps) {
+            b += 90 * secs;
+            b %= 360;
+            var v = vectorFromAngles(a, b, c, 1.5);
+            cube.position.copy(v);
+            lt = now;
+            renderer.render(scene, camera);
+        }
+    };
+    loop();
+}
+    ());
