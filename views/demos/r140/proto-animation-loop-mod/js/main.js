b331bf21050be17d7432d9df48dcbce49c49647f
dustinpfister <dustin.pfister@gmail.com>
2022-09-27 15:46:27 -0400

    loop mod proto looks good so far

--- views/demos/r140/proto-animation-loop-mod/js/main.js
+++ views/demos/r140/proto-animation-loop-mod/js/main.js
@@ -1,47 +1,70 @@
-//-------- ----------
-// SCENE, CAMERA, RENDERER
-//-------- ----------
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(50, 640 / 480, 0.1, 1000);
-camera.position.set(2, 2, 2);
-camera.lookAt(0, 0, 0);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
 // ---------- ----------
-// MESH
+// ANIMATION LOOP MODULE - r0 - prototype
+// ---------- ----------
+const loopMod = (function(){
+    //-------- ----------
+    // HELPERS
+    //-------- ----------
+    // create loop helper
+    const createLoopFunction = (opt) => {
+        opt = opt || {};
+        const loopObj = {
+            frame: 0,
+            lt: new Date(),
+            FRAME_MAX: opt.FRAME_MAX || 90,
+            fps_update: opt.fps_update || 12,
+            fps_movement: opt.fps_movement || 30,
+            update: opt.update || function(){},
+            scene: opt.scene || new THREE.Scene(),
+            camera: opt.camera || new THREE.PerspectiveCamera(50, 640 / 480, 0.1, 1000),
+            renderer: new THREE.WebGLRenderer()
+        };
+        loopObj.loop = function(){
+            const now = new Date(),
+            secs = (now - loopObj.lt) / 1000;
+            requestAnimationFrame(loopObj.loop);
+            if(secs > 1 / loopObj.fps_update){
+                // update, render
+                loopObj.update( Math.floor(loopObj.frame), loopObj.FRAME_MAX);
+                loopObj.renderer.render(loopObj.scene, loopObj.camera);
+                // step frame
+                loopObj.frame += loopObj.fps_movement * secs;
+                loopObj.frame %= loopObj.FRAME_MAX;
+                loopObj.lt = now;
+            }
+        };
+        return loopObj;
+    };
+    //-------- ----------
+    // PUBLIC API
+    //-------- ----------
+    const api = {};
+    // create loop function
+    api.create = (opt) => {
+        opt = opt || {};
+        const loopObj = createLoopFunction(opt);
+        loopObj.camera.position.set(2, 2, 2);
+        loopObj.camera.lookAt(0, 0, 0);
+        loopObj.renderer.setSize(640, 480);
+        return loopObj;
+    };
+    // return public api
+    return api;
+}());
+
+
+// ---------- ----------
+// DEMO
 // ---------- ----------
 var mesh = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
-scene.add(mesh);
-// ---------- ----------
-// ANIMATION LOOP
-// ---------- ----------
-const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
-FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
-FRAME_MAX = 120;
-let secs = 0,
-frame = 0,
-lt = new Date();
-// update
-const update = function(frame, frameMax){
-    const degree = 360 * (frame / frameMax);
-    mesh.rotation.x = THREE.MathUtils.degToRad(degree);
-};
-// loop
-const loop = () => {
-    const now = new Date(),
-    secs = (now - lt) / 1000;
-    requestAnimationFrame(loop);
-    if(secs > 1 / FPS_UPDATE){
-        // update, render
-        update( Math.floor(frame), FRAME_MAX);
-        renderer.render(scene, camera);
-        // step frame
-        frame += FPS_MOVEMENT * secs;
-        frame %= FRAME_MAX;
-        lt = now;
+const loopObj = loopMod.create({
+    update: function(frame, frameMax){
+        const degree = 360 * (frame / frameMax);
+        mesh.rotation.x = THREE.MathUtils.degToRad(degree);
     }
-};
-loop();
+});
+(document.getElementById('demo') || document.body).appendChild(loopObj.renderer.domElement);
+loopObj.scene.add(mesh);
+loopObj.loop();
