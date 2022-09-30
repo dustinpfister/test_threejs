4c4e788b5c6c9c2cf6381f6d1d924c146bc5e8a3
dustinpfister <dustin.pfister@gmail.com>
2022-09-30 15:08:16 -0400

    proto-iframe

---
+++ views/demos/r140/proto-iframe/README.md
@@ -0,0 +1,3 @@
+# proto-iframe
+
+I would like to see if I can work out a way to have more than one version of threejs on the same page by making use of iframes.
---
+++ views/demos/r140/proto-iframe/js/main.js
@@ -0,0 +1,13 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(60, 640 / 240, 0.1, 1000);
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 240);
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+// revision used
+console.log('r' + THREE.REVISION)
+// add something to scene
+scene.add(new THREE.Mesh( new THREE.BoxGeometry(1, 1, 1), new THREE.MeshNormalMaterial() ))
+// render
+renderer.render(scene, camera);
