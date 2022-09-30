b4c43b0960beb523ab020f34bfe077adcfd7476a
dustinpfister <dustin.pfister@gmail.com>
2022-09-30 11:26:45 -0400

    parking data in userData object of scene

--- views/forpost/threejs-examples-animation-loop-module/r0-2-random/js/main.js
+++ views/forpost/threejs-examples-animation-loop-module/r0-2-random/js/main.js
@@ -6,6 +6,11 @@ const loopObj = loopMod.create({
     // this is called once the loopObj is ready but has not been 
     // started yet for first time
     init: function(loopObj, scene, camera, renderer){
+        const sud = scene.userData;
+        sud.vMin = new THREE.Vector3(-5, -5, -5);
+        sud.vMax = new THREE.Vector3(5, 5, 5);
+        sud.vCenter = new THREE.Vector3();
+        sud.dMax = 5;
         // SETUP MESH GROUP
         const group = scene.userData.group = new THREE.Group();
         const len = 60;
@@ -13,36 +18,50 @@ const loopObj = loopMod.create({
         while(i < len){
             const mesh = new THREE.Mesh(
                 new THREE.BoxGeometry(0.5, 0.5, 0.5),
-                new THREE.MeshNormalMaterial());
-            const ud = mesh.userData;
-            ud.dir = new THREE.Vector3(
+                new THREE.MeshNormalMaterial({transparent: true}));
+            const mud = mesh.userData;
+            // setting a normalize direction
+            mud.dir = new THREE.Vector3(
                -3 + 6 * Math.random() ,
-               1 + 3 * Math.random() * (Math.random() > 0.5 ? 1 : -1),
+               1 + 9 * Math.random() * (Math.random() > 0.5 ? 1 : -1),
                -3 + 6 * Math.random() );
+            mud.dir.normalize();
+            // setting a unit length to use to create a delta vector3
+            mud.unit_length = 0.25 + 2.75 * Math.random();
             group.add(mesh);
             i += 1;
         };
         scene.add(group);
         ( document.getElementById('demo') || document.body ).appendChild( loopObj.container );
-        scene.add( new THREE.GridHelper() );
+        // update camera and size
         camera.aspect = 640 / 200;
         camera.updateProjectionMatrix();
         loopObj.setSize(640, 200);
-    },
-    // what needs to happen each time the loop starts
-    onStart: function(loopObj, scene, camera, renderer){
         camera.position.set(2, 2, 2);
         camera.lookAt(0, 0, 0);
     },
     // update method
     update: function(loop, scene, camera){
         const group = scene.userData.group;
+        const sud = scene.userData;
+        // update children
         group.children.forEach( (mesh, i) => {
              const ud = mesh.userData;
-             mesh.position.x += ud.dir.x * loop.secs;
-             mesh.position.y += ud.dir.y * loop.secs;
-             mesh.position.z += ud.dir.z * loop.secs;
-             loopMod.wrapVector(mesh.position, new THREE.Vector3(-5, -5, -5),new THREE.Vector3(5, 5, 5))
+             // creating a delta vector that will be used to step position
+             // based on current value of ud.dir, and ud.unit_length
+             const delta = ud.dir.clone().multiplyScalar(ud.unit_length * loop.secs);
+             mesh.position.x += delta.x;
+             mesh.position.y += delta.y;
+             mesh.position.z += delta.z;
+             // USING THE WRAP VECTOR METHOD HERE
+             loopMod.wrapVector(mesh.position, sud.vMin, sud.vMax);
+             // setting opacity
+             const d = mesh.position.distanceTo( sud.vCenter );
+
+             let dAlpha = d / sud.dMax;
+             dAlpha = THREE.MathUtils.clamp(dAlpha, 0, 1);
+             mesh.material.opacity = 0.5 - 0.5 * dAlpha;
+             // mesh objects look at 0, 0, 0;
              mesh.lookAt(0, 0, 0);
         });
     }
