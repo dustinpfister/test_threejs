7bfa7844e0f3bb4c5c778a6d000d7955172e5698
dustinpfister <dustin.pfister@gmail.com>
2022-10-03 14:45:52 -0400

    combined examples into a basic section

--- views/forpost/threejs-canvas-texture/s1-2-basic-helperfunctions/js/main.js
+++ views/forpost/threejs-canvas-texture/s1-2-basic-helperfunctions/js/main.js
@@ -1,15 +1,28 @@
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+scene.add( new THREE.GridHelper(10, 10) );
+const camera = new THREE.PerspectiveCamera(75, 320 / 240, .025, 20);
+camera.position.set(1, 1, 1);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+(document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// HELPERS
+//-------- ----------
 // create and return a canvas texture
-var createCanvasTexture = function (draw) {
-    var canvas = document.createElement('canvas'),
+const createCanvasTexture = function (draw) {
+    const canvas = document.createElement('canvas'),
     ctx = canvas.getContext('2d');
     canvas.width = 32;
     canvas.height = 32;
     draw(ctx, canvas);
-    var texture = new THREE.CanvasTexture(canvas);
-    return texture;
+    return new THREE.CanvasTexture(canvas);
 };
 // create a cube the makes use of a canvas texture
-var createCanvasCube = function (draw) {
+const createCanvasCube = function (draw) {
     return new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshBasicMaterial({
@@ -17,16 +30,6 @@ var createCanvasCube = function (draw) {
         })
     );
 };
-
-// Scene
-var scene = new THREE.Scene();
-scene.add( new THREE.GridHelper(10, 10) );
-
-// Camera
-var camera = new THREE.PerspectiveCamera(75, 320 / 240, .025, 20);
-camera.position.set(1, 1, 1);
-camera.lookAt(0, 0, 0);
-
 // add cube to scene that makes use
 // of the canvas texture
 scene.add(createCanvasCube(function(ctx, canvas){
@@ -36,9 +39,7 @@ scene.add(createCanvasCube(function(ctx, canvas){
     ctx.strokeStyle = '#af0000';
     ctx.strokeRect(2.5, 2.5, canvas.width - 4, canvas.height - 4);
 }));
-
+//-------- ----------
 // RENDER
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
+//-------- ----------
 renderer.render(scene, camera);
