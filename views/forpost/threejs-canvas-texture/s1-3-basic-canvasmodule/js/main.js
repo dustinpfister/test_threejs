369f5e467a2b51ef9701d60fcae566a060d455db
dustinpfister <dustin.pfister@gmail.com>
2022-10-03 14:49:50 -0400

    doc body default and const

--- views/forpost/threejs-canvas-texture/s1-3-basic-canvasmodule/js/main.js
+++ views/forpost/threejs-canvas-texture/s1-3-basic-canvasmodule/js/main.js
@@ -1,41 +1,43 @@
-// Scene
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
 var scene = new THREE.Scene();
 scene.add( new THREE.GridHelper(10, 10) );
- 
-// Camera
-var camera = new THREE.PerspectiveCamera(75, 320 / 240, .025, 20);
+const camera = new THREE.PerspectiveCamera(75, 320 / 240, .025, 20);
 camera.position.set(2, 2, 2);
 camera.lookAt(0, 0, 1);
- 
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+(document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// CANVAS
+//-------- ----------
 // create texture with default draw method
-var canvasObj = canvasMod.createCanvasTexture();
+const canvasObj = canvasMod.createCanvasTexture();
 // create cube with the texture
-var cube = canvasMod.createCube(canvasObj.texture);
+const cube = canvasMod.createCube(canvasObj.texture);
 scene.add(cube);
-
 // create texture with custom draw method that makes use of a state object
-var draw = function (ctx, canvas, state) {
+const draw = function (ctx, canvas, state) {
     ctx.fillStyle = 'red';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'black';
     ctx.beginPath();
-    var hw = canvas.width / 2,
+    const hw = canvas.width / 2,
     sx = hw,
     sy = canvas.height / 2,
     radius = hw - hw * state.rPer;
     ctx.arc(sx, sy, radius, 0, Math.PI * 2);
     ctx.fill();
 };
-var state = {
+const state = {
    rPer: 0.1
 };
-var canvasObj = canvasMod.createCanvasTexture(state, draw);
+const canvasObj = canvasMod.createCanvasTexture(state, draw);
 cube = canvasMod.createCube(canvasObj.texture);
 cube.position.set(0, 0, 2)
 scene.add(cube);
-
+//-------- ----------
 // RENDER
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
+//-------- ----------
 renderer.render(scene, camera);
