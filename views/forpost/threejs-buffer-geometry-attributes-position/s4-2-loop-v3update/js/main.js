29f28cef72cbbf66e294b795852defed08d1ae91
dustinpfister <dustin.pfister@gmail.com>
2022-10-03 10:48:14 -0400

    starting an update geo function

--- views/forpost/threejs-buffer-geometry-attributes-position/s4-2-loop-v3update/js/main.js
+++ views/forpost/threejs-buffer-geometry-attributes-position/s4-2-loop-v3update/js/main.js
@@ -1,74 +1,91 @@
 (function () {
     //-------- ----------
-    // HELPERS
-    //-------- ----------
-    // set location of a vert given an index value in geometry.index
-    const setVert = function(geometry, vertIndex, pos){
-        pos = pos || {};
-        const posIndex = geometry.index.array[vertIndex] * 3,
-        position = geometry.getAttribute('position');
-        position.array[posIndex] = pos.x === undefined ? position.array[posIndex]: pos.x;
-        position.array[posIndex + 1] = pos.y === undefined ? position.array[posIndex + 1]: pos.y;
-        position.array[posIndex + 2] = pos.z === undefined ? position.array[posIndex + 2]: pos.z;
-    };
-    // set pos for tri index
-    const setTri = function(geometry, triIndex, pos){
-        pos = pos || {};
-        const vertIndex = triIndex * 3;
-        setVert(geometry, vertIndex, pos);
-        setVert(geometry, vertIndex + 1, pos);
-        setVert(geometry, vertIndex + 2, pos);
-    };
-    // update method for a box geo
-    const updateBoxGeo = function(geometry, per){
-        const bias = 1 - Math.abs(per - 0.5) / 0.5,
-        size = 0.5 + 1 * bias,
-        position = geometry.getAttribute('position'),
-        triCount = geometry.getIndex().count / 3;
-        let i = 0, pos, axis;
-        while(i < triCount){
-            axis = ['x', 'y', 'z'][Math.floor(i / 4)];
-            pos = {};
-            pos[axis] = size * ( i % 4 < 2 ? 1: -1);
-            setTri(geometry, i, pos);
-            i += 1;
-        }
-        // MUST SET THE needsUpdate prop of position to true
-        position.needsUpdate = true;
-    };
-    //-------- ----------
     // SCENE, CAMERA, RENDERER
     //-------- ----------
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
-    camera.position.set(3, 3, 3);
+    camera.position.set(5, 5, 5);
+    camera.lookAt(0, 0, 0);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
     //-------- ----------
-    // GEOMETRY, MESH
+    // HELPERS
     //-------- ----------
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({
-        side: THREE.DoubleSide
-    }));
-    scene.add(mesh);
-    camera.lookAt(mesh.position);
+    // Buffer Geometry from v3Array
+    const Vector3ArrayToGeometry = (v3Array) => {
+        return new THREE.BufferGeometry().setFromPoints(v3Array);
+    };
+    // Vector3 array from geometry
+    const Vector3ArrayFromGeometry = (geometry) => {
+        const pos = geometry.getAttribute('position');
+        let i = 0;
+        const len = pos.count, v3Array = [];
+        while(i < len){
+            const v = new THREE.Vector3(pos.getX(i), pos.getY(i), pos.getZ(i))
+            v3Array.push(v);
+            i += 1;
+        }
+        return v3Array;
+    };
+    // Vector3 Array to Typed Array
+    const Vector3ArrayToTyped = (v3Array) => {
+        let i = 0, len = v3Array.length, vertArray = [];
+        while(i < len){
+            let v = v3Array[i];
+            vertArray.push(v.x, v.y, v.z);
+            i += 1;
+        }
+        return vertArray;
+        //return new THREE.Float32BufferAttribute(vertArray, 3)
+    };
     //-------- ----------
-    // LOOP
+    // GEO AND POINTS
     //-------- ----------
-    const maxFrames = 300,
-    FPS = 30;
-    let lt = new Date(), per = 0;
-    const loop = function(){
+    let geo_sphere = new THREE.SphereGeometry(1.5, 30, 30);
+    let geo_torus = new THREE.TorusGeometry(1, 0.5, 30, 30);
+
+
+    let points = new THREE.Points( geo_sphere.clone(), new THREE.PointsMaterial({ size: 0.1}) );
+    scene.add(points);
+
+    const updateGeo = (geo, geoa, geob) => {
+        let v3array = Vector3ArrayFromGeometry(geo_torus);
+        let typed = Vector3ArrayToTyped(v3array);
+        let pos = geo_sphere.getAttribute('position');
+        let alpha = 1;
+        pos.array = pos.array.map( (n, i) => {
+            let d  = typed[i] === undefined ? 0: typed[i];
+            return n + d * alpha;
+        });
+        pos.needsUpdate = true;
+    };
+
+    // ---------- ----------
+    // ANIMATION LOOP
+    // ---------- ----------
+    const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+    FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+    FRAME_MAX = 120;
+    let secs = 0,
+    frame = 0,
+    lt = new Date();
+    // update
+    const update = function(frame, frameMax){
+ 
+    };
+    // loop
+    const loop = () => {
         const now = new Date(),
         secs = (now - lt) / 1000;
         requestAnimationFrame(loop);
-        if(secs > 1 / FPS){
-            per += 1 / (maxFrames / FPS) * secs;
-            per %= 1;
-            updateBoxGeo(geometry, per);
+        if(secs > 1 / FPS_UPDATE){
+            // update, render
+            update( Math.floor(frame), FRAME_MAX);
             renderer.render(scene, camera);
+            // step frame
+            frame += FPS_MOVEMENT * secs;
+            frame %= FRAME_MAX;
             lt = now;
         }
     };
