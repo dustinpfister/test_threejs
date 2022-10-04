593ddf518ad787786ffc54f77177fea98cb76ffb
dustinpfister <dustin.pfister@gmail.com>
2022-10-04 10:04:01 -0400

    comments

--- views/forpost/threejs-canvas-texture/s2-1-loop-update/js/main.js
+++ views/forpost/threejs-canvas-texture/s2-1-loop-update/js/main.js
@@ -1,3 +1,6 @@
+//-------- ----------
+// CANVAS MODULE
+//-------- ----------
 (function(api){
     // create and return a canvasObj with texture
     api.createCanvasObject = function (state, drawFunc) {
@@ -23,8 +26,28 @@
         return canvasObj;
     };
 }( this['canvasMod'] = {} ));
-
+//-------- ----------
+// DEMO
+//-------- ----------
 (function () {
+    //-------- ----------
+    // SCENE, CAMERA, RENDERER, FOG
+    //-------- ----------
+    const scene = new THREE.Scene();
+    scene.add( new THREE.GridHelper(10, 10))
+    const fogColor = new THREE.Color(0xffffff);
+    scene.background = fogColor;
+    scene.fog = new THREE.FogExp2(fogColor, 0.35);
+    const camera = new THREE.PerspectiveCamera(75, 320 / 240, 0.025, 100);
+    camera.position.set(1.75, 1.75, 1.75);
+    camera.lookAt(0, 0, 0);
+    scene.add(camera);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480);
+    ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+    //-------- ----------
+    // CANVAS OBJECT
+    //-------- ----------
     // state object
     const state = {
        frame: 0,
@@ -50,33 +73,24 @@
     const canvasObj = canvasMod.createCanvasObject(state, draw);
     // filter
     canvasObj.texture.magFilter = THREE.NearestFilter;
-    // SCENE
-    const scene = new THREE.Scene();
-    scene.add( new THREE.GridHelper(10, 10))
-    const fogColor = new THREE.Color(0xffffff);
-    scene.background = fogColor;
-    scene.fog = new THREE.FogExp2(fogColor, 0.35);
-    // CAMERA
-    const camera = new THREE.PerspectiveCamera(75, 320 / 240, 0.025, 100);
-    camera.position.set(1.75, 1.75, 1.75);
-    camera.lookAt(0, 0, 0);
-    scene.add(camera);
-    // light
+    //-------- ----------
+    // LIGHT
+    //-------- ----------
     const light = new THREE.PointLight();
     light.position.set(0,0.5,0)
     camera.add(light);
-    // using create cube method
+    //-------- ----------
+    // MESH
+    //-------- ----------
     const mesh = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshStandardMaterial({
                 map: canvasObj.texture
             }));
     scene.add(mesh);
-    // Render
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
-    // Loop
+    //-------- ----------
+    // LOOP
+    //-------- ----------
     const loop = function () {
         const now = new Date(),
         secs = (now - state.lt) / 1000;
