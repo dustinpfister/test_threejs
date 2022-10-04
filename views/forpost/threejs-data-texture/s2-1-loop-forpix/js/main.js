6c1c59c17d1f602fe052c455d6dc842e8e706474
dustinpfister <dustin.pfister@gmail.com>
2022-10-04 12:38:02 -0400

    better random for pix method

--- views/forpost/threejs-data-texture/s2-1-loop-forpix/js/main.js
+++ views/forpost/threejs-data-texture/s2-1-loop-forpix/js/main.js
@@ -1,6 +1,6 @@
-//******** **********
+//-------- ----------
 // SCENE, CAMERA, RENDERER
-//******** **********
+//-------- ----------
 const scene = new THREE.Scene();
 scene.add(new THREE.GridHelper(8,8))
 const camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
@@ -9,15 +9,15 @@ camera.lookAt(0, 0, 0);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
-//******** **********
+//-------- ----------
 // ADD A LIGHT BECUASE THIS IS THE STANDARD MATERIAL THAT I AM USING
-//******** **********
-const light = new THREE.PointLight(new THREE.Color(1, 1, 1));
-light.position.set(1, 3, 2);
+//-------- ----------
+const light = new THREE.PointLight( new THREE.Color(1, 1, 1), 1 );
+light.position.set(6, 3, 1);
 scene.add(light);
-//******** **********
-// DATA TEXTURE HELPER
-//******** **********
+//-------- ----------
+// HELPER FUNCTIONS
+//-------- ----------
 // create data texture method
 const createDataTexture = function(opt){
     opt = opt || {};
@@ -47,39 +47,72 @@ const createDataTexture = function(opt){
     texture.needsUpdate = true;
     return texture;
 };
-//******** **********
-// MESH OBJECTS WITH DATA TEXTURES
-//******** **********
-// default sudo random texture
-const tex1 = createDataTexture();
-const mesh1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshStandardMaterial({
-        map: tex1
-    })
-);
-scene.add(mesh1);
-//******** **********
-// LOOP
-//******** **********
-let lt = new Date();
-const frame = 0,
-maxFrame = 200,
-fps = 20;
-const loop = function () {
-    const now = new Date(),
-    per = frame / maxFrame,
-    bias = 1 - Math.abs(per - 0.5) / 0.5,
-    secs = (now - lt) / 1000;
-    requestAnimationFrame(loop);
-    if (secs > 1 / fps) {
-        // new data texture for mesh1
-        mesh1.material.map = createDataTexture();
-        renderer.render(scene, camera);
-        frame += fps * secs;
-        frame %= maxFrame;
-        lt = now;
-    }
-
+// get random from range
+const getRndFromRange = (range) => {
+    return range[0] + THREE.MathUtils.seededRandom() * ( range[1] - range[0] );
 };
-loop();
+// make cube
+const makeCube = (x, y, z) => {
+    const mesh = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshStandardMaterial()
+    );
+    mesh.position.set(x, y, z);
+    return mesh;
+};
+//-------- ----------
+// CREATE FOR PIX FUNCTIONS
+//-------- ----------
+const forPix = {};
+// better random function
+forPix.rndChannel = (r, g, b) => {
+   r = r || [0, 255];
+   g = g || [0, 255];
+   b = b || [0, 255];
+   return function(color, x, y, i, opt){
+        color.r = getRndFromRange(r);
+        color.g = getRndFromRange(g);
+        color.b = getRndFromRange(b);
+        return color;
+    };
+};
+//-------- ----------
+// MESH OBJECTS WITH DATA TEXTURES
+//-------- ----------
+const mesh1 = makeCube(0, 0, 0);
+scene.add(mesh1);
+const mesh2 = makeCube(-3, 0, 0);
+scene.add(mesh2);
+    // ---------- ----------
+    // ANIMATION LOOP
+    // ---------- ----------
+    const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+    FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+    FRAME_MAX = 300;
+    let secs = 0,
+    frame = 0,
+    lt = new Date();
+    // update
+    const update = function(frame, frameMax){
+        // mesh1 - rand default
+        mesh1.material.map = createDataTexture({ forPix: forPix.rndChannel() });
+        // mesh2- rand red
+        mesh2.material.map = createDataTexture({ forPix: forPix.rndChannel([64, 200], [0,0], [0,0]) });
+    };
+    // loop
+    const loop = () => {
+        const now = new Date(),
+        secs = (now - lt) / 1000;
+        requestAnimationFrame(loop);
+        if(secs > 1 / FPS_UPDATE){
+            // update, render
+            update( Math.floor(frame), FRAME_MAX);
+            renderer.render(scene, camera);
+            // step frame
+            frame += FPS_MOVEMENT * secs;
+            frame %= FRAME_MAX;
+            lt = now;
+        }
+    };
+    loop();
+
