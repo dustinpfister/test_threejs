e765ea813644d6adc32374d6e6ad6bcb2cd70166
dustinpfister <dustin.pfister@gmail.com>
2022-10-05 07:31:45 -0400

    cont let and doc body for all look at examples

--- views/forpost/threejs-object3d-lookat/s2-1-relative-to-world/js/main.js
+++ views/forpost/threejs-object3d-lookat/s2-1-relative-to-world/js/main.js
@@ -1,20 +1,29 @@
-
-// creating a scene
-var scene = new THREE.Scene();
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
 scene.add(new THREE.GridHelper(5, 5));
-
+const camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 100);
+camera.position.set(0, 4, 4);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+//  GROUP, MESH
+//-------- ----------
 // creating a group
-var group = new THREE.Group();
+const group = new THREE.Group();
 // creating and adding a pointer mesh to the group
-var geo = new THREE.CylinderGeometry(0, 0.5, 1, 12);
+const geo = new THREE.CylinderGeometry(0, 0.5, 1, 12);
 geo.rotateX(Math.PI * 0.5);
-var pointer = new THREE.Mesh(
+const pointer = new THREE.Mesh(
         geo,
         new THREE.MeshNormalMaterial());
 pointer.position.set(0, 0, 0);
 group.add(pointer);
 // creating and adding a cube
-var cube = new THREE.Mesh(
+const cube = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
 cube.position.set(0, 0, 4);
@@ -25,16 +34,9 @@ group.add(new THREE.BoxHelper(group));
 group.position.set(-2.0, 0, -2.0);
 // add group to the scene
 scene.add(group);
-
 // POINTER LOOKS AT CUBE POSITION RELATIVE TO THE SCENE, BUT NOT RELATIVE TO THE GROUP
 pointer.lookAt(cube.position);
-
-// camera and renderer
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 100);
-camera.position.set(0, 4, 4);
-camera.lookAt(0, 0, 0);
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
+//-------- ----------
+// RENDER
+//-------- ----------
 renderer.render(scene, camera);
--- views/forpost/threejs-object3d-lookat/s2-2-relative-to-group/js/main.js
+++ views/forpost/threejs-object3d-lookat/s2-2-relative-to-group/js/main.js
@@ -1,20 +1,28 @@
-
-// creating a scene
-var scene = new THREE.Scene();
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
 scene.add(new THREE.GridHelper(5, 5));
-
-// creating a group
-var group = new THREE.Group();
+const camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 100);
+camera.position.set(0, 4, 4);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+//  GROUP, MESH
+//-------- ----------
+const group = new THREE.Group();
 // creating and adding a pointer mesh to the group
-var geo = new THREE.CylinderGeometry(0, 0.5, 1, 12);
+const geo = new THREE.CylinderGeometry(0, 0.5, 1, 12);
 geo.rotateX(Math.PI * 0.5);
-var pointer = new THREE.Mesh(
+const pointer = new THREE.Mesh(
         geo,
         new THREE.MeshNormalMaterial());
 pointer.position.set(0, 0, 0);
 group.add(pointer);
 // creating and adding a cube
-var cube = new THREE.Mesh(
+const cube = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
 cube.position.set(0, 0, 4);
@@ -25,20 +33,13 @@ group.add(new THREE.BoxHelper(group));
 group.position.set(-2.0, 0, -2.0);
 // add group to the scene
 scene.add(group);
-
 // IF I WANT TO HAVE THE POINTER LOOK AT THE CUBE
 // THAT IS A CHILD OF THE GROUP, THEN I WILL WANT TO ADJUST 
 // FOR THAT FOR THIS THERE IS THE getWorldPosition Method
-var target = new THREE.Vector3();
+const target = new THREE.Vector3();
 cube.getWorldPosition(target)
 pointer.lookAt( target );
-
-// camera and renderer
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 100);
-camera.position.set(0, 4, 4);
-camera.lookAt(0, 0, 0);
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
+//-------- ----------
+// RENDER
+//-------- ----------
 renderer.render(scene, camera);
