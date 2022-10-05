d7481bdb752181c6146fa3c447beaec93cc8e942
dustinpfister <dustin.pfister@gmail.com>
2022-10-05 11:29:30 -0400

    change boxgeometry example

--- views/forpost/threejs-buffer-geometry-attributes-position/s2-1-boxgeometry/js/main.js
+++ views/forpost/threejs-buffer-geometry-attributes-position/s2-1-boxgeometry/js/main.js
@@ -4,7 +4,7 @@
     //-------- ----------
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
-    camera.position.set(2, 2, 2);
+    camera.position.set(2, 2, 3);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
@@ -17,13 +17,15 @@
     console.log( position.count ); // 24
     console.log( position.array.length ); // 72
     console.log( position.count * 3 === position.array.length); // true
+    // get the index for all the trangles
     const index = geometry.getIndex();
     console.log( index.count );      // 36
-    console.log( 2 * 6 );            // 12 ( number of triangles )
     console.log( index.count / 3);   /* 12 (index.count / 3 === number of triangles ) */
     // mutating a position
-    const vertIndex = index.array[0] * 3;
-    position.array[vertIndex] = 1;
+    const vertIndex = index.array[0];
+    position.array[vertIndex] = 0.8;
+    position.array[vertIndex + 1] = 0.5;
+    position.array[vertIndex + 2] = 0.5;
     position.needsUpdate = true;
     // use the geometry with a mesh
     const mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({
