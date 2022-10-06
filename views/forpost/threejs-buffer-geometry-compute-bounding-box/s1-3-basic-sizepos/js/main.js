46d092d4b469e3213fc31ca771c71b45f85960e2
dustinpfister <dustin.pfister@gmail.com>
2022-10-06 11:12:17 -0400

    position mesh example looks good

--- views/forpost/threejs-buffer-geometry-compute-bounding-box/s1-3-basic-sizepos/js/main.js
+++ views/forpost/threejs-buffer-geometry-compute-bounding-box/s1-3-basic-sizepos/js/main.js
@@ -10,23 +10,35 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480);
 (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
 //-------- ----------
-// GEOMETRY
+// HELPERS
 //-------- ----------
-const geo_box = new THREE.BoxGeometry(1, 4, 3.5);
-// COMPUTE THE BOUNDING BOX AND GET bb REF TO IT
-geo_box.computeBoundingBox();
-const bb = geo_box.boundingBox;
-// GET SIZE
-const v_size = new THREE.Vector3();
-bb.getSize(v_size);
-console.log(v_size)
+const positionMesh = (mesh, x, z) => {
+    const geo = mesh.geometry;
+    // COMPUTE THE BOUNDING BOX AND GET bb REF TO IT
+    geo.computeBoundingBox();
+    const bb = geo.boundingBox;
+    // GET SIZE, and use size to position MESH
+    const v_size = new THREE.Vector3();
+    bb.getSize(v_size);
+    mesh.position.set(x, v_size.y / 2, z);
+    return mesh;
+};
 //-------- ----------
 // MESH, MATERIAL
 //-------- ----------
-const mesh = new THREE.Mesh( geo_box, new THREE.MeshNormalMaterial({}) );
-scene.add(mesh);
-// USE SIZE TO POSITION MESH
-mesh.position.set(0, v_size.y / 2, 0)
+const m = new THREE.MeshNormalMaterial();
+scene.add( positionMesh(
+    new THREE.Mesh(
+        new THREE.BoxGeometry(1, 3.25, 3), m),
+        -4.5, -3.5));
+scene.add( positionMesh(
+    new THREE.Mesh(
+        new THREE.BoxGeometry(1, 5, 1), m),
+        3.5, 1.5));
+scene.add( positionMesh(
+    new THREE.Mesh(
+        new THREE.BoxGeometry(2, 2, 2), m),
+        -3.0, 3.0));
 //-------- ----------
 //  RENDER
 //-------- ----------
