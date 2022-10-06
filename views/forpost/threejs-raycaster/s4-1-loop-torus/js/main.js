b49ea512c2e4622fcad7bd4662b2025e4ed5b133
dustinpfister <dustin.pfister@gmail.com>
2022-10-06 15:53:30 -0400

    loop one looks good

--- views/forpost/threejs-raycaster/s4-1-loop-torus/js/main.js
+++ views/forpost/threejs-raycaster/s4-1-loop-torus/js/main.js
@@ -35,48 +35,43 @@ const getLookAt = (deg, radius) => {
     let radian = Math.PI / 180 * deg;
     return new THREE.Vector3(1, 0, 0).applyEuler( new THREE.Euler(0, radian, 0) ).multiplyScalar(radius);
 };
-
 //-------- ----------
 // MESH - SPHERE
 //-------- ----------
+const torus_radius = 4;
 const torus = new THREE.Mesh(
-        new THREE.TorusGeometry(4, 0.75, 20, 20),
+        new THREE.TorusGeometry(torus_radius, 1.25, 20, 20),
         new THREE.MeshNormalMaterial({wireframe: true}));
 torus.geometry.rotateX(Math.PI * 0.5)
 scene.add(torus);
 // create mesh at point
 const box = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.BoxGeometry(1.5, 1.5, 1.5),
     new THREE.MeshNormalMaterial());
 scene.add(box);
 //-------- ----------
 // RAYCASTER
 //-------- ----------
-let v_ray_origin = new THREE.Vector3(0, 3, 0); // where the ray comes from
-let v_lookat = getLookAt(300, 4);
-let v_ray_dir = getDir(v_ray_origin,  v_lookat);
-
 // create raycaster
-const near = 0, far = 100;
-const raycaster = new THREE.Raycaster(v_ray_origin, v_ray_dir, near, far);
-
-
-setMeshIfHit(raycaster, box, torus, v_lookat);
-
+const raycaster = new THREE.Raycaster();
 // ---------- ----------
 // ANIMATION LOOP
 // ---------- ----------
 const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
 FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
-FRAME_MAX = 120;
+FRAME_MAX = 300;
 let secs = 0,
 frame = 0,
 lt = new Date();
 // update
 const update = function(frame, frameMax){
-
-
-
+    let a = frame / frameMax;
+    let b = 1 - Math.abs(0.5 - a * 2 % 1 ) / 0.5;
+    let v_lookat = getLookAt(360 * a, torus_radius);
+    let v_ray_origin = new THREE.Vector3(0, -20 + 40 * b, 0)
+    let v_ray_dir = getDir(v_ray_origin,  v_lookat);
+    raycaster.set(v_ray_origin, v_ray_dir);
+    setMeshIfHit(raycaster, box, torus, v_lookat);
 };
 // loop
 const loop = () => {
