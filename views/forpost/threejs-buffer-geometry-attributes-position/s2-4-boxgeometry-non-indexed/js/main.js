c801f794056a7abbcff537ed2df655cb790d4f9b
dustinpfister <dustin.pfister@gmail.com>
2022-10-06 07:58:14 -0400

    looks like I have a good non indexed example

--- views/forpost/threejs-buffer-geometry-attributes-position/s2-4-boxgeometry-non-indexed/js/main.js
+++ views/forpost/threejs-buffer-geometry-attributes-position/s2-4-boxgeometry-non-indexed/js/main.js
@@ -5,11 +5,16 @@
     // set location of a vert given an index value in geometry.index
     const setVert = function(geometry, vertIndex, pos){
         pos = pos || {};
-        const posIndex = geometry.index.array[vertIndex] * 3,
-        position = geometry.getAttribute('position');
-        position.array[posIndex] = pos.x === undefined ? position.array[posIndex]: pos.x;
-        position.array[posIndex + 1] = pos.y === undefined ? position.array[posIndex + 1]: pos.y;
-        position.array[posIndex + 2] = pos.z === undefined ? position.array[posIndex + 2]: pos.z;
+        const position = geometry.getAttribute('position');
+        let i = vertIndex * 3;
+        // do we have an index?
+        if(geometry.index){
+            //then use that
+            i = geometry.index.array[vertIndex] * 3;
+       }
+       position.array[i] = pos.x === undefined ? position.array[i]: pos.x;
+       position.array[i + 1] = pos.y === undefined ? position.array[i + 1]: pos.y;
+       position.array[i + 2] = pos.z === undefined ? position.array[i + 2]: pos.z;
     };
     // set triangle
     const setTri = function(geometry, triIndex, pos){
@@ -19,40 +24,54 @@
         setVert(geometry, vertIndex + 1, pos);
         setVert(geometry, vertIndex + 2, pos);
     };
+    // triangle movement helper
+    const triMoveOne = (geometry) => {
+        setTri(geometry, 0, {x: 0.8});
+        setTri(geometry, 1, {x: 1.1});
+        setTri(geometry, 2, {x: -0.8});
+        setTri(geometry, 3, {x: -1.1});
+        setTri(geometry, 4, {y: 0.8});
+        setTri(geometry, 5, {y: 1.1});
+        setTri(geometry, 6, {y: -0.8});
+        setTri(geometry, 7, {y: -1.1});
+        setTri(geometry, 8, {z: 0.8});
+        setTri(geometry, 9, {z: 1.1});
+        setTri(geometry, 10, {z: -0.8});
+        setTri(geometry, 11, {z: -1.1});
+    };
     //-------- ----------
     // SCENE, CAMERA, RENDERER
     //-------- ----------
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
-    camera.position.set(2, 2, 2);
+    camera.position.set(5, 5, 5);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480);
     (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
     //-------- ----------
     // GEOMETRY
     //-------- ----------
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const geo_source = new THREE.BoxGeometry(1, 1, 1);
+	const geo_indexed = geo_source.clone();
+	const geo_nonindexed = geo_source.clone().toNonIndexed();
     // example 2 on set tri helper
-    setTri(geometry, 0, {x: 1});
-    setTri(geometry, 1, {x: 1});
-    setTri(geometry, 2, {x: -1});
-    setTri(geometry, 3, {x: -1});
-    setTri(geometry, 4, {y: 1});
-    setTri(geometry, 5, {y: 1});
-    setTri(geometry, 6, {y: -1});
-    setTri(geometry, 7, {y: -1});
-    setTri(geometry, 8, {z: 1});
-    setTri(geometry, 9, {z: 1});
-    setTri(geometry, 10, {z: -1});
-    setTri(geometry, 11, {z: -1});
+
+triMoveOne(geo_indexed);
+triMoveOne(geo_nonindexed);
+
     //-------- ----------
     // MESH
     //-------- ----------
-    const mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({
+    const mesh1 = new THREE.Mesh(geo_indexed, new THREE.MeshNormalMaterial({
         side: THREE.DoubleSide
     }));
-    scene.add(mesh);
-    camera.lookAt(mesh.position);
+    scene.add(mesh1);
+    camera.lookAt(mesh1.position);
+    const mesh2 = new THREE.Mesh(geo_nonindexed, new THREE.MeshNormalMaterial({
+        side: THREE.DoubleSide
+    }));
+    mesh2.position.x = -3;
+    scene.add(mesh2);
     //-------- ----------
     // LOOP
     //-------- ----------
