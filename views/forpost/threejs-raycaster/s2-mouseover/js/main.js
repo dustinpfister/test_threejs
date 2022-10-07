0d0fc7d5134975a832ef17fc23bfe857152e7a07
dustinpfister <dustin.pfister@gmail.com>
2022-10-07 12:56:12 -0400

    mouse over event clean up

--- views/forpost/threejs-raycaster/s2-mouseover/js/main.js
+++ views/forpost/threejs-raycaster/s2-mouseover/js/main.js
@@ -1,53 +1,46 @@
-var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2(1, 1);
- 
-var onMouseMove = function( event ) {
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(9, 9));
+const camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(5, 5, 5);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// MOUSE OVER EVENT
+//-------- ----------
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2(-5, -5);
+// update mouse Vector2 on pointer down event
+const onDown = ( event ) => {
     // calculate mouse position in normalized device coordinates
     // (-1 to +1) for both components
-    var canvas = event.target,
+    const canvas = event.target,
     box = canvas.getBoundingClientRect(),
     x = event.clientX - box.left,
     y = event.clientY - box.top;
     // set mouse Vector2 values
     mouse.x = ( x / canvas.scrollWidth ) * 2 - 1;
     mouse.y = - ( y / canvas.scrollHeight ) * 2 + 1;
-    //console.log(mouse.x.toFixed(2), mouse.y.toFixed(2));
 };
- 
-var update = function(group){
-    // default scale
-    group.children.forEach(function(obj){
-        obj.scale.set(1, 1, 1);
-    });
-    // update the picking ray with the camera and mouse position
-    raycaster.setFromCamera( mouse, camera );
-    var intersects = raycaster.intersectObjects(group.children, true );
-    if(intersects.length > 0){
-        var mesh = intersects[0].object;
-        mesh.scale.set(2, 2, 2);
-    }
+// set vector2 baco to -5 -5 on pointer up
+const onUp = ( event ) => {
+    mouse.x = -5;
+    mouse.y = - -5;
 };
- 
-// camera and renderer
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(5, 5, 5);
-camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
- 
 // Attach mouse event
-renderer.domElement.addEventListener( 'mousemove', onMouseMove, false );
- 
-// creating a scene
-var scene = new THREE.Scene();
-scene.add(new THREE.GridHelper(9, 9));
- 
-var boxGroup = new THREE.Group();
+renderer.domElement.addEventListener( 'pointerdown', onDown, false );
+renderer.domElement.addEventListener( 'pointerup', onUp, false );
+//-------- ----------
+// CHILD OBJECTS
+//-------- ----------
+const boxGroup = new THREE.Group();
 scene.add(boxGroup);
- 
 // box 1
-var box = new THREE.Mesh(
+let box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
 box.position.set(0, 0, 0);
@@ -64,15 +57,28 @@ box = new THREE.Mesh(
         new THREE.MeshNormalMaterial());
 box.position.set(-3, 0, 0);
 boxGroup.add(box);
- 
+//-------- ----------
+// LOOP
+//-------- ----------
 // orbit controls
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
- 
-// loop
-var lt = new Date(),
-fps = 30;
-var loop = function () {
-    var now = new Date(),
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+let lt = new Date();
+const fps = 30;
+const update = (group) => {
+    // default scale
+    group.children.forEach(function(obj){
+        obj.scale.set(1, 1, 1);
+    });
+    // update the picking ray with the camera and mouse position
+    raycaster.setFromCamera( mouse, camera );
+    const intersects = raycaster.intersectObjects(group.children, true );
+    if(intersects.length > 0){
+        const mesh = intersects[0].object;
+        mesh.scale.set(2, 2, 2);
+    }
+};
+const loop = function () {
+    const now = new Date(),
     secs = (now - lt) / 1000;
     requestAnimationFrame(loop);
     if (secs > 1 / fps) {
