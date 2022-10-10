81c82f70ff1fa9c17beba320b08dc5ba2724547d
dustinpfister <dustin.pfister@gmail.com>
2022-10-10 11:47:23 -0400

    updated heart example

--- views/forpost/threejs-shape/s3-1-heart/js/main.js
+++ views/forpost/threejs-shape/s3-1-heart/js/main.js
@@ -1,31 +1,48 @@
- 
-// creating a scene
-var scene = new THREE.Scene();
-
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+scene.add( new THREE.GridHelper(10, 10) );
+const camera = new THREE.PerspectiveCamera(60, 64 / 48, 0.1, 1000);
+camera.position.set(3, 3, 3);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
 // SHAPE
-var heartShape = new THREE.Shape();
-heartShape.moveTo( 25, 25 );
-heartShape.bezierCurveTo( 25, 25, 20, 0, 0, 0 );
-heartShape.bezierCurveTo( - 30, 0, - 30, 35, - 30, 35 );
-heartShape.bezierCurveTo( - 30, 55, - 10, 77, 25, 95 );
-heartShape.bezierCurveTo( 60, 77, 80, 55, 80, 35 );
-heartShape.bezierCurveTo( 80, 35, 80, 0, 50, 0 );
-heartShape.bezierCurveTo( 35, 0, 25, 25, 25, 25 );
-
-// geometry
-var extrudeSettings = { depth: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
-var geometry = new THREE.ExtrudeGeometry( heartShape, extrudeSettings );
-geometry.rotateX(Math.PI * 1)
-// mesh
-var mesh = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial() );
+//-------- ----------
+const heartShape = new THREE.Shape();
+heartShape.moveTo( 2.5, 2.5 );
+heartShape.bezierCurveTo( 2.5, 2.5, 2.0, 0, 0, 0 );
+heartShape.bezierCurveTo( - 3.0, 0, - 3.0, 3.5, - 3.0, 3.5 );
+heartShape.bezierCurveTo( - 3.0, 5.5, - 1.0, 7.7, 2.5, 9.5 );
+heartShape.bezierCurveTo( 6.0, 7.7, 8.0, 5.5, 8.0, 3.5 );
+heartShape.bezierCurveTo( 8.0, 3.5, 8.0, 0, 5.0, 0 );
+heartShape.bezierCurveTo( 3.5, 0, 2.5, 2.5, 2.5, 2.5 );
+//-------- ----------
+// GEOMETRY
+//-------- ----------
+const extrudeSettings = {
+    depth: 1.5,
+    bevelEnabled: true,
+    curveSegments: 40,
+    bevelSegments: 20,
+    steps: 8,
+    bevelThickness: 0.75,
+    bevelSize: 0.75 };
+const geometry = new THREE.ExtrudeGeometry( heartShape, extrudeSettings );
+geometry.rotateX(Math.PI * 1);
+geometry.center();
+//-------- ----------
+// MESH
+//-------- ----------
+const mesh = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial() );
+let s = 0.25;
+mesh.scale.set(s, s, s);
 // add the mesh to the scene
 scene.add(mesh);
- 
-// camera and renderer
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(100, 100, 100);
-camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
+//-------- ---------- 
+// RENDER
+//-------- ----------
 renderer.render(scene, camera);
