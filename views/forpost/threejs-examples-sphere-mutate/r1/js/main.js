d4d729cd3966ac92ddbd150d572e07f35810abad
dustinpfister <dustin.pfister@gmail.com>
2022-10-10 19:08:58 -0400

    started an r1 folder for sphere mutate example

---
+++ views/forpost/threejs-examples-sphere-mutate/r1/js/main.js
@@ -0,0 +1,54 @@
+
+(function () {
+
+    // scene
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0.02,0.02,0.02)
+    const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
+    camera.position.set(1, 1, 1);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480, false);
+    (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+    const dl = new THREE.DirectionalLight(0xffffff, 1);
+    dl.position.set(3, 1, -2);
+    scene.add(dl);
+    // ---------- ----------
+    // GEOMETRY, MESH
+    // ---------- ----------
+    const mesh = new THREE.Mesh(
+        new THREE.SphereGeometry(0.5, 30, 30), 
+        new THREE.MeshStandardMaterial({
+                color: 'red'
+            }));
+    scene.add(mesh);
+    camera.lookAt(mesh.position);
+    // ---------- ----------
+    // ANIMATION LOOP
+    // ---------- ----------
+    const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+    FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+    FRAME_MAX = 120;
+    let secs = 0,
+    frame = 0,
+    lt = new Date();
+    // update
+    const update = function(frame, frameMax){
+    };
+    // loop
+    const loop = () => {
+        const now = new Date(),
+        secs = (now - lt) / 1000;
+        requestAnimationFrame(loop);
+        if(secs > 1 / FPS_UPDATE){
+            // update, render
+            update( Math.floor(frame), FRAME_MAX);
+            renderer.render(scene, camera);
+            // step frame
+            frame += FPS_MOVEMENT * secs;
+            frame %= FRAME_MAX;
+            lt = now;
+        }
+    };
+    loop();
+}
+    ());
---
+++ views/forpost/threejs-examples-sphere-mutate/todo.md
@@ -0,0 +1,7 @@
+# threejs-examples-sphere-mutate todo list
+
+## () r1 - buffer attribute methods
+* () new example in which I am using the buffer Attribute methods
+
+## ( done 06/10/2022 ) - r0 of sphere mutation example
+* (done) first version of the example done
