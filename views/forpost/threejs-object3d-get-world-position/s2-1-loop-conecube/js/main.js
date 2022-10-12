776cc1887fd6001f2ebcf1077c122b80d32e7e7f
dustinpfister <dustin.pfister@gmail.com>
2022-10-12 10:32:44 -0400

    added loop

--- views/forpost/threejs-object3d-get-world-position/s2-1-loop-conecube/js/main.js
+++ views/forpost/threejs-object3d-get-world-position/s2-1-loop-conecube/js/main.js
@@ -1,57 +1,77 @@
-var createGroup = function (color) {
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 100);
+camera.position.set(0, 4, 4);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+( document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+//-------- ----------
+// HELPERS
+//-------- ----------
+const createGroup = function (color) {
     color = color || new THREE.Color(1, 1, 1);
-    // creating a group
-    var group = new THREE.Group();
-    // creating and adding a pointer mesh to the group
-    var geo = new THREE.CylinderGeometry(0, 0.5, 1, 12);
+    const group = new THREE.Group();
+    const geo = new THREE.CylinderGeometry(0, 0.5, 1, 12);
     geo.rotateX(Math.PI * 0.5);
-    var pointer = group.userData.pointer = new THREE.Mesh(
+    const pointer = group.userData.pointer = new THREE.Mesh(
             geo,
             new THREE.MeshNormalMaterial());
     pointer.position.set(0, 0, 0);
-    pointer.rotation.y = 1.57; // BY DEFAULT THE POINTER IS NOT POINTING AT THE CUBE
+    pointer.rotation.y = 1.57;
     group.add(pointer);
-    // creating and adding a cube
-    var cube = group.userData.cube = new THREE.Mesh(
+    const cube = group.userData.cube = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshBasicMaterial({ color: color, transparent: true, opacity: 0.5 }));
     cube.position.set(0, 0, 1);
     group.add(cube);
-    // box helper for the group
     group.add(new THREE.BoxHelper(group));
     return group;
 };
- 
-// creating a scene
-var scene = new THREE.Scene();
+//-------- ----------
+// SCENE CHILD OBJECTS
+//-------- ----------
 scene.add(new THREE.GridHelper(5, 5));
- 
-var group = createGroup(0xff0000); // group 1
-scene.add(group);
-group.position.set(-2.0, 0, 0.0);
-var group2 = createGroup(0x00ff00); // group2
+// just set up group1 and group2
+const group1 = createGroup(0xff0000);
+scene.add(group1);
+group1.position.set(-2.0, 0, 0.0);
+const group2 = createGroup(0x00ff00);
 scene.add(group2);
 group2.position.set(2.0, 0, 0.0);
- 
-// the first group in am just using the look at method, and passing
-// the value of the cube.position instance of vector3. THIS RESULTS IN THE
-// CONE NOT POINTING AT THE CUBE, but at the location of the cube if it where
-// positioned relative to world space rather than a location relative to the group
-group.userData.pointer.lookAt(group.userData.cube.position);
- 
-// IF I WANT TO HAVE THE POINTER LOOK AT THE CUBE
-// THAT IS A CHILD OF THE GROUP, THEN I WILL WANT TO ADJUST
-// FOR THAT FOR THIS THERE IS THE getWorldPosition METHOD
-var v = new THREE.Vector3(0, 0, 0);
-group2.userData.cube.getWorldPosition(v);
-group2.userData.pointer.lookAt(v);
- 
-// camera and renderer
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 100);
-camera.position.set(0, 4, 4);
-camera.lookAt(0, 0, 0);
- 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
-renderer.render(scene, camera);
+// ---------- ----------
+// ANIMATION LOOP
+// ---------- ----------
+const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+FRAME_MAX = 120;
+let secs = 0,
+frame = 0,
+lt = new Date();
+// update
+const update = function(frame, frameMax){
+    // with group1 I am just passing lookAt the LOCAL position of the cube
+    group1.userData.pointer.lookAt(group1.userData.cube.position);
+    // with group to I am USING GETWORLDPOSITION to get a vector to pass to lookAt
+    const v = new THREE.Vector3(0, 0, 0);
+    group2.userData.cube.getWorldPosition(v);
+    group2.userData.pointer.lookAt(v);
+};
+// loop
+const loop = () => {
+    const now = new Date(),
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if(secs > 1 / FPS_UPDATE){
+        // update, render
+        update( Math.floor(frame), FRAME_MAX);
+        renderer.render(scene, camera);
+        // step frame
+        frame += FPS_MOVEMENT * secs;
+        frame %= FRAME_MAX;
+        lt = now;
+    }
+};
+loop();
