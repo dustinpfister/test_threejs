eaca9fdbaa142cadae18e58b051ad97a73a1e23d
dustinpfister <dustin.pfister@gmail.com>
2022-10-13 16:58:15 -0400

    materials are looking good

--- views/forpost/threejs-plane/s5-2-loop-video2/js/main.js
+++ views/forpost/threejs-plane/s5-2-loop-video2/js/main.js
@@ -64,48 +64,21 @@
         texture.image.data = data;
         texture.needsUpdate = true;
     };
-    // simple gray scale seeded random texture
-/*
-    const seededRandom = function (w, h, rPer, gPer, bPer, range) {
-        w = w === undefined ? 5 : w,
-        h = h === undefined ? 5 : h;
-        rPer = rPer === undefined ? 1 : rPer;
-        gPer = gPer === undefined ? 1 : gPer;
-        bPer = bPer === undefined ? 1 : bPer;
-        range = range || [0, 255]
-        const size = w * h;
-        const data = new Uint8Array(4 * size);
-        for (let i = 0; i < size; i++) {
-            const stride = i * 4;
-            const v = Math.floor(range[0] + THREE.MathUtils.seededRandom() * (range[1] - range[0]));
-            data[stride] = v * rPer;
-            data[stride + 1] = v * gPer;
-            data[stride + 2] = v * bPer;
-            data[stride + 3] = 255;
-        }
-        return createDataTexture({
-            data: data,
-            width: w,
-            height: h
-        });
-    };
-*/
     //-------- ----------
     // TEXTURES
     //-------- ----------
-    
     const opt_data_texture = {
         alpha: 1,
         forPix: function(color, x, y, i, opt){
-            color.setRGB(0, 50 + 100 * opt.alpha + 100 * opt.alpha * Math.random(), 0);
-            let v = 0;
+            let v = 50 + 100 * opt.alpha + 100 * opt.alpha * Math.random();
+            color.setRGB(0, v, 0);
             if(y % 2 === 0 && x % 2 === 0){
                v = 25 + 50 * (1 - opt.alpha) + 25 * Math.random();
-               color.setRGB(v, 0, v);
+               color.setRGB(0, v, 0);
             }
             if(y % 2 != 0 && x % 2 != 0){
                v = 50 + 100 * (1 - opt.alpha) + 50 * Math.random();
-               color.setRGB(v, 0, v);
+               color.setRGB(0, v, 0);
             }
             return color;
         }
@@ -114,9 +87,9 @@
     // random texture options
     const opt_data_texture_rnd = {
         forPix: function(color, x, y, i, opt){
-            color.r = 32 + 200 * Math.random();
+            //color.r = 32 + 200 * Math.random();
             color.g = 32 + 200 * Math.random();
-            color.b = 32 + 200 * Math.random();
+            //color.b = 32 + 200 * Math.random();
             return color;
         }
     };
@@ -139,17 +112,24 @@
     //-------- ----------
     // GRID SOURCE OBJECTS
     //-------- ----------
-    const tw = 5,
-    th = 5,
+    const tw = 8,
+    th = 8,
     space = 3.1;
-    const ground = TileMod.create({w: 3, h: 3, sw: 2, sh: 2, materials: MATERIALS});
-    TileMod.setCheckerBoard(ground)
+    const ground1 = TileMod.create({w: 3, h: 3, sw: 4, sh: 4, materials: MATERIALS[0]});
+    TileMod.setCheckerBoard(ground1);
+    const ground2 = TileMod.create({w: 3, h: 3, sw: 4, sh: 4, materials: MATERIALS});
+    TileMod.setCheckerBoard(ground2);
+    const ground3 = TileMod.create({w: 3, h: 3, sw: 4, sh: 4, materials: MATERIALS[1]});
+    TileMod.setCheckerBoard(ground3);
     const array_source_objects = [
-        ground
+        ground1,
+        ground2,
+        ground3
     ];
     const array_oi = [],
     len = tw * th;
     let i = 0;
+    // random index values for source objects?
     while(i < len){
         array_oi.push( Math.floor( array_source_objects.length * THREE.MathUtils.seededRandom() ) );
         i += 1;
@@ -161,7 +141,8 @@
         space: space,
         tw: tw,
         th: th,
-        aOpacity: 1.25,
+        effects: ['opacity2'],
+        //aOpacity: 1.25,
         sourceObjects: array_source_objects,
         objectIndices: array_oi
     });
@@ -169,7 +150,7 @@
     // ---------- ----------
     // LOOP
     // ---------- ----------
-    const fps_move = 30, fps_update = 8;
+    const fps_move = 30, fps_update = 20;
     let f = 0, fm = 300, lt = new Date();
     const loop = () => {
         const now = new Date();
