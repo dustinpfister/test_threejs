f5cc6680b5b78573ee5b046f15e13872fee6e113
dustinpfister <dustin.pfister@gmail.com>
2022-10-13 16:27:08 -0400

    clean up of loop1 example is done

--- views/forpost/threejs-plane/s5-1-loop-video1/js/main.js
+++ views/forpost/threejs-plane/s5-1-loop-video1/js/main.js
@@ -10,30 +10,24 @@
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480, false);
     (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
-
-
-    var dl = new THREE.DirectionalLight();
+    // ---------- ----------
+    // LIGHT
+    // ---------- ----------
+    const dl = new THREE.DirectionalLight();
     scene.add(dl);
-
-//******** **********
-// GRID OPTIONS WITH TILE INDEX
-//******** **********
-var tw = 6,
-th = 6,
-space = 3.1;
-// source objects
-
+    // ---------- ----------
+    // HELPERS
+    // ---------- ----------
+    // make a data texture
     const mkDataTexture = function (data, w) {
         data = data || [];
         w = w || 0;
-        var width = w, //20,
+        const width = w,
         height = data.length / 4 / w;
-        var texture = new THREE.DataTexture(data, width, height);
+        const texture = new THREE.DataTexture(data, width, height);
         texture.needsUpdate = true;
         return texture;
     };
-
-
     // simple gray scale seeded random texture
     const seededRandom = function (w, h, rPer, gPer, bPer, range) {
         w = w === undefined ? 5 : w,
@@ -42,11 +36,11 @@ space = 3.1;
         gPer = gPer === undefined ? 1 : gPer;
         bPer = bPer === undefined ? 1 : bPer;
         range = range || [0, 255]
-        var size = w * h;
-        var data = new Uint8Array(4 * size);
+        const size = w * h;
+        const data = new Uint8Array(4 * size);
         for (let i = 0; i < size; i++) {
-            var stride = i * 4;
-            var v = Math.floor(range[0] + THREE.MathUtils.seededRandom() * (range[1] - range[0]));
+            const stride = i * 4;
+            const v = Math.floor(range[0] + THREE.MathUtils.seededRandom() * (range[1] - range[0]));
             data[stride] = v * rPer;
             data[stride + 1] = v * gPer;
             data[stride + 2] = v * bPer;
@@ -54,61 +48,60 @@ space = 3.1;
         }
         return mkDataTexture(data, w);
     };
-
-
-    var textureRND1 = seededRandom(80, 80, 1, 1, 1, [130, 250]);
-    var textureRND2 = seededRandom(160, 160, 1, 1, 1, [64, 170]);
-
-    var MATERIALS = [
+    //-------- ----------
+    // TEXTURES
+    //-------- ----------
+    const textureRND1 = seededRandom(80, 80, 1, 1, 1, [130, 250]);
+    const textureRND2 = seededRandom(160, 160, 1, 1, 1, [64, 170]);
+    //-------- ----------
+    // MATERIALS
+    //-------- ----------
+    const MATERIALS = [
         new THREE.MeshStandardMaterial({
             color: 0x00ff00,
             map: textureRND1,
-            //emissive: 0x00ff00,
-            //emissiveIntensity: 0.75,
             side: THREE.DoubleSide
         }),
         new THREE.MeshStandardMaterial({
             color: 0x00aa00,
             map: textureRND2,
-            //emissive: 0x00ff00,
-            //emissiveIntensity: 0.2,
             side: THREE.DoubleSide
         })
     ];
-
-
-    var ground = TileMod.create({w: 3, h: 3, sw: 2, sh: 2, materials: MATERIALS});
+    //-------- ----------
+    // GRID SOURCE OBJECTS
+    //-------- ----------
+    const tw = 5,
+    th = 5,
+    space = 3.1;
+    const ground = TileMod.create({w: 3, h: 3, sw: 2, sh: 2, materials: MATERIALS});
     TileMod.setCheckerBoard(ground)
-    //scene.add(ground);
-
-var array_source_objects = [
-    ground
-];
-var array_oi = [],
-len = tw * th, i = 0;
-while(i < len){
-    array_oi.push( Math.floor( array_source_objects.length * THREE.MathUtils.seededRandom() ) );
-    i += 1;
-}
-//******** **********
-// CREATE GRID
-//******** **********
-var grid = ObjectGridWrap.create({
-    space: space,
-    tw: tw,
-    th: th,
-    aOpacity: 1.25,
-    sourceObjects: array_source_objects,
-    objectIndices: array_oi
-});
-scene.add(grid);
-
-
-
+    const array_source_objects = [
+        ground
+    ];
+    const array_oi = [],
+    len = tw * th;
+    let i = 0;
+    while(i < len){
+        array_oi.push( Math.floor( array_source_objects.length * THREE.MathUtils.seededRandom() ) );
+        i += 1;
+    }
+    //-------- ----------
+    // CREATE GRID
+    //-------- ----------
+    const grid = ObjectGridWrap.create({
+        space: space,
+        tw: tw,
+        th: th,
+        aOpacity: 1.25,
+        sourceObjects: array_source_objects,
+        objectIndices: array_oi
+    });
+    scene.add(grid);
     // ---------- ----------
     // LOOP
     // ---------- ----------
-    const fps_move = 30, fps_update = 12;
+    const fps_move = 30, fps_update = 8;
     let f = 0, fm = 300, lt = new Date();
     const loop = () => {
         const now = new Date();
@@ -117,12 +110,8 @@ scene.add(grid);
        //const b = 1 - Math.abs( 0.5 - a ) / 0.5;
         requestAnimationFrame(loop);
         if(secs >= 1 / fps_update){
-
-
             ObjectGridWrap.setPos(grid, 1 - a, 0 );
             ObjectGridWrap.update(grid);
-
-
             f += fps_move * secs;
             f %= fm;
             renderer.render(scene, camera);
