0767674f9bf12e0fe76926e26c8b9bdb7063671b
dustinpfister <dustin.pfister@gmail.com>
2022-10-19 16:10:13 -0400

    proto is looking good

--- views/demos/r140/proto-object-paths/js/main.js
+++ views/demos/r140/proto-object-paths/js/main.js
@@ -13,24 +13,29 @@ renderer.setSize(640, 480, false);
 //-------- ----------
 
 
-const v1 = new THREE.Vector3(5, 0.1, 5);
-const v2 = new THREE.Vector3(0, 0.1, -5);
-const vControl = new THREE.Vector3(5, 0, -5);
+const v1 = new THREE.Vector3(5, 0, 5);
+const v2 = new THREE.Vector3(0, 2, -5);
+const vControl = new THREE.Vector3(5, 2, -5);
 const curve = new THREE.QuadraticBezierCurve3( v1, vControl, v2);
 // cretaing a v3 array with the curve
-const len = 1000;
+/*
+const len = 3;
 const v3Array = [];
 let i = 0;
-while(i < len){
+while(i <= len){
     v3Array.push( curve.getPoint( i / len) );
     i += 1;
 }
 
+console.log(v3Array);
+*/
+// you can just use get points
+const v3Array = curve.getPoints(30);
+console.log();
 const geometry = new THREE.BufferGeometry();
 geometry.setFromPoints(v3Array);
-const line = new THREE.Line(geometry, new THREE.LineBasicMaterial({color: 0xff0000 }));
-scene.add(line);
-console.log( geometry );
+const points = new THREE.Points(geometry, new THREE.PointsMaterial({color: 0xff0000, size: 0.25 }));
+scene.add(points);
 
 //-------- ----------
 // SCENE CHILD OBJECTS
