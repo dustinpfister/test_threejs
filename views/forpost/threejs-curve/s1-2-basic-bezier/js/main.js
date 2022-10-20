ab0fd4abbf9236953429af535901abae3fb2bfff
dustinpfister <dustin.pfister@gmail.com>
2022-10-20 09:34:03 -0400

    basic bezier example done

---
+++ views/forpost/threejs-curve/s1-2-basic-bezier/js/main.js
@@ -0,0 +1,32 @@
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+scene.add( new THREE.GridHelper(10, 10, 0x00ff00, 0x4a4a4a) )
+const camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
+camera.position.set(10, 5, 7);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480, false);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// CURVE
+//-------- ----------
+const v1 = new THREE.Vector3(5, 0, 5);
+const v2 = new THREE.Vector3(-5, 0, -5);
+const vControl = new THREE.Vector3(5, 0, -5);
+const curve = new THREE.QuadraticBezierCurve3( v1, vControl, v2);
+//-------- ----------
+// SCENE CHILD OBJECTS
+//-------- ----------
+scene.add( new THREE.GridHelper(10, 10, 0x00ff00, 0x4a4a4a) );
+// points
+const v3Array = curve.getPoints(20);
+const geometry = new THREE.BufferGeometry();
+geometry.setFromPoints(v3Array);
+const points = new THREE.Points(geometry, new THREE.PointsMaterial({color: 0xff0000, size: 0.25 }));
+scene.add(points);
+//-------- ----------
+// RENDER
+//-------- ----------
+renderer.render(scene, camera);
