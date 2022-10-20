7854fa354cd5b6accd505fa287151ca5ab76de23
dustinpfister <dustin.pfister@gmail.com>
2022-10-20 08:53:44 -0400

    first basic example

--- views/forpost/threejs-curve-quadratic-bezier-curve3/s1-basic/js/main.js
+++ views/forpost/threejs-curve-quadratic-bezier-curve3/s1-basic/js/main.js
@@ -1,49 +1,31 @@
-//******** **********
+//-------- ----------
 // SCENE, CAMERA, RENDERER
-//******** **********
-let scene = new THREE.Scene();
-scene.background = new THREE.Color('#000000');
-scene.add( new THREE.GridHelper(10, 10, 0x00ff00, 0x4a4a4a) )
-let camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(0, 5, 10);
-camera.lookAt(0, 3, 0);
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
-//******** **********
-// LIGHT
-//******** **********
-let dl = new THREE.DirectionalLight(0xffffff, 1);
-dl.position.set(3, 10, 1).normalize();
-scene.add(dl);
-scene.add( new THREE.AmbientLight(0xffffff, 0.05) )
-//******** **********
-// CURVE, TubeGeometry, Mesh
-//******** **********
-// basic cuve class extending three curve
-class BasicCurve extends THREE.Curve {
-    constructor() {
-        super();
-    }
-    getPoint( t, optionalTarget = new THREE.Vector3() ) {
-        var tx = -2 + 4 * t,
-        ty = Math.pow(2, 5 * t) / Math.pow(2, 5) * 7,
-        tz = 0;
-        return optionalTarget.set( tx, ty, tz );
-    }
-};
-// creating a tube geometry with path and additional arguments using basic curve
-let path = new BasicCurve(),
-tubularSegments = 800,
-radius = 0.25,
-radialSegments = 20;
-let mesh = new THREE.Mesh( 
-    new THREE.TubeGeometry( path, tubularSegments, radius, radialSegments, false ), 
-    new THREE.MeshStandardMaterial( { color: 0xff0000, side: THREE.DoubleSide })
-);
-scene.add( mesh );
-//******** **********
+//-------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+camera.position.set(7, 5, 10);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480, false);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// CURVE
+//-------- ----------
+const v1 = new THREE.Vector3(5, 0, 5);
+const v2 = new THREE.Vector3(-5, 0, -5);
+const vControl = new THREE.Vector3(5, 0, -5);
+const curve = new THREE.QuadraticBezierCurve3( v1, vControl, v2);
+//-------- ----------
+// SCENE CHILD OBJECTS
+//-------- ----------
+scene.add( new THREE.GridHelper(10, 10, 0x00ff00, 0x4a4a4a) );
+// points
+const v3Array = curve.getPoints(50);
+const geometry = new THREE.BufferGeometry();
+geometry.setFromPoints(v3Array);
+const points = new THREE.Points(geometry, new THREE.PointsMaterial({color: 0x00ff00, size: 0.25 }));
+scene.add(points);
+//-------- ----------
 // RENDER
-//******** **********
-renderer.render(scene, camera);      
-
+//-------- ----------
+renderer.render(scene, camera);
