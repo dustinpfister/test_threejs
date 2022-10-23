d90c4698c6102ee8c56c0c138b58d5bb3c1eb372
dustinpfister <dustin.pfister@gmail.com>
2022-10-23 08:40:35 -0400

    r2 of sphere mutate

--- views/forpost/threejs-examples-sphere-mutate/r2/js/main.js
+++ views/forpost/threejs-examples-sphere-mutate/r2/js/main.js
@@ -21,7 +21,7 @@
     // create the mesh object
     const createMesh = (texture) => {
         const mesh = new THREE.Mesh(
-            new THREE.SphereGeometry(0.25, 30, 30, 0, Math.PI * 2), 
+            new THREE.SphereGeometry(0.25, 60, 60, 0, Math.PI * 2), 
             new THREE.MeshPhongMaterial({
                 color: 'white',
                 map: texture || null,
@@ -54,7 +54,7 @@
         }else{
             if(x < w){
                 const vs = new THREE.Vector3(pos_base.getX(i), pos_base.getY(i), pos_base.getZ(i));
-                const v = vs.clone().normalize().multiplyScalar(1 + 0.5 * Math.random());
+                const v = vs.clone().normalize().multiplyScalar(0.5 + 0.95 * Math.random());
                 pos.setXYZ(i, v.x, v.y, v.z);
             }else{
                 // deal with seam
