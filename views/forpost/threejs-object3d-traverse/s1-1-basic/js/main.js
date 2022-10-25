5cf69714bbf351f184ebe15c831b9430f742857d
dustinpfister <dustin.pfister@gmail.com>
2022-10-25 09:50:13 -0400

    cleaned up basic example

--- views/forpost/threejs-object3d-traverse/s1-1-basic/js/main.js
+++ views/forpost/threejs-object3d-traverse/s1-1-basic/js/main.js
@@ -1,55 +1,56 @@
 
 (function () {
 
-    // Scene
-    var scene = new THREE.Scene();
-
+    // SCENE, CAMERA, RENDERER
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(50, 64 / 48, 0.1, 1000);
+    camera.position.set(8, 8, 8);
+    camera.lookAt(0, 0, 0);
+    scene.add(camera); // ADDING CAMERA OBJECT TO THE SCENE
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480, false);
+    ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+    //-------- ---------
     // ADDING GRID HELPERS TO THE SCENE
-    var helper = new THREE.GridHelper(10, 10, 'white', '#2a2a2a');
-    scene.add(helper);
-    helper = new THREE.GridHelper(10, 10, 'white', '#2a2a2a');
-    helper.rotation.z = Math.PI * 0.5;
-    scene.add(helper);
-
+    //-------- ---------
+    const helper1 = new THREE.GridHelper(10, 10, 'white', '#2a2a2a');
+    scene.add(helper1);
+    const helper2 = new THREE.GridHelper(10, 10, 'white', '#2a2a2a');
+    helper2.rotation.z = Math.PI * 0.5;
+    scene.add(helper2);
+    //-------- ---------
+    // HELPER
+    //-------- ---------
+    const getRNDAxisValue = () => {
+        return 0.5 + -5 + Math.floor(10 * Math.random());
+    }
+    //-------- ---------
     // ADDING A GROUP OF MESH OBJECTS
-    var group = new THREE.Group();
-    var i = 20;
+    //-------- ---------
+    const group = new THREE.Group();
+    let i = 20;
     while(i--){
         group.add( new THREE.Mesh( new THREE.BoxGeometry(1,1, 1), new THREE.MeshNormalMaterial() ));
     }
     scene.add( group );
-
-    // camera, renderer
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 0.5, 25);
-    camera.position.set(8, 8, 8);
-    camera.lookAt(0, 0, 0);
-    scene.add(camera); // ADDING CAMERA OBJECT TO THE SCENE
-
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-
+    //-------- ---------
     // TRAVERSING ALL OBJECTS IN THE SCENE
+    //-------- ---------
     scene.traverse(function(obj){
+        // for all grid helpers in scene
         if(obj.type === 'GridHelper'){
-            obj.material.color = new THREE.Color(0, 1, 0);
+            obj.material.color = new THREE.Color(1, 0, 0);
         }
+        // for all mesh objects in scene
         if(obj.type === 'Mesh'){
-            obj.position.x = -5 + Math.floor(10 * Math.random());
-            obj.position.z = -5 + Math.floor(10 * Math.random());
-            obj.rotation.y = Math.PI * 2 * Math.random();
-        }
-        if(obj.type === 'Group'){
-            var len = obj.children.length;
-            obj.children.forEach(function(child, i){
-                child.position.y = -5 + Math.floor( 10 * (i / len) );
-                var s = 0.25 + 1.75 * (1 - i / len);
-                child.scale.set(s, s, s);
-            });
+            obj.position.x = getRNDAxisValue();
+            obj.position.z = getRNDAxisValue();
+            obj.position.y = getRNDAxisValue();
         }
     });
-
+    //-------- ---------
+    // RENDER
+    //-------- ---------
     renderer.render(scene, camera);
-
 }
     ());
