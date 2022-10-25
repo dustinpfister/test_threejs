514224aa09f2f0a0c912b3efbcfea6fe2ebfe7e9
dustinpfister <dustin.pfister@gmail.com>
2022-10-25 13:03:25 -0400

    started a new prototype

---
+++ views/demos/r140/proto-curve-paths-tool/js/main.js
@@ -0,0 +1,45 @@
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+scene.add( new THREE.GridHelper(10, 10) );
+const camera = new THREE.PerspectiveCamera(50, 640 / 480, 0.1, 1000);
+camera.position.set(10, 10, 10);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480, false);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// HELPERS
+//-------- ----------
+// create curve helper
+const createCurve = (vStart, vEnd, vDelta) => {
+    vDelta = vDelta || new THREE.Vector3();
+    // control point is half way between vStart and vEnd
+    vControl = vStart.clone().lerp(vEnd, 0.5).add( vDelta );
+    return new THREE.QuadraticBezierCurve3(vStart, vControl, vEnd);
+};
+// create points helper
+const createPoints = (curve) => {
+    return new THREE.Points(
+         (new THREE.BufferGeometry).setFromPoints( curve.getPoints(50) ),
+         new THREE.PointsMaterial({ size: 0.4, color: new THREE.Color(0,1,0) })
+    );
+};
+
+
+const curve = new THREE.CurvePath();
+
+curve.add(createCurve(
+    new THREE.Vector3(-5, 0, 5),
+    new THREE.Vector3(-5, 0, -5),
+    new THREE.Vector3(20, -5, -4)
+));
+
+const points = createPoints(curve);
+scene.add(points);
+
+//-------- ----------
+//  RENDER
+//-------- ----------
+renderer.render(scene, camera);
