99fd4b129c339010ff71129662e383446b2e44fd
dustinpfister <dustin.pfister@gmail.com>
2022-10-25 13:26:38 -0400

    have mesh objects

--- views/demos/r140/proto-curve-paths-tool/js/main.js
+++ views/demos/r140/proto-curve-paths-tool/js/main.js
@@ -4,7 +4,7 @@
 const scene = new THREE.Scene();
 scene.add( new THREE.GridHelper(10, 10) );
 const camera = new THREE.PerspectiveCamera(50, 640 / 480, 0.1, 1000);
-camera.position.set(10, 10, 10);
+camera.position.set(-10, 10, 10);
 camera.lookAt(0, 0, 0);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480, false);
@@ -13,14 +13,12 @@ renderer.setSize(640, 480, false);
 // HELPERS
 //-------- ----------
 // get control point
-const getControlPoint = (vStart, vEnd, vDelta) => {
-    return vStart.clone().lerp(vEnd, 0.5).add( vDelta );
-};
+//const getControlPoint = (vStart, vEnd, vDelta) => {
+//    return vStart.clone().lerp(vEnd, 0.5).add( vDelta );
+//};
 // create curve helper
 const createCurve = (vStart, vEnd, vDelta) => {
     vDelta = vDelta || new THREE.Vector3();
-    // control point is half way between vStart and vEnd
-    vControl = getControlPoint(vStart, vEnd, vDelta);
     return new THREE.QuadraticBezierCurve3(vStart, vControl, vEnd);
 };
 // create points helper
@@ -30,16 +28,37 @@ const createPoints = (curve) => {
          new THREE.PointsMaterial({ size: 0.4, color: new THREE.Color(0,1,0) })
     );
 };
-
-const vStart = new THREE.Vector3(-5, 0, 5)
-const vEnd = new THREE.Vector3(-5, 0, -5)
-const vDelta = new THREE.Vector3(0, 0, 0)
-
+// create mesh helper
+const createMesh = () => {
+    const mesh = new THREE.Mesh(
+        new THREE.SphereGeometry(0.5, 20, 20),
+        new THREE.MeshNormalMaterial()
+    );
+    return mesh;
+};
+//-------- ----------
+// MESH OBJECTS
+//-------- ----------
+const mesh_start = createMesh();
+const mesh_end = createMesh();
+const mesh_control = createMesh();
+scene.add(mesh_start);
+scene.add(mesh_end);
+scene.add(mesh_control);
+//-------- ----------
+// CURVE POINTS
+//-------- ----------
+const vStart = new THREE.Vector3(-5, 0, 5);
+const vEnd = new THREE.Vector3(-5, 0, -5);
+const vControl = new THREE.Vector3(15, 0, 0);
 const curve = new THREE.CurvePath();
-curve.add( createCurve( vStart, vEnd, vDelta) );
-
+curve.add( createCurve( vStart, vEnd, vControl) );
 const points = createPoints(curve);
 scene.add(points);
+
+mesh_start.position.copy(vStart);
+mesh_end.position.copy(vEnd);
+mesh_control.position.copy(vControl);
 
 //-------- ----------
 //  RENDER
