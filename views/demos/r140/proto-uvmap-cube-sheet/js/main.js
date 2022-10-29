d263f4e57c4e3e5249d00fa33fd98682657941b9
dustinpfister <dustin.pfister@gmail.com>
2022-10-29 15:07:59 -0400

    have a cube

--- views/demos/r140/proto-uvmap-cube-sheet/js/main.js
+++ views/demos/r140/proto-uvmap-cube-sheet/js/main.js
@@ -3,11 +3,22 @@
 //-------- ----------
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50, 640 / 480, 0.1, 1000);
-camera.position.set(3, 5, 5);
-camera.lookAt(0, -0.75, 0);
+camera.position.set(2, 3, 3);
+camera.lookAt(0, 0, 0);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480, false);
 ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+//  CUBE
+//-------- ----------
+const geo = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshPhongMaterial({
+    emissive: new THREE.Color(),
+	//emissiveMap: texture
+});
+const mesh = new THREE.Mesh(geo, material);
+scene.add(mesh);
+
 //-------- ----------
 //  RENDER
 //-------- ----------
