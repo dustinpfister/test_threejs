0fa0ec3c205e8d1bc2b2fd455d512c0c04348d39
dustinpfister <dustin.pfister@gmail.com>
2022-10-29 15:47:25 -0400

    got somewhere with cube uv proto

--- views/demos/r140/proto-uvmap-cube-sheet/js/main.js
+++ views/demos/r140/proto-uvmap-cube-sheet/js/main.js
@@ -3,7 +3,7 @@
 //-------- ----------
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(50, 640 / 480, 0.1, 1000);
-camera.position.set(2, 3, 3);
+camera.position.set(3, 2, 1);
 camera.lookAt(0, 0, 0);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480, false);
@@ -20,16 +20,21 @@ canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 // draw to canvas
 let y = 0, x = 0, i = 0;
-const colors = ['red','lime', 'blue', 'pink', 'orange', 'yellow', 'white', 'black'];
+const colors = [
+    'red','lime', 'blue', 'pink', 'orange', 'yellow', 'green', 'purple',
+    'red','lime', 'blue', 'pink', 'orange', 'yellow', 'green', 'purple'
+    ];
 while(y < CELL_COUNT){
     x = 0;
     while(x < CELL_COUNT){
         i = y * CELL_COUNT + x;
-        console.log( i, x, y );
         const px = x * CANVAS_CELLSIZE;
         const py = y * CANVAS_CELLSIZE;
         ctx.fillStyle = colors[i] || 'white'
-        ctx.fillRect(px, py, CANVAS_CELLSIZE, CANVAS_CELLSIZE)
+        ctx.fillRect(px, py, CANVAS_CELLSIZE, CANVAS_CELLSIZE);
+        ctx.fillStyle = 'black';
+        ctx.textBaseline = 'top';
+        ctx.fillText(i, px, py);
         x += 1;
     }
     y += 1;
@@ -45,6 +50,24 @@ const material = new THREE.MeshPhongMaterial({
 });
 const mesh = new THREE.Mesh(geo, material);
 scene.add(mesh);
+//-------- ----------
+//  MUTATE UV ATTRIBUTE
+//-------- ----------
+
+const uv = geo.getAttribute('uv');
+console.log(uv)
+
+uv.setXY(0, 0, 0.25);
+uv.setXY(1, 0.25, 0.25);
+uv.setXY(2, 0, 0);
+uv.setXY(3, 0.25, 0);
+
+//uv.setX(1, 0);
+//uv.setY(1, 0);
+
+
+//console.log( uv.count );
+//console.log( uv.getX(1)  )
 
 //-------- ----------
 //  RENDER
