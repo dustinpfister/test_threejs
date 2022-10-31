f0670756e5e899cb5e0313e636d1aff7a3887c80
dustinpfister <dustin.pfister@gmail.com>
2022-10-31 13:19:31 -0400

    good basic example

--- views/forpost/threejs-examples-uvmap-cube-canvas-update/r0-1-basic/js/main.js
+++ views/forpost/threejs-examples-uvmap-cube-canvas-update/r0-1-basic/js/main.js
@@ -3,14 +3,14 @@
     // SCENE, CAMERA, and RENDERER
     // ---------- ---------- ----------
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, 320 / 240, 0.1, 1000);
-    camera.position.set(2, 2, 2);
+    const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+    camera.position.set(1.25, 1.25, 1.25);
     camera.lookAt(0,0,0);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(640, 480, false);
     ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
     // ---------- ---------- ----------
-    // ADD A MESH
+    // CREATE AND UPDATE MESH
     // ---------- ---------- ----------
     // create the mesh object
     let mesh = uvMapCube.create({
@@ -22,36 +22,9 @@
     scene.add(mesh);
     // I can now use the draw face method
     uvMapCube.drawFace(mesh, 'front', {i:1, sx: 0, sy: 0, sw: 32, sh: 32});
-    uvMapCube.drawFace(mesh, 'back', {i:1, sx: 0, sy: 0, sw: 16, sh: 64});
+    uvMapCube.drawFace(mesh, 'top', {i:1, sx: 0, sy: 0, sw: 128, sh: 128});
     // ---------- ----------
-    // ANIMATION LOOP
+    // RENDER
     // ---------- ----------
-    const controls = new THREE.OrbitControls(camera, renderer.domElement);
-    const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
-    FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
-    FRAME_MAX = 120;
-    let secs = 0,
-    frame = 0,
-    lt = new Date();
-    // update
-    const update = function(frame, frameMax){
-//        const degree = 360 * (frame / frameMax);
-//        mesh.rotation.x = THREE.MathUtils.degToRad(degree);
-    };
-    // loop
-    const loop = () => {
-        const now = new Date(),
-        secs = (now - lt) / 1000;
-        requestAnimationFrame(loop);
-        if(secs > 1 / FPS_UPDATE){
-            // update, render
-            update( Math.floor(frame), FRAME_MAX);
-            renderer.render(scene, camera);
-            // step frame
-            frame += FPS_MOVEMENT * secs;
-            frame %= FRAME_MAX;
-            lt = now;
-        }
-    };
-    loop();
+    renderer.render(scene, camera);
 }());
