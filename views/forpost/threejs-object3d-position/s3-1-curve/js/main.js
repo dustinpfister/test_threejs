46eba8024a323f74b773fdcc416e367d06d0c2c5
dustinpfister <dustin.pfister@gmail.com>
2022-10-31 10:23:32 -0400

    curve example looks great

--- views/forpost/threejs-object3d-position/s3-1-curve/js/main.js
+++ views/forpost/threejs-object3d-position/s3-1-curve/js/main.js
@@ -37,72 +37,44 @@
         const curve = QBC3(vs, ve, vc);
         return curve;
     };
-    // custom get alpha method
-    const getAlpha = (a1) => {
-        const a2 = THREE.MathUtils.pingpong(a1, 0.5);
-        return THREE.MathUtils.smoothstep(a2 * 2, 0, 1);
-    };
     //-------- ----------
     // CURVE
     //-------- ----------
-    const curve = QBC3(0, 0, 5, 0, 0, -5, -15, 7, 2.5);
+    const curve = QBC3(0, 0, 5, 0, 0, -5, -20, 6, 4.5);
     //-------- ----------
     // OBJECTS
     //-------- ----------
+    // mesh object that will be positioned along the curve
+    const MESH_COUNT = 10;
+    const POINT_COUNT = 100;
+    let i = 0;
+    while(i < MESH_COUNT){
+        const mesh = new THREE.Mesh(
+            new THREE.BoxGeometry(0.75, 0.75, 0.75),
+            new THREE.MeshNormalMaterial({transparent: true, opacity: 0.5}));
+        const a1 = Math.round(i / MESH_COUNT * POINT_COUNT) / (POINT_COUNT - POINT_COUNT / MESH_COUNT );
+        const a2 = a1; //0.1 + a1 * 0.9;
+        // USING THE getPoint METHOD OF THE CURVE TO GET a Vector3
+        // OBJECT THAT I CAN THEN USE WITH THE COPY METHOD TO SET
+        // THE STATE OF THE POSITION OF THIS MESH OBJECT
+        const v3 =  curve.getPoint( a2);
+        mesh.position.copy( v3 );
+        mesh.lookAt(0, 0, 0);
+        scene.add(mesh);
+        i += 1;
+    }
     // grid helper
     const grid = new THREE.GridHelper(10, 10);
     scene.add(grid);
-    // mesh object that will be positioned along the curve
-    const mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(1,1,1),
-        new THREE.MeshNormalMaterial());
-    scene.add(mesh);
-    // points
-    const POINT_COUNT = 300;
-    let i = 0, v3Array = [];
-    while(i < POINT_COUNT){
-        const a2 = getAlpha( i / POINT_COUNT);
-        v3Array.push( curve.getPoint( a2 ) );
-        i += 1;
-    }
+    // points to get an indea of what the deal is with the curve
     const points = new THREE.Points(
-        new THREE.BufferGeometry().setFromPoints( v3Array ),
-        new THREE.PointsMaterial({size: 0.075})
+        new THREE.BufferGeometry().setFromPoints( curve.getPoints(POINT_COUNT) ),
+        new THREE.PointsMaterial({size: 0.125, color: new THREE.Color(0, 1, 0)})
     );
     scene.add(points);
     //-------- ----------
-    // ANIMATION LOOP
+    // RENDER
     //-------- ----------
-    const FPS_UPDATE = 20,    // fps rate to update ( low fps for low CPU use, but choppy video )
-    FPS_MOVEMENT = 30;        // fps rate to move object by that is independent of frame update rate
-    FRAME_MAX = POINT_COUNT;
-    let secs = 0,
-    frame = 0,
-    lt = new Date();
-    // update
-    const v_start = new THREE.Vector3(0, 0, 1);
-    const v_delta = new THREE.Vector3(0, 0, 3);
-    const update = function(frame, frameMax){
-        const a1 = frame / frameMax;
-        const a2 = getAlpha(a1);
-        const v3 = curve.getPoint( a2 );
-        mesh.position.copy( v3 );
-        mesh.lookAt(0, 0, 0);
-    };
-    // loop
-    const loop = () => {
-        const now = new Date(),
-        secs = (now - lt) / 1000;
-        requestAnimationFrame(loop);
-        if(secs > 1 / FPS_UPDATE){
-            // update, render
-            update( Math.floor(frame), FRAME_MAX);
-            renderer.render(scene, camera);
-            // step frame
-            frame += FPS_MOVEMENT * secs;
-            frame %= FRAME_MAX;
-            lt = now;
-        }
-    };
-    loop();
+    renderer.render(scene, camera);
+
 }());
