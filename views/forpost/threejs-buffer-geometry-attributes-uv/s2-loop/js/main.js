3105ffecb511f9d7e7c49bc0c56e2981e6106296
dustinpfister <dustin.pfister@gmail.com>
2022-11-01 12:01:52 -0400

    updated the loop example

--- views/forpost/threejs-buffer-geometry-attributes-uv/s2-loop/js/main.js
+++ views/forpost/threejs-buffer-geometry-attributes-uv/s2-loop/js/main.js
@@ -1,8 +1,19 @@
 
 (function () {
-
-    // creating a simple canvas generated texture
-    var canvas = document.createElement('canvas'),
+    //-------- ----------
+    // SCENE, CAMERA, RENDERER
+    //-------- ----------
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
+    camera.position.set(2, 2, 2);
+    camera.lookAt(0, 0, 0);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480, false);
+    ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+    //-------- ----------
+    // CANVAS TEXTURE
+    //-------- ----------
+    const canvas = document.createElement('canvas'),
     ctx = canvas.getContext('2d');
     canvas.width = 32;
     canvas.height = 32;
@@ -17,46 +28,52 @@
     ctx.beginPath(); // draw white square
     ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.stroke();
-    var texture = new THREE.Texture(canvas);
+    const texture = new THREE.Texture(canvas);
     texture.needsUpdate = true;
-
-    // scene
-    var scene = new THREE.Scene();
-
-    // GEOMETRY - using a plane and getting the uv attribute
-    var geometry = new THREE.PlaneGeometry(1, 1);
-    var uv = geometry.getAttribute('uv');
-    var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+    //-------- ----------
+    // GEOMETRY
+    //-------- ----------
+    const geometry = new THREE.PlaneGeometry(2, 2, 1, 1);
+    const uv = geometry.getAttribute('uv');
+    const mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
                 side: THREE.DoubleSide,
                 map: texture
             }));
-    mesh.position.set(0, 0, 0);
     scene.add(mesh);
 
-    // camera, render
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
-    camera.position.set(1, 1, 1);
-    camera.lookAt(0, 0, 0);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
 
-    var frame = 0,
-    maxFrame = 300;
-    var loop = function(){
-        var per = frame / maxFrame,
-        bias = 1 - Math.abs(per - 0.5) / 0.5;
-        requestAnimationFrame(loop);
-        // MUTATING UV VALUES IN THE LOOP MAKING SURE TO SET
-        // uv.needsUpdate to true
+    // ---------- ----------
+    // ANIMATION LOOP
+    // ---------- ----------
+    const FPS_UPDATE = 12, // fps rate to update ( low fps for low CPU use, but choppy video )
+    FPS_MOVEMENT = 20;     // fps rate to move object by that is independent of frame update rate
+    FRAME_MAX = 120;
+    let secs = 0,
+    frame = 0,
+    lt = new Date();
+    // update
+    const update = function(frame, frameMax){
+        const a = frame / frameMax;
+        const bias = 1 - Math.abs(a - 0.5) / 0.5;
         uv.array[0] = -2 + 2 * bias;
         uv.array[1] = 2 - 1 * bias;
         uv.needsUpdate = true;
-        renderer.render(scene, camera);
-        frame += 1;
-        frame %= maxFrame;
+    };
+    // loop
+    const loop = () => {
+        const now = new Date(),
+        secs = (now - lt) / 1000;
+        requestAnimationFrame(loop);
+        if(secs > 1 / FPS_UPDATE){
+            // update, render
+            update( Math.floor(frame), FRAME_MAX);
+            renderer.render(scene, camera);
+            // step frame
+            frame += FPS_MOVEMENT * secs;
+            frame %= FRAME_MAX;
+            lt = now;
+        }
     };
     loop();
-
 }
     ());
