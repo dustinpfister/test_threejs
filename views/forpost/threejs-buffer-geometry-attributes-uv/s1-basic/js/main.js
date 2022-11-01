8501bee2b7291e403321eae38f0c663ad1cc63db
dustinpfister <dustin.pfister@gmail.com>
2022-11-01 11:55:33 -0400

    cleaned up basic example

--- views/forpost/threejs-buffer-geometry-attributes-uv/s1-basic/js/main.js
+++ views/forpost/threejs-buffer-geometry-attributes-uv/s1-basic/js/main.js
@@ -1,8 +1,18 @@
-
 (function () {
-
-    // creating a simple canvas generated texture
-    var canvas = document.createElement('canvas'),
+    //-------- ----------
+    // SCENE, CAMERA, RENDERER
+    //-------- ----------
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
+    camera.position.set(2, 2, 2);
+    camera.lookAt(0, 0, 0);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480, false);
+    ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+    //-------- ----------
+    // CANVAS TEXTURE
+    //-------- ----------
+    const canvas = document.createElement('canvas'),
     ctx = canvas.getContext('2d');
     canvas.width = 32;
     canvas.height = 32;
@@ -17,65 +27,50 @@
     ctx.beginPath(); // draw white square
     ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.stroke();
-    var texture = new THREE.Texture(canvas);
+    const texture = new THREE.Texture(canvas);
     texture.needsUpdate = true;
-
-    // scene
-    var scene = new THREE.Scene();
-
-    // GEOMETRY - starting with a plane
-    var geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
-
-    var uv = geometry.getAttribute('uv'),
+    //-------- ----------
+    // GEOMETRY - Mutation of a plane
+    //-------- ----------
+    const geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+    const uv = geometry.getAttribute('uv'),
     position = geometry.getAttribute('position');
-
     // the position attribute
     console.log(position.count); // 4 ( the are points or vertices )
     console.log(position.array.length); // 12 ( x, y, and z for each point )
-
     // THE UV ATTRIBUTE
     console.log(uv.count); // 4 ( the are points or vertices )
     console.log(uv.array.length); // 8 ( there is a u and v value for each point )
-
     // MUTATING THE UV VALUES
     uv.array[0] = 0.27;
     uv.array[1] = 0.73;
-
     uv.array[2] = 0.73;
     uv.array[3] = 0.73;
-
     uv.array[4] = 0.27;
     uv.array[5] = 0.27;
-
     uv.array[6] = 0.73;
     uv.array[7] = 0.27;
-
+    //-------- ----------
+    // MESH
+    //-------- ----------
     // use the geometry with a mesh
-    var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+    const mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
                 side: THREE.DoubleSide,
                 map: texture
             }));
     mesh.position.set(1, 0, 0);
     scene.add(mesh);
-
     // another mesh where I am not doing anything to the uv values
-    var geometry = new THREE.PlaneGeometry(1, 1);
-    var mesh2 = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+    const geometry2 = new THREE.PlaneGeometry(1, 1, 1, 1);
+    const mesh2 = new THREE.Mesh(geometry2, new THREE.MeshBasicMaterial({
                 side: THREE.DoubleSide,
                 map: texture
             }));
     mesh2.position.set(-1, 0, 0);
     scene.add(mesh2);
-
-    // camera, render
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
-    camera.position.set(2, 2, 2);
-    camera.lookAt(0, 0, 0);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-
+    //-------- ----------
+    // RENDER
+    //-------- ----------
     renderer.render(scene, camera);
-
 }
     ());
--- views/forpost/threejs-buffer-geometry-attributes-uv/todo.md
+++ views/forpost/threejs-buffer-geometry-attributes-uv/todo.md
@@ -1,4 +1,8 @@
 # threejs-buffer-geometry-attributes-uv
 
+## () - update examples
+* () update examples to new code style
+* () rename folders for new pattern for section folders
+
 ## ( done 09/21/2022 ) - started for post folder
 * (done) started for post folder with current set of r127 demo examples
