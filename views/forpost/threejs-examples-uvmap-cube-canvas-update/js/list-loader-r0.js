df5e9b417b528feaed0a387028f2db75d58f3045
dustinpfister <dustin.pfister@gmail.com>
2022-11-01 15:16:45 -0400

    cleaned up list

--- views/forpost/threejs-examples-uvmap-cube-canvas-update/js/list-loader-r0.js
+++ views/forpost/threejs-examples-uvmap-cube-canvas-update/js/list-loader-r0.js
@@ -1,36 +1,23 @@
 // list-loader.js - r0 - from threejs-examples-uvmap-cube-canvas-update
 (function (api) {
-
     //-------- ----------
     // MANAGER
     //-------- ----------
     const createLoadingManager = (onDone, onError) => {
         const manager = new THREE.LoadingManager();
-        // starting
-        manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
-            //console.log( 'Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
-        };
         // done
         manager.onLoad = function ( ) {
-            console.log( 'Loading complete!');
             onDone();
-        };
-        // progress
-        manager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
-            //console.log( 'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
         };
         // ERROR
         manager.onError = function ( url ) {
-            //console.log( 'There was an error loading ' + url );
-            onError();
+            onError(url);
         };
         return manager
     };
-
     //-------- ----------
     // TEXTURE LOADER
     //-------- ----------
-
     api.load = (opt) => {
         opt = opt || {};
         opt.URLS_BASE = opt.URLS_BASE || '';
@@ -60,5 +47,4 @@
             });
         });
     }
-
 }( this['listLoader'] = {} ));
