3dcbab2dd9ab0a04eee49aad8c55df8bc0dcc4fd
dustinpfister <dustin.pfister@gmail.com>
2022-11-03 10:43:46 -0400

    setting a uv attribute

--- views/forpost/threejs-examples-house/js/house-r1.js
+++ views/forpost/threejs-examples-house/js/house-r1.js
@@ -1,13 +1,12 @@
 (function (HouseMod) {
-
     // default materials
-    var materials_default = {
+    const materials_default = {
         base: new THREE.MeshStandardMaterial({
             color: 0xff0000,
             side: THREE.DoubleSide
         }),
         tri: new THREE.MeshStandardMaterial({
-            color: 0xaf0000,
+            color: 0xff0000,
             side: THREE.DoubleSide
         }),
         roof: new THREE.MeshStandardMaterial({
@@ -15,30 +14,34 @@
             side: THREE.DoubleSide
         })
     };
-
     // create a triangle part of the house
-    var HouseTriangle = function(materials){
+    const HouseTriangle = function(materials){
         materials = materials || materials_default;
-        var geometry = new THREE.BufferGeometry();
-        var vertices = new Float32Array([
+        const geometry = new THREE.BufferGeometry();
+        const vertices1 = new Float32Array([
                 -1, 0, 0,
                 0.5, 1.5, 0,
                 2, 0, 0
             ]);
-        geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+        geometry.setAttribute('position', new THREE.BufferAttribute(vertices1, 3));
         geometry.computeVertexNormals(); // compute vertex normals
         geometry.addGroup(0, 3, 0); // just one group
+        // uv attribute
+        const vertices2 = new Float32Array([
+                0, 0,
+                1, 1,
+                0, 1
+            ]);
+        geometry.setAttribute('uv', new THREE.BufferAttribute(vertices2, 2));
         return new THREE.Mesh(
             geometry, 
             materials.tri);
     };
-
     // create and return a house
     HouseMod.create = function(materials){
         materials = materials || materials_default;
         // mian house group
         var house = new THREE.Group();
-
         // base of house is just a BOX
         var base = new THREE.Mesh(new THREE.BoxGeometry(3, 2, 4), materials.base);
         house.add(base);
