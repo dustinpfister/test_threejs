954ef437f8acc40d4184e9242b447c152cf1ffb1
dustinpfister <dustin.pfister@gmail.com>
2022-11-06 11:11:03 -0500

    loading the raw html so far

--- views/demos/r146/get-audio-alphas/js/main.js
+++ views/demos/r146/get-audio-alphas/js/main.js
@@ -17,14 +17,6 @@
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
     scene.add(box);
-    // render
-    renderer.render(scene, camera);
-    // ---------- ----------
-    // CONTROLS
-    // ---------- ----------
-    const controls = new THREE.OrbitControls(camera, renderer.domElement);
-    const sample_alphas = [0.424515, 0.274725, 0.31055, 0.505405, 0.479755, 0.544555, 0.75354, 0.14410499999999998, 0.76496, 0.69522, 0.407955, 0.43923, 0.577925, 0.394345, 0.48138, 0.60859, 0.474185, 0.55078, 0.50121, 0.499275, 0.39415500000000003, 0.52537, 0.38104, 0.589525, 0.57545, 0.440005, 0.471935, 0.469535, 0.42057500000000003, 0.720395, 0.203915, 0, 0.712525, 0.7855449999999999, 0.25698, 0.540525, 0.797335, 0.502695, 0.662415, 0.462275, 0.550295, 0.466985, 0.539865, 0.555775, 0.51845, 0.35769, 0.36982000000000004, 0.53537, 0.51834, 0.47631, 0.36127, 0.496835, 0.37699499999999997, 0.45486, 0.58705, 0.700835, 0.56691, 0.502695, 0.56705,0.460595];
-const m = Math.max.apply(null, sample_alphas);
     // ---------- ----------
     // ANIMATION LOOP
     // ---------- ----------
@@ -36,11 +28,11 @@ const m = Math.max.apply(null, sample_alphas);
     lt = new Date();
     // update
     const update = function(frame, frameMax){
-        const a1 = frame / frameMax;
-        const sampleAlpha = sample_alphas[ Math.floor( sample_alphas.length * a1) ] / m;
-        const s = 0.65 + 0.35 * sampleAlpha;
-        box.scale.set(s, s, s);
-        box.rotation.y = -0.25 + 0.15 * sampleAlpha;
+        //const a1 = frame / frameMax;
+        //const sampleAlpha = sample_alphas[ Math.floor( sample_alphas.length * a1) ] / m;
+        //const s = 0.65 + 0.35 * sampleAlpha;
+        //box.scale.set(s, s, s);
+        //box.rotation.y = -0.25 + 0.15 * sampleAlpha;
     };
     // loop
     const loop = () => {
@@ -57,5 +49,16 @@ const m = Math.max.apply(null, sample_alphas);
             lt = now;
         }
     };
-    loop();
+    // ---------- ----------
+    // LOADER
+    // ---------- ----------
+    sampleAlpha.load({
+        URLS_BASE: '/demos/r146/get-audio-alphas/sample-data/',
+        URLS: ['glavin.html']
+    })
+    .then((result)=>{
+        console.log(result);
+    })
+
+
 }());
---
+++ views/demos/r146/get-audio-alphas/js/sample_alphas.js
@@ -0,0 +1,49 @@
+// sample_alphas.js - r0 - protoype 
+(function(api){
+    //-------- ----------
+    // MANAGER
+    //-------- ----------
+    const createLoadingManager = (onDone, onError) => {
+        const manager = new THREE.LoadingManager();
+        // done
+        manager.onLoad = function ( ) { onDone(); };
+        // ERROR
+        manager.onError = function ( url ) { onError(url); };
+        return manager;
+    };
+    //-------- ----------
+    // MAIN LOAD PUBLIC METHOD
+    //-------- ----------
+    api.load = function(opt){
+        opt = opt || {};
+        opt.URLS_BASE = opt.URLS_BASE || '';
+        opt.URLS = opt.URLS || [];
+
+
+const files = [];
+
+        // return a promise
+        return new Promise(function(resolve, reject){
+            const manager = createLoadingManager(
+                () => {
+                    resolve(files);
+                },
+                (url) => {
+                    reject(url);
+                }
+            );
+            const loader = new THREE.FileLoader(manager);
+            opt.URLS.forEach((url) => {
+                // set base url path
+                loader.setPath(opt.URLS_BASE);
+                // load files from base
+                loader.load(url, (html) => {
+
+files.push(html)
+
+                });
+            });
+        });
+    };
+
+}( this['sampleAlpha'] = {} ));
