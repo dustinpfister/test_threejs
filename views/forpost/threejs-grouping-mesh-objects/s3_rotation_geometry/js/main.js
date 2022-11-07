2854d3c9db1e5b3be7a6e988bf5ed500a39f98ab
dustinpfister <dustin.pfister@gmail.com>
2022-11-07 11:25:23 -0500

    using r146 for rotation section example

--- views/forpost/threejs-grouping-mesh-objects/s3_rotation_geometry/js/main.js
+++ views/forpost/threejs-grouping-mesh-objects/s3_rotation_geometry/js/main.js
@@ -1,17 +1,28 @@
 (function () {
- 
-    var createConeGroup = function (coneRotation) {
+    //-------- ----------
+    // SCENE, CAMERA, RENDERER
+    //-------- ----------
+    const scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(10, 10));
+    const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.1, 1000);
+    camera.position.set(8, 8, 8);
+    camera.lookAt(0, 0, 0);
+    const renderer = new THREE.WebGL1Renderer();
+    renderer.setSize(640, 480, false);
+    ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+    //-------- ----------
+    // HELPERS
+    //-------- ----------
+    const createConeGroup = function (coneRotation) {
         coneRotation = coneRotation === undefined ? Math.PI * 1.5 : coneRotation;
-        var group = new THREE.Group();
-        var i = 0,
-        radius = 2,
-        count = 8;
+        const group = new THREE.Group(), radius = 2,count = 8;
+        let i = 0;
         while (i < count) {
             // creating a mesh
-            var geo = new THREE.ConeGeometry(0.5, 1, 10, 10);
+            const geo = new THREE.ConeGeometry(0.5, 1, 10, 10);
             // ROTATING THE CONE GEOMERTY
             geo.rotateX(coneRotation);
-            var bx = new THREE.Mesh(
+            const bx = new THREE.Mesh(
                     geo,
                     new THREE.MeshNormalMaterial()),
             r = Math.PI * 2 / count * i;
@@ -27,29 +38,19 @@
         }
         return group;
     };
- 
-    // Scene
-    var scene = new THREE.Scene();
-    scene.add(new THREE.GridHelper(10, 10));
- 
+    //-------- ----------
+    // GROUPS
+    //-------- ----------
     // add groups
-    var group1 = createConeGroup();
+    const group1 = createConeGroup();
     group1.position.set(-4, 0, -4);
     group1.rotation.z = Math.PI / 180 * 90;
     scene.add(group1);
- 
-    var group2 = createConeGroup(Math.PI * 0.5);
+    const group2 = createConeGroup(Math.PI * 0.5);
     group2.position.set(2, 0, 2);
     scene.add(group2);
- 
-    // Camera and Render
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 50);
-    camera.position.set(8, 8, 8);
-    camera.lookAt(0, 0, 0);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
+    //-------- ----------
+    // RENDER
+    //-------- ----------
     renderer.render(scene, camera);
- 
-}
-    ());
+}());
