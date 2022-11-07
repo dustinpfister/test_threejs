e03d61059ef5de9575b378075e6a9ed931dba9d3
dustinpfister <dustin.pfister@gmail.com>
2022-11-07 11:21:22 -0500

    s2 basic example using r146

--- views/forpost/threejs-grouping-mesh-objects/s2_basic/js/main.js
+++ views/forpost/threejs-grouping-mesh-objects/s2_basic/js/main.js
@@ -1,16 +1,23 @@
 (function () {
-    // Scene
-    var scene = new THREE.Scene();
+    //-------- ----------
+    // SCENE, CAMERA, RENDERER
+    //-------- ----------
+    const scene = new THREE.Scene();
     scene.add(new THREE.GridHelper(10, 10));
- 
+    const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.1, 1000);
+    camera.position.set(8, 8, 8);
+    camera.lookAt(0, 0, 0);
+    const renderer = new THREE.WebGL1Renderer();
+    renderer.setSize(640, 480, false);
+    ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+    //-------- ----------
     // CREATING A GROUP
-    var group = new THREE.Group();
-    var i = 0,
-    radius = 2,
-    count = 8;
+    //-------- ----------
+    const group = new THREE.Group(), radius = 2, count = 8;
+    let i = 0;
     while (i < count) {
         // creating a mesh
-        var bx = new THREE.Mesh(
+        const bx = new THREE.Mesh(
                 new THREE.BoxGeometry(1, 1, 1),
                 new THREE.MeshNormalMaterial()),
         r = Math.PI * 2 / count * i;
@@ -24,19 +31,11 @@
         i += 1;
     }
     scene.add(group);
- 
     // changing position and rotation of the group
     group.position.set(-4, 0, -4);
     group.rotation.z = Math.PI / 180 * 90;
- 
-    // Camera and Render
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 50);
-    camera.position.set(8, 8, 8);
-    camera.lookAt(0, 0, 0);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
+    //-------- ----------
+    // RENDER
+    //-------- ----------
     renderer.render(scene, camera);
- 
-}
-    ());
+}());
