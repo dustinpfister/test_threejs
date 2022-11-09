6253f800d9ee7c93c00925372439adbdf6cd4068
dustinpfister <dustin.pfister@gmail.com>
2022-11-09 08:29:11 -0500

    new options for update method

--- views/forpost/threejs-grouping-mesh-objects/s2-2-georotate-update/js/main.js
+++ views/forpost/threejs-grouping-mesh-objects/s2-2-georotate-update/js/main.js
@@ -23,17 +23,20 @@
         return cone;
     };
     // update a group
-    const conCircleUpdate = (group, v3_lookat, radius) => {
+    //const coneCircleUpdate = (group, v3_lookat, radius) => {
+    const coneCircleUpdate = (group, alpha ) => {
+        alpha = alpha || 0;
         // position the mesh
         const count = group.children.length;
+        const gud = group.userData;
         let i = 0;
         while (i < count) {
             const cone = group.children[i];
             const vs = new THREE.Vector3(0, 0, 1);
             const e = new THREE.Euler(0, Math.PI / 180 * 360 * (i / count), 0 );
-            cone.position.copy(vs).applyEuler(e).multiplyScalar( radius );
+            cone.position.copy(vs).applyEuler(e).multiplyScalar( gud.radius[0] + gud.radius[1] * alpha );
             // set look at point
-            cone.lookAt(v3_lookat);
+            cone.lookAt(gud.v3_lookat);
             i += 1;
         };
     };
@@ -42,24 +45,35 @@
         opt = opt || {};
         opt.count = opt.count === undefined ? 4 : opt.count;
         const group = new THREE.Group();
+        const gud = group.userData;
+        gud.v3_lookat = opt.v3_lookat || new THREE.Vector3();
+        gud.radius = opt.radius === undefined ? [1, 1] : opt.radius;
         let i = 0;
         while (i < opt.count) {
             // creating a mesh
             const cone = createConeMesh();
             // add mesh to the group
             group.add(cone);
-            conCircleUpdate(group, new THREE.Vector3(0, 0, 0), 1);
+            coneCircleUpdate(group, 0);
             i += 1;
         }
         return group;
     };
     //-------- ----------
+    // MESH
+    //-------- ----------
+    const mesh = new THREE.Mesh( new THREE.SphereGeometry(0.5, 30, 30), new THREE.MeshNormalMaterial());
+    mesh.position.set(0, 4, 0);
+    scene.add(mesh)
+    //-------- ----------
     // GROUPS
     //-------- ----------
-    const group1 = createConeCircle({count: 6});
+    const opt = {count: 8, radius: [1, 4], v3_lookat: mesh.position };
+    const group1 = createConeCircle(opt);
+    coneCircleUpdate(group1, 0);
     scene.add(group1);
-    const group2 = createConeCircle({count: 10});
-    conCircleUpdate(group2, new THREE.Vector3(0, 5, 0), 4);
+    const group2 = createConeCircle(opt);
+    coneCircleUpdate(group2, 1);
     scene.add(group2);
     //-------- ----------
     // RENDER
