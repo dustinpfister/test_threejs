1146d8f7652551ae9ae6e8985c48dd59ae90855f
dustinpfister <dustin.pfister@gmail.com>
2022-11-13 10:54:02 -0500

    todo

--- views/forpost/threejs-materials-transparent/s4-1-transbg/js/main.js
+++ views/forpost/threejs-materials-transparent/s4-1-transbg/js/main.js
@@ -1,39 +1,40 @@
 (function () {
-    // ********** **********
+    //-------- ----------
     // SCENE, CAMERA, and RENDERER
-    // ********** **********
+    //-------- ----------
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
     camera.position.set(1.5, 1.25, 1.5);
     camera.lookAt(0, 0, 0);
+    // setting alpha option to true, and then using set clear color as needed
     const renderer = new THREE.WebGL1Renderer( { alpha: true } );
     renderer.setClearColor( 0x0000ff, 0.25 );
     renderer.setSize(640, 480, false);
     (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
-    // ********** **********
+    //-------- ----------
     // LIGHT
-    // ********** **********
+    //-------- ----------
     const dl = new THREE.DirectionalLight(0xffffff, 1);
     dl.position.set(3, 2, 1);
     scene.add(dl);
-    // ********** **********
+    //-------- ----------
     // CREATE MESH OBJECTS
-    // ********** **********
-    var createCube = function (size, x, y, z, material ) {
+    //-------- ----------
+    const createCube = function (size, x, y, z, material ) {
         var geometry = new THREE.BoxGeometry(size, size, size),
         cube = new THREE.Mesh(geometry, material);
         cube.position.set(x, y, z);
         return cube;
     };
-    var material =  new THREE.MeshPhongMaterial({
+    const material =  new THREE.MeshPhongMaterial({
         color: 'yellow',
         transparent: true,
         opacity: 0.5
     });
-    var cube1 = createCube(1, 0, 0, 0, material);
+    const cube1 = createCube(1, 0, 0, 0, material);
     scene.add(cube1);
-    // ********** **********
+    //-------- ----------
     // RENDER
-    // ********** **********
+    //-------- ----------
     renderer.render(scene, camera);
 }());
--- views/forpost/threejs-materials-transparent/todo.md
+++ views/forpost/threejs-materials-transparent/todo.md
@@ -1,7 +1,7 @@
 # threejs-materials-transparent todo list
 
-## () s4-1-transbg
-* () new section in transparent backgrounds
+## ( done 11/13/2022 ) s4-1-transbg
+* (done) new section in transparent backgrounds
 
 ## ( done 02/25/2022 ) - just have section folders for current set of examples
 * (done) section folders for each example in post
