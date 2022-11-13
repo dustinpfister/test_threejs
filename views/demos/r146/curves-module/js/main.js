520d6361468279254df281a86a8b3ee7972e73de
dustinpfister <dustin.pfister@gmail.com>
2022-11-13 15:31:16 -0500

    cuve moduel is looking good for now

--- views/demos/r146/curves-module/js/main.js
+++ views/demos/r146/curves-module/js/main.js
@@ -20,12 +20,12 @@ try{
 //-------- ----------
 // CAMERA PATHS
 //-------- ----------
-const v3Array_campos = [ 
+const v3Arrays_meshpos = [ 
     curveMod.QBV3Array([
-        [5, 0, 5, -5, 0, -5,    5,0,-5,      60]
+        [5, 0, 5, -5, 0, -5,    5,0,-5,      100]
     ]),
     curveMod.GlavinPoints2(
-        500, // number of points
+        100, // number of points
         new THREE.Vector3(-5, 0, -5), // origin
         new THREE.Vector2(0, 1),      // unit vector length range
         new THREE.Vector2(0, 360),    // a1
@@ -35,19 +35,35 @@ const v3Array_campos = [
 //-------- ----------
 // LINES AND POINTS
 //-------- ----------
-scene.add(curveMod.debugLines(v3Array_campos) )
-scene.add(curveMod.debugPoints(v3Array_campos) )
+scene.add(curveMod.debugLines(v3Arrays_meshpos) )
+scene.add(curveMod.debugPoints(v3Arrays_meshpos) )
+//-------- ----------
+// MESH
+//-------- ----------
+const mesh = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 30, 30),
+    new THREE.MeshNormalMaterial()
+);
+scene.add(mesh);
 // ---------- ----------
 // ANIMATION LOOP
 // ---------- ----------
-const FPS_UPDATE = 12, // fps rate to update ( low fps for low CPU use, but choppy video )
+const FPS_UPDATE = 30, // fps rate to update ( low fps for low CPU use, but choppy video )
 FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
-FRAME_MAX = 120;
+FRAME_MAX = 400;
 let secs = 0,
 frame = 0,
 lt = new Date();
 // update
-const update = function(frame, frameMax){};
+const update = function(frame, frameMax){
+     const a1 = frame / frameMax;
+     const arrayLen = v3Arrays_meshpos.length;
+     const arrayIndex = Math.floor( arrayLen * a1);
+     const a2 = a1 - (1 / arrayLen * arrayIndex);
+     const a3 = a2 / (1 / arrayLen);
+     const v3Array = v3Arrays_meshpos[ arrayIndex ];
+     mesh.position.copy( v3Array[ Math.floor( v3Array.length * a3 ) ] );
+};
 // loop
 const loop = () => {
     const now = new Date(),
