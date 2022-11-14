26adfb66d0a2b3e5968216367ec387e9b6587ca3
dustinpfister <dustin.pfister@gmail.com>
2022-11-14 15:15:08 -0500

    clean up

--- views/forpost/threejs-examples-guy-one/s1-demo/js/main.js
+++ views/forpost/threejs-examples-guy-one/s1-demo/js/main.js
@@ -1,41 +1,41 @@
 (function () {
-    // ********** **********
+    //-------- ----------
     // SCENE, CAMERA, and RENDERER
-    // ********** **********
-    var scene = new THREE.Scene();
+    //-------- ----------
+    const scene = new THREE.Scene();
     scene.add( new THREE.GridHelper(28, 7) );
-    var camera = new THREE.PerspectiveCamera(50, 8 / 6, .05, 100);
-    var renderer = new THREE.WebGLRenderer();
+    const camera = new THREE.PerspectiveCamera(50, 8 / 6, .05, 100);
+    const renderer = new THREE.WebGL1Renderer();
     camera.position.set(10, 10, 10);
     camera.lookAt(0, 0, 0);
     camera.add(new THREE.PointLight());
     scene.add(camera);
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-    // ********** **********
+    renderer.setSize(640, 480, false);
+    (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+    //-------- ----------
     // ADDING GUY OBJECTS TO SCENE
-    // ********** **********
-    var guy1 = new Guy();
+    //-------- ----------
+    const guy1 = new Guy();
     guy1.group.position.set(0, 3, 0)
     scene.add(guy1.group);
-    var guy2 = new Guy();
+    const guy2 = new Guy();
     guy2.group.position.set(5, 3, 0);
     scene.add(guy2.group);
-    var guy3 = new Guy();
+    const guy3 = new Guy();
     guy3.group.position.set(-5, 3, 0);
     scene.add(guy3.group);
-    // ********** **********
+    //-------- ----------
     // ANIMATION LOOP
-    // ********** **********
-    var frame = 0,
-    maxFrame = 200,
+    //-------- ----------
+    let frame = 0,
     lt = new Date();
-    var loop = function () {
-        var now = new Date(),
+    const maxFrame = 200;
+    const loop = function () {
+        const now = new Date(),
         secs = (now - lt) / 1000;
         requestAnimationFrame(loop);
         if (secs > 0.05) {
-            var per = frame / maxFrame,
+            const per = frame / maxFrame,
             bias = Math.abs(.5 - per) / .5,
             r = Math.PI * 2 * per;
             // guy1 walks around, and moves head
@@ -62,6 +62,5 @@
         }
     };
     loop();
-
 }
     ());
