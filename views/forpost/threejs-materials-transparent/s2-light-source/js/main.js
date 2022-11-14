1496ce05720659b88774a66799a3b90c5ffb0170
dustinpfister <dustin.pfister@gmail.com>
2022-11-14 09:00:35 -0500

    updated source code examples

--- views/forpost/threejs-materials-transparent/s2-light-source/js/main.js
+++ views/forpost/threejs-materials-transparent/s2-light-source/js/main.js
@@ -1,26 +1,30 @@
 (function () {
-    // ********** **********
-    // SCENE, CAMERA, RENDERER, and LIGHT
-    // ********** **********
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
-    camera.position.set(0.9, 0, 3.5);
-    camera.lookAt(-1, 0, 0);
-    var renderer = new THREE.WebGLRenderer();
-    document.getElementById('demo').appendChild(renderer.domElement);
-    var pointLight = new THREE.PointLight(0xffffff);
-    pointLight.position.set(2, -5, 5);
-    scene.add(pointLight);
-    // ********** **********
+    //-------- ----------
+    // SCENE, CAMERA, and RENDERER
+    //-------- ----------
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
+    camera.position.set(1, 0, 5);
+    camera.lookAt(0, 0, 0);
+    const renderer = new THREE.WebGL1Renderer();
+    renderer.setSize(640, 480, false);
+    (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+    //-------- ----------
+    // LIGHT
+    //-------- ----------
+    const pl = new THREE.PointLight(0xffffff);
+    pl.position.set(2, -5, 5);
+    scene.add(pl);
+    //-------- ----------
     // CREATE MESH OBJECTS
-    // ********** **********
-    var createCube = function (size, material, x, y, z) {
-        var geometry = new THREE.BoxGeometry(size, size, size),
+    //-------- ----------
+    const createCube = function (size, material, x, y, z) {
+        const geometry = new THREE.BoxGeometry(size, size, size),
         cube = new THREE.Mesh(geometry, material);
         cube.position.set(x, y, z);
         return cube;
     };
-    var materials = {};
+    const materials = {};
     materials.sand = new THREE.MeshStandardMaterial({
         color: 'yellow'
     });
@@ -29,12 +33,12 @@
         transparent: true,
         opacity: 0.4
     });
-    var glassCube = createCube(1, materials.glass, 0, 0, 2),
-    cube = createCube(1, materials.sand, 0, 0, 0);
+    const glassCube = createCube(1, materials.glass, 0, 0, 2);
+    const cube = createCube(1, materials.sand, 0, 0, 0);
     scene.add(glassCube);
     scene.add(cube);
-    // ********** **********
+    //-------- ----------
     // RENDER
-    // ********** **********
+    //-------- ----------
     renderer.render(scene, camera);
 }());
