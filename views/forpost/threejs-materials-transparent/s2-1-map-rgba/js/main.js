bbc1c95188ccef3d3c70c306913d53d96d0d45d8
dustinpfister <dustin.pfister@gmail.com>
2022-11-14 09:47:55 -0500

    example looks good

--- views/forpost/threejs-materials-transparent/s2-1-map-rgba/js/main.js
+++ views/forpost/threejs-materials-transparent/s2-1-map-rgba/js/main.js
@@ -3,31 +3,23 @@
     // SCENE, CAMERA, and RENDERER
     //-------- ----------
     const scene = new THREE.Scene();
+    scene.add(new THREE.GridHelper(10, 10));
     const camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
-    camera.position.set(1, 0, 5);
+    camera.position.set(1.25, 1, 1.75);
     camera.lookAt(0, 0, 0);
     const renderer = new THREE.WebGL1Renderer();
     renderer.setSize(640, 480, false);
     (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
     //-------- ----------
-    // LIGHT
-    //-------- ----------
-    const pl = new THREE.PointLight(0xffffff);
-    pl.position.set(2, -5, 5);
-    scene.add(pl);
-    const al = new THREE.AmbientLight(0xffffff);
-    al.intensity = 0.4;
-    scene.add(al);
-    //-------- ----------
     // HELPERS
     //-------- ----------
-    const createCube = function (size, material, x, y, z) {
+    const createCube = (size, material, x, y, z) => {
         const geometry = new THREE.BoxGeometry(size, size, size),
         cube = new THREE.Mesh(geometry, material);
         cube.position.set(x, y, z);
         return cube;
     };
-    const createCanvasTexture = function (draw, size) {
+    const createCanvasTexture = (draw, size) => {
         const canvas = document.createElement('canvas'),
         ctx = canvas.getContext('2d');
         canvas.width = size || 64;
@@ -36,34 +28,43 @@
         return new THREE.CanvasTexture(canvas);
     };
     //-------- ----------
-    // CREATE MESH OBJECTS WITH CANVAS ALPHA MAP TEXTURE
+    // TEXTURE
     //-------- ----------
-    const alphaMap = createCanvasTexture(function (ctx, canvas) {
-            // drawing gray scale areas
-            ctx.fillStyle = '#f0f0f0';
-            ctx.fillRect(0, 0, 32, 32);
-            ctx.fillStyle = '#000000';
-            ctx.fillRect(32, 0, 32, 32);
-            ctx.fillStyle = '#f0f0f0';
-            ctx.fillRect(0, 32, 32, 32);
-            ctx.fillStyle = '#f0f0f0';
-            ctx.fillRect(32, 32, 32, 32);
-        });
-    const materials = {};
-    materials.sand = new THREE.MeshStandardMaterial({
-            color: 'yellow'
-        });
-    materials.glass = new THREE.MeshStandardMaterial({
-            color: 'cyan',
-            alphaMap: alphaMap, // using an alpha map
-            side: THREE.DoubleSide,
-            depthFunc: THREE.AlwaysDepth,
-            transparent: true,
-            opacity: 0.2
-        });
-    const glassCube = createCube(1, materials.glass, 0, 0, 2);
-    const cube = createCube(1, materials.sand, 0, 0, 0);
-    scene.add(glassCube);
+    const texture_map = createCanvasTexture( (ctx, canvas) => {
+        // USING rgba TO SET STYLE
+        ctx.fillStyle = 'rgba(64,64,64,1)';
+        ctx.fillRect(0,0, canvas.width, canvas.height);
+        // CLEAR RECT CAN BE USED TO SET AN AREA AS TRANSPARENT
+        // THEN FILL WITH AN rgba STYLE
+        ctx.clearRect(6, 6, 24, 24);
+        ctx.fillStyle = 'rgba(0,255,255,0.4)';
+        ctx.fillRect(6, 6, 24, 24);
+        // can also rotate
+        ctx.save();
+        ctx.translate(34 + 7, 34 + 7);
+        ctx.rotate( Math.PI / 180 * 45 );
+        ctx.clearRect(-12, -12, 24, 24);
+        ctx.fillStyle = 'rgba(0,255,255,0.4)';
+        ctx.fillRect(-12, -12, 24, 24);
+        ctx.restore();
+        // FRAME
+        ctx.strokeStyle = 'rgba(0,0,0,1)';
+        ctx.strokeRect(2, 2, canvas.width - 4, canvas.height - 4)
+    });
+    //-------- ----------
+    // MATERIAL
+    //-------- ----------
+    const material =  new THREE.MeshBasicMaterial({
+        color: new THREE.Color(1, 1, 1),
+        map: texture_map,
+        side: THREE.DoubleSide,
+        transparent: true,
+        opacity: 0.8
+    });
+    //-------- ----------
+    // MESH
+    //-------- ----------
+    const cube = createCube(1, material, 0, 0, 0);
     scene.add(cube);
     //-------- ----------
     // RENDER
