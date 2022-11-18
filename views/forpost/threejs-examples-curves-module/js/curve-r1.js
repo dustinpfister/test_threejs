e938261b300034222b21c8558560fcea8ba4bea6
dustinpfister <dustin.pfister@gmail.com>
2022-11-18 12:54:40 -0500

    started an r1 module of curves.js

---
+++ views/forpost/threejs-examples-curves-module/js/curve-r1.js
@@ -0,0 +1,189 @@
+// curve.js - r0 - r146 prototype
+(function(api){
+    //-------- ----------
+    // HELPERS
+    //-------- ----------
+    // for path data helper used by QBCurvePath and QBV3Array
+    const forPathData = (data, collection, forCurve) => {
+        collection = collection || new THREE.CurvePath();
+        forCurve = forCurve || function(a, curve, curvePath, data){
+            curvePath.add(curve);
+        };
+        data.forEach( ( a ) => {
+            const curve = api.QBDelta.apply(null, a.slice(0, a.length - 1));
+            forCurve(a, curve, collection, data);
+        });
+        return collection;
+    };
+    const createAlphaCurve = (grc_points) => {
+        let i = 0, len = grc_points.length;
+        const data = [];
+        while(i < len - 1){
+            const s = grc_points[i];
+            const e = grc_points[i + 1];
+            data.push([ 0, s[0], 0,   0, e[0], 0,   0, s[1], 0 ]);
+            i += 1;
+        }
+        return curveMod.QBCurvePath(data);
+    };
+    //-------- ----------
+    // RETURN CURVE
+    //-------- ----------
+    // just a short hand for THREE.QuadraticBezierCurve3
+    api.QBC3 = function(x1, y1, z1, x2, y2, z2, x3, y3, z3){
+        let vs = x1;
+        let ve = y1;
+        let vc = z1;
+        if(arguments.length === 9){
+            vs = new THREE.Vector3(x1, y1, z1);
+            ve = new THREE.Vector3(x2, y2, z2);
+            vc = new THREE.Vector3(x3, y3, z3);
+        }
+        return new THREE.QuadraticBezierCurve3( vs, vc, ve );
+    };
+    // QBDelta helper using QBC3
+    // this works by giving deltas from the point that is half way between
+    // the two start and end points rather than a direct control point for x3, y3, and x3
+    api.QBDelta = function(x1, y1, z1, x2, y2, z2, x3, y3, z3) {
+        const vs = new THREE.Vector3(x1, y1, z1);
+        const ve = new THREE.Vector3(x2, y2, z2);
+        // deltas
+        const vDelta = new THREE.Vector3(x3, y3, z3);
+        const vc = vs.clone().lerp(ve, 0.5).add(vDelta);
+        const curve = api.QBC3(vs, ve, vc);
+        return curve;
+    };
+    // return a path curve by just calling the for path data helper with default options
+    api.QBCurvePath = function(data){
+        return forPathData(data);
+    };
+    //-------- ----------
+    // RETURN V3ARRAY
+    //-------- ----------
+    // QBV3Array
+    api.QBV3Array = function(data) {
+        const v3Array = [];
+        forPathData(data, v3Array, function(a, curve, v3Array){
+            v3Array.push( curve.getPoints( a[9]) );
+        });
+        return v3Array.flat();
+    };
+    // Glavin Points are random points used for camera pos
+    api.GlavinPoints2 = (count, origin, VULR, A1, A2 ) => {
+        count = count === undefined ? 50 : count;
+        origin = origin === undefined ? new THREE.Vector3() : origin;
+        VULR = VULR === undefined ? new THREE.Vector2(0, 1) : VULR; // Max and min unit vector length
+        A1 = A1 === undefined ? new THREE.Vector2(0, 360) : A1;
+        A2 = A2 === undefined ? new THREE.Vector2(-90, 90) : A2;
+        const v3Array = [];
+        let i  = 0;
+        while(i < count){
+            // random euler
+            const e = new THREE.Euler();
+            e.x = Math.PI / 180 * ( A1.x + ( A1.y - A1.x ) * THREE.MathUtils.seededRandom() );
+            e.y = Math.PI / 180 * ( A2.x + ( A2.y - A2.x ) * THREE.MathUtils.seededRandom() );
+            // random unit length
+            const ul = VULR.x + ( VULR.y - VULR.x ) * THREE.MathUtils.seededRandom();
+            // v3 is a random dir and unit length from origin
+            const v = origin.clone().add( new THREE.Vector3( 0, 0, 1).applyEuler(e).multiplyScalar(ul) )
+            v3Array.push(v);
+            i += 1;
+        }
+        return v3Array;
+    };
+    //-------- ----------
+    // ALPHA FUNCTION
+    //-------- ----------
+    api.createAlphaFunciton1 = ( grc_points, clamp ) => {
+        clamp = clamp === undefined ? true : clamp;
+        const curve = createAlphaCurve(grc_points);
+        return function(givenAlpha){
+            let a = curve.getPoint(givenAlpha).y;
+            if(clamp){
+                a = THREE.MathUtils.clamp(a, 0, 1);
+            }
+            return a;
+        };
+    };
+    api.createAlphaFunciton2 = ( grc_points, clamp ) => {
+        clamp = clamp === undefined ? true : clamp;
+        // use each path by itself
+        const cp = createAlphaCurve(grc_points);
+        return function(alpha){
+            alpha = alpha === 1 ? 0.9999999999 : alpha;
+            const cLen = cp.curves.length;
+            const curveIndex = Math.floor( cLen * alpha);
+            const cc = cp.curves[ curveIndex];
+            const a_cc = alpha %  ( 1 / cLen ) * ( cLen );
+            const v3 = cc.getPoint( a_cc );
+            let a = v3.y;
+            if(clamp){
+                a = THREE.MathUtils.clamp(a, 0, 0.9999999999);
+            }
+            return a;
+        };
+    };
+    //-------- ----------
+    // DEBUG HELPERS
+    //-------- ----------
+    // debug lines
+    api.debugLines = ( arrays ) => {
+        const line_debug = new THREE.Line(
+            new THREE.BufferGeometry().setFromPoints(arrays.flat()),
+            new THREE.LineBasicMaterial({ transparent: true, opacity: 0.3})
+        );
+        return line_debug;
+    };
+    // debug points
+    api.debugPoints = ( arrays ) => {
+        const points_debug = new THREE.Points(
+            new THREE.BufferGeometry().setFromPoints(arrays.flat()),
+            new THREE.PointsMaterial({ size: 0.25, color: new THREE.Color(0, 1, 0)})
+        );
+        return points_debug;
+    };
+    // debug points for a curve
+    api.debugPointsCurve = ( curve, opt ) => {
+        opt = opt || {};
+        opt.count = opt.count === undefined ? 100 : opt.count;
+        opt.getAlpha = opt.getAlpha || function(alpha){ return alpha; };
+        opt.size = opt.size === undefined ? 0.2 : opt.size;
+        opt.color = opt.color === undefined ? new THREE.Color(0,1,1) : opt.color;
+        const v3Array = [];
+        let i = 0;
+        while(i < opt.count){
+            v3Array.push( curve.getPoint( opt.getAlpha(i / opt.count ) ) );
+            i += 1;
+        }
+        const points_debug = new THREE.Points(
+            new THREE.BufferGeometry().setFromPoints(v3Array),
+            new THREE.PointsMaterial({ size: opt.size, color: opt.color})
+        );
+        return points_debug;
+    };
+    // debug and alpha function
+    api.debugAlphaFunction = (alphaFunc, opt) => {
+        opt = opt || {};
+        opt.count = opt.count === undefined ? 200 : opt.count;
+        opt.sx = opt.sx === undefined ? -5 : opt.sx;
+        opt.sz = opt.sz === undefined ? 5 : opt.sz;
+        opt.w = opt.w === undefined ? 10 : opt.w;
+        opt.h = opt.h === undefined ? -10 : opt.h;
+        opt.size = opt.size === undefined ? 0.2 : opt.size;
+        opt.color = opt.color === undefined ? new THREE.Color(0,1,1) : opt.color;
+        const v3Array = [];
+        let i = 0;
+        while(i < opt.count){
+            const a1 = i / opt.count;
+            const x = opt.sx + a1 * opt.w;
+            const z = opt.sz + alphaFunc(a1) * opt.h;
+            v3Array.push( new THREE.Vector3( x, 0 , z) );
+            i += 1;
+        }
+        const points = new THREE.Points(
+            new THREE.BufferGeometry().setFromPoints( v3Array ),
+            new THREE.PointsMaterial({ size: opt.size, color: opt.color})
+        );
+        return points;
+    };
+}(this['curveMod'] = {} ));
