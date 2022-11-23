749d9be3f2adf1109165d7a6b6ec19087556de71
dustinpfister <dustin.pfister@gmail.com>
2022-11-23 14:38:55 -0500

    s2-animation loop example updated

--- views/forpost/threejs-fog/s2-animation-loop/js/main.js
+++ views/forpost/threejs-fog/s2-animation-loop/js/main.js
@@ -1,60 +1,70 @@
 (function () {
- 
-    // SCNEN
-    var scene = new THREE.Scene();
-    scene.add( new THREE.GridHelper(8, 8, 0xffffff, 0x000000))
- 
+    //-------- ----------
+    // SCENE, CAMERA, RENDERER
+    //-------- ----------
+    const scene = new THREE.Scene();
+    scene.add( new THREE.GridHelper(8, 8, 0xffffff, 0x000000));
+    const camera = new THREE.PerspectiveCamera(75, 320 / 240, .025, 20);
+    camera.position.set(1.5, 0.75, 1.5);
+    camera.lookAt(0, 0, 0);
+    const renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer() : new THREE.WebGLRenderer;
+    renderer.setSize(640, 480, false);
+    (document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+    //-------- ----------
+    // LIGHT
+    //-------- ----------
+    // adding a point light to the camera
+    const light = new THREE.PointLight(0xffffff);
+    light.position.y = 0.5;
+    camera.add(light);
+    scene.add(camera);
+    //-------- ----------
     // ADDING BACKGROUND AND FOG
+    //-------- ----------
     fogColor = new THREE.Color(0x00af00);
     scene.background = fogColor;
     scene.fog = new THREE.FogExp2(fogColor, 0.5);
- 
     // Use a Material that SUPPORTS FOG
     // when making a Mesh such as the standard material
-    var mesh = new THREE.Mesh(
+    const mesh = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshStandardMaterial({
                 color: 0xff0000
             }));
     scene.add(mesh);
- 
-    // Camera
-    var camera = new THREE.PerspectiveCamera(75, 320 / 240, .025, 20);
-    camera.position.set(2, 0.75, 2);
-    camera.lookAt(0, 0, 0);
-    // adding a point light to the camera
-    var light = new THREE.PointLight(0xffffff);
-    light.position.y = 0.5;
-    camera.add(light);
-    scene.add(camera);
-    // Render
-    const renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer() : new THREE.WebGLRenderer;
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
- 
-    // Loop
-    var frame = 0,
-    maxFrame = 500,
-    target_fps = 30;
+    // ---------- ----------
+    // ANIMATION LOOP
+    // ---------- ----------
+    const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+    FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+    FRAME_MAX = 400;
+    let secs = 0,
+    frame = 0,
     lt = new Date();
-    var loop = function () {
-        var per = frame / maxFrame,
-        bias = Math.abs(.5 - per) / .5,
-        now = new Date(),
+    // update
+    const update = function(frame, frameMax){
+        const per = frame / frameMax,
+        bias = 1 - Math.abs(0.5 - per) / 0.5;
+        mesh.position.z = 1 + 4 * bias;
+        mesh.rotation.x = Math.PI * 2 * 4 * per;
+        mesh.rotation.y = Math.PI * 2 * 2 * per;
+        camera.lookAt(mesh.position);
+    };
+    // loop
+    const loop = () => {
+        const now = new Date(),
         secs = (now - lt) / 1000;
         requestAnimationFrame(loop);
-        if (secs > 1 / target_fps) {
-            mesh.position.z = 1 + 4 * bias;
-            mesh.rotation.x = Math.PI * 2 * 4 * per;
-            mesh.rotation.y = Math.PI * 2 * 2 * per;
-            camera.lookAt(mesh.position);
+        if(secs > 1 / FPS_UPDATE){
+            // update, render
+            update( Math.floor(frame), FRAME_MAX);
             renderer.render(scene, camera);
-            frame += target_fps * secs;
-            frame = frame % maxFrame;
+            // step frame
+            frame += FPS_MOVEMENT * secs;
+            frame %= FRAME_MAX;
             lt = now;
         }
     };
- 
     loop();
 }
     ());
