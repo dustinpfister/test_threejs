b805581879d1ea8da6803b7957e50ec17f463f95
dustinpfister <dustin.pfister@gmail.com>
2022-11-26 15:26:01 -0500

    updated loop example

--- views/forpost/threejs-vector3-apply-axis-angle/s2-1-loop/js/main.js
+++ views/forpost/threejs-vector3-apply-axis-angle/s2-1-loop/js/main.js
@@ -1,42 +1,41 @@
-
 (function () {
-
-    // scene
-    var scene = new THREE.Scene();
-    scene.add(new THREE.GridHelper(6, 6));
-
-    // mesh
-    var mesh = new THREE.Mesh(
+    // ---------- ---------- ----------
+    // SCENE, CAMERA, and RENDERER
+    // ---------- ---------- ----------
+    const scene = new THREE.Scene();
+    scene.add( new THREE.GridHelper(10, 10) );
+    const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+    camera.position.set(4, 4, 4);
+    camera.lookAt(0,0,0);
+    const renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer() : new THREE.WebGLRenderer;
+    renderer.setSize(640, 480, false);
+    ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+    // ---------- ---------- ----------
+    // MESH
+    // ---------- ---------- ----------
+    const mesh = new THREE.Mesh(
             new THREE.ConeGeometry(0.5, 1, 30, 30),
             new THREE.MeshNormalMaterial());
     mesh.geometry.rotateX(Math.PI * 0.5);
     mesh.position.set(1, 0, 1);
     scene.add(mesh);
-
-    // camera, render
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
-    camera.position.set(5, 5, 5);
-    camera.lookAt(0, 0, 0);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-
+    // ---------- ---------- ----------
     // LOOP
-    var degree = 0,
-    v = new THREE.Vector3(0, 1, 0),
-    lt = new Date(),
-    fps = 30;
+    // ---------- ---------- ----------
+    const v = new THREE.Vector3(0, 1, 0);
+    const fps = 30;
+    let lt = new Date();
     // update method
-    var update = function (secs) {
+    const update = function (secs) {
         v.x += 0.25 * secs;
         v.x %= 1;
-        degree = 45 * secs;
+        const degree = 45 * secs;
         mesh.position.applyAxisAngle(v, Math.PI / 180 * degree);
         mesh.lookAt(0, 0, 0);
     };
     // loop method
-    var loop = function () {
-        var now = new Date(),
+    const loop = function () {
+        const now = new Date(),
         secs = (now - lt) / 1000;
         requestAnimationFrame(loop);
         if (secs > 1 / fps) {
@@ -46,6 +45,5 @@
         }
     };
     loop();
-
 }
     ());
--- views/forpost/threejs-vector3-apply-axis-angle/todo.md
+++ views/forpost/threejs-vector3-apply-axis-angle/todo.md
@@ -1,7 +1,10 @@
 # threejs-vector3-apply-axis-angle todo list
 
-## () - update examples
-* () update source code examples to new standards for r146
+## () s2-2-loop-group
+* () new animaiton loop example that will be video1 for this post
+
+## ( done 11/26/2022 ) - update examples
+* (done) update source code examples to new standards for r146
 
 ## ( done 11/26/2022 ) - start for post folder
 * (done) start for post folder for this post using r127 demos
