c934103f817957dab4f1b7a0d35838688dfa19b8
dustinpfister <dustin.pfister@gmail.com>
2022-11-26 15:20:12 -0500

    basic example updated

--- views/forpost/threejs-vector3-apply-axis-angle/s1-1-basic/js/main.js
+++ views/forpost/threejs-vector3-apply-axis-angle/s1-1-basic/js/main.js
@@ -1,32 +1,33 @@
-
 (function () {
-
-    // scene
+    // ---------- ---------- ----------
+    // SCENE, CAMERA, and RENDERER
+    // ---------- ---------- ----------
     const scene = new THREE.Scene();
-    scene.add( new THREE.GridHelper(6, 6) );
-    const camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
-    camera.position.set(5, 5, 5);
-    camera.lookAt(0, 0, 0);
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-
-    // mesh
-    var mesh = new THREE.Mesh(
+    scene.add( new THREE.GridHelper(10, 10) );
+    const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+    camera.position.set(2, 2, 2);
+    camera.lookAt(0,0,0);
+    const renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer() : new THREE.WebGLRenderer;
+    renderer.setSize(640, 480, false);
+    ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+    // ---------- ---------- ----------
+    // MESH
+    // ---------- ---------- ----------
+    const mesh = new THREE.Mesh(
             new THREE.ConeGeometry(0.5, 1, 30, 30),
             new THREE.MeshNormalMaterial());
-
     mesh.geometry.rotateX(Math.PI * 0.5);
     mesh.position.set(1, 0, 1);
-    //mesh.lookAt(0, 3, 0);
     scene.add(mesh);
-
-    var v = new THREE.Vector3(0, 1, 0);
+    // ---------- ---------- ----------
+    // USING APPLY AXIS ANGLE
+    // ---------- ---------- ----------
+    const v = new THREE.Vector3(0, 1, 0);
     mesh.position.applyAxisAngle(v, Math.PI / 180 * 180);
-
-    console.log(mesh.position.clone().round()); // {x: -1, y: 0, z: -1}
-
+    console.log( mesh.position.clone().round() ); // {x: -1, y: 0, z: -1}
+    // ---------- ---------- ----------
+    // RENDER
+    // ---------- ---------- ----------
     renderer.render(scene, camera);
-
 }
     ());
