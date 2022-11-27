a75f22757b782332e04eb95e8c9c27da85392ff3
dustinpfister <dustin.pfister@gmail.com>
2022-11-27 15:23:39 -0500

    smooth step get alpha

--- views/forpost/threejs-object3d-position/s3-3-curve-getalpha/js/main.js
+++ views/forpost/threejs-object3d-position/s3-3-curve-getalpha/js/main.js
@@ -10,8 +10,7 @@
     renderer.setSize(640, 480, false);
     ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
     //-------- ----------
-    // HELPERS - from 'frink3' project in videoground-beta-world
-    //           ( https://github.com/dustinpfister/videoground-beta-world )
+    // HELPERS
     //-------- ----------
     // just a short hand for THREE.QuadraticBezierCurve3
     const QBC3 = function(x1, y1, z1, x2, y2, z2, x3, y3, z3){
@@ -25,10 +24,32 @@
         }
         return new THREE.QuadraticBezierCurve3( vs, vc, ve );
     };
+    // create curve points
+    const createCurvePoints = (curve, point_count, point_size, point_color, get_alpha) => {
+        point_count = point_count === undefined ? 100 : point_count;
+        point_size = point_size === undefined ? 1 : point_size;
+        point_color = point_color || new THREE.Color(1, 1, 1);
+        get_alpha = get_alpha || function(a1){ return a1; };
+        const v3_array = [];
+        let i = 0;
+        while(i < point_count){
+            v3_array.push( curve.getPoint( get_alpha(i / point_count) ));
+            i += 1;
+        }
+        const points = new THREE.Points(
+            new THREE.BufferGeometry().setFromPoints( v3_array ),
+            new THREE.PointsMaterial({size: point_size, color: point_color})
+        );
+        return points;
+    };
+    // smooth get alpha
+    const getAlphaSmooth = (a1) => {
+        return THREE.MathUtils.smoothstep(a1, 0, 1);
+    };
     //-------- ----------
     // CURVE
     //-------- ----------
-    const curve = QBC3(0, 0, 5, 0, 0, -5, 0, 0, 0);
+    const curve1 = QBC3(2, 0, 5, 2, 0, -5, 6, 0, 0);
     //-------- ----------
     // OBJECTS
     //-------- ----------
@@ -41,7 +62,7 @@
             new THREE.BoxGeometry(0.75, 0.75, 0.75),
             new THREE.MeshNormalMaterial({transparent: true, opacity: 0.5}));
         const a1 = Math.round(i / MESH_COUNT * POINT_COUNT) / (POINT_COUNT - POINT_COUNT / MESH_COUNT );
-        const v3 =  curve.getPoint(a1);
+        const v3 =  curve1.getPoint(getAlphaSmooth(a1));
         mesh.position.copy( v3 );
         mesh.lookAt(0, 0, 0);
         scene.add(mesh);
@@ -50,12 +71,9 @@
     // grid helper
     const grid = new THREE.GridHelper(10, 10);
     scene.add(grid);
-    // points to get an indea of what the deal is with the curve
-    const points = new THREE.Points(
-        new THREE.BufferGeometry().setFromPoints( curve.getPoints(POINT_COUNT) ),
-        new THREE.PointsMaterial({size: 0.125, color: new THREE.Color(0, 1, 0)})
-    );
-    scene.add(points);
+
+    scene.add( createCurvePoints(curve1, 50, 0.125, new THREE.Color(0,1,0), getAlphaSmooth ) )
+
     //-------- ----------
     // RENDER
     //-------- ----------
