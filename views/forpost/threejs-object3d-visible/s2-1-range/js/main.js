bd638683ac28f9eb2cae72f0f31fd4a52476db59
dustinpfister <dustin.pfister@gmail.com>
2022-11-29 10:58:58 -0500

    code updated to work well with r146

--- views/forpost/threejs-object3d-visible/s2-1-range/js/main.js
+++ views/forpost/threejs-object3d-visible/s2-1-range/js/main.js
@@ -1,37 +1,46 @@
-var scene = new THREE.Scene();
-scene.add(new THREE.GridHelper(10, 10));
-
-// some values
-var near = 8.25,
-far = 20,
-maxDist = 10;
-
-// CREATING CAMERA WITH NEAR AND FAR VALUES
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 8.25, far);
-camera.position.set(8, 8, 8);
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(20, 20))
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+camera.position.set(8,8,8);
 camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
-
-var box = new THREE.Mesh(
+const renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer() : new THREE.WebGLRenderer;
+renderer.setSize(640, 480, false);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// MESH
+//-------- ----------
+const box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
 scene.add(box);
-
-var lt = new Date(),
+//-------- ----------
+// CAMERA STATE
+//-------- ----------
+let near = 8.25,
+far = 20,
+maxDist = 10;
+camera.near = near;
+camera.far = far;
+camera.updateProjectionMatrix();
+//-------- ----------
+// LOOP
+//-------- ----------
+let lt = new Date(),
 frame = 0,
 maxFrame = 240,
 fps = 15;
-var loop = function () {
-    var now = new Date(),
+const loop = function () {
+    const now = new Date(),
     per = frame / maxFrame,
     bias = 1 - Math.abs(per - 0.5) / 0.5,
     secs = (now - lt) / 1000;
     requestAnimationFrame(loop);
     if (secs > 1 / fps) {
         // CHANGING POSITION OF BOX SO THAT IT GOES IN ANY OUT OF THE RENDER RANGE OF THE CAMERA
-        var dist = maxDist - (maxDist * 2) * bias;
+        const dist = maxDist - (maxDist * 2) * bias;
         box.position.x = dist * -1;
         box.position.z = dist * -1;
         renderer.render(scene, camera);
--- views/forpost/threejs-object3d-visible/s3-1-remove/js/main.js
+++ views/forpost/threejs-object3d-visible/s3-1-remove/js/main.js
@@ -1,26 +1,32 @@
-var scene = new THREE.Scene();
-// Creating and adding the box to the scene
-var box = new THREE.Mesh(
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
+const renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer() : new THREE.WebGLRenderer;
+renderer.setSize(640, 480, false);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// MESH
+//-------- ----------
+const box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
 scene.add(box);
-// camera and renderer
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(1, 1, 1);
-camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
-
-var lt = new Date(),
+//-------- ----------
+// LOOP
+//-------- ----------
+let lt = new Date(),
 fps = 2;
-var loop = function () {
-    var now = new Date(),
+const loop = function () {
+    const now = new Date(),
     secs = (now - lt) / 1000;
     requestAnimationFrame(loop);
     if (secs > 1 / fps) {
         // ADDING AND REMOVING CHILD
-        var child = scene.getObjectById(box.id);
+        const child = scene.getObjectById(box.id);
         if (child) {
             scene.remove(box);
         } else {
