078248e3502476e3b4ac0e47d580bf3eeb6388a9
dustinpfister <dustin.pfister@gmail.com>
2022-11-30 11:52:39 -0500

    code examples updated

--- views/forpost/threejs-face3-color/s2-existing/js/main.js
+++ views/forpost/threejs-face3-color/s2-existing/js/main.js
@@ -1,11 +1,16 @@
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
 camera.position.set(1, 1.2, 1);
 camera.lookAt(0, 0, 0);
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(320, 240);
-document.getElementById('demo').appendChild(renderer.domElement);
-
+renderer.setSize(640, 480);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// FACE3
+//-------- ----------
 // a box geometry
 var box = new THREE.BoxGeometry(1, 1, 1);
 // for each face3
@@ -21,10 +26,15 @@ box.faces.forEach(function (face3, i) {
         face3.color = new THREE.Color(0xffffff);
     }
 });
-
+//-------- ----------
+// MESH
+//-------- ----------
 var mesh = new THREE.Mesh(box,
         new THREE.MeshBasicMaterial({
             vertexColors: THREE.FaceColors
         }));
 scene.add(mesh);
+//-------- ----------
+// RENDER
+//-------- ----------
 renderer.render(scene, camera);
--- views/forpost/threejs-face3-color/s3-buffer-geometry/js/main.js
+++ views/forpost/threejs-face3-color/s3-buffer-geometry/js/main.js
@@ -1,5 +1,26 @@
-// create a buffed geometry
-var geometry = new THREE.PlaneGeometry(1, 2, 1, 1);
+//-------- ----------
+// SCENE
+//-------- ----------
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
+var renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer() : new THREE.WebGLRenderer();
+renderer.setSize(640, 480, false);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// BUFFER GEOMERTY
+//-------- ----------
+var geometry = new THREE.BufferGeometry();
+var vertices = new Float32Array([
+    -1, 0, 0,
+     1, 0, 0,
+     1, 1.25, 0
+]);
+geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+//-------- ----------
+// COLORS
+//-------- ----------
 // add a colors prop to the geometry
 var colors = new Uint8Array([
             255, 0, 0,
@@ -11,20 +32,17 @@ var colors = new Uint8Array([
         ]);
 // Don't forget to normalize the array! (third param = true)
 geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3, true));
-
-// SCENE
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 1000);
-camera.position.set(2, 2, 2);
-camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-document.getElementById('demo').appendChild(renderer.domElement);
-
+//-------- ----------
+// MESH
+//-------- ----------
 // MESH that uses the vertex colors
 var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
             side: THREE.DoubleSide,
-            vertexColors: true
+            vertexColors: true,
+            side: THREE.DoubleSide
         }));
 scene.add(mesh);
-
+//-------- ----------
+// RENDERER
+//-------- ----------
 renderer.render(scene, camera);
