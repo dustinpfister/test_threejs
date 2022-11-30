94a2b81c5041bf4bf316114b3fe24e3808560a6e
dustinpfister <dustin.pfister@gmail.com>
2022-11-30 17:02:25 -0500

    basic example looks good

--- views/forpost/threejs-clock/s1-1-basic-clock/js/main.js
+++ views/forpost/threejs-clock/s1-1-basic-clock/js/main.js
@@ -1,31 +1,37 @@
+// ---------- ---------- ----------
+// SCENE, CAMERA, and RENDERER
+// ---------- ---------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+camera.position.set(1.2, 1.2, 1.2);
+camera.lookAt(0, -0.15, 0);
+const renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer() : new THREE.WebGLRenderer;
+renderer.setSize(640, 480, false);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+// ---------- ---------- ----------
 // A STATE OBJECT WITH A THREE.CLOCK INSTANCE
-var state = {
+// ---------- ---------- ----------
+const state = {
     clock: new THREE.Clock(),
     frame: 0,
     maxFrame: 90,
     fps: 30,
     per: 0
 };
-// a scene
-var scene = new THREE.Scene();
-// a mesh
-var box = new THREE.Mesh(
+// ---------- ---------- ----------
+// MESH
+// ---------- ---------- ----------
+const box = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
 scene.add(box);
-// camera render
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(1, 1, 1);
-camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
-// START CLOCK
+// ---------- ---------- ----------
+// CLOCK / LOOP
+// ---------- ---------- ----------
 state.clock.start();
-// loop
-var loop = function () {
+const loop = function () {
     // USING THE GET DELTA METHOD
-    var secs = state.clock.getDelta();
+    const secs = state.clock.getDelta();
     state.per = state.frame / state.maxFrame;
     requestAnimationFrame(loop);
     box.rotation.y = Math.PI * 2 * state.per;
---
+++ views/forpost/threejs-clock/todo.md
@@ -0,0 +1,5 @@
+# threejs-clock todo list
+
+## ( done 11/30/2022 ) - started for post folder
+* (done) for post folder started with r127 demos
+* (done) updated source to work well with r146
