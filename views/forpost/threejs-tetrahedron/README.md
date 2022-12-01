9408e040a3cdfa9cffa6a92a9d8a1eea7932bb9f
dustinpfister <dustin.pfister@gmail.com>
2022-12-01 15:25:34 -0500

    new for post folder threejs-tetrahedron

---
+++ views/forpost/threejs-tetrahedron/README.md
@@ -0,0 +1,19 @@
+# threejs-terahedron
+
+## Regular tetrahedron
+
+It would seem that the THREE.Terahedron Constructor creates what is really called a "[Regular tetrahedron](https://en.wikipedia.org/wiki/Tetrahedron)"
+
+## Irregular tetrahedron
+
+If I where to take the time to make a custom function for creating this kind of geometry then the focus should be on making some of th other kinds of tetrahedron that do not have equal sides.
+
+## Geometry Browser Style
+
+On A side note I made Objects that are like that of what are used in the Geometry browser used in the threejs docs.
+They are not just using Mesh objects, but rather Groups with a mesh Object as well as a Line Segments obejcts added
+As Children
+
+```
+https://github.com/mrdoob/three.js/blob/dev/docs/scenes/geometry-browser.html
+```
---
+++ views/forpost/threejs-tetrahedron/s3-1-loop/main.js
@@ -0,0 +1,72 @@
+(function(){
+    // ---------- ----------
+    // SCENE, CAMERA, RENDERER
+    // ---------- ----------
+    const scene = new THREE.Scene();
+    scene.add( new THREE.GridHelper(10, 10) );
+    const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+    camera.position.set(3, 3, 3);
+    camera.lookAt(0, 0, 0);
+    const renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer() : new THREE.WebGLRenderer;
+    renderer.setSize(640, 480, false);
+    ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+    // ---------- ----------
+    // Tetrahedron Geometry
+    // ---------- ----------
+    const geo = new THREE.TetrahedronGeometry(1, 0);
+    // ---------- ----------
+    // GROUP - made of mesh and line
+    // ---------- ----------
+    // materials
+    const line_material = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 3 } );
+    const mesh_material = new THREE.MeshPhongMaterial( { color: 0x00ff88, flatShading: true } );
+    // Group, Mesh, LineSegments
+    const group = new THREE.Group();
+    const line = new THREE.LineSegments( geo, line_material );
+    group.add(line);
+    const mesh = new THREE.Mesh( geo, mesh_material );
+    group.add(mesh);
+    scene.add(group);
+    // ---------- ----------
+    // LIGHT
+    // ---------- ----------
+    const dl = new THREE.DirectionalLight(0xffffff, 1.0);
+    dl.position.set(3, 1, 2);
+    scene.add(dl);
+    const al = new THREE.AmbientLight(0xffffff, 0.1);
+    scene.add(al);
+    // ---------- ----------
+    // CONTROLS
+    // ---------- ----------
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    // ---------- ----------
+    // ANIMATION LOOP
+    // ---------- ----------
+    const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+    FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+    FRAME_MAX = 120;
+    let secs = 0,
+    frame = 0,
+    lt = new Date();
+    // update
+    const update = function(frame, frameMax){
+        const a1 = frame / frameMax;
+        group.rotation.y = Math.PI * 2 * a1;
+    };
+    // loop
+    const loop = () => {
+        const now = new Date(),
+        secs = (now - lt) / 1000;
+        requestAnimationFrame(loop);
+        if(secs > 1 / FPS_UPDATE){
+            // update, render
+            update( Math.floor(frame), FRAME_MAX);
+            renderer.render(scene, camera);
+            // step frame
+            frame += FPS_MOVEMENT * secs;
+            frame %= FRAME_MAX;
+            lt = now;
+        }
+    };
+    loop();
+}());
