0a33bc8455a8d72f354ae8c3b36321772b7a3a3e
dustinpfister <dustin.pfister@gmail.com>
2022-12-01 17:58:09 -0500

    nice video1 loop ready

--- views/forpost/threejs-tetrahedron/s3-1-loop-video1/main.js
+++ views/forpost/threejs-tetrahedron/s3-1-loop-video1/main.js
@@ -11,22 +11,40 @@
     renderer.setSize(640, 480, false);
     ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
     // ---------- ----------
+    // HELPERS
+    // ---------- ----------
+    const appendLine = (mesh, color, lw, opacity ) => {
+        const line_material = new THREE.LineBasicMaterial( {
+            color: color|| 0xffffff,
+            linewidth: lw === undefined ? 6 : lw,
+            transparent: true,
+            opacity: opacity === undefined ? 1 : opacity
+        });
+        const line = new THREE.LineSegments( mesh.geometry, line_material );
+        mesh.add(line)
+    };
+    // ---------- ----------
     // Tetrahedron Geometry
     // ---------- ----------
-    const geo = new THREE.TetrahedronGeometry(1, 0);
+    const geo = new THREE.TetrahedronGeometry(3, 0);
     // ---------- ----------
-    // GROUP - made of mesh and line
+    // MESH
     // ---------- ----------
-    // materials
-    const line_material = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 3 } );
-    const mesh_material = new THREE.MeshPhongMaterial( { color: 0x00ff88, flatShading: true } );
-    // Group, Mesh, LineSegments
-    const group = new THREE.Group();
-    const line = new THREE.LineSegments( geo, line_material );
-    group.add(line);
-    const mesh = new THREE.Mesh( geo, mesh_material );
-    group.add(mesh);
-    scene.add(group);
+    const mesh_material1 = new THREE.MeshPhongMaterial( {
+        color: 0x00ff88, flatShading: true,
+        side: THREE.DoubleSide,
+        transparent: true, opacity: 0.50 } );
+    const mesh_material2 = new THREE.MeshPhongMaterial( {
+        color: 0x00ff00, flatShading: true,
+        transparent: true, opacity: 0.25
+    });
+    const mesh = new THREE.Mesh( geo, mesh_material1 );
+    scene.add(mesh);
+    appendLine(mesh);
+    // sphere mesh
+    const mesh_sphere = new THREE.Mesh( new THREE.SphereGeometry(3.0, 30, 30), mesh_material2);
+    scene.add(mesh_sphere);
+    appendLine(mesh_sphere, 0xffffff, 3, 0.2);
     // ---------- ----------
     // LIGHT
     // ---------- ----------
@@ -44,14 +62,15 @@
     // ---------- ----------
     const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
     FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
-    FRAME_MAX = 120;
+    FRAME_MAX = 800;
     let secs = 0,
     frame = 0,
     lt = new Date();
     // update
     const update = function(frame, frameMax){
         const a1 = frame / frameMax;
-        group.rotation.y = Math.PI * 2 * a1;
+        mesh.rotation.y = Math.PI * 2 * a1;
+        mesh.rotation.z = Math.PI * 4 * a1;
     };
     // loop
     const loop = () => {
