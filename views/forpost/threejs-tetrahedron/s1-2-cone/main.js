a425aac63ad63ef888e22e8db0b14ad9526dcd6d
dustinpfister <dustin.pfister@gmail.com>
2022-12-01 15:54:21 -0500

    cone example looks good

--- views/forpost/threejs-tetrahedron/s1-2-cone/main.js
+++ views/forpost/threejs-tetrahedron/s1-2-cone/main.js
@@ -5,20 +5,38 @@
     const scene = new THREE.Scene();
     scene.add( new THREE.GridHelper(10, 10) );
     const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+    camera.position.set(2, 2, 2);
+    camera.lookAt(0, 0 ,0);
     const renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer() : new THREE.WebGLRenderer;
     renderer.setSize(640, 480, false);
     ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
     // ---------- ----------
-    // Tetrahedron Geometry
+    // Cone Geometry set up to result in a kind of Tetrahedron
     // ---------- ----------
-    const geo = new THREE.TetrahedronGeometry(1, 0);
-    const mesh_material = new THREE.MeshNormalMaterial();
+    const geo = new THREE.ConeGeometry(0.75, 1, 3, 1, true);
+    geo.rotateX(Math.PI * 0.5);
+    // ---------- ----------
+    // MESH AND POINTS
+    // ---------- ----------
+    const mesh_material = new THREE.MeshNormalMaterial({ side: THREE.DoubleSide });
     const mesh = new THREE.Mesh( geo, mesh_material );
     scene.add(mesh);
+    const points = new THREE.Points(geo, new THREE.PointsMaterial({ size: 0.25}));
+    mesh.add(points);
+    mesh.position.set(0,0,0);
+    mesh.lookAt(10,0,0);
+    // ---------- ----------
+    // CONTROLS
+    // ---------- ----------
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
     // ---------- ----------
     // RENDER
     // ---------- ----------
-    camera.position.set(3.25, 2.25, 3).normalize().multiplyScalar(2);
-    camera.lookAt(mesh.position);
-    renderer.render(scene, camera);
+    // loop
+    const loop = () => {
+        requestAnimationFrame(loop);
+        renderer.render(scene, camera);
+    };
+    loop();
+
 }());
