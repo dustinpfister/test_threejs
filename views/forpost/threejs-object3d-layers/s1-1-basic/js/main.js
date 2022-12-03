a6a7b63504fe08397f427334aa501a12a7d899f9
dustinpfister <dustin.pfister@gmail.com>
2022-12-03 09:12:46 -0500

    updated basic layers example

--- views/forpost/threejs-object3d-layers/s1-1-basic/js/main.js
+++ views/forpost/threejs-object3d-layers/s1-1-basic/js/main.js
@@ -1,27 +1,33 @@
-
 (function () {
-    // scene
-    var scene = new THREE.Scene();
-
-    var grid = new THREE.GridHelper(10, 10);  // ADDING A GRID THAT I AM ENABLING FOR ALL LAYERS
+    //-------- ----------
+    // SCENE, CAMERA, RENDERER
+    //-------- ----------
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
+    camera.position.set(5, 5, 5);
+    camera.lookAt(0, 0, 0);
+    const renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer(): new THREE.WebGLRenderer();
+    renderer.setSize(640, 480, false);
+    ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+    //-------- ----------
+    // ADDING A GRID THAT I AM ENABLING FOR ALL LAYERS
+    //-------- ----------
+    const grid = new THREE.GridHelper(10, 10);
     grid.layers.enableAll();
     scene.add(grid);
-    var mesh = new THREE.Mesh(  // SINGLE MESH FOR LAYER 1
+    //-------- ----------
+    // SINGLE MESH FOR LAYER 1
+    //-------- ----------
+    const mesh = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
             new THREE.MeshNormalMaterial());
     mesh.layers.set(1);
     scene.add(mesh);
-
-    // camera, and renderer
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(5, 5, 5);
-    camera.lookAt(0, 0, 0);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-    // update
-    var cameraLayer = 0;
-    var update = function () {
+    //-------- ----------
+    // RENDER
+    //-------- ----------
+    let cameraLayer = 0;
+    const update = function () {
         camera.layers.set(cameraLayer);
         renderer.render(scene, camera);
         cameraLayer += 1;
