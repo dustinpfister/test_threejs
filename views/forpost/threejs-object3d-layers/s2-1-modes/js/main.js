4a2c3d692427295f11c813c8bc2113eb0045b71e
dustinpfister <dustin.pfister@gmail.com>
2022-12-03 09:15:45 -0500

    updated modes example

--- views/forpost/threejs-object3d-layers/s2-1-modes/js/main.js
+++ views/forpost/threejs-object3d-layers/s2-1-modes/js/main.js
@@ -1,35 +1,45 @@
-
 (function () {
-
-    var layerModes = [[0], [1], [2], [0, 1]],
-    layerModeIndex = 0;
-
-    var setToLayerMode = function (obj, index) {
+    //-------- ----------
+    // SCENE, CAMERA, RENDERER
+    //-------- ----------
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
+    camera.position.set(5, 5, 5);
+    camera.lookAt(0, 0, 0);
+    const renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer(): new THREE.WebGLRenderer();
+    renderer.setSize(640, 480, false);
+    ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+    //-------- ----------
+    // LAYER MODES
+    //-------- ----------
+    const layerModes = [[0], [1], [2], [0, 1]];
+    let layerModeIndex = 0;
+    //-------- ----------
+    // HELPER FUNCTIONS
+    //-------- ----------
+    const setToLayerMode = function (obj, index) {
         obj.layers.disableAll();
         layerModes[index].forEach(function (layerNum) {
             obj.layers.enable(layerNum);
         });
     };
-
-    var createBoxForLayer = function (layerMode, color, x) {
-        var mesh = new THREE.Mesh(
+    const createBoxForLayer = function (layerMode, color, x) {
+        const mesh = new THREE.Mesh(
                 new THREE.BoxGeometry(1, 1, 1),
                 new THREE.MeshBasicMaterial({
                     color: color
                 }));
-        var boxHelper = new THREE.BoxHelper(mesh);
+        const boxHelper = new THREE.BoxHelper(mesh);
         setToLayerMode(boxHelper, 3);
         mesh.add(boxHelper);
         mesh.position.set(x, 0, 0);
         setToLayerMode(mesh, layerMode);
         return mesh;
     };
-
-    // scene
-    var scene = new THREE.Scene();
-
-    // ADDING A GRID THAT I AM ENABLING FOR ALL LAYERS
-    var grid = new THREE.GridHelper(10, 10);
+    //-------- ----------
+    // GRID AND MESH OBJECTS
+    //-------- ----------
+    const grid = new THREE.GridHelper(10, 10);
     grid.layers.enableAll(); // enable all will set all layers true
     scene.add(grid);
     // ADDING A MESH FOR LAYER MODE 0 ONLY
@@ -38,19 +48,12 @@
     scene.add(createBoxForLayer(1, 'lime', -2));
     // ADDING A MESH FOR LAYER MODE 2 ONLY
     scene.add(createBoxForLayer(2, 'white', 0));
-
-    // camera, and renderer
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(5, 5, 5);
-    camera.lookAt(0, 0, 0);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-
-    // loop
-    var lt = new Date();
-    var loop = function () {
-        var now = new Date(),
+    //-------- ----------
+    // LOOP
+    //-------- ----------
+    let lt = new Date();
+    const loop = function () {
+        const now = new Date(),
         secs = (now - lt) / 1000;
         requestAnimationFrame(loop);
         if (secs > 1) {
@@ -64,6 +67,5 @@
     setToLayerMode(camera, layerModeIndex);
     renderer.render(scene, camera);
     loop();
-
 }
     ());
