0df3a54f3dedd3d7ffb6823de09c542bdbacc822
dustinpfister <dustin.pfister@gmail.com>
2022-12-06 11:58:23 -0500

    todo

--- views/forpost/threejs-examples-clock-basic/r0-1-demo/main.js
+++ views/forpost/threejs-examples-clock-basic/r0-1-demo/main.js
@@ -1,9 +1,21 @@
-
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 320 / 240, 1, 1000);
+camera.position.set(15, 15, 15);
+camera.lookAt(0, 0, 0);
+const renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer() : new THREE.WebGLRenderer();
+renderer.setSize(640, 480, false);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// HELPERS
+//-------- ----------
 // create a THREE.Group of face marks for each hour
-var createFaceCubes = function (material) {
-    var group = new THREE.Group();
+const createFaceCubes = function (material) {
+    const group = new THREE.Group();
     clock.createFacePoints(0, 0, 0, 10).map(function (facePoints) {
-        var cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
+        const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
         cube.position.set(facePoints[0], facePoints[1], facePoints[2]);
         cube.lookAt(0, 0, 0);
         group.add(cube);
@@ -11,17 +23,16 @@ var createFaceCubes = function (material) {
     });
     return group;
 };
-
 // create hand cubes
-createHandCubes = function (material) {
-    var group = new THREE.Group();
+const createHandCubes = function (material) {
+    const group = new THREE.Group();
     group.add(new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material),
         new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material),
         new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material))
-    var update = function (clockObj) {
+    const update = function (clockObj) {
         clockObj = clockObj || clock.get();
         clock.createHandPoints(clockObj, 0, 0, 0, 10).forEach(function (point, i) {
-            var cube = group.children[i];
+            const cube = group.children[i];
             cube.position.set(point[0], point[1], point[2]);
         });
     };
@@ -29,18 +40,12 @@ createHandCubes = function (material) {
     return {
         group: group,
         update: update
-    }
+    };
 };
-
-// the demo
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, 320 / 240, 1, 1000);
-var renderer = new THREE.WebGLRenderer();
-document.getElementById('demo').appendChild(renderer.domElement);
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-var materials = [
+//-------- ----------
+// MATERIALS
+//-------- ----------
+const materials = [
     new THREE.MeshBasicMaterial({
         color: 0x00ff00,
         wireframe: true
@@ -49,21 +54,18 @@ var materials = [
         color: 0xff0000,
         wireframe: true
     })];
-
-// position and rotate camera
-camera.position.set(11, 11, 11);
-camera.lookAt(0, 0, 0);
-renderer.setSize(640, 480);
-
+//-------- ----------
+// OBJECTS
+//-------- ----------
 // create and add face cubes
 scene.add(createFaceCubes(materials[0]));
-
 // create hands
-var hands = createHandCubes(materials[1]);
+const hands = createHandCubes(materials[1]);
 scene.add(hands.group);
-
-// loop
-var loop = function () {
+//-------- ----------
+// LOOP
+//-------- ----------
+const loop = function () {
     hands.update(clock.get(new Date()));
     requestAnimationFrame(loop);
     renderer.render(scene, camera);
---
+++ views/forpost/threejs-examples-clock-basic/todo.md
@@ -0,0 +1,6 @@
+# threejs-examples-clock-basic
+
+## ( done 12/06/2022 ) - for post folder started
+* (done) for post folder started with r127 demo
+* () get this example working with r146
+
