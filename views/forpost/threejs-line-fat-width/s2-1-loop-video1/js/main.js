2f30a1ed030815ddd178a889616ad6e412d6f056
dustinpfister <dustin.pfister@gmail.com>
2022-12-06 15:07:41 -0500

    loop1 video started

--- views/forpost/threejs-line-fat-width/s2-1-loop-video1/js/main.js
+++ views/forpost/threejs-line-fat-width/s2-1-loop-video1/js/main.js
@@ -15,9 +15,7 @@
     // LINE2
     //-------- ----------
     const geo = new THREE.LineGeometry();
-    geo.setPositions([0,0,0, 0,3,0, -3,3,-5, -3,3,5, 0,0,5, 0,0,0]);
-    // use the set colors method an pass an array of color channel values
-    // in the from of 0 to 1 values for each color channel (rgb)
+    geo.setPositions([0,0,0, 0,1,0 ]);
     geo.setColors([0,1,0, 0,1,1, 0,0.5,0, 0,0.5,0.5, 0,0.25,0, 0,0.25,0.25]);
     // use vertex colors when setting up the material
     const line_material = new THREE.LineMaterial({
@@ -28,6 +26,36 @@
     //-------- ----------
     // RENDER
     //-------- ----------
-    renderer.render(scene, camera);
+    // ---------- ----------
+    // ANIMATION LOOP
+    // ---------- ----------
+    const FPS_UPDATE = 30, // fps rate to update ( low fps for low CPU use, but choppy video )
+    FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+    FRAME_MAX = 120;
+    let secs = 0,
+    frame = 0,
+    lt = new Date();
+    // update
+    const update = function(frame, frameMax){
+        const a1 = frame / frameMax;
+        const a2 = 1 - Math.abs(0.5 - a1) / 0.5;
+        geo.setPositions([0,0,0, -10 + 20 * a2,1,0 ]);
+    };
+    // loop
+    const loop = () => {
+        const now = new Date(),
+        secs = (now - lt) / 1000;
+        requestAnimationFrame(loop);
+        if(secs > 1 / FPS_UPDATE){
+            // update, render
+            update( Math.floor(frame), FRAME_MAX);
+            renderer.render(scene, camera);
+            // step frame
+            frame += FPS_MOVEMENT * secs;
+            frame %= FRAME_MAX;
+            lt = now;
+        }
+    };
+    loop();
 }
     ());
