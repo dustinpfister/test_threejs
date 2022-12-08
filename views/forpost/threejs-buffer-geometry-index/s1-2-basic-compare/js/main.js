f8cdb7391e2a956c12f9923960ef63e769b149c5
dustinpfister <dustin.pfister@gmail.com>
2022-12-08 13:57:29 -0500

    compare example done

--- views/forpost/threejs-buffer-geometry-index/s1-2-basic-compare/js/main.js
+++ views/forpost/threejs-buffer-geometry-index/s1-2-basic-compare/js/main.js
@@ -5,28 +5,40 @@
     const scene = new THREE.Scene();
     scene.add( new THREE.GridHelper(10, 10) );
     const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
-    camera.position.set(2, 5, 5);
-    camera.lookAt(-2, 0, -2);
+    camera.position.set(4, 4, 4);
+    camera.lookAt(0, 0, 0);
     const renderer = new THREE.WebGL1Renderer();
     renderer.setSize(640, 480, false);
     (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
     //-------- ----------
     // INDEX GEO
     //-------- ----------
-    const geometry = new THREE.BufferGeometry();
-    // position array of 4 points
-    const pos = new THREE.BufferAttribute(new Float32Array([0,-3,0,0,3,0,-5,0,0,0,0,-5]), 3);
-    geometry.setAttribute('position', pos);
-    // using 4 points to draw two trangles by adding an index
-    const index = new THREE.BufferAttribute( new Uint8Array([0,1,2,0,1,3]) , 1);
-    geometry.setIndex(index)
-    // using this to create normal attribute
-    geometry.computeVertexNormals();
+    const geo_indexed = new THREE.BufferGeometry();
+    // position array of 4 points to draw two triangles, by using an index
+    geo_indexed.setAttribute('position',
+        new THREE.BufferAttribute(new Float32Array([0,0,0, 0,2,0, -2,0,0, 0,0,-2]), 3)
+    );
+    const index = new THREE.BufferAttribute( new Uint8Array([0,1,2,0,1,3]), 1);
+    geo_indexed.setIndex(index)
+    geo_indexed.computeVertexNormals();
+    //-------- ----------
+    // INDEX GEO
+    //-------- ----------
+    const geo_non_indexed = new THREE.BufferGeometry();
+    // position array of 6 points for two triangles ( no index )
+    geo_non_indexed.setAttribute('position',
+        new THREE.BufferAttribute(new Float32Array([0,0,0, 0,2,0, -2,0,0, 0,0,0, 0,2,0, 0,0,-2]), 3)
+    );
+    geo_non_indexed.computeVertexNormals();
     //-------- ----------
     // MESH
     //-------- ----------
-    const mesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({ side: THREE.DoubleSide }) );
-    scene.add(mesh);
+    const mesh_indexed = new THREE.Mesh(geo_indexed, new THREE.MeshNormalMaterial({ side: THREE.DoubleSide }) );
+    mesh_indexed.position.x = -1;
+    scene.add(mesh_indexed);
+    const mesh_non_indexed = new THREE.Mesh(geo_non_indexed, new THREE.MeshNormalMaterial({ side: THREE.DoubleSide }) );
+    mesh_non_indexed.position.x = 2;
+    scene.add(mesh_non_indexed);
     //-------- ----------
     // RENDER
     //-------- ----------
--- views/forpost/threejs-buffer-geometry-index/todo.md
+++ views/forpost/threejs-buffer-geometry-index/todo.md
@@ -1,7 +1,7 @@
 # threejs-buffer-geometry-index todo list
 
-## () s1-2-basic-compare
-* () make two geos each of just two trangles one indexed and one not
+## ( done 12/08/2022 ) s1-2-basic-compare
+* (done) make two geos each of just two trangles one indexed and one not
 
 ## ( done 12/08/2022 ) s1-1-basic
 * (done) start a basic example
