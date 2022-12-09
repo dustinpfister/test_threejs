3f0e6fc7deb06f359251b7658d77155e4d757934
dustinpfister <dustin.pfister@gmail.com>
2022-12-09 12:34:32 -0500

    fixed up color map example

--- views/forpost/threejs-standard-material/s2-color-map/js/main.js
+++ views/forpost/threejs-standard-material/s2-color-map/js/main.js
@@ -1,5 +1,18 @@
-(function (utils) {
-    utils.createCanvasTexture = function (draw, size) {
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0.2,0.2,0.2);
+const camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+camera.position.set(6, 6, 6);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// HELPERS
+//-------- ----------
+const createCanvasTexture = function (draw, size) {
         var canvas = document.createElement('canvas'),
         ctx = canvas.getContext('2d');
         canvas.width = size || 32;
@@ -9,32 +22,29 @@
         draw(ctx, canvas);
         return new THREE.CanvasTexture(canvas);
     };
-}
-    (this['utils'] = {}));
- 
-var colorMap = utils.createCanvasTexture(function (ctx, canvas) {
-        ctx.fillStyle = 'red';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.strokeStyle = 'white';
-        ctx.beginPath();
-        ctx.lineWidth = 3;
-        ctx.rect(1, 1, canvas.width - 2, canvas.height - 2);
-        ctx.stroke();
-    });
- 
+//-------- ----------
+// TEXTURE
+//-------- ----------
+var colorMap = createCanvasTexture(function (ctx, canvas) {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = 'white';
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.rect(1, 1, canvas.width - 2, canvas.height - 2);
+    ctx.stroke();
+});
+//-------- ----------
+// MESH, LIGHT
+//-------- ----------
 // creating a box with the standard material
 var box = new THREE.Mesh(
         new THREE.BoxGeometry(3, 3, 3),
         new THREE.MeshStandardMaterial({
             map: colorMap
         }));
- 
-// creating a scene
-var scene = new THREE.Scene();
- 
 // add the box mesh to the scene
 scene.add(box);
- 
 // adding a light source
 var sun = new THREE.Mesh(
         new THREE.SphereGeometry(0.25, 40, 40),
@@ -42,34 +52,22 @@ var sun = new THREE.Mesh(
 sun.add(new THREE.PointLight(0xffffff, 1));
 sun.position.set(2, 6, 16);
 scene.add(sun);
- 
-// camera and renderer
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(6, 6, 6);
-camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
- 
-var lt = new Date(),
+//-------- ----------
+// LOOP
+//-------- ----------
+let lt = new Date(),
 radian = 0;
- 
-var loop = function () {
- 
-    var now = new Date(),
+const loop = function () {
+    const now = new Date(),
     secs = (now - lt) / 1000;
     lt = now;
- 
     requestAnimationFrame(loop);
- 
     radian += 1.57 * secs;
     radian %= Math.PI * 2;
- 
-    var x = Math.cos(radian) * 5,
+    const x = Math.cos(radian) * 5,
     y = 2 - 2 * Math.cos(radian),
     z = Math.sin(radian) * 5;
     sun.position.set(x, y, z);
- 
     renderer.render(scene, camera);
 };
 loop();
