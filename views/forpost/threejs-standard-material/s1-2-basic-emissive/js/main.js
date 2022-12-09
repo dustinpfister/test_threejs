ac079389e9b91bf3bd44506acf06ff23af81981e
dustinpfister <dustin.pfister@gmail.com>
2022-12-09 12:28:00 -0500

    emissive basic example looks good

--- views/forpost/threejs-standard-material/s1-2-basic-emissive/js/main.js
+++ views/forpost/threejs-standard-material/s1-2-basic-emissive/js/main.js
@@ -3,7 +3,7 @@
 //-------- ----------
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(1.0, 1.7, 2.0);
+camera.position.set(1,1,1);
 camera.lookAt(0, 0, 0);
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
@@ -12,17 +12,19 @@ renderer.setSize(640, 480, false);
 // LIGHT
 //-------- ----------
 const dl = new THREE.DirectionalLight(0xffffff, 1);
-dl.position.set(8, 4, 2);
+dl.position.set(8, 3, -5);
 scene.add(dl);
 //-------- ----------
 // CREATING A MESH WITH THE STANDARD MATERIAL
 //-------- ----------
 const material = new THREE.MeshStandardMaterial({
-    color: 'red'
+    color: new THREE.Color(1, 0, 0),
+    emissive: new THREE.Color(1, 1, 1), // WHITE emissive COLOR
+    emissiveIntensity: 0.25             // Intensity of emissive COLOR
 });
 scene.add( new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        material ) );
+    new THREE.SphereGeometry(0.75, 80, 80),
+    material) );
 //-------- ----------
 // RENDER
 //-------- ----------
--- views/forpost/threejs-standard-material/todo.md
+++ views/forpost/threejs-standard-material/todo.md
@@ -1,11 +1,11 @@
 # threejs-standard-material todo list
 
-## () s1-2-basic-emissive
-* () emissive color example
-* () emissive intesnity
+## ( done 12/09/2022 ) s1-2-basic-emissive
+* (done) emissive color example
+* (done) emissive intesnity
 
-## () s1-1-basic-color-light
-* () rename s1-basic to s1-basic-color-light
+## ( done 12/09/2022 ) s1-1-basic-color-light
+* (done) rename s1-basic to s1-basic-color-light
 
 ## ( done 04/21/2022 ) - first state of examples done
 * (done) just have section folders for each example in the post
