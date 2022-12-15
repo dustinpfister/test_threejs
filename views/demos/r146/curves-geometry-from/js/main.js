4dadac1c631081fa1e0ca0e6695668148aa79337
dustinpfister <dustin.pfister@gmail.com>
2022-12-15 13:07:23 -0500

    light

--- views/demos/r146/curves-geometry-from/js/main.js
+++ views/demos/r146/curves-geometry-from/js/main.js
@@ -2,7 +2,7 @@
 // SCENE, CAMERA, RENDERER
 //-------- ----------
 const scene = new THREE.Scene();
-scene.add( new THREE.GridHelper(10, 10) );
+//scene.add( new THREE.GridHelper(10, 10) );
 const camera = new THREE.PerspectiveCamera(50, 640 / 480, 0.1, 1000);
 camera.position.set(8, 8, 0);
 camera.lookAt(0, 0, 0);
@@ -10,10 +10,19 @@ const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
 // ---------- ----------
+// LIGHT
+// ---------- ----------
+const dl = new THREE.DirectionalLight(0xffffff, 1);
+dl.position.set(2, 1, 0);
+scene.add(dl);
+const dl2 = new THREE.DirectionalLight(0xffffff, 1);
+dl2.position.set(2, -1, 0);
+scene.add(dl2);
+// ---------- ----------
 // CURVES
 // ---------- ----------
 const c1_start = new THREE.Vector3(-5,0,5), 
-c1_control = new THREE.Vector3(-2,7,-2.5), 
+c1_control = new THREE.Vector3(-2,2,-4.5), 
 c1_end = new THREE.Vector3(5,0,5),
 c2_start = new THREE.Vector3(-5,0,-5), 
 c2_control = new THREE.Vector3(2,2,2.5), 
@@ -28,37 +37,35 @@ const curve2 = new THREE.QuadraticBezierCurve3(c2_start, c2_control, c2_end);
 //scene.add(line1);
 //const line2 = new THREE.Line( new THREE.BufferGeometry().setFromPoints( curve2.getPoints(50) ), material_line );
 //scene.add(line2);
-
 // ---------- ----------
-// GEO POSITION
+// GEO POSITION / UV
 // ---------- ----------
 const geo = new THREE.BufferGeometry();
-
 // position attribute data
 const pos_data = [];
+const uv_data = [];
 let pi = 0;
-const points_per_line = 20;
+const points_per_line = 50;
 while(pi < points_per_line){
     const a1 = pi / (points_per_line - 1);
     const v1 = curve1.getPoint(a1);
     const v2 = curve2.getPoint(a1);
     pos_data.push(v1.x, v1.y, v1.z, v2.x, v2.y, v2.z);
+    uv_data.push(0, 1, 0, 1);
     pi += 1;
 }
 geo.setAttribute('position', new THREE.Float32BufferAttribute( pos_data, 3 ) );
+geo.setAttribute('uv', new THREE.Float32BufferAttribute( uv_data, 2 ) );
 // ---------- ----------
 // GEO INDEX
 // ---------- ----------
-//geo.setIndex([0, 1, 2,  3, 2, 1 ]);
-
 const data_index = [];
 let pi2 = 0;
-while(pi2 < 20 - 1){
+while(pi2 < 50 - 1){
     const a = pi2 * 2;
     const b = a + 1;
     const c = a + 2;
     const d = a + 3;
-    //data_index.push(a, b, c,  d, c, b);
     data_index.push(b,c,d,c,b,a);
     pi2 += 1;
 }
@@ -69,9 +76,11 @@ geo.setIndex(data_index);
 // GEO NORMAL
 // ---------- ----------
 geo.computeVertexNormals();
-
-
-const mesh = new THREE.Mesh(geo, new THREE.MeshNormalMaterial({ wireframe: false, side: THREE.DoubleSide}))
+// ---------- ----------
+// MATERIAL AND MESH
+// ---------- ----------
+const material = new THREE.MeshPhongMaterial({ wireframe: false, side: THREE.DoubleSide});
+const mesh = new THREE.Mesh(geo, material)
 scene.add(mesh);
 
 // ---------- ----------
