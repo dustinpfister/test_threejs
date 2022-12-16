b4b1911d4bdb3f84601c8e2b57c9fd177db95428
dustinpfister <dustin.pfister@gmail.com>
2022-12-16 11:59:09 -0500

    static render for custom example

--- views/forpost/threejs-curve-geometry-from/s2-1-custom/js/main.js
+++ views/forpost/threejs-curve-geometry-from/s2-1-custom/js/main.js
@@ -5,8 +5,8 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(1, 1, 1);
 //scene.add( new THREE.GridHelper(10, 10) );
 const camera = new THREE.PerspectiveCamera(50, 640 / 480, 0.1, 1000);
-camera.position.set(8, 8, 0);
-camera.lookAt(0, 0, 0);
+camera.position.set(5, 9, -9);
+camera.lookAt(0, -1.75, 0);
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
@@ -23,10 +23,10 @@ scene.add(dl2);
 // CURVES
 // ---------- ----------
 const c1_start = new THREE.Vector3(-5,0,5), 
-c1_control = new THREE.Vector3(0, 5, 0), 
+c1_control = new THREE.Vector3(-2, 7, 0), 
 c1_end = new THREE.Vector3(5,0,5),
 c2_start = new THREE.Vector3(-5,0,-5), 
-c2_control = new THREE.Vector3(0, -5, 0), 
+c2_control = new THREE.Vector3(0, 0, 0), 
 c2_end = new THREE.Vector3(5,0,-5);
 const curve1 = new THREE.QuadraticBezierCurve3(c1_start, c1_control, c1_end);
 const curve2 = new THREE.QuadraticBezierCurve3(c2_start, c2_control, c2_end);
@@ -109,39 +109,6 @@ mesh.add(line);
 line.position.y = 0.025;
 scene.add(mesh);
 // ---------- ----------
-// CONTROLS
+// RENDER
 // ---------- ----------
-try{
-    const controls = new THREE.OrbitControls(camera, renderer.domElement);
-}catch(e){
-    console.warn('OrbitControls JSM module not loaded.');
-}
-// ---------- ----------
-// ANIMATION LOOP
-// ---------- ----------
-const FPS_UPDATE = 30, // fps rate to update ( low fps for low CPU use, but choppy video )
-FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
-FRAME_MAX = 400;
-let secs = 0,
-frame = 0,
-lt = new Date();
-// update
-const update = function(frame, frameMax){
-     const a1 = frame / frameMax;
-};
-// loop
-const loop = () => {
-    const now = new Date(),
-    secs = (now - lt) / 1000;
-    requestAnimationFrame(loop);
-    if(secs > 1 / FPS_UPDATE){
-        // update, render
-        update( Math.floor(frame), FRAME_MAX);
-        renderer.render(scene, camera);
-        // step frame
-        frame += FPS_MOVEMENT * secs;
-        frame %= FRAME_MAX;
-        lt = now;
-    }
-};
-loop();
+renderer.render(scene, camera);
