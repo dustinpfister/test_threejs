014c78e71f2913be242024c1064ea53433402d98
dustinpfister <dustin.pfister@gmail.com>
2022-12-18 10:48:01 -0500

    shell example looks good

--- views/forpost/threejs-examples-lines-sphere-circles/r1-1-shell/main.js
+++ views/forpost/threejs-examples-lines-sphere-circles/r1-1-shell/main.js
@@ -1,29 +1,26 @@
-//******** **********
+//-------- ----------
 // SCENE, CAMERA, RENDERER
-//******** **********
+//-------- ----------
 var scene = new THREE.Scene();
 scene.background = new THREE.Color('#000000');
 scene.add( new THREE.GridHelper(10, 10, 0x00ff00, 0x4a4a4a) )
 var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
 camera.position.set(10, 10, 10);
 camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
-//******** **********
+var renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
 // LINES
-//******** **********
-
+//-------- ----------
 // plain
 var g1 = LinesSphereCircles.create({ maxRadius: 4, pointsPerCircle: 20 });
 g1.position.set(-10,0,0)
 scene.add(g1);
-
 // seeded random
 var g2 = LinesSphereCircles.create({ maxRadius: 8, forPoint: 'seededRandom' });
 g2.position.set(-5,-2,-25)
 scene.add(g2);
-
 // seashell
 var opt = {
     circleCount: 20,
@@ -38,40 +35,28 @@ var opt = {
 };
 var g3 = LinesSphereCircles.create(opt);
 scene.add(g3);
-
-
 LinesSphereCircles.setByFrame(g3, 25, 100, opt);
-
-
 var g4 = LinesSphereCircles.create(opt);
 g4.position.set(0, -8, 0);
 scene.add(g4);
-
 var g5 = LinesSphereCircles.create(opt);
 g5.position.set(-8, -8, 0);
 scene.add(g5);
-
 var g6 = LinesSphereCircles.create(opt);
 g6.position.set(-16, -8, 0);
 scene.add(g6);
-
 var g7 = LinesSphereCircles.create(opt);
 g7.position.set(-24, -8, 0);
 scene.add(g7);
-
 var g8 = LinesSphereCircles.create(opt);
 g8.position.set(-32, -8, 0);
 scene.add(g8);
-
 var g9 = LinesSphereCircles.create(opt);
 g9.position.set(-40, -8, 0);
 scene.add(g9);
-
-
-
-//******** **********
+//-------- ----------
 // LOOP
-//******** **********
+//-------- ----------
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 var fps = 15,
 lt = new Date(),
@@ -82,7 +67,6 @@ var loop = function () {
     secs = (now - lt) / 1000;
     requestAnimationFrame(loop);
     if(secs > 1 / fps){
-
         LinesSphereCircles.setByFrame(g3, frame, frameMax, opt);
         LinesSphereCircles.setByFrame(g4, frame, frameMax, opt);
         LinesSphereCircles.setByFrame(g5, frame, frameMax, opt);
@@ -90,7 +74,6 @@ var loop = function () {
         LinesSphereCircles.setByFrame(g7, frame, frameMax, opt);
         LinesSphereCircles.setByFrame(g8, frame, frameMax, opt);
         LinesSphereCircles.setByFrame(g9, frame, frameMax, opt);
-
         renderer.render(scene, camera);
         frame += fps * secs;
         frame %= frameMax;
