772805bfc1c1c005e54f55160a66424f0713118e
dustinpfister <dustin.pfister@gmail.com>
2022-12-21 11:46:23 -0500

    working well with r146

--- views/forpost/threejs-examples-dae-tools-sphere-normals-invert/r0/js/main.js
+++ views/forpost/threejs-examples-dae-tools-sphere-normals-invert/r0/js/main.js
@@ -1,99 +1,64 @@
 
 (function () {
-
+    //------ ----------
     // SCENE
-    var scene = new THREE.Scene();
+    //------ ----------
+    const scene = new THREE.Scene();
     scene.add(new THREE.GridHelper(20, 20));
     scene.background = new THREE.Color('cyan');
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
+    const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
     camera.position.set(0, 10, 0);
     camera.lookAt(0, 5, 10);
-
     scene.add(camera);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(640, 480, false);
+    ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+    //------ ----------
     // LIGHT
-    var dl = new THREE.DirectionalLight(0xffffff, 1);
+    //------ ----------
+    const dl = new THREE.DirectionalLight(0xffffff, 1);
     dl.position.set(2, 1, 3)
     scene.add(dl);
-
-    // CONTROL
-    //var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-
-    var frame = 0, frameMax = 300;
-
-    // app loop
-    var loop = function () {
+    //------ ----------
+    // LOOP
+    //------ ----------
+    let frame = 0;
+    const frameMax = 300;
+    const loop = function () {
         requestAnimationFrame(loop);
         renderer.render(scene, camera);
-
-
-        var per = frame / frameMax,
-        bias = 1 - Math.abs( per - 0.5) / 0.5;
-        var r = Math.PI * 2 * per;
-        var x = Math.cos(r) * 10;
-        var z = Math.sin(r) * 10;
-
-        camera.lookAt(x, 15 - 15 * bias, z);
-
-
+        const per = frame / frameMax, bias = 1 - Math.abs( per - 0.5) / 0.5;
+        const r = Math.PI * 2 * per;
+        camera.lookAt(Math.cos(r) * 10, 15 - 15 * bias, Math.sin(r) * 10);
         frame += 1;
         frame %= frameMax;
-
-        //controls.update();
     };
     // USING DAE TOOLS TO LOAD THE *.dae file
-    var daeObjects = DAE.create();
+    const daeObjects = DAE.create();
     DAE.loadOne(daeObjects, "/dae/sphere-normal-invert/sphere-normal-invert.dae")
     .then(function(daeObjects){
-        var group = DAE.createGroup(daeObjects, 0);
+        const group = DAE.createGroup(daeObjects, 0);
         scene.add(group);
-
-        var mesh = group.children[0];
-
-        // OPTION1 - REPLACE MATERIAL WITH BASIC MATERIAL?
-        // A crude fix for this can be just be replacing the material
-        // with the basic material, using the THREE.DoubleSide valu for the
-        // side value
-        //mesh.material = new THREE.MeshBasicMaterial({
-        //    side: THREE.DoubleSide,
-        //    map: null
-        //});
-
-        // OPTION2 - MUTATE PHONG MATERIAL 
-        //var material = mesh.material;
-        //material.side = THREE.DoubleSide;
-
-        // OPTION3 - REPLACE WITH BASIC MATERIAL, USING MAP VALUE 
+        const mesh = group.children[0];
+        // REPLACE WITH BASIC MATERIAL, USING MAP VALUE 
         // FROM DAE FILE IMPORT
-        var sourceMaterial = mesh.material;
-        var newMaterial = new THREE.MeshBasicMaterial({
+        const sourceMaterial = mesh.material;
+        const newMaterial = new THREE.MeshBasicMaterial({
             map: sourceMaterial.map
         });
         mesh.material = newMaterial;
-
         // BASE GROUND MESH
-        var baseGround = new THREE.Mesh( new THREE.BoxGeometry(1, 0.1, 1), new THREE.MeshPhongMaterial({
+        const baseGround = new THREE.Mesh( new THREE.BoxGeometry(1, 0.1, 1), new THREE.MeshPhongMaterial({
              color: 0x00ff00
         }));
         baseGround.position.y = -0.125;
         baseGround.scale.set(60, 1, 60);
         scene.add(baseGround);
-
-        // VERTEX NORMALS HELPER
-        //var helper = new THREE.VertexNormalsHelper(mesh, 5, 0x00ff00, 3);
-        //scene.add(helper);
-
         loop();
     })
     .catch(function(e){
         console.log(e);
         loop();
     });
-
-
 }
     ());
--- views/forpost/threejs-examples-dae-tools-sphere-normals-invert/todo.md
+++ views/forpost/threejs-examples-dae-tools-sphere-normals-invert/todo.md
@@ -1,7 +1,11 @@
 # threejs-examples-dae-tools-sphere-normals-invert todo
 
 ## () - r1 -
-* new image file for a background
+* (done) new image file for a background
+* () module form of this project.
+
+## ( done 12/21/2022) - r0 - cleaned up r0 of the project
+* (done) working well with r146
 
 ## ( done 05/31/2022 ) - r0 of the example done
 * (done) first version done
