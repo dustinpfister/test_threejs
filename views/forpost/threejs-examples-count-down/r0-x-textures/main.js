30165c947ae98256fe49a5ccb4f2d3c7da8a13e2
dustinpfister <dustin.pfister@gmail.com>
2022-12-23 15:28:24 -0500

    cd3-nums-2 looks okay

--- views/forpost/threejs-examples-count-down/r0-x-textures/main.js
+++ views/forpost/threejs-examples-count-down/r0-x-textures/main.js
@@ -83,7 +83,7 @@ countDown.DAE_loader(
         const a1 = (frame + 1) / frameMax;
         let secs = Math.floor(SECS - SECS * a1);
         //countDown.set(count_sec, secs);
-		countDown.set(count_sec, '2');
+		countDown.set(count_sec, '3');
 		count_sec.rotation.y = Math.PI * 4 * a1;
         //countDown.set(count_frames, frame);
         // camera
