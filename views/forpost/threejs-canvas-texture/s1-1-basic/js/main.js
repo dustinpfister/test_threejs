f339973da9292b3ed94f30860cca29fca7f1a515
dustinpfister <dustin.pfister@gmail.com>
2022-12-30 07:19:15 -0500

    setting magFilter and minFilter for first basic canvas texture example

--- views/forpost/threejs-canvas-texture/s1-1-basic/js/main.js
+++ views/forpost/threejs-canvas-texture/s1-1-basic/js/main.js
@@ -3,34 +3,36 @@
 //-------- ----------
 const scene = new THREE.Scene();
 scene.add( new THREE.GridHelper(10, 10) );
-const camera = new THREE.PerspectiveCamera(75, 320 / 240, .025, 20);
-camera.position.set(1, 1, 1);
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, .025, 100);
+camera.position.set(1.25, 1.25, 1.25);
 camera.lookAt(0, 0, 0);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
+renderer.setSize(640, 480, false);
 (document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
 //-------- ----------
-// CANVAS ELEMENT, TEXTURE
+// CANVAS ELEMENT, 2D DRAWING CONTEXT
 //-------- ----------
-const canvas = document.createElement('canvas'),
-ctx = canvas.getContext('2d');
-canvas.width = 64;
-canvas.height = 64;
-ctx.fillStyle = '#222222';
-ctx.fillRect(-1, -1, canvas.width + 2, canvas.height + 2);
-ctx.lineWidth = 3;
-ctx.strokeStyle = '#00af00';
-ctx.strokeRect(2.5, 2.5, canvas.width - 4, canvas.height - 4);
+const canvas = document.createElement('canvas'), ctx = canvas.getContext('2d');
+canvas.width = 32; canvas.height = 32;
+ctx.lineWidth = 5;
+ctx.strokeStyle = '#ff0000';
+ctx.strokeRect(4, 4, canvas.width - 8, canvas.height - 8);
+//-------- ----------
+// CANVAS TEXTURE
+//-------- ----------
 const texture = new THREE.CanvasTexture(canvas);
+// Setting magFilter and minFilter to THREE.NearestFilter
+console.log(texture.magFilter); // 1006 ( THREE.LinearFilter - r146 )
+console.log(texture.minFilter); // 1008 ( THREE.LinearMipmapLinearFilter - r146 )
+console.log(THREE.NearestFilter); // 1003 ( r146 )
+texture.magFilter = THREE.NearestFilter;
+texture.minFilter = THREE.NearestFilter;
 //-------- ----------
-// MESH, MATERIAL
+// GEOMETRY, MATERIAL, MESH
 //-------- ----------
-const mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({
-            map: texture
-        })
-    );
+const geo = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ map: texture });
+const mesh = new THREE.Mesh( geo, material);
 scene.add(mesh);
 //-------- ----------
 // RENDER
