3051c908b5d707db69e24b1d7a2a0835e65f6200
dustinpfister <dustin.pfister@gmail.com>
2023-01-03 11:27:22 -0500

    great basic material demo

--- views/demos/r146/shader-material/main.js
+++ views/demos/r146/shader-material/main.js
@@ -10,6 +10,25 @@ const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
 // ---------- ----------
+// SHADER MATERIAL
+// ---------- ----------
+const material_shader = new THREE.ShaderMaterial( {
+    // just a default diffuse color of cyan for uniforms
+    uniforms: { 
+        'diffuse': { value: new THREE.Color(0, 1, 1) }
+    },
+    // just using the same code from 'MeshBasicMaterial' for
+    // vertex and fragment shaders
+    vertexShader: THREE.ShaderChunk[ 'meshbasic_vert' ],
+    fragmentShader: THREE.ShaderChunk[ 'meshbasic_frag' ],
+} );
+// ---------- ----------
+// GEOMETRY, MESH
+// ---------- ----------
+const geo = new THREE.BoxGeometry( 2, 2, 2);
+const mesh = new THREE.Mesh(geo, material_shader);
+scene.add(mesh);
+// ---------- ----------
 // RENDER
 // ---------- ----------
 renderer.render(scene, camera);
