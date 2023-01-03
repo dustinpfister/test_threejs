466b768596dedd23478246b8f32754727fe844a2
dustinpfister <dustin.pfister@gmail.com>
2023-01-03 08:46:19 -0500

    updated new basic example

--- views/forpost/threejs-line/s1-1-basic/main.js
+++ views/forpost/threejs-line/s1-1-basic/main.js
@@ -1,31 +1,30 @@
-(function () {
- 
-    // Scene
-    var scene = new THREE.Scene();
- 
-    var points = [];
-    points.push(
-        new THREE.Vector3(-10, -10, 0),
-        new THREE.Vector3(10, 0, 0),
-        new THREE.Vector3(-10, 10, 0));
-    var geometry = new THREE.BufferGeometry().setFromPoints( points );
-    // CREATE THE LINE
-    var line = new THREE.Line(
-            geometry,
-            new THREE.LineBasicMaterial({
-                color: 0x0000ff
-            }));
-    scene.add(line);
- 
-    // Camera
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(0, 0, -30);
-    camera.lookAt(0, 0, 0);
-    // Render
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(649, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-    renderer.render(scene, camera);
- 
-}
-    ());
+//-------- ---------
+// SCENE, CAMERA, RENDERER
+//-------- ---------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
+camera.position.set(0, 0, -30);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(649, 480, false);
+(document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ---------
+// CREATE THE LINE
+//-------- ---------
+const points = [];
+points.push(
+    new THREE.Vector3(-10, -10, 0),
+    new THREE.Vector3(10, 0, 0),
+    new THREE.Vector3(-10, 10, 0));
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line(
+    geometry,
+    new THREE.LineBasicMaterial({
+        color: 0x0000ff
+    })
+);
+scene.add(line);
+//-------- ---------
+// RENDERER
+//-------- ---------
+renderer.render(scene, camera);
