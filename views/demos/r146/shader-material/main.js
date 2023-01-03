5389d82d6b308ba288312a129235521465b4b1e9
dustinpfister <dustin.pfister@gmail.com>
2023-01-03 10:57:30 -0500

    started a shader-material r146 demo folder

---
+++ views/demos/r146/shader-material/main.js
@@ -0,0 +1,15 @@
+// ---------- ----------
+// SCENE, CAMERA, RENDERER
+// ---------- ----------
+const scene = new THREE.Scene();
+scene.add( new THREE.GridHelper(10, 10) );
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+camera.position.set(5, 5, 5);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+// ---------- ----------
+// RENDER
+// ---------- ----------
+renderer.render(scene, camera);
