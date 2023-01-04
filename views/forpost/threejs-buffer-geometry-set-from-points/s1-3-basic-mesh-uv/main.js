83db1f38f9b10f8ddc61ed57ed7460dec1bbbb50
dustinpfister <dustin.pfister@gmail.com>
2023-01-04 14:25:39 -0500

    draw tri looks good

--- views/forpost/threejs-buffer-geometry-set-from-points/s1-3-basic-mesh-uv/main.js
+++ views/forpost/threejs-buffer-geometry-set-from-points/s1-3-basic-mesh-uv/main.js
@@ -9,6 +9,36 @@ camera.lookAt(0, 0, 0);
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+//-------- ----------
+// HELPERS
+//-------- ----------
+const draw_tri = function(ctx, canvas){
+    const w = 5;
+    const hw = w / 2;
+    ctx.fillStyle = '#ffffff';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.lineWidth = w;
+    ctx.strokeStyle = '#ff0000';
+    ctx.beginPath();
+    ctx.moveTo(hw, hw);
+    ctx.lineTo(canvas.width - w, hw);
+    ctx.lineTo(hw, canvas.height - w);
+    ctx.lineTo(hw, hw);
+    ctx.stroke();
+};
+// create and return a canvas texture
+const createCanvasTexture = function (draw, size_canvas) {
+    size_canvas = size_canvas === undefined ? 32 : size_canvas;
+    const canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d');
+    canvas.width = size_canvas;
+    canvas.height = size_canvas;
+    draw(ctx, canvas);
+    const texture = new THREE.CanvasTexture(canvas);
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.NearestFilter;
+    return texture;
+};
 // ---------- ----------
 // POINTS
 // ---------- ----------
@@ -28,18 +58,51 @@ geo_source.setIndex([ 2,1,0, 0,1,3, 1,2,3, 2,0,3 ]);
 // non indexd geo from an indexed one
 const geo = geo_source.toNonIndexed();
 geo.computeVertexNormals();
+// uv attribute
+const pos = geo.getAttribute('position');
+let i = 0;
+const uv = [
+    1,1,
+    0,1,
+    0,0,
+    1,1,
+    0,1,
+    0,0,
+    1,1,
+    0,1,
+    0,0,
+    1,1,
+    0,1,
+    0,0
+];
+/*
+while(i < pos.count){
+   const faceIndex = Math.floor(i / 3);
+   uv.push(0,1);
+   i += 1;
+}
+*/
+geo.setAttribute('uv', new THREE.BufferAttribute( new Float32Array(uv), 2 ))
+console.log(geo)
 // ---------- ----------
 // LIGHT
 // ---------- ----------
 const dl = new THREE.DirectionalLight(0xffffff, 1);
 dl.position.set(0.2,1,0.1)
 scene.add(dl);
+//-------- ----------
+// TEXTURE
+//-------- ----------
+const texture = createCanvasTexture(draw_tri, 64);
 // ---------- ----------
 // Mesh, MeshPhongMaterial
 // ---------- ----------
-const material = new THREE.MeshPhongMaterial();
+const material = new THREE.MeshPhongMaterial({map: texture});
 const mesh = new THREE.Mesh(geo, material);
 scene.add(mesh);
+//const box = new THREE.Mesh( new THREE.BoxGeometry(2,2,2), material );
+//scene.add(box);
+
 // ---------- ----------
 // RENDER
 // ---------- ----------
