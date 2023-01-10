42b837e5759056e333dfbe451aaa4e27a6ee1cb9
dustinpfister <dustin.pfister@gmail.com>
2023-01-10 10:02:41 -0500

    fixed up basic example

--- views/forpost/threejs-box-helper/s1-basic/main.js
+++ views/forpost/threejs-box-helper/s1-basic/main.js
@@ -1,18 +1,23 @@
-// a mesh
-var mesh = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 30, 30), 
-    new THREE.MeshNormalMaterial());
-// add a box helper
-mesh.add(new THREE.BoxHelper(mesh, 0xffffff));
-// start a scene
-var scene = new THREE.Scene();
-// add the mesh to the scene
-scene.add(mesh);
-// everything else
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
 camera.position.set(2, 2, 2);
 camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+//-------- ----------
+// MESH
+//-------- ----------
+const mesh = new THREE.Mesh( new THREE.SphereGeometry(1, 30, 30),  new THREE.MeshNormalMaterial());
+scene.add( mesh );
+//-------- ----------
+// BOX HELPER OF MESH
+//-------- ----------
+mesh.add( new THREE.BoxHelper(mesh, 0xffffff) );
+//-------- ----------
+// RENDER
+//-------- ----------
 renderer.render(scene, camera);
