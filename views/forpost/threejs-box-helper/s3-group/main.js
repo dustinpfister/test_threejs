9a0d035e571807cb42f5fae24755649b6a3ad887
dustinpfister <dustin.pfister@gmail.com>
2023-01-10 10:07:36 -0500

    updated group example

--- views/forpost/threejs-box-helper/s3-group/main.js
+++ views/forpost/threejs-box-helper/s3-group/main.js
@@ -1,6 +1,19 @@
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(10, 10));
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+camera.position.set(5, 5, 5);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+//-------- ----------
+// GROUP
+//-------- ----------
 // create a GROUP
-var group = new THREE.Group();
-
+const group = new THREE.Group();
 // adding a Sphere, and box to the group, and
 // adjusting the position of one of the children of the group
 group.add(new THREE.Mesh(
@@ -10,25 +23,14 @@ group.add(new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1), 
     new THREE.MeshNormalMaterial()));
 group.children[1].position.set(2, 0, 0);
-
 // BOX HELPER
-group.add(new THREE.BoxHelper(group, 0xffffff));
-
+group.add( new THREE.BoxHelper(group, 0xffffff) );
 // Once the helper is added I can then change the position
 group.position.set(0,0,0);
 group.rotation.set(0,1,1);
-
-// start a scene
-var scene = new THREE.Scene();
-scene.add(new THREE.GridHelper(8, 8, 0xffffff, 0xff0000));
 // add the GROUP to the scene
 scene.add(group);
-
-// everything else
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(5, 5, 5);
-camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
+//-------- ----------
+// RENDER
+//-------- ----------
 renderer.render(scene, camera);
