f26d98cd74416e239a96b4313f725c276dfb7168
dustinpfister <dustin.pfister@gmail.com>
2023-01-12 11:58:35 -0500

    s1-3-basic-shader-chunk-code example done

--- views/forpost/threejs-shader-material/s1-3-basic-shader-chunk-code/main.js
+++ views/forpost/threejs-shader-material/s1-3-basic-shader-chunk-code/main.js
@@ -9,7 +9,7 @@ const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
 // ---------- ----------
-// SHADER OBJECT - Using THREE.ShaderChunk for the vertex and fragment shaders
+// SHADER OBJECT - Using GLSL code from THREE.ShaderChunk
 // ---------- ----------
 const shdaer_basic =  {
     // just a default diffuse color of cyan for uniforms
@@ -17,8 +17,7 @@ const shdaer_basic =  {
         'diffuse': { value: new THREE.Color(1, 1, 1) }
     },
     // just using the same code from 'MeshBasicMaterial' for
-    // vertex and fragment shaders
-    //vertexShader: THREE.ShaderChunk[ 'meshbasic_vert' ],
+    // vertex and fragment shaders but now I am puling the actual shader code in
     vertexShader: [
         '#include <common>',
         '#include <uv_pars_vertex>', 
@@ -53,12 +52,58 @@ const shdaer_basic =  {
         '    #include <fog_vertex>',
         '}'
     ].join('\n'),
-
-    fragmentShader: THREE.ShaderChunk[ 'meshbasic_frag' ],
+    fragmentShader: [
+        'uniform vec3 diffuse;',
+        'uniform float opacity;',
+        '#ifndef FLAT_SHADED',
+        '    varying vec3 vNormal;',
+        '#endif',
+        '#include <common>',
+        '#include <dithering_pars_fragment>',
+        '#include <color_pars_fragment>',
+        '#include <uv_pars_fragment>',
+        '#include <uv2_pars_fragment>',
+        '#include <map_pars_fragment>',
+        '#include <alphamap_pars_fragment>',
+        '#include <alphatest_pars_fragment>',
+        '#include <aomap_pars_fragment>',
+        '#include <lightmap_pars_fragment>',
+        '#include <envmap_common_pars_fragment>',
+        '#include <envmap_pars_fragment>',
+        '#include <fog_pars_fragment>',
+        '#include <specularmap_pars_fragment>',
+        '#include <logdepthbuf_pars_fragment>',
+        '#include <clipping_planes_pars_fragment>',
+        'void main() {',
+        '    #include <clipping_planes_fragment>',
+        '    vec4 diffuseColor = vec4( diffuse, opacity );',
+        '    #include <logdepthbuf_fragment>',
+        '    #include <map_fragment>',
+        '    #include <color_fragment>',
+        '    #include <alphamap_fragment>',
+        '    #include <alphatest_fragment>',
+        '    #include <specularmap_fragment>',
+        '    ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );',
+        '    #ifdef USE_LIGHTMAP',
+        '        vec4 lightMapTexel = texture2D( lightMap, vUv2 );',
+        '        reflectedLight.indirectDiffuse += lightMapTexel.rgb * lightMapIntensity * RECIPROCAL_PI;',
+        '    #else',
+        '        reflectedLight.indirectDiffuse += vec3( 1.0 );',
+        '    #endif',
+        '    #include <aomap_fragment>',
+        '    reflectedLight.indirectDiffuse *= diffuseColor.rgb;',
+        '    vec3 outgoingLight = reflectedLight.indirectDiffuse;',
+        '    #include <envmap_fragment>',
+        '    #include <output_fragment>',
+        '    #include <tonemapping_fragment>',
+        '    #include <encodings_fragment>',
+        '    #include <fog_fragment>',
+        '    #include <premultiplied_alpha_fragment>',
+        '    #include <dithering_fragment>',
+        '}'
+    ].join('\n')
 };
-
 //console.log(THREE.ShaderChunk[ 'meshbasic_frag' ])
-
 // ---------- ----------
 // SHADER MATERIAL
 // ---------- ----------
