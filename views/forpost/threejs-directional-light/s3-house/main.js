b1689bf238480252c9d6609e39579b0a8d4cc2e1
dustinpfister <dustin.pfister@gmail.com>
2023-01-17 10:42:26 -0500

    main

--- views/forpost/threejs-directional-light/s3-house/main.js
+++ views/forpost/threejs-directional-light/s3-house/main.js
@@ -1,31 +1,31 @@
-var scene = new THREE.Scene();
-
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+const renderer = new THREE.WebGL1Renderer();
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+renderer.setSize(640, 480, false);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// LIGHT - directional and ambient
+//-------- ----------
 // directional light
-var dl = new THREE.DirectionalLight(0xffffff, 1);
+const dl = new THREE.DirectionalLight(0xffffff, 1);
 dl.castShadow = true;
-scene.add(dl);
- 
 dl.shadow.mapSize.width = 256;
 dl.shadow.mapSize.height = 256;
 dl.shadow.camera.near = 0.5;
 dl.shadow.camera.far = 15;
- 
-// add AmbientLight
-var light = new THREE.AmbientLight(0xffffff);
-light.intensity = 0.1;
-scene.add(light);
- 
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 1, 1000);
-camera.position.set(7, 10, 7);
-camera.lookAt(0, 0, 0);
- 
-var renderer = new THREE.WebGLRenderer();
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
- 
-var materials = {
+scene.add(dl);
+const al = new THREE.AmbientLight(0xffffff);
+al.intensity = 0.1;
+scene.add(al);
+//-------- ----------
+// MATERIALS
+//-------- ----------
+const materials = {
     house_sides: new THREE.MeshStandardMaterial({
         color: 0xffffff
     }),
@@ -33,30 +33,32 @@ var materials = {
         color: 0x00ff00
     })
 };
- 
-var house = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), materials.house_sides);
+//-------- ----------
+// MESH
+//-------- ----------
+const house = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), materials.house_sides);
 house.position.y = 1;
 house.castShadow = true; //default is false
 house.receiveShadow = false; //default
 scene.add(house);
- 
-var plane = new THREE.Mesh(new THREE.PlaneGeometry(12, 12, 8), materials.ground);
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(12, 12, 8), materials.ground);
 plane.rotation.set(-Math.PI / 2, 0, 0);
 plane.castShadow = false; //default is false
 plane.receiveShadow = true; //default
 scene.add(plane);
- 
-// Loop in which the directional light position changes
-var frame = 0,
+//-------- ----------
+// LOOP
+//-------- ----------
+camera.position.set(8, 8, 8);
+camera.lookAt(house.position);
+let frame = 0,
 maxFrame = 100;
-var loop = function () {
+const loop = function () {
     setTimeout(loop, 33);
-    var per = frame / maxFrame,
+    const per = frame / maxFrame,
     r = Math.PI * 2 * per;
- 
     // change directional light position
     dl.position.set(Math.cos(r) * 5, 5, Math.sin(r) * 5 );
- 
     frame = (frame + 1) % maxFrame;
     renderer.render(scene, camera);
 };
