56a49e553f76ec206be5a6675a1d43bea66ad329
dustinpfister <dustin.pfister@gmail.com>
2023-01-18 09:28:58 -0500

    new code style for s1-ambient demo

--- views/forpost/threejs-light/s1-ambient/main.js
+++ views/forpost/threejs-light/s1-ambient/main.js
@@ -1,35 +1,29 @@
-(function () {
-    // ---------- ----------
-    // SCENE, CAMERA, AND RENDERER
-    // ---------- ----------
-    // creating a scene
-    var scene = new THREE.Scene();
-    scene.add( new THREE.GridHelper(6, 6));
-    // camera
-    var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.6, 100);
-    camera.position.set(1, 1, 1);
-    camera.lookAt(0, 0, 0);
-    scene.add(camera);
-    // render
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-    // ---------- ----------
-    // ADDING AMBIENT LIGHT TO THE SCENE
-    // ---------- ----------
-    var light = new THREE.AmbientLight(0x2a2a2a, 0.5);
-    scene.add(light);
-    // ---------- ----------
-    // ADDING A MESH WITH, SPHERE GEOMETRY, AND THE STANDARD MATERIAL TO THE SCENE
-    // ---------- ----------
-    var mesh1 = new THREE.Mesh(
-            new THREE.SphereGeometry(0.5),
-            new THREE.MeshStandardMaterial( { color: new THREE.Color('red') } )
-    );
-    scene.add(mesh1);
-    // ---------- ----------
-    // CALLING RENDER OF RENDERER
-    // ---------- ----------
-    renderer.render(scene, camera);
-}
-    ());
+// ---------- ----------
+// SCENE, CAMERA, AND RENDERER
+// ---------- ----------
+const scene = new THREE.Scene();
+scene.add( new THREE.GridHelper(6, 6));
+const camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.6, 100);
+camera.position.set(1, 1, 1);
+camera.lookAt(0, 0, 0);
+scene.add(camera);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+// ---------- ----------
+// ADDING AMBIENT LIGHT TO THE SCENE
+// ---------- ----------
+const al = new THREE.AmbientLight(0x2a2a2a, 0.5);
+scene.add( al );
+// ---------- ----------
+// ADDING A MESH WITH, SPHERE GEOMETRY, AND THE STANDARD MATERIAL TO THE SCENE
+// ---------- ----------
+const mesh1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5),
+    new THREE.MeshStandardMaterial( { color: new THREE.Color('red') } )
+);
+scene.add(mesh1);
+// ---------- ----------
+// CALLING RENDER OF RENDERER
+// ---------- ----------
+renderer.render(scene, camera);
