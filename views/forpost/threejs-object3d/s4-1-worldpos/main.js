d1bf43d7d1023306d0c8db9386e6d106d749d4ad
dustinpfister <dustin.pfister@gmail.com>
2023-01-23 16:31:07 -0500

    s4-1-worldpos demo looks good

--- views/forpost/threejs-object3d/s4-1-worldpos/main.js
+++ views/forpost/threejs-object3d/s4-1-worldpos/main.js
@@ -9,34 +9,32 @@ const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 (document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
 //-------- ----------
-// CREATING OBJECTS WITH NAMES
+// MESH OBJECT WITH CHILD OBJECTS
 //-------- ----------
-const PREFIX = 'box';
-const COLORS = ['red', 'blue', 'green'];
-const COUNT = COLORS.length;
-let i = 0;
-while(i < COUNT){
-    const geo = new THREE.BoxGeometry();
-    const mat = new THREE.MeshBasicMaterial();
-    const mesh = new THREE.Mesh(geo, mat);
-    mesh.name = PREFIX + i;
-    scene.add(mesh)
-    i += 1;
-}
+const geo = new THREE.SphereGeometry(1, 30, 30);
+const mat = new THREE.MeshNormalMaterial();
+const mesh_parent = new THREE.Mesh(geo, mat);
+const mesh_child = new THREE.Mesh(geo, mat);
+mesh_parent.add(mesh_child);
+scene.add(mesh_parent);
+// setting position values for parent and child objects
+mesh_parent.position.set(-8,0,2);
+mesh_child.position.set(8,0,-4);
 //-------- ----------
-// USING GET By NAME TO GET REFERNCE TO OBJECTS
-//-------- ----------
-COLORS.forEach((colorStr, i) => {
-    const mesh = scene.getObjectByName(PREFIX + i);
-    const a_mpos = i  / ( COUNT - 1 );
-    if(mesh){
-        mesh.position.x = -5 + 10 * a_mpos;
-        mesh.material.color = new THREE.Color(colorStr);
-    }
-});
-//-------- ----------
-// RENDER
+// LOOP
 //-------- ----------
 camera.position.set(10, 10, 10);
-camera.lookAt(0, 0, 0);
-renderer.render(scene, camera);
+const v1 = mesh_child.position.clone();
+const v2 = mesh_child.getWorldPosition( new THREE.Vector3() );
+let f = 0;
+const fm = 90;
+const loop = () => {
+    requestAnimationFrame(loop);
+    const a1 = f / fm;
+    const a2 = 1 - Math.abs(0.5 - a1) / 0.5;
+    camera.lookAt( v2.clone().lerp(v1, a2) );
+    renderer.render(scene, camera);
+    f += 1;
+    f %= fm;
+};
+loop();
