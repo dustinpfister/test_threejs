64d144692b922a0f273a2c858b03ca3ac8df2374
dustinpfister <dustin.pfister@gmail.com>
2023-01-27 12:44:45 -0500

    static render example

--- views/forpost/threejs-examples-house-two/r0-2-basic-video1/main.js
+++ views/forpost/threejs-examples-house-two/r0-2-basic-video1/main.js
@@ -6,6 +6,35 @@ const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+// ---------- ----------
+// ANIMATION LOOP
+// ---------- ----------
+camera.position.set(1.25, 1.25, 1.25);
+camera.lookAt(0, 0, 0);
+const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+FRAME_MAX = 120;
+let secs = 0,
+frame = 0,
+lt = new Date();
+// update
+const update = function(frame, frameMax){};
+// loop
+const loop = () => {
+    const now = new Date(),
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if(secs > 1 / FPS_UPDATE){
+        // update, render
+        update( Math.floor(frame), FRAME_MAX);
+        renderer.render(scene, camera);
+        // step frame
+        frame += FPS_MOVEMENT * secs;
+        frame %= FRAME_MAX;
+        lt = now;
+    }
+};
+loop();
 //-------- ----------
 // LOADING
 //-------- ----------
@@ -31,15 +60,12 @@ DAE_loader({
 })
 .then( (scene_source) => {
     console.log('done loading');
-    // add in source object
+    camera.position.set(2, 1, -2);
     scene.add( new THREE.GridHelper(10, 40) )
     const mesh_house = scene_source.getObjectByName('house_0').clone();
     scene.add( mesh_house )
-    // camera
-    camera.position.set(2, 1, -2);
     camera.lookAt(mesh_house.position);
-    // render
-    renderer.render(scene, camera);
+    loop();
 })
 .catch( (e) => {
     console.warn(e);
