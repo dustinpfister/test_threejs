6f72a4374702421052665d30a25b07083a3f419a
dustinpfister <dustin.pfister@gmail.com>
2023-01-30 12:42:46 -0500

    daehelper module

---
+++ views/forpost/threejs-object3d-traverse/js/dae-helper-r0.js
@@ -0,0 +1,36 @@
+// dae-helper.js - r0 - from threejs-dae-collada-loader
+(function(global){
+    // a hard coded default cloner function
+    const DEFAULT_CLONER = function(obj, scene_source, scene_result, result){
+        scene_source.add(obj.clone());
+    };
+    // The public DAE_loader function
+    global.DAE_loader = function( opt ){
+        opt = opt || {};
+        opt.urls_dae = opt.urls_dae || [];
+        opt.urls_resource = opt.urls_resource || [];
+        // use given cloner or defult to add everything
+        opt.cloner = opt.cloner || DEFAULT_CLONER;
+        const manager = new THREE.LoadingManager();
+        const scene_source = new THREE.Scene();
+        return new Promise( (resolve, reject) => {
+            manager.onError = function(url){
+                reject( new Error( 'error when loading: ' + url ) );
+            };
+            manager.onLoad = function(){
+                resolve(scene_source);
+            };
+            opt.urls_dae.forEach((url, i) => {
+                const loader = new THREE.ColladaLoader( manager );
+                if(opt.urls_resource[i]){
+                    loader.setResourcePath( opt.urls_resource[i] );
+                }
+                loader.load(url, function(result){
+                    result.scene.traverse( (obj) => {
+                          opt.cloner(obj, scene_source, result.scene, result);
+                    });
+                });
+            });
+        });
+    };
+}( window ));
---
+++ views/forpost/threejs-object3d-traverse/s3-1-daehelper/main.js
@@ -0,0 +1,72 @@
+// ---------- ----------
+// SCENE, CAMERA, RENDERER
+// ---------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+// ---------- ----------
+// ANIMATION LOOP
+// ---------- ----------
+camera.position.set(1.25, 1.25, 1.25);
+camera.lookAt(0, 0, 0);
+const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+FRAME_MAX = 120;
+let secs = 0,
+frame = 0,
+lt = new Date();
+// update
+const update = function(frame, frameMax){};
+// loop
+const loop = () => {
+    const now = new Date(),
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if(secs > 1 / FPS_UPDATE){
+        // update, render
+        update( Math.floor(frame), FRAME_MAX);
+        renderer.render(scene, camera);
+        // step frame
+        frame += FPS_MOVEMENT * secs;
+        frame %= FRAME_MAX;
+        lt = now;
+    }
+};
+loop();
+//-------- ----------
+// LOADING
+//-------- ----------
+DAE_loader({
+    // custom cloner
+    cloner: (obj, scene_source ) => {
+        if(obj.type === 'Mesh'){
+            const mat = new THREE.MeshBasicMaterial({
+                map: obj.material.map
+            });
+            const mesh = new THREE.Mesh(obj.geometry, mat);
+            mesh.name = obj.name;
+            mesh.rotation.copy(obj.rotation);
+            scene_source.add(mesh);
+        }
+    },
+    urls_dae: [
+        '/dae/house_two/house_2.dae'
+    ],
+    urls_resource: [
+        '/dae/house_two/skins/'
+    ]
+})
+.then( (scene_source) => {
+    console.log('done loading');
+    camera.position.set(2, 1, -2);
+    scene.add( new THREE.GridHelper(10, 40) )
+    const mesh_house = scene_source.getObjectByName('house_0').clone();
+    scene.add( mesh_house )
+    camera.lookAt(mesh_house.position);
+    loop();
+})
+.catch( (e) => {
+    console.warn(e);
+});
