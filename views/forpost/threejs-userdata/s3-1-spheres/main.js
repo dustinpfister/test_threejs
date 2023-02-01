b8b96c7a6c8526a396773dd6ddb7280a2a4391df
dustinpfister <dustin.pfister@gmail.com>
2023-02-01 10:50:20 -0500

    todo

--- views/forpost/threejs-userdata/s3-1-spheres/main.js
+++ views/forpost/threejs-userdata/s3-1-spheres/main.js
@@ -1,103 +1,99 @@
-(function () {
- 
-    var GROUPSIZE = 9,
-    MAXDIST = 5,
-    PPS_MIN = 1,
-    PPS_MAX = 7;
- 
-    // materials
-    var materials = [
-        new THREE.MeshBasicMaterial({
-            color: 0xff0000,
-            wireframe: true
-        }),
-        new THREE.MeshBasicMaterial({
-            color: 0x00ff00,
-            wireframe: true
-        }),
-        new THREE.MeshBasicMaterial({
-            color: 0x0000ff,
-            wireframe: true
-        })
-    ];
-    // random angles helper
-    var randomAngles = function (mesh) {
-        mesh.userData.pitch = Math.PI * 2 * Math.random();
-        mesh.userData.heading = Math.PI * 2 * Math.random();
-    };
-    // random speed value
-    var randomSpeed = function (mesh) {
-        mesh.userData.pitchPPS = PPS_MIN + (PPS_MAX - PPS_MIN) * Math.random();
-        mesh.userData.headingPPS = PPS_MIN + (PPS_MAX - PPS_MIN) * Math.random();
-    };
-    // create a sphere group
-    var createSphereGroup = function () {
-        var group = new THREE.Group();
-        var i = 0;
-        while (i < GROUPSIZE) {
-            var mesh = new THREE.Mesh(
-                    new THREE.SphereGeometry(1, 20),
-                    materials[0]);
-            // SETTING VALUES IN USER DATA OBJECT
-            mesh.userData.materalIndex = i % materials.length;
-            randomSpeed(mesh);
+// SCENE, CAMERA, RENDERER
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 100);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+//-------- ----------
+// SETTINGS
+//-------- ----------
+const GROUPSIZE = 9,
+MAXDIST = 5,
+PPS_MIN = 1,
+PPS_MAX = 7;
+// materials
+const materials = [
+    new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        wireframe: true
+    }),
+    new THREE.MeshBasicMaterial({
+        color: 0x00ff00,
+        wireframe: true
+    }),
+    new THREE.MeshBasicMaterial({
+        color: 0x0000ff,
+        wireframe: true
+    })
+];
+//-------- ----------
+// HELPERS
+//-------- ----------
+// random angles helper
+const randomAngles = function (mesh) {
+    mesh.userData.pitch = Math.PI * 2 * Math.random();
+    mesh.userData.heading = Math.PI * 2 * Math.random();
+};
+// random speed value
+const randomSpeed = function (mesh) {
+    mesh.userData.pitchPPS = PPS_MIN + (PPS_MAX - PPS_MIN) * Math.random();
+    mesh.userData.headingPPS = PPS_MIN + (PPS_MAX - PPS_MIN) * Math.random();
+};
+// create a sphere group
+const createSphereGroup = function () {
+    const group = new THREE.Group();
+    let i = 0;
+    while (i < GROUPSIZE) {
+        const mesh = new THREE.Mesh(
+                new THREE.SphereGeometry(1, 20),
+                materials[0]);
+        // SETTING VALUES IN USER DATA OBJECT
+        mesh.userData.materalIndex = i % materials.length;
+        randomSpeed(mesh);
+        randomAngles(mesh);
+        group.add(mesh);
+        i += 1;
+    }
+    return group;
+};
+// update a sphere group
+const updateSphereGroup = function (group, secs) {
+    group.children.forEach(function (mesh) {
+        // USING VALUES IN USER DATA OBJECT
+        const ud = mesh.userData;
+        mesh.material = materials[ud.materalIndex];
+        mesh.position.x += Math.cos(ud.pitch) * ud.pitchPPS * secs;
+        mesh.position.y += Math.sin(ud.pitch) * ud.pitchPPS * secs;
+        mesh.position.z += Math.cos(ud.heading) * ud.headingPPS * secs;
+        const d = mesh.position.distanceTo(new THREE.Vector3(0, 0, 0));
+        if (d >= MAXDIST) {
+            mesh.position.set(0, 0, 0);
             randomAngles(mesh);
-            group.add(mesh);
-            i += 1;
+            randomSpeed(mesh);
         }
-        return group;
-    };
-    // update a sphere group
-    var updateSphereGroup = function (group, secs) {
-        group.children.forEach(function (mesh) {
-            // USING VALUES IN USER DATA OBJECT
-            var ud = mesh.userData;
-            mesh.material = materials[ud.materalIndex];
-            mesh.position.x += Math.cos(ud.pitch) * ud.pitchPPS * secs;
-            mesh.position.y += Math.sin(ud.pitch) * ud.pitchPPS * secs;
-            mesh.position.z += Math.cos(ud.heading) * ud.headingPPS * secs;
-            var d = mesh.position.distanceTo(new THREE.Vector3(0, 0, 0));
-            if (d >= MAXDIST) {
-                mesh.position.set(0, 0, 0);
-                randomAngles(mesh);
-                randomSpeed(mesh);
-            }
-        });
-    };
- 
-    // Scene
-    var scene = new THREE.Scene();
- 
-    // Camera
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(8, 8, 8);
-    camera.lookAt(0, 0, 0);
- 
-    // create and add sphere group
-    var group = createSphereGroup();
-    updateSphereGroup(group, 0);
-    scene.add(group);
- 
-    // Render
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
- 
-    // loop
-    var lt = new Date(),
-    fps = 24;
-    function loop() {
-        var now = new Date(),
-        secs = (now - lt) / 1000;
-        requestAnimationFrame(loop);
-        if (secs > 1 / fps) {
-            updateSphereGroup(group, secs);
-            renderer.render(scene, camera);
-            lt = now;
-        }
-    };
- 
-    loop();
- 
-}
-    ());
+    });
+};
+//-------- ----------
+// OBJECTS
+//-------- ----------
+const group = createSphereGroup();
+updateSphereGroup(group, 0);
+scene.add(group);
+//-------- ----------
+// LOOP
+//-------- ----------
+camera.position.set(8, 8, 8);
+camera.lookAt(0, 0, 0);
+let lt = new Date();
+const fps = 24;
+const loop = () => {
+    const now = new Date(),
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if (secs > 1 / fps) {
+        updateSphereGroup(group, secs);
+        renderer.render(scene, camera);
+        lt = now;
+    }
+};
+loop();
--- views/forpost/threejs-userdata/todo.md
+++ views/forpost/threejs-userdata/todo.md
@@ -1,5 +1,8 @@
 # threejs-userdata todo list
 
-## ( done 04/07/2022 ) basic example folder
+## ( done 02/01/2023 ) - code examples updated to r146 style
+* (done) all examples updated to r146 style
+
+## ( done 04/07/2022 )  - basic example folder
 * (done) have folders for all current examples in the post
 * (done) make sure they are all working with r135
