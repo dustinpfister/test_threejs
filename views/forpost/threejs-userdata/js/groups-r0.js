caf790111b5a6a6d605a41917a82b57f71b7dbbc
dustinpfister <dustin.pfister@gmail.com>
2023-02-01 12:06:16 -0500

    removed demo code from module

--- views/forpost/threejs-userdata/js/groups-r0.js
+++ views/forpost/threejs-userdata/js/groups-r0.js
@@ -1,14 +1,11 @@
 // groups.js - r0 - from threejs-userdata
 (function (api) {
- 
     var ANGLES_A = [225, 315, 135, 45];
- 
     var toRadians = function (array) {
         return array.map(function(deg){
             return Math.PI / 180 * deg;
         });
     };
- 
     // create a single cube mesh
     var createCube = function (rotationCounts, position) {
         var cube = new THREE.Mesh(
@@ -20,7 +17,6 @@
         cube.position.copy(position || new THREE.Vector3(0, 0, 0));
         return cube;
     };
- 
     // update a single cube
     var updateCube = function (cube, per) {
         var ud = cube.userData,
@@ -30,7 +26,6 @@
         cube.rotation.y = pi2 * rc[1] * per;
         cube.rotation.z = pi2 * rc[2] * per;
     };
- 
     // public method to create a cube group
     api.create = function(opt) {
         opt = opt || {};
@@ -56,14 +51,12 @@
         };        
         return cubes;
     };
- 
     var setCubesRotation = function(cubes, per){
         var x = Math.PI * 0 * per,
         y = Math.PI * 0 * per,
         z = Math.PI * 0 * per;
         cubes.rotation.set(x, y, z);
     };
- 
     // update the group
     api.update = function(cubes, secs) {
         // GROUP USER DATA OBJECT
@@ -98,72 +91,4 @@
             gud.secs %= 1 / gud.fps; 
         }
     };
- 
-}
-    (this['CubeGroupMod'] = {}));
-
-(function () {
- 
-    var scene = new THREE.Scene();
- 
-    var gridHelper = new THREE.GridHelper(10, 10);
-    scene.add(gridHelper);
- 
-    var cubes1 = CubeGroupMod.create({maxFrame: 50, fps: 30});
-    cubes1.position.set(4,0,4);
-    scene.add(cubes1);
- 
-    var cubes2 = CubeGroupMod.create({maxFrame: 50, fps: 1});
-    cubes2.position.set(-4,0,4);
-    scene.add(cubes2);
- 
-    var cubes3 = CubeGroupMod.create({
-       anglesA:[180, 270, 90, 0],
-       yDelta: 1.25,
-       xzDelta: 0.75,
-       maxFrame: 60,
-       fps: 30,
-       cubeRotations: [
-          [0, 0, 1],
-          [0, 1, 0],
-          [0, 1, 1],
-          [1, 0, 0],
-          [1, 0, 1],
-          [1, 1, 0],
-          [1, 1, 1],
-          [0, 0, 1]
-       ]
-    });
-    scene.add(cubes3);
- 
-    // Camera
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(0, 10, 10);
-    camera.lookAt(0, 0, 0);
-    // Render
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-   
-    var controls = new THREE.OrbitControls(camera, renderer.domElement);
- 
-    // loop
-    var lt = new Date(),
-    fps = 24;
-    function loop() {
-        var now = new Date(),
-        secs = (now - lt) / 1000;
-        requestAnimationFrame(loop);
-        if (secs > 1 / fps) {
-            CubeGroupMod.update(cubes1, secs);
-            CubeGroupMod.update(cubes2, secs);
-            CubeGroupMod.update(cubes3, secs);
-            renderer.render(scene, camera);
-            lt = now;
-        }
-    };
- 
-    loop();
- 
-}
-    ());
+}(this['CubeGroupMod'] = {}));
