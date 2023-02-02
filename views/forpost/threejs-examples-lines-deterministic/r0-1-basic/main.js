3b5fac97cc4a22f7283a3c9935b9cb5cfdf0c111
dustinpfister <dustin.pfister@gmail.com>
2023-02-02 10:02:03 -0500

    using all

--- views/forpost/threejs-examples-lines-deterministic/r0-1-basic/main.js
+++ views/forpost/threejs-examples-lines-deterministic/r0-1-basic/main.js
@@ -1,20 +1,19 @@
-//******** **********
+//-------- ----------
 // SCENE, CAMERA, RENDERER
-//******** **********
-var scene = new THREE.Scene();
+//-------- ----------
+const scene = new THREE.Scene();
 scene.background = new THREE.Color('#000000');
 scene.add( new THREE.GridHelper(10, 10, 0x00ff00, 0x4a4a4a) )
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(10, 10, 0);
-camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
-//******** **********
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+camera.position.set(12, 12, 0);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
 // LINES GROUP(s)
-//******** **********
+//-------- ----------
 // built in 'tri' type
-var lg1Base = {
+const lg1Base = {
     homeVectors: [
         new THREE.Vector3(0, 0, 0),
         new THREE.Vector3(0, 0, 0),
@@ -28,28 +27,31 @@ var lg1Base = {
     rBase: 0,
     rDelta: 2
 };
-var lg1 = LineGroup.create();
+const lg1 = LineGroup.create();
 lg1.position.set(3, 0, 0);
 lg1.scale.set(0.5, 0.5, 0.5)
 scene.add(lg1);
 // the 'circleStack' type
-var lg2 = LineGroup.create('circleStack', { lineCount: 20 } );
+const lg2 = LineGroup.create('circleStack', { lineCount: 20 } );
 lg2.position.set(-5, 0, -5);
 scene.add(lg2);
 // the 'sphereCircles' type base off my other lines example
-var lg3 = LineGroup.create('sphereCircles', { } );
+const lg3 = LineGroup.create('sphereCircles', { } );
 lg3.position.set(-5, 0, 5);
 scene.add(lg3);
-//******** **********
+//-------- ----------
 // LOOP
-//******** **********
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-var fps = 30,
-lt = new Date(),
-frame = 0,
+//-------- ----------
+let controls = null;
+if(THREE.OrbitControls){
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+}
+const fps = 30,
 frameMax = 90;
-var loop = function () {
-    var now = new Date(),
+let lt = new Date(),
+frame = 0;
+const loop = function () {
+    const now = new Date(),
     secs = (now - lt) / 1000;
     requestAnimationFrame(loop);
     if(secs > 1 / fps){
