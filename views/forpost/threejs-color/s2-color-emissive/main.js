3163270b69e77e58651496f7ae7611d6b4357c31
dustinpfister <dustin.pfister@gmail.com>
2023-02-08 11:09:46 -0500

    s2-color-emissive demo of threejs-color updated to r146 style

--- views/forpost/threejs-color/s2-color-emissive/main.js
+++ views/forpost/threejs-color/s2-color-emissive/main.js
@@ -1,30 +1,32 @@
-// creating a scene
-var scene = new THREE.Scene();
-
-// Box With a material that uses a color, and emissive color
-var box = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshStandardMaterial({
-            color: new THREE.Color(1, 1, 1),
-            emissiveIntensity: 0.8,
-            emissive: new THREE.Color(1, 0, 0)
-        }));
-
-// add the box mesh to the scene
-scene.add(box);
-
-// ADD A LIGHT BECUASE THIS IS THE STANDARD MATERIAL
-var light = new THREE.PointLight(new THREE.Color(1, 1, 1));
-light.position.set(1, 3, 2);
-scene.add(light);
-
-// camera
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(1, 1, 1);
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// OBJECTS
+//-------- ----------
+var mesh1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshStandardMaterial({
+        color: new THREE.Color(1, 1, 1),
+        emissiveIntensity: 0.8,
+        emissive: new THREE.Color(1, 0, 0)
+    })
+);
+scene.add(mesh1);
+//-------- ----------
+// LIGHT
+//-------- ----------
+var pl = new THREE.PointLight(new THREE.Color(1, 1, 1));
+pl.position.set(1, 3, 2);
+scene.add(pl);
+//-------- ----------
+// RENDER
+//-------- ----------
+camera.position.set(1, 1.5, 1);
 camera.lookAt(0, 0, 0);
-// renderer
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
-
 renderer.render(scene, camera);
