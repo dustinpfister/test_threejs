6199ba62428cfeef1086cb2969a95d5c2bf94241
dustinpfister <dustin.pfister@gmail.com>
2023-02-16 13:27:34 -0500

    s2-1-group updated to r146 style

--- views/forpost/threejs-capsule-geometry/s2-1-group/main.js
+++ views/forpost/threejs-capsule-geometry/s2-1-group/main.js
@@ -1,19 +1,16 @@
-//******** **********
+//-------- ----------
 // SCENE, CAMERA, RENDERER
-//******** **********
-var scene = new THREE.Scene();
+//-------- ----------
+const scene = new THREE.Scene();
 scene.add( new THREE.GridHelper(10, 10) );
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(-10, 5, 10);
-camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
-//******** **********
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+//-------- ----------
 // GROUP OF MESH OBJECTS
-//******** **********
-// array of vector values
-var vectors = [
+//-------- ----------
+const vectors = [
     [0, 0, 0 ],
     [ 0, -5, -5 ],
     [ 0, -5, 0 ],
@@ -22,27 +19,25 @@ var vectors = [
     [ 4, 5, 4 ],
     [ 4, 5, -5 ],
     [ -5, 5, -5 ]
-];
-// turn vectors array into an array of vector3 instances
-vectors = vectors.map(function(a){
+].map(function(a){
     return new THREE.Vector3( a[0], a[1], a[2] );
 });
 // create a group and add that to the scene
-var group = new THREE.Group();
+const group = new THREE.Group();
 scene.add( group );
 // make mesh objects and add them to the group
-var i = 0,
-thickness = 0.75,
+let i = 0;
+const thickness = 0.75,
 len = vectors.length;
 while(i < len - 1){
-    var v = vectors[i],
+    const v = vectors[i],
     nv = vectors[i + 1],
     d = v.distanceTo(nv); // distance from current vector to next vector
-    var mesh = new THREE.Mesh(
+    const mesh = new THREE.Mesh(
         new THREE.CapsuleGeometry(thickness, d, 10, 20),
         new THREE.MeshNormalMaterial({wireframe: true}));
     // position should be a mid point between v and nv
-    var mv = v.add(nv).divideScalar(2);
+    const mv = v.add(nv).divideScalar(2);
     mesh.position.copy(mv);
     // adjust geo to work well with lookAt
     mesh.geometry.rotateX(Math.PI * 0.5);
@@ -51,7 +46,9 @@ while(i < len - 1){
     group.add(mesh);
     i += 1;
 }
-//******** **********
+//-------- ----------
 // RENDER THE SCENE
-//******** **********
+//-------- ----------
+camera.position.set(-10, 5, 10);
+camera.lookAt(0, 0, 0);
 renderer.render(scene, camera);
