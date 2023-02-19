7e645faa2684ad836eef26d6f430b43e251f1796
dustinpfister <dustin.pfister@gmail.com>
2023-02-19 07:41:39 -0500

    s2-2-loop-video2 demo updated to r146 style

--- views/forpost/threejs-emissive-map/s2-2-loop-video2/main.js
+++ views/forpost/threejs-emissive-map/s2-2-loop-video2/main.js
@@ -1,31 +1,40 @@
-//******** **********
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+scene.add( new THREE.GridHelper(10, 10));
+const camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480, false);
+( document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+//-------- ----------
 // DATA TEXTURE METHODS
-//******** **********
+//-------- ----------
 // make a data texture by passing w, h, and a function
 // that will be called for each pixel
-var datatex = (function () {
-    var api = {};
+const datatex = (function () {
+    const api = {};
     // mk data texture helper
-    api.mkDataTexture = function (data, w) {
+    api.mkDataTexture = (data, w) => {
         data = data || [];
         w = w || 0;
-        var width = w,
+        const width = w,
         height = data.length / 4 / w;
-        var texture = new THREE.DataTexture(data, width, height);
+        const texture = new THREE.DataTexture(data, width, height);
         texture.needsUpdate = true;
         return texture;
     };
     // create a data texture with a method that will be called for each pix
-    api.forEachPix = function (w, h, forEach) {
-        var width = w === undefined ? 5 : w,
+    api.forEachPix = (w, h, forEach) => {
+        const width = w === undefined ? 5 : w,
         height = h === undefined ? 5 : h;
-        var size = width * height;
-        var data = new Uint8Array(4 * size);
+        const size = width * height;
+        const data = new Uint8Array(4 * size);
         for (let i = 0; i < size; i++) {
-            var stride = i * 4;
-            var x = i % width;
-            var y = Math.floor(i / width);
-            var obj = forEach(x, y, w, h, i, stride, data);
+            const stride = i * 4;
+            const x = i % width;
+            const y = Math.floor(i / width);
+            let obj = forEach(x, y, w, h, i, stride, data);
             obj = obj || {};
             data[stride] = obj.r || 0;
             data[stride + 1] = obj.g || 0;
@@ -39,12 +48,12 @@ var datatex = (function () {
 }
     ());
 // square texture
-var circleTexture = function(t){
+const circleTexture = function(t){
     t = t === undefined ? 1 : t;
-    return datatex.forEachPix(16, 16, function(x, y, w, h, i, stride, data){
-        var v = new THREE.Vector2(x, y);
-        var d = v.distanceTo( new THREE.Vector2(w / 2, h / 2) );
-        var cv = d / ( 16 * t ) * 255;
+    return datatex.forEachPix(16, 16, (x, y, w, h, i, stride, data) => {
+        const v = new THREE.Vector2(x, y);
+        const d = v.distanceTo( new THREE.Vector2(w / 2, h / 2) );
+        let cv = d / ( 16 * t ) * 255;
         cv = cv > 255 ? 255 : cv;
         cv = cv < 0 ? 0 : cv;
         return {
@@ -53,9 +62,9 @@ var circleTexture = function(t){
     });
 };
 //  rnd
-var rndTexture = function(){
-    return datatex.forEachPix(16, 16, function(x, y, w, h, i, stride, data){
-        var cv = Math.round( Math.random() * 255 );
+const rndTexture = function(){
+    return datatex.forEachPix(16, 16, (x, y, w, h, i, stride, data) => {
+        const cv = Math.round( Math.random() * 255 );
         return {
             r: cv,
             g: cv,
@@ -63,29 +72,18 @@ var rndTexture = function(){
         };
     });
 };
-//******** **********
-// SCENE, CAMERA, RENDERER
-//******** **********
-var scene = new THREE.Scene();
-scene.add( new THREE.GridHelper(10, 10));
-var camera = new THREE.PerspectiveCamera(40, 320 / 240, 0.1, 1000);
-camera.position.set(10, 5, 10);
-camera.lookAt(0, 0, 0);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(640, 480);
-document.getElementById('demo').appendChild(renderer.domElement);
-//******** **********
+//-------- ----------
 // LIGHT
-//******** **********
-var dl = new THREE.DirectionalLight(0xafafaf, 0.5);
+//-------- ----------
+const dl = new THREE.DirectionalLight(0xafafaf, 0.5);
 dl.position.set(8, 10, 2);
 scene.add(dl);
-var helper = new THREE.DirectionalLightHelper( dl, 5 );
+const helper = new THREE.DirectionalLightHelper( dl, 5 );
 scene.add( helper );
-//******** **********
+//-------- ----------
 // MESH
-//******** **********
-var mesh = new THREE.Mesh(
+//-------- ----------
+const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(5, 5, 5),
     new THREE.MeshStandardMaterial({
         emissive: new THREE.Color('white'),
@@ -93,37 +91,43 @@ var mesh = new THREE.Mesh(
     })
 );
 scene.add(mesh);
-//******** **********
-// LOOP
-//******** **********
-var fps = 30,
-lt = new Date(),
+// ---------- ----------
+// ANIMATION LOOP
+// ---------- ----------
+camera.position.set(10, 5, 10);
+camera.lookAt(0, 0, 0);
+const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+FRAME_MAX = 800;
+let secs = 0,
 frame = 0,
-maxFrame = 300;
-var loop = function () {
-    var now = new Date(),
-    per = frame / maxFrame,
-    bias = 1 - Math.abs(0.5 - per) / 0.5,
+lt = new Date();
+// update
+const update = function(frame, frameMax){
+    const a1 = frame / frameMax,
+    a2 = 1 - Math.abs(0.5 - a1) / 0.5;
+    mesh.material.emissiveMap = circleTexture(a2 * 4);
+    // new color map
+    mesh.material.map = rndTexture();
+    // moving directional light
+    var r = Math.PI * 4 * a1,
+    x = Math.cos(r) * 4,
+    z = Math.sin(r) * 4;
+    dl.position.set(x, 4, z);
+    helper.update();
+};
+// loop
+const loop = () => {
+    const now = new Date(),
     secs = (now - lt) / 1000;
     requestAnimationFrame(loop);
-    if(secs > 1 / fps){
- 
-        // new emmisiveMap
-        mesh.material.emissiveMap = circleTexture(bias * 4);
-        // new color map
-        mesh.material.map = rndTexture();
- 
-        // moving directional light
-        var r = Math.PI * 4 * per,
-        x = Math.cos(r) * 4,
-        z = Math.sin(r) * 4;
-        dl.position.set(x, 4, z);
- 
-        helper.update();
- 
+    if(secs > 1 / FPS_UPDATE){
+        // update, render
+        update( Math.floor(frame), FRAME_MAX);
         renderer.render(scene, camera);
-        frame += fps * secs;
-        frame %= maxFrame;
+        // step frame
+        frame += FPS_MOVEMENT * secs;
+        frame %= FRAME_MAX;
         lt = now;
     }
 };
--- views/forpost/threejs-emissive-map/todo.md
+++ views/forpost/threejs-emissive-map/todo.md
@@ -1,16 +1,24 @@
 # threejs-emissive-map todo list
 
-## () s4-emissive-intensity-loop
-* have an example where emissive intesnity is updated for a collection of mesh objects
+## () s2-4-basic-emissive-intensity
+* () basic static example of emsiive intensity
+* () create many mesh objects rather than having a loop
+
+## () s1-3-basic-textureloader
+* () basic texture loader example
+
+## () make s1-1-basic-datatexture more basic
+* () see about making the first data texture exmaple more basic by mabe jjust having one mesh object
+* () reduce the volume of code by any means
 
 ## () s2-1-loop-video1
 * () new demo based on what I made for video1 for the post
 
-## () rename folders and update to r146
+## ( done 02/19/2023 ) rename folders and update to r146
 * (done) rename s1-data-texture to s1-1-basic-datatexture
 * (done) rename s2-canvas-texture to s1-2-basic-canvastexture
 * (done) rename s3-ani-loop to s2-2-loop-video2
-* () update all demos to r146 code style
+* (done) update all demos to r146 code style
 
 ## ( done 07/25/2022 ) - new s3-ani-loop example
 * (done) start a new example that will be an animation loop
