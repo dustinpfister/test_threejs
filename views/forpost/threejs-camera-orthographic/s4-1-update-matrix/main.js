47215a651bfb172a9359aae71a196c5aeb016ed8
dustinpfister <dustin.pfister@gmail.com>
2023-02-21 12:30:24 -0500

    s4-1-update demo updated to r146 style

--- views/forpost/threejs-camera-orthographic/s4-1-update-matrix/main.js
+++ views/forpost/threejs-camera-orthographic/s4-1-update-matrix/main.js
@@ -1,65 +1,74 @@
-(function () {
-    //******** **********
-    // SCENE, RENDERER, LIGHT
-    //******** **********
-    var scene = new THREE.Scene();
-    scene.background = new THREE.Color(0.25, 0.25, 0.25);
-    scene.add(new THREE.GridHelper(10,10));
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-    var light = new THREE.PointLight();
-    light.position.set(0, 3, 6);
-    scene.add(light);
-    //******** **********
-    // CAMERA
-    //******** **********
-    var width = 4.0,
-    height = 4.0;
-    var camera = new THREE.OrthographicCamera(
-            -width,
-            width,
-            height,
-            -height,
-            0.01,
-            100);
-    camera.position.set(5,2,5);
-    camera.lookAt(0, 0, 0);
-    //******** **********
-    // MESH
-    //******** **********
-    var material = new THREE.MeshNormalMaterial();
-    var m1 = new THREE.Mesh( new THREE.BoxGeometry(1,1,1), material );
-    m1.position.set(0, 0.5, 0);
-    scene.add( m1 );
-    var m2 = new THREE.Mesh( new THREE.BoxGeometry(1,3,1), material );
-    m2.position.set(-3, 1.5, 1);
-    scene.add( m2 );
-    var m3 = new THREE.Mesh( new THREE.BoxGeometry(1,3,1), material );
-    m3.position.set(3, 1.5, -1);
-    scene.add( m3 );
-    //******** **********
-    // lOOP
-    //******** **********
-    var frame = 0,
-    maxFrame = 300;
-    var loop = function () {
-        var per = frame / maxFrame;
-        requestAnimationFrame(loop);
-        // SETTING LEFT, RIGHT, TOP, AND BOTTOM PROPS
-        var b = 1 - Math.abs(0.5 - per) / 0.5;
-        width = 4 + 4 * b;
-        height = 4 - 2 * b;
-        camera.left = width * -1;
-        camera.right = width;
-        camera.top = height;
-        camera.bottom = height * -1;
-        // CALLING UPDATE PROJECTION MATRIX METHOD
-        camera.updateProjectionMatrix();
-        // render, step
+//-------- ----------
+// SCENE, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0.25, 0.25, 0.25);
+scene.add(new THREE.GridHelper(10,10));
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+document.getElementById('demo').appendChild(renderer.domElement);
+//-------- ----------
+// LIGHT
+//-------- ----------
+const pl = new THREE.PointLight();
+pl.position.set(0, 3, 6);
+scene.add(pl);
+//-------- ----------
+// CAMERA
+//-------- ----------
+const width = 4.0,
+height = 4.0;
+const camera = new THREE.OrthographicCamera( -width, width, height, -height, 0.01, 100);
+camera.position.set(5,2,5);
+camera.lookAt(0, 0, 0);
+//-------- ----------
+// MESH
+//-------- ----------
+const material = new THREE.MeshNormalMaterial();
+const m1 = new THREE.Mesh( new THREE.BoxGeometry(1,1,1), material );
+m1.position.set(0, 0.5, 0);
+scene.add( m1 );
+const m2 = new THREE.Mesh( new THREE.BoxGeometry(1,3,1), material );
+m2.position.set(-3, 1.5, 1);
+scene.add( m2 );
+const m3 = new THREE.Mesh( new THREE.BoxGeometry(1,3,1), material );
+m3.position.set(3, 1.5, -1);
+scene.add( m3 );
+// ---------- ----------
+// ANIMATION LOOP
+// ---------- ----------
+const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+FRAME_MAX = 120;
+let secs = 0,
+frame = 0,
+lt = new Date();
+// update
+const update = function(frame, frameMax){
+    const a1 = frame / frameMax;
+    const a2 = 1 - Math.abs(0.5 - a1) / 0.5;
+    const width = 4 + 4 * a2;
+    const height = 4 - 2 * a2;
+    camera.left = width * -1;
+    camera.right = width;
+    camera.top = height;
+    camera.bottom = height * -1;
+    // CALLING UPDATE PROJECTION MATRIX METHOD
+    camera.updateProjectionMatrix();
+};
+// loop
+const loop = () => {
+    const now = new Date(),
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if(secs > 1 / FPS_UPDATE){
+        // update, render
+        update( Math.floor(frame), FRAME_MAX);
         renderer.render(scene, camera);
-        frame += 1;
-        frame = frame % maxFrame;
-    };
-    loop();
-}());
+        // step frame
+        frame += FPS_MOVEMENT * secs;
+        frame %= FRAME_MAX;
+        lt = now;
+    }
+};
+loop();
