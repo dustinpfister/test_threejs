f67f22428f61ec3acc2830caa064e8c36c5fb7de
dustinpfister <dustin.pfister@gmail.com>
2023-02-21 12:16:30 -0500

    s2-1-cubestack demo updated to r146 style

--- views/forpost/threejs-camera-orthographic/s2-1-cubestack/main.js
+++ views/forpost/threejs-camera-orthographic/s2-1-cubestack/main.js
@@ -1,73 +1,71 @@
-
-(function () {
-    // scene
-    const scene = new THREE.Scene();
-    scene.add(new THREE.GridHelper(10, 10));
-
-    // ORTHOGRAPHIC CAMERA
-    var width = 3.2,
-    height = 2.4,
-    left = width * -1,
-    right = width,
-    top = height,
-    bottom = height * -1,
-    near = 1,
-    far = 20,
-    camera = new THREE.OrthographicCamera(
-            left,
-            right,
-            top,
-            bottom,
-            near,
-            far);
-    camera.position.set(4, 4, 4);
+//-------- ----------
+// SCENE, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(10, 10));
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+//-------- ----------
+// ORTHOGRAPHIC CAMERA
+//-------- ----------
+const width = 3.2,
+height = 2.4,
+left = width * -1,
+right = width,
+top2 = height,
+bottom = height * -1,
+near = 1,
+far = 20;
+const camera = new THREE.OrthographicCamera( left, right, top2, bottom, near, far);
+camera.zoom = 0.75;
+camera.updateProjectionMatrix();
+//-------- ----------
+// LIGHT
+//-------- ----------
+const pl = new THREE.PointLight();
+pl.position.set(0, 2, 3);
+scene.add(pl); 
+//-------- ----------
+// CUBE STACK
+//-------- ----------
+const stack = new CubeStack({
+        boxCount: 25
+    });
+stack.group.position.set(0, 0.5, 0);
+scene.add(stack.group);
+// ---------- ----------
+// ANIMATION LOOP
+// ---------- ----------
+const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+FRAME_MAX = 120;
+let secs = 0,
+frame = 0,
+lt = new Date();
+// update
+const update = function(frame, frameMax){
+    const a1 = frame / frameMax;
+    const radian = Math.PI * 2 * a1;
+    const x = Math.cos(radian) * 5,
+    y = 3 + Math.sin(radian) * 2,
+    z = Math.sin(radian) * 5;
+    camera.position.set(x, y, z);
     camera.lookAt(0, 0, 0);
-    camera.zoom = .75;
-    camera.updateProjectionMatrix();
-    var light = new THREE.PointLight(); // point light above the camera
-    light.position.set(0, 2, 0);
-    camera.add(light);
-    scene.add(camera); // make the camera part of the scene
-
-    // create and add the cube stack
-    var stack = new CubeStack({
-            boxCount: 25
-        });
-    stack.group.position.set(0, 0.5, 0);
-    scene.add(stack.group);
-    // render
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-    // loop
-    var frame = 0,
-    maxFrame = 200,
-    fps = 30,
-    radian,
-    x,
-    y,
-    z,
-    lt = new Date();
-    var loop = function () {
-        var now = new Date(),
-        secs = (now - lt) / 1000,
-        per = frame / maxFrame;
-        requestAnimationFrame(loop);
-        if (secs > 1 / fps) {
-            renderer.render(scene, camera);
-            radian = Math.PI * 2 * per;
-            x = Math.cos(radian) * 5;
-            y = 3 + Math.sin(radian) * 2;
-            z = Math.sin(radian) * 5;
-            camera.position.set(x, y, z);
-            camera.lookAt(0, 0, 0);
-            lt = now;
-            frame += fps * secs;
-            frame %= maxFrame;
-        }
-    };
-
-    loop();
-
-}
-    ());
+};
+// loop
+const loop = () => {
+    const now = new Date(),
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if(secs > 1 / FPS_UPDATE){
+        // update, render
+        update( Math.floor(frame), FRAME_MAX);
+        renderer.render(scene, camera);
+        // step frame
+        frame += FPS_MOVEMENT * secs;
+        frame %= FRAME_MAX;
+        lt = now;
+    }
+};
+loop();
