5db702ac821e23bd41bf5ffbb0500596cd6a0f74
dustinpfister <dustin.pfister@gmail.com>
2023-02-22 09:50:29 -0500

    s1-1-basic updated to r146 style

--- views/forpost/threejs-wireframe/s1-1-basic/main.js
+++ views/forpost/threejs-wireframe/s1-1-basic/main.js
@@ -1,24 +1,27 @@
-
-
-    // mesh in wire frame mode
-    var mesh = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshBasicMaterial({
-                color: 0xffffff,
-                wireframe: true
-            }));
-
-    // Scene
-    var scene = new THREE.Scene();
-    scene.background = new THREE.Color('blue');
-    scene.add(mesh);
-    // Camera
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, .5, 100);
-    camera.position.set(2, 2, 2);
-    camera.lookAt(0, 0, 0);
-    // Render
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-    renderer.render(scene, camera);
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+var scene = new THREE.Scene();
+scene.background = new THREE.Color('blue');
+var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 100);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(640, 480);
+document.getElementById('demo').appendChild(renderer.domElement);
+//-------- ----------
+// MESH, GEOMETRY, MATREIAL - in wireframe mode
+//-------- ----------
+var mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1.50, 1.50, 1.50),
+    new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        wireframe: true
+    })
+);
+scene.add(mesh);
+//-------- ----------
+// RENDER
+//-------- ----------
+camera.position.set(1.5, 1.25, 2);
+camera.lookAt(0, -0.2, 0);
+renderer.render(scene, camera);
 
