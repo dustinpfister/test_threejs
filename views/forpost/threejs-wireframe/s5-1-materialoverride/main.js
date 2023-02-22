315d65c7cfcbe60c5663cacc7aa652efd2d38b5a
dustinpfister <dustin.pfister@gmail.com>
2023-02-22 10:27:32 -0500

    all threejs-wireframe demos updated to r146 style

--- views/forpost/threejs-wireframe/s5-1-materialoverride/main.js
+++ views/forpost/threejs-wireframe/s5-1-materialoverride/main.js
@@ -1,64 +1,64 @@
-(function () {
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x2f2f2f);
+const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.75, 10);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// MATERIAL OVERRIDE
+//-------- ----------
+const material_override = new THREE.MeshBasicMaterial({
+    color: new THREE.Color('lime'),
+    wireframe: true
+});
+scene.overrideMaterial = material_override;
+//-------- ----------
+// MESH
+//-------- ----------
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(0.5, 0.5, 0.5),
+    new THREE.MeshDepthMaterial());
+scene.add(mesh);
+const mesh2 = new THREE.Mesh(
+    new THREE.BoxGeometry(0.5, 0.5, 0.5),
+    new THREE.MeshNormalMaterial());
+mesh2.position.set(-1.5, 0, 0);
+scene.add(mesh2);
+//-------- ----------
+// LIGHT
+//-------- ----------
+const pl = new THREE.PointLight(0xffffff, 1);
+pl.position.set(1, 1, 0);
+scene.add(pl);
+//-------- ----------
+// LOOP
+//-------- ----------
+camera.position.set(1.15, 0.85, 0.75);
+camera.lookAt(0, 0, 0);
 
-    // Scene
-    var scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x2f2f2f);
-
-    var material_override = new THREE.MeshBasicMaterial({
-            color: new THREE.Color('lime'),
-            wireframe: true
-        });
+const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+FRAME_MAX = 300;
+let secs = 0,
+frame = 0,
+lt = new Date();
+const update = function(frame, frameMax){
+    const a1 = frame / frameMax * 8 % 1;
     scene.overrideMaterial = material_override;
-
-    // mesh1
-    var mesh = new THREE.Mesh(
-            new THREE.BoxGeometry(0.5, 0.5, 0.5),
-            new THREE.MeshDepthMaterial());
-    scene.add(mesh);
-
-    var mesh2 = new THREE.Mesh(
-            new THREE.BoxGeometry(0.5, 0.5, 0.5),
-            new THREE.MeshNormalMaterial());
-    mesh2.position.set(-1.5, 0, 0);
-    scene.add(mesh2);
-
-    // Camera
-    var camera = new THREE.PerspectiveCamera(45, 4 / 3, 0.75, 10);
-    camera.position.set(1.15, 0.85, 0.75);
-    camera.lookAt(0, 0, 0);
-    var light = new THREE.PointLight(0xffffff, 1);
-    light.position.set(1, 1, 0);
-    camera.add(light)
-    scene.add(camera);
-    // Render
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-
-    var lt = new Date(),
-    time = 0,
-    switchTime = 1.0,
-    fps = 30;
-    var loop = function () {
-        var now = new Date(),
-        secs = (now - lt) / 1000;
-        requestAnimationFrame(loop);
-        if (secs > 1 / fps) {
-            time += secs;
-            if (time >= switchTime) {
-                if (scene.overrideMaterial) {
-                    scene.overrideMaterial = null;
-                } else {
-                    scene.overrideMaterial = material_override;
-                }
-                time %= switchTime;
-            }
-            renderer.render(scene, camera);
-            lt = now;
-        }
-    };
-
-    loop();
-
-}
-    ());
+    if ( a1 < 0.5) {
+        scene.overrideMaterial = null;
+    }
+};
+const loop = () => {
+    const now = new Date(),
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if(secs > 1 / FPS_UPDATE){
+        update( Math.floor(frame), FRAME_MAX);
+        renderer.render(scene, camera);
+        frame += FPS_MOVEMENT * secs;
+        frame %= FRAME_MAX;
+        lt = now;
+    }
+};
+loop();
--- views/forpost/threejs-wireframe/todo.md
+++ views/forpost/threejs-wireframe/todo.md
@@ -6,14 +6,13 @@
 ### () - s1-2-basic-linewidth
 * () have an example where I am setting the line width
 
-## () - rename folders, use r146
+## ( done 02/22/2023 ) - rename folders, use r146
 * (done) rename all folders
 * (done) update s1-1-basic to r146 style
 * (done) update s2-1-edges-lines to r146 style
 * (done) update s3-1-canvas to r146 style
 * (done) update s4-1-lighting to r146 style
-* () update s5-1-materialoverride to r146 style
-
+* (done) update s5-1-materialoverride to r146 style
 
 ### ( done 08/03/2022 ) for post folder started
 * (done) this for post folder started
