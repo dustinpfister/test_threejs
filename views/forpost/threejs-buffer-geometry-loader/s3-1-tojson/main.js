447bc4ae8f879f20f94658f3b24e9c8c01b14105
dustinpfister <dustin.pfister@gmail.com>
2023-02-28 12:57:14 -0500

    s3-1-tojson demo updated to r146 style

--- views/forpost/threejs-buffer-geometry-loader/s3-1-tojson/main.js
+++ views/forpost/threejs-buffer-geometry-loader/s3-1-tojson/main.js
@@ -1,30 +1,27 @@
+//-------- ----------
+// SCENE, CAMERA, RENDERER
+//-------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.1, 100);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+//-------- ----------
+// BUFFER GEOMETRY TO TEXT
+//-------- ----------
+const geo = new THREE.SphereGeometry(1, 10, 10);
+// make sure to use to non indexed before calling to json
+const buffObj = geo.toNonIndexed().toJSON();
+const text = JSON.stringify(buffObj);
+//-------- ----------
+// TEXT TO BUFFER GEOMETRY
+//-------- ----------
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
+const loader = new THREE.BufferGeometryLoader();
+const obj = JSON.parse(text);
+const geo2 = loader.parse( obj );
+const mesh = new THREE.Mesh(geo2)
+scene.add(mesh)
+renderer.render(scene, camera)
 
-(function () {
-    //******** *********
-    // Scene, Camera, renderer
-    //******** *********
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(65, 4 / 3, .5, 10);
-    camera.position.set(2, 2, 2);
-    camera.lookAt(0, 0, 0);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-    //******** *********
-    // BUFFER GEOMETRY TO TEXT
-    //******** *********
-    var geo = new THREE.SphereGeometry(1, 10, 10);
-    // make sure to use to non indexed before calling to json
-    var buffObj = geo.toNonIndexed().toJSON();
-    var text = JSON.stringify(buffObj);
-    //******** *********
-    // TEXT TO BUFFER GEOMETRY
-    //******** *********
-    const loader = new THREE.BufferGeometryLoader();
-    var obj = JSON.parse(text);
-    var geo2 = loader.parse( obj );
-    var mesh = new THREE.Mesh(geo2)
-    scene.add(mesh)
-    renderer.render(scene, camera)
-}
-    ());
--- views/forpost/threejs-buffer-geometry-loader/todo.md
+++ views/forpost/threejs-buffer-geometry-loader/todo.md
@@ -11,11 +11,11 @@
 ## () - s4-1-manager
 * () start a s4-1-manager demo that will be a basic getting started type demo of the loading manager
 
-## () rename folders, and update to r146 style
+## ( done 02/28/2023 ) rename folders, and update to r146 style
 * (done) rename demo folders
 * (done) update s1-1-basic demo to r146 style
 * (done) update s2-1-callbacks demo to r146 style
-* () update s3-1-tojson demo to r146 style
+* (done) update s3-1-tojson demo to r146 style
 
 ## ( done 08/04/2022 ) - s3-to-json
 * (done) have an example where I am converting a buffer geometry to JSON
