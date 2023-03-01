bff0fe76f0f14292ce426624e3d2ecf2de6625ee
dustinpfister <dustin.pfister@gmail.com>
2023-03-01 10:44:22 -0500

    started working on zoom example

--- views/forpost/threejs-camera-perspective/s2-3-change-zoom/main.js
+++ views/forpost/threejs-camera-perspective/s2-3-change-zoom/main.js
@@ -1,4 +1,4 @@
-(function () {
+
     // CAMERA
     var camera = new THREE.PerspectiveCamera(50, 640 / 480, 0.1, 1000);
     camera.position.set(10, 10, 10);
@@ -13,30 +13,36 @@
     scene.add(new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial()));
-    // LOOP
-    var state = {
-        frame: 0,
-        maxFrame: 90,
-        fps: 30,
-        lt: new Date()
-    };
-    var loop = function(){
-        var now = new Date(),
-        secs = (now - state.lt) / 1000;
-        var per = state.frame / state.maxFrame;
-        var bias = 1 - Math.abs(per - 0.5) / 0.5;
-        requestAnimationFrame(loop);
-        if(secs > 1 / state.fps){
-            // ZOOM
-            camera.zoom = 0.05 + 20.95 * bias;
-            camera.updateProjectionMatrix();
-            state.frame += state.fps * secs;
-            state.frame %= state.maxFrame;
-            renderer.render(scene, camera);
-            state.lt = new Date();
-        }
-    };
-    loop();
 
-}
-    ());
+// ---------- ----------
+// ANIMATION LOOP
+// ---------- ----------
+const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+FPS_MOVEMENT = 20;     // fps rate to move object by that is independent of frame update rate
+FRAME_MAX = 60;
+let secs = 0,
+frame = 0,
+lt = new Date();
+// update method
+const update = function (frame, frameMax) {
+    const a1 = frame / frameMax;
+    const a2 = 1 - Math.abs(0.5 - a1) / 0.5;
+    camera.zoom = 0.05 + 20.95 * a2;
+    camera.updateProjectionMatrix();
+};
+// loop
+const loop = () => {
+    const now = new Date(),
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if(secs > 1 / FPS_UPDATE){
+        // update, render
+        update( Math.floor(frame), FRAME_MAX);
+        renderer.render(scene, camera);
+        // step frame
+        frame += FPS_MOVEMENT * secs;
+        frame %= FRAME_MAX;
+        lt = now;
+    }
+};
+loop();
