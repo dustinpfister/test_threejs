b74ee7b1997f064347d3249a524db5c7fd4c7413
dustinpfister <dustin.pfister@gmail.com>
2023-03-06 08:02:43 -0500

    s1-2-basic-material demo updated to r146 style

--- views/forpost/threejs-mesh-copy/s1-2-basic-material/main.js
+++ views/forpost/threejs-mesh-copy/s1-2-basic-material/main.js
@@ -1,48 +1,57 @@
+//-------- ----------
 // SCENE
-var scene = new THREE.Scene();
-
-// CAMERA
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(10, 6, 10);
-camera.lookAt(0, 0, 0);
-
-// RENDER
-var renderer = new THREE.WebGLRenderer();
-document.getElementById('demo').appendChild(renderer.domElement);
-renderer.setSize(640, 480);
-
-// MESH original
-var original = new THREE.Mesh(
+//-------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
+// add a light source
+//-------- ----------
+const sun = new THREE.Mesh(
+        new THREE.SphereGeometry(0.5, 40, 40),
+        new THREE.MeshBasicMaterial());
+sun.add(new THREE.PointLight(0xffffff, 1));
+sun.position.set(8, 3, 0);
+scene.add(sun);
+//-------- ----------
+// MESH - original and clones
+//-------- ----------
+const original = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
        // Now using the Standard material
         new THREE.MeshStandardMaterial({
             color: 'red'
         }));
 scene.add(original);
-
-// add a light source
-var sun = new THREE.Mesh(
-        new THREE.SphereGeometry(0.5, 40, 40),
-        new THREE.MeshBasicMaterial());
-sun.add(new THREE.PointLight(0xffffff, 1));
-sun.position.set(8, 3, 0);
-scene.add(sun);
-
-// Mesh cloned a bunch of times from original
-var i = 0, mesh, rad, x, z;
+let i = 0;
 while (i < 10) {
-    mesh = original.clone();
+    const mesh = original.clone();
     // changes made to position and rotation to not effect original
-    rad = Math.PI * 2 * (i / 10);
-    x = Math.cos(rad) * 3;
-    z = Math.sin(rad) * 3;
+    const rad = Math.PI * 2 * (i / 10);
+    const x = Math.cos(rad) * 3;
+    const z = Math.sin(rad) * 3;
     mesh.position.set(x, 0, z);
+    mesh.name = 'clone' + i;
     mesh.lookAt(original.position);
     scene.add(mesh);
     i += 1;
 }
-
+//-------- ----------
+// CHNAGE TO THE MATERIAL
+//   * a change to one material will effect all because they all reference the same material
+// * this can eb fixed by cloning the material as well for any mesh objects that should have there one material
+//-------- ----------
 // a change to the color of the original will effect all the clones also
 original.material.color.setRGB(0, 1, 0);
-
+// I can clone the material of one though and those changes to that will not effect all
+const mesh_copy = scene.getObjectByName('clone3');
+mesh_copy.material = mesh_copy.material.clone();
+mesh_copy.material.color.setRGB(1, 0, 0)
+//-------- ----------
+// RENDER
+//-------- ----------
+camera.position.set(10, 6, 10);
+camera.lookAt(0, 0, 0);
 renderer.render(scene, camera);
