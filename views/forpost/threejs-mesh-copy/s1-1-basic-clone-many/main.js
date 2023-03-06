642411075f25831cec76511cb08068144ac522f9
dustinpfister <dustin.pfister@gmail.com>
2023-03-06 07:50:08 -0500

    s1-1-basic-clone-many updated to r146 style

--- views/forpost/threejs-mesh-copy/s1-1-basic-clone-many/main.js
+++ views/forpost/threejs-mesh-copy/s1-1-basic-clone-many/main.js
@@ -1,34 +1,36 @@
+//-------- ----------
 // SCENE
-var scene = new THREE.Scene();
-
-// CAMERA
-var camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(5, 3, 5);
-camera.lookAt(0, 0, 0);
-
-// RENDER
-var renderer = new THREE.WebGLRenderer();
-document.getElementById('demo').appendChild(renderer.domElement);
-renderer.setSize(640, 480);
-
+//-------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
+//-------- ----------
 // MESH original
-var original = new THREE.Mesh(
+//-------- ----------
+const original = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
         new THREE.MeshNormalMaterial());
 scene.add(original);
-
+//-------- ----------
 // Mesh cloned a bunch of times from original
-var i = 0, mesh, rad, x, z;
+//-------- ----------
+let i = 0;
 while (i < 10) {
-    mesh = original.clone();
+    const mesh = original.clone();
     // changes made to position and rotation to not effect original
-    rad = Math.PI * 2 * (i / 10);
-    x = Math.cos(rad) * 3;
-    z = Math.sin(rad) * 3;
+    const rad = Math.PI * 2 * (i / 10);
+    const x = Math.cos(rad) * 3;
+    const z = Math.sin(rad) * 3;
     mesh.position.set(x, 0, z);
     mesh.lookAt(original.position);
     scene.add(mesh);
     i += 1;
 }
-
+//-------- ----------
+// RENDER
+//-------- ----------
+camera.position.set(5, 3, 5);
+camera.lookAt(0, 0, 0);
 renderer.render(scene, camera);
