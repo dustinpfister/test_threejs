f2545734d86cbe1374fdc90bec62ce8b68dfcd1a
dustinpfister <dustin.pfister@gmail.com>
2023-03-09 14:22:20 -0500

    whitespace

--- views/forpost/threejs-vector3-wrap/s3-1-wrapvector/main.js
+++ views/forpost/threejs-vector3-wrap/s3-1-wrapvector/main.js
@@ -7,99 +7,96 @@ const camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
-
-
-
-    //-------- ----------
-    // HELPERS
-    //-------- ----------
-    // Wrap method based off of the method from Phaser3 
-    // ( https://github.com/photonstorm/phaser/blob/v3.55.2/src/math/Wrap.js )
-    // * Added some code for case: Wrap(0, 0, 0)
-    // * Using Math.min and Math.max so that Wrap(value, 2, 10) is same as Wrap(value, 10, 2)
-    //
-    const wrap = function (value, a, b){
-        // get min and max this way
-        const max = Math.max(a, b);
-        const min = Math.min(a, b);
-        // return 0 for Wrap(value, 0, 0);
-        if(max === 0 && min === 0){
-             return 0;
-        }
-        const range = max - min;
-        return (min + ((((value - min) % range) + range) % range));
-    };
-    // wrap an axis
-    const wrapAxis = function(vec, vecMin, vecMax, axis){
-        axis = axis || 'x';
-        vec[axis] = wrap( vec[axis], vecMin[axis], vecMax[axis] );
-        return vec;
-    };
-    // wrap a vector
-    const wrapVector = function (vec, vecMin, vecMax) {
-        vecMin = vecMin || new THREE.Vector3(0, 0, 0);
-        vecMax = vecMax || new THREE.Vector3(1, 1, 1);
-        wrapAxis(vec, vecMin, vecMax, 'x');
-        wrapAxis(vec, vecMin, vecMax, 'y');
-        wrapAxis(vec, vecMin, vecMax, 'z');
-    };
-    // create group
-    const createGroup = function () {
-        const group = new THREE.Group();
-        let i = 0,
-        len = 50;
-        while (i < len) {
-            const mesh = new THREE.Mesh(
-                new THREE.BoxGeometry(1.0, 1.0, 1.0), 
-                new THREE.MeshNormalMaterial({
-                    transparent: true,
-                    opacity: 0.60
-                })
-            );
-            mesh.position.x = -2 + 4 * Math.random();
-            mesh.position.y = -2 + 4 * Math.random();
-            mesh.position.z = -2 + 4 * Math.random();
-            group.add(mesh);
-            i += 1;
-        }
-        return group;
-    };
-    // update a group
-    const updateGroup = function (group, secs, bias) {
-       group.children.forEach(function(mesh){
-            mesh.position.x += (2 - 4 * bias) * secs;
-            mesh.position.y += (-2 + 4 * bias ) * secs;
-            mesh.position.z += 2 * secs;
-            wrapVector(
-                mesh.position,
-                new THREE.Vector3(-2, -2, -2),
-                new THREE.Vector3(2, 2, 2));
-        });
-    };
-    //-------- ----------
-    // LOOP
-    //-------- ----------
-    camera.position.set(5, 5, 5);
-    camera.lookAt(0, 0, 0);
-    const group = createGroup();
-    scene.add(group);
-    let frame = 0,
-    maxFrame = 300,
-    fps = 20,
-    lt = new Date();
-    const loop = function () {
-        const now = new Date(),
-        secs = (now - lt) / 1000,
-        per = frame / maxFrame,
-        bias = 1 - Math.abs(0.5 - per) / 0.5;
-        requestAnimationFrame(loop);
-        if (secs > 1 / fps) {
-            updateGroup(group, secs, bias)
-            renderer.render(scene, camera);
-            frame += fps * secs;
-            frame %= maxFrame;
-            lt = now;
-        }
-    };
-    loop();
+//-------- ----------
+// HELPERS
+//-------- ----------
+// Wrap method based off of the method from Phaser3 
+// ( https://github.com/photonstorm/phaser/blob/v3.55.2/src/math/Wrap.js )
+// * Added some code for case: Wrap(0, 0, 0)
+// * Using Math.min and Math.max so that Wrap(value, 2, 10) is same as Wrap(value, 10, 2)
+//
+const wrap = function (value, a, b){
+    // get min and max this way
+    const max = Math.max(a, b);
+    const min = Math.min(a, b);
+    // return 0 for Wrap(value, 0, 0);
+    if(max === 0 && min === 0){
+         return 0;
+    }
+    const range = max - min;
+    return (min + ((((value - min) % range) + range) % range));
+};
+// wrap an axis
+const wrapAxis = function(vec, vecMin, vecMax, axis){
+    axis = axis || 'x';
+    vec[axis] = wrap( vec[axis], vecMin[axis], vecMax[axis] );
+    return vec;
+};
+// wrap a vector
+const wrapVector = function (vec, vecMin, vecMax) {
+    vecMin = vecMin || new THREE.Vector3(0, 0, 0);
+    vecMax = vecMax || new THREE.Vector3(1, 1, 1);
+    wrapAxis(vec, vecMin, vecMax, 'x');
+    wrapAxis(vec, vecMin, vecMax, 'y');
+    wrapAxis(vec, vecMin, vecMax, 'z');
+};
+// create group
+const createGroup = function () {
+    const group = new THREE.Group();
+    let i = 0,
+    len = 50;
+    while (i < len) {
+        const mesh = new THREE.Mesh(
+            new THREE.BoxGeometry(1.0, 1.0, 1.0), 
+            new THREE.MeshNormalMaterial({
+                transparent: true,
+                opacity: 0.60
+            })
+        );
+        mesh.position.x = -2 + 4 * Math.random();
+        mesh.position.y = -2 + 4 * Math.random();
+        mesh.position.z = -2 + 4 * Math.random();
+        group.add(mesh);
+        i += 1;
+    }
+    return group;
+};
+// update a group
+const updateGroup = function (group, secs, bias) {
+    group.children.forEach(function(mesh){
+        mesh.position.x += (2 - 4 * bias) * secs;
+        mesh.position.y += (-2 + 4 * bias ) * secs;
+        mesh.position.z += 2 * secs;
+        wrapVector(
+            mesh.position,
+            new THREE.Vector3(-2, -2, -2),
+            new THREE.Vector3(2, 2, 2));
+    });
+};
+//-------- ----------
+// LOOP
+//-------- ----------
+camera.position.set(5, 5, 5);
+camera.lookAt(0, 0, 0);
+const group = createGroup();
+scene.add(group);
+let frame = 0,
+maxFrame = 300,
+fps = 20,
+lt = new Date();
+const loop = function () {
+    const now = new Date(),
+    secs = (now - lt) / 1000,
+    per = frame / maxFrame,
+    bias = 1 - Math.abs(0.5 - per) / 0.5;
+    requestAnimationFrame(loop);
+    if (secs > 1 / fps) {
+        updateGroup(group, secs, bias)
+        renderer.render(scene, camera);
+        frame += fps * secs;
+        frame %= maxFrame;
+        lt = now;
+    }
+};
+loop();
 
