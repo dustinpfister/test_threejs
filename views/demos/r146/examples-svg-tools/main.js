2b1593216819b6f9d94527f1fad7400157ee732e
dustinpfister <dustin.pfister@gmail.com>
2023-03-12 15:24:26 -0400

    rotation

--- views/demos/r146/examples-svg-tools/main.js
+++ views/demos/r146/examples-svg-tools/main.js
@@ -19,7 +19,7 @@ lt = new Date();
 const update = function(frame, frameMax){
     const a1 = frame / frameMax;
     const e = new THREE.Euler();
-    e.y = Math.PI * 2 * a1;
+    e.y = 0; //Math.PI * 2 * a1;
     camera.position.set(100, 100, 100).applyEuler(e);
     camera.lookAt(0,0,0);
 };
@@ -55,16 +55,17 @@ SVGTools.load({
    processor: (st, data, i_url, url) => {
         const svg_width = data.xml.width.baseVal.value;
         const svg_height = data.xml.height.baseVal.value;
-        const depth = st.opt_extrude.depth;
-        const count = st.urls.length;
-        const zSpace = count + 2;
-        const sz = zSpace * depth / 2 * -1;
-        const a_data = i_url / count;
+        //const depth = st.opt_extrude.depth;
+        //const count = (st.urls.length - 1);
+        //const zSpace = count + 2;
+        //const sz = zSpace * depth / 2 * -1;
+        //const a_data = i_url / count;
         st.dataToShape(data, (shape, si, pi) => {
             const geo = new THREE.ExtrudeGeometry(shape, st.opt_extrude);
-            geo.translate(svg_width / 2 * -1, svg_height / 2 * -1, 0);
+            geo.rotateX(Math.PI * 1);
+            geo.translate( svg_width / 2 * -1, svg_height / 2 * 1, 0);
             const mesh = new THREE.Mesh(geo, st.material);
-            mesh.position.z = sz + (zSpace * depth) * a_data;
+            //mesh.position.z = sz + (zSpace * depth) * a_data;
             st.scene.add(mesh);
         });
    }
