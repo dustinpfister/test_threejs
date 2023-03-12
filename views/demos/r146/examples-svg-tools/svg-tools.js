9f4fe468e380e7a0b44465c8545e34552558376a
dustinpfister <dustin.pfister@gmail.com>
2023-03-12 13:20:50 -0400

    renamed opt_load to st

--- views/demos/r146/examples-svg-tools/svg-tools.js
+++ views/demos/r146/examples-svg-tools/svg-tools.js
@@ -1,25 +1,35 @@
 // svg-tools.js - r0 - r146 prototype
 (function(api){
     //-------- ----------
+    // DEFAULTS
+    //-------- ----------
+    const DEFAULT_LOAD_OPTIONS = {
+        urls: [],
+        processor: 'extrude',
+        scene: new THREE.Scene(),
+        opt_extrude: { depth: 1 },
+        material: new THREE.MeshNormalMaterial()
+    };
+    //-------- ----------
     // HELPERS - internal helper funcitons used by the public api, and other built in features
     //-------- ----------
     // what to do for each SVG file that loads
-    const onFileLoaded = (url, i_url, loader, opt_load, resolve, reject) => {
-        const scene = opt_load.scene;
+    const onFileLoaded = (url, i_url, loader, st, resolve, reject) => {
+        const scene = st.scene;
         return (data) => {
             console.log('file loaded **********');
             console.log(url);
             console.log('paths: ' + data.paths.length);
             console.log('**********************');
-            opt_load.processor(opt_load, data, i_url, url);
+            st.processor(st, data, i_url, url);
         }
     };
     // on file progress and error methods
-    const onFileProgress = (url, i_url, loader, opt_load, resolve, reject) => {
+    const onFileProgress = (url, i_url, loader, st, resolve, reject) => {
         return (xhr) => {
         };
     };
-    const onFileError = (url, i_url, loader, opt_load, resolve, reject) => {
+    const onFileError = (url, i_url, loader, st, resolve, reject) => {
         return (error) => {
             reject(error);
         };
@@ -29,16 +39,16 @@
     //-------- ----------
     const SVG_PROCESSOR = {};
     // extrude
-    SVG_PROCESSOR.extrude = (opt_load, data, i_url, url) => {
+    SVG_PROCESSOR.extrude = (st, data, i_url, url) => {
         let pi = 0;
         while(pi < data.paths.length){
             const shapes = THREE.SVGLoader.createShapes( data.paths[pi] );
             let si = 0;
             while(si < shapes.length){
-                const geo = new THREE.ExtrudeGeometry(shapes[si], opt_load.opt_extrude || { depth: 1 } );
-                const mesh = new THREE.Mesh(geo, opt_load.material || new THREE.MeshNormalMaterial());
+                const geo = new THREE.ExtrudeGeometry(shapes[si], st.opt_extrude || { depth: 1 } );
+                const mesh = new THREE.Mesh(geo, st.material || new THREE.MeshNormalMaterial());
                 //mesh.position.z = 50 * i_url;
-                opt_load.scene.add(mesh);
+                st.scene.add(mesh);
                 si += 1;
             }
             pi += 1;
@@ -47,13 +57,15 @@
     //-------- ----------
     // PUBLIC API
     //-------- ----------
-    api.load = (opt_load) => {
-        opt_load = opt_load || {};
-        opt_load.urls = opt_load.urls || [];
-        opt_load.scene = opt_load.scene || new THREE.Scene();
-        opt_load.processor = opt_load.processor || SVG_PROCESSOR.extrude;
-        if(typeof opt_load.processor === 'string'){
-            opt_load.processor = SVG_PROCESSOR[opt_load.processor];
+    api.load = (st) => {
+
+        st = st || {};
+        st.urls = st.urls || [];
+        st.scene = st.scene || new THREE.Scene();
+        st.processor = st.processor || SVG_PROCESSOR.extrude;
+
+        if(typeof st.processor === 'string'){
+            st.processor = SVG_PROCESSOR[st.processor];
         }
 
         // return a promise
@@ -62,20 +74,20 @@
             const loading_manager = new THREE.LoadingManager();
             loading_manager.onLoad = () => {
                 console.log('All Files loaded.');
-                resolve(opt_load)
+                resolve(st)
             };
             let i_url = 0;
-            const len_url = opt_load.urls.length;
+            const len_url = st.urls.length;
             while(i_url < len_url){
                 // svg loader instance a loader
                 const loader = new THREE.SVGLoader(loading_manager);
                 // load a SVG resource
-                const url = opt_load.urls[i_url];
+                const url = st.urls[i_url];
                 loader.load(
                     url,
-                    onFileLoaded(url, i_url, loader, opt_load, resolve, reject),
-                    onFileProgress(url, i_url, loader, opt_load, resolve, reject),
-                    onFileError(url, i_url, loader, opt_load, resolve, reject)
+                    onFileLoaded(url, i_url, loader, st, resolve, reject),
+                    onFileProgress(url, i_url, loader, st, resolve, reject),
+                    onFileError(url, i_url, loader, st, resolve, reject)
                 );
                 i_url += 1;
             }
--- views/demos/r146/examples-svg-tools/todo.md
+++ views/demos/r146/examples-svg-tools/todo.md
@@ -4,7 +4,11 @@
 * (done) have helper functions for on file load, progress, error
 * (done) have a opt-load option that can be used to define custom logic for parsing svg files
 * (done) Make use of a loading manager to be able to load more than one file
+* (done) custom processor example in main.js demo
+
+* () opt_load should be called st for SvgTools
+* () use Object.assign and hard coded defaults objects in SVGTools.load
 
 * () data to mesh helper
+* () can use to data mesh helper in custom processor
 * () simple shape built in processor
-* () custom processor example in main.js demo
