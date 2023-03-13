2135ce7f92dad72e7754a72550fa079617aac94d
dustinpfister <dustin.pfister@gmail.com>
2023-03-13 11:20:57 -0400

    s1-1-basic demo updated to r146 style for threejs-vector3-distance-to

--- views/forpost/threejs-vector3-distance-to/s1-1-basic/main.js
+++ views/forpost/threejs-vector3-distance-to/s1-1-basic/main.js
@@ -1,38 +1,43 @@
-
-(function () {
-
-    // simple create cube helper
-    var createCube = function () {
-        var cube = new THREE.Mesh(
-                new THREE.BoxGeometry(1, 1, 1),
-                new THREE.MeshNormalMaterial());
-        return cube;
-    };
-
-    // scene
-    var scene = new THREE.Scene();
-    scene.add(new THREE.GridHelper(7, 7));
-
-    // cubes
-    var cube1 = createCube();
-    scene.add(cube1);
-    var cube2 = createCube();
-    cube2.position.set(0, 0, 0);
-    scene.add(cube2);
-
-    // USING Vector3.distanceTo TO ADJUST THE POSITION OF CUBE2
-    if (cube2.position.distanceTo(cube1.position) < 2) {
-        cube2.position.set(2, 0, 0)
-    }
-
-    // camera, render
-    var camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
-    camera.position.set(8, 10, 8);
-    camera.lookAt(0, 0, 0);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(640, 480);
-    document.getElementById('demo').appendChild(renderer.domElement);
-    renderer.render(scene, camera);
-
+//-------- ---------
+// SCENE, CAMERA, RNEDERER
+//-------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 4 / 3, .5, 1000);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+//-------- ---------
+// HELPER
+//-------- ----------
+const createCube = function () {
+    var cube = new THREE.Mesh(
+           new THREE.BoxGeometry(1, 1, 1),
+           new THREE.MeshNormalMaterial());
+    return cube;
+};
+//-------- ---------
+// OBJECTS
+//-------- ----------
+scene.add( new THREE.GridHelper(10, 10) );
+const cube1 = createCube();
+cube1.position.set(3, 0.5, 2);
+scene.add(cube1);
+const cube2 = createCube();
+cube2.position.set(4, 0.5, -2);
+scene.add(cube2);
+// doing somehting with distance
+const d = cube2.position.distanceTo(cube1.position);
+console.log(d);
+cube2.scale.set(0, 0, 0)
+if (d < 5) {
+    const s = 1 - (d / 5); 
+    cube2.scale.set(s, s, s);
+    cube2.position.y = s / 2;
 }
-    ());
+//-------- ---------
+// RENDER
+//-------- ----------
+camera.position.set(8, 2, 8);
+camera.lookAt(cube1.position);
+renderer.render(scene, camera);
+
