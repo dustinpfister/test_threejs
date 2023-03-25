9cf43327aefbb072735f0e84804599f821f6a58f
dustinpfister <dustin.pfister@gmail.com>
2023-03-25 09:49:42 -0400

    simple s2-1-vector3-applyeuler demo for threejs-euler

--- views/forpost/threejs-euler/s2-1-vector3-applyeuler/main.js
+++ views/forpost/threejs-euler/s2-1-vector3-applyeuler/main.js
@@ -3,44 +3,45 @@
 //-------- ----------
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(60, 320 / 240, 0.1, 1000);
-camera.position.set(2, 2, 2);
-camera.lookAt(0, 0, 0);
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
 // ---------- ----------
-// Adding a Mesh
+// OBJECTS
 // ---------- ----------
+scene.add( new THREE.GridHelper(2, 2) );
 const mesh = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.SphereGeometry(0.5, 20, 20),
         new THREE.MeshNormalMaterial());
 scene.add(mesh);
 // ---------- ----------
 // LOOP - Using Vector3.applyEuler with an instance of THREE.Euler
 // ---------- ----------
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
 const state = {
     lt: new Date(),
     fps: 24,
-    radian : 0,
-    euler : new THREE.Euler(0, 0, 0)
+    frame: 0,
+    frameMax: 100,
+    euler : new THREE.Euler()
+};
+const update = (secs) => {
+    const a1 = state.frame / state.frameMax;
+    state.euler.y = Math.PI * 2 * a1;
+    // setting position
+    mesh.position.set(1, 0, 0);
+    mesh.position.applyEuler(state.euler);
 };
 const loop = function () {
     const now = new Date(),
     secs = (now - state.lt) / 1000;
     requestAnimationFrame(loop);
     if (secs >= 1 / state.fps) {
-        // updating state.euler, and using Vector3.applyEuler with state.euler
-        // by way of mesh.position which is an instance of Vector3
-        state.euler.z += THREE.MathUtils.degToRad(90) * secs;
-        state.euler.z = THREE.MathUtils.euclideanModulo(state.euler.z, Math.PI * 2);
-        mesh.position.set(1, 0, 0);
-        mesh.position.applyEuler(state.euler);
-        // doing a spin also
-        mesh.rotation.y += THREE.MathUtils.degToRad(360) * secs;
-        mesh.rotation.y = THREE.MathUtils.euclideanModulo(mesh.rotation.y, Math.PI * 2);
-        // render
+        update(secs);
         renderer.render(scene, camera);
         state.lt = now;
+        state.frame = ( state.frame += 1) % state.frameMax;
     }
 };
 loop();
--- views/forpost/threejs-euler/todo.md
+++ views/forpost/threejs-euler/todo.md
@@ -1,5 +1,17 @@
 # threejs-euler todo list
 
+## () s3-1-gimballock
+* () start a demo that shows what the deal is with gimbal lock
+
+## () s2-2-vector3-multiplyscalar
+* () demo using apply euler and multiply scalar methods
+
+## ( done 03/25/2023 ) s2-1-vector3-applyeuler simple
+* (done) add a grid helper
+* (done) move the mesh in a circle along the xz plane
+* (done) do away with the rotation of the mesh
+* (done) update method for loop part
+
 ## ( done 01/04/2023 ) update code to r146 style
 * (done) update code of all examples to r146 style
 
