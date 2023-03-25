233d97d9ae7dd4c29cb9128d17c380c12cb627b8
dustinpfister <dustin.pfister@gmail.com>
2023-03-25 11:51:28 -0400

    s3-1-loop-sphererotation demo for threejs-quaternion

---
+++ views/forpost/threejs-quaternion/s3-1-loop-sphererotation/main.js
@@ -0,0 +1,72 @@
+// ---------- ----------
+// SCENE, CAMERA, RENDERER
+// ---------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+// ---------- ----------
+// OBJECTS
+// ---------- ----------
+scene.add( new THREE.GridHelper( 10,10 ) );
+const geo = new THREE.SphereGeometry(1, 16, 16);
+const material = new THREE.MeshNormalMaterial({wireframe: true, wireframeLinewidth: 3});
+const mesh1 = new THREE.Mesh( geo, material);
+scene.add(mesh1);
+const arrowHelper = new THREE.ArrowHelper();
+arrowHelper.setLength(1.5);
+arrowHelper.line.material.linewidth = 6;
+scene.add(arrowHelper);
+// ---------- ----------
+// ANIMATION LOOP
+// ---------- ----------
+camera.position.set(2, 1, 2);
+camera.lookAt(0,0,0);
+const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
+FPS_MOVEMENT = 30;     // fps rate to move object by that is independent of frame update rate
+FRAME_MAX = 900;
+let secs = 0,
+frame = 0,
+lt = new Date();
+// update
+const v_up = new THREE.Vector3(0, 1, 0);
+const update = function(frame, frameMax){
+    const a1 = frame / frameMax;
+    const a2 = 0.75 - 0.75 * a1; //1 - Math.abs(0.5 - (a1 * 4 % 1)) / 0.5;
+    const a3 = a1 * 1 % 1;
+    const a4 = a1 * 6 % 1;
+    // axis vector
+    const v_axis = v_up.clone();
+    const e = new THREE.Euler();
+    e.y = Math.PI / 180 * ( 180 * a2);
+    e.z = Math.PI / 180 * ( 360 * a3 );
+    v_axis.applyEuler(e);
+    // two Quaternion objects using 
+    // setFromUnitVectors and setFromAxisAngle methods
+    const q1 = new THREE.Quaternion();
+    q1.setFromUnitVectors(v_up, v_axis);
+    mesh1.quaternion.copy(q1);
+    const q2 = new THREE.Quaternion();
+    q2.setFromAxisAngle(v_axis, Math.PI * 2 * a4);
+    // premultiply with q2 and q1
+    mesh1.quaternion.premultiply(q2, q1);
+    arrowHelper.setDirection(v_axis);
+};
+// loop
+const loop = () => {
+    const now = new Date(),
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if(secs > 1 / FPS_UPDATE){
+        // update, render
+        update( Math.floor(frame), FRAME_MAX);
+        renderer.render(scene, camera);
+        // step frame
+        frame += FPS_MOVEMENT * secs;
+        frame %= FRAME_MAX;
+        lt = now;
+    }
+};
+loop();
+
--- views/forpost/threejs-quaternion/todo.md
+++ views/forpost/threejs-quaternion/todo.md
@@ -1,5 +1,13 @@
 # threejs-quaternion todo list
 
+
+## () sx-1-getaxis
+* () have a get axis demo that is about how to get the axis of a quaternion when it is not known
+
+## ( done 03/25/2023 ) s3-1-loop-sphererotation demo
+* (done) start this demo based on the code from my quaternion-rotate-sphere r146 demo
+* (done) clean up the code removing stuff that is not used, an add comments
+
 ## ( done 03/24/2023 ) s2-1-methods-setfromaxisangle - improve for video1
 * (done) Do not rotate the geometry
 * (done) see about using just the quaternion and rotation props to get the same outcome
