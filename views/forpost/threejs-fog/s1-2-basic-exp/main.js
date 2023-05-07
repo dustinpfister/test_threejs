a81406f5995f94f14815baff359f610e95de578e
dustinpfister <dustin.pfister@gmail.com>
2023-05-07 12:27:39 -0400

    s1-2-basic-exp demo started for threejs-fog

--- views/forpost/threejs-fog/s1-2-basic-exp/main.js
+++ views/forpost/threejs-fog/s1-2-basic-exp/main.js
@@ -2,7 +2,7 @@
 // SCENE, CAMERA, RENDERER
 //-------- ----------
 const scene = new THREE.Scene();
-scene.add( new THREE.GridHelper(8, 8, 0xffffff, 0x000000));
+scene.add( new THREE.GridHelper(9, 9, 0xffffff, 0x000000));
 const camera = new THREE.PerspectiveCamera(75, 320 / 240, 0.025, 20);
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
@@ -10,20 +10,26 @@ renderer.setSize(640, 480, false);
 //-------- ----------
 // FOG AND BACKGROUND
 //-------- ----------
-// ADDING BACKGROUND AND FOG
 const fogColor = new THREE.Color(0xffffff);
-scene.background = fogColor; // Setting fogColor as the background color also
+scene.background = fogColor; 
 scene.fog = new THREE.Fog(fogColor, 0.25, 4);
 //-------- ----------
 // MESH
 //-------- ----------
-// Use a Material that SUPPORTS FOG such as the basic material
 const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({
-        color: 0xff0000
+    new THREE.MeshPhongMaterial({
+        color: 0x00ff00,
+        emissive: 0xffffff,
+        emissiveIntensity: 0.25
     }));
 scene.add(mesh);
+//-------- ----------
+// LIGHT
+//-------- ----------
+const dl = new THREE.DirectionalLight(0xffffff, 1);
+dl.position.set(2, 5, 3).normalize();
+scene.add(dl);
 //-------- ----------
 // RENDER
 //-------- ----------
--- views/forpost/threejs-fog/todo.md
+++ views/forpost/threejs-fog/todo.md
@@ -9,10 +9,10 @@ check it: https://www.youtube.com/watch?v=k1zGz55EqfU
 ## () section on material fog property
 * () have a section on the material fog boolean
 
-## () s1-2-basic-exp fog
-* () have an example that will help show the difference between the two fog constructors
+## ( done 05/07/2023 ) s1-2-basic-exp fog
+* (done) have an example that will help show the difference between the two fog constructors
 
-## (done 05/07/2023 ) rename static section demo
+## ( done 05/07/2023 ) rename static section demo
 * (done) rename s1-1-static-scene to s1-1-basic-linear
 * (done) use the basic material for this demo
 
