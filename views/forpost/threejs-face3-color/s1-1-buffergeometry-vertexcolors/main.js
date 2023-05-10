8588ec9f5c950ec05d114f4ca12256dded46def8
dustinpfister <dustin.pfister@gmail.com>
2023-05-10 12:05:37 -0400

    renamed demos for threejs-face3-color

--- views/forpost/threejs-face3-color/s1-1-buffergeometry-vertexcolors/main.js
+++ views/forpost/threejs-face3-color/s1-1-buffergeometry-vertexcolors/main.js
@@ -1,18 +1,16 @@
 //-------- ----------
 // SCENE
 //-------- ----------
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
-camera.position.set(2, 2, 2);
-camera.lookAt(0, 0, 0);
-var renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer() : new THREE.WebGLRenderer();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
+const renderer = THREE.WebGL1Renderer ? new THREE.WebGL1Renderer() : new THREE.WebGLRenderer();
 renderer.setSize(640, 480, false);
 ( document.getElementById('demo') || document.body ).appendChild(renderer.domElement);
 //-------- ----------
 // BUFFER GEOMERTY
 //-------- ----------
-var geometry = new THREE.BufferGeometry();
-var vertices = new Float32Array([
+const geometry = new THREE.BufferGeometry();
+const vertices = new Float32Array([
     -1, 0, 0,
      1, 0, 0,
      1, 1.25, 0
@@ -22,27 +20,28 @@ geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
 // COLORS
 //-------- ----------
 // add a colors prop to the geometry
-var colors = new Uint8Array([
-            255, 0, 0,
-            0, 255, 0,
-            0, 0, 255,
-            0, 0, 255,
-            0, 255, 0,
-            255, 0, 0,
-        ]);
+const colors = new Uint8Array([
+    255, 0, 0,
+    0, 255, 0,
+    0, 0, 255,
+    0, 0, 255,
+    0, 255, 0,
+    255, 0, 0,
+]);
 // Don't forget to normalize the array! (third param = true)
 geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3, true));
 //-------- ----------
 // MESH
 //-------- ----------
-// MESH that uses the vertex colors
-var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
-            side: THREE.DoubleSide,
-            vertexColors: true,
-            side: THREE.DoubleSide
-        }));
+const mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+    side: THREE.DoubleSide,
+    vertexColors: true,
+    side: THREE.DoubleSide
+}));
 scene.add(mesh);
 //-------- ----------
 // RENDERER
 //-------- ----------
+camera.position.set(2, 2, 2);
+camera.lookAt(0, 0, 0);
 renderer.render(scene, camera);
--- views/forpost/threejs-face3-color/todo.md
+++ views/forpost/threejs-face3-color/todo.md
@@ -1,5 +1,10 @@
 # threejs-face3-color todo list
 
+## ( done 05/10/2023 ) s1-1-buffergeometry-vertexcolors
+* (done) make the buffer geometry demo the s1 section
+* (done) update style
+* (done) rename older face3 demos
+
 ## ( done 11/30/2022 ) update examples
 * (done) update examples to work with new code standards
 
