10effda56ab7bdcf4de8fd8f244d5bd5ed6df412
dustinpfister <dustin.pfister@gmail.com>
2023-05-30 14:16:48 -0400

    main js code for examples-curve-nellipse r152 demo

--- views/demos/r152/examples-curve-nellipse/main.js
+++ views/demos/r152/examples-curve-nellipse/main.js
@@ -11,18 +11,30 @@ const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 (document.querySelector('#demo') || document.body).appendChild(renderer.domElement);
-// ---------- ----------
-// CURVE
-// ---------- ----------
-const curve = new MultifocalEllipseCurve();
 scene.add( new THREE.GridHelper( 10,10 ) );
-const points1 = new THREE.Points(
-    new THREE.BufferGeometry().setFromPoints( curve.getPoints(50) ),
-    new THREE.PointsMaterial({ size: 0.4 }));
-scene.add(points1);
+// ---------- ----------
+// CURVES
+// ---------- ----------
+const foci = [ 
+    new THREE.Vector2(-0.200,  0.15), 
+    new THREE.Vector2( 2.000, -0.10),
+    new THREE.Vector2( 2.100,  0.00)
+     ];
+let i = 0;
+const count = 12;
+while(i < count){
+    const radius = 0.125 + 0.70 * ( i / count);
+    const curve = new MultifocalEllipseCurve(foci, radius);
+    const line = new THREE.Line(
+        new THREE.BufferGeometry().setFromPoints( curve.getPoints(50) ),
+        new THREE.LineBasicMaterial({ linewidth: 4 })
+    );
+    scene.add(line);
+    i += 1;
+}
 // ---------- ----------
 // RENDER
 // ---------- ----------
-camera.position.set(8, 8, 8);
+camera.position.set(0, 1, 8);
 camera.lookAt(0, 0, 0);
 renderer.render(scene, camera);
