bea2dd813cec21092768b172e5e2b7d0a8a84c67
dustinpfister <dustin.pfister@gmail.com>
2023-06-04 11:53:20 -0400

    s4-3-curves-update demo for threejs-buffer-geometry-attributes-position

---
+++ views/forpost/threejs-buffer-geometry-attributes-position/s4-3-curves-update/main.js
@@ -0,0 +1,106 @@
+//-------- ----------
+// SCENE, CAMERA, RENDERER, GRID
+//-------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 4 / 3, 0.5, 1000);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+scene.add( new THREE.GridHelper(10, 10));
+//-------- ----------
+// HELPERS
+//-------- ----------
+// update a curve by radian, radius, and a control vector
+const updateCurve = (curve, degree = 0, radius = 3, v_control = new THREE.Vector3(0,2,0) ) => {
+    const e = new THREE.Euler();
+    const radian = Math.PI / 180 * degree;
+    e.y = radian;
+    curve.v0.set(1, 0, 0).applyEuler(e).multiplyScalar(radius);
+    curve.v1.copy(v_control);
+    e.y = radian + Math.PI;
+    curve.v2.set(1, 0, 0).applyEuler(e).multiplyScalar(radius);
+};
+// create and return a curve ( HREE.QuadraticBezierCurve3 )
+const createCurve = (degree = 45, radius = 3, y = 5) => {
+    const curve = new THREE.QuadraticBezierCurve3();
+    updateCurve(curve, Math.PI / 180 * degree, radius, new THREE.Vector3(0, y, 0) );
+    return curve;
+};
+// create a curve geometry
+const createCurveGeometry = ( curve = createCurve() ) => {
+    const geometry = new THREE.BufferGeometry().setFromPoints( curve.getPoints(25) );
+    geometry.userData.curve = curve;
+    return geometry;
+};
+// update a curve geometry to the given curve, or userData.curve of there
+const updateCurveGeometry = (geometry, curve) => {
+    curve = curve || geometry.userData.curve || createCurve();
+    const att_pos = geometry.getAttribute('position');
+    let i = 0;
+    while(i < att_pos.count){
+       const v = curve.getPoint(i / att_pos.count);
+       att_pos.setXYZ(i, v.x, v.y, v.z);
+       i += 1;
+    }
+    att_pos.needsUpdate = true;
+};
+const getBiasAlpha = (a1, count) => {
+    let a = 1 - Math.abs(0.5 - (a1 * count % 1) ) / 0.5;
+    return a;
+};
+//-------- ----------
+// GEOMETRY
+//-------- ----------
+const geometry = createCurveGeometry();
+
+//-------- ----------
+// POINTS
+//-------- ----------
+const points = new THREE.Points(geometry, new THREE.PointsMaterial({size: 0.5, color: 0x00ff00 }));
+scene.add(points);
+// ---------- ----------
+// ANIMATION LOOP
+// ---------- ----------
+camera.position.set(5, 5, 8);
+camera.lookAt(points.position);
+const sm = {
+   FPS_UPDATE: 30,    // FPS RATE
+   FRAME_MAX: 450,
+   secs: 0,
+   frame: 0,         // 30 / 450
+   tick: 0,          //  1 / 450 ( about 1 FPS then )
+   now: new Date(),
+   lt: new Date()
+};
+const update = function(sm){
+    const a1 = sm.frame / sm.FRAME_MAX;
+    const a2 = getBiasAlpha(a1, 3);
+    const a3 = getBiasAlpha(a1, 12);
+    const a4 = getBiasAlpha(a1, 5);
+    const a5 = getBiasAlpha(a1, 1);
+
+    const deg = 360 * a1;
+    const radius = 7 - 3.5 * a5;
+
+    const x = 5 - 10 * a2;
+    const y = 8 - 16 * a3;
+    const z = -5 + 10 * a4
+    const v_control = new THREE.Vector3(x, y, z);
+    updateCurve(geometry.userData.curve, deg, radius, v_control);
+    updateCurveGeometry(geometry);
+
+};
+const loop = () => {
+    sm.now = new Date();
+    sm.secs = (sm.now - sm.lt) / 1000;
+    requestAnimationFrame(loop);
+    if(sm.secs > 1 / sm.FPS_UPDATE){
+        // update, render
+        update(sm);
+        renderer.render(scene, camera);
+        // step frame
+        sm.frame = ( sm.frame += 1 ) % sm.FRAME_MAX;
+        sm.lt = sm.now;
+    }
+};
+loop();
--- views/forpost/threejs-buffer-geometry-attributes-position/todo.md
+++ views/forpost/threejs-buffer-geometry-attributes-position/todo.md
@@ -3,11 +3,11 @@
 ## () sx-1-extends-lathegeometry
 * () start a section on the subject of extending built in geometry, starting with lathe geometry
 
-## () s4-3-curves-update
-* () curves demo in which I update the curve and geometry over time
-* () can do so with a single quadratic curve
-* () move the start and end points around in a circle
-* () set setfrompoints to create the geo, but directly mutate the position attribute to update
+## ( done 06/04/2023 ) s4-3-curves-update
+* (done) curves demo in which I update the curve and geometry over time
+* (done) can do so with a single quadratic curve
+* (done) move the start and end points around in a circle
+* (done) set setfrompoints to create the geo, but directly mutate the position attribute to update
 
 ## ( done 06/04/2023 ) s4-1-curves-setfrompoints
 * (done) rename s1-1-curves t0 s1-2-curves-setattribute
