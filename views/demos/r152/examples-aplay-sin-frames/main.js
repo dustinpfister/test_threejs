6615a8e89921aae6efb9c2095794473c50b6fd26
dustinpfister <dustin.pfister@gmail.com>
2023-06-15 14:06:58 -0400

    examples-aplay-sin-frames r152 demo looking good so far

--- views/demos/r152/examples-aplay-sin-frames/main.js
+++ views/demos/r152/examples-aplay-sin-frames/main.js
@@ -12,59 +12,75 @@ const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 (document.querySelector('#demo') || document.body).appendChild(renderer.domElement);
 // ---------- ----------
-// OBJECTS
+// CURVE
+// ---------- ----------
+const v_start = new THREE.Vector3(-5, -5, 5);
+const v_end = new THREE.Vector3(-5, 5, -5);
+const v_c1 = v_start.clone().lerp(v_end, 0.25).add( new THREE.Vector3(10, 5, 0) );
+const v_c2 = v_start.clone().lerp(v_end, 0.75).add( new THREE.Vector3(10, -5, 0) );
+const curve = new THREE.CubicBezierCurve3(v_start, v_c1, v_c2, v_end);
+// ---------- ----------
+// SCENE CHILD OBJECTS
 // ---------- ----------
 scene.add( new THREE.GridHelper( 10,10 ) );
-const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshNormalMaterial());
-scene.add(mesh);
-// ---------- ----------
-// CONTROLS
-// ---------- ----------
-let controls = null;
-try {
-    controls = new OrbitControls(camera, renderer.domElement);
-}catch(e){
-    console.warn('OrbitControls was not imported');
-}
+const mesh1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 30, 30),
+    new THREE.MeshNormalMaterial({ transparent: true }));
+scene.add(mesh1);
 // ---------- ----------
 // ANIMATION LOOP
 // ---------- ----------
-camera.position.set(1.25, 1.25, 1.25);
+camera.position.set(10, 5, 10);
 camera.lookAt(0, 0, 0);
-// constant values and state for main app loop
-const FPS_UPDATE = 20, // fps rate to update ( low fps for low CPU use, but choppy video )
-FPS_MOVEMENT = 30,     // fps rate to move object by that is independent of frame update rate
-FRAME_MAX = 120,
-CLOCK = new THREE.Clock(true); // USING THREE.Clock in place of new Date() or Date.now()
+
+const FPS_UPDATE = 30, // 30 frames per second
+FRAME_MAX = 90,
+CLOCK = new THREE.Clock(true); 
 let secs = 0,
 frame = 0,
 lt = CLOCK.getElapsedTime();
+
+const data = {
+    bytes_per_frame: '1400',
+    frames: []
+};
+
 // update
 const update = (frame, frameMax) => {
-    // alpha values come up all the time. I have started getting in the habbit of having
-    // a Naming pattern for them like this:
     const a1 = frame / frameMax;
-    const a2 = 1 - Math.abs(0.5 - a1 ) / 0.5;
-    const a3 = THREE.MathUtils.smoothstep(a2, 0, 1);
-    // update content
-    const degree = 360 * a3
-    mesh.rotation.x = THREE.MathUtils.degToRad(degree);
+    //const a2 = 1 - Math.abs(0.5 - a1 ) / 0.5;
+    //const a3 = THREE.MathUtils.smoothstep(a2, 0, 1);
+    mesh1.position.copy( curve.getPoint(a1) );
+
+    // use x for smaples per wavecount
+    const a_x = ( mesh1.position.x + 5 ) / 10;
+    const samps_per_wavecount = 1 + Math.round( 499 * a_x);
+
+    // using z axis for amp argumnet
+    const a_z = ( mesh1.position.z + 5 ) / 10;
+    const amp = Math.round(5 + 35 * a_z);
+
+    // (z also effects opacity of mesh )
+    mesh1.material.opacity = a_z;
+
+    // might use y for wave count then
+    const a_y = ( mesh1.position.y + 5 ) / 10;
+    const wavecount = 1 + Math.round(19 * a_y);
+
+    // set arguments for this frame
+    data.frames[frame] = [samps_per_wavecount, amp, wavecount];
 };
 // loop
 const loop = () => {
-    const now = CLOCK.getElapsedTime(),
-    secs = (now - lt);
-    requestAnimationFrame(loop);
-    if(secs > 1 / FPS_UPDATE){
-        // update, render
+    if(frame === FRAME_MAX){
+        console.log( JSON.stringify(data) );
+    }
+    if(frame < FRAME_MAX){
+        requestAnimationFrame(loop);
         update( Math.floor(frame), FRAME_MAX);
         renderer.render(scene, camera);
-        // step frame
-        frame += FPS_MOVEMENT * secs;
-        frame %= FRAME_MAX;
-        lt = now;
+        frame += 1;
     }
+
 };
 loop();
