d8d691155796874f4395cb589f3a328fda517537
dustinpfister <dustin.pfister@gmail.com>
2023-06-19 11:31:30 -0400

    s2-ani-target updated to r146 style for threejs-spotlights

---
+++ views/forpost/threejs-spotlights/s2-ani-target/main.js
@@ -0,0 +1,78 @@
+// ---------- ----------
+// SCENE, CAMERA, RENDERER
+// ---------- ----------
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#0f0f0f');
+const camera = new THREE.PerspectiveCamera(50, 320 / 240, 0.1, 1000);
+const renderer = new THREE.WebGL1Renderer();
+(document.getElementById('demo') || document.body).appendChild(renderer.domElement);
+renderer.setSize(640, 480, false);
+// ---------- ----------
+// SPOTLIGHT
+// ---------- ----------
+const color = new THREE.Color('white'),
+intensity = 1,
+distance = 30,
+angle = Math.PI * 0.05,
+penumbra = 0.25,
+decay = 0.5;
+const spotLight = new THREE.SpotLight(color, intensity, distance, angle, penumbra, decay);
+spotLight.position.set(8, 8, 0);
+scene.add(spotLight);
+scene.add( new THREE.AmbientLight(0xffffff, 0.07));
+// ---------- ----------
+// MESH OBJECTS
+// ---------- ----------
+const mesh_cube = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshStandardMaterial({
+            color: 0xff0000
+        }));
+mesh_cube.position.set(0, 1, 0);
+scene.add(mesh_cube);
+const mesh_floor = new THREE.Mesh(
+        new THREE.BoxGeometry(10, 1, 10),
+        new THREE.MeshStandardMaterial({
+            color: 0x008800
+        }));
+scene.add(mesh_floor);
+// ---------- ----------
+// CALLING RENDER OF RENDERER IN AN ANIMATION LOOP
+// ---------- ----------
+camera.position.set(5, 8, 12);
+camera.lookAt(0,0,0);
+// APP LOOP
+let secs = 0,
+fps_update = 30,   // fps rate to update ( low fps for low CPU use, but choppy video )
+fps_movement = 60, // fps rate to move camera
+frame = 0,
+frameMax = 600,
+lt = new Date(),
+spotTarget = new THREE.Object3D(); // spotlight target
+spotLight.target = spotTarget; // set spotLight target for spotLight
+scene.add(spotTarget); // add spotLight target to the scene
+// update
+const update = function(){
+    const per = Math.round(frame) / frameMax,
+    bias = 1 - Math.abs(0.5 - per) / 0.5,
+    radian = Math.PI * 2 * per,
+    x = Math.cos(radian) * 8, 
+    y = 10,
+    z = Math.sin(radian) * 8;
+    spotLight.position.set(x, y, z);
+    spotTarget.position.set(0, 0, -3 + 6 * bias);
+};
+// loop
+const loop = function () {
+    const now = new Date(),
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if(secs > 1 / fps_update){
+        update();
+        renderer.render(scene, camera);
+        frame += fps_movement * secs;
+        frame %= frameMax;
+        lt = now;
+    }
+};
+loop();
--- views/forpost/threejs-spotlights/todo.md
+++ views/forpost/threejs-spotlights/todo.md
@@ -1,7 +1,23 @@
 # threejs-spotlights todo list
 
-## () - transparent code effect example
-* I would like to have another example that adds a transparent code effect to the spot light
+<!-- COMBINE ALL ANI demos to new LOOP SECTION-->
+## ( ) Rename dmeo folders
+* () have an s2-1-shadow demo folder
+* () have an s3-1-helper demo folder
+* () s2-ani-target to s4-1-loop-target
+* () s4-ani-values to s4-2-loop-values
+* () s6-ani-helper to s4-3-loop-helper
+
+<!-- UPDATE TO R146 STYLE-->
+
+## ( ) update all demos to r146 style
+* () update all demos demos to r146 style
+* () move js files to root of demo folders
+
+<!-- DONE -->
+
+## ( done 06/19/2023 ) - s2-ani-target - updated to r146 style
+* (done) updated s2-ani-target to r146 style
 
 ## ( done 01/11/2023 ) update s1-basic
 * (done) Updated example to use r146
