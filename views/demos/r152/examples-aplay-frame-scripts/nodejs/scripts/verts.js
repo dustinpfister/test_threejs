e1fce63cb452ce9b047fc174bc249b5b6df0d35d
dustinpfister <dustin.pfister@gmail.com>
2023-06-19 18:50:25 -0400

    verts

--- views/demos/r152/examples-aplay-frame-scripts/nodejs/scripts/verts.js
+++ views/demos/r152/examples-aplay-frame-scripts/nodejs/scripts/verts.js
@@ -1,4 +1,26 @@
 module.exports = (frameData, a_bytes, i_byte, bytes_per_frame, data) => {
-    const n = 128;
+
+    let i = 0;
+    const len = frameData.length / 3;
+
+    let xMean = 0;
+    let yMean = 0;
+    while(i < len){
+
+        const s = i * 3;
+        const x = frameData[i];
+        const y = frameData[i + 1];
+        const z = frameData[i + 2];
+
+        xMean += x;
+        yMean += y;
+
+        i += 1;
+    }
+
+    xMean /= len;
+    yMean /= len;
+
+    const n = Math.round( 128 + xMean * 32);
     return n;
 };
