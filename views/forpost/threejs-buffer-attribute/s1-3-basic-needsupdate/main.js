6d6604268aba6abd3ea9073dc4434a9627f8f6f0
dustinpfister <dustin.pfister@gmail.com>
2023-06-21 14:35:00 -0400

    started a new s1-2-basic-needsupdate demo for threejs-buffer-attribute

--- views/forpost/threejs-buffer-attribute/s1-3-basic-needsupdate/main.js
+++ views/forpost/threejs-buffer-attribute/s1-3-basic-needsupdate/main.js
@@ -13,16 +13,8 @@ renderer.setSize(640, 480, false);
 // ---------- ----------
 // GEOMETRY
 // ---------- ----------
-const geometry = new THREE.SphereGeometry(0.25, 30, 30);
+const geometry = new THREE.BoxGeometry(1, 1, 1, 4, 4, 4);
 const att_pos = geometry.getAttribute('position');
-let i = 0;
-while(i < att_pos.count){
-    const v = new THREE.Vector3().fromBufferAttribute( att_pos, i );
-    v.normalize().multiplyScalar( 0.25 + 0.75 * Math.random() );
-    att_pos.setXYZ(i,  v.x, v.y, v.z );
-    i += 1;
-}
-
 // ---------- ----------
 // SCENE CHILD OBJECTS
 // ---------- ----------
@@ -30,8 +22,30 @@ scene.add( new THREE.GridHelper(10, 10) );
 const points1 = new THREE.Points( geometry, new THREE.PointsMaterial({ size: 0.1, color: 0x00ff00 }));
 scene.add(points1);
 // ---------- ----------
-// RENDER
+// LOOP
 // ---------- ----------
 camera.position.set( 1.5, 1, 1.7 );
 camera.lookAt(0, 0, 0);
-renderer.render(scene, camera);
+const update = () => {
+    let i = 0;
+    while(i < att_pos.count){
+        const v = new THREE.Vector3().fromBufferAttribute( att_pos, i );
+        v.normalize().multiplyScalar( 0.25 + 0.75 * Math.random() );
+        att_pos.setXYZ(i,  v.x, v.y, v.z );
+        att_pos.needsUpdate = true;
+        i += 1;
+    }
+};
+const FPS = 4;
+let lt = new Date();
+const loop = () => {
+    const now = new Date();
+    const secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    if(secs >= 1 / FPS){
+        update();
+        renderer.render(scene, camera);
+        lt = now;
+    }
+};
+loop();
--- views/forpost/threejs-buffer-attribute/todo.md
+++ views/forpost/threejs-buffer-attribute/todo.md
@@ -8,33 +8,33 @@
 
 <!--  NORMAL SECTION -->
 
-## s3-1-normal-helper
+## () - s3-1-normal-helper
 * () I will want to use the vertex normals helper for this demo
 * () still use compute vertex normals to create the normal attribute
 
-## s3-2-normal-custom
+## () - s3-2-normal-custom
 * () same as first, but now I will be making a custom normal attribute from the gorund up
 
-## s3-3-normal-flip
+## () - s3-3-normal-flip
 * () demo where I flip just some of the normals of a box geometry
 * () I can then move the camera in and out of the box
 
 <!--  POSITION SECTION -->
 
-## s2-1-position-mesh
+## () - s2-1-position-mesh
 * () create a very simple geometry that is just a few triangles
 * () use compute vertex normals
 * () use the normal material
 
-## s2-2-position-index
+## () - s2-2-position-index
 * () same code as the first position demo, but now I will want to add an index
 
 <!-- BASIC SECTION -->
 
-## 31-3-basic-needsupdate
-* () loop demo of the needs update prop
-
 <!-- Done -->
+
+## ( done 06/21/2023 ) - s1-3-basic-needsupdate
+* (done) loop demo of the needs update prop
 
 ## ( done 06/21/2023 ) - s1-2-basic-vector3-from-attribute
 * (done) basic example of the from attribute method of the Vector3 class
