8f7d66a6f6153c0e426b313e9866e9f174768203
dustinpfister <dustin.pfister@gmail.com>
2023-07-03 14:25:16 -0400

    examples-tri12-scene r152 demo started

--- views/demos/r152/examples-tri12-scene/main.js
+++ views/demos/r152/examples-tri12-scene/main.js
@@ -13,93 +13,55 @@ const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 ( document.getElementById('demo')  || document.body ).appendChild(renderer.domElement);
 //-------- ----------
+// HELPER
+//-------- ----------
+const loadBufferGeometryJSON = ( urls = [], w = 2, scale = 5, material = new THREE.MeshNormalMaterial() ) => {
+    const scene_source = new THREE.Scene();
+    const onBuffLoad =  (geometry, i) => {
+        const x = i % w;
+        const z = Math.floor( i / w);
+        const mesh = new THREE.Mesh( geometry, material);
+        mesh.name = 'buffer_source_' + i;
+        mesh.position.set(x, 0, z).multiplyScalar(scale);
+        scene_source.add(mesh);
+    };
+    const onBuffProgress =  (geometry) => {};
+    return new Promise( ( resolve, reject ) => {
+        const manager = new THREE.LoadingManager();
+        manager.onLoad = () => {
+            resolve(scene_source);
+        };
+        const onBuffError =  (err) => {
+           reject(err);
+        };
+        const loader = new THREE.BufferGeometryLoader(manager);
+        urls.forEach( (url, index) => {
+            loader.load(url, (geometry) => { onBuffLoad(geometry, index) }, onBuffProgress, onBuffError);
+        });
+    });
+};
+//-------- ----------
 // GRID
 //-------- ----------
-//scene.add( new THREE.GridHelper(10, 10) );
+scene.add( new THREE.GridHelper(10, 10) );
 //-------- ----------
-// ORBIT CONTROLS
+// LOAD GEOMETRY
 //-------- ----------
-new OrbitControls(camera, renderer.domElement);
-//-------- ----------
-// CANVAS TEXTURE
-//-------- ----------
-const canvas = document.createElement('canvas');
-canvas.width = 32;
-canvas.height = 32;
-const ctx = canvas.getContext('2d');
-const gSize = 8;
-const len = gSize * gSize;
-const color = new THREE.Color(0,0,0);
-const s = canvas.width / gSize;
-let i = 0;
-while(i < len){
-    const x = i % gSize;
-    const y = Math.floor( i / gSize );
-    const r = 0.25 + 0.75 * Math.random();
-    const g = Math.random() * 0.05;
-    const b = Math.random() * 0.05;
-    color.setRGB(r, g, b);
-    ctx.fillStyle = color.getStyle();
-    ctx.fillRect(x * s, y * s, s, s);
-    i += 1;
-}
-// texture from canvas
-const texture = new THREE.CanvasTexture(canvas);
-//-------- ----------
-// Loader
-//-------- ----------
-camera.position.set(3, 3, 3);
+camera.position.set(-8, 8, 12);
 camera.lookAt(0, 0.5, 0);
-const loader = new THREE.BufferGeometryLoader();
-// load a resource
-loader.load(
-    // resource URL
+const urls = [
     '/json/tri12-bufferfly/set1/0.json',
-    // onLoad callback
-    (geometry) => {
-        // add mesh
-        const mesh = new THREE.Mesh(
-            geometry,
-            new THREE.MeshBasicMaterial({
-                vertexColors: false, 
-                side: THREE.DoubleSide,
-                map: texture
-            })
-        );
-        scene.add(mesh);
-        const pos_att = geometry.getAttribute('position');
-        const pos_att_home = pos_att.clone();
-        const updateByMorph = (geometry, pos_att_home, alpha = 1, index = 0) => {
-            const att_morph = geometry.morphAttributes.position[index];
-            pos_att.array = pos_att_home.array.map( (n, i) => {
-                return n + (att_morph.array[i] * alpha);
-            });
-            pos_att.needsUpdate = true;
-        };
-        updateByMorph(geometry, pos_att_home, 1, 0);
-        // vertex helper
-        const helper = new VertexNormalsHelper(mesh, 1, 0x00ff00);
-        scene.add(helper);
-        // loop
-        let frame = 0;
-        const frame_max = 30 * 5;
-        const loop = () => {
-            requestAnimationFrame(loop);
-            const a1 = frame / frame_max;
-            const a_wings = 1 - Math.abs( 0.5 - (a1 * 8 % 1) ) / 0.5;
-            const a_bounce = a1 * 2 % 1;
-            const a_rock = Math.sin( Math.PI * 2 * a1 ) / Math.PI;
-            //mesh.morphTargetInfluences[ 0 ] = a_wings;
-            updateByMorph(geometry, pos_att_home, a_wings, 0);
-            geometry.computeVertexNormals();
-            helper.update();
-            // object3d position / rotation
-            mesh.position.y = Math.sin( Math.PI * 2 * a_bounce ) * 0.5;
-            mesh.rotation.y = Math.PI / 180 * ( 90 * a_rock );
-            frame += 1;
-            frame %= frame_max;
-            renderer.render(scene, camera);
-        };
-        loop();
-    }
-);
+    '/json/tri12-trees/deciduous-one-full/0.json',
+    '/json/tri12-trees/deciduous-one-full/1.json',
+    '/json/tri12-trees/deciduous-one-full/2.json',
+    '/json/tri12-trees/deciduous-one-full/3.json',
+];
+const material = new THREE.MeshBasicMaterial({
+    side: THREE.DoubleSide,
+    vertexColors: true
+});
+loadBufferGeometryJSON(urls, 2, 5, material)
+.then(( scene_source )=>{
+    scene.add(scene_source);
+    renderer.render(scene, camera);
+});
