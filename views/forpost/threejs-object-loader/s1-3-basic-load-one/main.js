8b6a57b7226b7779eb0e149eb976326ad9913a8c
dustinpfister <dustin.pfister@gmail.com>
2023-07-20 14:20:02 -0400

    white space, commnets, url fix for s1-3-basic-load-one for threejs-object-loader

--- views/forpost/threejs-object-loader/s1-3-basic-load-one/main.js
+++ views/forpost/threejs-object-loader/s1-3-basic-load-one/main.js
@@ -9,17 +9,14 @@ const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 (document.querySelector('#demo') || document.body).appendChild(renderer.domElement);
-
+// ---------- ----------
+// LOAD, RENDER
+// ---------- ----------
 const loader = new THREE.ObjectLoader();
-const url = 'http://localhost:8030/forpost/threejs-object-loader/s1-3-basic-load-one/scene.json';
+const url = '/forpost/threejs-object-loader/s1-3-basic-load-one/scene.json';
 const on_done = (obj) => {
     camera.position.set(2, 2, 2);
     camera.lookAt(0, 0, 0);
     renderer.render(obj, camera);
 };
 loader.load(url, on_done);
-
-// ---------- ----------
-// RENDER
-// ---------- ----------
-
