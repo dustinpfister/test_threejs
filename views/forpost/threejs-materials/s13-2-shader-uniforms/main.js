518b2be49d685d9a5fa6acc5569874fbb2b0218d
dustinpfister <dustin.pfister@gmail.com>
2023-07-26 13:52:22 -0400

    more than one mesh for s13-2-shader-uniforms demo for threejs-materials

--- views/forpost/threejs-materials/s13-2-shader-uniforms/main.js
+++ views/forpost/threejs-materials/s13-2-shader-uniforms/main.js
@@ -15,27 +15,31 @@ const material1 = new THREE.ShaderMaterial({
         opacity: { value: 1 }
     },
     vertexShader: `
-        varying vec2 vHighPrecisionZW;
         void main() {
             gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
         }`,
     fragmentShader: `
         uniform vec3 baseColor;
         uniform float opacity;
-        uniform float depthAlpha;
-        varying vec2 vHighPrecisionZW;
         void main() {
-            float d = (1.0 - gl_FragCoord.z);
+            float d = 1.0 - gl_FragCoord.z;
             gl_FragColor = vec4( baseColor * d, opacity );
         }`
 });
 // ---------- ----------
 // GEOMETRY, MESH
 // ---------- ----------
-const geometry = new THREE.BoxGeometry( 1.5, 1.5, 1.5);
-const mesh = new THREE.Mesh(geometry, material1);
-mesh.rotation.y = Math.PI / 180 * 20;
-scene.add(mesh);
+const geometry = new THREE.BoxGeometry( 1, 1, 1);
+const mesh1 = new THREE.Mesh(geometry, material1);
+mesh1.rotation.y = Math.PI / 180 * 20;
+scene.add(mesh1);
+const material2 = material1.clone();
+material2.uniforms.baseColor.value = new THREE.Color('lime');
+console.log(material2);
+const mesh2 = new THREE.Mesh(geometry, material2);
+mesh2.position.set(-1, 0, 1)
+mesh2.rotation.y = Math.PI / 180 * 20;
+scene.add(mesh2);
 // ---------- ----------
 // RENDER
 // ---------- ----------
