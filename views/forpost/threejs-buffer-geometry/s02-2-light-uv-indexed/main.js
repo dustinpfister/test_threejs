e3861f70d5e8cc90a6051160a518e8f4f82b0a46
dustinpfister <dustin.pfister@gmail.com>
2023-08-22 12:06:30 -0400

    s02-2-light-uv-indexed done with it for now

--- views/forpost/threejs-buffer-geometry/s02-2-light-uv-indexed/main.js
+++ views/forpost/threejs-buffer-geometry/s02-2-light-uv-indexed/main.js
@@ -7,8 +7,27 @@ const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(640, 480, false);
 (document.getElementById('demo') || document.body).appendChild(renderer.domElement);
 //-------- ----------
-// MATERIALS
+// TEXTURES / MATERIALS
 //-------- ----------
+const data_normalmap = [
+    [1,1,1],[1,1,1],[1,1,1],[1,1,1],
+    [9,1,1],[1,1,1],[1,1,1],[1,1,1],
+    [9,1,1],[9,9,9],[1,1,1],[1,1,1],
+    [9,2,0],[9,1,1],[9,1,1],[1,1,1]
+].map( (ci_array) => {
+    const r = Math.round( 255 * (ci_array[0] / 9) );
+    const g = Math.round( 255 * (ci_array[0] / 9) );
+    const b = Math.round( 255 * (ci_array[0] / 9) );
+    return [ r, g, b, 255];
+}).flat();
+
+const texture_normal = new THREE.DataTexture( new Uint8Array( data_normalmap ), 4,  4 );
+texture_normal.needsUpdate = true;
+const material_nm = new THREE.MeshPhongMaterial({
+    color: 0xff0000,
+    normalMap: texture_normal,
+    side: THREE.FrontSide
+});
 const material = new THREE.MeshPhongMaterial({
     color: 0xff0000,
     side: THREE.FrontSide
@@ -26,6 +45,14 @@ const vertices = new Float32Array([
 geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
 geometry.setIndex([0,1,2,1,3,2]);
 geometry.computeVertexNormals();
+const data_uv = new Float32Array([
+    0.00,  1.00,
+    0.00,  0.00,
+    1.00,  0.00,
+    1.00,  1.00,
+]);
+geometry.setAttribute('uv', new THREE.BufferAttribute(data_uv, 2));
+geometry.computeTangents();
 //-------- ----------
 // GEOMETRY - non-indexed
 //-------- ----------
@@ -35,7 +62,7 @@ geometry_ni.computeVertexNormals();
 // SCENE CHILD OBJECTS - GRID, MESH, AND LIGHTS
 //-------- ----------
 scene.add( new THREE.GridHelper(10, 10) );
-const mesh1 = new THREE.Mesh( geometry, material);
+const mesh1 = new THREE.Mesh( geometry, material_nm);
 mesh1.position.set(0,0.01,2);
 scene.add(mesh1);
 const mesh2 = new THREE.Mesh( geometry_ni, material);
