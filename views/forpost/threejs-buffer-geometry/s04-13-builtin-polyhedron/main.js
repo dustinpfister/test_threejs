14c580477a51ea7811709c5ebb393248b46e9de6
dustinpfister <dustin.pfister@gmail.com>
2023-09-09 06:31:42 -0400

    s04-13-buitin-polyheadron demo for threejs-buffer-geometry

---
+++ views/forpost/threejs-buffer-geometry/s04-13-builtin-polyhedron/main.js
@@ -0,0 +1,35 @@
+// ---------- ----------
+// SCENE, CAMERA, RENDERER
+// ---------- ----------
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, 32 / 24, 0.1, 1000);
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(640, 480, false);
+(document.querySelector('#demo') || document.body).appendChild(renderer.domElement);
+// ---------- ----------
+// GEOMETRY
+// ---------- ----------
+const vertices = [
+    1,1,1,    -1,1,1,    1,-1,1,   1,1,-1
+];
+const indices_of_faces = [
+    0,1,2,
+    0,3,1,
+    0,2,3,
+    1,2,3
+];
+
+const radius = 1;
+const detail = 3;
+const geometry = new THREE.PolyhedronGeometry(vertices, indices_of_faces, radius, detail);
+// ---------- ----------
+// OBJECTS
+// ---------- ----------
+const mesh1 = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({ side: THREE.DoubleSide, wireframe: true}) );
+scene.add(mesh1);
+// ---------- ----------
+// RENDER
+// ---------- ----------
+camera.position.set(2, 1, 2);
+camera.lookAt(0, 0.2, 0);
+renderer.render(scene, camera);
